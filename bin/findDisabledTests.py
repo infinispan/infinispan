445893eceb76a554d8096d87a489437b4567c953
Manik Surtani <manik@surtani.org>
2009-04-20 11:32:48 +0000

    Added version compatibility

--- bin/findDisabledTests.py
+++ bin/findDisabledTests.py
@@ -1,18 +1,26 @@
 #!/usr/bin/python
-from __future__ import with_statement
 import os
 import fnmatch
 import re
 import time
 import sys
 
-def assertValidPython():
-    if (sys.version < 2.5):
-        raise Error("Incompatible version of Python.  Need at least Python 2.5.0")
+### Crappy implementation of creating a Set from a List.  To cope with older Python versions
+def toSet(list):
+    tempDict = {}
+    for entry in list:
+        tempDict[entry] = "dummy"
+    return tempDict.keys()
 
+def getSearchPath(executable):
+    inBinDir = re.compile('^.*/?bin/.*.py')
+    if inBinDir.search(executable):
+        return "./"
+    else:
+        return "../"
 
-assertValidPython()
-
+def stripLeadingDots(filename):
+    return filename.strip('/. ')
 
 class GlobDirectoryWalker:
     # a forward iterator that traverses a directory tree
@@ -41,16 +49,6 @@ class GlobDirectoryWalker:
                 if fnmatch.fnmatch(file, self.pattern):
                     return fullname
 
-def getSearchPath(executable):
-    inBinDir = re.compile('^.*/?bin/.*.py')
-    if inBinDir.search(executable):
-        return "./"
-    else:
-        return "../"
-
-def stripLeadingDots(filename):
-    return filename.strip('/. ')
-
 ## Walk through all files that end with Test.java
 startTime = time.clock()
 disabledTestFiles = []
@@ -59,15 +57,18 @@ testAnnotationMatcher = re.compile('^\s*@Test')
 disabledMatcher = re.compile('enabled\s*=\s*false')
 
 for testFile in GlobDirectoryWalker(getSearchPath(sys.argv[0]), '*Test.java'):
-    with open(testFile) as tf:
+    tf = open(testFile)
+    try:
         for line in tf:
             if testAnnotationMatcher.search(line):
                 if disabledMatcher.search(line):
                     disabledTestFiles.append(testFile)
                 break
+    finally:
+        tf.close()
 
 print "Files containing disabled tests: \n"
-uniqueTests=set(disabledTestFiles)
+uniqueTests=toSet(disabledTestFiles)
 i=1
 for f in uniqueTests:
     print str(i) + ". " + stripLeadingDots(f)
