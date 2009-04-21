fc0246c30ab578f3577b0bca32bd05a01ed90ff4
Manik Surtani <manik@surtani.org>
2009-04-21 01:04:34 +0000

    Named test

--- core/src/test/java/org/infinispan/factories/ComponentRegistryDebugTest.java
+++ core/src/test/java/org/infinispan/factories/ComponentRegistryDebugTest.java
@@ -2,7 +2,7 @@ package org.infinispan.factories;
 
 import org.testng.annotations.Test;
 
-@Test(groups = "unit, functional")
+@Test(groups = "unit, functional", testName = "factories.ComponentRegistryDebugTest")
 public class ComponentRegistryDebugTest {
    public void testDebugStatus() {
       assert !AbstractComponentRegistry.DEBUG_DEPENDENCIES : "Please set DEBUG_DEPENDENCIES to false!!";
