5932da6f154e8837ff455c7351ce43ba093bcce1
Manik Surtani <manik@surtani.org>
2009-04-30 16:42:57 +0000

    Fixed typo, bug in GUI demo and added a windows bat file

--- gui-demo/src/main/resources/runGuiDemo.sh
+++ gui-demo/src/main/resources/runGuiDemo.sh
@@ -1,26 +1,34 @@
 #!/bin/bash
 
-## TODO: A Windows .cmd version of this script!
+curdir=`pwd`
+echo $curdir | grep "/bin$"
 
-CP=./etc
 
-for i in modules/core/*.jar ; do
+if [ "$?" -ne "0" ] ; then
+  basedir="./"
+else
+  basedir="../"
+fi
+
+CP=${basedir}etc
+
+for i in ${basedir}modules/core/*.jar ; do
    CP=${i}:${CP}
 done
 
-for i in modules/core/lib/*.jar ; do
+for i in ${basedir}modules/core/lib/*.jar ; do
    CP=${i}:${CP}
 done
 
-for i in modules/gui-demo/*.jar ; do
+for i in ${basedir}modules/gui-demo/*.jar ; do
    CP=${i}:${CP}
 done
 
-for i in modules/gui-demo/lib/*.jar ; do
+for i in ${basedir}modules/gui-demo/lib/*.jar ; do
    CP=${i}:${CP}
 done
 
-JVM_PARAMS="-Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=./etc/log4j.xml"
+JVM_PARAMS="-Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=${basedir}etc/log4j.xml"
 
 java -cp ${CP} ${JVM_PARAMS} org.infinispan.demo.InfinispanDemo &
 
