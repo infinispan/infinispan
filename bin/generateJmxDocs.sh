437546eaa057d2edd7a1e23751b3f4c58aae471c
Manik Surtani <manik@surtani.org>
2009-06-04 00:35:23 +0000

    Added tools to generate JMX documentation

--- bin/generateJmxDocs.sh
+++ bin/generateJmxDocs.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-
-
-if [[ $0 =~ bin/.*.sh ]] ; then
-   DIR_PREFIX=".."
-else
-   DIR_PREFIX="."
-fi
-
-cd ${DIR_PREFIX}
-mvn install -Pjmxdoc -Dmaven.test.skip.exec=true
+mvn install -Dmaven.test.skip.exec=true -Pjmxdoc
 
