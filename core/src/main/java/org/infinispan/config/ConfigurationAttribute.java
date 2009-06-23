6f6d7871a9d1d91546c57be91f2ee15e706bd6e4
Vladimir Blagojevic <vblagoje@redhat.com>
2009-06-23 13:40:14 +0000

    [ISPN-89] - Document all configuration options available in XML

---
+++ core/src/main/java/org/infinispan/config/ConfigurationAttribute.java
@@ -0,0 +1,47 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.infinispan.config;
+
+import java.lang.annotation.*;
+
+/**
+ *  Represents an attribute of any XML element from a valid Infinispan configuration file. 
+ * 
+ * @author Vladimir Blagojevic
+ * @version $Id: ConfigurationAttribute.java,v 1.5 2008/05/23 11:11:02 belaban Exp $
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target( { ElementType.METHOD })
+public @interface ConfigurationAttribute {
+
+   String containingElement();
+
+   String name();
+
+   String allowedValues() default "";
+
+   String defaultValue() default "";
+
+   String description() default "";
+
+}
