9abec831a3b060c7d2164526582b57e0e79eb7ff
Galder Zamarreno <galder@jboss.org>
2009-07-01 09:35:43 +0000

    Modified the script to be able to execute it several times from a central script. To do this, absolute paths needed to be resolved. On top of that, central script can provide specific VM params or system properties via exporting variables. The modified script is able to cope with this better. The script still works fine when executed individually.

--- gui-demo/src/main/resources/runGuiDemo.sh
+++ gui-demo/src/main/resources/runGuiDemo.sh
@@ -1,34 +1,32 @@
 #!/bin/bash
 
-curdir=`pwd`
-echo $curdir | grep "/bin$"
+DIRNAME=`dirname $0`
 
-
-if [ "$?" -ne "0" ] ; then
-  basedir="./"
-else
-  basedir="../"
+# Setup ISPN_HOME
+if [ "x$ISPN_HOME" = "x" ]; then
+    # get the full path (without any relative bits)
+    ISPN_HOME=`cd $DIRNAME/..; pwd`
 fi
+export ISPN_HOME
 
-CP=${basedir}etc
+CP=${CP}:${ISPN_HOME}/etc
 
-for i in ${basedir}modules/core/*.jar ; do
+for i in ${ISPN_HOME}/modules/core/*.jar ; do
    CP=${i}:${CP}
 done
 
-for i in ${basedir}modules/core/lib/*.jar ; do
+for i in ${ISPN_HOME}/modules/core/lib/*.jar ; do
    CP=${i}:${CP}
 done
 
-for i in ${basedir}modules/gui-demo/*.jar ; do
+for i in ${ISPN_HOME}/modules/gui-demo/*.jar ; do
    CP=${i}:${CP}
 done
 
-for i in ${basedir}modules/gui-demo/lib/*.jar ; do
+for i in ${ISPN_HOME}/modules/gui-demo/lib/*.jar ; do
    CP=${i}:${CP}
 done
 
-JVM_PARAMS="-Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=${basedir}etc/log4j.xml"
+JVM_PARAMS="${JVM_PARAMS} -Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:${ISPN_HOME}/etc/log4j.xml"
 
 java -cp ${CP} ${JVM_PARAMS} org.infinispan.demo.InfinispanDemo &
-
