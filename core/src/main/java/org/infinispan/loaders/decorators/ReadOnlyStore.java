683230dbce5796f353534ce74ead5fe673fde5a8
Galder Zamarreno <galder@jboss.org>
2009-07-02 08:56:40 +0000

    Added TRACE log messages so that it's more obvious when a read only cache store is in use.

--- core/src/main/java/org/infinispan/loaders/decorators/ReadOnlyStore.java
+++ core/src/main/java/org/infinispan/loaders/decorators/ReadOnlyStore.java
@@ -4,6 +4,8 @@ import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.loaders.CacheStore;
 import org.infinispan.loaders.modifications.Modification;
 import org.infinispan.transaction.xa.GlobalTransaction;
+import org.infinispan.util.logging.Log;
+import org.infinispan.util.logging.LogFactory;
 
 import java.io.ObjectInput;
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.List;
  * @since 4.0
  */
 public class ReadOnlyStore extends AbstractDelegatingStore {
+   private static final Log log = LogFactory.getLog(ReadOnlyStore.class);
 
    public ReadOnlyStore(CacheStore delegate) {
       super(delegate);
@@ -23,41 +26,42 @@ public class ReadOnlyStore extends AbstractDelegatingStore {
 
    @Override
    public void store(InternalCacheEntry ed) {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring store invocation"); 
    }
 
    @Override
    public void fromStream(ObjectInput inputStream) {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring writing contents of stream to store");
    }
 
    @Override
    public void clear() {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring clear invocation");
    }
 
    @Override
    public boolean remove(Object key) {
+      if (log.isTraceEnabled()) log.trace("Ignoring removal of key");
       return false;  // no-op
    }
 
    @Override
    public void purgeExpired() {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring purge expired invocation");
    }
 
    @Override
    public void commit(GlobalTransaction tx) {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring transactional commit call");
    }
 
    @Override
    public void rollback(GlobalTransaction tx) {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring transactional rollback call");
    }
 
    @Override
    public void prepare(List<? extends Modification> list, GlobalTransaction tx, boolean isOnePhase) {
-      // no-op
+      if (log.isTraceEnabled()) log.trace("Ignoring transactional prepare call");
    }
 }
