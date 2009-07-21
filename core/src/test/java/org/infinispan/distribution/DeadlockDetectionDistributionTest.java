f8b5d3d353c21bf4ab4d0dbe0fb00a048744a079
Mircea Markus <mircea.markus@gmail.com>
2009-07-21 13:54:09 +0000

    [ISPN-38] - (eager deadlock detection) - added UT for DIST

---
+++ core/src/test/java/org/infinispan/distribution/DeadlockDetectionDistributionTest.java
@@ -0,0 +1,24 @@
+package org.infinispan.distribution;
+
+import org.infinispan.config.Configuration;
+import org.infinispan.tx.DeadlockDetectionTest;
+import static org.testng.Assert.fail;
+import org.testng.annotations.Test;
+
+/**
+ * Test deadlock detection when cache is configured for invalidation.
+ *
+ * @author Mircea.Markus@jboss.com
+ */
+@Test(groups = "functional", enabled = false, testName = "tx.DeadlockDetectionDistributionTest")
+public class DeadlockDetectionDistributionTest extends DeadlockDetectionTest {
+
+   public DeadlockDetectionDistributionTest() {
+      cacheMode = Configuration.CacheMode.DIST_SYNC;
+   }
+
+
+   public void testDeadlockDetectedTwoTransactions() throws Exception {
+      fail("This test should be updated to make sure tx replicate on opposite nodes");
+   }
+}
