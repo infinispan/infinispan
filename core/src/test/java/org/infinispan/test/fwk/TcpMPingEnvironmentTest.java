741daaa103bb908bb1c981a96b7848a17e13af4e
Mircea Markus <mircea.markus@gmail.com>
2009-08-06 19:25:44 +0000

    added test to check weather TCP+MPING works in this environment

---
+++ core/src/test/java/org/infinispan/test/fwk/TcpMPingEnvironmentTest.java
@@ -0,0 +1,96 @@
+package org.infinispan.test.fwk;
+
+import org.jgroups.JChannel;
+import org.jgroups.View;
+import org.testng.annotations.Test;
+import org.testng.annotations.AfterMethod;
+import static org.testng.Assert.fail;
+import org.infinispan.util.logging.Log;
+import org.infinispan.util.logging.LogFactory;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Properties;
+
+/**
+ * The purpose of this class is to test that/if tcp + mping works fine in the given environment.
+ *
+ *  @author Mircea.Markus@jboss.com
+ */
+@Test(testName = "test,fwk.TcpMPingEnvironmentTest", groups = "functional")
+public class TcpMPingEnvironmentTest {
+
+   Log log = LogFactory.getLog(TcpMPingEnvironmentTest.class);
+
+   List<JChannel> openedChannles = new ArrayList<JChannel>();
+   private boolean success = false;
+
+   @AfterMethod
+   public void destroyCaches() {
+      for (JChannel ch : openedChannles) {
+         ch.disconnect();
+         ch.close();
+      }
+      if (!success) {
+         Properties properties = System.getProperties();
+         log.trace("System props are " + properties);
+         System.out.println("System props are " + properties);
+      }
+   }
+
+   /**
+    * Tests that different clusters are created and that they don't overlap.
+    */
+   public void testDifferentClusters() throws Exception {
+      log.trace("aaaaa");
+      log.info("aaaaa");
+
+      JChannel first1 = new JChannel("stacks/tcp_mping/tcp1.xml");
+      JChannel first2 = new JChannel("stacks/tcp_mping/tcp1.xml");
+      JChannel first3 = new JChannel("stacks/tcp_mping/tcp1.xml");
+      initiChannel(first1);
+      initiChannel(first2);
+      initiChannel(first3);
+
+      expectView(first1, first2, first3);
+
+      JChannel second1 = new JChannel("stacks/tcp_mping/tcp2.xml");
+      JChannel second2 = new JChannel("stacks/tcp_mping/tcp2.xml");
+      JChannel second3 = new JChannel("stacks/tcp_mping/tcp2.xml");
+      initiChannel(second1);
+      initiChannel(second2);
+      initiChannel(second3);
+
+      expectView(first1, first2, first3);
+      expectView(second1, second2, second3);
+      success = true;
+   }
+
+   private void expectView(JChannel... channels) throws Exception {
+      for (int i = 0; i < 20; i++) {
+         boolean success = true;
+         for (int j = 0; j < channels.length; j++) {
+            View view = channels[j].getView();
+            if (view == null) {
+               success = false;
+               break;
+            }
+            System.out.println("view = " + view);
+            success = success && (view.size() == channels.length);
+         }
+         if (success) return;
+         Thread.sleep(1000);
+      }
+      fail("Could not for cluster in given timeout");
+   }
+
+
+   public void initiChannel(JChannel channel) throws Exception {
+      openedChannles.add(channel);
+      channel.setOpt(org.jgroups.Channel.LOCAL, false);
+      channel.setOpt(org.jgroups.Channel.AUTO_RECONNECT, true);
+      channel.setOpt(org.jgroups.Channel.AUTO_GETSTATE, false);
+      channel.setOpt(org.jgroups.Channel.BLOCK, true);
+      channel.connect("someChannel");
+   }
+}
