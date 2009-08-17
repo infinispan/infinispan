ae101e9f8d190b0b37fed215eeb1a1412f8c5304
Manik Surtani <manik@surtani.org>
2009-08-17 13:42:59 +0000

    More robust inter thread comms

--- core/src/test/java/org/infinispan/util/concurrent/WatchableValueTest.java
+++ core/src/test/java/org/infinispan/util/concurrent/WatchableValueTest.java
@@ -4,18 +4,21 @@ import org.testng.annotations.Test;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 
 @Test(groups = "unit", testName = "util.concurrent.WatchableValueTest")
 public class WatchableValueTest {
    public void testNotifier() throws InterruptedException {
       final WatchableValue vn = new WatchableValue(10);
       final List<Integer> threadsCompleted = new LinkedList<Integer>();
-
+      final CountDownLatch threadsReady = new CountDownLatch(3), valueSet1 = new CountDownLatch(2), valueSet2 = new CountDownLatch(1);
 
       Thread t1 = new Thread() {
          public void run() {
             try {
+               threadsReady.countDown();
                vn.awaitValue(50);
+               valueSet1.countDown();
                threadsCompleted.add(1);
             } catch (Exception e) {
                throw new RuntimeException(e);
@@ -26,7 +29,9 @@ public class WatchableValueTest {
       Thread t2 = new Thread() {
          public void run() {
             try {
+               threadsReady.countDown();
                vn.awaitValue(50);
+               valueSet1.countDown();
                threadsCompleted.add(2);
             } catch (Exception e) {
                throw new RuntimeException(e);
@@ -37,7 +42,9 @@ public class WatchableValueTest {
       Thread t3 = new Thread() {
          public void run() {
             try {
+               threadsReady.countDown();
                vn.awaitValue(40);
+               valueSet2.countDown();
                threadsCompleted.add(3);
             } catch (Exception e) {
                throw new RuntimeException(e);
@@ -49,16 +56,16 @@ public class WatchableValueTest {
       t2.start();
       t3.start();
 
-      Thread.sleep(100);
+      threadsReady.await();
       assert threadsCompleted.isEmpty();
       vn.setValue(50);
-      Thread.sleep(100);
+      valueSet1.await();
       assert threadsCompleted.size() == 2;
       assert threadsCompleted.contains(1);
       assert threadsCompleted.contains(2);
 
       vn.setValue(40);
-      Thread.sleep(100);
+      valueSet2.await();
       assert threadsCompleted.size() == 3;
       assert threadsCompleted.contains(3);
    }
