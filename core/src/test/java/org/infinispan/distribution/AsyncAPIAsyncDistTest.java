59b44988cc8854a86add9fdf502312216115795e
Manik Surtani <manik@surtani.org>
2009-09-29 10:01:38 +0000

    Enabled tests

--- core/src/test/java/org/infinispan/distribution/AsyncAPIAsyncDistTest.java
+++ core/src/test/java/org/infinispan/distribution/AsyncAPIAsyncDistTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
-@Test(groups = "functional", testName = "distribution.AsyncAPIAsyncDistTest", enabled = false)
+@Test(groups = "functional", testName = "distribution.AsyncAPIAsyncDistTest")
 public class AsyncAPIAsyncDistTest extends AsyncAPISyncDistTest {
 
    ReplListener rl;
--- core/src/test/java/org/infinispan/distribution/AsyncAPISyncDistTest.java
+++ core/src/test/java/org/infinispan/distribution/AsyncAPISyncDistTest.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
-@Test(groups = "functional", testName = "distribution.AsyncAPISyncDistTest", enabled = false)
+@Test(groups = "functional", testName = "distribution.AsyncAPISyncDistTest")
 public class AsyncAPISyncDistTest extends AsyncAPISyncReplTest {
 
    @SuppressWarnings("unchecked")
