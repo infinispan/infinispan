0a8f0f5f83e0ccca34fa34749b380cf95b17535f
Manik Surtani <manik@surtani.org>
2009-10-01 15:36:53 +0000

    Wait for init join sequence to complete

--- core/src/test/java/org/infinispan/distribution/AsyncAPISyncDistTest.java
+++ core/src/test/java/org/infinispan/distribution/AsyncAPISyncDistTest.java
@@ -4,12 +4,14 @@ import org.infinispan.Cache;
 import org.infinispan.config.Configuration;
 import org.infinispan.context.Flag;
 import org.infinispan.replication.AsyncAPISyncReplTest;
+import org.infinispan.test.TestingUtil;
 import org.infinispan.test.data.Key;
 import org.infinispan.util.Util;
 import org.testng.annotations.Test;
 
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 @Test(groups = "functional", testName = "distribution.AsyncAPISyncDistTest")
 public class AsyncAPISyncDistTest extends AsyncAPISyncReplTest {
@@ -23,6 +25,21 @@ public class AsyncAPISyncDistTest extends AsyncAPISyncReplTest {
       List<Cache<Key, String>> l = createClusteredCaches(2, getClass().getSimpleName(), c);
       c1 = l.get(0);
       c2 = l.get(1);
+
+      // wait for any rehashing to complete
+      waitForInitRehashToComplete(c1, c2);
+   }
+
+   protected void waitForInitRehashToComplete(Cache... caches) {
+      int gracetime = 60000; // 60 seconds?
+      long giveup = System.currentTimeMillis() + gracetime;
+      for (Cache c: caches) {
+         DistributionManagerImpl dmi = (DistributionManagerImpl) TestingUtil.extractComponent(c, DistributionManager.class);
+         while (!dmi.joinComplete) {
+            if (System.currentTimeMillis() > giveup) throw new RuntimeException("Timed out waiting for initial join sequence to complete!");
+            LockSupport.parkNanos(TimeUnit.SECONDS.toNanos(1));
+         }
+      }
    }
 
    @Override
