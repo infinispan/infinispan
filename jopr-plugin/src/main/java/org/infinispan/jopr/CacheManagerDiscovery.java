f9a60f481f8f6707dbf6dd4bd99803af8cdfc6ac
Galder Zamarreno <galder@jboss.org>
2009-10-07 13:01:11 +0000

    [ISPN-135] (Automatically generate the JOPR XML descriptor for the JOPR plugin) Second infinispan package name is redundant.

--- jopr-plugin/src/main/java/org/infinispan/jopr/CacheManagerDiscovery.java
+++ jopr-plugin/src/main/java/org/infinispan/jopr/CacheManagerDiscovery.java
@@ -19,7 +19,7 @@
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
  */
-package org.infinispan.jopr.infinispan;
+package org.infinispan.jopr;
 
 import static org.infinispan.jmx.CacheManagerJmxRegistration.*;
 import static org.infinispan.jmx.ComponentsJmxRegistration.*;
--- jopr-plugin/src/main/java/org/infinispan/jopr/ConnectionHelper.java
+++ jopr-plugin/src/main/java/org/infinispan/jopr/ConnectionHelper.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
-package org.infinispan.jopr.infinispan;
+package org.infinispan.jopr;
 
 import org.infinispan.util.Util;
 import org.infinispan.util.logging.Log;
