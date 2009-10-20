8a26a00f6b39dfac93dd9e0dc41e77736474f371
Manik Surtani <manik@surtani.org>
2009-10-20 00:27:57 +0000

    Fixed script

--- bin/fixlineendings.sh
+++ bin/fixlineendings.sh
@@ -2,9 +2,9 @@
 
 
 if [[ $0 =~ bin/.*.sh ]] ; then
-   DIR_PREFIX=".."
-else
    DIR_PREFIX="."
+else
+   DIR_PREFIX=".."
 fi
 
 find ${DIR_PREFIX}/ -name \*.java -or -name \*.xml -or -name \*.txt -or -name \*.xsd -or -name \*.sh -or -name \*.py | xargs svn ps --force svn:eol-style LF
