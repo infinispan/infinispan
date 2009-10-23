d307ca3c79227db5fd06cbb18b9abbe9045fb500
Manik Surtani <manik@surtani.org>
2009-10-23 16:42:35 +0000

    Better thread naming

--- core/src/test/java/org/infinispan/profiling/testinternals/TaskRunner.java
+++ core/src/test/java/org/infinispan/profiling/testinternals/TaskRunner.java
@@ -2,7 +2,9 @@ package org.infinispan.profiling.testinternals;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Essentially a delegate to an ExecutorService, but a special one that is only used by perf tests so it can be ignored
@@ -12,7 +14,17 @@ public class TaskRunner {
    ExecutorService exec;
 
    public TaskRunner(int numThreads) {
-      this.exec = Executors.newFixedThreadPool(numThreads);
+      this(numThreads, false);
+   }
+   public TaskRunner(int numThreads, final boolean warmup) {
+      final AtomicInteger counter = new AtomicInteger(0);
+      final ThreadGroup tg = new ThreadGroup(Thread.currentThread().getThreadGroup(), warmup ? "WarmupLoadGenerators" : "LoadGenerators");
+      this.exec = Executors.newFixedThreadPool(numThreads, new ThreadFactory() {
+
+         public Thread newThread(Runnable r) {
+            return new Thread(tg, r, (warmup ? "WarmupLoadGenerator-" : "LoadGenerator-") + counter.incrementAndGet());
+         }
+      });
    }
 
    public void execute(Runnable r) {
