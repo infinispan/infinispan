697c4fbc8e0775a42ce69a02e79d47c00def552c
Manik Surtani <manik@surtani.org>
2009-11-04 14:14:18 +0000

    Fixed script

--- core/src/main/resources/importConfig.sh
+++ core/src/main/resources/importConfig.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
-if [ -e ../modules/core/lib ]
+
+DIRNAME=`dirname $0`
+
+# Setup ISPN_HOME
+if [ "x$ISPN_HOME" = "x" ]; then
+    # get the full path (without any relative bits)
+    ISPN_HOME=`cd $DIRNAME/..; pwd`
+fi
+export ISPN_HOME
+
+if [ -e ${ISPN_HOME}/modules/core/lib ]
 then
-   for JAR in ../modules/core/lib/*
+   for JAR in ${ISPN_HOME}/modules/core/lib/*
    do
       CLASSPATH=$CLASSPATH:$JAR
    done
 fi
-CLASSPATH=../modules/core/infinispan-core.jar$CLASSPATH
+CLASSPATH=${ISPN_HOME}/modules/core/infinispan-core.jar:$CLASSPATH
 java -classpath $CLASSPATH org.infinispan.config.parsing.ConfigFilesConvertor ${*}
