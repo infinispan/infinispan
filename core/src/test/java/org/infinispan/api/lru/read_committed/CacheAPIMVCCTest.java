b2af280b050f0fccacde487d47207651872ad600
Manik Surtani <manik@surtani.org>
2009-11-04 15:32:23 +0000

    Corrected test name

--- core/src/test/java/org/infinispan/api/lru/read_committed/CacheAPIMVCCTest.java
+++ core/src/test/java/org/infinispan/api/lru/read_committed/CacheAPIMVCCTest.java
@@ -6,7 +6,7 @@ import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.util.concurrent.IsolationLevel;
 import org.testng.annotations.Test;
 
-@Test(groups = "functional", testName = "api.mvcc.read_committed.CacheAPIMVCCTest")
+@Test(groups = "functional", testName = "api.lru.read_committed.CacheAPIMVCCTest")
 public class CacheAPIMVCCTest extends CacheAPITest {
    @Override
    protected IsolationLevel getIsolationLevel() {
