c03030f5e7a732d1b0d60d63ca8a46419d9038b8
Manik Surtani <manik@surtani.org>
2010-01-11 18:52:16 +0000

    Overloaded to use byte arrays

--- core/src/main/java/org/infinispan/io/UnsignedNumeric.java
+++ core/src/main/java/org/infinispan/io/UnsignedNumeric.java
@@ -68,4 +68,60 @@ public class UnsignedNumeric {
       out.writeByte((byte) i);
    }
 
+     /**
+    * Reads an int stored in variable-length format.  Reads between one and five bytes.  Smaller values take fewer
+    * bytes.  Negative numbers are not supported.
+    */
+   public static int readUnsignedInt(byte[] bytes, int offset) throws IOException {
+      byte b = bytes[offset++];
+      int i = b & 0x7F;
+      for (int shift = 7; (b & 0x80) != 0; shift += 7) {
+         b = bytes[offset++];
+         i |= (b & 0x7FL) << shift;
+      }
+      return i;
+   }
+
+   /**
+    * Writes an int in a variable-length format.  Writes between one and five bytes.  Smaller values take fewer bytes.
+    * Negative numbers are not supported.
+    *
+    * @param i int to write
+    */
+   public static void writeUnsignedInt(byte[] bytes, int offset, int i) throws IOException {
+      while ((i & ~0x7F) != 0) {
+         bytes[offset++] = (byte) ((i & 0x7f) | 0x80);
+         i >>>= 7;
+      }
+      bytes[offset] = (byte) i;
+   }
+
+
+   /**
+    * Reads an int stored in variable-length format.  Reads between one and nine bytes.  Smaller values take fewer
+    * bytes.  Negative numbers are not supported.
+    */
+   public static long readUnsignedLong(byte[] bytes, int offset) throws IOException {
+      byte b = bytes[offset++];
+      long i = b & 0x7F;
+      for (int shift = 7; (b & 0x80) != 0; shift += 7) {
+         b = bytes[offset++];
+         i |= (b & 0x7FL) << shift;
+      }
+      return i;
+   }
+
+   /**
+    * Writes an int in a variable-length format.  Writes between one and nine bytes.  Smaller values take fewer bytes.
+    * Negative numbers are not supported.
+    *
+    * @param i int to write
+    */
+   public static void writeUnsignedLong(byte[] bytes, int offset, long i) throws IOException {
+      while ((i & ~0x7F) != 0) {
+         bytes[offset++] = (byte) ((i & 0x7f) | 0x80);
+         i >>>= 7;
+      }
+      bytes[offset] = (byte) i;
+   }
 }
