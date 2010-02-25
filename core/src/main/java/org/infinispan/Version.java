b91ce048340305a375c6d9ce6eaba9e359950c6f
Manik Surtani <manik@surtani.org>
2010-02-25 10:50:28 +0000

    Updated version

--- core/src/main/java/org/infinispan/Version.java
+++ core/src/main/java/org/infinispan/Version.java
@@ -31,10 +31,10 @@ import net.jcip.annotations.Immutable;
  */
 @Immutable
 public class Version {
-   public static final String version = "4.0.0.FINAL";
+   public static final String version = "4.0.1-SNAPSHOT";
    public static final String codename = "Starobrno";
    public static final String projectName = "Infinispan";
-   static final byte[] version_id = {'4', '0', '0', 'F', 'I', 'N', 'A', 'L'};
+   static final byte[] version_id = {'4', '0', '1', 'S'};
    private static final int MAJOR_SHIFT = 11;
    private static final int MINOR_SHIFT = 6;
    private static final int MAJOR_MASK = 0x00f800;
