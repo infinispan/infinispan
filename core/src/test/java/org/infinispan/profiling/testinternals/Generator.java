cb482887ea5d87e58d07218ae3f39118d0493452
Manik Surtani <manik@surtani.org>
2010-04-08 15:27:45 +0000

    Added generators

--- core/src/test/java/org/infinispan/profiling/testinternals/Generator.java
+++ core/src/test/java/org/infinispan/profiling/testinternals/Generator.java
@@ -2,6 +2,10 @@ package org.infinispan.profiling.testinternals;
 
 //import org.infinispan.tree.Fqn;
 
+import org.infinispan.remoting.transport.Address;
+import org.infinispan.remoting.transport.jgroups.JGroupsAddress;
+import org.jgroups.util.UUID;
+
 import java.util.List;
 import java.util.Random;
 
@@ -9,8 +13,12 @@ public class Generator {
    private static final Random r = new Random();
 
    public static String getRandomString() {
+      return getRandomString(10);
+   }
+
+   public static String getRandomString(int maxKeySize) {
       StringBuilder sb = new StringBuilder();
-      int len = r.nextInt(10);
+      int len = r.nextInt(maxKeySize);
 
       for (int i = 0; i < len; i++) {
          sb.append((char) (63 + r.nextInt(26)));
@@ -24,5 +32,16 @@ public class Generator {
 
    public static Object createRandomKey() {
       return Integer.toHexString(r.nextInt(Integer.MAX_VALUE));
+   }
+
+   public static byte[] getRandomByteArray(int maxByteArraySize) {
+      int sz = r.nextInt(maxByteArraySize);
+      byte[] b = new byte[sz];
+      for (int i=0; i<sz; i++) b[i] = (byte) r.nextInt(Byte.MAX_VALUE);
+      return b;
+   }
+
+   public static Address generateAddress() {
+      return new JGroupsAddress(UUID.randomUUID());
    }
 }
