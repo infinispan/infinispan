0524e610f012bb6afb50208c356af48e3be6dc09
Manik Surtani <manik@surtani.org>
2010-05-25 15:09:06 +0000

    [ISPN-450] (Add AtomicMapLookup method to use in Read Only operation without creating new map if none exists.)

--- core/src/main/java/org/infinispan/atomic/AtomicMapLookup.java
+++ core/src/main/java/org/infinispan/atomic/AtomicMapLookup.java
@@ -1,6 +1,12 @@
 package org.infinispan.atomic;
 
 import org.infinispan.Cache;
+import org.infinispan.util.Immutables;
+
+import java.util.Collections;
+import java.util.Map;
+
+import static org.infinispan.util.Immutables.immutableMapWrap;
 
 /**
  * A helper that locates atomic maps within a given cache.  This should be the <b>only</b> way AtomicMaps are created/retrieved.
@@ -11,18 +17,59 @@ import org.infinispan.Cache;
 public class AtomicMapLookup {
 
    /**
-    * Retrieves an atomic map from a given cache, stored under a given key.  If an AtomicMap did not exist, one is created
+    * Retrieves an atomic map from a given cache, stored under a given key.  If an atomic map did not exist, one is created
     * and registered in an atomic fashion.
+    *
     * @param cache underlying cache
+    * @param key key under which the atomic map exists
+    * @param <MK> key param of the cache
     * @param <K> key param of the AtomicMap
     * @param <V> value param of the AtomicMap
     * @return an AtomicMap
     */
    @SuppressWarnings("unchecked")
-   public static <MK, K, V> AtomicMap<K, V> getAtomicMap(Cache<?, ?> cache, MK key) {
+   public static <MK, K, V> AtomicMap<K, V> getAtomicMap(Cache<MK, ?> cache, MK key) {
+      return getAtomicMap(cache, key, true);
+   }
+
+   /**
+    * Retrieves an atomic map from a given cache, stored under a given key.
+    *
+    * @param cache underlying cache
+    * @param key key under which the atomic map exists
+    * @param createIfAbsent if true, a new atomic map is created if one did not exist.
+    * @param <MK> key param of the cache
+    * @param <K> key param of the AtomicMap
+    * @param <V> value param of the AtomicMap
+    * @return an AtomicMap, or null if one did not exist.
+    */
+   @SuppressWarnings("unchecked")
+   public static <MK, K, V> AtomicMap<K, V> getAtomicMap(Cache<MK, ?> cache, MK key, boolean createIfAbsent) {
       Object value = cache.get(key);
-      if (value == null) value = AtomicHashMap.newInstance(cache, key);
+      if (value == null) {
+         if (createIfAbsent)
+            value = AtomicHashMap.newInstance(cache, key);
+         else return null;
+      }
       AtomicHashMap<K, V> castValue = (AtomicHashMap<K, V>) value;
       return castValue.getProxy(cache, key, cache.getAdvancedCache().getBatchContainer(), cache.getAdvancedCache().getInvocationContextContainer());
+   }
+
+   /**
+    * Retrieves an atomic map from a given cache, stored under a given key, for reading only.  The atomic map returned
+    * will not support updates, and if the map did not in fact exist, an empty map is returned.
+    * @param cache underlying cache
+    * @param key key under which the atomic map exists
+    * @param <MK> key param of the cache
+    * @param <K> key param of the AtomicMap
+    * @param <V> value param of the AtomicMap
+    * @return an immutable, read-only map
+    */
+   public static <MK, K, V> Map<K, V> getReadOnlyAtomicMap(Cache<MK, ?> cache, MK key) {
+      AtomicMap<K, V> am = getAtomicMap(cache, key, false);
+      if (am == null)
+         return Collections.emptyMap();
+      else
+         return immutableMapWrap(am);
    }
 }
