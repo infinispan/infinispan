3759308887f2d91a148653ef2fc24acf96daf859
Galder Zamarreno <galder@jboss.org>
2010-05-27 06:55:29 +0000

    [ISPN-460] (REST server should pre-start caches to avoid concurrent startup issues) Fixed.

--- server/rest/src/main/scala/org/infinispan/rest/StartupListener.scala
+++ server/rest/src/main/scala/org/infinispan/rest/StartupListener.scala
@@ -1,22 +1,32 @@
 package org.infinispan.rest
 
-
 import javax.servlet.{ServletContextListener, ServletContextEvent}
 import org.infinispan.manager.DefaultCacheManager
-
+import scala.collection.JavaConversions._
 
 /**
  * To init the cache manager. Nice to do this on startup as any config problems will be picked up before any
  * requests are attempted to be serviced. Less kitten carnage.
+ *
+ * @author Michael Neale
+ * @author Galder Zamarreno
+ * @since 4.0
  */
 class StartupListener extends ServletContextListener {
   val INFINISPAN_CONF = "infinispan.server.rest.cfg"
-  def contextInitialized(ev: ServletContextEvent) = {
 
-    ManagerInstance.instance = makeCacheManager(ev)
-    ManagerInstance.instance.start
+  override def contextInitialized(ev: ServletContextEvent) {
+     ManagerInstance.instance = makeCacheManager(ev)
+     ManagerInstance.instance.start
+
+     // Start defined caches to avoid issues with lazily started caches
+     for (cacheName <- asIterator(ManagerInstance.instance.getCacheNames.iterator))
+        ManagerInstance.instance.getCache(cacheName)
+     // Finally, start default cache as well
+     ManagerInstance.instance.getCache[String, Any]
   }
-  def contextDestroyed(ev: ServletContextEvent) =  ManagerInstance.instance.stop
+
+  override def contextDestroyed(ev: ServletContextEvent) =  ManagerInstance.instance.stop
 
   /** Prefer the system property, but also allow the servlet context to set the path to the config */
   def makeCacheManager(ev: ServletContextEvent) = 
