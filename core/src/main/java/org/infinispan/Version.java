7437c1fd6cfcf686cf5c662446489a220fb547ea
Manik Surtani <manik@surtani.org>
2010-07-02 09:55:59 +0000

    5.0.0 has a new codename!

--- core/src/main/java/org/infinispan/Version.java
+++ core/src/main/java/org/infinispan/Version.java
@@ -32,7 +32,7 @@ import net.jcip.annotations.Immutable;
 @Immutable
 public class Version {
    public static final String version = "5.0.0.SNAPSHOT";
-   public static final String codename = "Radegast";
+   public static final String codename = "Pagoa";
    public static final String projectName = "Infinispan";
    static final byte[] version_id = {'0', '5', '0', '0', 'S'};
    private static final int MAJOR_SHIFT = 11;
