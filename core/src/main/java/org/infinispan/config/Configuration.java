757a967f3c55784dba2d5f60f40aa7e1532061bd
Vladimir Blagojevic <vblagoje@redhat.com>
2010-07-06 16:52:56 +0000

    [ISPN-511] - The Eviction Algorithms have an unexpected way to manage the parameter maxEntries

--- core/src/main/java/org/infinispan/config/Configuration.java
+++ core/src/main/java/org/infinispan/config/Configuration.java
@@ -1310,8 +1310,12 @@ public class Configuration extends AbstractNamedCacheConfigurationBean {
       /** @configRef desc="Eviction strategy. Available options are 'UNORDERED', 'FIFO', 'LRU' and 'NONE' (to disable eviction)."*/
       protected EvictionStrategy strategy=EvictionStrategy.NONE;
 
-      /** @configRef desc="Maximum number of entries in a cache instance.  -1 means no limit." */
-      protected Integer maxEntries=-1;
+      /**
+       * @configRef desc="Maximum number of entries in a cache instance. If selected value is not a
+       *            power of two the actual value will default to the least power of two larger than selected
+       *            value. -1 means no limit."
+       */
+      protected Integer maxEntries = -1;
       
       /** @configRef desc="Threading policy for eviction." */
       protected EvictionThreadPolicy threadPolicy=EvictionThreadPolicy.DEFAULT;
