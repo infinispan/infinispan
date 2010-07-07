03211629d3475c5796e2ecca40860a104edabc84
Mircea Markus <mircea.markus@gmail.com>
2010-07-07 12:33:08 +0000

    [ISPN-513] = hotrod client:optimize serialization of key/values

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/HotRodMarshaller.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/HotRodMarshaller.java
@@ -2,6 +2,8 @@ package org.infinispan.client.hotrod;
 
 import net.jcip.annotations.ThreadSafe;
 
+import java.util.Properties;
+
 /**
  * Used for un/marshalling objects sent between hotrod client and server (hotrod is a binary protocol).
  * A single instance of this class is shared by all threads, so this class needs to be thread safe.
@@ -12,7 +14,13 @@ import net.jcip.annotations.ThreadSafe;
 @ThreadSafe 
 public interface HotRodMarshaller {
 
-   byte[] marshallObject(Object toMarshall);
+   void init(Properties config);
+
+   /**
+    * @param isKeyHint if true the object passed to the marshaller is a key. This info can be used to optimize the
+    * size of the allocated byte[].
+    */
+   byte[] marshallObject(Object toMarshall, boolean isKeyHint);
 
    Object readObject(byte[] bytes);
 }
--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/SerializationMarshaller.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/SerializationMarshaller.java
@@ -2,19 +2,27 @@ package org.infinispan.client.hotrod.impl;
 
 import org.infinispan.client.hotrod.HotRodMarshaller;
 import org.infinispan.client.hotrod.exceptions.HotRodClientException;
+import org.infinispan.io.ExposedByteArrayOutputStream;
 import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.util.Arrays;
+import java.util.Properties;
 
 /**
  * Default marshaller implementation based on object serialization.
- * todo - the marshaller should only be when writing to the actual transport
+ * Supports two configuration elements:
+ * <ul>
+ *  <li>marshaller.default-array-size.key - the size of the {@link ExposedByteArrayOutputStream} that will be
+ *   created for marshalling keys</li>
+ *  <li> marshaller.default-array-size.value - the size of the {@link ExposedByteArrayOutputStream} that will be
+ *   created for marshalling values
+ *  </li>
+ * </ul>
  *
  * @author Mircea.Markus@jboss.com
  * @since 4.1
@@ -23,15 +31,36 @@ public class SerializationMarshaller implements HotRodMarshaller {
 
    private static Log log = LogFactory.getLog(SerializationMarshaller.class);
 
+   private volatile int defaultArraySizeForKey = 128;
+   private volatile int defaultArraySizeForValue = 256;
+
    @Override
-   public byte[] marshallObject(Object toMarshall) {
-      ByteArrayOutputStream result = new ByteArrayOutputStream(1000);
+   public void init(Properties config) {
+      if (config.contains("marshaller.default-array-size.key")) {
+         defaultArraySizeForKey = Integer.parseInt(config.getProperty("marshaller.default-array-size.key"));
+      }
+      if (config.contains("marshaller.default-array-size.value")) {
+         defaultArraySizeForValue = Integer.parseInt(config.getProperty("marshaller.default-array-size.value"));
+      }
+   }
+
+   @Override
+   public byte[] marshallObject(Object toMarshall, boolean isKeyHint) {
+      ExposedByteArrayOutputStream result = getByteArray(isKeyHint);
       try {
          ObjectOutputStream oos = new ObjectOutputStream(result);
          oos.writeObject(toMarshall);
          return result.toByteArray();
       } catch (IOException e) {
          throw new HotRodClientException("Unexpected!", e);
+      }
+   }
+
+   private ExposedByteArrayOutputStream getByteArray(boolean keyHint) {
+      if (keyHint) {
+         return new ExposedByteArrayOutputStream(defaultArraySizeForKey);
+      } else {
+         return new ExposedByteArrayOutputStream(defaultArraySizeForValue);
       }
    }
 
