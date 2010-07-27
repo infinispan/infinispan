617d5a5b97f33f4a6f4b65134795b4443862d261
Manik Surtani <manik@surtani.org>
2010-07-27 16:07:09 +0000

    [ISPN-547] (Improve Flag documentation either in the ref doc or in JavaDoc)

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/Flag.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/Flag.java
@@ -1,7 +1,9 @@
 package org.infinispan.client.hotrod;
 
+import java.util.Map;
+
 /**
- * Defines all the flags available in hotrod that can influence the behavior of operations.
+ * Defines all the flags available in the Hot Rod client that can influence the behavior of operations.
  *
  * @author Mircea.Markus@jboss.com
  * @since 4.1
@@ -9,9 +11,11 @@ package org.infinispan.client.hotrod;
 public enum Flag {
 
    /**
-    * By default, previously existing values for Map operations are not returned. E.g. remoteCache.put(k,v) does not return
-    * the previous value associated with k. Passing this flag overrides default behavior and previous existing values are
-    * returned.
+    * By default, previously existing values for {@link Map} operations are not returned. E.g. {@link RemoteCache#put(Object, Object)}
+    * does <i>not</i> return the previous value associated with the key
+    * <p />
+    * By applying this flag, this default behavior is overridden for the scope of a single invocation, and the previous
+    * existing value is returned.
     */
    FORCE_RETURN_VALUE(0x0001);
 
