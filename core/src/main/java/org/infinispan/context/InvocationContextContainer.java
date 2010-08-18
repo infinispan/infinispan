d2a0f9fd573f90c132e39ed7e837b79c1327dc29
Vladimir Blagojevic <vblagoje@redhat.com>
2010-08-18 12:46:24 +0000

    javadoc spelling, formatting

--- core/src/main/java/org/infinispan/context/InvocationContextContainer.java
+++ core/src/main/java/org/infinispan/context/InvocationContextContainer.java
@@ -8,9 +8,10 @@ import org.infinispan.factories.scopes.Scope;
 import org.infinispan.factories.scopes.Scopes;
 
 /**
- * Manages the association between an {@link org.infinispan.context.InvocationContext} and the calling thread. Also acts
- * as a factory for creating various types of {@link org.infinispan.context.InvocationContext}s.
- *
+ * Manages the association between an {@link org.infinispan.context.InvocationContext} and the
+ * calling thread. Also acts as a factory for creating various types of
+ * {@link org.infinispan.context.InvocationContext}s.
+ * 
  * @author Manik Surtani (<a href="mailto:manik@jboss.org">manik@jboss.org</a>)
  * @author Mircea.Markus@jboss.com
  * @since 4.0
@@ -20,49 +21,55 @@ import org.infinispan.factories.scopes.Scopes;
 public interface InvocationContextContainer {
 
    /**
-    * If we are in a tx scope this will return an {@link org.infinispan.context.impl.TxInvocationContext}. Otherwise it
-    * will return an {@link org.infinispan.context.impl.NonTxInvocationContext}. Either way, both context will be marked
-    * as local, i.e. {@link InvocationContext#isOriginLocal()} will be true. The context is also associated with the
-    * current thread, so further calls to {@link #getInvocationContext()} will return same instace.
+    * If we are in a tx scope this will return an
+    * {@link org.infinispan.context.impl.TxInvocationContext}. Otherwise it will return an
+    * {@link org.infinispan.context.impl.NonTxInvocationContext}. Either way, both context will be
+    * marked as local, i.e. {@link InvocationContext#isOriginLocal()} will be true. The context is
+    * also associated with the current thread, so further calls to {@link #getInvocationContext()}
+    * will return same instance.
     */
    InvocationContext createInvocationContext();
 
    /**
-    * Will create an {@link org.infinispan.context.impl.NonTxInvocationContext} with the {@link org.infinispan.context.impl.NonTxInvocationContext#isOriginLocal()}
-    * returning true. 
+    * Will create an {@link org.infinispan.context.impl.NonTxInvocationContext} with the
+    * {@link org.infinispan.context.impl.NonTxInvocationContext#isOriginLocal()} returning true.
     */
    NonTxInvocationContext createNonTxInvocationContext();
 
    /**
-    * Returns a {@link org.infinispan.context.impl.LocalTxInvocationContext}. The context is also associated with the
-    * current thread, so further calls to {@link #getInvocationContext()} will return same instace.
+    * Returns a {@link org.infinispan.context.impl.LocalTxInvocationContext}. The context is also
+    * associated with the current thread, so further calls to {@link #getInvocationContext()} will
+    * return same instance.
     */
    LocalTxInvocationContext createTxInvocationContext();
 
    /**
-    * Returns an {@link org.infinispan.context.impl.RemoteTxInvocationContext}. The context is also associated with the
-    * current thread, so further calls to {@link #getInvocationContext()} will return same instace.
+    * Returns an {@link org.infinispan.context.impl.RemoteTxInvocationContext}. The context is also
+    * associated with the current thread, so further calls to {@link #getInvocationContext()} will
+    * return same instance.
     */
    RemoteTxInvocationContext createRemoteTxInvocationContext();
 
    /**
-    * Returns an {@link org.infinispan.context.impl.NonTxInvocationContext} whose {@link
-    * org.infinispan.context.impl.NonTxInvocationContext#isOriginLocal()} flag will be true. The context is also
-    * associated with the current thread, so further calls to {@link #getInvocationContext()} will return same instace.
+    * Returns an {@link org.infinispan.context.impl.NonTxInvocationContext} whose
+    * {@link org.infinispan.context.impl.NonTxInvocationContext#isOriginLocal()} flag will be true.
+    * The context is also associated with the current thread, so further calls to
+    * {@link #getInvocationContext()} will return same instance.
     */
    NonTxInvocationContext createRemoteInvocationContext();
 
    /**
-    * Returns the {@link InvocationContext}  that is currently associated with the calling thread. Important:
-    * implementations of this metrhod is most likely expensive (ThreadLocal.get), it is recommanded to cache the result
-    * of this method rather than repeting the call.
-    *
-    * @throws IllegalStateException if there is no context associated with the current thread.
+    * Returns the {@link InvocationContext} that is currently associated with the calling thread.
+    * Important: implementations of this method is most likely expensive (ThreadLocal.get), it is
+    * recommended to cache the result of this method rather than repeating the call.
+    * 
+    * @throws IllegalStateException
+    *            if there is no context associated with the current thread.
     */
    InvocationContext getInvocationContext();
 
    /**
-    * Dissasociates thread's invocation context and returns the existing value.
+    * Disassociates thread's invocation context and returns the existing value.
     */
    InvocationContext suspend();
 
