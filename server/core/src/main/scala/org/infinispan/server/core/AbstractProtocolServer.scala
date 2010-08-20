0f1c9cdeba00c815f9ba6de0640024aba5b023cd
Galder Zamarreno <galder@jboss.org>
2010-08-20 09:15:41 +0000

    [ISPN-612] (Allow substitution of Infinispan server properties) Done.

--- server/core/src/main/scala/org/infinispan/server/core/AbstractProtocolServer.scala
+++ server/core/src/main/scala/org/infinispan/server/core/AbstractProtocolServer.scala
@@ -27,33 +27,33 @@ abstract class AbstractProtocolServer(threadNamePrefix: String) extends Protocol
    def start(properties: Properties, cacheManager: EmbeddedCacheManager, defaultPort: Int) {
       val typedProps = TypedProperties.toTypedProperties(properties)
       // Enabled added to make it easy to enable/disable endpoints in JBoss MC based beans for EDG
-      val toStart = typedProps.getBooleanProperty("enabled", true)
+      val toStart = typedProps.getBooleanProperty("enabled", true, true)
 
       if (toStart) {
          // By doing parameter validation here, both programmatic and command line clients benefit from it.
-         this.host = typedProps.getProperty(PROP_KEY_HOST, HOST_DEFAULT)
-         this.port = typedProps.getIntProperty(PROP_KEY_PORT, defaultPort)
-         this.masterThreads = typedProps.getIntProperty(PROP_KEY_MASTER_THREADS, MASTER_THREADS_DEFAULT)
+         this.host = typedProps.getProperty(PROP_KEY_HOST, HOST_DEFAULT, true)
+         this.port = typedProps.getIntProperty(PROP_KEY_PORT, defaultPort, true)
+         this.masterThreads = typedProps.getIntProperty(PROP_KEY_MASTER_THREADS, MASTER_THREADS_DEFAULT, true)
          if (masterThreads < 0)
             throw new IllegalArgumentException("Master threads can't be lower than 0: " + masterThreads)
 
-         this.workerThreads = typedProps.getIntProperty(PROP_KEY_WORKER_THREADS, WORKER_THREADS_DEFAULT)
+         this.workerThreads = typedProps.getIntProperty(PROP_KEY_WORKER_THREADS, WORKER_THREADS_DEFAULT, true)
          if (workerThreads < 0)
             throw new IllegalArgumentException("Worker threads can't be lower than 0: " + masterThreads)
 
          this.cacheManager = cacheManager
-         val idleTimeout = typedProps.getIntProperty(PROP_KEY_IDLE_TIMEOUT, IDLE_TIMEOUT_DEFAULT)
+         val idleTimeout = typedProps.getIntProperty(PROP_KEY_IDLE_TIMEOUT, IDLE_TIMEOUT_DEFAULT, true)
          if (idleTimeout < -1)
             throw new IllegalArgumentException("Idle timeout can't be lower than -1: " + idleTimeout)
 
-         val tcpNoDelay = typedProps.getBooleanProperty(PROP_KEY_TCP_NO_DELAY, TCP_NO_DELAY_DEFAULT)
+         val tcpNoDelay = typedProps.getBooleanProperty(PROP_KEY_TCP_NO_DELAY, TCP_NO_DELAY_DEFAULT, true)
 
-         val sendBufSize = typedProps.getIntProperty(PROP_KEY_SEND_BUF_SIZE, SEND_BUF_SIZE_DEFAULT)
+         val sendBufSize = typedProps.getIntProperty(PROP_KEY_SEND_BUF_SIZE, SEND_BUF_SIZE_DEFAULT, true)
          if (sendBufSize < 0) {
             throw new IllegalArgumentException("Send buffer size can't be lower than 0: " + sendBufSize)
          }
 
-         val recvBufSize = typedProps.getIntProperty(PROP_KEY_RECV_BUF_SIZE, RECV_BUF_SIZE_DEFAULT)
+         val recvBufSize = typedProps.getIntProperty(PROP_KEY_RECV_BUF_SIZE, RECV_BUF_SIZE_DEFAULT, true)
          if (recvBufSize < 0) {
             throw new IllegalArgumentException("Send buffer size can't be lower than 0: " + sendBufSize)
          }
@@ -62,12 +62,12 @@ abstract class AbstractProtocolServer(threadNamePrefix: String) extends Protocol
          cacheManager.addListener(getRankCalculatorListener)
          // Start default cache
          startDefaultCache
-         val address = new InetSocketAddress(host, port)
-         startTransport(address, idleTimeout, tcpNoDelay, sendBufSize, recvBufSize)
+         startTransport(idleTimeout, tcpNoDelay, sendBufSize, recvBufSize)
       }
    }
 
-   def startTransport(address: InetSocketAddress, idleTimeout: Int, tcpNoDelay: Boolean, sendBufSize: Int, recvBufSize: Int) {
+   def startTransport(idleTimeout: Int, tcpNoDelay: Boolean, sendBufSize: Int, recvBufSize: Int) {
+      val address = new InetSocketAddress(host, port)
       val encoder = getEncoder
       val nettyEncoder = if (encoder != null) new EncoderAdapter(encoder) else null
       transport = new NettyTransport(this, nettyEncoder, address, masterThreads, workerThreads, idleTimeout,
