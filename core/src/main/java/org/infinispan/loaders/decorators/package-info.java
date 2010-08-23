72d983082dc5febf53139ab20048cd01080f581e
Vladimir Blagojevic <vblagoje@redhat.com>
2010-08-23 11:30:49 +0000

    [ISPN-591] - Generated Schema declares namespace as 4.0 rather than 4.1 Merge onto trunk. Assume 4.2 is next release from trunk. Will modify NamespaceFilter if not.

--- core/src/main/java/org/infinispan/loaders/decorators/package-info.java
+++ core/src/main/java/org/infinispan/loaders/decorators/package-info.java
@@ -2,11 +2,12 @@
  * This package contains loaders and stores, which are used for overflow or persistence.
  * Need @XmlSchema annotation for AsyncStoreConfig.java and SingletonStoreConfig.java
  */
-@XmlSchema(namespace = "urn:infinispan:config:4.0", elementFormDefault = XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED, 
+@XmlSchema(namespace = ISPN_NS, elementFormDefault = XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED, 
          xmlns = {
-         @javax.xml.bind.annotation.XmlNs(prefix = "tns", namespaceURI = "urn:infinispan:config:4.0"),
+         @javax.xml.bind.annotation.XmlNs(prefix = "tns", namespaceURI = ISPN_NS),
          @javax.xml.bind.annotation.XmlNs(prefix = "xs", namespaceURI = "http://www.w3.org/2001/XMLSchema") })
 package org.infinispan.loaders.decorators;
 
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.*;
+import static org.infinispan.config.parsing.NamespaceFilter.ISPN_NS;
--- core/src/main/java/org/infinispan/loaders/package-info.java
+++ core/src/main/java/org/infinispan/loaders/package-info.java
@@ -2,11 +2,12 @@
  * This package contains loaders and stores, which are used for overflow or persistence.
  * Need @XmlSchema annotation for CacheLoaderConfig.java and AbstractCacheStoreConfig.java
  */
-@XmlSchema(namespace = "urn:infinispan:config:4.0", elementFormDefault = XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED, 
+@XmlSchema(namespace = ISPN_NS, elementFormDefault = XmlNsForm.QUALIFIED, attributeFormDefault = XmlNsForm.UNQUALIFIED, 
          xmlns = {
-         @javax.xml.bind.annotation.XmlNs(prefix = "tns", namespaceURI = "urn:infinispan:config:4.0"),
+         @javax.xml.bind.annotation.XmlNs(prefix = "tns", namespaceURI = ISPN_NS),
          @javax.xml.bind.annotation.XmlNs(prefix = "xs", namespaceURI = "http://www.w3.org/2001/XMLSchema") })
 package org.infinispan.loaders;
 
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.*;
+import static org.infinispan.config.parsing.NamespaceFilter.ISPN_NS;
