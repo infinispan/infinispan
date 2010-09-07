e0c2d748884dd22134ea43befc79e4609ce88442
Manik Surtani <manik@surtani.org>
2010-09-07 12:33:43 +0000

    [ISPN-608] (Make replication queue implementation pluggable)

--- core/src/test/java/org/infinispan/config/ConfigurationValidationTest.java
+++ core/src/test/java/org/infinispan/config/ConfigurationValidationTest.java
@@ -22,7 +22,9 @@
 package org.infinispan.config;
 
 import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.test.AbstractInfinispanTest;
 import org.infinispan.test.SingleCacheManagerTest;
+import org.infinispan.test.TestingUtil;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.testng.annotations.Test;
 
@@ -31,25 +33,36 @@ import static org.infinispan.config.Configuration.CacheMode.REPL_ASYNC;
 
 /**
  * ConfigurationValidationTest.
- * 
+ *
  * @author Sanne Grinovero
  * @since 4.0
  */
 @Test(groups = "functional", testName = "config.ConfigurationValidationTest")
-public class ConfigurationValidationTest extends SingleCacheManagerTest {
-   
-   @Test (expectedExceptions=ConfigurationException.class)
-   public void testWrongCacheModeConfiguration() {
-      cacheManager.getCache().put("key", "value");
+public class ConfigurationValidationTest extends AbstractInfinispanTest {
+
+   @Test(expectedExceptions = ConfigurationException.class)
+   public void testWrongCacheModeConfiguration() throws Exception {
+      EmbeddedCacheManager cacheManager = null;
+      try {
+         cacheManager = createCacheManager();
+         cacheManager.getCache().put("key", "value");
+      } finally {
+         TestingUtil.killCacheManagers(cacheManager);
+      }
    }
 
-   public void testCacheModeConfiguration() {
-      cacheManager.getCache("local").put("key", "value");
+   public void testCacheModeConfiguration() throws Exception {
+      EmbeddedCacheManager cacheManager = null;
+      try {
+         cacheManager = createCacheManager();
+         cacheManager.getCache("local").put("key", "value");
+      } finally {
+         TestingUtil.killCacheManagers(cacheManager);
+      }
    }
 
 
-   @Override
-   protected EmbeddedCacheManager createCacheManager() throws Exception {
+   private EmbeddedCacheManager createCacheManager() throws Exception {
       GlobalConfiguration gc = GlobalConfiguration.getNonClusteredDefault();
       Configuration config = new Configuration();
       config.setCacheMode(REPL_ASYNC);
