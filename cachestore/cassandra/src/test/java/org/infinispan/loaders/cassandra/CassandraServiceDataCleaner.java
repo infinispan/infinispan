86da7feb32d96fc6f5b3609ab2179a139328a31d
Tristan Tarrant <tristan.tarrant@gmail.com>
2010-09-17 09:51:51 +0000

    Use TestingUtils

--- cachestore/cassandra/src/test/java/org/infinispan/loaders/cassandra/CassandraServiceDataCleaner.java
+++ cachestore/cassandra/src/test/java/org/infinispan/loaders/cassandra/CassandraServiceDataCleaner.java
@@ -1,12 +1,12 @@
 package org.infinispan.loaders.cassandra;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.io.util.FileUtils;
+import org.infinispan.test.TestingUtil;
 
 public class CassandraServiceDataCleaner {
 	/**
@@ -28,7 +28,7 @@ public class CassandraServiceDataCleaner {
 	 */
 	public void cleanupDataDirectories() throws IOException {
 		for (String s : getDataDirs()) {
-			cleanDir(s);
+			TestingUtil.recursiveFileRemove(s);
 		}
 	}
 
@@ -67,30 +67,6 @@ public class CassandraServiceDataCleaner {
 	 */
 	private void mkdir(String dir) throws IOException {
 		FileUtils.createDirectory(dir);
-	}
-
-	/**
-	 * Removes all directory content from file the system
-	 * 
-	 * @param dir
-	 * @throws IOException
-	 */
-	private void cleanDir(String dir) throws IOException {
-		File dirFile = new File(dir);
-		if (dirFile.exists() && dirFile.isDirectory()) {
-			deleteDir(dirFile);
-		}
-	}
-
-	public static void deleteDir(File dir) throws IOException {
-		if (dir.isDirectory()) {
-			String[] children = dir.list();
-			for (int i = 0; i < children.length; i++) {
-				deleteDir(new File(dir, children[i]));
-			}
-		}
-
-		dir.delete();
 	}
 
 }
