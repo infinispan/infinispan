d549bfa90642c2f20bf223baf331d574935a1a2e
Manik Surtani <manik@surtani.org>
2010-10-29 13:01:37 +0000

    ISPN-747 - Ensure java executable is properly detected for different OSs and JDKs in all shell scripts

--- demos/lucene-directory-demo/src/main/resources/runLuceneDemo.sh
+++ demos/lucene-directory-demo/src/main/resources/runLuceneDemo.sh
@@ -1,5 +1,61 @@
 #!/bin/bash
 
+# OS specific support.
+cygwin=false;
+darwin=false;
+mingw=false
+case "`uname`" in
+  CYGWIN*) cygwin=true ;;
+  MINGW*) mingw=true;;
+  Darwin*) darwin=true
+           if [ -z "$JAVA_VERSION" ] ; then
+             JAVA_VERSION="CurrentJDK"
+           else
+             echo "Using Java version: $JAVA_VERSION"
+           fi
+           if [ -z "$JAVA_HOME" ] ; then
+             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
+           fi
+           ;;
+esac
+
+if [ -z "$JAVA_HOME" ] ; then
+  if [ -r /etc/gentoo-release ] ; then
+    JAVA_HOME=`java-config --jre-home`
+  fi
+fi
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+fi
+
+# For Migwn, ensure paths are in UNIX format before anything is touched
+if $mingw ; then
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
+fi
+
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+      # IBM's JDK on AIX uses strange locations for the executables
+      JAVACMD="$JAVA_HOME/jre/sh/java"
+    else
+      JAVACMD="$JAVA_HOME/bin/java"
+    fi
+  else
+    JAVACMD="`which java`"
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly."
+  echo "  We cannot execute $JAVACMD"
+  exit 1
+fi
+
 DIRNAME=`dirname $0`
 
 # Setup ISPN_HOME
@@ -23,9 +79,14 @@ for i in ${ISPN_HOME}/modules/lucene-directory-demo/lib/*.jar ; do
    CP=${i}:${CP}
 done
 
+if $cygwin; then
+   # Turn paths into Windows style for Cygwin
+   CP=`cygpath -wp ${CP}`
+fi
+
 JVM_PARAMS="${JVM_PARAMS} -Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:${ISPN_HOME}/etc/log4j.xml"
 
 # Sample JPDA settings for remote socket debugging
 #JVM_PARAMS="$JVM_PARAMS -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
 
-java -cp ${CP} ${JVM_PARAMS} org.infinispan.lucenedemo.DemoDriver
+${JAVACMD} -cp ${CP} ${JVM_PARAMS} org.infinispan.lucenedemo.DemoDriver
--- server/core/src/main/resources/startServer.sh
+++ server/core/src/main/resources/startServer.sh
@@ -10,6 +10,68 @@ add_to_classpath()
   fi
 }
 
+# OS specific support.
+cygwin=false;
+darwin=false;
+mingw=false
+case "`uname`" in
+  CYGWIN*) cygwin=true ;;
+  MINGW*) mingw=true;;
+  Darwin*) darwin=true
+           if [ -z "$JAVA_VERSION" ] ; then
+             JAVA_VERSION="CurrentJDK"
+           else
+             echo "Using Java version: $JAVA_VERSION"
+           fi
+           if [ -z "$JAVA_HOME" ] ; then
+             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
+           fi
+           ;;
+esac
+
+if [ -z "$JAVA_HOME" ] ; then
+  if [ -r /etc/gentoo-release ] ; then
+    JAVA_HOME=`java-config --jre-home`
+  fi
+fi
+
+# For Cygwin, ensure paths are in UNIX format before anything is touched
+if $cygwin ; then
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+fi
+
+# For Migwn, ensure paths are in UNIX format before anything is touched
+if $mingw ; then
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
+fi
+
+if [ -z "$JAVACMD" ] ; then
+  if [ -n "$JAVA_HOME"  ] ; then
+    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
+      # IBM's JDK on AIX uses strange locations for the executables
+      JAVACMD="$JAVA_HOME/jre/sh/java"
+    else
+      JAVACMD="$JAVA_HOME/bin/java"
+    fi
+  else
+    JAVACMD="`which java`"
+  fi
+fi
+
+if [ ! -x "$JAVACMD" ] ; then
+  echo "Error: JAVA_HOME is not defined correctly."
+  echo "  We cannot execute $JAVACMD"
+  exit 1
+fi
+
+# For Cygwin, switch paths to Windows format before running java
+if $cygwin; then
+  [ -n "$JAVA_HOME" ] &&
+    JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
+fi
+
 DIRNAME=`dirname $0`
 
 # Setup ISPN_HOME
@@ -18,13 +80,6 @@ if [ "x$ISPN_HOME" = "x" ]; then
     ISPN_HOME=`cd $DIRNAME/..; pwd`
 fi
 export ISPN_HOME
-
-# Detect Cygwin
-# Cygwin fix courtesy of Supin Ko
-cygwin=false
-case "`uname`" in
-CYGWIN*) cygwin=true;;
-esac
 
 add_to_classpath ${ISPN_HOME}
 add_to_classpath ${ISPN_HOME}/lib
@@ -48,4 +103,4 @@ JVM_PARAMS="${JVM_PARAMS} -Djava.net.preferIPv4Stack=true  -Dlog4j.configuration
 # Sample JPDA settings for remote socket debuging
 #JVM_PARAMS="$JVM_PARAMS -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
 
-java -cp $CP ${JVM_PARAMS} org.infinispan.server.core.Main ${*}
+${JAVACMD} -cp $CP ${JVM_PARAMS} org.infinispan.server.core.Main ${*}
