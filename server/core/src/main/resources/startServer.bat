bdd98d75d7c188fd4182a3213d415d3b5ec37339
Manik Surtani <manik@surtani.org>
2010-12-16 12:57:40 +0000

    ISPN-834 startServer.bat problems

--- server/core/src/main/resources/startServer.bat
+++ server/core/src/main/resources/startServer.bat
@@ -10,9 +10,9 @@ for %%f in (..\modules\websocket\lib\*.jar) do set LIB=!LIB!;%%f
 rem echo libs: %LIB%
 
 set CP=%LIB%;..\infinispan-core.jar;..\modules\core\infinispan-server-memcached.jar;%CP%
-set CP=%LIB%;..\modules\memcached\infinispan-server-memcached.jar;%CP%
-set CP=%LIB%;..\modules\hotrod\infinispan-server-hotrod.jar;%CP%
-set CP=%LIB%;..\modules\hotrod\infinispan-server-websocket.jar;%CP%
+set CP=..\modules\memcached\infinispan-server-memcached.jar;%CP%
+set CP=..\modules\hotrod\infinispan-server-hotrod.jar;%CP%
+set CP=..\modules\websocket\infinispan-server-websocket.jar;%CP%
 
 java -classpath "%CP%" -Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=..\etc\log4j.xml org.infinispan.server.core.Main %*
 
