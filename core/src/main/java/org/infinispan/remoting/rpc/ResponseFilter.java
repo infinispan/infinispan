73c4401b919319b39e92123b198f6a2243cd0839
Manik Surtani <manik@surtani.org>
2011-01-12 14:20:54 +0000

    ISPN-865 - Use JGroups' NotifyingFuture when performing remote GETs
    * A better impl taking exceptional circumstances into account, including target nodes disappearing forever
    * Cleaner while loop

--- core/src/main/java/org/infinispan/remoting/rpc/ResponseFilter.java
+++ core/src/main/java/org/infinispan/remoting/rpc/ResponseFilter.java
@@ -4,8 +4,7 @@ import org.infinispan.remoting.responses.Response;
 import org.infinispan.remoting.transport.Address;
 
 /**
- * A mechanism of filtering RPC responses.  Used with {@link RpcManager#invokeRemotely(java.util.List,
- * org.infinispan.commands.ReplicableCommand, ResponseMode, long, boolean, ResponseFilter)}
+ * A mechanism of filtering RPC responses.  Used with the RPC manager.
  *
  * @author Manik Surtani
  * @since 4.0
