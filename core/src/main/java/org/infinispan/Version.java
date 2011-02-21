8d0de92410b288c1d37dcfc0acba1cdf98c42735
Mircea Markus <Mircea.Markus@gmail.com>
2011-02-21 13:00:33 +0000

    'Release Script: update versions for 4.2.1.CR3'

--- core/src/main/java/org/infinispan/Version.java
+++ core/src/main/java/org/infinispan/Version.java
@@ -39,10 +39,10 @@ public class Version {
    // -----------------------------------------------------------------------------------------------------------------
 
    public static final String major = "4.2";
-   public static final String version = major + ".1-SNAPSHOT";
+   public static final String version = major + ".1.CR3";
    public static final String codename = "Ursus";
    public static final String projectName = "Infinispan";
-   static final byte[] version_id = {'0', '4', '2', '1', 'S'};
+   static final byte[] version_id = {'4', '2', '1', 'C', 'R', '3'};
    private static final int MAJOR_SHIFT = 11;
    private static final int MINOR_SHIFT = 6;
    private static final int MAJOR_MASK = 0x00f800;
