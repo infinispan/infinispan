23e6bfc21535ef1b6d7d9962b9687662030c6396
Manik Surtani <manik@surtani.org>
2011-03-03 11:42:16 +0000

    Temporarily disable some tests

--- core/src/test/java/org/infinispan/distribution/topologyaware/TopologyInfoBroadcastNoRehashTest.java
+++ core/src/test/java/org/infinispan/distribution/topologyaware/TopologyInfoBroadcastNoRehashTest.java
@@ -7,7 +7,7 @@ import org.testng.annotations.Test;
  * @author Mircea.Markus@jboss.com
  * @since 4.2
  */
-@Test(groups = "functional", testName = "distribution.TopologyInfoBroadcastNoRehashTest")
+@Test(groups = "functional", testName = "distribution.TopologyInfoBroadcastNoRehashTest", enabled = false, description = "Left out for now until new, push-based rehash is implemented")
 public class TopologyInfoBroadcastNoRehashTest extends TopologyInfoBroadcastTest {
 
    @Override
