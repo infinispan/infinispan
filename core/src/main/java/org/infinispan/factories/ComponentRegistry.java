687a312e8c389ec8f3901ebbf52c5ea758aede3f
Dan Berindei <dberinde@redhat.com>
2011-04-03 22:27:45 +0300

    ISPN-1017 - HotRod client test suite hangs eating 100% CPU
    Concurrent put() calls in the component scopes HashMap were causing get() to enter an infinite loop.
    I changed the map to a ConcurrentHashMap to ensure atomic updates.
    We can still waste work and parse the annotations of a class twice, but it's not that expensive.

--- core/src/main/java/org/infinispan/factories/ComponentRegistry.java
+++ core/src/main/java/org/infinispan/factories/ComponentRegistry.java
@@ -16,8 +16,8 @@ import org.infinispan.util.ModuleProperties;
 import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import static java.util.Collections.emptyMap;
 import static org.infinispan.factories.KnownComponentNames.MODULE_COMMAND_INITIALIZERS;
@@ -31,7 +31,7 @@ import static org.infinispan.factories.KnownComponentNames.MODULE_COMMAND_INITIA
 public class ComponentRegistry extends AbstractComponentRegistry {
 
    // cached component scopes
-   private static final Map<Class, Scopes> componentScopeLookup = new HashMap<Class, Scopes>();
+   private static final Map<Class, Scopes> componentScopeLookup = new ConcurrentHashMap<Class, Scopes>();
 
    private final GlobalComponentRegistry globalComponents;
    private final String cacheName;
@@ -137,6 +137,8 @@ public class ComponentRegistry extends AbstractComponentRegistry {
    private boolean isGlobal(Class clazz) {
       Scopes componentScope = componentScopeLookup.get(clazz);
       if (componentScope == null) {
+         // Because the detectScope call is not protected by a lock, we can end up doing duplicate work
+         // However this will happen rarely enough that we can afford to ignore the duplicate work.
          componentScope = ScopeDetector.detectScope(clazz);
          componentScopeLookup.put(clazz, componentScope);
       }
