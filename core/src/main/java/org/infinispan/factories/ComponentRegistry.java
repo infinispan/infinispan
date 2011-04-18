d094e7195e6524ae0ea3aaa802663cf0a71efc26
Dan Berindei <dberinde@redhat.com>
2011-04-18 17:07:23 +0300

    ISPN-1051 - Potential concurrency issues when retrieving a cache concurrently (for the first time)
    The issue is that multiple caches were starting at the same time and were
    all trying to register the same components in the global component registry.
    I updated ConcurrentCacheManagerTest to reproduce the issue.
    
    To fix the issue I modified DefaultCacheManager to create just one cache at a time.
    Since the component registry is only modified during cache initialization,
    the synchronization in DCM allows us to not use synchronization at all in
    AbstractComponentRegistry and its subclasses.
    
    I also fixed some small issues in AbstractComponentRegistry:
    * Named components created by factories were registered under the class name key
      instead of using their name.
    * If a factory returned a null object, it would be registered as null but
      the next call would try to create it again.

--- core/src/main/java/org/infinispan/factories/ComponentRegistry.java
+++ core/src/main/java/org/infinispan/factories/ComponentRegistry.java
@@ -31,7 +31,7 @@ import static org.infinispan.factories.KnownComponentNames.MODULE_COMMAND_INITIA
 public class ComponentRegistry extends AbstractComponentRegistry {
 
    // cached component scopes
-   private static final Map<Class, Scopes> componentScopeLookup = new ConcurrentHashMap<Class, Scopes>();
+   private static final Map<Class, Scopes> componentScopeLookup = new ConcurrentHashMap<Class, Scopes>(1);
 
    private final GlobalComponentRegistry globalComponents;
    private final String cacheName;
@@ -92,10 +92,7 @@ public class ComponentRegistry extends AbstractComponentRegistry {
 
    @SuppressWarnings("unchecked")
    public final <T> T getLocalComponent(Class<T> componentType, String name) {
-      Component wrapper = lookupLocalComponent(componentType, name);
-      if (wrapper == null) return null;
-
-      return (T) (wrapper.instance == NULL_COMPONENT ? null : wrapper.instance);
+      return super.getComponent(componentType, name);
    }
 
    public final <T> T getLocalComponent(Class<T> componentType) {
@@ -126,11 +123,11 @@ public class ComponentRegistry extends AbstractComponentRegistry {
    }
 
    @Override
-   public final void registerComponent(Object component, String name) {
+   protected void registerComponentInternal(Object component, String name, boolean nonVolatile) {
       if (isGlobal(component.getClass())) {
-         globalComponents.registerComponent(component, name);
+         globalComponents.registerComponentInternal(component, name, nonVolatile);
       } else {
-         super.registerComponent(component, name);
+         super.registerComponentInternal(component, name, nonVolatile);
       }
    }
 
@@ -148,9 +145,7 @@ public class ComponentRegistry extends AbstractComponentRegistry {
 
    @Override
    public void start() {
-      if (globalComponents.getStatus() != ComponentStatus.RUNNING || globalComponents.getStatus() != ComponentStatus.INITIALIZING) {
-         globalComponents.start();
-      }
+      globalComponents.start();
       boolean needToNotify = state != ComponentStatus.RUNNING && state != ComponentStatus.INITIALIZING;
 
       // set this up *before* starting the components since some components - specifically state transfer - needs to be
