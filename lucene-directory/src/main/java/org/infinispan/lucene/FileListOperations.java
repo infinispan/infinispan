b435e37ef1ec4f448e87abb55d563b996170ec1a
Trustin Lee <trustin@gmail.com>
2011-04-22 10:30:47 +0900

    ISPN-1064 - Fix the license headers
    Updated all license headers with IntelliJ IDEA copyright plugin. The copyrig
    year has been updated with the year when the file was checked into the
    repository first time.

--- lucene-directory/src/main/java/org/infinispan/lucene/FileListOperations.java
+++ lucene-directory/src/main/java/org/infinispan/lucene/FileListOperations.java
@@ -1,8 +1,9 @@
 /*
- * JBoss, Home of Professional Open Source.
- * Copyright 2009, Red Hat Middleware LLC, and individual contributors
- * as indicated by the @author tags. See the copyright.txt file in the
- * distribution for a full listing of individual contributors.
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
  *
  * This is free software; you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as
