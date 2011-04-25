4092c8a2010c3e407e1581e1487c13eac819551c
Trustin Lee <trustin@gmail.com>
2011-04-26 00:09:34 +0900

    ISPN-1027 - Space in path seems to break shell scripts
    Extracted common functionality from all shell scripts into functions.sh
    Set saner file modes for executables

--- demos/gui/src/main/resources/runGuiDemo.sh
+++ demos/gui/src/main/resources/runGuiDemo.sh
@@ -1,100 +1,17 @@
 #!/bin/bash
 
-add_to_classpath()
-{
-  OLD_IFS=$IFS
-  IFS=$'\n'
-  DIR=${1}
-  for i in ${DIR}/*.jar ; do
-    CP=${CP}:${i}
-  done
-  IFS=$OLD_IFS
-}
+source "`dirname "$0"`/functions.sh"
 
-# OS specific support.
-cygwin=false;
-darwin=false;
-mingw=false
-case "`uname`" in
-  CYGWIN*) cygwin=true ;;
-  MINGW*) mingw=true;;
-  Darwin*) darwin=true
-           if [ -z "$JAVA_VERSION" ] ; then
-             JAVA_VERSION="CurrentJDK"
-           else
-             echo "Using Java version: $JAVA_VERSION"
-           fi
-           if [ -z "$JAVA_HOME" ] ; then
-             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
-           fi
-           ;;
-esac
+add_classpath "${ISPN_HOME}"/*.jar
+add_classpath "${ISPN_HOME}/lib"
+add_classpath "${ISPN_HOME}/modules/gui"
 
-if [ -z "$JAVA_HOME" ] ; then
-  if [ -r /etc/gentoo-release ] ; then
-    JAVA_HOME=`java-config --jre-home`
-  fi
-fi
+add_jvm_args $JVM_PARAMS
+add_jvm_args '-Dbind.address=127.0.0.1'
+add_jvm_args '-Djava.net.preferIPv4Stack=true'
 
-# For Cygwin, ensure paths are in UNIX format before anything is touched
-if $cygwin ; then
-  [ -n "$JAVA_HOME" ] &&
-    JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
-fi
+# Sample JPDA settings for remote socket debugging
+#add_jvm_args "-Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
 
-# For Migwn, ensure paths are in UNIX format before anything is touched
-if $mingw ; then
-  [ -n "$JAVA_HOME" ] &&
-    JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
-fi
+start org.infinispan.demo.InfinispanDemo &
 
-if [ -z "$JAVACMD" ] ; then
-  if [ -n "$JAVA_HOME"  ] ; then
-    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
-      # IBM's JDK on AIX uses strange locations for the executables
-      JAVACMD="$JAVA_HOME/jre/sh/java"
-    else
-      JAVACMD="$JAVA_HOME/bin/java"
-    fi
-  else
-    JAVACMD="`which java`"
-  fi
-fi
-
-if [ ! -x "$JAVACMD" ] ; then
-  echo "Error: JAVA_HOME is not defined correctly."
-  echo "  We cannot execute $JAVACMD"
-  exit 1
-fi
-
-DIRNAME=`dirname $0`
-
-# Setup ISPN_HOME
-if [ "x$ISPN_HOME" = "x" ]; then
-    # get the full path (without any relative bits)
-    ISPN_HOME=`cd $DIRNAME/..; pwd`
-fi
-export ISPN_HOME
-
-CP=${CP}:${ISPN_HOME}/etc
-
-add_to_classpath "${ISPN_HOME}"
-add_to_classpath "${ISPN_HOME}/lib"
-add_to_classpath "${ISPN_HOME}/modules/gui"
-add_to_classpath "${ISPN_HOME}/modules/gui/lib"
-
-if $cygwin; then
-   # Turn paths into Windows style for Cygwin
-   CP=`cygpath -wp "${CP}"`
-   LOG4J_CONFIG=`cygpath -w "${ISPN_HOME}/etc/log4j.xml"`
-else
-   LOG4J_CONFIG=${ISPN_HOME}/etc/log4j.xml
-fi
-
-JVM_PARAMS="${JVM_PARAMS} -Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file://${LOG4J_CONFIG// /%20}"
-
-# Sample JPDA settings for remote socket debuging
-#JVM_PARAMS="$JVM_PARAMS -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
-
-#${JAVACMD} -cp "${CP}" "${JVM_PARAMS}" org.infinispan.demo.InfinispanDemo &
-${JAVACMD} -cp "${CP}" ${JVM_PARAMS} org.infinispan.demo.InfinispanDemo &
