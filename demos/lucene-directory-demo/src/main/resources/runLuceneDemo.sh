78886c711c65a5ecc9d056ac3dc52321acd89385
Trustin Lee <trustin@gmail.com>
2011-04-26 00:09:34 +0900

    ISPN-1027 - Space in path seems to break shell scripts
    Extracted common functionality from all shell scripts into functions.sh
    Set saner file modes for executables

--- demos/lucene-directory-demo/src/main/resources/runLuceneDemo.sh
+++ demos/lucene-directory-demo/src/main/resources/runLuceneDemo.sh
@@ -1,92 +1,17 @@
 #!/bin/bash
 
-# OS specific support.
-cygwin=false;
-darwin=false;
-mingw=false
-case "`uname`" in
-  CYGWIN*) cygwin=true ;;
-  MINGW*) mingw=true;;
-  Darwin*) darwin=true
-           if [ -z "$JAVA_VERSION" ] ; then
-             JAVA_VERSION="CurrentJDK"
-           else
-             echo "Using Java version: $JAVA_VERSION"
-           fi
-           if [ -z "$JAVA_HOME" ] ; then
-             JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Versions/${JAVA_VERSION}/Home
-           fi
-           ;;
-esac
+source "`dirname "$0"`/functions.sh"
 
-if [ -z "$JAVA_HOME" ] ; then
-  if [ -r /etc/gentoo-release ] ; then
-    JAVA_HOME=`java-config --jre-home`
-  fi
-fi
+add_classpath "${ISPN_HOME}"/*.jar
+add_classpath "${ISPN_HOME}/lib"
+add_classpath "${ISPN_HOME}/modules/lucene-directory-demo"
 
-# For Cygwin, ensure paths are in UNIX format before anything is touched
-if $cygwin ; then
-  [ -n "$JAVA_HOME" ] &&
-    JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
-fi
-
-# For Migwn, ensure paths are in UNIX format before anything is touched
-if $mingw ; then
-  [ -n "$JAVA_HOME" ] &&
-    JAVA_HOME="`(cd "$JAVA_HOME"; pwd)`"
-fi
-
-if [ -z "$JAVACMD" ] ; then
-  if [ -n "$JAVA_HOME"  ] ; then
-    if [ -x "$JAVA_HOME/jre/sh/java" ] ; then
-      # IBM's JDK on AIX uses strange locations for the executables
-      JAVACMD="$JAVA_HOME/jre/sh/java"
-    else
-      JAVACMD="$JAVA_HOME/bin/java"
-    fi
-  else
-    JAVACMD="`which java`"
-  fi
-fi
-
-if [ ! -x "$JAVACMD" ] ; then
-  echo "Error: JAVA_HOME is not defined correctly."
-  echo "  We cannot execute $JAVACMD"
-  exit 1
-fi
-
-DIRNAME=`dirname $0`
-
-# Setup ISPN_HOME
-if [ "x$ISPN_HOME" = "x" ]; then
-    # get the full path (without any relative bits)
-    ISPN_HOME=`cd $DIRNAME/..; pwd`
-fi
-export ISPN_HOME
-
-CP=${CP}:${ISPN_HOME}/infinispan-core.jar
-
-for i in ${ISPN_HOME}/lib/*.jar ; do
-   CP=${i}:${CP}
-done
-
-for i in ${ISPN_HOME}/modules/lucene-directory-demo/*.jar ; do
-   CP=${i}:${CP}
-done
-
-for i in ${ISPN_HOME}/modules/lucene-directory-demo/lib/*.jar ; do
-   CP=${i}:${CP}
-done
-
-if $cygwin; then
-   # Turn paths into Windows style for Cygwin
-   CP=`cygpath -wp ${CP}`
-fi
-
-JVM_PARAMS="${JVM_PARAMS} -Dbind.address=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=file:${ISPN_HOME}/etc/log4j.xml"
+add_jvm_args $JVM_PARAMS
+add_jvm_args '-Dbind.address=127.0.0.1'
+add_jvm_args '-Djava.net.preferIPv4Stack=true'
 
 # Sample JPDA settings for remote socket debugging
-#JVM_PARAMS="$JVM_PARAMS -Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
+#add_jvm_args "-Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
 
-${JAVACMD} -cp ${CP} ${JVM_PARAMS} org.infinispan.lucenedemo.DemoDriver
+start org.infinispan.lucenedemo.DemoDriver
+
