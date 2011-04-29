05c75f8301b59dce1f6ea2a639d89ab68736f854
Manik Surtani <manik@surtani.org>
2011-04-29 12:47:11 -0400

    Refactor demos for readability, better comments and configuration

---
+++ demos/distexec/src/main/java/org/infinispan/demo/CacheBuilder.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+package org.infinispan.demo;
+
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.util.FileLookup;
+
+import java.io.IOException;
+
+/**
+ * Builds CacheManager given Infinispan configuration and transport file.
+ */
+public class CacheBuilder {
+
+   private EmbeddedCacheManager cacheManager;
+
+   public CacheBuilder(String ispnConfigFile) throws IOException {
+      cacheManager = new DefaultCacheManager(findConfigFile(ispnConfigFile));
+   }
+
+   public EmbeddedCacheManager getCacheManager() {
+      return this.cacheManager;
+   }
+
+   private String findConfigFile(String configFile) {
+      String selectedConfigFile = null;
+      FileLookup fl = new FileLookup();
+      if (configFile != null && fl.lookupFile(configFile) != null) selectedConfigFile = configFile;
+
+      return selectedConfigFile;
+   }
+}
