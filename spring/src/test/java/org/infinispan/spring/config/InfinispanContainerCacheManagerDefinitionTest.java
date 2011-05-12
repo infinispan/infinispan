8eb9cd12c26eb8477cfaeaa3ee030b77ea107a87
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-05-12 02:22:41 -0400

    ISPN-1067 Initial support for Spring namespace
    * Added an additional type of FactoryBean for creating Spring
      CacheManagers using an existing CacheContainer reference
      (Currently supporting only EmbeddedCacheManager and
      RemoteCacheManager references)
    
    * Added namespace elements for:
      - <embedded-cache-manager> with optional configuration file reference
      - <remote-cache-manager> with optional configuration file reference
      - <container-cache-manager> with a mandatory cache container reference

---
+++ spring/src/test/java/org/infinispan/spring/config/InfinispanContainerCacheManagerDefinitionTest.java
@@ -0,0 +1,54 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other
+ * contributors as indicated by the @author tags. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing of
+ * individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *    ~
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.infinispan.spring.config;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.CacheManager;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * @author Marius Bogoevici
+ */
+
+@ContextConfiguration
+public class InfinispanContainerCacheManagerDefinitionTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired @Qualifier("cacheManager")
+    private CacheManager containerCacheManager;
+
+    @Autowired @Qualifier("cacheManager2")
+    private CacheManager containerCacheManager2;
+
+    @Test
+    public void testContainerCacheManagerExists() {
+       Assert.assertNotNull(containerCacheManager);
+       Assert.assertNotNull(containerCacheManager2);
+    }
+
+}
