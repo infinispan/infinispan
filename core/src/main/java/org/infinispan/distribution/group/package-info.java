4f1ade5a9b7407ca9a4b4bf4fdab366f80b6dbbd
Pete Muir <pmuir@bleepbleep.org.uk>
2011-05-19 18:36:28 +0100

    Add some package javadoc

---
+++ core/src/main/java/org/infinispan/distribution/group/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * <p>
+ * Groups allow keys with differeing hash codes to be co-located on the same node.
+ * </p>
+ * 
+ * <p>
+ * Infinispan offers support for both instrinsic grouping ( see{@link org.infinispan.distribution.group.Group})
+ * and extrinsic grouping (see {@link org.infinispan.distribution.group.Grouper}). 
+ * </p>
+ */
+package org.infinispan.distribution.group;
+
