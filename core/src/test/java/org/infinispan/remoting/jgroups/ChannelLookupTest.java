34c30037b5d9e444ea9ad48f38065ec7cbc1ca96
Dan Berindei <dberinde@redhat.com>
2011-05-28 20:32:09 +0300

    ISPN-1123 - Fix easymock exception on test suite shutdown
    Call EasyMock.verify at the end of the test for the JChannel mock

--- core/src/test/java/org/infinispan/remoting/jgroups/ChannelLookupTest.java
+++ core/src/test/java/org/infinispan/remoting/jgroups/ChannelLookupTest.java
@@ -25,7 +25,6 @@ package org.infinispan.remoting.jgroups;
 import org.easymock.classextension.EasyMock;
 import org.infinispan.config.GlobalConfiguration;
 import org.infinispan.factories.GlobalComponentRegistry;
-import org.infinispan.manager.CacheContainer;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.remoting.transport.Transport;
 import org.infinispan.remoting.transport.jgroups.JGroupsChannelLookup;
@@ -44,49 +43,53 @@ import java.util.Properties;
 
 @Test(testName = "remoting.jgroups.ChannelLookupTest", groups = "functional")
 public class ChannelLookupTest extends AbstractInfinispanTest {
-    public void channelLookupTest() {
-        EmbeddedCacheManager cm = null;
-        try {
-            GlobalConfiguration gc = GlobalConfiguration.getClusteredDefault();
-            Properties p = new Properties();
-            p.setProperty("channelLookup", DummyLookup.class.getName());
-            gc.setTransportProperties(p);
-            cm = TestCacheManagerFactory.createCacheManager(gc);
-            cm.start();
-            cm.getCache();
-            GlobalComponentRegistry gcr = TestingUtil.extractGlobalComponentRegistry(cm);
-            Transport t = gcr.getComponent(Transport.class);
-            assert t != null;
-            assert t instanceof JGroupsTransport;
-            assert !(((JGroupsTransport) t).getChannel() instanceof JChannel);
-        } finally {
-            TestingUtil.killCacheManagers(cm);
-        }
-    }
+   static Channel mockChannel = EasyMock.createNiceMock(Channel.class);
+   Address a = EasyMock.createNiceMock(Address.class);
+   ProtocolStack ps = EasyMock.createNiceMock(ProtocolStack.class);
 
-    public static class DummyLookup implements JGroupsChannelLookup {
-        Channel mockChannel;
-        Address a = EasyMock.createNiceMock(Address.class);
-        ProtocolStack ps = EasyMock.createNiceMock(ProtocolStack.class);
+   public void channelLookupTest() {
+      EasyMock.reset(mockChannel);
+      EasyMock.expect(mockChannel.getAddress()).andReturn(a);
+      EasyMock.expect(mockChannel.getProtocolStack()).andReturn(ps);
+      EasyMock.expect(ps.getTransport()).andReturn(new UDP());
+      EasyMock.replay(mockChannel, a, ps);
 
-        public DummyLookup() {
-            mockChannel = EasyMock.createNiceMock(Channel.class);
-            EasyMock.expect(mockChannel.getAddress()).andReturn(a);
-            EasyMock.expect(mockChannel.getProtocolStack()).andReturn(ps);
-            EasyMock.expect(ps.getTransport()).andReturn(new UDP());
-            EasyMock.replay(mockChannel, a, ps);           
-        }
+      EmbeddedCacheManager cm = null;
+      try {
+         GlobalConfiguration gc = GlobalConfiguration.getClusteredDefault();
+         Properties p = new Properties();
+         p.setProperty("channelLookup", DummyLookup.class.getName());
+         gc.setTransportProperties(p);
+         cm = TestCacheManagerFactory.createCacheManager(gc);
+         cm.start();
+         cm.getCache();
+         EasyMock.verify(mockChannel, a, ps);
 
-        public Channel getJGroupsChannel(Properties p) {
-            return mockChannel;
-        }
+         GlobalComponentRegistry gcr = TestingUtil.extractGlobalComponentRegistry(cm);
+         Transport t = gcr.getComponent(Transport.class);
+         assert t != null;
+         assert t instanceof JGroupsTransport;
+         assert !(((JGroupsTransport) t).getChannel() instanceof JChannel);
+      } finally {
+         TestingUtil.killCacheManagers(cm);
+      }
+   }
 
-        public boolean shouldStartAndConnect() {
-            return false;
-        }
+   public static class DummyLookup implements JGroupsChannelLookup {
 
-        public boolean shouldStopAndDisconnect() {
-            return false;
-        }
-    }
+      public DummyLookup() {
+      }
+
+      public Channel getJGroupsChannel(Properties p) {
+         return mockChannel;
+      }
+
+      public boolean shouldStartAndConnect() {
+         return false;
+      }
+
+      public boolean shouldStopAndDisconnect() {
+         return false;
+      }
+   }
 }
