25d0369d4a75ce2b449791343c65e01e1d6abde1
Sanne Grinovero <sanne@hibernate.org>
2011-06-16 19:36:54 +0100

    SearchFactory leak

--- query/src/main/java/org/infinispan/query/impl/LifecycleManager.java
+++ query/src/main/java/org/infinispan/query/impl/LifecycleManager.java
@@ -140,13 +140,14 @@ public class LifecycleManager extends AbstractModuleLifecycle {
       //TODO move this to cacheStopped event (won't work right now as the ComponentRegistry is half empty at that point: ISPN-1006)
       SearchFactoryIntegrator searchFactoryImplementor = cr.getComponent(SearchFactoryIntegrator.class);
       if (searchFactoryImplementor != null) {
+         cr.registerComponent(NoOpSearchFactory.INSTANCE, SearchFactoryIntegrator.class);
          searchFactoriesToShutdown.put(cacheName, searchFactoryImplementor);
       }
    }
    
    @Override
    public void cacheStopped(ComponentRegistry cr, String cacheName) {
-      SearchFactoryIntegrator searchFactoryIntegrator = searchFactoriesToShutdown.get(cacheName);
+      SearchFactoryIntegrator searchFactoryIntegrator = searchFactoriesToShutdown.remove(cacheName);
       if (searchFactoryIntegrator != null) {
          searchFactoryIntegrator.close();
       }
