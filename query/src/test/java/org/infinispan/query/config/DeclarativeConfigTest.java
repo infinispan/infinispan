6650b6ac61bd22ab847996aacbbaa95ff2ed6715
Sanne Grinovero <sanne@hibernate.org>
2011-08-01 20:08:55 +0100

    ISPN-200 - update to latest Search v.4 snapshot

--- query/src/test/java/org/infinispan/query/config/DeclarativeConfigTest.java
+++ query/src/test/java/org/infinispan/query/config/DeclarativeConfigTest.java
@@ -23,8 +23,10 @@
 package org.infinispan.query.config;
 
 import org.apache.lucene.queryParser.ParseException;
+import org.hibernate.search.engine.spi.EntityIndexBinder;
+import org.hibernate.search.indexes.impl.DirectoryBasedIndexManager;
+import org.hibernate.search.indexes.spi.IndexManager;
 import org.hibernate.search.spi.SearchFactoryIntegrator;
-import org.hibernate.search.store.DirectoryProvider;
 import org.hibernate.search.store.impl.RAMDirectoryProvider;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.query.CacheQuery;
@@ -70,7 +72,7 @@ public class DeclarativeConfigTest extends SingleCacheManagerTest {
 
    public void simpleIndexTest() throws ParseException {
       cache.put("1", new Person("A Person's Name", "A paragraph containing some text", 75));
-      CacheQuery cq = TestQueryHelperFactory.createCacheQuery(cache, "name", "Person");
+      CacheQuery cq = TestQueryHelperFactory.createCacheQuery(cache, "name", "Name");
       assertEquals(1, cq.getResultSize());
       List<Object> l =  cq.list();
       assertEquals(1, l.size());
@@ -79,14 +81,17 @@ public class DeclarativeConfigTest extends SingleCacheManagerTest {
       assertEquals("A paragraph containing some text", p.getBlurb());
       assertEquals(75, p.getAge());
    }
-   
+
    @Test(dependsOnMethods="simpleIndexTest") //depends as otherwise the Person index is not initialized yet
    public void testPropertiesWhereRead() {
       SearchFactoryIntegrator searchFactory = TestQueryHelperFactory.extractSearchFactory(cache);
-      DirectoryProvider[] directoryProviders = searchFactory.getDirectoryProviders(Person.class);
-      assertEquals(1, directoryProviders.length);
-      assertNotNull(directoryProviders[0]);
-      assertTrue(directoryProviders[0] instanceof RAMDirectoryProvider);
+      EntityIndexBinder<Person> indexBindingForEntity = searchFactory.getIndexBindingForEntity(Person.class);
+      IndexManager[] managers = indexBindingForEntity.getIndexManagers();
+      assertEquals(1, managers.length);
+      assertNotNull(managers[0]);
+      assertTrue(managers[0] instanceof DirectoryBasedIndexManager);
+      DirectoryBasedIndexManager dbim = (DirectoryBasedIndexManager) managers[0];
+      assertTrue(dbim.getDirectoryProvider() instanceof RAMDirectoryProvider);
    }
-   
+
 }
