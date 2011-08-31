9783da56d88f7fa0b908869bc2959681d2cb1ed7
Galder Zamarreño <galder@jboss.org>
2011-08-31 11:26:24 +0200

    ISPN-1357 Fix several RHQ monitoring issues
    * Make sure JBoss Logging gets included within the RHQ plugin,
    otherwise ClassNotFoundException is thrown.
    * CacheManager component should be able to cope with measurements
    that are ints, long...etc.
    * Cache discovery was broken due for default cache due to different
    cache name representations. Make sure the internal name, which is the
    one used in the MBean is used to locate the default cache.
    * Added commented lines to demo scripts to make it easier for users
    to enable RHQ monitoring system properties.

--- demos/lucene-directory-demo/src/main/resources/runLuceneDemo.sh
+++ demos/lucene-directory-demo/src/main/resources/runLuceneDemo.sh
@@ -10,6 +10,11 @@ add_jvm_args $JVM_PARAMS
 add_jvm_args '-Dbind.address=127.0.0.1'
 add_jvm_args '-Djava.net.preferIPv4Stack=true'
 
+# RHQ monitoring options
+#add_jvm_args '-Dcom.sun.management.jmxremote.ssl=false'
+#add_jvm_args '-Dcom.sun.management.jmxremote.authenticate=false'
+#add_jvm_args '-Dcom.sun.management.jmxremote.port=6996'
+
 # Sample JPDA settings for remote socket debugging
 #add_jvm_args "-Xrunjdwp:transport=dt_socket,address=8686,server=y,suspend=n"
 
