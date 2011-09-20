4fbe4880529e421f8a31581ad6381905c9b460bf
Sanne Grinovero <sanne@hibernate.org>
2011-09-20 12:01:41 +0100

    Query examples: map/reduce or indexed

---
+++ core/src/test/java/org/infinispan/distexec/mapreduce/Book.java
@@ -0,0 +1,43 @@
+/* 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+
+package org.infinispan.distexec.mapreduce;
+
+import java.io.Serializable;
+
+/**
+ * Example object to run a query using MapReduce.
+ * Using Java here, but it could be anything such
+ * as free text, JSON, YAML, ...
+ *
+ * @author Sanne Grinovero <sanne@infinispan.org> (C) 2011 Red Hat Inc.
+ */
+public class Book implements Serializable {
+
+   final String title;
+   final String author;
+   final String editor;
+
+   public Book(String title, String author, String editor) {
+      this.title = title;
+      this.author = author;
+      this.editor = editor;
+   }
+
+}
