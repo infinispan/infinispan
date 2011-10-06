539e8cef935b6297c30bbbf08e104702e9f20368
Sanne Grinovero <sanne@hibernate.org>
2011-10-06 12:31:15 +0100

    example usage with dynamic properties

---
+++ query/src/test/java/org/infinispan/query/dynamicexample/DynamicPropertiesEntity.java
@@ -0,0 +1,50 @@
+/* 
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011 Red Hat Inc. and/or its affiliates and other contributors
+ * as indicated by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU Lesser General Public License, v. 2.1.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License,
+ * v.2.1 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301, USA.
+ */
+package org.infinispan.query.dynamicexample;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.Field;
+import org.hibernate.search.annotations.FieldBridge;
+import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.ProvidedId;
+import org.hibernate.search.annotations.Store;
+
+/**
+ * @author Sanne Grinovero <sanne@infinispan.org> (C) 2011 Red Hat Inc.
+ */
+@ProvidedId @Indexed
+public class DynamicPropertiesEntity {
+
+   private final Map<String,String> properties = new HashMap<String, String>();
+
+   @Field(analyze=Analyze.YES, store=Store.YES)
+   @FieldBridge(impl=StringKeyedMapBridge.class)
+   public Map<String, String> getProperties() {
+      return properties;
+   }
+
+   public DynamicPropertiesEntity set(String key, String value) {
+      properties.put(key, value);
+      return this;
+   }
+
+}
