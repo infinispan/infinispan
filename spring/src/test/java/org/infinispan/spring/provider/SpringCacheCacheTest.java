03dc02f2e69bc82033dd3e5e96c8ba0bc43f93b3
Marius Bogoevici <marius.bogoevici@gmail.com>
2011-10-21 09:58:26 -0400

    ISPN-1468: Upgrade to Spring 3.1.0.RC1
    - replace DefaultValueWrapper with ValueWrapperImpl (class renamed in Spring)

--- spring/src/test/java/org/infinispan/spring/provider/SpringCacheCacheTest.java
+++ spring/src/test/java/org/infinispan/spring/provider/SpringCacheCacheTest.java
@@ -31,7 +31,6 @@ import static org.testng.AssertJUnit.assertTrue;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.spring.support.embedded.InfinispanNamedEmbeddedCacheFactoryBean;
 import org.springframework.cache.Cache;
-import org.springframework.cache.interceptor.DefaultValueWrapper;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
