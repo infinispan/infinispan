2bc69e0cd3dbcde87ad642094f88462fab10aa99
Manik Surtani <manik@surtani.org>
2011-12-05 14:36:34 +0000

    Fix broken build due to CDI integration tests

--- cdi/extension/src/test/java/org/infinispan/cdi/test/cache/remote/NamedCacheTest.java
+++ cdi/extension/src/test/java/org/infinispan/cdi/test/cache/remote/NamedCacheTest.java
@@ -22,7 +22,7 @@
  */
 package org.infinispan.cdi.test.cache.remote;
 
-import org.infinispan.BasicCache;
+import org.infinispan.api.BasicCache;
 import org.infinispan.cdi.Remote;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
