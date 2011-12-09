e4e0efc1cb245674f65c88cec02b677964842ddb
Pete Muir <pmuir@bleepbleep.org.uk>
2011-12-09 10:45:40 +0000

    ISPN-1598 again

--- core/src/main/java/org/infinispan/configuration/cache/TransactionConfigurationBuilder.java
+++ core/src/main/java/org/infinispan/configuration/cache/TransactionConfigurationBuilder.java
@@ -69,7 +69,7 @@ public class TransactionConfigurationBuilder extends AbstractConfigurationChildB
       return this;
    }
    
-   public TransactionConfigurationBuilder transactionSyncronizationRegisteryLookup(TransactionSynchronizationRegistryLookup lookup) {
+   public TransactionConfigurationBuilder transactionSynchronizationRegistryLookup(TransactionSynchronizationRegistryLookup lookup) {
       this.transactionSynchronizationRegistryLookup = lookup;
       return this;
    }
