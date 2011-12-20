0d31df14987d27c3b447e8582e8744af1c24f1ac
Sanne Grinovero <sanne@hibernate.org>
2011-12-20 02:08:57 +0100

    ISPN-1628 Configuration parser: Indexing properties are a nested element

--- core/src/main/java/org/infinispan/configuration/parsing/Parser.java
+++ core/src/main/java/org/infinispan/configuration/parsing/Parser.java
@@ -22,6 +22,7 @@ import javax.xml.stream.XMLStreamReader;
 import org.infinispan.config.ConfigurationException;
 import org.infinispan.configuration.cache.AbstractLoaderConfigurationBuilder;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.IndexingConfigurationBuilder;
 import org.infinispan.configuration.cache.InterceptorConfiguration.Position;
 import org.infinispan.configuration.cache.FileCacheStoreConfigurationBuilder;
 import org.infinispan.configuration.cache.InterceptorConfigurationBuilder;
@@ -615,9 +616,23 @@ public class Parser {
                throw ParseUtils.unexpectedAttribute(reader, i);
          }
       }
-      
-      builder.indexing().withProperties(parseProperties(reader));
-      
+      Properties indexingProperties = null;
+      while (reader.hasNext() && (reader.nextTag() != XMLStreamConstants.END_ELEMENT)) {
+         Element element = Element.forName(reader.getLocalName());
+         switch (element) {
+            case PROPERTIES: {
+               indexingProperties = parseProperties(reader);
+               break;
+            }
+            default: {
+               throw ParseUtils.unexpectedElement(reader);
+            }
+         }
+      }
+      IndexingConfigurationBuilder indexing = builder.indexing();
+      if (indexingProperties != null) {
+         indexing.withProperties(indexingProperties);
+      }
    }
 
    private void parseExpiration(XMLStreamReader reader, ConfigurationBuilder builder) throws XMLStreamException {
