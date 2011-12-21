983621155039e910de08e8439655c818dbca900c
Manik Surtani <manik@surtani.org>
2011-12-21 11:08:57 +0000

    ISPN-1638 Return values should only be serialized if needed when using Distribution

---
+++ core/src/main/java/org/infinispan/remoting/responses/NoReturnValuesDistributionResponseGenerator.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+package org.infinispan.remoting.responses;
+
+import org.infinispan.commands.write.WriteCommand;
+
+public class NoReturnValuesDistributionResponseGenerator extends DistributionResponseGenerator {
+   @Override
+   protected Response handleWriteCommand(WriteCommand wc, Object returnValue) {
+      return wc.isSuccessful() ? null : UnsuccessfulResponse.INSTANCE;
+   }
+}
