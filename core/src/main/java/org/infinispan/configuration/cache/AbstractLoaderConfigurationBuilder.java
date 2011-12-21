ca11f828879263d6f0812ae990b68a0b538af33c
Galder Zamarreño <galder@zamarreno.com>
2011-12-21 10:35:00 +0100

    ISPN-1566 Remove duplicate async/singletonStore definitions
    This fixes Jbc2InfinispanTransformerTest which has been failing
    since the original jira was implemented.

--- core/src/main/java/org/infinispan/configuration/cache/AbstractLoaderConfigurationBuilder.java
+++ core/src/main/java/org/infinispan/configuration/cache/AbstractLoaderConfigurationBuilder.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright 2011 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
 package org.infinispan.configuration.cache;
 
 import java.util.Properties;
@@ -5,9 +24,9 @@ import java.util.Properties;
 /*
  * This is slightly different AbstractLoaderConfigurationChildBuilder, as it instantiates a new set of children (async and singletonStore)
  * rather than delegate to existing ones. 
- * 
  */
-public abstract class AbstractLoaderConfigurationBuilder<T extends AbstractLoaderConfiguration> extends AbstractLoadersConfigurationChildBuilder<T> {
+public abstract class AbstractLoaderConfigurationBuilder<T extends AbstractLoaderConfiguration>
+      extends AbstractLoadersConfigurationChildBuilder<T> {
 
    protected final AsyncLoaderConfigurationBuilder async;
    protected final SingletonStoreConfigurationBuilder singletonStore;
