73be4d073c216f2d8db964f9f804bb5bb52e12ef
Galder Zamarreño <galder@zamarreno.com>
2012-01-03 13:49:57 +0100

    ISPN-1655 Upgrade to JGroups 3.0.2

--- core/src/main/java/org/infinispan/remoting/InboundInvocationHandlerImpl.java
+++ core/src/main/java/org/infinispan/remoting/InboundInvocationHandlerImpl.java
@@ -143,8 +143,10 @@ public class InboundInvocationHandlerImpl implements InboundInvocationHandler {
       ComponentRegistry cr = gcr.getNamedComponentRegistry(cacheName);
 
       if (cr == null) {
-         if (!globalConfiguration.isStrictPeerToPeer())
+         if (!globalConfiguration.isStrictPeerToPeer()) {
+            if (trace) log.tracef("Strict peer to peer off, so silently ignoring that %s cache is not defined", cacheName);
             return null;
+         }
 
          log.namedCacheDoesNotExist(cacheName);
          return new ExceptionResponse(new NamedCacheNotFoundException(cacheName, "Cache has not been started on node " + transport.getAddress()));
