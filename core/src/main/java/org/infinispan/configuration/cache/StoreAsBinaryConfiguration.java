048a2f651c92fcf2f9e9909758ae22ffdd670317
Manik Surtani <manik@surtani.org>
2012-01-16 15:56:37 +0000

    ISPN-1735 StoreAsBinary - all properties missing configuration (javadoc and xsddoc)

--- core/src/main/java/org/infinispan/configuration/cache/StoreAsBinaryConfiguration.java
+++ core/src/main/java/org/infinispan/configuration/cache/StoreAsBinaryConfiguration.java
@@ -1,12 +1,15 @@
 package org.infinispan.configuration.cache;
 
 /**
- * A mechanism by which data is stored as a binary byte array. This allows
- * serialization and deserialization of objects is deferred till the point
- * in time in which they are used and needed. This typically means that any
- * deserialization happens using the thread context class loader of the
- * invocation that requires deserialization, and is an effective mechanism
- * to provide classloader isolation.
+ * Controls whether when stored in memory, keys and values are stored as references to their original objects, or in
+ * a serialized, binary format.  There are benefits to both approaches, but often if used in a clustered mode,
+ * storing objects as binary means that the cost of serialization happens early on, and can be amortized.  Further,
+ * deserialization costs are incurred lazily which improves throughput.
+ * <p />
+ * It is possible to control this on a fine-grained basis: you can choose to just store keys or values as binary, or
+ * both.
+ * <p />
+ * @see StoreAsBinaryConfigurationBuilder
  */
 public class StoreAsBinaryConfiguration {
 
@@ -20,14 +23,23 @@ public class StoreAsBinaryConfiguration {
       this.storeValuesAsBinary = storeValuesAsBinary;
    }
 
+   /**
+    * Enables storing both keys and values as binary.
+    */
    public boolean enabled() {
       return enabled;
    }
 
+   /**
+    * Enables storing keys as binary.
+    */
    public boolean storeKeysAsBinary() {
       return storeKeysAsBinary;
    }
 
+   /**
+    * Enables storing values as binary.
+    */
    public boolean storeValuesAsBinary() {
       return storeValuesAsBinary;
    }   
