ea5ea9c1cf7c7d0f4e140845d28b6890dd7cc44d
Sanne Grinovero <sanne@hibernate.org>
2012-01-31 11:21:20 +0000

    ISPN-1815 Refactoring: integrate methods, now hand-inlining is possible

--- core/src/main/java/org/infinispan/marshall/jboss/AbstractJBossMarshaller.java
+++ core/src/main/java/org/infinispan/marshall/jboss/AbstractJBossMarshaller.java
@@ -8,7 +8,6 @@ import org.infinispan.util.ConcurrentWeakKeyHashMap;
 import org.infinispan.util.logging.BasicLogFactory;
 import org.jboss.logging.BasicLogger;
 import org.jboss.marshalling.ExceptionListener;
-import org.jboss.marshalling.Marshaller;
 import org.jboss.marshalling.Marshalling;
 import org.jboss.marshalling.MarshallingConfiguration;
 import org.jboss.marshalling.TraceInformation;
@@ -98,23 +97,14 @@ public abstract class AbstractJBossMarshaller extends AbstractMarshaller impleme
    }
 
    final public ObjectOutput startObjectOutput(final OutputStream os, final boolean isReentrant, final int estimatedSize) throws IOException {
-      org.jboss.marshalling.Marshaller marshaller = getMarshaller(isReentrant, estimatedSize);
+      PerThreadInstanceHolder instanceHolder = marshallerTL.get();
+      org.jboss.marshalling.Marshaller marshaller = instanceHolder.getMarshaller(estimatedSize);
       marshaller.start(Marshalling.createByteOutput(os));
       return marshaller;
    }
 
    final public ObjectOutput startObjectOutput(final OutputStream os, final boolean isReentrant) throws IOException {
       return startObjectOutput(os, isReentrant, 512);
-   }
-
-   private final Marshaller getMarshaller(boolean isReentrant, final int estimatedSize) throws IOException {
-      PerThreadInstanceHolder instanceHolder = marshallerTL.get();
-      return instanceHolder.getMarshaller(estimatedSize);
-   }
-
-   private final Unmarshaller getUnmarshaller(boolean isReentrant) throws IOException {
-      PerThreadInstanceHolder instanceHolder = marshallerTL.get();
-      return instanceHolder.getUnmarshaller();
    }
 
    final public void finishObjectOutput(final ObjectOutput oo) {
@@ -141,7 +131,8 @@ public abstract class AbstractJBossMarshaller extends AbstractMarshaller impleme
    }
 
    final public ObjectInput startObjectInput(final InputStream is, final boolean isReentrant) throws IOException {
-      Unmarshaller unmarshaller = getUnmarshaller(isReentrant);
+      PerThreadInstanceHolder instanceHolder = marshallerTL.get();
+      Unmarshaller unmarshaller = instanceHolder.getUnmarshaller();
 
       if (trace)
          log.tracef("Start unmarshaller after retrieving marshaller from %s",
