fd89678a6c5c9637afc60e9e93154322e82f9e65
Galder Zamarreño <galder@zamarreno.com>
2012-02-08 12:17:50 +0100

    ISPN-1844 Revert to old schema location

--- core/src/test/java/org/infinispan/configuration/ConfigurationUnitTest.java
+++ core/src/test/java/org/infinispan/configuration/ConfigurationUnitTest.java
@@ -209,7 +209,7 @@ public class ConfigurationUnitTest {
    @Test
    public void testSchema() throws Exception {
       FileLookup lookup = FileLookupFactory.newInstance();
-      URL schemaFile = lookup.lookupFileLocation("infinispan-5.1.xsd", Thread.currentThread().getContextClassLoader());
+      URL schemaFile = lookup.lookupFileLocation("schema/infinispan-config-5.1.xsd", Thread.currentThread().getContextClassLoader());
       Source xmlFile = new StreamSource(lookup.lookupFile("configs/all.xml", Thread.currentThread().getContextClassLoader()));
       SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI).newSchema(schemaFile).newValidator().validate(xmlFile);
    }
