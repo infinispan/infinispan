07a49384efe6c6cbf87842a189728bd548fe6edd
Galder Zamarreño <galder@zamarreno.com>
2012-02-23 09:49:17 +0100

    ISPN-1874 Add RHQ monitoring options for .bat scripts too

--- server/core/src/main/resources/startServer.bat
+++ server/core/src/main/resources/startServer.bat
@@ -14,6 +14,11 @@ set CP=..\modules\memcached\infinispan-server-memcached.jar;%CP%
 set CP=..\modules\hotrod\infinispan-server-hotrod.jar;%CP%
 set CP=..\modules\websocket\infinispan-server-websocket.jar;%CP%
 
-java -classpath "%CP%" -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=..\etc\log4j.xml org.infinispan.server.core.Main %*
+:test
+set /a "TESTPORT=%RANDOM%+2000"
+netstat -an | findstr ":%TESTPORT% "
+if %ERRORLEVEL%==0 goto test
+
+java -classpath "%CP%" -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=%TESTPORT% -Djgroups.bind_addr=127.0.0.1 -Djava.net.preferIPv4Stack=true -Dlog4j.configuration=..\etc\log4j.xml org.infinispan.server.core.Main %*
 
 :fileEnd
