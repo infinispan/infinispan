9d9aee57c8ae0f7289657c09c3de9a86c9e35e17
Tristan Tarrant <ttarrant@redhat.com>
2012-02-24 09:33:09 +0100

    Modified the RHQ plugin: - depend on the RHQ 4.2.0 artifacts - use commons-logging instead of jboss-logging to align with the other RHQ plugins - specify that Infinispan may be found also in AS7 standalone and profile configurations - clean up the build so that the plugin doesn't depend on infinispan-core - Clear up the POMs - Check for null attributes in CacheManagerComponent (thanks Shane Johnson)

--- rhq-plugin/src/main/java/org/infinispan/rhq/CacheManagerComponent.java
+++ rhq-plugin/src/main/java/org/infinispan/rhq/CacheManagerComponent.java
@@ -22,8 +22,15 @@
  */
 package org.infinispan.rhq;
 
-import org.infinispan.rhq.logging.Log;
-import org.infinispan.util.logging.LogFactory;
+import static org.infinispan.rhq.RhqUtil.constructNumericMeasure;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.management.ObjectName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.mc4j.ems.connection.EmsConnection;
 import org.mc4j.ems.connection.bean.EmsBean;
 import org.mc4j.ems.connection.bean.attribute.EmsAttribute;
@@ -34,16 +41,8 @@ import org.rhq.core.domain.measurement.MeasurementDataTrait;
 import org.rhq.core.domain.measurement.MeasurementReport;
 import org.rhq.core.domain.measurement.MeasurementScheduleRequest;
 import org.rhq.core.pluginapi.inventory.ResourceContext;
-import org.rhq.plugins.jmx.JMXComponent;
 import org.rhq.plugins.jmx.MBeanResourceComponent;
 import org.rhq.plugins.jmx.ObjectNameQueryUtility;
-
-import javax.management.ObjectName;
-import java.util.List;
-import java.util.Set;
-
-import static org.infinispan.jmx.CacheManagerJmxRegistration.CACHE_MANAGER_JMX_GROUP;
-import static org.infinispan.rhq.RhqUtil.constructNumericMeasure;
 
 /**
  * The component class for the Infinispan manager
@@ -51,9 +50,9 @@ import static org.infinispan.rhq.RhqUtil.constructNumericMeasure;
  * @author Heiko W. Rupp
  * @author Galder Zamarreño
  */
-public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceComponent> {
-   private static final Log log = LogFactory.getLog(CacheManagerComponent.class, Log.class);
-   protected ResourceContext<JMXComponent> context;
+public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceComponent<?>> {
+   private static final Log log = LogFactory.getLog(CacheManagerComponent.class);
+   protected ResourceContext<MBeanResourceComponent<?>> context;
    private String cacheManagerPattern;
 
    /**
@@ -62,6 +61,7 @@ public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceC
     *
     * @see org.rhq.core.pluginapi.inventory.ResourceComponent#getAvailability()
     */
+   @Override
    public AvailabilityType getAvailability() {
       boolean trace = log.isTraceEnabled();
       EmsConnection conn = getEmsConnection();
@@ -70,7 +70,7 @@ public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceC
          EmsBean bean = queryCacheManagerBean(conn);
          if (bean != null) {
             bean.refreshAttributes();
-            if (trace) log.tracef("Cache manager %s could be found and attributes where refreshed, so it's up.", bean);
+            if (trace) log.trace("Cache manager "+bean+" could be found and attributes where refreshed, so it's up.");
             return AvailabilityType.UP;
          }
          if (trace) log.trace("Cache manager could not be found, so cache manager is down");
@@ -84,11 +84,11 @@ public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceC
    /**
     * Start the resource connection
     */
-   @SuppressWarnings("unchecked")
-   public void start(ResourceContext context) {
+   @Override
+   public void start(ResourceContext<MBeanResourceComponent<?>> context) {
       // TODO: Call super.start() ?
       this.context = context;
-      this.cacheManagerPattern = "*:" + CACHE_MANAGER_JMX_GROUP + ",name=" + ObjectName.quote(context.getResourceKey()) + ",*";
+      this.cacheManagerPattern = "*:" + CacheManagerDiscovery.CACHE_MANAGER_JMX_GROUP + ",name=" + ObjectName.quote(context.getResourceKey()) + ",*";
    }
 
    @Override
@@ -102,23 +102,26 @@ public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceC
     * @see org.rhq.core.pluginapi.measurement.MeasurementFacet#getValues(org.rhq.core.domain.measurement.MeasurementReport,
     *      java.util.Set)
     */
+   @Override
    public void getValues(MeasurementReport report, Set<MeasurementScheduleRequest> metrics) {
       boolean trace = log.isTraceEnabled();
-      if (trace) log.tracef("Get values for these metrics: %s", metrics);
+      if (trace) log.trace("Get values for these metrics: " + metrics);
       EmsConnection conn = getEmsConnection();
-      if (trace) log.tracef("Connection to ems server established: %s", conn);
+      if (trace) log.trace("Connection to ems server established: " + conn);
       EmsBean bean = queryCacheManagerBean(conn);
       bean.refreshAttributes();
-      if (trace) log.tracef("Querying returned bean: %s", bean);
+      if (trace) log.trace("Querying returned bean: " + bean);
       for (MeasurementScheduleRequest req : metrics) {
          DataType type = req.getDataType();
          if (type == DataType.MEASUREMENT) {
             EmsAttribute att = bean.getAttribute(req.getName());
-            MeasurementDataNumeric res = constructNumericMeasure(att.getTypeClass(), att.getValue(), req);
-            report.addData(res);
+            if (att != null) {
+               MeasurementDataNumeric res = constructNumericMeasure(att.getTypeClass(), att.getValue(), req);
+               report.addData(res);
+            }
          } else if (type == DataType.TRAIT) {
             String value = (String) bean.getAttribute(req.getName()).getValue();
-            if (trace) log.tracef("Metric (%s) is trait with value %s", req.getName(), value);
+            if (trace) log.trace("Metric ("+req.getName()+") is trait with value "+ value);
             MeasurementDataTrait res = new MeasurementDataTrait(req, value);
             report.addData(res);
          }
@@ -127,12 +130,14 @@ public class CacheManagerComponent extends MBeanResourceComponent<MBeanResourceC
 
    private EmsBean queryCacheManagerBean(EmsConnection conn) {
       String pattern = cacheManagerPattern;
-      if (log.isTraceEnabled()) log.tracef("Pattern to query is %s", pattern);
+      if (log.isTraceEnabled()) log.trace("Pattern to query is " + pattern);
       ObjectNameQueryUtility queryUtility = new ObjectNameQueryUtility(pattern);
       List<EmsBean> beans = conn.queryBeans(queryUtility.getTranslatedQuery());
       if (beans.size() > 1) {
          // If more than one are returned, most likely is due to duplicate domains which is not the general case
-         log.moreThanOneBeanReturned(pattern, beans);
+         if(log.isWarnEnabled()) {
+            log.warn(String.format("More than one bean returned from applying %s pattern: %s", pattern, beans));
+         }
       }
       return beans.get(0);
    }
