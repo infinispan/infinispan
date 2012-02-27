66436f9b91bf5b42cc01f80e9090042631cdadb6
Galder Zamarreño <galder@zamarreno.com>
2012-02-27 13:31:35 +0100

    ISPN-1887 Fixed buggy withCacheManager method and simplify it

---
+++ core/src/test/java/org/infinispan/test/MultiCacheManagerCallable.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+package org.infinispan.test;
+
+import org.infinispan.manager.EmbeddedCacheManager;
+
+/**
+ * A task that executes operations against a group of cache managers.
+ *
+ * @author Galder Zamarreño
+ * @since 5.1
+ */
+public class MultiCacheManagerCallable {
+
+   protected final EmbeddedCacheManager[] cms;
+
+   public MultiCacheManagerCallable(EmbeddedCacheManager... cms) {
+      this.cms = cms;
+   }
+
+   public void call() throws Exception {
+      // No-op
+   }
+
+}
