8fcd3cf53a24aa8c0be265b11e54b2ad114fc0ff
Manik Surtani <manik@surtani.org>
2012-04-04 11:20:44 +0100

    ISPN-1967 Expose CacheView string via JMX

--- core/src/main/java/org/infinispan/remoting/rpc/RpcManagerImpl.java
+++ core/src/main/java/org/infinispan/remoting/rpc/RpcManagerImpl.java
@@ -113,6 +113,18 @@ public class RpcManagerImpl implements RpcManager {
       statisticsEnabled = configuration.isExposeJmxStatistics();
    }
 
+   @ManagedAttribute(description = "Retrieves the committed view.")
+   @Metric(displayName = "Committed view", dataType = DataType.TRAIT)
+   public String getCommittedViewAsString() {
+      return cvm == null ? "N/A" : cvm.getCommittedView(configuration.getName()).toString();
+   }
+
+   @ManagedAttribute(description = "Retrieves the pending view.")
+   @Metric(displayName = "Pending view", dataType = DataType.TRAIT)
+   public String getPendingViewAsString() {
+      return cvm == null ? "N/A" : cvm.getPendingView(configuration.getName()).toString();
+   }
+
    private boolean useReplicationQueue(boolean sync) {
       return !sync && replicationQueue != null && replicationQueue.isEnabled();
    }
