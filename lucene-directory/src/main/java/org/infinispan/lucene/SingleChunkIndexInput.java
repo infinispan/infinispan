961bea9706a922e325e45a5add941bdc5d7dbb20
Sanne Grinovero <sanne@hibernate.org>
2012-04-17 10:29:35 +0200

    ISPN-1982 Resolve deprecated methods usage from Lucene 3.6 update

--- lucene-directory/src/main/java/org/infinispan/lucene/SingleChunkIndexInput.java
+++ lucene-directory/src/main/java/org/infinispan/lucene/SingleChunkIndexInput.java
@@ -44,6 +44,7 @@ final public class SingleChunkIndexInput extends IndexInput {
    private int bufferPosition;
 
    public SingleChunkIndexInput(final AdvancedCache<?, ?> chunksCache, final FileCacheKey fileKey, final FileMetadata fileMetadata) {
+      super(fileKey.getFileName());
       ChunkCacheKey key = new ChunkCacheKey(fileKey.getIndexName(), fileKey.getFileName(), 0);
       byte[] b = (byte[]) chunksCache.get(key);
       if (b == null) {
