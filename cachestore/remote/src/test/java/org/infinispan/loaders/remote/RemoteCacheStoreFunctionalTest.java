78510771c4a8954799bb2473c976101817878fbf
Galder Zamarreño <galder@zamarreno.com>
2012-05-14 11:47:35 +0200

    ISPN-1999 Removed unnecessary asserts

--- cachestore/remote/src/test/java/org/infinispan/loaders/remote/RemoteCacheStoreFunctionalTest.java
+++ cachestore/remote/src/test/java/org/infinispan/loaders/remote/RemoteCacheStoreFunctionalTest.java
@@ -66,16 +66,18 @@ public class RemoteCacheStoreFunctionalTest extends BaseCacheStoreFunctionalTest
 
    @Override
    public void testPreloadAndExpiry() {
-      assert true : "Remote cache store does not support preload";
+      // No-op, since remote cache store does not support preload
    }
 
    @Override
    public void testPreloadStoredAsBinary() {
-      assert true : "Remote cache store does not support store as binary since Hot Rod already stores them as binary";
+      // No-op, remote cache store does not support store as binary
+      // since Hot Rod already stores them as binary
    }
 
    @Override
    public void testTwoCachesSameCacheStore() {
       //not applicable
    }
+
 }
