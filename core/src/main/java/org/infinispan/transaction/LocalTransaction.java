69d81888c06b7cf913aa6fd83c60c35c4ab6f830
Sanne Grinovero <sanne@hibernate.org>
2012-05-29 14:56:29 +0100

    use the if(trace) check when trace is defined

--- core/src/main/java/org/infinispan/transaction/LocalTransaction.java
+++ core/src/main/java/org/infinispan/transaction/LocalTransaction.java
@@ -77,7 +77,7 @@ public abstract class LocalTransaction extends AbstractCacheTransaction {
    }
 
    public void locksAcquired(Collection<Address> nodes) {
-      log.tracef("Adding remote locks on %s. Remote locks are %s", nodes, remoteLockedNodes);
+      if (trace) log.tracef("Adding remote locks on %s. Remote locks are %s", nodes, remoteLockedNodes);
       if (remoteLockedNodes == null)
          remoteLockedNodes = new HashSet<Address>(nodes);
       else
