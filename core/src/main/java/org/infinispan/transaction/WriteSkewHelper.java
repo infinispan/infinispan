3be6c7cbd1f686a07a42753b9c15a8d75c7783cd
Manik Surtani <manik@surtani.org>
2012-05-30 16:37:26 +0100

    ISPN-2076 Detected write skew errors are logged as ERROR

--- core/src/main/java/org/infinispan/transaction/WriteSkewHelper.java
+++ core/src/main/java/org/infinispan/transaction/WriteSkewHelper.java
@@ -83,7 +83,7 @@ public class WriteSkewHelper {
                   uv.put(k, newVersion);
                } else {
                   // Write skew check detected!
-                  throw new CacheException("Write skew detected on key " + k + " for transaction " + context.getTransaction());
+                  throw new WriteSkewException("Write skew detected on key " + k + " for transaction " + context.getTransaction());
                }
             }
          }
