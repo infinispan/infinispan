5d1604f259eda08d0eb0c3fdd9e500782589b7a1
Adrian Nistor <anistor@redhat.com>
2012-08-03 17:19:19 +0300

    ISPN-1424 Javadoc improvement.

--- core/src/main/java/org/infinispan/notifications/cachemanagerlistener/event/ViewChangedEvent.java
+++ core/src/main/java/org/infinispan/notifications/cachemanagerlistener/event/ViewChangedEvent.java
@@ -28,23 +28,33 @@ import java.util.List;
 
 /**
  * This event is passed in to any method annotated with {@link org.infinispan.notifications.cachemanagerlistener.annotation.ViewChanged}.
+ * It represents a JGroups view change event.
  *
  * @author <a href="mailto:manik@jboss.org">Manik Surtani</a>
  * @since 4.0
  */
 public interface ViewChangedEvent extends Event {
+
    /**
+    * Gets the current list of members.
+    *
     * @return the new view associated with this view change. List cannot be null.
     */
    List<Address> getNewMembers();
 
    /**
+    * Gets the previous list of members.
+    *
     * @return the old view associated with this view change. List cannot be null.
     */
    List<Address> getOldMembers();
 
    Address getLocalAddress();
 
+   /**
+    * Get JGroups view id.
+    * @return
+    */
    int getViewId();
 
    boolean isMergeView();
