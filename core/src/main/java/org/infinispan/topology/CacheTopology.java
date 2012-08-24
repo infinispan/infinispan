b1c34b8335faa476dc133e1103997c0777812e6a
Dan Berindei <dan@infinispan.org>
2012-08-24 09:17:24 +0300

    ISPN-1424 Shorter toString for CacheTopology

--- core/src/main/java/org/infinispan/topology/CacheTopology.java
+++ core/src/main/java/org/infinispan/topology/CacheTopology.java
@@ -84,7 +84,7 @@ public class CacheTopology {
    @Override
    public String toString() {
       return "CacheTopology{" +
-            "topologyId=" + topologyId +
+            "id=" + topologyId +
             ", currentCH=" + currentCH +
             ", pendingCH=" + pendingCH +
             '}';
