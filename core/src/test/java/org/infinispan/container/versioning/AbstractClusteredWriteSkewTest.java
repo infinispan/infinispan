badc56769db4118ce8af75d36da5bded54b3346f
Dan Berindei <dan@infinispan.org>
2012-08-27 12:14:38 +0300

    ISPN-1424 Write skew tests consume way too much CPU when they fail

--- core/src/test/java/org/infinispan/container/versioning/AbstractClusteredWriteSkewTest.java
+++ core/src/test/java/org/infinispan/container/versioning/AbstractClusteredWriteSkewTest.java
@@ -26,6 +26,7 @@ import org.infinispan.configuration.cache.VersioningScheme;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.infinispan.transaction.LockingMode;
+import org.infinispan.transaction.WriteSkewException;
 import org.infinispan.util.concurrent.IsolationLevel;
 import org.testng.annotations.Test;
 
@@ -138,8 +139,10 @@ public abstract class AbstractClusteredWriteSkewTest extends MultipleCacheManage
       }
 
       @Override
-      public Boolean call() {
+      public Boolean call() throws InterruptedException {
+         int failuresCounter = 0;
          while (lastValue < counterMaxValue && !Thread.interrupted()) {
+            boolean success = false;
             try {
                //start transaction, get the counter value, increment and put it again
                //check for duplicates in case of success
@@ -154,16 +157,22 @@ public abstract class AbstractClusteredWriteSkewTest extends MultipleCacheManage
                transactionManager.commit();
 
                unique = uniqueValuesSet.add(value);
+               success = true;
             } catch (Exception e) {
-               try {
-                  //lets rollback
-                  if (transactionManager.getStatus() != Status.STATUS_NO_TRANSACTION)
-                     transactionManager.rollback();
-               } catch (Throwable t) {
-                  //the only possible exception is thrown by the rollback. just ignore it
-                  log.trace("Exception during rollback", t);
-               }
+               // expected exception
+               failuresCounter++;
+               assertTrue(failuresCounter < 10 * counterMaxValue, "Too many failures incrementing the counter");
             } finally {
+               if (!success) {
+                  try {
+                     //lets rollback
+                     if (transactionManager.getStatus() != Status.STATUS_NO_TRANSACTION)
+                        transactionManager.rollback();
+                  } catch (Throwable t) {
+                     //the only possible exception is thrown by the rollback. just ignore it
+                     log.trace("Exception during rollback", t);
+                  }
+               }
                assertTrue(unique, "Duplicate value found in " + address(cache) + " (value=" + lastValue + ")");
             }
          }
