8a06c858efa9bd8ff62d55e616ca5fbf3d923c00
Dan Berindei <dan@infinispan.org>
2012-08-29 11:45:22 +0300

    ISPN-1424 Check more details in VersionedDistStateTransferTest

--- core/src/test/java/org/infinispan/container/versioning/VersionedDistStateTransferTest.java
+++ core/src/test/java/org/infinispan/container/versioning/VersionedDistStateTransferTest.java
@@ -23,7 +23,10 @@ import org.infinispan.Cache;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.VersioningScheme;
+import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.distribution.MagicKey;
+import org.infinispan.distribution.ch.ConsistentHash;
+import org.infinispan.remoting.transport.Address;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.infinispan.test.fwk.CleanupAfterMethod;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
@@ -68,27 +71,38 @@ public class VersionedDistStateTransferTest extends MultipleCacheManagersTest {
       Cache<Object, Object> cache2 = cache(2);
       Cache<Object, Object> cache3 = cache(3);
 
+      // TODO Add multiple keys, to ensure one of them ends up on the joiner
       MagicKey hello = new MagicKey("hello", cache2);
       cache0.put(hello, "world");
 
-      for (Cache<Object, Object> c: caches()) assert "world".equals(c.get(hello));
+      // no state transfer per se, but we check that the initial data is ok
+      checkStateTransfer(hello);
 
-      tm(1).begin();
-      assert "world".equals(cache1.get(hello));
+      tm(1).begin(); {
+         assert "world".equals(cache1.get(hello));
+      }
       Transaction t = tm(1).suspend();
 
+      log.debugf("Starting joiner");
       addClusterEnabledCacheManager(builder);
       Cache<Object, Object> cache4 = cache(4);
 
-      assert "world".equals(cache2.get(hello));
+      log.debugf("Joiner started, checking transferred data");
+      checkStateTransfer(hello);
+
+      log.debugf("Stopping cache %s", cache2);
       cacheManagers.get(2).stop();
+      cacheManagers.remove(2);
+
+      log.debugf("Leaver stopped, checking transferred data");
+      checkStateTransfer(hello);
 
       // Cause a write skew
       cache4.put(hello, "new world");
 
-      tm(1).resume(t);
-      cache1.put(hello, "world2");
-
+      tm(1).resume(t); {
+         cache1.put(hello, "world2");
+      }
       try {
          tm(1).commit();
          assert false: "Should fail";
@@ -101,5 +115,22 @@ public class VersionedDistStateTransferTest extends MultipleCacheManagersTest {
       // skip cache2, it has been killed.
       assert "new world".equals(cache3.get(hello));
       assert "new world".equals(cache3.get(hello));
+   }
+
+   private void checkStateTransfer(MagicKey hello) {
+      for (Cache<Object, Object> c: caches()) {
+         assert "world".equals(c.get(hello));
+         checkVersion(c, hello);
+      }
+   }
+
+   private void checkVersion(Cache<Object, Object> c, MagicKey hello) {
+      Address address = c.getCacheManager().getAddress();
+      ConsistentHash readConsistentHash = c.getAdvancedCache().getDistributionManager().getReadConsistentHash();
+      if (readConsistentHash.isKeyLocalToNode(address, hello)) {
+         InternalCacheEntry ice = c.getAdvancedCache().getDataContainer().get(hello);
+         assert ice != null;
+         assert ice.getVersion() != null;
+      }
    }
 }
