8ddec8841f6177be33dd0d4d3bce8c7e891cc163
Ales Justin <ales.justin@gmail.com>
2012-09-03 01:07:32 +0200

    Use cfg since it's passed as an argument.

--- query/src/main/java/org/infinispan/query/impl/LifecycleManager.java
+++ query/src/main/java/org/infinispan/query/impl/LifecycleManager.java
@@ -67,8 +67,7 @@ public class LifecycleManager extends AbstractModuleLifecycle {
     * Registers the Search interceptor in the cache before it gets started
     */
    @Override
-   public void cacheStarting(ComponentRegistry cr, Configuration configuration, String cacheName) {
-      Configuration cfg = cr.getComponent(Configuration.class);
+   public void cacheStarting(ComponentRegistry cr, Configuration cfg, String cacheName) {
       if (cfg.indexing().enabled()) {
          log.registeringQueryInterceptor();
          SearchFactoryIntegrator searchFactory = getSearchFactory(cfg.indexing().properties(), cr);
