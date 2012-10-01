b2fb60c0cca0b0bde860c432756b9ac51be79d44
Manik Surtani <manik@surtani.org>
2012-10-01 12:02:25 -0700

    ISPN-2345 Add the ability to dump the entire key set of a node under a well-known key, via JMX

---
+++ core/src/main/java/org/infinispan/upgrade/Migrator.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2012 Red Hat, Inc. and/or its affiliates.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ */
+
+package org.infinispan.upgrade;
+
+/**
+ * Records all known keys and stores them under a well-known key which can be used for retrieval.
+ *
+ * @author Manik Surtani
+ * @since 5.2
+ */
+public interface Migrator {
+   void recordKnownGlobalKeyset();
+
+   String getCacheName();
+}
