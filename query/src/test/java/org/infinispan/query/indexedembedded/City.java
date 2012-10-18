df129e6fbeb5ac1eed1dd9aa3c6953c8dce0fbbf
Sanne Grinovero <sanne@hibernate.org>
2012-10-18 22:00:11 +0100

    ISPN-2424 Fix EntryActivatingTest

--- query/src/test/java/org/infinispan/query/indexedembedded/City.java
+++ query/src/test/java/org/infinispan/query/indexedembedded/City.java
@@ -23,12 +23,14 @@
 
 package org.infinispan.query.indexedembedded;
 
+import java.io.Serializable;
+
 import org.hibernate.search.annotations.Field;
 
 /**
  * @author Sanne Grinovero <sanne@hibernate.org> (C) 2011 Red Hat Inc.
  */
-public class City {
+public class City implements Serializable {
 
    @Field String name;
 
