47e6e2fd8db764be66329f07a25e35aa4943c2bb
Galder Zamarreño <galder@zamarreno.com>
2012-10-18 11:17:21 +0200

    ISPN-2414 Slim down get command to remove notifier and entry return
    * Notifier is a singleton for the cache, no need to have a reference
    in each of the get commamds, this is wasteful. Instead notify via the
    call interceptor which is also a cache singleton.
    * Remove wasteful returnCacheEntry boolean and instead have a subclass
    of GetKeyValueCommand that is specialised in returning cache entries
    instead of values. As well as saving memory, it's more OO-friendly :)

--- core/src/main/java/org/infinispan/commands/remote/ClusteredGetCommand.java
+++ core/src/main/java/org/infinispan/commands/remote/ClusteredGetCommand.java
@@ -44,7 +44,6 @@ import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.EnumSet;
 import java.util.Set;
 
@@ -126,8 +125,7 @@ public class ClusteredGetCommand extends BaseRpcCommand implements FlagAffectedC
       // as our caller is already calling the ClusteredGetCommand on all the relevant nodes
       Set<Flag> commandFlags = EnumSet.of(Flag.SKIP_REMOTE_LOOKUP, Flag.CACHE_MODE_LOCAL);
       if (this.flags != null) commandFlags.addAll(this.flags);
-      GetKeyValueCommand command = commandsFactory.buildGetKeyValueCommand(key, commandFlags);
-      command.setReturnCacheEntry(true);
+      GetKeyValueCommand command = commandsFactory.buildGetCacheEntryCommand(key, commandFlags);
       InvocationContext invocationContext = icc.createRemoteInvocationContextForCommand(command, getOrigin());
       CacheEntry cacheEntry = (CacheEntry) invoker.invoke(invocationContext, command);
       if (cacheEntry == null) {
