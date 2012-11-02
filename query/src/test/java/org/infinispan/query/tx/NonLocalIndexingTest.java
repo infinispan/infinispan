d05a32ddb8496d951b223c4d552b1235050fe009
Sanne Grinovero <sanne@hibernate.org>
2012-11-02 21:39:17 +0000

    ISPN-2479 Test for replace operation on same index

--- query/src/test/java/org/infinispan/query/tx/NonLocalIndexingTest.java
+++ query/src/test/java/org/infinispan/query/tx/NonLocalIndexingTest.java
@@ -31,6 +31,7 @@ import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.query.CacheQuery;
 import org.infinispan.query.Search;
 import org.infinispan.query.SearchManager;
+import org.infinispan.query.test.AnotherGrassEater;
 import org.infinispan.query.test.Person;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.testng.Assert;
@@ -61,17 +62,21 @@ public class NonLocalIndexingTest extends MultipleCacheManagersTest {
    }
 
    public void testQueryAfterAddingNewNode() throws Exception {
-      store(new Person("Astronaut","is asking his timezone", 32), cache(0));
+      store("Astronaut", new Person("Astronaut","is asking his timezone", 32), cache(0));
       assertFind("timezone", 1);
       assertFind("asking", 1);
       assertFind("cat", 0);
-      store(new Person("Webdeveloper","is confused by the timezone concept", 32), cache(1));
+      store("Webdeveloper", new Person("Webdeveloper","is confused by the timezone concept", 32), cache(1));
       assertFind("cat", 0);
       assertFind("timezone", 2);
       //We're using the name as a key so this is an update in practice of the Astronaut record:
-      store(new Person("Astronaut","thinks about his cat", 32), cache(1));
+      store("Astronaut", new Person("Astronaut","thinks about his cat", 32), cache(1));
       assertFind("cat", 1);
       assertFind("timezone", 1);
+      store("Astronaut", new AnotherGrassEater("Astronaut", "is having a hard time to find grass"), cache(1));
+      //replacing same key with a new type:
+      assertFind("cat", 0);
+      assertFind("grass", 1);
    }
 
    private void assertFind(String keyword, int expectedCount) {
@@ -82,16 +87,16 @@ public class NonLocalIndexingTest extends MultipleCacheManagersTest {
    private static void assertFind(Cache cache, String keyword, int expectedCount) {
       SearchManager queryFactory = Search.getSearchManager(cache);
       Query luceneQuery = new TermQuery(new Term("blurb", keyword));
-      CacheQuery cacheQuery = queryFactory.getQuery(luceneQuery, Person.class);
+      CacheQuery cacheQuery = queryFactory.getQuery(luceneQuery);
       int resultSize = cacheQuery.getResultSize();
       Assert.assertEquals(resultSize, expectedCount);
    }
 
-   private void store(final Person person, final Cache<Object, Object> cache) throws Exception {
+   private void store(final String key, final Object value, final Cache<Object, Object> cache) throws Exception {
       Callable<Void> callable = new Callable<Void>() {
          @Override
          public Void call() throws Exception {
-            cache.put(person.getName(), person);
+            cache.put(key, value);
             return null;
          }
       };
