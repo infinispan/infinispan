294453a86badfafb182cb3b08d11d5ef4c0395a6
Manik Surtani <manik@surtani.org>
2013-02-06 12:57:01 +0000

    Slight cleanup
    * Javadoc for getNormalizedHash() in Util and ConsistentHash
    * Remove redundant "normalization" checks in ConsistentHashV1.getServer()

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/consistenthash/ConsistentHash.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/consistenthash/ConsistentHash.java
@@ -38,6 +38,11 @@ public interface ConsistentHash {
 
    SocketAddress getServer(byte[] key);
 
-   int getNormalizedHash(Object key);
-
+   /**
+    * Computes hash code of a given object, and then normalizes it to ensure a positive
+    * value is always returned.
+    * @param object to hash
+    * @return a non-null, non-negative normalized hash code for a given object
+    */
+   int getNormalizedHash(Object object);
 }
