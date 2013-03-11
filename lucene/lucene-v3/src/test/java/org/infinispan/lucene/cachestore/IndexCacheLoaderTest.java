b8abd340f075f3e25f12578db08b632d1a3d32fd
Sanne Grinovero <sanne@hibernate.org>
2013-03-11 18:14:58 +0000

    Move existing tests to correct package

--- lucene/lucene-v3/src/test/java/org/infinispan/lucene/cachestore/IndexCacheLoaderTest.java
+++ lucene/lucene-v3/src/test/java/org/infinispan/lucene/cachestore/IndexCacheLoaderTest.java
@@ -16,7 +16,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA  02110-1301, USA.
  */
-package org.infinispan.lucene.cacheloader;
+package org.infinispan.lucene.cachestore;
 
 import java.io.File;
 import java.io.IOException;
