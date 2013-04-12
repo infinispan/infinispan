112ef7009f5675f5c6af54502c6a9f58f88e102e
Nikolay Shestakov <ns@xtesseract.com>
2013-04-12 09:56:41 -0400

    HHH-7908 Logging level checking
    Conflicts:
    	hibernate-core/src/main/java/org/hibernate/internal/SessionFactoryImpl.java

--- src/test/java/org/hibernate/test/cache/infinispan/functional/ConcurrentWriteTest.java
+++ src/test/java/org/hibernate/test/cache/infinispan/functional/ConcurrentWriteTest.java
@@ -468,7 +468,7 @@ sessionFactory().getStatistics().clear();
 					contactExists();
 					thinkRandomTime();
 					++completedIterations;
-					if ( log.isTraceEnabled() ) {
+					if ( trace ) {
 						log.tracef( "Iteration completed {0}", completedIterations );
 					}
 				}
