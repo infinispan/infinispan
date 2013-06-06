989f44b241696c0a02c72e9c5612b519ccbcf10e
Pedro Ruivo <pruivo@redhat.com>
2013-06-06 11:36:16 +0100

    Remove deprecated methods/config/assert from ClusterFileCacheStoreFunctionalTest

--- core/src/test/java/org/infinispan/loaders/file/ClusterFileCacheStoreFunctionalTest.java
+++ core/src/test/java/org/infinispan/loaders/file/ClusterFileCacheStoreFunctionalTest.java
@@ -29,17 +29,18 @@ import javax.transaction.TransactionManager;
 
 import org.infinispan.Cache;
 import org.infinispan.atomic.AtomicMap;
-import org.infinispan.atomic.AtomicMapLookup;
-import org.infinispan.config.CacheLoaderManagerConfig;
-import org.infinispan.config.Configuration;
-import org.infinispan.config.GlobalConfiguration;
-import org.infinispan.loaders.CacheStoreConfig;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.infinispan.test.TestingUtil;
-import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
+
+import static org.infinispan.atomic.AtomicMapLookup.getAtomicMap;
+import static org.infinispan.test.fwk.TestCacheManagerFactory.createClusteredCacheManager;
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  *
@@ -51,8 +52,9 @@ public class ClusterFileCacheStoreFunctionalTest extends MultipleCacheManagersTe
 
    // createCacheManager executes before any @BeforeClass defined in the class, so simply use standard tmp folder.
    private final String tmpDirectory = TestingUtil.tmpDirectory(this);
+   private static final String CACHE_NAME = "clusteredFileCacheStore";
 
-   private Cache cache1, cache2;
+   private Cache<Object, ?> cache1, cache2;
 
    @AfterClass
    protected void clearTempDir() {
@@ -62,50 +64,45 @@ public class ClusterFileCacheStoreFunctionalTest extends MultipleCacheManagersTe
 
    @Override
    protected void createCacheManagers() throws Throwable {
-      EmbeddedCacheManager cacheManager1 = TestCacheManagerFactory.createCacheManager(GlobalConfiguration.getClusteredDefault(), new Configuration());
-      EmbeddedCacheManager cacheManager2 = TestCacheManagerFactory.createCacheManager(GlobalConfiguration.getClusteredDefault(), new Configuration());
+      EmbeddedCacheManager cacheManager1 = createClusteredCacheManager(GlobalConfigurationBuilder.defaultClusteredBuilder(),
+                                                                       new ConfigurationBuilder());
+      EmbeddedCacheManager cacheManager2 = createClusteredCacheManager(GlobalConfigurationBuilder.defaultClusteredBuilder(),
+                                                                       new ConfigurationBuilder());
       registerCacheManager(cacheManager1, cacheManager2);
 
-      Configuration config1 = getDefaultClusteredConfig(Configuration.CacheMode.REPL_SYNC, true);
-      CacheLoaderManagerConfig clMngrConfig = new CacheLoaderManagerConfig();
-      clMngrConfig.addCacheLoaderConfig(createCacheStoreConfig(1));
-      config1.setCacheLoaderManagerConfig(clMngrConfig);
+      ConfigurationBuilder builder1 = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, true);
+      addCacheStoreConfig(builder1, 1);
+      ConfigurationBuilder builder2 = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, true);
+      addCacheStoreConfig(builder2, 2);
 
-      Configuration config2 = getDefaultClusteredConfig(Configuration.CacheMode.REPL_SYNC, true);
-      CacheLoaderManagerConfig clMngrConfig2 = new CacheLoaderManagerConfig();
-      clMngrConfig2.addCacheLoaderConfig(createCacheStoreConfig(2));
-      config2.setCacheLoaderManagerConfig(clMngrConfig2);
-
-      cacheManager1.defineConfiguration("clusteredFileCacheStore", config1);
-      cacheManager2.defineConfiguration("clusteredFileCacheStore", config2);
-      cache1 = cache(0, "clusteredFileCacheStore");
-      cache2 = cache(1, "clusteredFileCacheStore");
+      cacheManager1.defineConfiguration(CACHE_NAME, builder1.build());
+      cacheManager2.defineConfiguration(CACHE_NAME, builder2.build());
+      cache1 = cache(0, CACHE_NAME);
+      cache2 = cache(1, CACHE_NAME);
    }
 
    public void testRestoreTransactionalAtomicMap(Method m) throws Exception{
+      final Object mapKey = m.getName();
       TransactionManager tm = cache1.getAdvancedCache().getTransactionManager();
       tm.begin();
-      final AtomicMap<String, String> map = AtomicMapLookup.getAtomicMap(cache1, m.getName());
+      final AtomicMap<String, String> map = getAtomicMap(cache1, mapKey);
       map.put("a", "b");
       tm.commit();
 
       //evict from memory
-      cache1.evict(m.getName());
+      cache1.evict(mapKey);
 
       // now re-retrieve the map and make sure we see the diffs
-      assert AtomicMapLookup.getAtomicMap(cache1, m.getName()).get("a").equals("b");
-      assert AtomicMapLookup.getAtomicMap(cache2, m.getName()).get("a").equals("b");
+      assertEquals("Wrong value for key [a] in atomic map.", "b", getAtomicMap(cache1, mapKey).get("a"));
+      assertEquals("Wrong value for key [a] in atomic map.", "b", getAtomicMap(cache2, mapKey).get("a"));
 
-      cache2.evict(m.getName());
-      assert AtomicMapLookup.getAtomicMap(cache1, m.getName()).get("a").equals("b");
-      assert AtomicMapLookup.getAtomicMap(cache2, m.getName()).get("a").equals("b");
+      cache2.evict(mapKey);
+      assertEquals("Wrong value for key [a] in atomic map.", "b", getAtomicMap(cache1, mapKey).get("a"));
+      assertEquals("Wrong value for key [a] in atomic map.", "b", getAtomicMap(cache2, mapKey).get("a"));
    }
 
-   protected CacheStoreConfig createCacheStoreConfig(int index) throws Exception {
-      FileCacheStoreConfig cfg = new FileCacheStoreConfig();
-      cfg.setLocation(tmpDirectory + "/" + index);
-      cfg.setPurgeSynchronously(true); // for more accurate unit testing
-      return cfg;
+   protected void addCacheStoreConfig(ConfigurationBuilder builder, int index) {
+      builder.loaders().addFileCacheStore().location(tmpDirectory + "/" + index).purgeSynchronously(true);
    }
 
 }
