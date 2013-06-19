f670ac682aa8925317e4fc0dd2d623471766b86c
Galder Zamarreño <galder@zamarreno.com>
2013-06-19 11:10:30 +0100

    ISPN-3208 Remove unusued literal classes

--- jcache/src/main/java/org/infinispan/jcache/annotation/AnnotationInjectExtension.java
+++ jcache/src/main/java/org/infinispan/jcache/annotation/AnnotationInjectExtension.java
@@ -7,7 +7,6 @@ import javax.cache.annotation.CacheResult;
 import javax.enterprise.event.Observes;
 import javax.enterprise.inject.spi.BeforeBeanDiscovery;
 import javax.enterprise.inject.spi.Extension;
-import javax.enterprise.inject.spi.ProcessAnnotatedType;
 
 /**
  * CDI extension to register additional interceptor bindings
