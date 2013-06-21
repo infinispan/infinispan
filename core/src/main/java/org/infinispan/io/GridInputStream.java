ed90f03c4a0db372e0cb494540e1dae4b02c30aa
Manik Surtani <manik@surtani.org>
2013-06-21 15:35:25 +0100

    Clean up commits 5d54f84cbb4d8fb799fb40e3f8fc1ee94c72b1d6 31ef50c9a2fdb628b36ace602a6c879f29b258e7 and 4d4f2e9a2ca1cf657f1fd96163531ae3dbbd142c

--- core/src/main/java/org/infinispan/io/GridInputStream.java
+++ core/src/main/java/org/infinispan/io/GridInputStream.java
@@ -30,29 +30,29 @@ import java.io.InputStream;
 /**
  * @author Bela Ban
  * @author Marko Luksa
+ * @author Manik Surtani
  */
 public class GridInputStream extends InputStream {
 
    private int index = 0;                // index into the file for writing
    private int localIndex = 0;
    private byte[] currentBuffer = null;
-   private int fileSize;
-   private boolean closed = false;
+   private int fSize;
+   private boolean streamClosed = false;
    private FileChunkMapper fileChunkMapper;
 
    GridInputStream(GridFile file, Cache<String, byte[]> cache) {
       fileChunkMapper = new FileChunkMapper(file, cache);
-      fileSize = (int)file.length();
+      fSize = (int)file.length();
    }
 
-   @Override
-   public int read() throws IOException {
-      checkClosed();
+   @Override public int read() throws IOException {
+      assertOpen();
       if (isEndReached())
          return -1;
       if (getBytesRemainingInChunk() == 0)
-         fetchChunk();
-      int retval = 0x0ff&currentBuffer[localIndex++];
+         getChunk();
+      int retval = 0x0ff & currentBuffer[localIndex++];
       index++;
       return retval;
    }
@@ -63,15 +63,15 @@ public class GridInputStream extends InputStream {
    }
 
    @Override
-   public int read(byte[] b, int off, int len) throws IOException {
-      checkClosed();
+   public int read(byte[] bytes, int offset, int length) throws IOException {
+      assertOpen();
       int totalBytesRead = 0;
-      while (len > 0) {
-         int bytesRead = readFromChunk(b, off, len);
+      while (length > 0) {
+         int bytesRead = readFromChunk(bytes, offset, length);
          if (bytesRead == -1)
             return totalBytesRead > 0 ? totalBytesRead : -1;
-         off += bytesRead;
-         len -= bytesRead;
+         offset += bytesRead;
+         length -= bytesRead;
          totalBytesRead += bytesRead;
       }
 
@@ -83,7 +83,7 @@ public class GridInputStream extends InputStream {
          return -1;
       int remaining = getBytesRemainingInChunk();
       if (remaining == 0) {
-         fetchChunk();
+         getChunk();
          remaining = getBytesRemainingInChunk();
       }
       int bytesToRead = Math.min(len, remaining);
@@ -93,18 +93,17 @@ public class GridInputStream extends InputStream {
       return bytesToRead;
    }
 
-   @Override
-   public long skip(long len) throws IOException {
-      checkClosed();
-      if (len <= 0)
+   @Override public long skip(long length) throws IOException {
+      assertOpen();
+      if (length <= 0)
          return 0;
 
-      int bytesToSkip = Math.min((int)len, getBytesRemainingInStream());
+      int bytesToSkip = Math.min((int)length, getBytesRemainingInStream());
       index += bytesToSkip;
       if (bytesToSkip <= getBytesRemainingInChunk()) {
          localIndex += bytesToSkip;
       } else {
-         fetchChunk();
+         getChunk();
          localIndex = index % getChunkSize();
       }
       return bytesToSkip;
@@ -112,24 +111,22 @@ public class GridInputStream extends InputStream {
 
    @Override
    public int available() throws IOException {
-      checkClosed();
-      return getBytesRemainingInChunk();
+      assertOpen();
+      return getBytesRemainingInChunk();  // Return bytes in chunk
    }
 
    @Override
    public void close() throws IOException {
       localIndex = index = 0;
-      closed = true;
+      streamClosed = true;
    }
 
    private boolean isEndReached() {
-      return index == fileSize;
+      return index == fSize;
    }
 
-   private void checkClosed() throws IOException{
-       if(closed){
-           throw new IOException("Stream is closed");
-       }
+   private void assertOpen() throws IOException{
+       if (streamClosed) throw new IOException("Stream is closed");
    }
 
    private int getBytesRemainingInChunk() {
@@ -137,10 +134,10 @@ public class GridInputStream extends InputStream {
    }
 
    private int getBytesRemainingInStream() {
-      return fileSize - index;
+      return fSize - index;
    }
 
-   private void fetchChunk() {
+   private void getChunk() {
       currentBuffer = fileChunkMapper.fetchChunk(getChunkNumber());
       localIndex = 0;
    }
