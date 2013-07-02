b8cb7eaa9a6aa488e58d8ba76e73e8c739678ed6
Manik Surtani <manik@surtani.org>
2013-07-02 15:32:24 +0100

    ISPN-3185 License and copyright header update

--- cli/cli-client/src/main/java/org/infinispan/cli/shell/ShellImpl.java
+++ cli/cli-client/src/main/java/org/infinispan/cli/shell/ShellImpl.java
@@ -1,21 +1,3 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2012 Red Hat Inc. and/or its affiliates and other contributors
- * as indicated by the @author tags. All rights reserved.
- * See the copyright.txt in the distribution for a
- * full listing of individual contributors.
- *
- * This copyrighted material is made available to anyone wishing to use,
- * modify, copy, or redistribute it subject to the terms and conditions
- * of the GNU Lesser General Public License, v. 2.1.
- * This program is distributed in the hope that it will be useful, but WITHOUT A
- * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
- * PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
- * You should have received a copy of the GNU Lesser General Public License,
- * v.2.1 along with this distribution; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA  02110-1301, USA.
- */
 package org.infinispan.cli.shell;
 
 import gnu.getopt.Getopt;
@@ -265,8 +247,8 @@ public class ShellImpl implements Shell {
 
    private void version() {
       System.out.println("ispn-cli " + Version.class.getPackage().getImplementationVersion());
-      System.out.println("Copyright (C) 2012 Red Hat Inc. and/or its affiliates and other contributors");
-      System.out.println("License GNU Lesser General Public License, v. 2.1. http://www.gnu.org/licenses/lgpl-2.1.txt");
+      System.out.println("Copyright (C) 2009-2013 Red Hat Inc. and/or its affiliates and other contributors");
+      System.out.println("License Apache License, v. 2.0. http://www.apache.org/licenses/LICENSE-2.0");
       System.exit(0);
    }
 
