2fd8fd47d767e5b96abf2f65ee4a4b552c25e8b6
Dan Berindei <dan@infinispan.org>
2013-07-15 16:44:55 +0300

    ISPN-2515 VersionedReplStateTransferTest.testStateTransfer fails randomly
    Wait for state transfer to end before checking the results

--- core/src/test/java/org/infinispan/container/versioning/VersionedReplStateTransferTest.java
+++ core/src/test/java/org/infinispan/container/versioning/VersionedReplStateTransferTest.java
@@ -14,6 +14,9 @@ import org.testng.annotations.Test;
 import javax.transaction.RollbackException;
 import javax.transaction.Transaction;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.fail;
+
 @Test(testName = "container.versioning.VersionedReplStateTransferTest", groups = "functional")
 @CleanupAfterMethod
 public class VersionedReplStateTransferTest extends MultipleCacheManagersTest {
@@ -52,20 +55,22 @@ public class VersionedReplStateTransferTest extends MultipleCacheManagersTest {
 
       cache0.put("hello", "world");
 
-      assert "world".equals(cache0.get("hello"));
-      assert "world".equals(cache1.get("hello"));
+      assertEquals("world", cache0.get("hello"));
+      assertEquals("world", cache1.get("hello"));
 
       tm(1).begin();
-      assert "world".equals(cache1.get("hello"));
+      assertEquals("world", cache1.get("hello"));
       Transaction t = tm(1).suspend();
 
       // create a cache2
       addClusterEnabledCacheManager(builder);
       Cache<Object, Object> cache2 = cache(2);
 
-      assert "world".equals(cache2.get("hello"));
+      assertEquals("world", cache2.get("hello"));
 
       cacheManagers.get(0).stop();
+      cacheManagers.remove(0);
+      waitForClusterToForm();
 
       // Cause a write skew
       cache2.put("hello", "new world");
@@ -75,12 +80,12 @@ public class VersionedReplStateTransferTest extends MultipleCacheManagersTest {
 
       try {
          tm(1).commit();
-         assert false : "Should fail";
+         fail("Write skew check should fail");
       } catch (RollbackException expected) {
          // Expected
       }
 
-      assert "new world".equals(cache(1).get("hello"));
-      assert "new world".equals(cache(2).get("hello"));
+      assertEquals("new world", cache1.get("hello"));
+      assertEquals("new world", cache2.get("hello"));
    }
 }
