1949ab3acd0e1ccce2a1af9ad8065377f1538f17
Tristan Tarrant <ttarrant@redhat.com>
2013-07-25 17:42:14 +0200

    ISPN-3272 CLI man pages: Don't attempt to get the last character of an empty string when formatting

--- cli/cli-client/src/test/java/org/infinispan/cli/Man2AnsiTest.java
+++ cli/cli-client/src/test/java/org/infinispan/cli/Man2AnsiTest.java
@@ -1,8 +1,12 @@
 package org.infinispan.cli;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 
 import org.infinispan.cli.shell.Man2Ansi;
+import org.infinispan.commons.util.Util;
 import org.testng.annotations.Test;
 
 @Test(groups="functional", testName="cli.shell.Man2AnsiTest")
@@ -21,5 +25,25 @@ public class Man2AnsiTest {
       assert matcher.groupCount()==2;
       assert matcher.group(1)==null;
       assert "Text".equals(matcher.group(2));
+   }
+
+   public void testAllManPages() throws Exception {
+      InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream("help");
+      BufferedReader r = new BufferedReader(new InputStreamReader(is));
+      for(String name = r.readLine(); name != null; name = r.readLine()) {
+         testManPage("help/"+name);
+      }
+      r.close();
+   }
+
+   private void testManPage(String name) throws Exception {
+      InputStream is = null;
+      try {
+         is = Thread.currentThread().getContextClassLoader().getResourceAsStream(name);
+         Man2Ansi man2ansi = new Man2Ansi(72);
+         man2ansi.render(is);
+      } finally {
+         Util.close(is);
+      }
    }
 }
