e70f07d0f66648d816b9f60ac11526aff8c264d4
Mircea Markus <mircea.markus@jboss.org>
2013-08-01 23:24:44 +0100

    allow the python version to be determined within the invoking script

--- bin/filter_failed_tests.sh
+++ bin/filter_failed_tests.sh
@@ -17,7 +17,7 @@ for LOG_FILE in `find . -name "*.log"` ; do
       echo "Processing $TEST from $LOG_FILE "
       echo "" > $TEST.log
       SHORTNAME=`perl -e '$t = $ARGV[0]; chomp $t; $t =~ s/[a-z0-9]//g; print $t;' $TEST`
-      cat $LOG_FILE | $DIR/greplog.py "\b$TEST\b" | perl -npe "s/(?![a-zA-Z.])$TEST(?![a-zA-Z.])/$SHORTNAME/g" > $TEST.log
+      cat $LOG_FILE | python $DIR/greplog.py "\b$TEST\b" | perl -npe "s/(?![a-zA-Z.])$TEST(?![a-zA-Z.])/$SHORTNAME/g" > $TEST.log
     done
 
     rm $LOG_FILE
