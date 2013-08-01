1c03912f288d77e89cfd7de4f801065a2a880088
Erik Salter <esalter@bnivideo.com>
2013-08-01 12:08:43 -0400

    ISPN-3389:  Account for the new lock owner for any local transactions to prevent the tx from being stale in the new owner's tx table.

--- core/src/main/java/org/infinispan/interceptors/locking/AbstractTxLockingInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/locking/AbstractTxLockingInterceptor.java
@@ -208,15 +208,20 @@ public abstract class AbstractTxLockingInterceptor extends AbstractLockingInterc
             }
 
             if (!txCompleted) {
-               throw newTimeoutException(key, txContext);
+               throw newTimeoutException(key, tx, txContext);
             }
          }
       }
    }
 
-   private TimeoutException newTimeoutException(Object key, TxInvocationContext txContext) {
+   private TimeoutException newTimeoutException(Object key, TxInvocationContext txContext) {                        
       return new TimeoutException("Could not acquire lock on " + key + " on behalf of transaction " +
-                                       txContext.getGlobalTransaction() + ". Lock is being held by " + lockManager.getOwner(key));
+                                       txContext.getGlobalTransaction() + "." + "Lock is being held by " + lockManager.getOwner(key));
+   }
+   
+   private TimeoutException newTimeoutException(Object key, CacheTransaction tx, TxInvocationContext txContext) {                  
+      return new TimeoutException("Could not acquire lock on " + key + " on behalf of transaction " +
+                                       txContext.getGlobalTransaction() + ". Waiting to complete tx: " + tx + ".");
    }
 
    private boolean releaseLockOnTxCompletion(TxInvocationContext ctx) {
