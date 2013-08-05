1d63c39241db5e7576c9e6810b13776a15f39343
Alan Field <afield@redhat.com>
2013-08-05 15:27:05 -0400

    Updated Mapper demo code
        WordCountMapper:
            - Use a regular expression to parse words in cache values
    
        WordCountMapperEmitPerValue:
            - An alternative implementation that emits after parsing the
    entire cache value to reduce the size of the intermediate results

--- demos/distexec/src/main/java/org/infinispan/demo/mapreduce/WordCountMapper.java
+++ demos/distexec/src/main/java/org/infinispan/demo/mapreduce/WordCountMapper.java
@@ -10,7 +10,11 @@ public class WordCountMapper implements Mapper<String, String, String, Integer> 
    @Override
    public void map(String key, String value, Collector<String, Integer> c) {
       chunks++;
-      for (String word : value.split(" ")) {
+      /*
+       * Split on punctuation or whitespace, except for ' and - to catch contractions and hyphenated
+       * words
+       */
+      for (String word : value.split("[\\p{Punct}\\s&&[^'-]]+")) {
          if (word != null) {
             String w = word.trim();
             if (w.length() > 0) {
@@ -20,6 +24,7 @@ public class WordCountMapper implements Mapper<String, String, String, Integer> 
          }
       }
 
-      if (chunks % 1000 == 0) System.out.printf("Analyzed %s words in %s lines%n", words, chunks);
+      if (chunks % 1000 == 0)
+         System.out.printf("Analyzed %s words in %s lines%n", words, chunks);
    }
 }
