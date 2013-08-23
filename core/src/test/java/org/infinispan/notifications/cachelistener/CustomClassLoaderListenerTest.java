4ed38931b956ba69f5a2b7403ffe12cda2b4c8fa
Jiri Holusa <jholusa@redhat.com>
2013-08-23 12:05:50 +0200

    Tests for cache listeners in compatibility mode

--- core/src/test/java/org/infinispan/notifications/cachelistener/CustomClassLoaderListenerTest.java
+++ core/src/test/java/org/infinispan/notifications/cachelistener/CustomClassLoaderListenerTest.java
@@ -4,20 +4,27 @@ import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.loaders.dummy.DummyInMemoryCacheStoreConfigurationBuilder;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.notifications.Listener;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntriesEvicted;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryActivated;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryEvicted;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryLoaded;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryPassivated;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryRemoved;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryVisited;
-import org.infinispan.notifications.cachelistener.event.Event;
+import org.infinispan.notifications.cachelistener.event.CacheEntryActivatedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryEvictedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryLoadedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryPassivatedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryRemovedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryVisitedEvent;
 import org.infinispan.test.SingleCacheManagerTest;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.testng.annotations.Test;
 
-import static org.testng.AssertJUnit.*;
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * CustomClassLoaderListenerTest.
@@ -42,16 +49,61 @@ public class CustomClassLoaderListenerTest extends SingleCacheManagerTest {
       ccl = new CustomClassLoader(Thread.currentThread().getContextClassLoader());
       ClassLoaderListener listener = new ClassLoaderListener();
       cache().getAdvancedCache().with(ccl).addListener(listener);
+
       cache().put("a", "a"); // Created + Modified
+      assertEquals(1, listener.createdCounter);
+      assertEquals(1, listener.modifiedCounter);
+      assertEquals(0, listener.removedCounter);
+      assertEquals(0, listener.visitedCounter);
+      assertEquals(0, listener.activatedCounter);
+      assertEquals(0, listener.passivatedCounter);
+      assertEquals(0, listener.evictedCounter);
+      assertEquals(0, listener.loadedCounter);
+      listener.reset();
+
       cache().replace("a", "b"); // Modified
+      assertEquals(0, listener.createdCounter);
+      assertEquals(1, listener.modifiedCounter);
+      assertEquals(0, listener.removedCounter);
+      assertEquals(0, listener.visitedCounter);
+      assertEquals(0, listener.activatedCounter);
+      assertEquals(0, listener.passivatedCounter);
+      assertEquals(0, listener.evictedCounter);
+      assertEquals(0, listener.loadedCounter);
+      listener.reset();
+
 
       cache().evict("a"); // Passivated + Evicted
+      assertEquals(0, listener.createdCounter);
+      assertEquals(0, listener.modifiedCounter);
+      assertEquals(0, listener.removedCounter);
+      assertEquals(0, listener.visitedCounter);
+      assertEquals(0, listener.activatedCounter);
+      assertEquals(1, listener.passivatedCounter);
+      assertEquals(1, listener.evictedCounter);
+      assertEquals(0, listener.loadedCounter);
+      listener.reset();
 
       cache().get("a"); // Loaded +  Activated + Visited
+      assertEquals(0, listener.createdCounter);
+      assertEquals(0, listener.modifiedCounter);
+      assertEquals(0, listener.removedCounter);
+      assertEquals(1, listener.visitedCounter);
+      assertEquals(1, listener.activatedCounter);
+      assertEquals(0, listener.passivatedCounter);
+      assertEquals(0, listener.evictedCounter);
+      assertEquals(1, listener.loadedCounter);
+      listener.reset();
 
-      cache().remove("a"); // Modified + Removed
-
-      assertEquals(10, listener.invocationCount);
+      cache().remove("a"); // Removed
+      assertEquals(0, listener.createdCounter);
+      assertEquals(0, listener.modifiedCounter);
+      assertEquals(1, listener.removedCounter);
+      assertEquals(0, listener.visitedCounter);
+      assertEquals(0, listener.activatedCounter);
+      assertEquals(0, listener.passivatedCounter);
+      assertEquals(0, listener.evictedCounter);
+      assertEquals(0, listener.loadedCounter);
    }
 
    public static class CustomClassLoader extends ClassLoader {
@@ -62,21 +114,89 @@ public class CustomClassLoaderListenerTest extends SingleCacheManagerTest {
 
    @Listener
    public class ClassLoaderListener {
-      int invocationCount = 0;
+      int createdCounter = 0;
+      int removedCounter = 0;
+      int modifiedCounter = 0;
+      int visitedCounter = 0;
+      int evictedCounter = 0;
+      int passivatedCounter = 0;
+      int loadedCounter = 0;
+      int activatedCounter = 0;
+
+
+      @CacheEntryCreated
+      public void handleCreated(CacheEntryCreatedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            createdCounter++;
+         }
+      }
+
+      @CacheEntryRemoved
+      public void handleRemoved(CacheEntryRemovedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            removedCounter++;
+         }
+      }
+
+      @CacheEntryModified
+      public void handleModified(CacheEntryModifiedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            modifiedCounter++;
+         }
+      }
+
+      @CacheEntryVisited
+      public void handleVisited(CacheEntryVisitedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            visitedCounter++;
+         }
+      }
+
+      @CacheEntryEvicted
+      public void handleEvicted(CacheEntryEvictedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            evictedCounter++;
+         }
+      }
+
+      @CacheEntryPassivated
+      public void handlePassivated(CacheEntryPassivatedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            passivatedCounter++;
+         }
+      }
 
       @CacheEntryActivated
-      @CacheEntryCreated
-      @CacheEntriesEvicted
-      @CacheEntryLoaded
-      @CacheEntryModified
-      @CacheEntryPassivated
-      @CacheEntryRemoved
-      @CacheEntryVisited
-      public void handle(Event e) {
+      public void handleActivated(CacheEntryActivatedEvent e) {
          assertEquals(ccl, Thread.currentThread().getContextClassLoader());
-         if(!e.isPre()) {
-            ++invocationCount;
+         if (!e.isPre()) {
+            activatedCounter++;
          }
+      }
+
+      @CacheEntryLoaded
+      public void handleLoaded(CacheEntryLoadedEvent e) {
+         assertEquals(ccl, Thread.currentThread().getContextClassLoader());
+         if (!e.isPre()) {
+            loadedCounter++;
+         }
+      }
+
+      void reset() {
+         createdCounter = 0;
+         removedCounter = 0;
+         modifiedCounter = 0;
+         visitedCounter = 0;
+         evictedCounter = 0;
+         passivatedCounter = 0;
+         loadedCounter = 0;
+         activatedCounter = 0;
       }
    }
 }
---
+++ integrationtests/compatibility-mode-it/src/test/java/org/infinispan/it/compatibility/TestCacheListener.java
@@ -0,0 +1,78 @@
+package org.infinispan.it.compatibility;
+
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryRemoved;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryVisited;
+import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryRemovedEvent;
+import org.infinispan.notifications.cachelistener.event.CacheEntryVisitedEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Cache listener for testing purposes with dedicated counter and field for every event so it can assure
+ * that correct event was fired and data is readable.
+ *
+ * @author Jiri Holusa [jholusa@redhat.com]
+ */
+@Listener
+public class TestCacheListener {
+   //Map is used instead of List so we could test if key is correct too
+   Map<Object, Object> created = new HashMap<Object, Object>();
+   Map<Object, Object> removed = new HashMap<Object, Object>();
+   Map<Object, Object> modified = new HashMap<Object, Object>();
+   Map<Object, Object> visited = new HashMap<Object, Object>();
+
+   int createdCounter = 0;
+   int removedCounter = 0;
+   int modifiedCounter = 0;
+   int visitedCounter = 0;
+
+   @CacheEntryCreated
+   public void handleCreated(CacheEntryCreatedEvent e) {
+      if (!e.isPre()) {
+         created.put(e.getKey(), e.getValue());
+         createdCounter++;
+      }
+   }
+
+   @CacheEntryRemoved
+   public void handleRemoved(CacheEntryRemovedEvent e) {
+      if (!e.isPre()) {
+         removed.put(e.getKey(), e.getOldValue());
+         removedCounter++;
+      }
+   }
+
+   @CacheEntryModified
+   public void handleModified(CacheEntryModifiedEvent e) {
+      if (!e.isPre()) {
+         modified.put(e.getKey(), e.getValue());
+         modifiedCounter++;
+      }
+   }
+
+   @CacheEntryVisited
+   public void handleVisited(CacheEntryVisitedEvent e) {
+      if (!e.isPre()) {
+         visited.put(e.getKey(), e.getValue());
+         visitedCounter++;
+      }
+   }
+
+   void reset() {
+      created.clear();
+      removed.clear();
+      modified.clear();
+      visited.clear();
+
+      createdCounter = 0;
+      removedCounter = 0;
+      modifiedCounter = 0;
+      visitedCounter = 0;
+   }
+}
