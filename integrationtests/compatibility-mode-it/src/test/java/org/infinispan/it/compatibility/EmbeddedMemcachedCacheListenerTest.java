da241deae8da69ad96e3ff8de85fcbe2d7ddf42d
Galder Zamarreño <galder@zamarreno.com>
2013-08-27 10:16:31 +0200

    ISPN-3430 Some server cache calls should skip listener invocations
    * Some cache operations in the server help optimise or gather
    information for the operation being called, and hence should not
    have an impact in the cache listener expectations.
    * Doing this keeps server cache listener invocation expectations
    in line with embedded cache.

--- integrationtests/compatibility-mode-it/src/test/java/org/infinispan/it/compatibility/EmbeddedMemcachedCacheListenerTest.java
+++ integrationtests/compatibility-mode-it/src/test/java/org/infinispan/it/compatibility/EmbeddedMemcachedCacheListenerTest.java
@@ -5,35 +5,20 @@ import net.spy.memcached.MemcachedClient;
 import net.spy.memcached.transcoders.SerializingTranscoder;
 import net.spy.memcached.transcoders.Transcoder;
 import org.infinispan.Cache;
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.VersionedValue;
 import org.infinispan.commons.io.ByteBuffer;
 import org.infinispan.commons.marshall.AbstractMarshaller;
-import org.infinispan.commons.util.concurrent.NotifyingFuture;
 import org.infinispan.configuration.cache.CacheMode;
-import org.infinispan.notifications.Listener;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryRemoved;
-import org.infinispan.notifications.cachelistener.annotation.CacheEntryVisited;
-import org.infinispan.notifications.cachelistener.event.CacheEntryCreatedEvent;
-import org.infinispan.notifications.cachelistener.event.CacheEntryModifiedEvent;
-import org.infinispan.notifications.cachelistener.event.CacheEntryRemovedEvent;
-import org.infinispan.notifications.cachelistener.event.CacheEntryVisitedEvent;
 import org.infinispan.test.AbstractInfinispanTest;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
-import static org.jgroups.util.Util.assertTrue;
-import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.*;
 
 /**
  * Test cache listeners bound to embedded cache and operation over Memcached cache.
