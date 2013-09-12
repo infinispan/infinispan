09fd14b11e6acfe6df022cc47396ee09ac2a3ac9
Mircea Markus <mircea.markus@jboss.org>
2013-09-12 17:20:14 +0100

    ISPN-3478 Polish CS API revamp
    * MarshalledEntryImpl is a parametriezed type now

--- core/src/main/java/org/infinispan/marshall/core/MarshalledEntryFactory.java
+++ core/src/main/java/org/infinispan/marshall/core/MarshalledEntryFactory.java
@@ -9,11 +9,11 @@ import org.infinispan.metadata.InternalMetadata;
  * @author Mircea Markus
  * @since 6.0
  */
-public interface MarshalledEntryFactory {
+public interface MarshalledEntryFactory<K,V> {
 
-   MarshalledEntry newMarshalledEntry(ByteBuffer key, ByteBuffer valueBytes, ByteBuffer metadataBytes);
+   MarshalledEntry<K,V> newMarshalledEntry(ByteBuffer key, ByteBuffer valueBytes, ByteBuffer metadataBytes);
 
-   MarshalledEntry newMarshalledEntry(Object key, ByteBuffer valueBytes, ByteBuffer metadataBytes);
+   MarshalledEntry<K,V> newMarshalledEntry(Object key, ByteBuffer valueBytes, ByteBuffer metadataBytes);
 
-   MarshalledEntry newMarshalledEntry(Object key, Object value, InternalMetadata im);
+   MarshalledEntry<K,V> newMarshalledEntry(Object key, Object value, InternalMetadata im);
 }
