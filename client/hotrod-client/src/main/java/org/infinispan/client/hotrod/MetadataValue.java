1ad787847c4a5346edc960005ce0bd6ac0c4da27
mlinhard <michal@linhard.sk>
2013-09-19 14:03:47 +0200

    add MetadataValue javadoc

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/MetadataValue.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/MetadataValue.java
@@ -1,19 +1,38 @@
 package org.infinispan.client.hotrod;
 
 /**
- * Besides the value, also contains a version and expiration information.
+ * Besides the value, also contains a version and expiration information. Time values are server
+ * time representations as returned by {@link org.infinispan.util.TimeService#wallClockTime}
  *
  * @author Tristan Tarrant
  * @since 5.2
  */
 public interface MetadataValue<V> extends VersionedValue<V> {
 
+   /**
+    *
+    * @return Time when entry was created. -1 for immortal entries.
+    */
    long getCreated();
 
+   /**
+    *
+    * @return Lifespan of the entry in seconds. Negative values are interpreted as unlimited
+    *         lifespan.
+    */
    int getLifespan();
 
+   /**
+    *
+    * @return Time when entry was last used. -1 for immortal entries.
+    */
    long getLastUsed();
 
+   /**
+    *
+    * @return The maximum amount of time (in seconds) this key is allowed to be idle for before it
+    *         is considered as expired.
+    */
    int getMaxIdle();
 
 }
