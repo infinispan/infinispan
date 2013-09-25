425b131c9b178737f8dedd5f89249b0679ac1e75
Pedro Ruivo <pruivo@redhat.com>
2013-09-25 15:09:24 +0100

    ISPN-3542 Read Only transaction optimization doesn't work properly for RepeatableRead
    * fix: CommitCommand was sent twice for read-only transactions

--- core/src/test/java/org/infinispan/tx/ReadOnlyTxTest.java
+++ core/src/test/java/org/infinispan/tx/ReadOnlyTxTest.java
@@ -1,8 +1,11 @@
 package org.infinispan.tx;
 
+import org.infinispan.commands.tx.CommitCommand;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.context.Flag;
+import org.infinispan.context.impl.TxInvocationContext;
+import org.infinispan.interceptors.base.BaseCustomInterceptor;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.test.SingleCacheManagerTest;
 import org.infinispan.test.TestingUtil;
@@ -11,9 +14,13 @@ import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.infinispan.transaction.LockingMode;
 import org.infinispan.transaction.TransactionTable;
 import org.infinispan.transaction.xa.LocalXaTransaction;
+import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
 import javax.transaction.Transaction;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author Mircea.Markus@jboss.com
@@ -68,8 +75,37 @@ public class ReadOnlyTxTest extends SingleCacheManagerTest {
       assert !TestingUtil.extractLockManager(cache).isLocked("k");
    }
 
+   public void testSingleCommitCommand() throws Exception {
+      cache.put("k", "v");
+      CommitCommandCounterInterceptor counterInterceptor = new CommitCommandCounterInterceptor();
+      cache.getAdvancedCache().addInterceptor(counterInterceptor, 0);
+      try {
+         tm().begin();
+         AssertJUnit.assertEquals("Wrong value for key k.", "v", cache.get("k"));
+         tm().commit();
+      } finally {
+         cache.getAdvancedCache().getAdvancedCache().removeInterceptor(counterInterceptor.getClass());
+      }
+      AssertJUnit.assertEquals("Wrong number of CommitCommand.", numberCommitCommand(), counterInterceptor.counter.get());
+   }
+
+   protected int numberCommitCommand() {
+      //in this case, the transactions are committed in 1 phase due to pessimistic locking.
+      return 0;
+   }
 
    private TransactionTable txTable() {
       return TestingUtil.getTransactionTable(cache);
+   }
+
+   private class CommitCommandCounterInterceptor extends BaseCustomInterceptor {
+
+      public final AtomicInteger counter = new AtomicInteger(0);
+
+      @Override
+      public Object visitCommitCommand(TxInvocationContext ctx, CommitCommand command) throws Throwable {
+         counter.incrementAndGet();
+         return invokeNextInterceptor(ctx, command);
+      }
    }
 }
