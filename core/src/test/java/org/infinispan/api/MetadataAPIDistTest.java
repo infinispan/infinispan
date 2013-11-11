45e21c3e3627e755352a00c445b3503f88086d69
Galder Zamarreño <galder@zamarreno.com>
2013-11-11 09:31:48 +0100

    ISPN-2999 Fixed in ISPN-2281 but added tests for getCacheEntry + DIST

---
+++ core/src/test/java/org/infinispan/api/MetadataAPIDistTest.java
@@ -0,0 +1,42 @@
+package org.infinispan.api;
+
+import org.infinispan.Cache;
+import org.infinispan.commons.equivalence.ByteArrayEquivalence;
+import org.infinispan.commons.equivalence.Equivalence;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.container.entries.CacheEntry;
+import org.infinispan.test.MultipleCacheManagersTest;
+import org.testng.annotations.Test;
+
+import static org.infinispan.distribution.DistributionTestHelper.getFirstNonOwner;
+import static org.infinispan.distribution.DistributionTestHelper.getFirstOwner;
+import static org.testng.AssertJUnit.assertNotNull;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
+
+@Test(groups = "functional", testName = "api.MetadataAPITest")
+public class MetadataAPIDistTest extends MultipleCacheManagersTest {
+
+   @Override
+   protected void createCacheManagers() throws Throwable {
+      ConfigurationBuilder builder = getDefaultClusteredCacheConfig(CacheMode.DIST_SYNC);
+      Equivalence<byte[]> eq = ByteArrayEquivalence.INSTANCE;
+      builder.dataContainer().keyEquivalence(eq).valueEquivalence(eq)
+             .clustering().hash().numOwners(1);
+      createCluster(builder, 2);
+   }
+
+   public void testGetCacheEntryNonOwner() {
+      byte[] key = {1, 2, 3};
+      Cache<byte[], byte[]> owner = getFirstOwner(key, this.<byte[], byte[]>caches());
+      Cache<byte[], byte[]> nonOwner = getFirstNonOwner(key, this.<byte[], byte[]>caches());
+
+      owner.put(key, new byte[]{4, 5, 6});
+      assertArrayEquals(new byte[]{4, 5, 6}, owner.get(key));
+
+      CacheEntry cacheEntry = nonOwner.getAdvancedCache().getCacheEntry(key);
+      assertNotNull(cacheEntry);
+      assertArrayEquals(new byte[]{4, 5, 6}, (byte[]) cacheEntry.getValue());
+   }
+
+}
--- core/src/test/java/org/infinispan/distribution/BaseDistFunctionalTest.java
+++ core/src/test/java/org/infinispan/distribution/BaseDistFunctionalTest.java
@@ -116,11 +116,11 @@ public abstract class BaseDistFunctionalTest<K, V> extends MultipleCacheManagers
    }
 
    protected Cache<K, V> getFirstNonOwner(Object key) {
-      return getNonOwners(key)[0];
+      return DistributionTestHelper.getFirstNonOwner(key, caches);
    }
    
    protected Cache<K, V> getFirstOwner(Object key) {
-      return getOwners(key)[0];
+      return DistributionTestHelper.getFirstOwner(key, caches);
    }
 
    protected Cache<K, V> getSecondNonOwner(String key) {
@@ -204,23 +204,15 @@ public abstract class BaseDistFunctionalTest<K, V> extends MultipleCacheManagers
    }
 
    protected Cache<K, V>[] getOwners(Object key) {
-      return getOwners(key, 2);
+      Cache<K, V>[] arr = new Cache[2];
+      DistributionTestHelper.getOwners(key, caches).toArray(arr);
+      return arr;
    }
 
    protected Cache<K, V>[] getOwners(Object key, int expectedNumberOwners) {
-      Cache<K, V>[] owners = new Cache[expectedNumberOwners];
-      int i = 0;
-      for (Cache<K, V> c : caches) {
-         if (isFirstOwner(c, key)) {
-            owners[i++] = c;
-            break;
-         }
-      }
-      for (Cache<K, V> c : caches) {
-         if (isOwner(c, key) && !isFirstOwner(c, key)) owners[i++] = c;
-      }
-      for (Cache<?, ?> c : owners) assert c != null : "Have not found enough owners for key [" + key + "]";
-      return owners;
+      Cache<K, V>[] arr = new Cache[expectedNumberOwners];
+      DistributionTestHelper.getOwners(key, caches).toArray(arr);
+      return arr;
    }
 
    protected Cache<K, V>[] getNonOwnersExcludingSelf(Object key, Address self) {
@@ -251,10 +243,7 @@ public abstract class BaseDistFunctionalTest<K, V> extends MultipleCacheManagers
 
    protected Cache<K, V>[] getNonOwners(Object key, int expectedNumberNonOwners) {
       Cache<K, V>[] nonOwners = new Cache[expectedNumberNonOwners];
-      int i = 0;
-      for (Cache<K, V> c : caches) {
-         if (!isOwner(c, key)) nonOwners[i++] = c;
-      }
+      DistributionTestHelper.getNonOwners(key, caches).toArray(nonOwners);
       return nonOwners;
    }
 
