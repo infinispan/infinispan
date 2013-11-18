2164cf39e7fd90200b3099280f824a732dd8788c
Mircea Markus <mircea.markus@jboss.org>
2013-11-18 20:10:26 +0000

    ISPN-3727 intermittent test failure AsyncReplExtendedStatisticTest.testReplaceWithOldVal
    * temporerly  disable test

--- extended-statistics/src/test/java/org/infinispan/stats/simple/AsyncReplExtendedStatisticTest.java
+++ extended-statistics/src/test/java/org/infinispan/stats/simple/AsyncReplExtendedStatisticTest.java
@@ -8,10 +8,14 @@ import org.testng.annotations.Test;
  * @author Pedro Ruivo
  * @since 6.0
  */
-@Test(groups = "functional", testName = "stats.simple.AsyncReplExtendedStatisticTest")
+@Test(groups = "functional", testName = "stats.simple.AsyncReplExtendedStatisticTest" )
 public class AsyncReplExtendedStatisticTest extends BaseClusteredExtendedStatisticTest {
 
    protected AsyncReplExtendedStatisticTest() {
       super(CacheMode.REPL_ASYNC, false, false, false);
+   }
+
+   @Test(enabled = false, description = "https://issues.jboss.org/browse/ISPN-3727")
+   public void testReplaceWithOldVal() {
    }
 }
