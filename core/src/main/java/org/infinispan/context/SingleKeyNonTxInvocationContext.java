47495870d84043fdddc2dfa1b570166d2e43cb01
Sanne Grinovero <sanne@hibernate.org>
2013-11-25 11:11:13 +0000

    ISPN-3779 Refactor the SingleKeyNonTxInvocationContext to not inherit from an abstract invocationcontext

--- core/src/main/java/org/infinispan/context/SingleKeyNonTxInvocationContext.java
+++ core/src/main/java/org/infinispan/context/SingleKeyNonTxInvocationContext.java
@@ -3,7 +3,8 @@ package org.infinispan.context;
 import org.infinispan.commons.equivalence.Equivalence;
 import org.infinispan.commons.util.InfinispanCollections;
 import org.infinispan.container.entries.CacheEntry;
-import org.infinispan.context.impl.AbstractInvocationContext;
+import org.infinispan.container.entries.InternalCacheEntry;
+import org.infinispan.remoting.transport.Address;
 
 import java.util.Collections;
 import java.util.Map;
@@ -11,26 +12,32 @@ import java.util.Set;
 
 /**
  * @author Mircea Markus
+ * @author Sanne Grinovero
  * @since 5.1
  */
-public class SingleKeyNonTxInvocationContext extends AbstractInvocationContext {
-
-   private final boolean isOriginLocal;
-
-   private Object key;
+public final class SingleKeyNonTxInvocationContext implements InvocationContext {
 
    /**
     * It is possible for the key to only be wrapped but not locked, e.g. when a get takes place.
     */
    private boolean isLocked;
+   private boolean useFutureReturnType;
+   private final boolean isOriginLocal;
+
+   private Object key;
 
    private CacheEntry cacheEntry;
 
+   //TODO move reference to Equivalence to InvocationContextFactory (Memory allocation cost)
    private final Equivalence keyEquivalence;
+   //TODO move reference to ClassLoader to InvocationContextFactory (Memory allocation cost)
+   private ClassLoader classLoader;
+   //TODO move the Origin's address to the InvocationContextFactory when isOriginLocal=true -> all addresses are the same  (Memory allocation cost)
+   //(verify if this is worth it by looking at object alignment - would need a different implementation as pointing to null wouldn't help)
+   private Address origin;
 
-   public SingleKeyNonTxInvocationContext(
-         boolean originLocal, Equivalence keyEquivalence) {
-      isOriginLocal = originLocal;
+   public SingleKeyNonTxInvocationContext(final boolean originLocal, final Equivalence keyEquivalence) {
+      this.isOriginLocal = originLocal;
       this.keyEquivalence = keyEquivalence;
    }
 
@@ -59,10 +66,11 @@ public class SingleKeyNonTxInvocationContext extends AbstractInvocationContext {
    public void clearLockedKeys() {
       key = null;
       cacheEntry = null;
+      isLocked = false;
    }
 
    @Override
-   public void addLockedKey(Object key) {
+   public void addLockedKey(final Object key) {
       if (cacheEntry != null && !keyEquivalence.equals(key, this.key))
          throw illegalStateException();
       isLocked = true;
@@ -73,8 +81,7 @@ public class SingleKeyNonTxInvocationContext extends AbstractInvocationContext {
    }
 
    @Override
-   @SuppressWarnings("unchecked")
-   public CacheEntry lookupEntry(Object key) {
+   public CacheEntry lookupEntry(final Object key) {
       if (key != null && this.key !=null && keyEquivalence.equals(key, this.key))
          return cacheEntry;
 
@@ -87,7 +94,7 @@ public class SingleKeyNonTxInvocationContext extends AbstractInvocationContext {
    }
 
    @Override
-   public void putLookedUpEntry(Object key, CacheEntry e) {
+   public void putLookedUpEntry(final Object key, final CacheEntry e) {
       this.key = key;
       this.cacheEntry = e;
    }
@@ -101,7 +108,7 @@ public class SingleKeyNonTxInvocationContext extends AbstractInvocationContext {
    }
 
    @Override
-   public void removeLookedUpEntry(Object key) {
+   public void removeLookedUpEntry(final Object key) {
       if (keyEquivalence.equals(key, this.key))
          clearLockedKeys();
    }
@@ -118,4 +125,64 @@ public class SingleKeyNonTxInvocationContext extends AbstractInvocationContext {
    public CacheEntry getCacheEntry() {
       return cacheEntry;
    }
+
+   @Override
+   public Address getOrigin() {
+      return origin;
+   }
+
+   @Override
+   public boolean isUseFutureReturnType() {
+      return useFutureReturnType;
+   }
+
+   @Override
+   public void setUseFutureReturnType(final boolean useFutureReturnType) {
+      this.useFutureReturnType = useFutureReturnType;
+   }
+
+   @Override
+   public ClassLoader getClassLoader() {
+      return classLoader;
+   }
+
+   @Override
+   public void setClassLoader(ClassLoader classLoader) {
+      this.classLoader = classLoader;
+   }
+
+   @Override
+   public boolean hasLockedKey(final Object key) {
+      return isLocked && keyEquivalence.equals(this.key, key);
+   }
+
+   @Override
+   public boolean replaceValue(final Object key, final InternalCacheEntry cacheEntry) {
+      CacheEntry ce = lookupEntry(key);
+      if (ce == null || ce.isNull() || ce.getValue() == null) {
+         if (ce != null) {
+            ce.setValue(cacheEntry.getValue());
+            ce.setMetadata(cacheEntry.getMetadata());
+         } else {
+            return false;
+         }
+      }
+      return true;
+   }
+
+   @Override
+   public boolean isEntryRemovedInContext(final Object key) {
+      CacheEntry ce = lookupEntry(key);
+      return ce != null && ce.isRemoved() && ce.isChanged();
+   }
+
+   @Override
+   public SingleKeyNonTxInvocationContext clone() {
+      try {
+         return (SingleKeyNonTxInvocationContext) super.clone();
+      } catch (CloneNotSupportedException e) {
+         throw new IllegalStateException("Impossible!");
+      }
+   }
+
 }
