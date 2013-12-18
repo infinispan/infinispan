35cefaa969f5c58292ae371627a01a82b09a9e92
Pedro Ruivo <pruivo@redhat.com>
2013-12-18 11:31:35 +0000

    Cleanup FlagsEnabledTest

--- core/src/test/java/org/infinispan/api/flags/NonTxFlagsEnabledTest.java
+++ core/src/test/java/org/infinispan/api/flags/NonTxFlagsEnabledTest.java
@@ -12,8 +12,6 @@ import org.testng.annotations.Test;
 import java.lang.reflect.Method;
 
 import static org.infinispan.context.Flag.CACHE_MODE_LOCAL;
-import static org.testng.AssertJUnit.assertEquals;
-import static org.testng.AssertJUnit.assertNull;
 
 /**
  * FlagsEnabledTest for non transactional caches.
@@ -24,6 +22,10 @@ import static org.testng.AssertJUnit.assertNull;
 @Test(groups = "functional", testName = "api.flags.NonTxFlagsEnabledTest")
 @CleanupAfterMethod
 public class NonTxFlagsEnabledTest extends FlagsEnabledTest {
+
+   public NonTxFlagsEnabledTest() {
+      super("non-tx-replication");
+   }
 
    @Override
    public void testReplicateSkipCacheLoaderWithinTxInCoordinator(Method m) throws Exception {
@@ -37,17 +39,17 @@ public class NonTxFlagsEnabledTest extends FlagsEnabledTest {
 
    public void testCacheLocalInNonOwner() {
       addClusterEnabledCacheManager(getConfigurationBuilder());
-      waitForClusterToForm("replication");
-      final AdvancedCache<Object, String> cache1 = advancedCache(0, "replication");
-      final AdvancedCache<Object, String> cache2 = advancedCache(1, "replication");
-      final AdvancedCache<Object, String> cache3 = advancedCache(2, "replication");
+      waitForClusterToForm(cacheName);
+      final AdvancedCache<Object, String> cache1 = advancedCache(0, cacheName);
+      final AdvancedCache<Object, String> cache2 = advancedCache(1, cacheName);
+      final AdvancedCache<Object, String> cache3 = advancedCache(2, cacheName);
       final Object key = new MagicKey("k-no", cache1);
 
       cache3.withFlags(CACHE_MODE_LOCAL).put(key, "value");
 
-      assertEquals("Cache '" + cache3 + "' should have the key.", "value", cache3.get(key));
-      assertNull("Cache '" + cache1 + "' should *not* have the key.", cache1.get(key));
-      assertNull("Cache '" + cache2 + "' should *not* have the key.", cache2.get(key));
+      assertCacheValue(cache3, key, "value");
+      assertCacheValue(cache1, key, null);
+      assertCacheValue(cache2, key, null);
    }
 
    @Override
@@ -57,7 +59,7 @@ public class NonTxFlagsEnabledTest extends FlagsEnabledTest {
             .persistence().addStore(UnnecessaryLoadingTest.CountingStoreConfigurationBuilder.class)
             .persistence().addStore(DummyInMemoryStoreConfigurationBuilder.class)
             .clustering().hash().numSegments(2);
-      createClusteredCaches(2, "replication", builder);
+      createClusteredCaches(2, cacheName, builder);
    }
 
    private ConfigurationBuilder getConfigurationBuilder() {
