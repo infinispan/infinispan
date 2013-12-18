71cd45cc26402e0866fda0de68e3460941b4e6ad
Galder Zamarreño <galder@zamarreno.com>
2013-12-18 16:16:41 +0100

    ISPN-3846 Handle EnumSets in order JVMs and unknown EnumSets

--- core/src/main/java/org/infinispan/marshall/exts/EnumSetExternalizer.java
+++ core/src/main/java/org/infinispan/marshall/exts/EnumSetExternalizer.java
@@ -13,6 +13,7 @@ import java.io.ObjectInput;
 import java.io.ObjectOutput;
 import java.util.AbstractSet;
 import java.util.EnumSet;
+import java.util.HashSet;
 import java.util.Set;
 
 /**
@@ -24,28 +25,46 @@ import java.util.Set;
 @Immutable
 public class EnumSetExternalizer extends AbstractExternalizer<Set> {
 
-   private static final int ENUM_SET = 0;
-   private static final int REGULAR_ENUM_SET = 1;
-   private static final int JUMBO_ENUM_SET = 2;
+   private static final int UNKNOWN_ENUM_SET = 0;
+   private static final int ENUM_SET = 1;
+   private static final int REGULAR_ENUM_SET = 2;
+   private static final int JUMBO_ENUM_SET = 3;
+   private static final int MINI_ENUM_SET = 4; // IBM class
+   private static final int HUGE_ENUM_SET = 5; // IBM class
 
    private final IdentityIntMap<Class<?>> numbers = new IdentityIntMap<Class<?>>(3);
 
    public EnumSetExternalizer() {
       numbers.put(EnumSet.class, ENUM_SET);
-      numbers.put(getRegularEnumSetClass(), REGULAR_ENUM_SET);
-      numbers.put(getJumboEnumSetClass(), JUMBO_ENUM_SET);
+      addEnumSetClass(getRegularEnumSetClass(), REGULAR_ENUM_SET);
+      addEnumSetClass(getJumboEnumSetClass(), JUMBO_ENUM_SET);
+      addEnumSetClass(getMiniEnumSetClass(), MINI_ENUM_SET);
+      addEnumSetClass(getHugeEnumSetClass(), HUGE_ENUM_SET);
+   }
+
+   private void addEnumSetClass(Class<EnumSet> clazz, int index) {
+      if (clazz != null)
+         numbers.put(clazz, index);
    }
 
    @Override
    public void writeObject(ObjectOutput output, Set set) throws IOException {
-      int number = numbers.get(set.getClass(), -1);
-      output.writeByte(number);
-      MarshallUtil.marshallCollection(set, output);
+      int number = numbers.get(set.getClass(), UNKNOWN_ENUM_SET);
+      if (number == UNKNOWN_ENUM_SET) {
+         // Fallback on standard object write
+         output.writeObject(set);
+      } else {
+         output.writeByte(number);
+         MarshallUtil.marshallCollection(set, output);
+      }
    }
 
    @Override
    public Set readObject(ObjectInput input) throws IOException, ClassNotFoundException {
       int magicNumber = input.readUnsignedByte();
+      if (magicNumber == UNKNOWN_ENUM_SET)
+         return (Set) input.readObject();
+
       AbstractSet<Enum> enumSet = null;
       int size = UnsignedNumeric.readUnsignedInt(input);
       for (int i = 0; i < size; i++) {
@@ -71,8 +90,18 @@ public class EnumSetExternalizer extends AbstractExternalizer<Set> {
 
    @Override
    public Set<Class<? extends Set>> getTypeClasses() {
-      return Util.<Class<? extends Set>>asSet(
-            EnumSet.class, getRegularEnumSetClass(), getJumboEnumSetClass());
+      Set<Class<? extends Set>> set = new HashSet<Class<? extends Set>>();
+      set.add(EnumSet.class);
+      addEnumSetType(getRegularEnumSetClass(), set);
+      addEnumSetType(getJumboEnumSetClass(), set);
+      addEnumSetType(getMiniEnumSetClass(), set);
+      addEnumSetType(getHugeEnumSetClass(), set);
+      return set;
+   }
+
+   private void addEnumSetType(Class<? extends Set> clazz, Set<Class<? extends Set>> typeSet) {
+      if (clazz != null)
+         typeSet.add(clazz);
    }
 
    private Class<EnumSet> getJumboEnumSetClass() {
@@ -83,8 +112,20 @@ public class EnumSetExternalizer extends AbstractExternalizer<Set> {
       return getEnumSetClass("java.util.RegularEnumSet");
    }
 
+   private Class<EnumSet> getMiniEnumSetClass() {
+      return getEnumSetClass("java.util.MiniEnumSet");
+   }
+
+   private Class<EnumSet> getHugeEnumSetClass() {
+      return getEnumSetClass("java.util.HugeEnumSet");
+   }
+
    private Class<EnumSet> getEnumSetClass(String className) {
-      return Util.loadClass(className, EnumSet.class.getClassLoader());
+      try {
+         return Util.loadClassStrict(className, EnumSet.class.getClassLoader());
+      } catch (ClassNotFoundException e) {
+         return null; // Ignore
+      }
    }
 
 }
