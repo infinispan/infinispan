fe352e58482581b7c8dd484d9569829789a1f986
Adrian Nistor <anistor@redhat.com>
2013-12-20 01:11:20 +0200

    ISPN-3778 ClusteredCacheWithAsyncDirTest throws lots of "org.h2.jdbc.JdbcSQLException: Unique index or primary key violation"
    The jdbc cache stores of two nodes collide using the same in-memory H2 database instance. The config file should not specify hardcoded jdbc connection pool settings; these should rather be set in code ensuring a unique database name for each node.

--- query/src/test/java/org/infinispan/query/blackbox/ClusteredCacheWithAsyncDirTest.java
+++ query/src/test/java/org/infinispan/query/blackbox/ClusteredCacheWithAsyncDirTest.java
@@ -1,7 +1,16 @@
 package org.infinispan.query.blackbox;
 
+import org.infinispan.commons.util.FileLookupFactory;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.StoreConfigurationBuilder;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.persistence.jdbc.configuration.AbstractJdbcStoreConfigurationBuilder;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.testng.annotations.Test;
+
+import java.io.InputStream;
 
 /**
  * Testing the ISPN Directory configuration with Async. JDBC CacheStore. The tests are performed for Clustered cache.
@@ -13,12 +22,34 @@ public class ClusteredCacheWithAsyncDirTest extends ClusteredCacheTest {
 
    @Override
    protected void createCacheManagers() throws Exception {
-      cacheManagers.add(TestCacheManagerFactory.fromXml("async-store-config.xml"));
-      cacheManagers.add(TestCacheManagerFactory.fromXml("async-store-config.xml"));
+      cacheManagers.add(createCacheManager(1));
+      cacheManagers.add(createCacheManager(2));
       waitForClusterToForm();
 
       cache1 = cacheManagers.get(0).getCache("JDBCBased_LocalIndex");
       cache2 = cacheManagers.get(1).getCache("JDBCBased_LocalIndex");
+   }
+
+   private EmbeddedCacheManager createCacheManager(int nodeIndex) throws Exception {
+      InputStream is = FileLookupFactory.newInstance().lookupFileStrict("async-store-config.xml",
+                                                                        Thread.currentThread().getContextClassLoader());
+      ParserRegistry parserRegistry = new ParserRegistry(Thread.currentThread().getContextClassLoader());
+
+      ConfigurationBuilderHolder holder = parserRegistry.parse(is);
+      is.close();
+
+      for (ConfigurationBuilder builder : holder.getNamedConfigurationBuilders().values()) {
+         for (StoreConfigurationBuilder storeBuilder : builder.persistence().stores()) {
+            if (storeBuilder instanceof AbstractJdbcStoreConfigurationBuilder) {
+               AbstractJdbcStoreConfigurationBuilder jdbcStoreBuilder = (AbstractJdbcStoreConfigurationBuilder) storeBuilder;
+               jdbcStoreBuilder.connectionPool()
+                     .driverClass("org.h2.Driver")
+                     .connectionUrl("jdbc:h2:mem:infinispan_string_based_" + nodeIndex + ";DB_CLOSE_DELAY=-1")
+                     .username("sa");
+            }
+         }
+      }
+      return TestCacheManagerFactory.createClusteredCacheManager(holder);
    }
 
    protected boolean transactionsEnabled() {
