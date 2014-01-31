a1ec450c7c3e4c636e3c78064d108442606878b1
Pedro Ruivo <pruivo@redhat.com>
2014-01-31 17:03:24 +0000

    ISPN-3942 HotRod client keep trying recover a connection to a cluster member after shutdown

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/transport/tcp/RoundRobinBalancingStrategy.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/transport/tcp/RoundRobinBalancingStrategy.java
@@ -45,7 +45,7 @@ public class RoundRobinBalancingStrategy implements RequestBalancingStrategy {
 
    private int index = 0;
 
-   private SocketAddress[] servers;
+   private volatile SocketAddress[] servers;
 
    @Override
    public void setServers(Collection<SocketAddress> servers) {
@@ -79,7 +79,11 @@ public class RoundRobinBalancingStrategy implements RequestBalancingStrategy {
    }
 
    private SocketAddress getServerByIndex(int pos) {
-      SocketAddress server = servers[pos];
+      SocketAddress[] copy = servers;
+      if (pos >= copy.length) {
+         pos = 0;
+      }
+      SocketAddress server = copy[pos];
       if (log.isTraceEnabled()) {
          log.tracef("Returning server: %s", server);
       }
@@ -91,6 +95,6 @@ public class RoundRobinBalancingStrategy implements RequestBalancingStrategy {
    }
 
    public int getNextPosition() {
-      return  index;
+      return index;
    }
 }
