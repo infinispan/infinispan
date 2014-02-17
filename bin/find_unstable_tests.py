7500c46938de6e37f36f37041a613b87ccc8621c
Dan Berindei <dan@infinispan.org>
2014-02-17 19:46:38 +0200

    ISPN-4054 Scripts to analyze test failures and corresponding issues
    * find_unstable_tests.py - list the tests with the @Test(groups =
      "unstable") annotation
    * find_unstable_tests_jira.py - list the tests with an open issue in JIRA
    * find_unstable_tests_teamcity.py - list the tests with failures in CI
    * diff_test_lists.py - find differences in the three sets of tests, e.g.
      tests with failures in CI and with an issue in JIRA but not in the
      unstable group.

--- bin/find_unstable_tests.py
+++ bin/find_unstable_tests.py
@@ -2,37 +2,46 @@
 import re
 import time
 import sys
-from utils import *
+import csv
+import argparse
+import os.path
+import fnmatch
 
-def main():
-  start_time = time.clock()
-  disabled_test_files = []
-  
-  test_annotation_matcher = re.compile('^\s*@Test')
-  disabled_matcher = re.compile('groups\s*=\s*"unstable"')
-  
-  for test_file in GlobDirectoryWalker(get_search_path(sys.argv[0]), '*Test.java'):
-    tf = open(test_file)
-    try:
-      for line in tf:
-        if test_annotation_matcher.search(line) and disabled_matcher.search(line):
-          disabled_test_files.append(test_file)
-          break
-    finally:
-      tf.close()
-      
-  print "Files containing disabled tests: \n"
-  unique_tests=to_set(disabled_test_files)
-  i = 1
-  for f in unique_tests:
-    zeropad=""
-    if i < 10 and len(unique_tests) > 9:
-      zeropad = " "
-    print "%s%s. %s" % (zeropad, str(i), strip_leading_dots(f))
-    i += 1
 
-  print "\n      (finished in " +  str(time.clock() - start_time) + " seconds)"
+def main(args):
+  base_dir = args.dir
+
+  annotated_test_files = []
   
+  disabled_test_matcher = re.compile('\s*@Test.*groups\s*=\s*("unstable|Array\("unstable"\))|@Category\(UnstableTest\.class\).*')
+  filename_matcher = re.compile('.*Test.(java|scala)')
+
+  for dirpath, dirnames, filenames in os.walk(base_dir):
+    for filename in filenames:
+      if filename_matcher.match(filename):
+        test_file = os.path.join(dirpath, filename)
+        with open(test_file) as tf:
+          for line in tf:
+            if disabled_test_matcher.search(line):
+              class_name = os.path.splitext(filename)[0]
+              rel_test_file = os.path.relpath(test_file, base_dir)
+              annotated_test_files.append((class_name, rel_test_file))
+              break
+
+  annotated_test_files=sorted(annotated_test_files)
+
+  csvwriter = csv.writer(sys.stdout, dialect='excel-tab')
+  for row in annotated_test_files:
+    csvwriter.writerow(row)
+
+def extract_class_name(f):
+  return splitext(basename(f))[0]
+
+
 if __name__ == '__main__':
-  main()
+  parser = argparse.ArgumentParser()
+  parser.add_argument("dir", help="base directory", nargs='?', default='.')
+  args = parser.parse_args()
+
+  main(args)
   
