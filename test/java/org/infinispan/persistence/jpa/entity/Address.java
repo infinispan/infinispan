7fe2d5ea5310722f3643639b911a846035cdd4d1
Vojtech Juranek <vjuranek@redhat.com>
2014-03-05 12:14:02 +0100

    ISPN-4072 JPA Cache Store fails to clear entity which conatins ElementCollection
    * Fix ISPN-4072
    * Add unit tests for entity containing fields with annotations
    @Embedded and @ElementCollection

---
+++ test/java/org/infinispan/persistence/jpa/entity/Address.java
@@ -0,0 +1,71 @@
+package org.infinispan.persistence.jpa.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address implements Serializable {
+
+   private static final long serialVersionUID = 5289488219505339148L;
+
+   private String city;
+   private String street;
+   private int zipCode;
+
+   public String getCity() {
+      return city;
+   }
+
+   public void setCity(String city) {
+      this.city = city;
+   }
+
+   public String getStreet() {
+      return street;
+   }
+
+   public void setStreet(String street) {
+      this.street = street;
+   }
+
+   public int getZipCode() {
+      return zipCode;
+   }
+
+   public void setZipCode(int zipCode) {
+      this.zipCode = zipCode;
+   }
+
+   public static long getSerialversionuid() {
+      return serialVersionUID;
+   }
+
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (o == null || getClass() != o.getClass())
+         return false;
+
+      final Address address = (Address) o;
+
+      if (city != null ? !city.equals(address.getCity()) : address.getCity() != null)
+         return false;
+      if (street != null ? !street.equals(address.getStreet()) : address.getStreet() != null)
+         return false;
+      if (zipCode != address.getZipCode())
+         return false;
+
+      return true;
+   }
+
+   public int hashCode() {
+      final int prime = 31;
+      int result;
+      result = (city != null ? city.hashCode() : 0);
+      result = prime * result + (street != null ? street.hashCode() : 0);
+      result = prime * result + zipCode;
+      return result;
+   }
+
+}
---
+++ test/java/org/infinispan/persistence/jpa/entity/Person.java
@@ -0,0 +1,110 @@
+package org.infinispan.persistence.jpa.entity;
+
+import java.io.Serializable;
+import java.util.Set;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+
+@Entity
+public class Person implements Serializable {
+
+   private static final long serialVersionUID = 4748311041613897465L;
+
+   @Id
+   private String id;
+
+   private String name;
+
+   @ElementCollection(fetch = FetchType.EAGER)
+   private Set<String> nickNames;
+
+   @Embedded
+   @AttributeOverrides({ @AttributeOverride(name = "zipCode", column = @Column(name = "zip")) })
+   private Address address;
+
+   @ElementCollection(fetch = FetchType.EAGER)
+   private Set<Address> secondaryAdresses;
+
+   public String getId() {
+      return id;
+   }
+
+   public void setId(String id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Set<String> getNickNames() {
+      return nickNames;
+   }
+
+   public void setNickNames(Set<String> nickNames) {
+      this.nickNames = nickNames;
+   }
+
+   public Address getAddress() {
+      return address;
+   }
+
+   public void setAddress(Address address) {
+      this.address = address;
+   }
+
+   public Set<Address> getSecondaryAdresses() {
+      return secondaryAdresses;
+   }
+
+   public void setSecondaryAdresses(Set<Address> secondaryAdresses) {
+      this.secondaryAdresses = secondaryAdresses;
+   }
+
+   public boolean equals(Object o) {
+      if (this == o)
+         return true;
+      if (o == null || getClass() != o.getClass())
+         return false;
+
+      final Person person = (Person) o;
+
+      if (id != null ? !id.equals(person.getId()) : person.getId() != null)
+         return false;
+      if (name != null ? !name.equals(person.getName()) : person.getName() != null)
+         return false;
+      if ( (nickNames != null && !nickNames.isEmpty()) ? !nickNames.equals(person.getNickNames()) : 
+         (person.getNickNames() != null && !person.getNickNames().isEmpty()))
+         return false;
+      if (address != null ? !address.equals(person.getAddress()) : person.getAddress() != null)
+         return false;
+      if ( (secondaryAdresses != null  && !secondaryAdresses.isEmpty() ) ? !secondaryAdresses.equals(person.getSecondaryAdresses()) : 
+         (person.getSecondaryAdresses() != null && !person.getSecondaryAdresses().isEmpty()))
+         return false;
+
+      return true;
+   }
+
+   public int hashCode() {
+      final int prime = 31;
+      int result;
+      result = (id != null ? id.hashCode() : 0);
+      result = prime * result + (name != null ? name.hashCode() : 0);
+      result = prime * result + (nickNames != null ? nickNames.hashCode() : 0);
+      result = prime * result + (address != null ? address.hashCode() : 0);
+      result = prime * result + (secondaryAdresses != null ? secondaryAdresses.hashCode() : 0);
+      return result;
+   }
+   
+}
