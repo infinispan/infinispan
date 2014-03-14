6482d04ca8e63eca118b0f60f7c1af8c10162452
Vitalii Chepeliuk <vchepeli@redhat.com>
2014-03-14 16:08:37 +0100

    chande ispn-arq-container version to released one

--- server/integration/testsuite/src/test/java/org/infinispan/server/test/query/RemoteQueryIspnDirTest.java
+++ server/integration/testsuite/src/test/java/org/infinispan/server/test/query/RemoteQueryIspnDirTest.java
@@ -2,6 +2,7 @@ package org.infinispan.server.test.query;
 
 import org.infinispan.arquillian.core.InfinispanResource;
 import org.infinispan.arquillian.core.RemoteInfinispanServer;
+import org.infinispan.arquillian.core.RunningServer;
 import org.infinispan.arquillian.core.WithRunningServer;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.runner.RunWith;
@@ -12,7 +13,7 @@ import org.junit.runner.RunWith;
  * @author Adrian Nistor
  */
 @RunWith(Arquillian.class)
-@WithRunningServer("remote-query-infinispan-dir")
+@WithRunningServer({@RunningServer(name = "remote-query-infinispan-dir")})
 public class RemoteQueryIspnDirTest extends RemoteQueryTest {
 
    @InfinispanResource("remote-query-infinispan-dir")
