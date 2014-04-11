5934666620075113283ffff3fc9314de243b0388
Alan Field <afield@redhat.com>
2014-04-11 15:24:46 -0400

    ISPN-4204: Sync distexec demo project with current test code
    Add a no argument constructor to WordCountCollator
    Add a new distributed executor that performs a regular expression search
    of the values in the cache
    Reset counters in constructor for WordCountMapperEmitPerValue, and only
    log progress after processing 5000 values

--- demos/distexec/src/main/java/org/infinispan/demo/mapreduce/WordCountMapperEmitPerValue.java
+++ demos/distexec/src/main/java/org/infinispan/demo/mapreduce/WordCountMapperEmitPerValue.java
@@ -8,7 +8,14 @@ import org.infinispan.distexec.mapreduce.Mapper;
 
 public class WordCountMapperEmitPerValue implements Mapper<String, String, String, Integer> {
    private static final long serialVersionUID = -5943370243108735560L;
-   private static int values = 0, words = 0;
+   private int values = 0, words = 0;
+
+   public WordCountMapperEmitPerValue() {
+      super();
+      // Reset counters
+      values = 0;
+      words = 0;
+   }
 
    @Override
    public void map(String key, String value, Collector<String, Integer> c) {
@@ -33,7 +40,8 @@ public class WordCountMapperEmitPerValue implements Mapper<String, String, Strin
          c.emit(entry.getKey().toLowerCase(), entry.getValue());
       }
 
-      if (values % 1000 == 0)
-         System.out.printf("Analyzed %s words in %s lines%n", words, values);
+      if (values % 5000 == 0) {
+         System.out.printf(Thread.currentThread().toString() + " : Analyzed %s words in %s values\n", words, values);
+      }
    }
 }
