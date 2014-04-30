a07eadd9d0eb942d9191a1254e30cc4e2e944028
Ion Savin <comp_@gmx.net>
2014-04-30 22:16:01 +0300

    ISPN-800 The osgi test bundle should export the test packages as they might contain classes which need to be loaded by ispn.

--- integrationtests/osgi/src/test/java/org/infinispan/it/osgi/BaseInfinispanCoreOSGiTest.java
+++ integrationtests/osgi/src/test/java/org/infinispan/it/osgi/BaseInfinispanCoreOSGiTest.java
@@ -1,8 +1,13 @@
 package org.infinispan.it.osgi;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.ProbeBuilder;
+import org.ops4j.pax.exam.TestProbeBuilder;
 
 import static org.infinispan.it.osgi.util.IspnKarafOptions.*;
 import static org.ops4j.pax.exam.CoreOptions.junitBundles;
@@ -24,5 +29,25 @@ public abstract class BaseInfinispanCoreOSGiTest extends MultipleCacheManagersTe
             junitBundles(),
             keepRuntimeFolder()
       );
+   }
+
+   @ProbeBuilder
+   public TestProbeBuilder exportTestPackages(TestProbeBuilder probeBuilder) {
+       StringBuilder builder = new StringBuilder();
+
+       /* Export all test subpackages. */
+       Package[] pkgs = Package.getPackages();
+       for (Package pkg : pkgs) {
+           String pkgName = pkg.getName();
+           if (pkgName.startsWith("org.infinispan.it.osgi")) {
+               if (builder.length() > 0) {
+                   builder.append(",");
+               }
+               builder.append(pkgName);
+           }
+       }
+
+       probeBuilder.setHeader("Export-Package", builder.toString());
+       return probeBuilder;
    }
 }
