5ab905043777b726beb12aad66a59fb064ff2aa9
Tristan Tarrant <ttarrant@redhat.com>
2014-05-14 09:43:47 +0200

    ISPN-4284 Special handling for the realm DIGEST-MD5 mech so that pre-digested and plain-text passwords are both supported when using the jboss-sasl digest mech

--- server/integration/integration/src/main/java/org/infinispan/server/endpoint/subsystem/EndpointServerAuthenticationProvider.java
+++ server/integration/integration/src/main/java/org/infinispan/server/endpoint/subsystem/EndpointServerAuthenticationProvider.java
@@ -3,6 +3,7 @@ package org.infinispan.server.endpoint.subsystem;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.Collection;
+import java.util.Map;
 
 import javax.security.auth.callback.Callback;
 import javax.security.auth.callback.UnsupportedCallbackException;
@@ -12,6 +13,7 @@ import org.infinispan.server.core.security.AuthorizingCallbackHandler;
 import org.infinispan.server.core.security.ServerAuthenticationProvider;
 import org.infinispan.server.core.security.SubjectUserInfo;
 import org.jboss.as.domain.management.AuthenticationMechanism;
+import org.jboss.as.domain.management.RealmConfigurationConstants;
 import org.jboss.as.domain.management.SecurityRealm;
 import org.jboss.logging.Logger;
 
@@ -22,6 +24,15 @@ import org.jboss.logging.Logger;
  * @since 7.0
  */
 public class EndpointServerAuthenticationProvider implements ServerAuthenticationProvider {
+   static final String SASL_OPT_REALM_PROPERTY = "com.sun.security.sasl.digest.realm";
+   static final String SASL_OPT_ALT_PROTO_PROPERTY = "org.jboss.sasl.digest.alternative_protocols";
+   static final String SASL_OPT_PRE_DIGESTED_PROPERTY = "org.jboss.sasl.digest.pre_digested";
+
+   static final String DIGEST_MD5 = "DIGEST-MD5";
+   static final String EXTERNAL = "EXTERNAL";
+   static final String GSSAPI = "GSSAPI";
+   static final String PLAIN = "PLAIN";
+
    private static final Logger logger = Logger.getLogger(EndpointServerAuthenticationProvider.class);
    private final SecurityRealm realm;
 
@@ -30,15 +41,26 @@ public class EndpointServerAuthenticationProvider implements ServerAuthenticatio
    }
 
    @Override
-   public AuthorizingCallbackHandler getCallbackHandler(String mechanismName) {
-      if ("GSSAPI".equals(mechanismName)) {
+   public AuthorizingCallbackHandler getCallbackHandler(String mechanismName, Map<String, String> mechanismProperties) {
+      if (GSSAPI.equals(mechanismName)) {
          // The EAP SecurityRealm doesn't actually support a GSSAPI mech yet so let's handle this ourselves
          return new GSSAPIEndpointAuthorizingCallbackHandler();
-      } else if ("PLAIN".equals(mechanismName)) {
+      } else if (PLAIN.equals(mechanismName)) {
          return new RealmAuthorizingCallbackHandler(realm.getAuthorizingCallbackHandler(AuthenticationMechanism.PLAIN));
-      } else if ("DIGEST-MD5".equals(mechanismName)) {
+      } else if (DIGEST_MD5.equals(mechanismName)) {
+         if (!mechanismProperties.containsKey(SASL_OPT_REALM_PROPERTY)) {
+            mechanismProperties.put(SASL_OPT_REALM_PROPERTY, realm.getName());
+         }
+         Map<String, String> mechConfig = realm.getMechanismConfig(AuthenticationMechanism.DIGEST);
+         boolean plainTextDigest = true;
+         if (mechConfig.containsKey(RealmConfigurationConstants.DIGEST_PLAIN_TEXT)) {
+             plainTextDigest = Boolean.parseBoolean(mechConfig.get(RealmConfigurationConstants.DIGEST_PLAIN_TEXT));
+         }
+         if (!plainTextDigest) {
+            mechanismProperties.put(SASL_OPT_PRE_DIGESTED_PROPERTY, "true");
+         }
          return new RealmAuthorizingCallbackHandler(realm.getAuthorizingCallbackHandler(AuthenticationMechanism.DIGEST));
-      } else if ("EXTERNAL".equals(mechanismName)) {
+      } else if (EXTERNAL.equals(mechanismName)) {
          return new RealmAuthorizingCallbackHandler(realm.getAuthorizingCallbackHandler(AuthenticationMechanism.CLIENT_CERT));
       } else {
          throw new IllegalArgumentException("Unsupported mech " + mechanismName);
