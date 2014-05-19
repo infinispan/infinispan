894af20f6e267d3062fac81a67d5dd5d5e9a1e12
Galder Zamarreño <galder@zamarreno.com>
2014-05-19 09:32:42 +0200

    ISPN-4296 Restore predefined cache limitation for Hot Rod servers
    * Revert "ISPN-833 Remove cache defined limitation for Infinispan servers"
    * This reverts commit 2d5acd4bf685f27b45d43dbd0dea53ecea411527.
    * This reverts commit 9d10e1b49899ca6edae6cff741cb9b3d311f7eeb.

--- server/hotrod/src/test/scala/org/infinispan/server/hotrod/HotRodFunctionalTest.scala
+++ server/hotrod/src/test/scala/org/infinispan/server/hotrod/HotRodFunctionalTest.scala
@@ -50,9 +50,10 @@ class HotRodFunctionalTest extends HotRodSingleNodeTest {
    }
 
    def testPutOnUndefinedCache(m: Method) {
-      val resp = client.execute(0xA0, 0x01, "boomooo", k(m), 0, 0, v(m), 0, 1, 0)
-      assertStatus(resp, Success)
-      assertHotRodEquals(cacheManager, "boomooo", k(m), v(m))
+      val resp = client.execute(0xA0, 0x01, "boomooo", k(m), 0, 0, v(m), 0, 1, 0).asInstanceOf[TestErrorResponse]
+      assertTrue(resp.msg.contains("CacheNotFoundException"))
+      assertEquals(resp.status, ParseError, "Status should have been 'ParseError' but instead was: " + resp.status)
+      client.assertPut(m)
    }
 
    def testPutOnTopologyCache(m: Method) {
