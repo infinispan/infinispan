1312ac34bdab1797dbf25076748955bf019b4921
Martin Gencur <mgencur@redhat.com>
2014-05-26 11:43:20 +0200

    BZ1075719 Fix tests now that validation of configs works properly

--- query/src/test/java/org/infinispan/query/blackbox/LocalCacheAsyncCacheStoreTest.java
+++ query/src/test/java/org/infinispan/query/blackbox/LocalCacheAsyncCacheStoreTest.java
@@ -21,7 +21,7 @@ public class LocalCacheAsyncCacheStoreTest extends LocalCacheTest {
 
    @Override
    protected EmbeddedCacheManager createCacheManager() throws Exception {
-      cacheManager = TestCacheManagerFactory.fromXml("async-store-config.xml");
+      cacheManager = TestCacheManagerFactory.fromXml("async-file-store-config.xml");
       cache = cacheManager.getCache("queryCache_lucenestore_async_filestore");
 
       return cacheManager;
