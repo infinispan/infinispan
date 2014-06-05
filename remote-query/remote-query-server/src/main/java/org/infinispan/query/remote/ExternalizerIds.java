1ceb73038aeb9a83172fa685b52e5b3e049f6fed
Adrian Nistor <anistor@redhat.com>
2014-06-05 12:27:08 +0300

    ISPN-4350 Infinispan remote query server module needs to use a free Externalizer ID range
    Change the range to 1700-1799

--- remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/ExternalizerIds.java
+++ remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/ExternalizerIds.java
@@ -1,10 +1,15 @@
 package org.infinispan.query.remote;
 
 /**
+ * Identifiers used by the Marshaller to delegate to specialized Externalizers. For details, read
+ * http://infinispan.org/docs/7.0.x/user_guide/user_guide.html#_preassigned_externalizer_id_ranges
+ * <p/>
+ * The range reserved for the Infinispan Remote Query module is from 1700 to 1799.
+ *
  * @author anistor@redhat.com
  * @since 6.0
  */
 public interface ExternalizerIds {
 
-   Integer PROTOBUF_VALUE_WRAPPER = 1400;
+   Integer PROTOBUF_VALUE_WRAPPER = 1700;
 }
