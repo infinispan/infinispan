4ed3ef710fa8276193d531f72d1deb8e2aa1be5d
Adrian Nistor <anistor@redhat.com>
2014-06-19 18:27:51 +0300

    Extract constant

--- remote-query/remote-query-client/src/main/java/org/infinispan/query/remote/client/MarshallerRegistration.java
+++ remote-query/remote-query-client/src/main/java/org/infinispan/query/remote/client/MarshallerRegistration.java
@@ -11,8 +11,10 @@ import java.io.IOException;
  */
 public class MarshallerRegistration {
 
+   public static final String PROTOBUF_RES = "/org/infinispan/query/remote/client/query.protobin";
+
    public static void registerMarshallers(SerializationContext ctx) throws IOException, Descriptors.DescriptorValidationException {
-      ctx.registerProtofile(MarshallerRegistration.class.getResourceAsStream("/org/infinispan/query/remote/client/query.protobin"));
+      ctx.registerProtofile(MarshallerRegistration.class.getResourceAsStream(PROTOBUF_RES));
       ctx.registerMarshaller(new QueryRequestMarshaller());
       ctx.registerMarshaller(new QueryResponseMarshaller());
    }
