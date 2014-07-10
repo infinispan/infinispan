ea87d6cad4c805c33348d8eda02a22b1107a1f7f
William Burns <mudokonman@gmail.com>
2014-07-10 15:30:37 -0400

    ISPN-4498 OutOfMemoryError when CI is ran with tracing
    * Commented out usage of NDC for now

--- commons/src/main/java/org/infinispan/commons/logging/LogFactory.java
+++ commons/src/main/java/org/infinispan/commons/logging/LogFactory.java
@@ -20,13 +20,19 @@ public class LogFactory {
    }
 
    public static void pushNDC(String cacheName, boolean isTrace) {
-      if (isTrace)
-         NDC.push(cacheName);
+      // Disabled NDC usage until https://issues.jboss.org/browse/JBLOGGING-106 is fixed or
+      // we introduce Log4j2 https://issues.jboss.org/browse/ISPN-3076
+      // See https://issues.jboss.org/browse/ISPN-4498 for more details
+//      if (isTrace)
+//         NDC.push(cacheName);
    }
 
    public static void popNDC(boolean isTrace) {
-      if (isTrace)
-         NDC.pop();
+      // Disabled NDC usage until https://issues.jboss.org/browse/JBLOGGING-106 is fixed or
+      // we introduce Log4j2 https://issues.jboss.org/browse/ISPN-3076
+      // See https://issues.jboss.org/browse/ISPN-4498 for more details
+//      if (isTrace)
+//         NDC.pop();
    }
 
 }
