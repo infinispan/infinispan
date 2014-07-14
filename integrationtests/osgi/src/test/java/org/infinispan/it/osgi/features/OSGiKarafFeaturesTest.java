fbebef75067697a54116d8a433fce85c064e914a
Ion Savin <comp_@gmx.net>
2014-07-14 12:06:51 +0300

    ISPN-4494 OSGi tests fail on jdk8
    Disable the OSGi tests when running on jdk8 and jdk9.

--- integrationtests/osgi/src/test/java/org/infinispan/it/osgi/features/OSGiKarafFeaturesTest.java
+++ integrationtests/osgi/src/test/java/org/infinispan/it/osgi/features/OSGiKarafFeaturesTest.java
@@ -19,7 +19,6 @@ import org.ops4j.pax.exam.TestProbeBuilder;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
-import org.ops4j.pax.exam.spi.reactors.PerSuite;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.FrameworkUtil;
