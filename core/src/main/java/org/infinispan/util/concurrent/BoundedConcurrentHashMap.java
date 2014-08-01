a51ad98092439d7d31bd2e96cc047e216049e860
Karsten Blees <blees@dcon.de>
2014-08-01 19:39:37 +0200

    Eviction: fix performance issue when removing many entries
    Removing many entries from a cache with eviction is very slow. The reason
    for this is that the internal data structure of ConcurrentLinkedQueue
    (used to track concurrent cache hits) degenerates to a very large linked
    list caontaining only null-entries.
    
    The effect can be reproduced with ConcurrentLinkedQueue as follows:
    
      ConcurrentLinkedQueue<Integer> q = new ConcurrentLinkedQueue();
      q.add(-1);
      for (int i = 0; i < 1000000; i++) {
        q.add(i);
        q.remove(i);
      }
    
    Note that this insuspicious looking loop takes more than 40 minutes for a
    million items.
    
    In EvitionPolicy.onEntryRemove(), do not remove entries from the concurrent
    accessQueue. This is futile anyway, as EvictionPolicy.onEntryHit() and
    Segment.get() are not synchronized, so the removed entry can be re-added
    at any time after returning from onEntryRemove().
    
    When processing enqueued cache hits in EvictionPolicy.execute(), ignore
    entries that have already been removed. For LRU, use LinkedHashMap.get()
    instead of put() to achieve this. LIRS already checks entry.isResident(),
    so no change is necessary.
    
    Signed-off-by: Karsten Blees <blees@dcon.de>

--- core/src/main/java/org/infinispan/util/concurrent/BoundedConcurrentHashMap.java
+++ core/src/main/java/org/infinispan/util/concurrent/BoundedConcurrentHashMap.java
@@ -455,7 +455,7 @@ public class BoundedConcurrentHashMap<K, V> extends AbstractMap<K, V>
       public Set<HashEntry<K, V>> execute() {
          Set<HashEntry<K, V>> evictedCopy = new HashSet<HashEntry<K, V>>();
          for (HashEntry<K, V> e : accessQueue) {
-            put(e, e.value);
+            get(e);
          }
          evictedCopy.addAll(evicted);
          accessQueue.clear();
@@ -498,10 +498,6 @@ public class BoundedConcurrentHashMap<K, V> extends AbstractMap<K, V>
       @Override
       public void onEntryRemove(HashEntry<K, V> e) {
          remove(e);
-         // we could have multiple instances of e in accessQueue; remove them all
-         while (accessQueue.remove(e)) {
-            accessQueueSize.decrementAndGet();            
-         }
       }
 
       @Override
@@ -1092,12 +1088,7 @@ public class BoundedConcurrentHashMap<K, V> extends AbstractMap<K, V>
 
       @Override
       public void onEntryRemove(HashEntry<K, V> e) {
-         
          ((LIRSHashEntry<K,V>)e).remove();
-         // we could have multiple instances of e in accessQueue; remove them all
-         while (accessQueue.remove(e)) {
-            accessQueueSize.decrementAndGet();
-         }
       }
 
       @Override
--- core/src/test/java/org/infinispan/util/concurrent/BoundedConcurrentHashMapTest.java
+++ core/src/test/java/org/infinispan/util/concurrent/BoundedConcurrentHashMapTest.java
@@ -1,14 +1,16 @@
 package org.infinispan.util.concurrent;
 
+import org.infinispan.commons.equivalence.AnyEquivalence;
 import org.infinispan.util.EquivalentHashMapTest;
+import org.infinispan.util.concurrent.BoundedConcurrentHashMap.Eviction;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import static org.testng.AssertJUnit.*;
-import static org.testng.AssertJUnit.assertTrue;
 
 /**
  * Tests bounded concurrent hash map logic against the JDK ConcurrentHashMap.
@@ -129,4 +131,38 @@ public class BoundedConcurrentHashMapTest extends EquivalentHashMapTest {
 //      }
 //   }
 
+   private void testRemovePerformance(Eviction eviction)
+   {
+      final int COUNT = 200000;
+      Map<Integer, Integer> bchm = new BoundedConcurrentHashMap<Integer, Integer>(
+            COUNT, 1, eviction, AnyEquivalence.INT, AnyEquivalence.INT);
+
+      // fill the cache
+      for (int i = 0; i < COUNT; i++)
+         bchm.put(i, i);
+
+      // force a single cache hit (so that accessQueue has a head item)
+      bchm.get(0);
+
+      // remove items
+      long start = System.currentTimeMillis();
+      for (int i = 1; i < COUNT; i++)
+      {
+         bchm.get(i);
+         bchm.remove(i);
+
+         // original version needs ~5 min for 200k entries (2h for 1M)
+         // fixed version needs < 200ms for 200k (500ms for 1M)
+         if (System.currentTimeMillis() - start > 5000)
+            fail(eviction.name() + ": removing " + COUNT + " entries takes more than 5 seconds!");
+      }
+   }
+
+   public void testLRURemovePerformance() {
+      testRemovePerformance(Eviction.LRU);
+   }
+
+   public void testLIRSRemovePerformance() {
+      testRemovePerformance(Eviction.LIRS);
+   }
 }
