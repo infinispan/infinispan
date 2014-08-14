c296b7cd210d44fda34588b666ccfc2f6f6b3d67
Sanne Grinovero <sanne@hibernate.org>
2014-08-14 16:58:11 +0100

    ISPN-2727 Re-enable functional test query.distributed.MultiNodeLocalTxTest

--- query/src/test/java/org/infinispan/query/distributed/MultiNodeLocalTxTest.java
+++ query/src/test/java/org/infinispan/query/distributed/MultiNodeLocalTxTest.java
@@ -5,20 +5,13 @@ import org.testng.annotations.Test;
 /**
  * Similiar to MultiNodeLocalTest, only uses transactional infinispan configuration.
  *
- * @TODO enable the test when ISPN-2727 is fixed.
- *
  * @author Anna Manukyan
  */
-@Test(groups = "unstable", testName = "query.distributed.MultiNodeLocalTxTest", description = "original group: functional -- ISPN-2727")
+@Test(groups = "functional", testName = "query.distributed.MultiNodeLocalTxTest")
 public class MultiNodeLocalTxTest extends MultiNodeLocalTest {
 
    public boolean transactionsEnabled() {
       return true;
    }
 
-   //@TODO enable the test when ISPN-2727 is fixed.
-   @Test(groups = "unstable")
-   public void testIndexingWorkDistribution() throws Exception {
-      super.testIndexingWorkDistribution();
-   }
 }
