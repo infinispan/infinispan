0f491fb08bbba50b15da3e9c24e55554c167323d
Adrian Nistor <anistor@redhat.com>
2014-08-20 19:19:45 +0300

    ISPN-4670 Matcher performance test

---
+++ object-filter/src/test/java/org/infinispan/objectfilter/test/perf/Profiling.java
@@ -0,0 +1,10 @@
+package org.infinispan.objectfilter.test.perf;
+
+/**
+ * Profiling test category.
+ *
+ * @author anistor@redhat.com
+ * @since 7.0
+ */
+public interface Profiling {
+}
---
+++ object-filter/src/test/java/org/infinispan/objectfilter/test/perf/ProtobufPerfTest.java
@@ -0,0 +1,34 @@
+package org.infinispan.objectfilter.test.perf;
+
+import org.infinispan.objectfilter.Matcher;
+import org.infinispan.objectfilter.impl.ProtobufMatcher;
+import org.infinispan.objectfilter.test.model.MarshallerRegistration;
+import org.infinispan.protostream.ConfigurationBuilder;
+import org.infinispan.protostream.ProtobufUtil;
+import org.infinispan.protostream.SerializationContext;
+import org.junit.Before;
+import org.junit.experimental.categories.Category;
+
+/**
+ * @author anistor@redhat.com
+ * @since 7.0
+ */
+@Category(Profiling.class)
+public class ProtobufPerfTest extends PerfTest {
+
+   private SerializationContext serCtx;
+
+   @Before
+   public void setUp() throws Exception {
+      serCtx = ProtobufUtil.newSerializationContext(new ConfigurationBuilder().build());
+      MarshallerRegistration.registerMarshallers(serCtx);
+   }
+
+   protected Matcher createMatcher() throws Exception {
+      return new ProtobufMatcher(serCtx);
+   }
+
+   protected Object createPerson1() throws Exception {
+      return ProtobufUtil.toWrappedByteArray(serCtx, super.createPerson1());
+   }
+}
