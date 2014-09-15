bcdce9cfa2217cfede21190712b5adf011dc2ada
Gustavo Fernandes <gustavonalle@gmail.com>
2014-09-15 16:46:33 +0100

    ISPN-2981 Infinispan as Lucene directory provider "No sub-file with id .fnm found"

--- lucene/lucene-directory/src/main/java/org/infinispan/lucene/impl/FileListOperations.java
+++ lucene/lucene-directory/src/main/java/org/infinispan/lucene/impl/FileListOperations.java
@@ -1,5 +1,6 @@
 package org.infinispan.lucene.impl;
 
+import net.jcip.annotations.GuardedBy;
 import org.infinispan.AdvancedCache;
 import org.infinispan.context.Flag;
 import org.infinispan.lucene.FileCacheKey;
@@ -7,6 +8,9 @@ import org.infinispan.lucene.FileListCacheKey;
 import org.infinispan.lucene.FileMetadata;
 import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
  * Collects operations on the existing fileList, stored as a Set<String> having key
@@ -24,13 +28,18 @@ public final class FileListOperations {
    private final AdvancedCache<FileListCacheKey, Object> cache;
    private final String indexName;
    private final AdvancedCache<FileListCacheKey, FileListCacheValue> cacheNoRetrieve;
+   private final Lock readLock;
+   private final Lock writeLock;
 
    @SuppressWarnings("unchecked")
-   public FileListOperations(AdvancedCache<?, ?> cache, String indexName){
+   public FileListOperations(AdvancedCache<?, ?> cache, String indexName) {
       this.cache = (AdvancedCache<FileListCacheKey, Object>) cache;
       this.cacheNoRetrieve = (AdvancedCache<FileListCacheKey, FileListCacheValue>) cache.withFlags(Flag.IGNORE_RETURN_VALUES);
       this.indexName = indexName;
       this.fileListCacheKey = new FileListCacheKey(indexName);
+      ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+      readLock = lock.readLock();
+      writeLock = lock.writeLock();
    }
 
    /**
@@ -38,12 +47,17 @@ public final class FileListOperations {
     * @param fileName
     */
    void addFileName(final String fileName) {
-      final FileListCacheValue fileList = getFileList();
-      boolean done = fileList.add(fileName);
-      if (done) {
-         cacheNoRetrieve.put(fileListCacheKey, fileList);
-         if (trace)
-            log.trace("Updated file listing: added " + fileName);
+      writeLock.lock();
+      try {
+         final FileListCacheValue fileList = getFileList();
+         boolean done = fileList.add(fileName);
+         if (done) {
+            cacheNoRetrieve.put(fileListCacheKey, fileList);
+            if (trace)
+               log.trace("Updated file listing: added " + fileName);
+         }
+      } finally {
+         writeLock.unlock();
       }
    }
 
@@ -63,13 +77,18 @@ public final class FileListOperations {
     * @param toAdd
     */
    public void removeAndAdd(final String toRemove, final String toAdd) {
-      FileListCacheValue fileList = getFileList();
-      boolean done = fileList.addAndRemove(toAdd, toRemove);
-      if (done) {
-         cacheNoRetrieve.put(fileListCacheKey, fileList);
-         if (trace && done) {
-            log.trace("Updated file listing: added " + toAdd + " and removed " + toRemove);
+      writeLock.lock();
+      try {
+         FileListCacheValue fileList = getFileList();
+         boolean done = fileList.addAndRemove(toAdd, toRemove);
+         if (done) {
+            cacheNoRetrieve.put(fileListCacheKey, fileList);
+            if (trace) {
+               log.trace("Updated file listing: added " + toAdd + " and removed " + toRemove);
+            }
          }
+      } finally {
+         writeLock.unlock();
       }
    }
 
@@ -77,7 +96,12 @@ public final class FileListOperations {
     * @return an array containing all names of existing "files"
     */
    public String[] listFilenames() {
-      return getFileList().toArray();
+      readLock.lock();
+      try {
+         return getFileList().toArray();
+      } finally {
+         readLock.unlock();
+      }
    }
 
    /**
@@ -85,32 +109,43 @@ public final class FileListOperations {
     * @return true if there is such a named file in this index
     */
    public boolean fileExists(final String fileName) {
-      return getFileList().contains(fileName);
+      readLock.lock();
+      try {
+         return getFileList().contains(fileName);
+      } finally {
+         readLock.unlock();
+      }
    }
 
    /**
     * Deleted a file from the list of files actively part of the index
     * @param fileName
     */
-   public void deleteFileName(String fileName) {
-      FileListCacheValue fileList = getFileList();
-      boolean done = fileList.remove(fileName);
-      if (done) {
-         cacheNoRetrieve.put(fileListCacheKey, fileList);
-         if (trace)
-            log.trace("Updated file listing: removed " + fileName);
+   public void deleteFileName(final String fileName) {
+      writeLock.lock();
+      try {
+         FileListCacheValue fileList = getFileList();
+         boolean done = fileList.remove(fileName);
+         if (done) {
+            cacheNoRetrieve.put(fileListCacheKey, fileList);
+            if (trace)
+               log.trace("Updated file listing: removed " + fileName);
+         }
+      } finally {
+         writeLock.unlock();
       }
    }
 
    /**
     * @return the current list of files being part of the index
     */
+   @GuardedBy("writeLock")
    private FileListCacheValue getFileList() {
       FileListCacheValue fileList = (FileListCacheValue) cache.get(fileListCacheKey);
       if (fileList == null) {
          fileList = new FileListCacheValue();
          FileListCacheValue prev = (FileListCacheValue) cache.putIfAbsent(fileListCacheKey, fileList);
-         if ( prev != null ) {
+         if (prev != null) {
             fileList = prev;
          }
       }
---
+++ query/src/test/java/org/infinispan/query/backend/MergeTest.java
@@ -0,0 +1,107 @@
+package org.infinispan.query.backend;
+
+import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.Index;
+import org.infinispan.query.test.Person;
+import org.infinispan.test.MultipleCacheManagersTest;
+import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.atomic.AtomicLong;
+
+import static org.infinispan.query.helper.StaticTestingErrorHandler.assertAllGood;
+import static org.infinispan.test.TestingUtil.killCacheManagers;
+
+/**
+ * Test to simulate concurrent index writing and merges using Infinispan Directory under
+ * the InfinispanIndexManager
+ *
+ * @author gustavonalle
+ * @since 7.0
+ */
+@Test(groups = "profiling", testName = "query.backend.MergeTest")
+public class MergeTest extends MultipleCacheManagersTest {
+
+   // Low merge factor means more frequent merges
+   private static final String MERGE_FACTOR = "3";
+   private static final int OBJECT_COUNT = 5000;
+   private static final int NUMBER_OF_THREADS = 2;
+   private static final boolean TX_ENABLED = false;
+
+   Cache<Long, Person> cache1, cache2;
+
+   /**
+    * Main method to escape from testng
+    */
+   public static void main(String[] args) throws Throwable {
+      MergeTest c = new MergeTest();
+      c.createBeforeClass();
+      c.createBeforeMethod();
+      c.testMergesWrites();
+   }
+
+   @Override
+   protected void createCacheManagers() throws Throwable {
+      ConfigurationBuilder cacheCfg = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, TX_ENABLED);
+      cacheCfg.clustering().sync().replTimeout(120000)
+            .indexing().index(Index.ALL)
+            .addProperty("default.directory_provider", "infinispan")
+            .addProperty("default.indexmanager", "org.infinispan.query.indexmanager.InfinispanIndexManager")
+            .addProperty("error_handler", "org.infinispan.query.helper.StaticTestingErrorHandler")
+            .addProperty("default.indexwriter.merge_factor", MERGE_FACTOR)
+            .addProperty("default.indexwriter.merge_max_size", "1024")
+            .addProperty("default.indexwriter.ram_buffer_size", "256");
+      List<Cache<Long, Person>> caches = createClusteredCaches(2, cacheCfg);
+      cache1 = caches.get(0);
+      cache2 = caches.get(1);
+   }
+
+   public void testMergesWrites() throws Exception {
+      final long start = System.currentTimeMillis();
+
+      ExecutorService executorService = Executors.newCachedThreadPool();
+      final CountDownLatch waitFor = new CountDownLatch(1);
+      final AtomicLong id = new AtomicLong(1);
+      ArrayList<Future> futures = new ArrayList<>();
+      final Random random = new Random();
+
+      for (int i = 0; i < NUMBER_OF_THREADS; i++) {
+         futures.add(executorService.submit(new Runnable() {
+            @Override
+            public void run() {
+               try {
+                  waitFor.await();
+                  Thread.sleep(random.nextInt(3000));
+                  for (int j = 0; j < OBJECT_COUNT; j++) {
+                     Cache<Long, Person> cache = (j % 2 == 0) ? cache1 : cache2;
+                     long key = id.incrementAndGet();
+                     cache.put(key, new Person("name" + key, "blurb", 30));
+                     if (j % 100 == 0) {
+                        System.out.println(j + " in " + (System.currentTimeMillis() - start) / 1000 + "s ");
+                     }
+                  }
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }));
+      }
+
+      waitFor.countDown();
+      for (Future f : futures) {
+         f.get();
+      }
+
+      assertAllGood(cache1, cache2);
+      System.out.println("Test took: " + (System.currentTimeMillis() - start) / 1000 + " s");
+      killCacheManagers(cacheManagers);
+   }
+}
