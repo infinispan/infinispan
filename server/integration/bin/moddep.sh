df8980c6e3814137016374860c8645e4f23905b2
Tristan Tarrant <ttarrant@redhat.com>
2014-09-30 22:30:38 +0200

    Re-enable server module trimming

--- server/integration/bin/moddep.sh
+++ server/integration/bin/moddep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIRNAME=`dirname "$0"`
 BASEDIR=$DIRNAME/..
-MODULES=`find $BASEDIR/server/target -name 'module.xml'`
-$DIRNAME/moduleTools.py $@ $BASEDIR/server/target/generated-configs/standalone/configuration/standalone-ha.xml $MODULES
+MODULES=`find $BASEDIR/build/target/infinispan-server-* -name 'module.xml'`
+$DIRNAME/moduleTools.py $@ $BASEDIR/build/target/generated-configs/standalone/configuration/clustered.xml $MODULES
 
 # moddep.sh | tr . / | sed 's/$/\/main\/\*\*/' > trim-modules.txt
