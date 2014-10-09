a96a758bf9b60a5be68f313c29accc811fc09d3b
Ion Savin <comp_@gmx.net>
2014-10-09 17:50:57 +0300

    ISPN-4818 Allow creation of Infinispan caches as OSGi managed service
    Add checkstyle plugin and correspoding clean-ups.

--- osgi/src/main/java/org/infinispan/osgi/InfinispanServiceFactory.java
+++ osgi/src/main/java/org/infinispan/osgi/InfinispanServiceFactory.java
@@ -1,23 +1,5 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package org.infinispan.osgi;
 
-import java.lang.Thread;
 import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Hashtable;
@@ -45,14 +27,14 @@ public class InfinispanServiceFactory implements ManagedServiceFactory {
     }
 
     @Override
-    synchronized public void updated(String pid, Dictionary properties) throws ConfigurationException {
+    public synchronized void updated(String pid, Dictionary properties) throws ConfigurationException {
         deleted(pid);
 
         String config = (String) properties.get("config");
         if (config == null) {
             throw new ConfigurationException("config", "Property must be set");
         }
-        String instanceId = (String)properties.get(INSTANCE_ID);
+        String instanceId = (String) properties.get(INSTANCE_ID);
         if (instanceId == null) {
             throw new ConfigurationException(INSTANCE_ID, "Property must be set");
         }
@@ -82,7 +64,7 @@ public class InfinispanServiceFactory implements ManagedServiceFactory {
     }
 
     @Override
-    synchronized public void deleted(String pid) {
+    public synchronized void deleted(String pid) {
         ServiceRegistration serviceRegistration = managedRegistrations.remove(pid);
         if (serviceRegistration != null) {
             try {
@@ -100,7 +82,7 @@ public class InfinispanServiceFactory implements ManagedServiceFactory {
         }
     }
 
-    synchronized public void destroy() {
+    public synchronized void destroy() {
         for (String cacheManager: cacheManagers.keySet()) {
             deleted(cacheManager);
         }
