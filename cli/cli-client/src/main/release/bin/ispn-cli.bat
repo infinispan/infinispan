033742c57c8a5e34fe770a38ea6c5833171c2f99
Tristan Tarrant <ttarrant@redhat.com>
2014-11-06 17:28:40 +0100

    ISPN-4948 Package the CLI as a self-standing JAR

--- cli/cli-client/src/main/release/bin/ispn-cli.bat
+++ cli/cli-client/src/main/release/bin/ispn-cli.bat
@@ -4,10 +4,7 @@ setlocal enabledelayedexpansion
 set ISPN_HOME=%~dp0
 set ISPN_HOME="%ISPN_HOME%.."
 
-set /p CP=<%ISPN_HOME%\modules\cli-client\runtime-classpath.txt
-set CP=%CP::=;%
-set CP=%CP:$ISPN_HOME=!ISPN_HOME!%
-set CP=%ISPN_HOME%\modules\cli-client\infinispan-cli-client.jar;%CP%
+set CP=%ISPN_HOME%\modules\cli\infinispan-cli.jar;%CP%
 rem echo libs: %CP%
 
 java -classpath "%CP%" org.infinispan.cli.Main %*
