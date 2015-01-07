fa1ff6b7af5d0553f49a3bb805fb2cc73b76fab0
Tristan Tarrant <ttarrant@redhat.com>
2015-01-07 12:22:36 +0100

    ISPN-5121 The CDIDistributedTaskLifecycle should not do anything when CDI is not available

--- cdi/src/main/java/org/infinispan/cdi/CDIDistributedTaskLifecycle.java
+++ cdi/src/main/java/org/infinispan/cdi/CDIDistributedTaskLifecycle.java
@@ -13,33 +13,49 @@ import org.infinispan.distexec.spi.DistributedTaskLifecycle;
 
 public class CDIDistributedTaskLifecycle implements
          DistributedTaskLifecycle {
+   private final boolean HAVE_CDI;
+   
+   public CDIDistributedTaskLifecycle() {
+      boolean success;
+      try {
+         this.getClass().getClassLoader().loadClass("javax.enterprise.inject.spi.BeanManager");
+         success = true;
+      } catch(ClassNotFoundException e) {
+         success = false;
+      }
+      HAVE_CDI = success;
+   }
 
 
    @Override
    @SuppressWarnings({ "unchecked" })
    public <T, K, V> void onPreExecute(Callable<T> task, Cache<K, V> inputCache) {
-      BeanManager bm = BeanManagerProvider.getInstance().getBeanManager();
-      ContextInputCache.set(inputCache);
-      Class<Callable<T>> clazz = (Class<Callable<T>>) task.getClass();
-      AnnotatedType<Callable<T>> type = bm.createAnnotatedType(clazz);
-      InjectionTarget<Callable<T>> it = bm.createInjectionTarget(type);
-      CreationalContext<Callable<T>> ctx = bm.createCreationalContext(null);
-      it.inject(task, ctx);
-      it.postConstruct(task);    
+      if (HAVE_CDI) {
+         BeanManager bm = BeanManagerProvider.getInstance().getBeanManager();
+         ContextInputCache.set(inputCache);
+         Class<Callable<T>> clazz = (Class<Callable<T>>) task.getClass();
+         AnnotatedType<Callable<T>> type = bm.createAnnotatedType(clazz);
+         InjectionTarget<Callable<T>> it = bm.createInjectionTarget(type);
+         CreationalContext<Callable<T>> ctx = bm.createCreationalContext(null);
+         it.inject(task, ctx);
+         it.postConstruct(task);
+      }
    }
 
    @Override
    @SuppressWarnings({ "unchecked" })
    public <T> void onPostExecute(Callable<T> task) {
-      try {
-         BeanManager bm = BeanManagerProvider.getInstance().getBeanManager();
-         Class<Callable<T>> clazz = (Class<Callable<T>>) task.getClass();
-         AnnotatedType<Callable<T>> type = bm.createAnnotatedType(clazz);
-         InjectionTarget<Callable<T>> it = bm.createInjectionTarget(type);
-         it.preDestroy(task);
-         it.dispose(task);
-      } finally {
-         ContextInputCache.clean();
+      if (HAVE_CDI) {
+         try {
+            BeanManager bm = BeanManagerProvider.getInstance().getBeanManager();
+            Class<Callable<T>> clazz = (Class<Callable<T>>) task.getClass();
+            AnnotatedType<Callable<T>> type = bm.createAnnotatedType(clazz);
+            InjectionTarget<Callable<T>> it = bm.createInjectionTarget(type);
+            it.preDestroy(task);
+            it.dispose(task);
+         } finally {
+            ContextInputCache.clean();
+         }
       }
    }
 }
