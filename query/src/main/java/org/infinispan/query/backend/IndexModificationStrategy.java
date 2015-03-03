522235b5704e33c48c58ffb6b693c7bc22b41558
Sanne Grinovero <sanne@hibernate.org>
2015-03-03 14:55:09 +0000

    ISPN-5258 Upgrade to Hibernate Search 5.1.1.Final, Apache Lucene 4.10.4

--- query/src/main/java/org/infinispan/query/backend/IndexModificationStrategy.java
+++ query/src/main/java/org/infinispan/query/backend/IndexModificationStrategy.java
@@ -1,6 +1,7 @@
 package org.infinispan.query.backend;
 
 import org.hibernate.search.engine.integration.impl.ExtendedSearchIntegrator;
+import org.hibernate.search.spi.IndexingMode;
 import org.hibernate.search.spi.SearchIntegrator;
 import org.infinispan.commands.FlagAffectedCommand;
 import org.infinispan.configuration.cache.Configuration;
@@ -60,7 +61,8 @@ public enum IndexModificationStrategy {
     * @return the appropriate IndexModificationStrategy
     */
    public static IndexModificationStrategy configuredStrategy(SearchIntegrator searchFactory, Configuration cfg) {
-      if ("manual".equals(searchFactory.unwrap(ExtendedSearchIntegrator.class).getIndexingStrategy())) {
+      IndexingMode indexingMode = searchFactory.unwrap(ExtendedSearchIntegrator.class).getIndexingMode();
+      if (indexingMode == IndexingMode.MANUAL) {
          return MANUAL;
       }
       else {
