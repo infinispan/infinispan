67a8bcfb234f1588b86eed43b0b5fd801cee7086
Alan Field <afield@redhat.com>
2015-03-18 14:13:16 -0400

    Additional Lucene Directory Wildfly integration tests
    Add methods to MemberRegistration:
       query using the Lucene API
       purge index
       create index
       
    Add tests to InfinispanModuleMemberRegistrationIT:
       use the Lucene API to search for members
       purge index and verify no members are found
       create index and verify members are found

--- integrationtests/as-lucene-directory/src/test/java/org/infinispan/test/integration/as/wildfly/controller/MemberRegistration.java
+++ integrationtests/as-lucene-directory/src/test/java/org/infinispan/test/integration/as/wildfly/controller/MemberRegistration.java
@@ -1,23 +1,29 @@
 package org.infinispan.test.integration.as.wildfly.controller;
 
-import org.apache.lucene.search.Query;
-import org.hibernate.search.jpa.FullTextEntityManager;
-import org.infinispan.test.integration.as.wildfly.model.Member;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Stateful;
 import javax.enterprise.inject.Model;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.inject.Named;
-import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.Query;
+import org.hibernate.search.MassIndexer;
+import org.hibernate.search.jpa.Search;
+import org.infinispan.test.integration.as.wildfly.model.Member;
 
 @Stateful
 @Model
 public class MemberRegistration {
 
-   @Inject
-   private FullTextEntityManager em;
+   @PersistenceContext
+   private EntityManager em;
 
    private Member newMember;
 
@@ -38,12 +44,27 @@ public class MemberRegistration {
 
    @SuppressWarnings("unchecked")
    public List<Member> search(String name) {
-      Query luceneQuery = em.getSearchFactory().buildQueryBuilder()
-            .forEntity(Member.class).get().keyword()
-            .onField("name").matching(name)
-            .createQuery();
+      Query luceneQuery = Search.getFullTextEntityManager(em).getSearchFactory().buildQueryBuilder()
+            .forEntity(Member.class).get().keyword().onField("name").matching(name).createQuery();
 
-      return em.createFullTextQuery(luceneQuery).getResultList();
+      return Search.getFullTextEntityManager(em).createFullTextQuery(luceneQuery).getResultList();
+   }
+
+   @SuppressWarnings("unchecked")
+   public List<Member> luceneSearch(String name) throws ParseException {
+      QueryParser parser = new QueryParser("name", new StandardAnalyzer());
+      org.apache.lucene.search.Query luceneQuery = parser.parse(name);
+
+      return Search.getFullTextEntityManager(em).createFullTextQuery(luceneQuery).getResultList();
+   }
+
+   public void purgeMemberIndex() {
+      Search.getFullTextEntityManager(em).purgeAll(Member.class);
+   }
+
+   public void indexMembers() throws InterruptedException {
+      MassIndexer mi = Search.getFullTextEntityManager(em).createIndexer(Member.class);
+      mi.startAndWait();
    }
 
    @PostConstruct
