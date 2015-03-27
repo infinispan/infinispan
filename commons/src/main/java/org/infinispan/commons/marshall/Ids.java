ca85d2168f870a6355eb82ef04b3d2b49b8d34d0
Dan Berindei <dan@infinispan.org>
2015-03-27 08:23:23 +0200

    ISPN-5019 Improve the memory usage and serialization of consistent hash classes

--- commons/src/main/java/org/infinispan/commons/marshall/Ids.java
+++ commons/src/main/java/org/infinispan/commons/marshall/Ids.java
@@ -14,7 +14,8 @@ public interface Ids {
    int EMPTY_SET = 88;
    int EMPTY_MAP = 89;
    int EMPTY_LIST = 90;
-   // internal collections (id=18 no longer in use, might get reused at a later stage)
+   
+   int IMMUTABLE_LIST = 18;
    int IMMUTABLE_MAP = 19;
    int BYTE_BUFFER = 106;
 }
