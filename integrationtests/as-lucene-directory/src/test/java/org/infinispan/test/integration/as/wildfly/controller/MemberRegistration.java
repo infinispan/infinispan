8b65ccc90f12b0c88e286fc3303329977ddad1d3
Alan Field <afield@redhat.com>
2015-05-01 06:23:45 -0400

    Added IndexReader and Spatial search tests
    IndexReader and spatial search tests are working.
    This commit includes changes suggested by Nicolas Helleringer to the
    spatial annotations on the Member class, and the
    spatialSearchWithDistance() method.

--- integrationtests/as-lucene-directory/src/test/java/org/infinispan/test/integration/as/wildfly/controller/MemberRegistration.java
+++ integrationtests/as-lucene-directory/src/test/java/org/infinispan/test/integration/as/wildfly/controller/MemberRegistration.java
@@ -1,5 +1,7 @@
 package org.infinispan.test.integration.as.wildfly.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,11 +13,17 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.Query;
 import org.hibernate.search.MassIndexer;
+import org.hibernate.search.SearchFactory;
+import org.hibernate.search.annotations.Spatial;
+import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
+import org.hibernate.search.query.dsl.Unit;
 import org.infinispan.test.integration.as.wildfly.model.Member;
 
 @Stateful
@@ -48,6 +56,59 @@ public class MemberRegistration {
             .forEntity(Member.class).get().keyword().onField("name").matching(name).createQuery();
 
       return Search.getFullTextEntityManager(em).createFullTextQuery(luceneQuery).getResultList();
+   }
+
+   @SuppressWarnings("unchecked")
+   public List<Member> spatialSearch(double latitude, double longitude, double distanceinKM) {
+      Query spatialQuery = Search.getFullTextEntityManager(em).getSearchFactory()
+            .buildQueryBuilder().forEntity( Member.class ).get().spatial()
+            .within( distanceinKM, Unit.KM )
+            .ofLatitude( latitude )
+            .andLongitude( longitude )
+            .createQuery();
+      return Search.getFullTextEntityManager(em).createFullTextQuery(spatialQuery, Member.class).getResultList();
+   }
+
+   @SuppressWarnings("unchecked")
+   public List<Object[]> spatialSearchWithDistance(double latitude, double longitude, double distanceinKM) {
+      Query spatialQuery = Search.getFullTextEntityManager(em).getSearchFactory()
+            .buildQueryBuilder().forEntity(Member.class).get().spatial()
+            .within(distanceinKM, Unit.KM)
+            .ofLatitude(latitude)
+            .andLongitude(longitude)
+            .createQuery();
+
+      FullTextQuery hibQuery = Search.getFullTextEntityManager(em).createFullTextQuery(spatialQuery, Member.class);
+      hibQuery.setProjection(FullTextQuery.SPATIAL_DISTANCE, FullTextQuery.THIS);
+      hibQuery.setSpatialParameters(latitude, longitude, Spatial.COORDINATES_DEFAULT_FIELD);
+      return hibQuery.getResultList();
+   }
+
+   public List<Document> indexSearch(String name) throws IOException {
+      ArrayList<Document> result = new ArrayList<Document>();
+      SearchFactory searchFactory = Search.getFullTextEntityManager(em).getSearchFactory();
+      IndexReader reader = searchFactory.getIndexReaderAccessor().open(Member.class);
+      try {
+         for (int i = 0; i < reader.maxDoc(); i++) {
+            Document member = reader.document(i);
+            if (member != null && member.get("name").contains(name)) {
+               result.add(member);
+            }
+         }
+      } finally {
+         searchFactory.getIndexReaderAccessor().close(reader);
+      }
+      return result;
+   }
+
+   public int indexSize() {
+      SearchFactory searchFactory = Search.getFullTextEntityManager(em).getSearchFactory();
+      IndexReader reader = searchFactory.getIndexReaderAccessor().open(Member.class);
+      try {
+         return reader.maxDoc();
+      } finally {
+         searchFactory.getIndexReaderAccessor().close(reader);
+      }
    }
 
    @SuppressWarnings("unchecked")
