67df523d857635298957effc75e23dd1ac02e89a
Dan Berindei <dan@infinispan.org>
2015-05-25 23:36:47 +0300

    ISPN-3924 NonTxPutIfAbsentDuringLeaveStressTest is no longer failing
    Fixed with ISPN-5494

--- core/src/test/java/org/infinispan/distribution/rehash/NonTxPutIfAbsentDuringLeaveStressTest.java
+++ core/src/test/java/org/infinispan/distribution/rehash/NonTxPutIfAbsentDuringLeaveStressTest.java
@@ -1,14 +1,9 @@
 package org.infinispan.distribution.rehash;
 
 import org.infinispan.Cache;
-import org.infinispan.commons.CacheException;
 import org.infinispan.commons.util.CollectionFactory;
-import org.infinispan.commons.util.concurrent.jdk8backported.EquivalentConcurrentHashMapV8;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
-import org.infinispan.remoting.RemoteException;
-import org.infinispan.remoting.transport.jgroups.SuspectException;
-import org.infinispan.statetransfer.OutdatedTopologyException;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.infinispan.test.TestingUtil;
 import org.infinispan.test.fwk.CleanupAfterMethod;
@@ -16,7 +11,6 @@ import org.infinispan.transaction.TransactionMode;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.Callable;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -59,7 +53,6 @@ public class NonTxPutIfAbsentDuringLeaveStressTest extends MultipleCacheManagers
       return c;
    }
 
-   @Test(groups = "unstable")
    public void testNodeLeavingDuringPutIfAbsent() throws Exception {
       Future[] futures = new Future[NUM_WRITERS];
       for (int i = 0; i < NUM_WRITERS; i++) {
@@ -70,35 +63,23 @@ public class NonTxPutIfAbsentDuringLeaveStressTest extends MultipleCacheManagers
                while (!stop) {
                   for (int j = 0; j < NUM_KEYS; j++) {
                      Cache<Object, Object> cache = cache(writerIndex % NUM_ORIGINATORS);
-                     putRetryOnSuspect(cache, "key_" + j, "value_" + j + "_" + writerIndex);
+                     doPut(cache, "key_" + j, "value_" + j + "_" + writerIndex);
                   }
                }
                return null;
             }
 
-            private void putRetryOnSuspect(Cache<Object, Object> cache, String key, String value) {
-               try {
-                  Object oldValue = cache.putIfAbsent(key, value);
-                  Object newValue = cache.get(key);
-                  if (oldValue == null) {
-                     // succeeded
-                     log.tracef("Successfully inserted value %s for key %s", value, key);
-                     assertEquals(value, newValue);
-                     assertNull(insertedValues.putIfAbsent(key, value));
-                  } else {
-                     // failed
-                     assertEquals(oldValue, newValue);
-                  }
-               } catch (CacheException e) {
-                  Throwable ce = e;
-                  while (ce instanceof RemoteException) {
-                     ce = ce.getCause();
-                  }
-                  // Retry on OutdatedTopologyException and SuspectException, rethrow any other exception
-                  if (!(ce instanceof OutdatedTopologyException) && !(ce instanceof SuspectException))
-                     throw e;
-
-                  putRetryOnSuspect(cache, key, value);
+            private void doPut(Cache<Object, Object> cache, String key, String value) {
+               Object oldValue = cache.putIfAbsent(key, value);
+               Object newValue = cache.get(key);
+               if (oldValue == null) {
+                  // succeeded
+                  log.tracef("Successfully inserted value %s for key %s", value, key);
+                  assertEquals(value, newValue);
+                  assertNull(insertedValues.putIfAbsent(key, value));
+               } else {
+                  // failed
+                  assertEquals(oldValue, newValue);
                }
             }
          });
