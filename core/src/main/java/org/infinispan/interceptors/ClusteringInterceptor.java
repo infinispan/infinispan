c9e42c4dc8c3677f7c0b1ca9d3f50279db651604
Dennis Reed <dereed@redhat.com>
2015-06-11 16:10:06 -0400

    [ISPN-5526] Replication: The DELTA_WRITE flag should force a remote get during state transfer

--- core/src/main/java/org/infinispan/interceptors/ClusteringInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/ClusteringInterceptor.java
@@ -76,17 +76,19 @@ public abstract class ClusteringInterceptor extends BaseRpcInterceptor {
    }
 
    protected boolean needsRemoteGet(InvocationContext ctx, AbstractDataCommand command) {
-      if (command.hasFlag(Flag.CACHE_MODE_LOCAL)
+      if (!command.hasFlag(Flag.DELTA_WRITE)
+         && (command.hasFlag(Flag.CACHE_MODE_LOCAL)
             || command.hasFlag(Flag.SKIP_REMOTE_LOOKUP)
-            || command.hasFlag(Flag.IGNORE_RETURN_VALUES)) {
+            || command.hasFlag(Flag.IGNORE_RETURN_VALUES))) {
          return false;
       }
       boolean shouldFetchFromRemote = false;
       CacheEntry entry = ctx.lookupEntry(command.getKey());
-      if (entry == null || entry.isNull() || entry.isLockPlaceholder()) {
+      if (entry == null || entry.isNull() || entry.isLockPlaceholder()
+         || command.hasFlag(Flag.DELTA_WRITE)) {
          Object key = command.getKey();
          ConsistentHash ch = stateTransferManager.getCacheTopology().getReadConsistentHash();
-         shouldFetchFromRemote = ctx.isOriginLocal() && !ch.isKeyLocalToNode(rpcManager.getAddress(), key) && !dataContainer.containsKey(key);
+         shouldFetchFromRemote = (ctx.isOriginLocal() && !ch.isKeyLocalToNode(rpcManager.getAddress(), key) || command.hasFlag(Flag.DELTA_WRITE)) && !dataContainer.containsKey(key);
          if (!shouldFetchFromRemote && getLog().isTraceEnabled()) {
             getLog().tracef("Not doing a remote get for key %s since entry is mapped to current node (%s) or is in L1. Owners are %s", key, rpcManager.getAddress(), ch.locateOwners(key));
          }
