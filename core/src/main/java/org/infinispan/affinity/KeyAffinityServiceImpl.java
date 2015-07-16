4d23f26c33fdd30342c7f9174095f8215eaa5ec6
baranowb <baranowb@gmail.com>
2015-07-16 12:30:21 +0200

    ISPN-5568 KeyAffinityService race condition on view change

--- core/src/main/java/org/infinispan/affinity/KeyAffinityServiceImpl.java
+++ core/src/main/java/org/infinispan/affinity/KeyAffinityServiceImpl.java
@@ -1,29 +1,8 @@
-/*
- * JBoss, Home of Professional Open Source
- * Copyright 2010 Red Hat Inc. and/or its affiliates and other
- * contributors as indicated by the @author tags. All rights reserved.
- * See the copyright.txt in the distribution for a full listing of
- * individual contributors.
- *
- * This is free software; you can redistribute it and/or modify it
- * under the terms of the GNU Lesser General Public License as
- * published by the Free Software Foundation; either version 2.1 of
- * the License, or (at your option) any later version.
- *
- * This software is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this software; if not, write to the Free
- * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
- * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */
 package org.infinispan.affinity;
 
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
+
 import org.infinispan.Cache;
 import org.infinispan.distribution.DistributionManager;
 import org.infinispan.distribution.ch.ConsistentHash;
@@ -60,7 +39,10 @@ public class KeyAffinityServiceImpl<K> implements KeyAffinityService<K> {
 
    // TODO During state transfer, we should try to assign keys to a node only if they are owners in both CHs
    public final static float THRESHOLD = 0.5f;
-   
+
+   //interval between key/queue poll
+   private static final int POOL_INTERVAL = 50;
+
    private static final Log log = LogFactory.getLog(KeyAffinityServiceImpl.class);
 
    private final Set<Address> filter;
@@ -122,20 +104,15 @@ public class KeyAffinityServiceImpl<K> implements KeyAffinityService<K> {
          throw new NullPointerException("Null address not supported!");
 
       BlockingQueue<K> queue = null;
-      maxNumberInvariant.readLock().lock();
-      try {
-         queue = address2key.get(address);
-         if (queue == null)
-            throw new IllegalStateException("Address " + address + " is no longer in the cluster");
-      } finally {
-         maxNumberInvariant.readLock().unlock();
-      }
       try {
          K result = null;
          while (result == null && !keyGenWorker.isStopped()) {
             // obtain the read lock inside the loop, otherwise a topology change will never be able
             // to obtain the write lock
             maxNumberInvariant.readLock().lock();
+            queue = address2key.get(address);
+            if (queue == null)
+               throw new IllegalStateException("Address " + address + " is no longer in the cluster");
             try {
                // first try to take an element without waiting
                result = queue.poll();
@@ -149,12 +126,17 @@ public class KeyAffinityServiceImpl<K> implements KeyAffinityService<K> {
             } finally {
                maxNumberInvariant.readLock().unlock();
             }
+            try {
+               Thread.currentThread().sleep(POOL_INTERVAL);
+            } catch (InterruptedException e) {
+               e.printStackTrace();
+            }
          }
          existingKeyCount.decrementAndGet();
          log.tracef("Returning key %s for address %s", result, address);
          return result;
       } finally {
-         if (queue.size() < bufferSize * THRESHOLD + 1) {
+         if (queue != null && queue.size() < bufferSize * THRESHOLD + 1) {
             keyProducerStartLatch.open();
          }
       }
@@ -249,8 +231,7 @@ public class KeyAffinityServiceImpl<K> implements KeyAffinityService<K> {
             }
          } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
-         }
-         finally {
+         } finally {
             log.debugf("Shutting down KeyAffinity service for key set: %s", filter);
          }
       }
@@ -354,10 +335,12 @@ public class KeyAffinityServiceImpl<K> implements KeyAffinityService<K> {
       ConsistentHash hash = distributionManager.getConsistentHash();
       return hash.getMembers().contains(address);
    }
+
    private DistributionManager getDistributionManager() {
       DistributionManager distributionManager = cache.getAdvancedCache().getDistributionManager();
       if (distributionManager == null) {
-         throw new IllegalStateException("Null distribution manager. Is this an distributed(v.s. replicated) cache?");
+         throw new IllegalStateException(
+               "Null distribution manager. Is this an distributed(v.s. replicated) cache?");
       }
       return distributionManager;
    }
