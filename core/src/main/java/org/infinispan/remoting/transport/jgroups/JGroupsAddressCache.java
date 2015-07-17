d50f99be9c1b94177edf96f187740654280112ad
Dan Berindei <dan@infinispan.org>
2015-07-17 17:03:39 +0300

    ISPN-5641 Reduce contention in JGroupsAddressCache
    Avoid computeIfAbsent for existent entries, because it synchronizes
    on the entry.

--- core/src/main/java/org/infinispan/remoting/transport/jgroups/JGroupsAddressCache.java
+++ core/src/main/java/org/infinispan/remoting/transport/jgroups/JGroupsAddressCache.java
@@ -19,12 +19,18 @@ public class JGroupsAddressCache {
    // HACK: Avoid the org.jgroups.Address reference in the signature so that local caches can work without the jgroups jar.
    // Otherwise, instantiating the JGroupsAddress externalizer will try to load the org.jgroups.Address class.
    static org.infinispan.remoting.transport.Address fromJGroupsAddress(Object address) {
-      final Address addr1 = (Address) address;
-      return addressCache.computeIfAbsent(addr1, uuid -> {
-         if (addr1 instanceof ExtendedUUID)
-            return new JGroupsTopologyAwareAddress((ExtendedUUID) addr1);
-         else
-            return new JGroupsAddress(addr1);
+      final Address jgAddress = (Address) address;
+      // New entries are rarely added added after startup, but computeIfAbsent synchronizes every time
+      JGroupsAddress ispnAddress = addressCache.get(jgAddress);
+      if (ispnAddress != null) {
+         return ispnAddress;
+      }
+      return addressCache.computeIfAbsent(jgAddress, uuid -> {
+         if (jgAddress instanceof ExtendedUUID) {
+            return new JGroupsTopologyAwareAddress((ExtendedUUID) jgAddress);
+         } else {
+            return new JGroupsAddress(jgAddress);
+         }
       });
    }
 
