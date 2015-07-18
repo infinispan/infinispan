9e378af7f701471ae2682515ee981c58a65ea6f6
Dan Berindei <dan@infinispan.org>
2015-07-18 22:00:09 +0300

    ISPN-5639 Avoid creating a Callable instance for timeout handling

--- core/src/main/java/org/infinispan/remoting/transport/jgroups/RspListFuture.java
+++ core/src/main/java/org/infinispan/remoting/transport/jgroups/RspListFuture.java
@@ -1,14 +1,11 @@
 package org.infinispan.remoting.transport.jgroups;
 
 import org.infinispan.remoting.responses.Response;
-import org.infinispan.util.concurrent.TimeoutException;
 import org.jgroups.blocks.GroupRequest;
 import org.jgroups.util.FutureListener;
-import org.jgroups.util.Rsp;
 import org.jgroups.util.RspList;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
@@ -16,9 +13,10 @@ import java.util.concurrent.Future;
  * @author Dan Berindei
  * @since 8.0
  */
-class RspListFuture extends CompletableFuture<RspList<Response>> implements FutureListener<RspList<Response>> {
+class RspListFuture extends CompletableFuture<RspList<Response>> implements FutureListener<RspList<Response>>,
+      Callable<Void> {
    private final GroupRequest<Response> request;
-   private Future<?> timeoutFuture = null;
+   private volatile Future<?> timeoutFuture = null;
 
    RspListFuture(GroupRequest<Response> request) {
       this.request = request;
@@ -36,15 +34,18 @@ class RspListFuture extends CompletableFuture<RspList<Response>> implements Futu
       }
    }
 
-   public void timeout() {
-      complete(request.getResults());
-      request.cancel(true);
-   }
-
    public void setTimeoutFuture(Future<?> timeoutFuture) {
       this.timeoutFuture = timeoutFuture;
       if (isDone()) {
          timeoutFuture.cancel(false);
       }
+   }
+
+   @Override
+   public Void call() throws Exception {
+      // The request timed out
+      complete(request.getResults());
+      request.cancel(true);
+      return null;
    }
 }
