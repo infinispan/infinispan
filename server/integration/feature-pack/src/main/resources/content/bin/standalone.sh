536b580c415baa4f94efb9fc847207171be708c3
Tristan Tarrant <ttarrant@redhat.com>
2015-07-20 11:10:50 +0200

    ISPN-5618 Upgrade server to WildFly 9.0.0.Final
    - Upgrade server to WildFly 9.0.0.Final
    - Don't specify CompressedOops explicitly in the server startup options as the JDK already does that automatically

--- server/integration/feature-pack/src/main/resources/content/bin/standalone.sh
+++ server/integration/feature-pack/src/main/resources/content/bin/standalone.sh
@@ -157,13 +157,6 @@ if [ "$PRESERVE_JAVA_OPTS" != "true" ]; then
         fi
     fi
 
-    if [ $CLIENT_VM = false ]; then
-        NO_COMPRESSED_OOPS=`echo $JAVA_OPTS | $GREP "\-XX:\-UseCompressedOops"`
-        if [ "x$NO_COMPRESSED_OOPS" = "x" ]; then
-            "$JAVA" $JVM_OPTVERSION -server -XX:+UseCompressedOops -version >/dev/null 2>&1 && PREPEND_JAVA_OPTS="$PREPEND_JAVA_OPTS -XX:+UseCompressedOops"
-        fi
-    fi
-
     JAVA_OPTS="$PREPEND_JAVA_OPTS $JAVA_OPTS"
 fi
 
