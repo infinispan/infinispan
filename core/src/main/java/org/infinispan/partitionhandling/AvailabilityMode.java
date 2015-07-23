217f52fdde3700dd20c23cd2c9e09db0a4388306
Dan Berindei <dan@infinispan.org>
2015-07-23 13:03:22 +0300

    ISPN-5627 Really remove UNAVAILABLE availability mode

--- core/src/main/java/org/infinispan/partitionhandling/AvailabilityMode.java
+++ core/src/main/java/org/infinispan/partitionhandling/AvailabilityMode.java
@@ -10,13 +10,9 @@ public enum AvailabilityMode {
    AVAILABLE,
    /** Data can not be safely accessed because of a network split or successive nodes leaving. */
    DEGRADED_MODE,
-   /** @deprecated Use {@link #DEGRADED_MODE} instead. */
-   UNAVAILABLE,
    ;
 
    public AvailabilityMode min(AvailabilityMode other) {
-      if (this == UNAVAILABLE || other == UNAVAILABLE)
-         return UNAVAILABLE;
       if (this == DEGRADED_MODE || other == DEGRADED_MODE)
          return DEGRADED_MODE;
       return AVAILABLE;
