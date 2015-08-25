895963bd7355d11917543f1b379a551159fbfb44
Galder Zamarreño <galder@zamarreno.com>
2015-08-25 10:22:26 +0200

    ISPN-5684 Make getAll work with compatibility mode in DIST
    * When compatibility mode is enabled, a clustered getAll requests
      results in a local getAll, whose results should not be unboxed.
    * Also, when unboxing the result back for the client, unboxing must
      happen regardless unless the value is null.

--- core/src/main/java/org/infinispan/interceptors/compat/BaseTypeConverterInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/compat/BaseTypeConverterInterceptor.java
@@ -145,6 +145,10 @@ public abstract class BaseTypeConverterInterceptor extends CommandInterceptor {
          command.setKeys(boxedKeys);
       }
       Object ret = invokeNextInterceptor(ctx, command);
+
+      if (ret != null && !needsUnboxing(ctx))
+         return ret;
+
       if (ret != null) {
          if (command.isReturnEntries()) {
             Map<Object, CacheEntry> map = (Map<Object, CacheEntry>) ret;
@@ -169,11 +173,7 @@ public abstract class BaseTypeConverterInterceptor extends CommandInterceptor {
             Map<Object, Object> unboxed = command.createMap();
             for (Map.Entry<Object, Object> entry : map.entrySet()) {
                Object value = entry == null ? null : entry.getValue();
-               if (command.getRemotelyFetched() == null || !command.getRemotelyFetched().containsKey(entry.getKey())) {
-                  unboxed.put(converter.unboxKey(entry.getKey()), entry == null ? null : converter.unboxValue(value));
-               } else {
-                  unboxed.put(converter.unboxKey(entry.getKey()), value);
-               }
+               unboxed.put(converter.unboxKey(entry.getKey()), entry == null ? null : converter.unboxValue(value));
             }
             return unboxed;
          }
