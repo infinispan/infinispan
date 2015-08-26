4fb321249048f40b6444e1096b58e93835c906af
Tristan Tarrant <ttarrant@redhat.com>
2015-08-26 11:55:08 +0200

    ISPN-5701 Drop the server RHQ management plugin

--- server/integration/management/server-rhq-plugin/src/main/java/org/infinispan/server/rhq/IspnServerComponent.java
+++
@@ -1,7 +0,0 @@
-package org.infinispan.server.rhq;
-
-import org.rhq.core.pluginapi.measurement.MeasurementFacet;
-
-public class IspnServerComponent extends MetricsRemappingComponent<IspnServerComponent> implements MeasurementFacet {
-
-}
