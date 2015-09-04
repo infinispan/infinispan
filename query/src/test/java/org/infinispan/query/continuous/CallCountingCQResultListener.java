c167f03f218e8849e1cc8ebb947093fc682e3624
Vojtech Juranek <vjuranek@redhat.com>
2015-09-04 16:46:09 +0200

    ISPN-5713 Add more tests for continuous query
    * Test of more complex queries combining more predicates
    * Test for CQ in cluster

---
+++ query/src/test/java/org/infinispan/query/continuous/CallCountingCQResultListener.java
@@ -0,0 +1,42 @@
+package org.infinispan.query.continuous;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 
+ * {@link ContinuousQueryResultListener} which counts number of calls for each key.
+ * 
+ * @author vjuranek
+ * @since 8.0
+ */
+public class CallCountingCQResultListener<K, V> implements ContinuousQueryResultListener<K, V> {
+
+   private final Map<K, Integer> joined = new HashMap<K, Integer>();
+   private final Map<K, Integer> left = new HashMap<K, Integer>();
+
+   @Override
+   public void resultJoining(K key, V value) {
+      incrementNumberOfCalls(key, joined);
+   }
+
+   @Override
+   public void resultLeaving(K key) {
+      incrementNumberOfCalls(key, left);
+   }
+
+   public Map<K, Integer> getJoined() {
+      return joined;
+   }
+
+   public Map<K, Integer> getLeft() {
+      return left;
+   }
+
+   private void incrementNumberOfCalls(K key, Map<K, Integer> callMap) {
+      synchronized (callMap) {
+         Integer calls = callMap.get(key);
+         callMap.put(key, calls == null ? 1 : calls + 1);
+      }
+   }
+}
