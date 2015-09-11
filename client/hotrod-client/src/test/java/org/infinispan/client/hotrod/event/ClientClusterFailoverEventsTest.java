f3e7993a6d5fd8dd198de3fdc9b3c051f6b1a46b
Martin Gencur <mgencur@redhat.com>
2015-09-11 16:17:50 +0200

    ISPN-5747 Failover tests for HotRod expiration events
    * test that when a node crashes and includeCurrentState is true,
        the listener won't receive events for already expired entries

--- client/hotrod-client/src/test/java/org/infinispan/client/hotrod/event/ClientClusterFailoverEventsTest.java
+++ client/hotrod-client/src/test/java/org/infinispan/client/hotrod/event/ClientClusterFailoverEventsTest.java
@@ -8,7 +8,12 @@ import org.infinispan.client.hotrod.test.MultiHotRodServersTest;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.server.hotrod.HotRodServer;
+import org.infinispan.test.TestingUtil;
+import org.infinispan.util.ControlledTimeService;
+import org.infinispan.util.TimeService;
 import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
 
 import static org.infinispan.client.hotrod.test.HotRodClientTestingUtil.findServerAndKill;
 import static org.infinispan.client.hotrod.test.HotRodClientTestingUtil.killRemoteCacheManager;
@@ -17,9 +22,19 @@ import static org.infinispan.server.hotrod.test.HotRodTestingUtil.hotRodCacheCon
 @Test(groups = "functional", testName = "client.hotrod.event.ClientClusterFailoverEventsTest")
 public class ClientClusterFailoverEventsTest extends MultiHotRodServersTest {
 
+   protected ControlledTimeService ts0;
+   protected ControlledTimeService ts1;
+
    @Override
    protected void createCacheManagers() throws Throwable {
       // Empty
+   }
+
+   private void injectTimeServices() {
+      ts0 = new ControlledTimeService(0);
+      TestingUtil.replaceComponent(server(0).getCacheManager(), TimeService.class, ts0, true);
+      ts1 = new ControlledTimeService(0);
+      TestingUtil.replaceComponent(server(1).getCacheManager(), TimeService.class, ts1, true);
    }
 
    public void testEventReplayWithAndWithoutStateAfterFailover() {
@@ -27,11 +42,14 @@ public class ClientClusterFailoverEventsTest extends MultiHotRodServersTest {
       //base.clustering().hash().numOwners(1);
       ConfigurationBuilder builder = hotRodCacheConfiguration(base);
       createHotRodServers(2, builder);
+      injectTimeServices();
       try {
-         final Integer key0 = HotRodClientTestingUtil.getIntKeyForServer(server(0));
+         final Integer key00 = HotRodClientTestingUtil.getIntKeyForServer(server(0));
+         final Integer key10 = HotRodClientTestingUtil.getIntKeyForServer(server(0));
          final Integer key11 = HotRodClientTestingUtil.getIntKeyForServer(server(1));
          final Integer key21 = HotRodClientTestingUtil.getIntKeyForServer(server(1));
          final Integer key31 = HotRodClientTestingUtil.getIntKeyForServer(server(1));
+         final Integer key41 = HotRodClientTestingUtil.getIntKeyForServer(server(1));
 
          org.infinispan.client.hotrod.configuration.ConfigurationBuilder clientBuilder =
                new org.infinispan.client.hotrod.configuration.ConfigurationBuilder();
@@ -47,16 +65,26 @@ public class ClientClusterFailoverEventsTest extends MultiHotRodServersTest {
             c.addClientListener(statelessListener);
             c.addClientListener(statefulListener);
             c.addClientListener(failoverListener);
-            c.put(key0, "zero");
-            statefulListener.expectOnlyCreatedEvent(key0, cache(0));
-            statelessListener.expectOnlyCreatedEvent(key0, cache(0));
-            failoverListener.expectOnlyCreatedEvent(key0, cache(0));
-            c.put(key11, "one");
+            c.put(key00, "zero");
+            statefulListener.expectOnlyCreatedEvent(key00, cache(0));
+            statelessListener.expectOnlyCreatedEvent(key00, cache(0));
+            failoverListener.expectOnlyCreatedEvent(key00, cache(0));
+            c.put(key10, "one", 1000, TimeUnit.MILLISECONDS);
+            statefulListener.expectOnlyCreatedEvent(key10, cache(0));
+            statelessListener.expectOnlyCreatedEvent(key10, cache(0));
+            failoverListener.expectOnlyCreatedEvent(key10, cache(0));
+            c.put(key11, "two");
             statefulListener.expectOnlyCreatedEvent(key11, cache(0));
             statelessListener.expectOnlyCreatedEvent(key11, cache(0));
             failoverListener.expectOnlyCreatedEvent(key11, cache(0));
+            c.put(key41, "three", 1000, TimeUnit.MILLISECONDS);
+            statefulListener.expectOnlyCreatedEvent(key41, cache(0));
+            statelessListener.expectOnlyCreatedEvent(key41, cache(0));
+            failoverListener.expectOnlyCreatedEvent(key41, cache(0));
+            ts0.advance(1001);
+            ts1.advance(1001);
             findServerAndKill(newClient, servers, cacheManagers);
-            c.put(key21, "two");
+            c.put(key21, "four");
             // Failover expectations
             statelessListener.expectNoEvents();
             statefulListener.expectFailoverEvent();
@@ -64,8 +92,11 @@ public class ClientClusterFailoverEventsTest extends MultiHotRodServersTest {
             // State expectations
             statelessListener.expectNoEvents();
             failoverListener.expectNoEvents();
-            statefulListener.expectUnorderedEvents(ClientEvent.Type.CLIENT_CACHE_ENTRY_CREATED, key0, key11, key21);
-            c.put(key31, "three");
+            //we should receive CLIENT_CACHE_ENTRY_CREATED only for entries that did not expire
+            statefulListener.expectUnorderedEvents(ClientEvent.Type.CLIENT_CACHE_ENTRY_CREATED, key00, key11, key21);
+            //now there should be no events for key10 and key41 as they expired
+            statefulListener.expectNoEvents();
+            c.put(key31, "five");
             statefulListener.expectUnorderedEvents(ClientEvent.Type.CLIENT_CACHE_ENTRY_CREATED, key31);
             c.remove(key11);
             c.remove(key21);
