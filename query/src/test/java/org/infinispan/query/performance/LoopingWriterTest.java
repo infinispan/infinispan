058b8b6dabcc75fd41d131db0be01eaa2d46fd2b
Dan Berindei <dan@infinispan.org>
2015-09-12 16:46:07 +0300

    ISPN-5719 Use log4j 2.0 everywhere

--- query/src/test/java/org/infinispan/query/performance/LoopingWriterTest.java
+++ query/src/test/java/org/infinispan/query/performance/LoopingWriterTest.java
@@ -19,7 +19,7 @@ import org.testng.annotations.Test;
 /**
  * Starts writing a lot of data in a loop. Used to measure ingestion rate.
  *
- * -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+UseParNewGC -Xss500k -Xmx12G -Xms12G -Dlog4j.configuration=file:/opt/infinispan-log4j.xml
+ * -server -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -XX:+UseParNewGC -Xss500k -Xmx12G -Xms12G -Dlog4j.configurationFile=log4j2.xml
  *
  * @author Sanne Grinovero
  * @since 5.3
