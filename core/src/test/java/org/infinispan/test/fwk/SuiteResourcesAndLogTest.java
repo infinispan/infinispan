7e7b3e4075ef1205d7459459b46f36f988941440
Dan Berindei <dan@infinispan.org>
2015-09-12 16:46:07 +0300

    ISPN-5719 Use log4j 2.0 everywhere

--- core/src/test/java/org/infinispan/test/fwk/SuiteResourcesAndLogTest.java
+++ core/src/test/java/org/infinispan/test/fwk/SuiteResourcesAndLogTest.java
@@ -36,7 +36,7 @@ public class SuiteResourcesAndLogTest {
       String preferIpV4 = System.getProperty("java.net.preferIPv4Stack");
       log("java.net.preferIPv4Stack = " + preferIpV4);
       log("java.net.preferIPv6Stack = " + System.getProperty("java.net.preferIPv6Stack"));
-      log("log4.configuration = " + System.getProperty("log4j.configuration"));
+      log("log4.configurationFile = " + System.getProperty("log4j.configurationFile"));
       log("MAVEN_OPTS = " + System.getProperty("MAVEN_OPTS"));
       log("~~~~~~~~~~~~~~~~~~~~~~~~~ ENVIRONMENT INFO ~~~~~~~~~~~~~~~~~~~~~~~~~~");
       
