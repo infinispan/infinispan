73f61f3987cc2196d5f83cf89dfa3e3bbf100dd1
Dan Berindei <dan@infinispan.org>
2015-10-29 11:49:45 +0200

    ISPN-5897 Enhance filter_failed_tests.sh to support compressed logs
    Also update failedTestLogs.sh to require a log file argument and to
    use perl more.

--- bin/filter_failed_tests.sh
+++ bin/filter_failed_tests.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
 DIR=`dirname $0`
-echo dir=$DIR
+#echo dir=$DIR
 
-for LOG_FILE in `find . -name "*.log"` ; do
-    FAILED_TESTS=`cat $LOG_FILE | egrep "Test .* (failed|skipped)\." | sed -e 's/.*(//' -e 's/).*//' | awk -F. ' { print $NF } ' | sort | uniq`
+for LOG_FILE in `find . -name "*.log*"` ; do
+    CAT=cat
+    if [ "${FILE##*.}" == "gz" ] ; then
+      CAT=zcat
+    fi
+    FAILED_TESTS=`$CAT $LOG_FILE | perl -ne '/Test .*\(.*\.(.*)\) (failed|skipped)\./ && print "$1\n";' | sort -u`
 
-    if [ -z $FAILED_TESTS ]
+    if [ -z "$FAILED_TESTS" ]
     then
       echo "Processing log file: $LOG_FILE. No failed tests."
     else
-      echo "Processing log file: $LOG_FILE. Failed tests:  $FAILED_TESTS "
+      echo "Processing log file: $LOG_FILE. Failed tests: $FAILED_TESTS"
     fi
 
     for TEST in $FAILED_TESTS ; do
       echo "Processing $TEST from $LOG_FILE "
-      echo "" > $TEST.log
-      SHORTNAME=`perl -e '$t = $ARGV[0]; chomp $t; $t =~ s/[a-z0-9]//g; print $t;' $TEST`
-      cat $LOG_FILE | python $DIR/greplog.py "\b$TEST\b" | perl -npe "s/(?![a-zA-Z.])$TEST(?![a-zA-Z.])/$SHORTNAME/g" > $TEST.log
+      TESTFILE=$(echo ${TEST}.log | tr / _)
+      TESTFILE=${TESTFILE//[^a-zA-Z0-9-_.]/}
+      $CAT $LOG_FILE | python $DIR/greplog.py "\b$TEST\b" | perl -npe "s/$TEST-Node/Node/g" > $TESTFILE
     done
 
     rm $LOG_FILE
