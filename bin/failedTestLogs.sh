c3251a945251a0ebe59fd4d13e674f5c8a622f9d
Dan Berindei <dan@infinispan.org>
2015-10-29 11:49:45 +0200

    ISPN-5897 Enhance filter_failed_tests.sh to support compressed logs
    Also update failedTestLogs.sh to require a log file argument and to
    use perl more.

--- bin/failedTestLogs.sh
+++ bin/failedTestLogs.sh
@@ -1,28 +1,30 @@
-#!/bin/sh
+#!/bin/bash
+
+FILE=$1 ; shift
+FAILED_TESTS="$*"
 
 DIR=`dirname $0`
-echo dir=$DIR
-if [ -n $1 ] ; then FAILED_TESTS=$1 ; fi
+#echo dir=$DIR
 
-FILE=infinispan.log
-ZFILE=infinispan.log.gz
 CAT=cat
-if [ \( ! -f $FILE \) -o \( $FILE -ot $ZFILE \) ] ; then FILE=$ZFILE ; CAT=zcat ; fi
-
-if [ ! -f $FILE ] ; then
-  echo "Could not open infinispan.log or infinispan.log.gz"
+if [ "${FILE##*.}" == "gz" ] ; then
+  CAT=zcat
 fi
 
-echo $CAT $FILE
-export CAT FILE
-
-if [ -z $FAILED_TESTS ] ; then
-  FAILED_TESTS=`$CAT $FILE | egrep "(Test failed)|(skipped\$)" | perl -ne '/.*\(.*\.(.*)\)$/; print "$1\n";' | sort | uniq`
+if [ ! -f "$FILE" ] ; then
+  echo "Could not read file $FILE"
 fi
 
+echo Processing $FILE
+
+if [ -z "$FAILED_TESTS" ] ; then
+  FAILED_TESTS=`$CAT $FILE | perl -ne '/Test .*\(.*\.(.*)\) (failed|skipped)\./ && print "$1\n";' | sort -u`
+fi
+echo Failed/skipped tests: $FAILED_TESTS 
+
+DATE=$(date +%Y%m%d)
 for TEST in $FAILED_TESTS ; do
-  SHORTNAME=`perl -e '$t = $ARGV[0]; chomp $t; $t =~ s/[a-z0-9]//g; print $t;' $TEST`
-  LOWSHORTNAME=`perl -e '$t = $ARGV[0]; chomp $t; $t =~ s/[a-z0-9]//g; print lc $t;' $TEST`
-  echo "$TEST > $LOWSHORTNAME.log"
-  $CAT $FILE | $DIR/greplog.py "\b$TEST\b" | perl -npe "s/(?![a-zA-Z.])$TEST(?![a-zA-Z.])/$SHORTNAME/g" > $LOWSHORTNAME.log
+  TESTFILE=${TEST}_${DATE}.log
+  echo "Writing $TEST log to $TESTFILE"
+  $CAT $FILE | $DIR/greplog.py "\b$TEST\b" | perl -npe "s/$TEST-Node/Node/g" > $TESTFILE
 done
