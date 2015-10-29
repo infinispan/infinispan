178c0eb7fbf0100d9d25b4f7f243a233a57f5b01
William Burns <mudokonman@gmail.com>
2015-10-29 12:18:26 -0400

    ISPN-5899 ContinuousQuery tests should disable expiration reaper
    * Disable reaper to prevent spurious failures with more than 1 event

--- client/hotrod-client/src/test/java/org/infinispan/client/hotrod/event/RemoteContinuousQueryTest.java
+++ client/hotrod-client/src/test/java/org/infinispan/client/hotrod/event/RemoteContinuousQueryTest.java
@@ -70,6 +70,7 @@ public class RemoteContinuousQueryTest extends MultiHotRodServersTest {
       cfgBuilder.indexing().index(Index.ALL)
             .addProperty("default.directory_provider", "ram")
             .addProperty("lucene_version", "LUCENE_CURRENT");
+      cfgBuilder.expiration().disableReaper();
       return cfgBuilder;
    }
 
