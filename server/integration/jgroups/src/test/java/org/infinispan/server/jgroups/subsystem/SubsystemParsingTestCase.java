5bd82a407c08fea26ca1b467b8472b3fc38b0c2b
Tristan Tarrant <ttarrant@redhat.com>
2015-11-30 11:27:13 +0100

    ISPN-5998 Enable jgroups server subsystem schema validation

--- server/integration/jgroups/src/test/java/org/infinispan/server/jgroups/subsystem/SubsystemParsingTestCase.java
+++ server/integration/jgroups/src/test/java/org/infinispan/server/jgroups/subsystem/SubsystemParsingTestCase.java
@@ -65,22 +65,29 @@ import org.junit.runners.Parameterized.Parameters;
 public class SubsystemParsingTestCase extends ClusteringSubsystemTest {
 
     private final int expectedOperationCount;
+    private final String xsdPath;
 
-    public SubsystemParsingTestCase(JGroupsSchema schema, int expectedOperationCount) {
+    public SubsystemParsingTestCase(JGroupsSchema schema, int expectedOperationCount, String xsdPath) {
         super(JGroupsExtension.SUBSYSTEM_NAME, new JGroupsExtension(), schema.format("subsystem-%s-%d_%d.xml").replaceAll(":", "_"));
         this.expectedOperationCount = expectedOperationCount;
+        this.xsdPath = xsdPath;
     }
 
     @Parameters
     public static Collection<Object[]> data() {
         Object[][] data = new Object[][] {
-                { JGroupsSchema.VERSION_1_1, 20 },
-                { JGroupsSchema.VERSION_2_0, 22 },
-                { JGroupsSchema.VERSION_3_0, 29 },
-                { JGroupsSchema.INFINISPAN_SERVER_JGROUPS_7_0, 25 },
-                { JGroupsSchema.INFINISPAN_SERVER_JGROUPS_8_0, 30 },
+                { JGroupsSchema.VERSION_1_1, 20, "schema/jboss-as-jgroups_1_1.xsd" },
+                { JGroupsSchema.VERSION_2_0, 22, "schema/jboss-as-jgroups_2_0.xsd" },
+                { JGroupsSchema.VERSION_3_0, 29, "schema/jboss-as-jgroups_3_0.xsd"},
+                { JGroupsSchema.INFINISPAN_SERVER_JGROUPS_7_0, 25, "schema/jboss-infinispan-jgroups_7_0.xsd" },
+                { JGroupsSchema.INFINISPAN_SERVER_JGROUPS_8_0, 30, "schema/jboss-infinispan-jgroups_8_0.xsd" },
         };
         return Arrays.asList(data);
+    }
+
+    @Override
+    protected String getSubsystemXsdPath() throws Exception {
+        return xsdPath;
     }
 
     private KernelServices buildKernelServices() throws Exception {
