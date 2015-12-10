54ca88b3ba3b6ea6f3c629c6a76f04250587f34a
Adrian Nistor <anistor@redhat.com>
2015-12-10 21:01:44 +0200

    Make ContinuousQuery.EntryListener private

--- query/src/main/java/org/infinispan/query/continuous/ContinuousQuery.java
+++ query/src/main/java/org/infinispan/query/continuous/ContinuousQuery.java
@@ -54,11 +54,11 @@ public final class ContinuousQuery<K, V> {
    }
 
    @Listener(clustered = true, includeCurrentState = true, observation = Listener.Observation.POST)
-   public static class EntryListener<K, V, C> {
+   private static class EntryListener<K, V, C> {
 
       private final ContinuousQueryResultListener<K, C> listener;
 
-      public EntryListener(ContinuousQueryResultListener<K, C> listener) {
+      EntryListener(ContinuousQueryResultListener<K, C> listener) {
          this.listener = listener;
       }
 
