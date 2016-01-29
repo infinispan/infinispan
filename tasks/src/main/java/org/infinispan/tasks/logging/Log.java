e09a293ee9ec26b35733a266d7880a97f0bfc1ad
Tristan Tarrant <ttarrant@redhat.com>
2016-01-29 10:27:07 +0100

    ISPN-6123 Fix logging ranges

--- tasks/src/main/java/org/infinispan/tasks/logging/Log.java
+++ tasks/src/main/java/org/infinispan/tasks/logging/Log.java
@@ -4,17 +4,17 @@ import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 
 /**
- * Log abstraction for the Tasks module. For this module, message ids ranging from 22001 to
- * 23000 inclusively have been reserved.
+ * Log abstraction for the Tasks module. For this module, message ids ranging from 27001 to
+ * 28000 inclusively have been reserved.
  *
  * @author Tristan Tarrant
  * @since 8.1
  */
 @MessageLogger(projectCode = "ISPN")
 public interface Log extends org.infinispan.util.logging.Log {
-   @Message(value = "Task Engine '%s' has already been registered", id = 22001)
+   @Message(value = "Task Engine '%s' has already been registered", id = 27001)
    IllegalStateException duplicateTaskEngineRegistration(String taskEngineName);
 
-   @Message(value = "Unknown task '%s'", id = 22002)
+   @Message(value = "Unknown task '%s'", id = 27002)
    IllegalArgumentException unknownTask(String taskName);
 }
