95bb0f9a49a84ec2420b696d0a71c1dbd08efda3
Adrian Nistor <anistor@redhat.com>
2016-02-01 13:14:03 +0200

    ISPN-5995 ParseUtils.unexpectedElement/unxpectedAttribute called but the exception is not thrown

--- server/integration/endpoint/src/main/java/org/infinispan/server/endpoint/subsystem/EndpointSubsystemReader_8_0.java
+++ server/integration/endpoint/src/main/java/org/infinispan/server/endpoint/subsystem/EndpointSubsystemReader_8_0.java
@@ -346,7 +346,7 @@ class EndpointSubsystemReader_8_0 implements XMLStreamConstants, XMLElementReade
             break;
          }
          default: {
-            ParseUtils.unexpectedAttribute(reader, i);
+            throw ParseUtils.unexpectedAttribute(reader, i);
          }
          }
       }
@@ -371,7 +371,7 @@ class EndpointSubsystemReader_8_0 implements XMLStreamConstants, XMLElementReade
             break;
          }
          default: {
-            ParseUtils.unexpectedAttribute(reader, i);
+            throw ParseUtils.unexpectedAttribute(reader, i);
          }
          }
       }
@@ -434,7 +434,7 @@ class EndpointSubsystemReader_8_0 implements XMLStreamConstants, XMLElementReade
             break;
          }
          default: {
-            ParseUtils.unexpectedAttribute(reader, i);
+            throw ParseUtils.unexpectedAttribute(reader, i);
          }
          }
       }
@@ -553,7 +553,7 @@ class EndpointSubsystemReader_8_0 implements XMLStreamConstants, XMLElementReade
             break;
          }
          default: {
-            ParseUtils.unexpectedAttribute(reader, i);
+            throw ParseUtils.unexpectedAttribute(reader, i);
          }
          }
 
