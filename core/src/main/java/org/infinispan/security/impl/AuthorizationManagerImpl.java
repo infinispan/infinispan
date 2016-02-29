7cb233862ef64d3417ef2c6bd10e35224e8ac4e2
Dan Berindei <dan@infinispan.org>
2016-02-29 08:55:26 +0200

    ISPN-6277 NullPointerException in InitialClusterSizeTest
    * Stop handling private caches specially in DefaultCacheManager.stop()
    * Add dependency from any secure cache to the ACL cache when in use.

--- core/src/main/java/org/infinispan/security/impl/AuthorizationManagerImpl.java
+++ core/src/main/java/org/infinispan/security/impl/AuthorizationManagerImpl.java
@@ -1,12 +1,11 @@
 package org.infinispan.security.impl;
 
-import java.util.concurrent.ConcurrentMap;
-
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.AuthorizationConfiguration;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.factories.annotations.Inject;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.security.AuditContext;
 import org.infinispan.security.AuthorizationManager;
 import org.infinispan.security.AuthorizationPermission;
@@ -26,10 +25,16 @@ public class AuthorizationManagerImpl implements AuthorizationManager {
    }
 
    @Inject
-   public void init(Cache<?, ?> cache, GlobalConfiguration globalConfiguration, Configuration configuration, GlobalSecurityManager globalSecurityManager) {
+   public void init(Cache<?, ?> cache, GlobalConfiguration globalConfiguration, Configuration configuration,
+         GlobalSecurityManager globalSecurityManager, EmbeddedCacheManager cacheManager) {
       this.configuration = configuration.security().authorization();
+      Cache<CachePrincipalPair, SubjectACL> globalACLCache =
+            (Cache<CachePrincipalPair, SubjectACL>) globalSecurityManager.globalACLCache();
       this.authzHelper = new AuthorizationHelper(globalConfiguration.security(), AuditContext.CACHE, cache.getName(),
-            (ConcurrentMap<CachePrincipalPair, SubjectACL>) globalSecurityManager.globalACLCache());
+            globalACLCache);
+      if (globalACLCache != null) {
+         cacheManager.addCacheDependency(cache.getName(), globalACLCache.getName());
+      }
    }
 
    @Override
