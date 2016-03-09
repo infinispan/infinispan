1614f6b8aab14b1300a6d96cf7dc723b0a43909b
andyuk1986 <anna.mnkyn@gmail.com>
2016-03-09 12:46:54 +0100

    ISPN-6345 - Fix DistributedServerTaskIT#shouldPutNewValueInRemoteCache failing on CI

--- server/integration/testsuite/src/test/java/org/infinispan/server/test/task/servertask/DistributedCacheUsingTask.java
+++ server/integration/testsuite/src/test/java/org/infinispan/server/test/task/servertask/DistributedCacheUsingTask.java
@@ -5,35 +5,43 @@ import org.infinispan.tasks.ServerTask;
 import org.infinispan.tasks.TaskContext;
 import org.infinispan.tasks.TaskExecutionMode;
 
-import java.io.IOException;
+import javax.transaction.TransactionManager;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
- * // TODO: Document this
+ * Task verifying that the marshaller is passed properly and the value is properly put into the cache.
  *
- * @author amanukya
- * @since 4.0
+ * @author Anna Manukyan
  */
 public class DistributedCacheUsingTask implements ServerTask {
 
     public static final String NAME = "serverTask_distributed_cacheUsage";
-    public static final String CACHE_NAME = "customTaskRepl";
+    public static final String CACHE_NAME = "customTaskReplTx";
     public static final String VALUE_PREFIX = "modified:";
     public static final String PARAM_KEY = "param";
+    public static final String NEW_VALUE = "newValue";
 
     private TaskContext taskContext;
+    private static AtomicInteger counter = new AtomicInteger(0);
 
     @Override
     @SuppressWarnings("unchecked")
-    public Object call() throws IOException, ClassNotFoundException {
+    public Object call() throws Exception {
         Cache<Object, Object> cache = (Cache<Object, Object>) taskContext.getCache().get();
         Map<String, String> parameters = (Map<String, String>) taskContext.getParameters().get();
 
         assert taskContext.getMarshaller().isPresent();
         Map.Entry<Object, Object> entry = cache.getCacheManager().getCache(CACHE_NAME).entrySet().iterator().next();
 
-        cache.getCacheManager().getCache(CACHE_NAME).put(entry.getKey(), VALUE_PREFIX + entry.getValue() + ":" + parameters.get(PARAM_KEY));
+        TransactionManager transactionManager = cache.getAdvancedCache().getTransactionManager();
+        transactionManager.begin();
+        cache.getCacheManager().getCache(CACHE_NAME).getAdvancedCache().lock(entry.getKey());
+        
+        cache.getCacheManager().getCache(CACHE_NAME).put(entry.getKey(),
+                VALUE_PREFIX + entry.getValue() + ":" + parameters.get(PARAM_KEY));
+        transactionManager.commit();
         return null;
     }
 
