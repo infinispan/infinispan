15de340592d801ccb44643533e534267003b1554
Dan Berindei <dan@infinispan.org>
2016-03-11 14:24:17 +0200

    ISPN-6357 Deadlock during server start
    * Fix cluster recovery when a ForkChannel is missing
    * Log more info when the cluster recovery fails
    * Test startup with ForkChannels

---
+++ core/src/test/java/org/infinispan/statetransfer/CustomChannelLookup.java
@@ -0,0 +1,55 @@
+package org.infinispan.statetransfer;
+
+import org.infinispan.commons.util.CollectionFactory;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.infinispan.configuration.global.TransportConfigurationBuilder;
+import org.infinispan.remoting.transport.jgroups.JGroupsChannelLookup;
+import org.infinispan.remoting.transport.jgroups.JGroupsTransport;
+import org.jgroups.Channel;
+import org.jgroups.JChannel;
+
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * JGroupsChannelLookup implementation that returns an existing channel.
+ *
+ * @author Dan Berindei
+ * @since 8.2
+ */
+public class CustomChannelLookup implements JGroupsChannelLookup {
+   private static final Map<String, JChannel> channelMap = CollectionFactory.makeConcurrentMap();
+   private boolean connect;
+
+   public static void registerChannel(String nodeName, JChannel channel,
+         GlobalConfigurationBuilder gcb, boolean connect) {
+      TransportConfigurationBuilder tcb = gcb.transport();
+      tcb.defaultTransport();
+      tcb.addProperty(JGroupsTransport.CHANNEL_LOOKUP, CustomChannelLookup.class.getName());
+      tcb.addProperty("customNodeName", nodeName);
+      tcb.addProperty("customConnect", Boolean.toString(connect));
+      channelMap.put(nodeName, channel);
+   }
+
+   @Override
+   public Channel getJGroupsChannel(Properties p) {
+      String nodeName = p.getProperty("customNodeName");
+      connect = Boolean.valueOf(p.getProperty("customConnect"));
+      return channelMap.remove(nodeName);
+   }
+
+   @Override
+   public boolean shouldConnect() {
+      return connect;
+   }
+
+   @Override
+   public boolean shouldDisconnect() {
+      return true;
+   }
+
+   @Override
+   public boolean shouldClose() {
+      return true;
+   }
+}
