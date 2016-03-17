0c64e458e4a4b8b818f9f6a101defb4431583e9e
Galder Zamarreño <galder@zamarreno.com>
2016-03-17 13:41:34 +0100

    Remove unused pushNDC/popNDC methods

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/logging/LogFactory.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/logging/LogFactory.java
@@ -1,7 +1,6 @@
 package org.infinispan.client.hotrod.logging;
 
 import org.jboss.logging.Logger;
-import org.jboss.logging.NDC;
 
 /**
  * Factory that creates {@link Log} instances.
@@ -17,22 +16,6 @@ public class LogFactory {
 
    public static <T> T getLog(Class<?> clazz, Class<T> logClass) {
       return Logger.getMessageLogger(logClass, clazz.getName());
-   }
-
-   public static void pushNDC(String cacheName, boolean isTrace) {
-      // Disabled NDC usage until https://issues.jboss.org/browse/JBLOGGING-106 is fixed or
-      // we introduce Log4j2 https://issues.jboss.org/browse/ISPN-3076
-      // See https://issues.jboss.org/browse/ISPN-4498 for more details
-//      if (isTrace)
-//         NDC.push(cacheName);
-   }
-
-   public static void popNDC(boolean isTrace) {
-      // Disabled NDC usage until https://issues.jboss.org/browse/JBLOGGING-106 is fixed or
-      // we introduce Log4j2 https://issues.jboss.org/browse/ISPN-3076
-      // See https://issues.jboss.org/browse/ISPN-4498 for more details
-//      if (isTrace)
-//         NDC.pop();
    }
 
 }
