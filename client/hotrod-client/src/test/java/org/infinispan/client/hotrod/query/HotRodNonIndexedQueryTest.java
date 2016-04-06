0036fc376fbe3feb316bc457e04c3e03d023a797
Adrian Nistor <anistor@redhat.com>
2016-04-06 23:57:02 +0300

    Re-enable working tests

--- client/hotrod-client/src/test/java/org/infinispan/client/hotrod/query/HotRodNonIndexedQueryTest.java
+++ client/hotrod-client/src/test/java/org/infinispan/client/hotrod/query/HotRodNonIndexedQueryTest.java
@@ -1,6 +1,5 @@
 package org.infinispan.client.hotrod.query;
 
-import org.infinispan.client.hotrod.exceptions.HotRodClientException;
 import org.infinispan.commons.equivalence.ByteArrayEquivalence;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.testng.annotations.Test;
@@ -17,11 +16,5 @@ public class HotRodNonIndexedQueryTest extends HotRodQueryTest {
       ConfigurationBuilder builder = new ConfigurationBuilder();
       builder.dataContainer().keyEquivalence(ByteArrayEquivalence.INSTANCE);
       return builder;
-   }
-
-   @Test(expectedExceptions = HotRodClientException.class, expectedExceptionsMessageRegExp = ".*ISPN000405:.*")
-   @Override
-   public void testInvalidEmbeddedAttributeQuery() throws Exception {
-      super.testInvalidEmbeddedAttributeQuery();
    }
 }
