bbec04b062384ea5d070de0551e86710bd2d6c2b
Pedro Ruivo <pruivo@redhat.com>
2016-04-07 11:12:31 +0100

    ISPN-6468 SyncReplExtendedStatisticTest.​testReplace is unstable
    * mark testReplace as unstable

--- extended-statistics/src/test/java/org/infinispan/stats/simple/SyncReplExtendedStatisticTest.java
+++ extended-statistics/src/test/java/org/infinispan/stats/simple/SyncReplExtendedStatisticTest.java
@@ -4,14 +4,22 @@ import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.stats.BaseNonTotalOrderClusteredExtendedStatisticsTest;
 import org.testng.annotations.Test;
 
+import java.lang.reflect.Method;
+
 /**
  * @author Pedro Ruivo
  * @since 6.0
  */
-@Test(groups = "functional", testName = "stats.simple.SyncReplExtendedStatisticTest", enabled = false, description = "To be fixed by ISPN-6468")
+@Test(groups = "functional", testName = "stats.simple.SyncReplExtendedStatisticTest")
 public class SyncReplExtendedStatisticTest extends BaseNonTotalOrderClusteredExtendedStatisticsTest {
 
    public SyncReplExtendedStatisticTest() {
       super(CacheMode.REPL_SYNC, false, false);
+   }
+
+   @Test(groups = "unstable", description = "To be fixed by ISPN-6468")
+   @Override
+   public void testReplace(Method method) throws InterruptedException {
+      super.testReplace(method);
    }
 }
