16e619c9fac1d7ae354f45410ecfa9509f3a53e6
gustavonalle <gustavonalle@gmail.com>
2016-04-21 08:46:31 +0100

    ISPN-6522 Modified test to reproduce the issue

--- integrationtests/as-integration-embedded/src/test/java/org/infinispan/test/integration/as/query/Book.java
+++ integrationtests/as-integration-embedded/src/test/java/org/infinispan/test/integration/as/query/Book.java
@@ -1,10 +1,14 @@
 package org.infinispan.test.integration.as.query;
 
-import java.io.Serializable;
-
 import org.hibernate.search.annotations.Analyze;
+import org.hibernate.search.annotations.DateBridge;
+import org.hibernate.search.annotations.EncodingType;
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
+import org.hibernate.search.annotations.Resolution;
+
+import java.io.Serializable;
+import java.util.Date;
 
 @Indexed
 public final class Book implements Serializable {
@@ -14,10 +18,20 @@ public final class Book implements Serializable {
       this.publisher = publisher;
    }
 
+   public Book(String title, String publisher, Date pubDate) {
+      this.title = title;
+      this.publisher = publisher;
+      this.pubDate = pubDate;
+   }
+
    @Field(analyze = Analyze.NO)
    final String publisher;
 
    @Field
    final String title;
+
+   @Field(analyze = Analyze.NO)
+   @DateBridge(encoding= EncodingType.STRING, resolution= Resolution.DAY)
+   private Date pubDate;
 
 }
