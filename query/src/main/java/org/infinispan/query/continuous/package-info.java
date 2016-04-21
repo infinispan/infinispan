a9fc6c31cf21f77ed675795b55796a07e7b4aa73
Adrian Nistor <anistor@redhat.com>
2016-04-21 16:00:40 +0300

    ISPN-6159 Remove deprecated continuous query API

--- query/src/main/java/org/infinispan/query/continuous/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Functionality related to continuous query, which sends updates to the client based on the client
- * query when there are any changes to the data which match the original query.
- *
- * @public
- */
-
-package org.infinispan.query.continuous;
