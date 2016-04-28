45d43ca955eda8512f3c499b8fc4ab9d5bfc56a8
Tristan Tarrant <ttarrant@redhat.com>
2016-04-28 11:52:39 +0200

    ISPN-6562 Support HTTP/HTTPS JMX remoting connections from the CLI

---
+++ cli/cli-client/src/main/java/org/infinispan/cli/connection/jmx/remoting/DirectJMXRemotingConnector.java
@@ -0,0 +1,23 @@
+package org.infinispan.cli.connection.jmx.remoting;
+
+import org.infinispan.cli.connection.Connection;
+import org.infinispan.cli.connection.Connector;
+import org.infinispan.cli.connection.jmx.JMXConnection;
+import org.kohsuke.MetaInfServices;
+
+@MetaInfServices
+public class DirectJMXRemotingConnector implements Connector {
+
+   public DirectJMXRemotingConnector() {
+   }
+
+   @Override
+   public Connection getConnection(final String connectionString) {
+      try {
+         return new JMXConnection(new DirectJMXRemotingUrl(connectionString));
+      } catch (Exception e) {
+         return null;
+      }
+   }
+
+}
