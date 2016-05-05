6ff9c27dfa5a1815166b82bedcc556065b5601cb
William Burns <mudokonman@gmail.com>
2016-05-05 16:30:28 +0100

    ISPN-6580 Hotrod performance regressions after ISPN-5342 ISPN-6545
    * Changed executor group to using execution handler
    ** Share threads between channel
    ** Prevents downstream from requiring thread switch

--- server/hotrod/src/main/scala/org/infinispan/server/hotrod/transport/HotRodChannelInitializer.scala
+++ server/hotrod/src/main/scala/org/infinispan/server/hotrod/transport/HotRodChannelInitializer.scala
@@ -1,7 +1,10 @@
 package org.infinispan.server.hotrod.transport
 
+import java.util.concurrent.Executors
+
 import io.netty.channel.{Channel, ChannelOutboundHandler}
-import io.netty.util.concurrent.{DefaultEventExecutorGroup, DefaultThreadFactory}
+import io.netty.util.concurrent.DefaultThreadFactory
+import org.infinispan.server.core.ExecutionHandler
 import org.infinispan.server.core.transport.{NettyChannelInitializer, NettyTransport}
 import org.infinispan.server.hotrod.logging.{LoggingContextHandler, HotRodAccessLoggingHandler}
 import org.infinispan.server.hotrod._
@@ -16,18 +19,21 @@ class HotRodChannelInitializer(val server: HotRodServer, transport: => NettyTran
                                val encoder: ChannelOutboundHandler, threadNamePrefix: String)
       extends NettyChannelInitializer(server, transport, encoder) {
 
+   val executionHandler = new ExecutionHandler(Executors.newFixedThreadPool(server.getConfiguration.workerThreads(),
+      new DefaultThreadFactory(threadNamePrefix + "ServerHandler")))
+
    override def initChannel(ch: Channel): Unit = {
       super.initChannel(ch)
-      // Any inbound handler after this point should really be using this executionGroup
-      val executionGroup = new DefaultEventExecutorGroup(transport.configuration.workerThreads,
-         new DefaultThreadFactory(threadNamePrefix + "ServerHandler"))
       if (server.getConfiguration.authentication().enabled()) {
          ch.pipeline().addLast("authentication", new AuthenticationHandler(server))
       }
       ch.pipeline.addLast("local-handler", new LocalContextHandler(transport))
 
-      ch.pipeline.addLast(executionGroup, "handler", new ContextHandler(server, transport))
-      ch.pipeline.addLast(executionGroup, "exception", new HotRodExceptionHandler)
+      // All inbound handlers after this are performed in the executor
+      ch.pipeline.addLast("execution-handler", executionHandler)
+
+      ch.pipeline.addLast("handler", new ContextHandler(server, transport))
+      ch.pipeline.addLast("exception", new HotRodExceptionHandler)
 
       // Logging handlers
       ch.pipeline.addBefore("decoder", "logging", new HotRodAccessLoggingHandler)
