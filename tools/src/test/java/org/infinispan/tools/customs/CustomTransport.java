a94cc53c14f5b2cbe511c6e57c0c68ad7ecf0f3d
andyuk1986 <anna.mnkyn@gmail.com>
2016-05-31 10:57:28 +0200

    ISPN-6731 - Test configuration conversion tool for library mode

---
+++ tools/src/test/java/org/infinispan/tools/customs/CustomTransport.java
@@ -0,0 +1,12 @@
+package org.infinispan.tools.customs;
+
+import org.infinispan.remoting.transport.jgroups.JGroupsTransport;
+
+/**
+ * Custom Transport class for testing the transport tag parsing.
+ *
+ * @author amanukya
+ */
+public class CustomTransport extends JGroupsTransport {
+
+}
