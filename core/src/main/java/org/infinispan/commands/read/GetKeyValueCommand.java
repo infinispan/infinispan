b383436cc9244535f7b5456ab87000c8a2f89af9
Radim Vansa <rvansa@redhat.com>
2016-08-09 16:09:53 +0200

    ISPN-7029 Remove RemoteFetchingCommand

--- core/src/main/java/org/infinispan/commands/read/GetKeyValueCommand.java
+++ core/src/main/java/org/infinispan/commands/read/GetKeyValueCommand.java
@@ -8,7 +8,6 @@ import java.io.ObjectOutput;
 
 import org.infinispan.commands.Visitor;
 import org.infinispan.container.entries.CacheEntry;
-import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.context.Flag;
 import org.infinispan.context.InvocationContext;
 import org.infinispan.util.logging.Log;
@@ -21,13 +20,11 @@ import org.infinispan.util.logging.LogFactory;
  * @author Manik Surtani (<a href="mailto:manik@jboss.org">manik@jboss.org</a>)
  * @since 4.0
  */
-public class GetKeyValueCommand extends AbstractDataCommand implements RemoteFetchingCommand {
+public class GetKeyValueCommand extends AbstractDataCommand {
 
    public static final byte COMMAND_ID = 4;
    private static final Log log = LogFactory.getLog(GetKeyValueCommand.class);
    private static final boolean trace = log.isTraceEnabled();
-
-   private InternalCacheEntry remotelyFetchedValue;
 
    public GetKeyValueCommand(Object key, long flagsBitSet) {
       super(key, flagsBitSet);
@@ -80,22 +77,6 @@ public class GetKeyValueCommand extends AbstractDataCommand implements RemoteFet
    public void readFrom(ObjectInput input) throws IOException, ClassNotFoundException {
       key = input.readObject();
       setFlagsBitSet(input.readLong());
-   }
-
-   /**
-    * @see #getRemotelyFetchedValue()
-    */
-   public void setRemotelyFetchedValue(InternalCacheEntry remotelyFetchedValue) {
-      this.remotelyFetchedValue = remotelyFetchedValue;
-   }
-
-   /**
-    * If the cache needs to go remotely in order to obtain the value associated to this key, then the remote value
-    * is stored in this field.
-    * TODO: this method should be able to removed with the refactoring from ISPN-2177
-    */
-   public InternalCacheEntry getRemotelyFetchedValue() {
-      return remotelyFetchedValue;
    }
 
    public String toString() {
