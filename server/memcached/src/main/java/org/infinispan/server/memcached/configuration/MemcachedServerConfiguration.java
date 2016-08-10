9628ef100625d2eeeda8fed8ac650fe894496819
Tristan Tarrant <ttarrant@redhat.com>
2016-08-10 11:07:33 +0200

    ISPN-6474 Restore setting the default cache name on protocol server start

--- server/memcached/src/main/java/org/infinispan/server/memcached/configuration/MemcachedServerConfiguration.java
+++ server/memcached/src/main/java/org/infinispan/server/memcached/configuration/MemcachedServerConfiguration.java
@@ -15,6 +15,9 @@ import java.util.Set;
 @BuiltBy(MemcachedServerConfigurationBuilder.class)
 public class MemcachedServerConfiguration extends ProtocolServerConfiguration {
 
+   public static final int DEFAULT_MEMCACHED_PORT = 11211;
+   public static final String DEFAULT_MEMCACHED_CACHE = "memcachedCache";
+
    MemcachedServerConfiguration(String defaultCacheName, String name, String host, int port, int idleTimeout, int recvBufSize, int sendBufSize, SslConfiguration ssl, boolean tcpNoDelay, int workerThreads, Set<String> ignoredCaches) {
       super(defaultCacheName, name, host, port, idleTimeout, recvBufSize, sendBufSize, ssl, tcpNoDelay, workerThreads, ignoredCaches);
    }
