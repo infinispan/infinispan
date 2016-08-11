1d44e3aa297abfb7cbd1ced7862f2dd5fa830564
Tristan Tarrant <ttarrant@redhat.com>
2016-08-11 21:03:31 +0200

    ISPN-4390 Realigned with ConcurrentHashMap from the JSR-166 repository

--- commons/src/main/java/org/infinispan/commons/util/concurrent/jdk8backported/StrippedConcurrentLinkedDeque.java
+++ commons/src/main/java/org/infinispan/commons/util/concurrent/jdk8backported/StrippedConcurrentLinkedDeque.java
@@ -126,7 +126,7 @@ class StrippedConcurrentLinkedDeque<E> {
 
        static {
            try {
-               UNSAFE = getUnsafe();
+               UNSAFE = Unsafe.getUnsafe();
                Class<?> k = DequeNode.class;
                prevOffset = UNSAFE.objectFieldOffset
                    (k.getDeclaredField("prev"));
@@ -689,7 +689,7 @@ class StrippedConcurrentLinkedDeque<E> {
       NEXT_TERMINATOR = new DequeNode<Object>();
       NEXT_TERMINATOR.prev = NEXT_TERMINATOR;
       try {
-          UNSAFE = getUnsafe();
+          UNSAFE = Unsafe.getUnsafe();
           Class<?> k = StrippedConcurrentLinkedDeque.class;
           headOffset = UNSAFE.objectFieldOffset
               (k.getDeclaredField("head"));
@@ -698,28 +698,5 @@ class StrippedConcurrentLinkedDeque<E> {
       } catch (Exception e) {
           throw new Error(e);
       }
-  }
-
-  static sun.misc.Unsafe getUnsafe() {
-     try {
-        return sun.misc.Unsafe.getUnsafe();
-     } catch (SecurityException tryReflectionInstead) {}
-     try {
-        return java.security.AccessController.doPrivileged
-              (new java.security.PrivilegedExceptionAction<sun.misc.Unsafe>() {
-                 public sun.misc.Unsafe run() throws Exception {
-                    Class<sun.misc.Unsafe> k = sun.misc.Unsafe.class;
-                    for (java.lang.reflect.Field f : k.getDeclaredFields()) {
-                       f.setAccessible(true);
-                       Object x = f.get(null);
-                       if (k.isInstance(x))
-                          return k.cast(x);
-                    }
-                    throw new NoSuchFieldError("the Unsafe");
-                 }});
-     } catch (java.security.PrivilegedActionException e) {
-        throw new RuntimeException("Could not initialize intrinsics",
-              e.getCause());
-     }
   }
 }
