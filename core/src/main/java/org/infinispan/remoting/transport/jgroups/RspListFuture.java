5dd205b542dacbb0cd3058fe252a5ef7d988d1de
Dan Berindei <dan@infinispan.org>
2016-09-29 12:22:35 +0300

    ISPN-7064 RPC to leaver times out instead of finishing immediately
    * Set the GroupRequest listener before the request is sent.
    * Send the state transfer cancel commands asynchronously.

--- core/src/main/java/org/infinispan/remoting/transport/jgroups/RspListFuture.java
+++ core/src/main/java/org/infinispan/remoting/transport/jgroups/RspListFuture.java
@@ -3,6 +3,7 @@ package org.infinispan.remoting.transport.jgroups;
 import org.infinispan.remoting.responses.Response;
 import org.jgroups.blocks.GroupRequest;
 import org.jgroups.util.FutureListener;
+import org.jgroups.util.RspList;
 
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
@@ -12,23 +13,36 @@ import java.util.concurrent.Future;
  * @author Dan Berindei
  * @since 8.0
  */
-public class RspListFuture extends CompletableFuture<Responses> implements FutureListener<Responses>,
+public class RspListFuture extends CompletableFuture<Responses> implements FutureListener<RspList<Response>>,
       Callable<Void> {
-   private final GroupRequest<Response> request;
+   private volatile GroupRequest<Response> request;
    private volatile Future<?> timeoutFuture = null;
 
-   RspListFuture(GroupRequest<Response> request) {
+   RspListFuture() {
+   }
+
+   /**
+    * Add a reference to the request.
+    *
+    * Must be called before scheduling the timeout task.
+    */
+   public void setRequest(GroupRequest<Response> request) {
       this.request = request;
-      if (request != null) {
-         request.setListener(this);
-      }
    }
 
    @Override
-   public void futureDone(Future<Responses> future) {
-      complete(new Responses(request.getResults()));
-      if (timeoutFuture != null) {
-         timeoutFuture.cancel(false);
+   public void futureDone(Future<RspList<Response>> future) {
+      // The request field may not be set at this time
+      // The future may be a
+      RspList<Response> rspList;
+      try {
+         rspList = future.get();
+         complete(new Responses(rspList));
+         if (timeoutFuture != null) {
+            timeoutFuture.cancel(false);
+         }
+      } catch (Throwable t) {
+         completeExceptionally(t);
       }
    }
 
