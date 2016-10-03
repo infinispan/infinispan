2e739c4f43caac6eb19e1fa36f365bab82583e7e
gustavonalle <gustavonalle@gmail.com>
2016-10-03 11:06:13 +0100

    Expose Lucene Directory javadoc

--- lucene/lucene-directory/src/main/java/org/infinispan/lucene/readlocks/package-info.java
+++ lucene/lucene-directory/src/main/java/org/infinispan/lucene/readlocks/package-info.java
@@ -3,5 +3,7 @@
  * Lucene's default IndexDeletionPolicy could remove a segment while it's still used by another IndexReader;
  * this is not an issue on a local filesystem, but could happen on Infinispan.
  * To prevent deletion of in-use segments a read-lock is acquired when a segment is opened.
+ *
+ * @public
  */
 package org.infinispan.lucene.readlocks;
