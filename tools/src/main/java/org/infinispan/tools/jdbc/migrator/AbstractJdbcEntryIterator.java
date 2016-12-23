f15dc3be903aa6387e743e3c33031a2c759ba843
Ryan Emerson <remerson@redhat.com>
2016-12-23 15:34:13 +0000

    ISPN-7124: Create JDBC Binary/Mixed Store Migrator.

---
+++ tools/src/main/java/org/infinispan/tools/jdbc/migrator/AbstractJdbcEntryIterator.java
@@ -0,0 +1,64 @@
+package org.infinispan.tools.jdbc.migrator;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Iterator;
+
+import org.infinispan.commons.marshall.StreamingMarshaller;
+import org.infinispan.marshall.core.MarshalledEntry;
+import org.infinispan.persistence.jdbc.JdbcUtil;
+import org.infinispan.persistence.jdbc.connectionfactory.ConnectionFactory;
+import org.infinispan.persistence.jdbc.table.management.TableManager;
+import org.infinispan.persistence.spi.PersistenceException;
+
+/**
+ * @author Ryan Emerson
+ * @since 9.0
+ */
+abstract class AbstractJdbcEntryIterator implements Iterator<MarshalledEntry>, AutoCloseable {
+   final ConnectionFactory connectionFactory;
+   final TableManager tableManager;
+   final StreamingMarshaller marshaller;
+   private Connection conn;
+   private PreparedStatement ps;
+   ResultSet rs;
+   int numberOfRows = 0;
+   int rowIndex = 0;
+
+   AbstractJdbcEntryIterator(ConnectionFactory connectionFactory, TableManager tableManager,
+                             StreamingMarshaller marshaller) {
+      this.connectionFactory = connectionFactory;
+      this.tableManager = tableManager;
+      this.marshaller = marshaller;
+
+      Statement st = null;
+      ResultSet countRs = null;
+      try {
+         conn = connectionFactory.getConnection();
+
+         st = conn.createStatement();
+         countRs = st.executeQuery(tableManager.getCountRowsSql());
+         countRs.next();
+         numberOfRows = countRs.getInt(1);
+
+         ps = conn.prepareStatement(tableManager.getLoadAllRowsSql(), ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY);
+         ps.setFetchSize(tableManager.getFetchSize());
+         rs = ps.executeQuery();
+      } catch (SQLException e) {
+         throw new PersistenceException("SQL error while fetching all StoredEntries", e);
+      } finally {
+         JdbcUtil.safeClose(st);
+         JdbcUtil.safeClose(countRs);
+      }
+   }
+
+   @Override
+   public void close() {
+      JdbcUtil.safeClose(rs);
+      JdbcUtil.safeClose(ps);
+      connectionFactory.releaseConnection(conn);
+   }
+}
---
+++ tools/src/main/java/org/infinispan/tools/jdbc/migrator/BinaryJdbcIterator.java
@@ -0,0 +1,67 @@
+package org.infinispan.tools.jdbc.migrator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+import org.infinispan.commons.marshall.StreamingMarshaller;
+import org.infinispan.marshall.core.MarshalledEntry;
+import org.infinispan.persistence.jdbc.connectionfactory.ConnectionFactory;
+import org.infinispan.persistence.jdbc.table.management.TableManager;
+import org.infinispan.persistence.spi.PersistenceException;
+
+/**
+ * @author Ryan Emerson
+ * @since 9.0
+ */
+class BinaryJdbcIterator extends AbstractJdbcEntryIterator {
+   private Iterator<MarshalledEntry> iterator = Collections.emptyIterator();
+
+   BinaryJdbcIterator(ConnectionFactory connectionFactory, TableManager tableManager, StreamingMarshaller marshaller) {
+      super(connectionFactory, tableManager, marshaller);
+   }
+
+   @Override
+   public boolean hasNext() {
+      return iterator.hasNext() || rowIndex < numberOfRows;
+   }
+
+   @Override
+   public MarshalledEntry next() {
+      if (!iterator.hasNext()) {
+         iterator = getNextBucketIterator();
+      }
+      rowIndex++;
+      return iterator.next();
+   }
+
+   private Iterator<MarshalledEntry> getNextBucketIterator() {
+      try {
+         if (rs.next()) {
+            InputStream inputStream = rs.getBinaryStream(1);
+            Map<Object, MarshalledEntry> bucketEntries = unmarshallBucketEntries(inputStream);
+            numberOfRows += bucketEntries.size() - 1; // Guaranteed that bucket size will never be 0
+            return bucketEntries.values().iterator();
+         } else {
+            close();
+            throw new NoSuchElementException();
+         }
+      } catch (SQLException e) {
+         throw new PersistenceException("SQL error while fetching all StoredEntries", e);
+      }
+   }
+
+   private Map<Object, MarshalledEntry> unmarshallBucketEntries(InputStream inputStream) {
+      try {
+         return (Map<Object, MarshalledEntry>) marshaller.objectFromInputStream(inputStream);
+      } catch (IOException e) {
+         throw new PersistenceException("I/O error while unmarshalling from stream", e);
+      } catch (ClassNotFoundException e) {
+         throw new PersistenceException(e);
+      }
+   }
+}
