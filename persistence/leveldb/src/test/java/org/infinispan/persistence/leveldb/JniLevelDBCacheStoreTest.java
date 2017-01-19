4c3038ec46e772663e74e32eb3cdcd7f73390493
Sebastian Laskawiec <slaskawi@redhat.com>
2017-01-19 11:25:25 +0100

    ISPN-4189 Disable LevelDB tests on Windows and Solaris
    The Fuse Team (who created the LevelDB library we depend on) explained that they support only UNIX systems.

--- persistence/leveldb/src/test/java/org/infinispan/persistence/leveldb/JniLevelDBCacheStoreTest.java
+++ persistence/leveldb/src/test/java/org/infinispan/persistence/leveldb/JniLevelDBCacheStoreTest.java
@@ -1,11 +1,13 @@
 package org.infinispan.persistence.leveldb;
 
+import org.infinispan.commons.test.skip.SkipOnOs;
 import org.infinispan.configuration.cache.PersistenceConfigurationBuilder;
 import org.infinispan.persistence.leveldb.configuration.LevelDBStoreConfiguration;
 import org.infinispan.persistence.leveldb.configuration.LevelDBStoreConfigurationBuilder;
 import org.testng.annotations.Test;
 
 @Test(groups = "unit", testName = "persistence.leveldb.JniLevelDBCacheStoreTest")
+@SkipOnOs({SkipOnOs.OS.SOLARIS, SkipOnOs.OS.WINDOWS})
 public class JniLevelDBCacheStoreTest extends LevelDBStoreTest {
 
    protected LevelDBStoreConfigurationBuilder createCacheStoreConfig(PersistenceConfigurationBuilder lcb) {
