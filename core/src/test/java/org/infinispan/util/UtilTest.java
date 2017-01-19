4423158e9e3d2d3e3cef1291f255b6fb56cb062a
gustavonalle <gustavonalle@gmail.com>
2017-01-19 19:27:27 +0000

    ISPN-7364 Fixed failing test

--- core/src/test/java/org/infinispan/util/UtilTest.java
+++ core/src/test/java/org/infinispan/util/UtilTest.java
@@ -21,18 +21,18 @@ public class UtilTest {
             37, 108, -102, -113, 82, 73, -88, 103, 88, -44, -49, -58, 127, -93,
             83, 32, 124, 18, 83, 35, -116, -38, 43, -26, -87, 20, -82, -29, 36,
             -20, 50, 97, -18, -30, 80};
-      assertEquals("dcfc1b6f033176cf374a190c39a01189e28f62d6cc314a5d2ca227b9ca" +
-            "256c9a8f5249a86758d4cfc67fa353207c1253238cda2be6a914aee324ec3261eee250",
+      assertEquals("DCFC1B6F033176CF374A190C39A01189E28F62D6CC314A5D2CA227B9CA" +
+                  "256C9A8F5249A86758D4CFC67FA353207C1253238CDA2BE6A914AEE324EC3261EEE250",
             Util.toHexString(sample1));
 
       byte[] sample2 = { -36, -4, 27, 111, 3, 49, 118 };
-      assertEquals("dcfc1b6f033176", Util.toHexString(sample2, 8));
+      assertEquals("DCFC1B6F033176", Util.toHexString(sample2, 8));
 
       byte[] sample3 = { -36, -4, 27, 111, 3, 49, 118, -49 };
-      assertEquals("dcfc1b6f033176cf", Util.toHexString(sample3, 8));
+      assertEquals("DCFC1B6F033176CF", Util.toHexString(sample3, 8));
 
       byte[] sample4 = { -36, -4, 27, 111, 3, 49, 118, -49, 55};
-      assertEquals("dcfc1b6f033176cf", Util.toHexString(sample4, 8));
+      assertEquals("DCFC1B6F033176CF", Util.toHexString(sample4, 8));
    }
 
 }
