9d139563a834329cc5671deae52e90d5a79495b4
Adrian Nistor <anistor@redhat.com>
2017-01-23 09:45:35 +0200

    Small Javadoc clarifications

--- query-dsl/src/main/java/org/infinispan/query/api/continuous/ContinuousQueryListener.java
+++ query-dsl/src/main/java/org/infinispan/query/api/continuous/ContinuousQueryListener.java
@@ -10,8 +10,8 @@ public interface ContinuousQueryListener<K, V> {
 
    /**
     * Receives notification that a cache entry has joined the matching set. This is invoked initially when receiving the
-    * existing entries then match the query and later whenever a previously non-matching entry is updated and starts to
-    * match.
+    * existing entries that match the query and subsequently whenever a previously non-matching entry is updated and
+    * starts to match.
     *
     * @param key   the key of the joining entry
     * @param value the joining entry or the Object[] projection if a projection was specified
@@ -21,7 +21,7 @@ public interface ContinuousQueryListener<K, V> {
 
    /**
     * Receives notification that a cache entry from the matching set was updated and continues to match the query. The
-    * modified attributes are not necessarily part of the query.
+    * modified attributes causing this update are not necessarily part of the query.
     *
     * @param key   the key of the joining entry
     * @param value the joining entry or the Object[] projection if specified
