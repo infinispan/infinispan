3f8c3f87d45a8d51de1402c6dbff89a0350d1738
gustavonalle <gustavonalle@gmail.com>
2017-02-23 10:31:45 +0000

    ISPN-7506 Added scripts to allow tests from IDE

---
+++ integrationtests/elasticsearch-indexmanager-it/start-elastic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+VERSION=$(mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=version.elasticsearch | grep -v '\[.*\]')
+
+function waitForElastic()
+{
+  while ! curl http://localhost:9200  >/dev/null 2>&1; do sleep 1; done;
+}
+
+function startElastic()
+{
+  docker run --name elastic -d -p 9200:9200 -v "$PWD/elasticsearch.yml:/usr/share/elasticsearch/config/elasticsearch.yml" elasticsearch:$VERSION
+  waitForElastic
+}
+
+function installPlugins()
+{
+  docker exec elastic bin/plugin install delete-by-query
+}
+
+function restartElastic()
+{
+  docker restart elastic
+  waitForElastic
+}
+
+startElastic
+installPlugins
+restartElastic
