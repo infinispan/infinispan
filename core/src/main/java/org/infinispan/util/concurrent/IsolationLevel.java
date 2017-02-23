c532f727ebcd298d38a1a8aeb3a2469f1b23f7aa
Radim Vansa <rvansa@redhat.com>
2017-02-23 17:06:44 +0100

    Clarify repeatable-reads semantics

--- core/src/main/java/org/infinispan/util/concurrent/IsolationLevel.java
+++ core/src/main/java/org/infinispan/util/concurrent/IsolationLevel.java
@@ -1,14 +1,14 @@
 package org.infinispan.util.concurrent;
 
 /**
- * Various transaction isolation levels as an enumerated class.  Note that <a href="http://wiki.jboss.org/wiki/JBossCacheMVCC">MVCC</a>
- * only supports {@link #READ_COMMITTED} and {@link #REPEATABLE_READ}, upgrading where possible.
+ * Various transaction isolation levels as an enumerated class.  Note that Infinispan
+ * supports only {@link #READ_COMMITTED} and {@link #REPEATABLE_READ}, upgrading where possible.
  * <p/>
  * Also note that Infinispan defaults to {@link #READ_COMMITTED}.
  * <p/>
  *
  * @author (various)
- * @see <a href="http://en.wikipedia.org/wiki/Isolation_%28computer_science%29">Isolation levels</a>
+ * @see <a href="http://infinispan.org/docs/stable/user_guide/user_guide.html#isolation_levels">Isolation levels</a>
  * @since 4.0
  */
 public enum IsolationLevel {
