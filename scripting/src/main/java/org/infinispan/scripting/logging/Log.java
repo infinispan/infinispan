e3f4b76e5b1079481062c33f1ddd6b677f9c6b40
Tristan Tarrant <ttarrant@redhat.com>
2017-02-27 15:10:09 +0100

    ISPN-6724 Assign new scripting logging id range

--- scripting/src/main/java/org/infinispan/scripting/logging/Log.java
+++ scripting/src/main/java/org/infinispan/scripting/logging/Log.java
@@ -9,8 +9,8 @@ import org.jboss.logging.annotations.Message;
 import org.jboss.logging.annotations.MessageLogger;
 
 /**
- * Log abstraction for the Scripting module. For this module, message ids ranging from 26001 to
- * 27000 inclusively have been reserved.
+ * Log abstraction for the Scripting module. For this module, message ids ranging from 27501 to
+ * 28000 inclusively have been reserved.
  *
  * @author Tristan Tarrant
  * @since 7.2
@@ -18,41 +18,41 @@ import org.jboss.logging.annotations.MessageLogger;
 @MessageLogger(projectCode = "ISPN")
 public interface Log extends org.infinispan.util.logging.Log {
    @LogMessage(level = ERROR)
-   @Message(value = "Could not register interpreter MBean", id = 26001)
+   @Message(value = "Could not register interpreter MBean", id = 27501)
    void jmxRegistrationFailed();
 
    @LogMessage(level = ERROR)
-   @Message(value = "Could not unregister interpreter MBean", id = 26002)
+   @Message(value = "Could not unregister interpreter MBean", id = 27502)
    void jmxUnregistrationFailed();
 
-   @Message(value = "Script execution error", id = 26003)
+   @Message(value = "Script execution error", id = 27503)
    CacheException scriptExecutionError(@Cause Throwable t);
 
-   @Message(value = "Compiler error for script '%s'", id = 26004)
+   @Message(value = "Compiler error for script '%s'", id = 27504)
    CacheException scriptCompilationException(@Cause Throwable t, String name);
 
-   @Message(value = "No script named '%s'", id = 26005)
+   @Message(value = "No script named '%s'", id = 27505)
    CacheException noNamedScript(String name);
 
-   @Message(value = "Unknown script mode: '%s'", id = 26006)
+   @Message(value = "Unknown script mode: '%s'", id = 27506)
    CacheException unknownScriptProperty(String value);
 
-   @Message(value = "Cannot find an appropriate script engine for '%s'", id = 26007)
+   @Message(value = "Cannot find an appropriate script engine for '%s'", id = 27507)
    IllegalArgumentException noScriptEngineForScript(String name);
 
-   @Message(value = "Script '%s' cannot be invoked directly since it specifies mode '%s'", id = 26008)
+   @Message(value = "Script '%s' cannot be invoked directly since it specifies mode '%s'", id = 27508)
    IllegalArgumentException cannotInvokeScriptDirectly(String scriptName, String property);
 
-   @Message(value = "Distributed script '%s' invoked without a cache binding", id = 26009)
+   @Message(value = "Distributed script '%s' invoked without a cache binding", id = 27509)
    IllegalStateException distributedTaskNeedCacheInBinding(String scriptName);
 
-   @Message(value = "Cannot find an appropriate script engine for script '%s'", id = 26010)
+   @Message(value = "Cannot find an appropriate script engine for script '%s'", id = 27510)
    IllegalArgumentException noEngineForScript(String name);
 
-   @Message(value = "Script parameters must be declared using the array notation, e.g. [a,b,c]", id = 26011)
+   @Message(value = "Script parameters must be declared using the array notation, e.g. [a,b,c]", id = 27511)
    IllegalArgumentException parametersNotArray();
 
-   @Message(value = "Scripts can only access named caches", id = 26012)
+   @Message(value = "Scripts can only access named caches", id = 27512)
    IllegalArgumentException scriptsCanOnlyAccessNamedCaches();
 
 }
