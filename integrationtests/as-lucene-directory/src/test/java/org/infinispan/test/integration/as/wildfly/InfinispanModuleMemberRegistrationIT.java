b568b87e837de4b65a76b29ee9371dae9045f4bd
Sanne Grinovero <sanne@hibernate.org>
2017-03-20 22:01:10 +0000

    ISPN-7572 Correct the testing configuration for index storage in H2

--- integrationtests/as-lucene-directory/src/test/java/org/infinispan/test/integration/as/wildfly/InfinispanModuleMemberRegistrationIT.java
+++ integrationtests/as-lucene-directory/src/test/java/org/infinispan/test/integration/as/wildfly/InfinispanModuleMemberRegistrationIT.java
@@ -56,7 +56,7 @@ public class InfinispanModuleMemberRegistrationIT extends MemberRegistrationBase
               .version("2.0")
               .createPersistenceUnit()
               .name("primary")
-              .jtaDataSource("java:jboss/datasources/ExampleDS")
+              .jtaDataSource("java:jboss/datasources/HibernateTestingDS")
               .getOrCreateProperties()
               .createProperty()
               .name("hibernate.hbm2ddl.auto")
