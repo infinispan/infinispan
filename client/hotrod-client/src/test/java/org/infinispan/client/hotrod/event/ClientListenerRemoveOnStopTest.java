2dfc849ddde6f66ecab478a4ea057043e6074718
Katia Aresti <karesti@redhat.com>
2017-04-18 22:34:06 +0200

    ISPN-6776 Remove the listener on the client side when the operation was not executed on the server. This happens when the listener does not exist on the server anymore after a server shutdown

--- client/hotrod-client/src/test/java/org/infinispan/client/hotrod/event/ClientListenerRemoveOnStopTest.java
+++ client/hotrod-client/src/test/java/org/infinispan/client/hotrod/event/ClientListenerRemoveOnStopTest.java
@@ -6,7 +6,9 @@ import static org.testng.AssertJUnit.assertTrue;
 import java.util.Set;
 
 import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.test.HotRodClientTestingUtil;
 import org.infinispan.client.hotrod.test.SingleHotRodServerTest;
+import org.infinispan.server.hotrod.configuration.HotRodServerConfigurationBuilder;
 import org.testng.annotations.Test;
 
 @Test(groups = "functional", testName = "client.hotrod.event.ClientListenerRemoveOnStopTest")
@@ -26,6 +28,24 @@ public class ClientListenerRemoveOnStopTest extends SingleHotRodServerTest {
       assertTrue(listeners.contains(eventListener1));
       assertTrue(listeners.contains(eventListener2));
       remoteCacheManager.stop();
+      listeners = rcache.getListeners();
+      assertEquals(0, listeners.size());
+   }
+
+   public void testRemoveListenerAfterStopAndRestart() {
+      remoteCacheManager.start();
+      final RemoteCache<Integer, String> rcache = remoteCacheManager.getCache();
+      final EventLogListener<Integer> eventListener1 = new EventLogListener<>(rcache);
+      rcache.addClientListener(eventListener1);
+      Set<Object> listeners = rcache.getListeners();
+      assertEquals(1, listeners.size());
+      assertTrue(listeners.contains(eventListener1));
+      int port = this.hotrodServer.getPort();
+      HotRodClientTestingUtil.killServers(this.hotrodServer);
+      listeners = rcache.getListeners();
+      assertEquals(1, listeners.size());
+      hotrodServer = HotRodClientTestingUtil.startHotRodServer(this.cacheManager, port, new HotRodServerConfigurationBuilder());
+      rcache.removeClientListener(eventListener1);
       listeners = rcache.getListeners();
       assertEquals(0, listeners.size());
    }
