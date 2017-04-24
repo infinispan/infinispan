a0a307f581636ab04f52c30b1b3e51bfdfa766e0
William Burns <mudokonman@gmail.com>
2017-04-24 14:04:27 -0400

    ISPN-7827 Additional Documentation for Cluster Executor
    * Deprecate Distributed Executor

--- core/src/main/java/org/infinispan/distexec/DefaultExecutorService.java
+++ core/src/main/java/org/infinispan/distexec/DefaultExecutorService.java
@@ -49,6 +49,7 @@ import org.infinispan.interceptors.AsyncInterceptor;
 import org.infinispan.interceptors.AsyncInterceptorChain;
 import org.infinispan.interceptors.locking.ClusteringDependentLogic;
 import org.infinispan.lifecycle.ComponentStatus;
+import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.remoting.responses.Response;
 import org.infinispan.remoting.responses.SuccessfulResponse;
 import org.infinispan.remoting.rpc.ResponseMode;
@@ -78,7 +79,7 @@ import org.infinispan.util.logging.LogFactory;
  *
  * @author Vladimir Blagojevic
  * @since 5.0
- *
+ * @deprecated since 9.1 Please use {@link EmbeddedCacheManager#executor()} or {@link org.infinispan.CacheStream} instead.
  */
 public class DefaultExecutorService extends AbstractExecutorService implements DistributedExecutorService {
 
