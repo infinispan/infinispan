a5e4cc092402c05d9fccfe130e7879d6f16123f7
gustavonalle <gustavonalle@gmail.com>
2017-06-19 11:28:44 +0100

    ISPN-4065 Refuse to store an index on an ASYNC Cache

--- lucene/lucene-directory/src/main/java/org/infinispan/lucene/directory/DirectoryBuilder.java
+++ lucene/lucene-directory/src/main/java/org/infinispan/lucene/directory/DirectoryBuilder.java
@@ -1,7 +1,14 @@
 package org.infinispan.lucene.directory;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 import org.infinispan.Cache;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ClusteringConfiguration;
 import org.infinispan.lucene.impl.DirectoryBuilderImpl;
+import org.infinispan.lucene.logging.Log;
+import org.infinispan.util.logging.LogFactory;
 
 /**
  * Builder class to create instances of the {@link org.apache.lucene.store.Directory} implementation which stored data
@@ -9,20 +16,35 @@ import org.infinispan.lucene.impl.DirectoryBuilderImpl;
  */
 public final class DirectoryBuilder {
 
-    private DirectoryBuilder() {
-        //not to be created
-    }
+   private static final Log log = LogFactory.getLog(DirectoryBuilder.class, Log.class);
 
-    /**
-     * Starting point to create a Directory instance.
-     *
-     * @param metadataCache contains the metadata of stored elements
-     * @param chunksCache cache containing the bulk of the index; this is the larger part of data
-     * @param distLocksCache cache to store locks; should be replicated and not using a persistent CacheStore
-     * @param indexName identifies the index; you can store different indexes in the same set of caches using different identifiers
-     */
-    public static BuildContext newDirectoryInstance(Cache<?, ?> metadataCache, Cache<?, ?> chunksCache, Cache<?, ?> distLocksCache, String indexName) {
-        return new DirectoryBuilderImpl(metadataCache, chunksCache, distLocksCache, indexName);
-    }
+
+   private DirectoryBuilder() {
+      //not to be created
+   }
+
+   /**
+    * Starting point to create a Directory instance.
+    *
+    * @param metadataCache  contains the metadata of stored elements
+    * @param chunksCache    cache containing the bulk of the index; this is the larger part of data
+    * @param distLocksCache cache to store locks; should be replicated and not using a persistent CacheStore
+    * @param indexName      identifies the index; you can store different indexes in the same set of caches using
+    *                       different identifiers
+    */
+   public static BuildContext newDirectoryInstance(Cache<?, ?> metadataCache, Cache<?, ?> chunksCache, Cache<?, ?> distLocksCache, String indexName) {
+      validateIndexCaches(indexName, metadataCache, chunksCache, distLocksCache);
+      return new DirectoryBuilderImpl(metadataCache, chunksCache, distLocksCache, indexName);
+   }
+
+   private static void validateIndexCaches(String indexName, Cache<?, ?>... caches) {
+      Arrays.stream(caches).filter(Objects::nonNull).forEach(cache -> {
+         ClusteringConfiguration clusteringConfiguration = cache.getCacheConfiguration().clustering();
+         CacheMode cacheMode = clusteringConfiguration.cacheMode();
+         if (cacheMode.isClustered() && !cacheMode.isSynchronous()) {
+            throw log.cannotStoreIndexOnAsyncCaches(indexName, cache.getName(), cacheMode);
+         }
+      });
+   }
 
 }
