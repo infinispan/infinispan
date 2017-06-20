927ae693046d602010de0b6576fa5681d7818ba2
gustavonalle <gustavonalle@gmail.com>
2017-06-20 11:17:04 +0100

    ISPN-7949 Avoid leaving behind registered MBeans if server fails to start

--- client/hotrod-client/src/test/java/org/infinispan/client/hotrod/DroppedConnectionsTest.java
+++ client/hotrod-client/src/test/java/org/infinispan/client/hotrod/DroppedConnectionsTest.java
@@ -8,7 +8,6 @@ import org.infinispan.client.hotrod.test.HotRodClientTestingUtil;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.server.hotrod.HotRodServer;
 import org.infinispan.test.SingleCacheManagerTest;
-import org.infinispan.test.TestingUtil;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
