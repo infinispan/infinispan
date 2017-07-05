806610176d649bd68ab1997fb9eb4b400b53a099
Galder Zamarreño <galder@zamarreno.com>
2017-07-05 09:31:08 +0200

    ISPN-7963 Fix import rebasing

--- hibernate-cache/src/main/java/org/hibernate/cache/infinispan/tm/HibernateTransactionManagerLookup.java
+++ hibernate-cache/src/main/java/org/hibernate/cache/infinispan/tm/HibernateTransactionManagerLookup.java
@@ -5,37 +5,12 @@
  * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
  */
 package org.hibernate.cache.infinispan.tm;
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import java.util.Properties;
-=======
 
-<<<<<<< HEAD
->>>>>>> HHH-8159 - Apply fixups indicated by analysis tools
 import javax.transaction.TransactionManager;
-=======
->>>>>>> HHH-8741 - More checkstyle cleanups
-import java.util.Properties;
-import javax.transaction.TransactionManager;
-
-<<<<<<< HEAD
-import org.hibernate.cfg.Settings;
-<<<<<<< HEAD
-import org.hibernate.transaction.TransactionManagerLookup;
-=======
-
-import org.hibernate.cfg.Settings;
-import org.hibernate.service.jta.platform.spi.JtaPlatform;
-=======
-=======
 import org.hibernate.boot.spi.SessionFactoryOptions;
->>>>>>> HHH-9762 - Complete deprecation of Settings contract
 import org.hibernate.engine.transaction.jta.platform.spi.JtaPlatform;
->>>>>>> HHH-7556 - Clean up packages
-
-import javax.transaction.TransactionManager;
-import java.util.Properties;
->>>>>>> HHH-5949 - Migrate, complete and integrate TransactionFactory as a service
 
 /**
  * Hibernate transaction manager lookup class for Infinispan, so that
