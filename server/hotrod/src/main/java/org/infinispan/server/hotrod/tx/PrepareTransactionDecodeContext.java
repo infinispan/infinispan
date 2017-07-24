048d7dd17c99ba4ffe4fd6765935637d69ce871e
Pedro Ruivo <pruivo@redhat.com>
2017-07-24 19:25:06 +0100

    Remove TM

--- server/hotrod/src/main/java/org/infinispan/server/hotrod/tx/PrepareTransactionDecodeContext.java
+++ server/hotrod/src/main/java/org/infinispan/server/hotrod/tx/PrepareTransactionDecodeContext.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
 
 import org.infinispan.AdvancedCache;
+import org.infinispan.cache.impl.CacheImpl;
+import org.infinispan.cache.impl.DecoratedCache;
+import org.infinispan.cache.impl.EncoderCache;
 import org.infinispan.commands.write.WriteCommand;
 import org.infinispan.commons.tx.XidImpl;
+import org.infinispan.context.Flag;
+import org.infinispan.context.InvocationContext;
+import org.infinispan.context.impl.LocalTxInvocationContext;
 import org.infinispan.transaction.impl.LocalTransaction;
 import org.infinispan.transaction.tm.EmbeddedTransaction;
+import org.infinispan.transaction.tm.EmbeddedTransactionManager;
 import org.infinispan.transaction.xa.GlobalTransaction;
 import org.infinispan.transaction.xa.TransactionFactory;
 
@@ -28,6 +34,7 @@ public class PrepareTransactionDecodeContext extends TransactionDecodeContext {
 
    private final TransactionFactory transactionFactory;
    private EmbeddedTransaction tx;
+   private LocalTxInvocationContext localTxInvocationContext;
 
    public PrepareTransactionDecodeContext(AdvancedCache<byte[], byte[]> cache, XidImpl xid) {
       super(cache, xid);
@@ -40,27 +47,20 @@ public class PrepareTransactionDecodeContext extends TransactionDecodeContext {
     * @return {@code true} if the transaction can be started, {@code false} otherwise.
     */
    public boolean startTransaction() {
-      try {
-         tm.begin();
-      } catch (NotSupportedException | SystemException e) {
-         return false;
-      }
-      EmbeddedTransaction tx = tm.getTransaction();
+
+      EmbeddedTransaction tx = new EmbeddedTransaction(EmbeddedTransactionManager.getInstance());
       tx.setXid(xid);
       LocalTransaction localTransaction = transactionTable
             .getOrCreateLocalTransaction(tx, false, this::newGlobalTransaction);
       TxState txState = new TxState(localTransaction.getGlobalTransaction());
       if (!serverTransactionTable.addGlobalState(xid, txState)) {
+         //no need to rollback. nothing is enlisted in the transaction.
          transactionTable.removeLocalTransaction(localTransaction);
-         try {
-            tm.rollback();
-         } catch (SystemException e) {
-            //no-op
-         }
          return false;
       } else {
          this.txState = txState;
          this.tx = tx;
+         this.localTxInvocationContext = new LocalTxInvocationContext(localTransaction);
          serverTransactionTable.createLocalTx(xid, tx);
          transactionTable.enlist(tx, localTransaction);
          return true;
@@ -103,8 +103,42 @@ public class PrepareTransactionDecodeContext extends TransactionDecodeContext {
       }
    }
 
-   public void setRollbackOnly() throws SystemException {
-      tx.setRollbackOnly();
+   public void setRollbackOnly() {
+      try {
+         tx.setRollbackOnly();
+      } catch (SystemException e) {
+         //our tx implementation doesn't throw SystemException
+         throw new IllegalStateException(e);
+      }
+   }
+
+   public <K, V> AdvancedCache<K, V> decorateCache(AdvancedCache<K, V> cache) {
+      if (cache instanceof EncoderCache && ((EncoderCache) cache).getDelegate() instanceof CacheImpl) {
+         return decorateEncoderCache((EncoderCache<K, V>) cache);
+      } else if (cache instanceof CacheImpl) {
+         return withTransaction((CacheImpl<K, V>) cache);
+      } else {
+         throw new IllegalArgumentException("Unable to decorate cache");
+      }
+   }
+
+   private <K, V> AdvancedCache<K, V> decorateEncoderCache(EncoderCache<K, V> cache) {
+      AdvancedCache<K, V> txCache = withTransaction((CacheImpl<K, V>) cache.getDelegate());
+      return cache.withCache(txCache);
+   }
+
+   private <K, V> AdvancedCache<K, V> withTransaction(CacheImpl<K, V> cache) {
+      return new DecoratedCache<K, V>(cache, Flag.FORCE_WRITE_LOCK) {
+         @Override
+         protected InvocationContext readContext(int size) {
+            return localTxInvocationContext;
+         }
+
+         @Override
+         protected InvocationContext writeContext(int size) {
+            return localTxInvocationContext;
+         }
+      };
    }
 
    private int commit() {
