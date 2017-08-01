c36982447be98c8a44185f4daceab3176f8f074b
Pedro Ruivo <pruivo@redhat.com>
2017-08-01 14:19:15 +0100

    Re-enable AtomicMapAPITest

--- core/src/test/java/org/infinispan/atomic/AtomicMapAPITest.java
+++ core/src/test/java/org/infinispan/atomic/AtomicMapAPITest.java
@@ -3,12 +3,12 @@ package org.infinispan.atomic;
 import static org.infinispan.atomic.AtomicMapLookup.getAtomicMap;
 import static org.infinispan.atomic.AtomicMapLookup.getFineGrainedAtomicMap;
 import static org.infinispan.test.Exceptions.expectException;
-import static org.testng.AssertJUnit.fail;
 
 import java.util.Map;
 
 import org.infinispan.Cache;
 import org.infinispan.distribution.MagicKey;
+import org.infinispan.test.Exceptions;
 import org.testng.annotations.Test;
 
 /**
@@ -27,7 +27,6 @@ public class AtomicMapAPITest extends BaseAtomicHashMapAPITest {
       expectException(IllegalStateException.class, () -> getAtomicMap(cache1, key));
    }
 
-   @Test(enabled = false, description = "Doesn't work when the originator isn't the primary owner, see ISPN-5988")
    public void testAtomicMapAfterFineGrainedAtomicMapBackup() throws Exception {
       Cache<MagicKey, Object> cache1 = cache(0, "atomic");
       Cache<MagicKey, Object> cache2 = cache(1, "atomic");
@@ -35,12 +34,7 @@ public class AtomicMapAPITest extends BaseAtomicHashMapAPITest {
       MagicKey key = new MagicKey(cache2);
       getFineGrainedAtomicMap(cache1, key);
 
-      try {
-         getAtomicMap(cache1, key);
-         fail("Should have failed with an IllegalArgumentException");
-      } catch (IllegalArgumentException e) {
-         // Expected
-      }
+      Exceptions.expectException(IllegalStateException.class, "ISPN000457:.*", () -> getAtomicMap(cache1, key));
    }
 
    @Override
