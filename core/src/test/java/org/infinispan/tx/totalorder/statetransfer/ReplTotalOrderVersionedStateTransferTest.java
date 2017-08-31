ad7440e8c7ade19f9b883e870b314fecae27aa21
Pedro Ruivo <pruivo@redhat.com>
2017-08-31 15:18:43 +0100

    ISPN-6827 Disable ReplTotalOrderVersionedStateTransferTest

--- core/src/test/java/org/infinispan/tx/totalorder/statetransfer/ReplTotalOrderVersionedStateTransferTest.java
+++ core/src/test/java/org/infinispan/tx/totalorder/statetransfer/ReplTotalOrderVersionedStateTransferTest.java
@@ -9,7 +9,7 @@ import org.testng.annotations.Test;
  * @author Mircea Markus <mircea.markus@jboss.com> (C) 2011 Red Hat Inc.
  * @since 5.3
  */
-@Test(groups = "functional", testName = "tx.totalorder.statetransfer.ReplTotalOrderVersionedStateTransferTest")
+@Test(groups = "unstable", testName = "tx.totalorder.statetransfer.ReplTotalOrderVersionedStateTransferTest", description = "ISPN-6827,functional group")
 public class ReplTotalOrderVersionedStateTransferTest extends VersionedReplStateTransferTest {
 
    @Override
