541b267531d2d93832fae7a01e3ece47d05b6a96
Dan Berindei <dan@infinispan.org>
2017-09-13 15:34:37 +0300

    ISPN-8293 @Group and Grouper are not included in the javadoc

--- core/src/main/java/org/infinispan/distribution/group/package-info.java
+++ core/src/main/java/org/infinispan/distribution/group/package-info.java
@@ -7,5 +7,7 @@
  * Infinispan offers support for both instrinsic grouping ( see{@link org.infinispan.distribution.group.Group})
  * and extrinsic grouping (see {@link org.infinispan.distribution.group.Grouper}).
  * </p>
+ *
+ * @public
  */
 package org.infinispan.distribution.group;
