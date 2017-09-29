infinispan/infinispan

commit 048d7dd17c99ba4ffe4fd6765935637d69ce871e

Remove TM

core/src/main/java/org/infinispan/cache/impl/EncoderCache.java
@@ -915,4 +915,11 @@ public class EncoderCache<K, V> extends AbstractDelegatingAdvancedCache<K, V> {
          super.addFilteredListener(listener, filter, converter, filterAnnotations);
       }
    }
+
+   //HACK!
+   public EncoderCache<K, V> withCache(AdvancedCache<K, V> otherCache) {
+      EncoderCache<K, V> cache = new EncoderCache<>(otherCache, keyEncoderClass, valueEncoderClass, keyWrapperClass, valueWrapperClass);
+      initState(cache, this);
+      return cache;
+   }
 }

core/src/main/java/org/infinispan/cache/impl/DecoratedCache.java
@@ -100,7 +100,7 @@ public class DecoratedCache<K, V> extends AbstractDelegatingAdvancedCache<K, V> 
 
    @Override
    public AdvancedCache<K, V> lockAs(Object lockOwner) {
-      Objects.nonNull(lockOwner);
+      Objects.requireNonNull(lockOwner);
       if (lockOwner != this.lockOwner) {
          return new DecoratedCache<>(cacheImplementation, lockOwner, flags);
       }
@@ -456,7 +456,7 @@ public class DecoratedCache<K, V> extends AbstractDelegatingAdvancedCache<K, V> 
 
    @Override
    public boolean containsValue(Object value) {
-      Objects.nonNull(value);
+      Objects.requireNonNull(value);
       return values().stream().anyMatch(StreamMarshalling.equalityPredicate(value));
    }
 
@@ -636,7 +636,7 @@ public class DecoratedCache<K, V> extends AbstractDelegatingAdvancedCache<K, V> 
       return cacheImplementation.getCacheEntry(key, flags, readContext(1));
    }
 
-   InvocationContext readContext(int size) {
+   protected InvocationContext readContext(int size) {
       InvocationContext ctx = cacheImplementation.invocationContextFactory.createInvocationContext(false, size);
       if (lockOwner != null) {
          ctx.setLockOwner(lockOwner);
@@ -644,7 +644,7 @@ public class DecoratedCache<K, V> extends AbstractDelegatingAdvancedCache<K, V> 
       return ctx;
    }
 
-   InvocationContext writeContext(int size) {
+   protected InvocationContext writeContext(int size) {
       InvocationContext ctx = cacheImplementation.getInvocationContextWithImplicitTransaction(false, size);
       if (lockOwner != null) {
          ctx.setLockOwner(lockOwner);

server/hotrod/src/main/java/org/infinispan/server/hotrod/CacheDecodeContext.java
@@ -117,15 +117,15 @@ public final class CacheDecodeContext {
 
       //forces the write-lock. used by pessimistic transaction. it ensures the key is not written after is it read and validated
       //optimistic transaction will use the write-skew check.
-      AdvancedCache<byte[], byte[]> readCache = cache.withFlags(Flag.FORCE_WRITE_LOCK);
+      AdvancedCache<byte[], byte[]> txCache = txContext.decorateCache(cache);
 
       try {
          for (TransactionWrite write : context.writes()) {
-            if (isValid(write, readCache)) {
+            if (isValid(write, txCache)) {
                if (write.isRemove()) {
-                  cache.remove(write.key);
+                  txCache.remove(write.key);
                } else {
-                  cache.put(write.key, write.value, buildMetadata(write.lifespan, write.maxIdle));
+                  txCache.put(write.key, write.value, buildMetadata(write.lifespan, write.maxIdle));
                }
             } else {
                txContext.setRollbackOnly();

server/hotrod/src/main/java/org/infinispan/server/hotrod/tx/PrepareTransactionDecodeContext.java
@@ -4,17 +4,23 @@ import java.util.List;
 
 import javax.transaction.HeuristicMixedException;
 import javax.transaction.HeuristicRollbackException;
-import javax.transaction.NotSupportedException;
 import javax.transaction.RollbackException;
 import javax.transaction.SystemException;
 import javax.transaction.xa.XAException;
 import javax.transaction.xa.XAResource;
 
 import org.infinispan.AdvancedCache;
+import org.infinispan.cache.impl.CacheImpl;
+import org.infinispan.cache.impl.DecoratedCache;
+import org.infinispan.cache.impl.EncoderCache;
 import org.infinispan.commands.write.WriteCommand;
 import org.infinispan.commons.tx.XidImpl;
+import org.infinispan.context.Flag;
+import org.infinispan.context.InvocationContext;
+import org.infinispan.context.impl.LocalTxInvocationContext;
 import org.infinispan.transaction.impl.LocalTransaction;
 import org.infinispan.transaction.tm.EmbeddedTransaction;
+import org.infinispan.transaction.tm.EmbeddedTransactionManager;
 import org.infinispan.transaction.xa.GlobalTransaction;
 import org.infinispan.transaction.xa.TransactionFactory;
 
@@ -28,6 +34,7 @@ public class PrepareTransactionDecodeContext extends TransactionDecodeContext {
 
    private final TransactionFactory transactionFactory;
    private EmbeddedTransaction tx;
+   private LocalTxInvocationContext localTxInvocationContext;
 
    public PrepareTransactionDecodeContext(AdvancedCache<byte[], byte[]> cache, XidImpl xid) {
       super(cache, xid);
@@ -40,27 +47,20 @@ public class PrepareTransactionDecodeContext extends TransactionDecodeContext {
     * @return {@code true} if the transaction can be started, {@code false} otherwise.
     */
    public boolean startTransaction() {
-      try {
-         tm.begin();
-      } catch (NotSupportedException | SystemException e) {
-         return false;
-      }
-      EmbeddedTransaction tx = tm.getTransaction();
+
+      EmbeddedTransaction tx = new EmbeddedTransaction(EmbeddedTransactionManager.getInstance());
       tx.setXid(xid);
       LocalTransaction localTransaction = transactionTable
             .getOrCreateLocalTransaction(tx, false, this::newGlobalTransaction);
       TxState txState = new TxState(localTransaction.getGlobalTransaction());
       if (!serverTransactionTable.addGlobalState(xid, txState)) {
+         //no need to rollback. nothing is enlisted in the transaction.
          transactionTable.removeLocalTransaction(localTransaction);
-         try {
-            tm.rollback();
-         } catch (SystemException e) {
-            //no-op
-         }
          return false;
       } else {
          this.txState = txState;
          this.tx = tx;
+         this.localTxInvocationContext = new LocalTxInvocationContext(localTransaction);
          serverTransactionTable.createLocalTx(xid, tx);
          transactionTable.enlist(tx, localTransaction);
          return true;
@@ -103,8 +103,42 @@ public class PrepareTransactionDecodeContext extends TransactionDecodeContext {
       }
    }
 
-   public void setRollbackOnly() throws SystemException {
-      tx.setRollbackOnly();
+   public void setRollbackOnly() {
+      try {
+         tx.setRollbackOnly();
+      } catch (SystemException e) {
+         //our tx implementation doesn't throw SystemException
+         throw new IllegalStateException(e);
+      }
+   }
+
+   public <K, V> AdvancedCache<K, V> decorateCache(AdvancedCache<K, V> cache) {
+      if (cache instanceof EncoderCache && ((EncoderCache) cache).getDelegate() instanceof CacheImpl) {
+         return decorateEncoderCache((EncoderCache<K, V>) cache);
+      } else if (cache instanceof CacheImpl) {
+         return withTransaction((CacheImpl<K, V>) cache);
+      } else {
+         throw new IllegalArgumentException("Unable to decorate cache");
+      }
+   }
+
+   private <K, V> AdvancedCache<K, V> decorateEncoderCache(EncoderCache<K, V> cache) {
+      AdvancedCache<K, V> txCache = withTransaction((CacheImpl<K, V>) cache.getDelegate());
+      return cache.withCache(txCache);
+   }
+
+   private <K, V> AdvancedCache<K, V> withTransaction(CacheImpl<K, V> cache) {
+      return new DecoratedCache<K, V>(cache, Flag.FORCE_WRITE_LOCK) {
+         @Override
+         protected InvocationContext readContext(int size) {
+            return localTxInvocationContext;
+         }
+
+         @Override
+         protected InvocationContext writeContext(int size) {
+            return localTxInvocationContext;
+         }
+      };
    }
 
    private int commit() {

server/hotrod/src/main/java/org/infinispan/server/hotrod/tx/TransactionDecodeContext.java
@@ -12,7 +12,6 @@ import org.infinispan.remoting.inboundhandler.DeliverOrder;
 import org.infinispan.remoting.rpc.RpcManager;
 import org.infinispan.remoting.transport.Address;
 import org.infinispan.transaction.impl.TransactionTable;
-import org.infinispan.transaction.tm.EmbeddedTransactionManager;
 
 /**
  * A base decode context to handle prepare, commit and rollback requests from a client.
@@ -27,7 +26,6 @@ abstract class TransactionDecodeContext {
    final RpcManager rpcManager;
    final Address localAddress;
    final TransactionTable transactionTable;
-   final EmbeddedTransactionManager tm;
    final CommandsFactory commandsFactory;
    private final ClusteringDependentLogic clusteringDependentLogic;
    TxState txState;
@@ -41,7 +39,6 @@ abstract class TransactionDecodeContext {
       this.transactionTable = registry.getComponent(TransactionTable.class);
       this.clusteringDependentLogic = registry.getComponent(ClusteringDependentLogic.class);
       this.serverTransactionTable = registry.getComponent(ServerTransactionTable.class);
-      this.tm = (EmbeddedTransactionManager) cache.getTransactionManager();
       this.txState = serverTransactionTable.getGlobalState(xid);
    }
 

commit 9a6e702060b1cb8391e9f445cb2ccd268bae6053

Fix code broken by 048d7dd17c99ba4ffe4fd6765935637d69ce871e

core/src/main/java/org/infinispan/cache/impl/EncoderCache.java
@@ -918,7 +918,7 @@ public class EncoderCache<K, V> extends AbstractDelegatingAdvancedCache<K, V> {
 
    //HACK!
    public EncoderCache<K, V> withCache(AdvancedCache<K, V> otherCache) {
-      EncoderCache<K, V> cache = new EncoderCache<>(otherCache, keyEncoderClass, valueEncoderClass, keyWrapperClass, valueWrapperClass);
+      EncoderCache<K, V> cache = new EncoderCache<>(otherCache, keyDataConversion, valueDataConversion);
       initState(cache, this);
       return cache;
    }