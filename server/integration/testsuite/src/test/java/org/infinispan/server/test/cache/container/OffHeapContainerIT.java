7bbe7b763788356b7cb0457899c3c0d923ab8000
Vojtech Juranek <vojtech.juranek@gmail.com>
2017-10-27 13:37:56 +0200

    ISPN-8435 Create test for REST with off-heap data container

--- server/integration/testsuite/src/test/java/org/infinispan/server/test/cache/container/OffHeapContainerIT.java
+++ server/integration/testsuite/src/test/java/org/infinispan/server/test/cache/container/OffHeapContainerIT.java
@@ -1,5 +1,6 @@
 package org.infinispan.server.test.cache.container;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
@@ -11,6 +12,15 @@ import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.infinispan.arquillian.core.InfinispanResource;
 import org.infinispan.arquillian.core.RemoteInfinispanServer;
 import org.infinispan.arquillian.core.RunningServer;
@@ -20,6 +30,7 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.commons.util.CloseableIterator;
 import org.infinispan.server.test.util.ITestUtils;
+import org.infinispan.server.test.util.RemoteInfinispanMBeans;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Before;
 import org.junit.Test;
@@ -32,13 +43,14 @@ import org.junit.runner.RunWith;
  * @since 9.2
  */
 @RunWith(Arquillian.class)
-@WithRunningServer({@RunningServer(name = "off-heap")})
+@WithRunningServer({@RunningServer(name = OffHeapContainerIT.CONFIG_NAME)})
 public class OffHeapContainerIT {
 
-   @InfinispanResource("off-heap")
-   RemoteInfinispanServer server1;
-
+   public static final String CONFIG_NAME = "off-heap";
    private static RemoteCacheManager remoteCacheManager;
+
+   @InfinispanResource(OffHeapContainerIT.CONFIG_NAME)
+   RemoteInfinispanServer server1;
 
    @Before
    public void setUp() {
@@ -122,5 +134,34 @@ public class OffHeapContainerIT {
          assertTrue(keyVal.containsKey("key" + i));
          assertEquals(new Integer(i), keyVal.get("key" + i));
       });
+   }
+
+   @Test
+   public void testRestEndpoint() throws Exception {
+      String cacheName = "off-heap-default";
+      CloseableHttpClient restClient = null;
+      try {
+         restClient = HttpClients.createDefault();
+         RemoteInfinispanMBeans mbean = RemoteInfinispanMBeans.create(server1, OffHeapContainerIT.CONFIG_NAME, cacheName, "local");
+         String restUrl = "http://" + mbean.server.getHotrodEndpoint().getInetAddress().getHostName() + ":8080"
+               + mbean.server.getRESTEndpoint().getContextPath() + "/" + cacheName;
+
+         String key = "key";
+         byte[] value = "value".getBytes();
+         HttpPut put = new HttpPut(restUrl + "/" + key);
+         put.setEntity(new ByteArrayEntity(value, ContentType.APPLICATION_OCTET_STREAM));
+         HttpResponse putResponse = restClient.execute(put);
+         assertEquals(HttpStatus.SC_OK, putResponse.getStatusLine().getStatusCode());
+
+         HttpGet get = new HttpGet(restUrl + "/" + key);
+         get.addHeader("Accept", ContentType.APPLICATION_OCTET_STREAM.toString());
+         HttpResponse getResponse = restClient.execute(get);
+         assertEquals(HttpStatus.SC_OK, getResponse.getStatusLine().getStatusCode());
+         assertArrayEquals(value, EntityUtils.toByteArray(getResponse.getEntity()));
+      } finally {
+         if (restClient != null) {
+            restClient.close();
+         }
+      }
    }
 }
