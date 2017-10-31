335260f471f908f043e90b89a8a782246b9591b8
Dan Berindei <dan@infinispan.org>
2017-11-01 00:17:34 +0200

    ISPN-8326 Use regular methods to skip tests on some OSes

---
+++ commons-test/src/main/java/org/infinispan/commons/test/skip/OS.java
@@ -0,0 +1,23 @@
+package org.infinispan.commons.test.skip;
+
+/**
+ * Operating system family.
+ *
+ * Technically Solaris is UNIX, but for test purposes we are classifying it as a separate family.
+ *
+ * @since 9.2
+ */
+public enum OS {
+   UNIX, WINDOWS, SOLARIS;
+
+   static OS getCurrentOs() {
+      String os = System.getProperty("os.name").toLowerCase();
+      if (os.contains("win")) {
+         return WINDOWS;
+      } else if (os.contains("sunos")) {
+         return SOLARIS;
+      } else {
+         return UNIX;
+      }
+   }
+}
