c869c8d2c4d1c2208a181d0689c903e4b8ae5744
Adrian Nistor <anistor@redhat.com>
2017-11-22 17:59:37 +0200

    Beautify with lambdas and generics

--- commons/src/main/java/org/infinispan/commons/util/SecurityActions.java
+++ commons/src/main/java/org/infinispan/commons/util/SecurityActions.java
@@ -36,24 +36,12 @@ final class SecurityActions {
       SysProps PRIVILEGED = new SysProps() {
          @Override
          public String getProperty(final String name, final String defaultValue) {
-            PrivilegedAction<String> action = new PrivilegedAction<String>() {
-               @Override
-               public String run() {
-                  return System.getProperty(name, defaultValue);
-               }
-            };
-            return AccessController.doPrivileged(action);
+            return AccessController.doPrivileged((PrivilegedAction<String>) () -> System.getProperty(name, defaultValue));
          }
 
          @Override
          public String getProperty(final String name) {
-            PrivilegedAction<String> action = new PrivilegedAction<String>() {
-               @Override
-               public String run() {
-                  return System.getProperty(name);
-               }
-            };
-            return AccessController.doPrivileged(action);
+            return AccessController.doPrivileged((PrivilegedAction<String>) () -> System.getProperty(name));
          }
       };
 
@@ -85,52 +73,50 @@ final class SecurityActions {
    }
 
    static Object invokeAccessibly(Object instance, Method method, Object[] parameters) {
-       return doPrivileged((PrivilegedAction<Object>) () -> {
-          try {
-             method.setAccessible(true);
-             return method.invoke(instance, parameters);
-          } catch (InvocationTargetException e) {
-             Throwable cause = e.getCause() != null ? e.getCause() : e;
-             throw new CacheException("Unable to invoke method " + method + " on object of type " + (instance == null ? "null" : instance.getClass().getSimpleName()) +
-                                            (parameters != null ? " with parameters " + Arrays.asList(parameters) : ""), cause);
-          } catch (Exception e) {
-             throw new CacheException("Unable to invoke method " + method + " on object of type " + (instance == null ? "null" : instance.getClass().getSimpleName()) +
-                   (parameters != null ? " with parameters " + Arrays.asList(parameters) : ""), e);
-          }
-       });
+      return doPrivileged(() -> {
+         try {
+            method.setAccessible(true);
+            return method.invoke(instance, parameters);
+         } catch (InvocationTargetException e) {
+            Throwable cause = e.getCause() != null ? e.getCause() : e;
+            throw new CacheException("Unable to invoke method " + method + " on object of type " + (instance == null ? "null" : instance.getClass().getSimpleName()) +
+                  (parameters != null ? " with parameters " + Arrays.asList(parameters) : ""), cause);
+         } catch (Exception e) {
+            throw new CacheException("Unable to invoke method " + method + " on object of type " + (instance == null ? "null" : instance.getClass().getSimpleName()) +
+                  (parameters != null ? " with parameters " + Arrays.asList(parameters) : ""), e);
+         }
+      });
    }
 
    static ClassLoader[] getClassLoaders(ClassLoader appClassLoader) {
-      return doPrivileged((PrivilegedAction<ClassLoader[]>) () -> {
-         return new ClassLoader[] { appClassLoader,   // User defined classes
-               Util.class.getClassLoader(),           // Infinispan classes (not always on TCCL [modular env])
-               ClassLoader.getSystemClassLoader(),    // Used when load time instrumentation is in effect
-               Thread.currentThread().getContextClassLoader() //Used by jboss-as stuff
-         };
+      return doPrivileged(() -> new ClassLoader[]{
+            appClassLoader,   // User defined classes
+            Util.class.getClassLoader(),           // Infinispan classes (not always on TCCL [modular env])
+            ClassLoader.getSystemClassLoader(),    // Used when load time instrumentation is in effect
+            Thread.currentThread().getContextClassLoader() //Used by jboss-as stuff
       });
    }
 
    private static ClassLoader getOSGiClassLoader() {
-       // Make loading class optional
-       try {
-           Class<?> osgiClassLoader = Class.forName("org.infinispan.commons.util.OsgiClassLoader");
-           return (ClassLoader) osgiClassLoader.getMethod("getInstance", null).invoke(null);
-       } catch (ClassNotFoundException e) {
-           // fall back option - it can't hurt if we scan ctx class loader 2 times.
-           return Thread.currentThread().getContextClassLoader();
-       } catch (Exception e) {
-           throw new RuntimeException("Unable to call getInstance on OsgiClassLoader", e);
-       }
+      // Make loading class optional
+      try {
+         Class<?> osgiClassLoader = Class.forName("org.infinispan.commons.util.OsgiClassLoader");
+         return (ClassLoader) osgiClassLoader.getMethod("getInstance", null).invoke(null);
+      } catch (ClassNotFoundException e) {
+         // fall back option - it can't hurt if we scan ctx class loader 2 times.
+         return Thread.currentThread().getContextClassLoader();
+      } catch (Exception e) {
+         throw new RuntimeException("Unable to call getInstance on OsgiClassLoader", e);
+      }
    }
 
    static ClassLoader[] getOSGIContextClassLoaders(ClassLoader appClassLoader) {
-      return doPrivileged((PrivilegedAction<ClassLoader[]>) () -> {
-         return new ClassLoader[] { appClassLoader,   // User defined classes
-               getOSGiClassLoader(), // OSGi bundle context needs to be on top of TCCL, system CL, etc.
-               Util.class.getClassLoader(),           // Infinispan classes (not always on TCCL [modular env])
-               ClassLoader.getSystemClassLoader(),    // Used when load time instrumentation is in effect
-               Thread.currentThread().getContextClassLoader() //Used by jboss-as stuff
-         };
+      return doPrivileged(() -> new ClassLoader[]{
+            appClassLoader,   // User defined classes
+            getOSGiClassLoader(), // OSGi bundle context needs to be on top of TCCL, system CL, etc.
+            Util.class.getClassLoader(),           // Infinispan classes (not always on TCCL [modular env])
+            ClassLoader.getSystemClassLoader(),    // Used when load time instrumentation is in effect
+            Thread.currentThread().getContextClassLoader() //Used by jboss-as stuff
       });
    }
 }
