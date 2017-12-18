5aad119759978260c368e12f160d3da56b14e2e3
Paul Ferraro <paul.ferraro@redhat.com>
2017-12-18 17:23:04 -0500

    ISPN-8638 Create @Deprecated RegionFactory classes for compatibility

---
+++ hibernate-cache/src/main/java/org/hibernate/cache/infinispan/InfinispanRegionFactory.java
@@ -0,0 +1,14 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.cache.infinispan;
+
+/**
+ * @deprecated Use {@link org.infinispan.hibernate.cache.InfinispanRegionFactory} instead.
+ */
+@Deprecated
+public class InfinispanRegionFactory extends org.infinispan.hibernate.cache.InfinispanRegionFactory {
+}
