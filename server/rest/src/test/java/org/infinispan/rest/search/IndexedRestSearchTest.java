a57f1c3f53e850a7b005f659cb18f4afa49d6b1d
Gustavo Fernandes <gustavonalle@gmail.com>
2018-01-05 15:47:16 +0000

    ISPN-8566 Fix replacing of indexed values

--- server/rest/src/test/java/org/infinispan/rest/search/IndexedRestSearchTest.java
+++ server/rest/src/test/java/org/infinispan/rest/search/IndexedRestSearchTest.java
@@ -1,5 +1,9 @@
 package org.infinispan.rest.search;
 
+import static org.testng.Assert.assertEquals;
+
+import org.codehaus.jackson.JsonNode;
+import org.eclipse.jetty.client.api.ContentResponse;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.cache.Index;
@@ -23,4 +27,14 @@ public class IndexedRestSearchTest extends BaseRestSearchTest {
       return configurationBuilder;
    }
 
+   @Test
+   public void testReplaceIndexedDocument() throws Exception {
+      put(10, createPerson(0, "P", "", "?", "?", "MALE").toString());
+      put(10, createPerson(0, "P", "Surname", "?", "?", "MALE").toString());
+
+      ContentResponse response = get("10", "application/json");
+
+      JsonNode person = MAPPER.readTree(response.getContentAsString());
+      assertEquals("Surname", person.get("surname").asText());
+   }
 }
