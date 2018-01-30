522240dc1446aaa14e3f86385d3a2aa728de687c
diegolovison <diegolovison@gmail.com>
2018-01-30 17:54:47 -0200

    ISPN-8670 Add a configuration storage for standalone servers
    Created a test case the verify if the configuration remains the same
    after the server has been restarted when shared equals true.

--- core/src/test/java/org/infinispan/manager/CacheManagerAdminSharedPermanentTest.java
+++ core/src/test/java/org/infinispan/manager/CacheManagerAdminSharedPermanentTest.java
@@ -1,5 +1,10 @@
 package org.infinispan.manager;
 
+import org.infinispan.commons.api.CacheContainerAdmin;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.test.TestingUtil;
 import org.infinispan.test.fwk.CleanupAfterMethod;
 import org.testng.annotations.Test;
 
@@ -11,8 +16,33 @@ import org.testng.annotations.Test;
 @Test(testName = "manager.CacheManagerAdminSharedPermanentTest", groups = "functional")
 @CleanupAfterMethod
 public class CacheManagerAdminSharedPermanentTest extends CacheManagerAdminPermanentTest {
+
    @Override
    protected boolean isShared() {
       return true;
+   }
+
+   public void testSharedClusterCache() {
+
+      waitForClusterToForm();
+
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.clustering().cacheMode(CacheMode.DIST_SYNC);
+      Configuration configuration = builder.build();
+
+      // Create a persistent cache
+      manager(0).administration().withFlags(CacheContainerAdmin.AdminFlag.PERMANENT).createCache("a", configuration);
+      waitForClusterToForm("a");
+      checkConsistencyAcrossCluster("a", configuration);
+
+      // shutdown
+      TestingUtil.killCacheManagers(this.cacheManagers);
+      cacheManagers.clear();
+
+      // recreate the cache manager
+      createStatefulCacheManager("A", true);
+      createStatefulCacheManager("B", true);
+
+      checkConsistencyAcrossCluster("a", configuration);
    }
 }
