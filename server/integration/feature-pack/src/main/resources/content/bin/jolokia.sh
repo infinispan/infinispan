11a34964249c72286019f58114286415e788bcdf
Sebastian Laskawiec <slaskawi@redhat.com>
2018-02-15 09:56:45 +0100

    ISPN-8781 Do not fail on Dash Shell

--- server/integration/feature-pack/src/main/resources/content/bin/jolokia.sh
+++ server/integration/feature-pack/src/main/resources/content/bin/jolokia.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+if [ -z "$JMX_CONF" ]
+then
+   if [ -z "$1" ]
+   then
+       echo "Neither JMX_CONF nor input parameters are defined. Exiting"
+       exit 1
+   else
+       JMX_CONF=$1
+   fi
+fi
+
 AGENT_BOND_JAR=$(ls $JBOSS_HOME/modules/system/layers/base/io/fabric8/agent-bond/main/*.jar | sed -e "s| |:|g")
-JAVA_OPTS="$JAVA_OPTS -javaagent:$AGENT_BOND_JAR=$1"
+export JAVA_OPTS="$JAVA_OPTS -javaagent:$AGENT_BOND_JAR=$JMX_CONF"
