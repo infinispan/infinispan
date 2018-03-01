60e68054c229b429fdbd2bf628c03a377f8752f6
Yeray Borges <yborgess@redhat.com>
2018-03-01 17:58:02 +0000

    ISPN-8893 Do not ignore the asking thread stack trace when reporting a remoting exception.
    ISPN-8893 Use main exception instead of create a new one, added site backup operations

--- core/src/main/java/org/infinispan/remoting/transport/jgroups/JGroupsBackupResponse.java
+++ core/src/main/java/org/infinispan/remoting/transport/jgroups/JGroupsBackupResponse.java
@@ -74,8 +74,10 @@ public class JGroupsBackupResponse implements BackupResponse {
             errors.put(siteName, newTimeoutException(entry, entry.getKey().getTimeout()));
             addCommunicationError(siteName);
          } catch (ExecutionException ue) {
-            log.tracef(ue.getCause(), "Communication error with site %s", siteName);
-            errors.put(siteName, filterException(ue.getCause()));
+            Throwable cause = ue.getCause();
+            cause.addSuppressed(ue);
+            log.tracef(cause, "Communication error with site %s", siteName);
+            errors.put(siteName, filterException(cause));
             addCommunicationError(siteName);
          } finally {
             elapsedTime += timeService.timeDuration(startNanos, MILLISECONDS);
