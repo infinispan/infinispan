0d9a3b31f2b85e35a85238334ddc01bef6af3c0d
Gustavo Fernandes <gustavonalle@gmail.com>
2018-03-02 18:42:53 +0000

    ISPN-8898 Fixed test

--- remote-query/remote-query-server/src/test/java/org/infinispan/query/remote/impl/indexing/ProtobufWrapperIndexingTest.java
+++ remote-query/remote-query-server/src/test/java/org/infinispan/query/remote/impl/indexing/ProtobufWrapperIndexingTest.java
@@ -49,11 +49,11 @@ public class ProtobufWrapperIndexingTest extends SingleCacheManagerTest {
       MarshallerRegistration.registerMarshallers(serCtx);
 
       // Store some test data:
-      ProtobufValueWrapper wrapper1 = new ProtobufValueWrapper(createMarshalledUser(serCtx, "Adrian", "Nistor"));
-      ProtobufValueWrapper wrapper2 = new ProtobufValueWrapper(createMarshalledUser(serCtx, "John", "Batman"));
+      byte[] value1 = createMarshalledUser(serCtx, "Adrian", "Nistor");
+      byte[] value2 = createMarshalledUser(serCtx, "John", "Batman");
 
-      cache.put(1, wrapper1);   //todo how do we index if the key is a byte array?
-      cache.put(2, wrapper2);
+      cache.put(new byte[]{1, 2, 3}, value1);
+      cache.put(new byte[]{4, 5, 6}, value2);
 
       SearchManager sm = Search.getSearchManager(cache);
 
@@ -69,10 +69,10 @@ public class ProtobufWrapperIndexingTest extends SingleCacheManagerTest {
             .matching("Adrian")
             .createQuery();
 
-      List<ProtobufValueWrapper> list = sm.<ProtobufValueWrapper>getQuery(luceneQuery).list();
+      List<byte[]> list = sm.<byte[]>getQuery(luceneQuery).list();
       assertEquals(1, list.size());
-      ProtobufValueWrapper pvw = list.get(0);
-      User unwrapped = ProtobufUtil.fromWrappedByteArray(serCtx, pvw.getBinary());
+      byte[] pvw = list.get(0);
+      User unwrapped = ProtobufUtil.fromWrappedByteArray(serCtx, pvw);
       assertEquals("Adrian", unwrapped.getName());
 
       // an alternative approach ...
