3f3780e5a8a2760bc6f180575eebf598f64a34b8
Tristan Tarrant <ttarrant@redhat.com>
2018-03-15 11:23:22 +0100

    ISPN-8984 Reversion script now updates XSDs

--- bin/reversion.sh
+++ bin/reversion.sh
@@ -1,9 +1,43 @@
 #!/bin/bash
+PROCESS_SCHEMAS=true
+
+function rewriteXSDs() {
+    SEPARATOR=$1
+    OLDMAJOR=$2
+    OLDMINOR=$3
+    NEWMAJOR=$4
+    NEWMINOR=$5
+    
+    SCHEMAS=`find . -path "*/src/main/resources/*$OLDMAJOR$SEPARATOR$OLDMINOR.xsd"`
+    for OLDSCHEMA in $SCHEMAS; do
+        # Rewrite the filename
+        NEWSCHEMA=`echo "$OLDSCHEMA"|sed "s/$OLDMAJOR$SEPARATOR$OLDMINOR/$NEWMAJOR$SEPARATOR$NEWMINOR/"`
+        echo "$OLDSCHEMA ==> $NEWSCHEMA"
+        mv $OLDSCHEMA $NEWSCHEMA
+        # We need to do different replacements
+        sed -i "s/:$OLDMAJOR.$OLDMINOR\"/:$NEWMAJOR.$NEWMINOR\"/g" $NEWSCHEMA
+        sed -i "s/-$OLDMAJOR$SEPARATOR$OLDMINOR.xsd/-$NEWMAJOR$SEPARATOR$NEWMINOR.xsd/g" $NEWSCHEMA
+        git rm $OLDSCHEMA
+        git add $NEWSCHEMA
+    done
+}
+
+function rewriteXML() {
+    XML=$1
+    OLDMAJOR=$2
+    OLDMINOR=$3
+    NEWMAJOR=$4
+    NEWMINOR=$5
+
+    sed -i "s/:$OLDMAJOR.$OLDMINOR/:$NEWMAJOR.$NEWMINOR/g" $XML
+    sed -i "s/-$OLDMAJOR.$OLDMINOR.xsd/-$NEWMAJOR.$NEWMINOR.xsd/g" $XML
+    git add $XML
+}
 
 while test $# -gt 0; do
     case "$1" in
             -h|--help)
-                    echo "This script changes revisions for JDG package"
+                    echo "This script changes the version"
                     echo " "
                     echo "if no arguments specified - use interactive mode"
                     echo " "
@@ -11,6 +45,7 @@ while test $# -gt 0; do
                     echo "-h, --help                Show brief help"
                     echo "--new-revision 			Specify new revision"
                     echo "--pom-location 			Main POM file location"
+                    echo "--no-schemas              Do not upgrade the schemas"
                     exit 1
                     ;;
             --new-revision*)
@@ -19,6 +54,10 @@ while test $# -gt 0; do
                     ;;
             --pom-location*)
                     POMLOCATION=`echo $2`
+                    shift
+                    ;;
+            --no-schemas*)
+                    PROCESS_SCHEMAS=false
                     shift
                     ;;
             *)
@@ -43,4 +82,48 @@ fi
 
 echo "Changing from $VERSION to $NEWVERSION"
 
-find . -name 'pom.xml' -exec sed -i "s/<version>$VERSION<\/version>/<version>$NEWVERSION<\/version>/g" {} \;
+for POM in $(find . -name 'pom.xml'); do
+   sed -i "s/<version>$VERSION<\/version>/<version>$NEWVERSION<\/version>/g" $POM
+   git add $POM
+done
+
+OLDSCHEMAMAJOR=`echo $VERSION|cut -d. -f1`
+OLDSCHEMAMINOR=`echo $VERSION|cut -d. -f2`
+OLDSCHEMAVERSION=`echo $VERSION|cut -d. -f1,2`
+NEWSCHEMAMAJOR=`echo $NEWVERSION|cut -d. -f1`
+NEWSCHEMAMINOR=`echo $NEWVERSION|cut -d. -f2`
+NEWSCHEMAVERSION=`echo $NEWVERSION|cut -d. -f1,2`
+
+if [ "$OLDSCHEMAVERSION" != "$NEWSCHEMAVERSION" ] && [ "$PROCESS_SCHEMAS" = true ] ;  then
+    echo "Current schema: $OLDSCHEMAVERSION"
+    echo "New schema:     $NEWSCHEMAVERSION"
+    
+    # Set master schema version
+    sed -i "s/<infinispan.base.version>$OLDSCHEMAVERSION<\/infinispan.base.version>/<infinispan.base.version>$NEWSCHEMAVERSION<\/infinispan.base.version>/g" pom.xml
+    git add pom.xml
+    
+    # Create new unified configuration
+    cp core/src/test/resources/configs/unified/$OLDSCHEMAVERSION.xml core/src/test/resources/configs/unified/$NEWSCHEMAVERSION.xml
+    rewriteXML core/src/test/resources/configs/unified/$NEWSCHEMAVERSION.xml $OLDSCHEMAMAJOR $OLDSCHEMAMINOR $NEWSCHEMAMAJOR $NEWSCHEMAMINOR
+
+    # Update the distribution configurations
+    CONFIGS=`find distribution/src/main/release/common/configs/config-samples -name '*.xml'`
+    for CONFIG in $CONFIGS; do
+         rewriteXML $CONFIG $OLDSCHEMAMAJOR $OLDSCHEMAMINOR $NEWSCHEMAMAJOR $NEWSCHEMAMINOR
+    done
+
+    # Rewrite the XSDs
+    rewriteXSDs . $OLDSCHEMAMAJOR $OLDSCHEMAMINOR $NEWSCHEMAMAJOR $NEWSCHEMAMINOR
+    rewriteXSDs _ $OLDSCHEMAMAJOR $OLDSCHEMAMINOR $NEWSCHEMAMAJOR $NEWSCHEMAMINOR
+
+    echo ""
+    echo "WARNING: you will need to manually modify the server namespaces and parsing testcases to look for the new schema"
+    echo "- org.infinispan.server.jgroups.subsystem.JGroupsSchema"
+    echo "- org.infinispan.server.jgroups.subsystem.SubsystemParsingTestCase"
+    echo "- org.jboss.as.clustering.infinispan.subsystem.Namespace"
+    echo "- org.jboss.as.clustering.infinispan.subsystem.SubsystemParsingTestCase"
+    echo "- org.infinispan.server.endpoint.EndpointSubsystemTestCase"
+fi
+
+
+
