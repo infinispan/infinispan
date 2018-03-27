cb3ce5f806b01d55269aac73dd1dee6f7f6b6a8c
Katia Aresti <karesti@redhat.com>
2018-03-27 21:10:53 +0200

    ISPN-8994 Cluster health status takes in account internal caches

--- core/src/main/java/org/infinispan/health/impl/ClusterHealthImpl.java
+++ core/src/main/java/org/infinispan/health/impl/ClusterHealthImpl.java
@@ -2,53 +2,61 @@ package org.infinispan.health.impl;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.infinispan.health.ClusterHealth;
 import org.infinispan.health.HealthStatus;
 import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.registry.InternalCacheRegistry;
 
 public class ClusterHealthImpl implements ClusterHealth {
 
-    private final EmbeddedCacheManager cacheManager;
+   private final EmbeddedCacheManager cacheManager;
+   private final InternalCacheRegistry internalCacheRegistry;
 
-    public ClusterHealthImpl(EmbeddedCacheManager cacheManager) {
-        this.cacheManager = cacheManager;
-    }
+   public ClusterHealthImpl(EmbeddedCacheManager cacheManager) {
+      this.cacheManager = cacheManager;
+      internalCacheRegistry = cacheManager.getGlobalComponentRegistry().getComponent(InternalCacheRegistry.class);
+   }
 
-    @Override
-    public HealthStatus getHealthStatus() {
-        Set<HealthStatus> healthStatuses = cacheManager.getCacheNames().stream()
-                .map(cacheName -> cacheManager.getCache(cacheName))
-                .map(cache -> new CacheHealthImpl(cache))
-                .map(cacheHealth -> cacheHealth.getStatus())
-                .collect(Collectors.toSet());
+   @Override
+   public HealthStatus getHealthStatus() {
+      HealthStatus globalHealthStatus = HealthStatus.HEALTHY;
 
-        if (healthStatuses.contains(HealthStatus.UNHEALTHY)) {
-            return HealthStatus.UNHEALTHY;
-        } else if (healthStatuses.contains(HealthStatus.REBALANCING)) {
-            return HealthStatus.REBALANCING;
-        }
-        return HealthStatus.HEALTHY;
-    }
+      Set<HealthStatus> healthStatuses = Stream.concat(cacheManager.getCacheNames().stream(), internalCacheRegistry.getInternalCacheNames().stream())
+            .map(cacheName -> cacheManager.getCache(cacheName, false))
+            .filter(Objects::nonNull)
+            .map(CacheHealthImpl::new)
+            .map(CacheHealthImpl::getStatus)
+            .collect(Collectors.toSet());
 
-    @Override
-    public String getClusterName() {
-        return cacheManager.getClusterName();
-    }
+      if (healthStatuses.contains(HealthStatus.UNHEALTHY)) {
+         globalHealthStatus = HealthStatus.UNHEALTHY;
+      } else if (healthStatuses.contains(HealthStatus.REBALANCING)) {
+         globalHealthStatus = HealthStatus.REBALANCING;
+      }
+      return globalHealthStatus;
+   }
 
-    @Override
-    public int getNumberOfNodes() {
-        return Optional.ofNullable(cacheManager.getTransport()).map(t -> t.getMembers().size()).orElse(1);
-    }
+   @Override
+   public String getClusterName() {
+      return cacheManager.getClusterName();
+   }
 
-    @Override
-    public List<String> getNodeNames() {
-        return Optional.ofNullable(cacheManager.getTransport()).map(t -> t.getMembers()).orElse(Collections.emptyList())
-              .stream()
-              .map(member -> member.toString())
-              .collect(Collectors.toList());
-    }
+   @Override
+   public int getNumberOfNodes() {
+      return Optional.ofNullable(cacheManager.getTransport()).map(t -> t.getMembers().size()).orElse(1);
+   }
+
+   @Override
+   public List<String> getNodeNames() {
+      return Optional.ofNullable(cacheManager.getTransport()).map(t -> t.getMembers()).orElse(Collections.emptyList())
+            .stream()
+            .map(member -> member.toString())
+            .collect(Collectors.toList());
+   }
 }
