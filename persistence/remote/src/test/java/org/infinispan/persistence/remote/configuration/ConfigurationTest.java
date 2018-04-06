6776cd0b29cfa5d50525e1f529dd4ac3c833fc3a
Radoslav Husar <radosoft@gmail.com>
2018-04-06 17:20:53 +0200

    ISPN-9045 Failure to change 'protocolVersion' in RemoteStoreConfiguration

--- persistence/remote/src/test/java/org/infinispan/persistence/remote/configuration/ConfigurationTest.java
+++ persistence/remote/src/test/java/org/infinispan/persistence/remote/configuration/ConfigurationTest.java
@@ -1,13 +1,22 @@
 package org.infinispan.persistence.remote.configuration;
 
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.util.Properties;
+
+import org.infinispan.client.hotrod.ProtocolVersion;
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.testng.annotations.Test;
+
 
 @Test(groups = "unit", testName = "persistence.remote.configuration.ConfigurationTest")
 public class ConfigurationTest {
 
    public void testRemoteCacheStoreConfigurationAdaptor() {
+      Properties p = new Properties();
+      p.setProperty("protocolVersion", "2.7");
       ConfigurationBuilder b = new ConfigurationBuilder();
       b.persistence().addStore(RemoteStoreConfigurationBuilder.class)
          .remoteCacheName("RemoteCache")
@@ -20,30 +29,34 @@ public class ConfigurationTest {
             .maxActive(10)
             .minIdle(5)
             .exhaustedAction(ExhaustedAction.EXCEPTION)
-            .minEvictableIdleTime(10000)
-         .async().enable();
+            .minEvictableIdleTime(10_000)
+         .async().enable()
+         .withProperties(p);
+
       Configuration configuration = b.build();
       RemoteStoreConfiguration store = (RemoteStoreConfiguration) configuration.persistence().stores().get(0);
-      assert store.remoteCacheName().equals("RemoteCache");
-      assert store.servers().size() == 2;
-      assert store.connectionPool().maxActive() == 10;
-      assert store.connectionPool().minIdle() == 5;
-      assert store.connectionPool().exhaustedAction() == ExhaustedAction.EXCEPTION;
-      assert store.connectionPool().minEvictableIdleTime() == 10000;
-      assert store.fetchPersistentState();
-      assert store.async().enabled();
+      assertEquals("RemoteCache", store.remoteCacheName());
+      assertEquals(2, store.servers().size());
+      assertEquals(10, store.connectionPool().maxActive());
+      assertEquals(5, store.connectionPool().minIdle());
+      assertEquals(ExhaustedAction.EXCEPTION, store.connectionPool().exhaustedAction());
+      assertEquals(10_000, store.connectionPool().minEvictableIdleTime());
+      assertTrue(store.fetchPersistentState());
+      assertTrue(store.async().enabled());
+      assertEquals(ProtocolVersion.PROTOCOL_VERSION_27, store.protocol());
 
       b = new ConfigurationBuilder();
       b.persistence().addStore(RemoteStoreConfigurationBuilder.class).read(store);
       Configuration configuration2 = b.build();
       RemoteStoreConfiguration store2 = (RemoteStoreConfiguration) configuration2.persistence().stores().get(0);
-      assert store2.remoteCacheName().equals("RemoteCache");
-      assert store2.servers().size() == 2;
-      assert store2.connectionPool().maxActive() == 10;
-      assert store2.connectionPool().minIdle() == 5;
-      assert store2.connectionPool().exhaustedAction() == ExhaustedAction.EXCEPTION;
-      assert store2.connectionPool().minEvictableIdleTime() == 10000;
-      assert store2.fetchPersistentState();
-      assert store2.async().enabled();
+      assertEquals("RemoteCache", store2.remoteCacheName());
+      assertEquals(2, store2.servers().size());
+      assertEquals(10, store2.connectionPool().maxActive());
+      assertEquals(5, store2.connectionPool().minIdle());
+      assertEquals(ExhaustedAction.EXCEPTION, store2.connectionPool().exhaustedAction());
+      assertEquals(10_000, store2.connectionPool().minEvictableIdleTime());
+      assertTrue(store2.fetchPersistentState());
+      assertTrue(store2.async().enabled());
+      assertEquals(ProtocolVersion.PROTOCOL_VERSION_27, store2.protocol());
    }
 }
