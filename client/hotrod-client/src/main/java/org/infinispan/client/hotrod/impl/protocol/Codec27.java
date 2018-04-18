56f4f1ffca03b7196c6713d43b4d4eb25a508151
William Burns <mudokonman@gmail.com>
2018-04-18 09:03:39 -0400

    ISPN-8719 KeySet.(iterator|spliterator|stream) not compatible with versions before 9.1
    * Fixed issue where 9.1+ clients couldn't talk to 9.0 still

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/protocol/Codec27.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/protocol/Codec27.java
@@ -1,5 +1,10 @@
 package org.infinispan.client.hotrod.impl.protocol;
 
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.impl.operations.OperationsFactory;
+import org.infinispan.commons.util.CloseableIterator;
+import org.infinispan.commons.util.CloseableIteratorMapper;
+
 import io.netty.buffer.ByteBuf;
 
 /**
@@ -10,5 +15,14 @@ public class Codec27 extends Codec26 {
    @Override
    public HeaderParams writeHeader(ByteBuf buf, HeaderParams params) {
       return writeHeader(buf, params, HotRodConstants.VERSION_27);
+   }
+
+   @Override
+   public <K> CloseableIterator<K> keyIterator(RemoteCache<K, ?> remoteCache, OperationsFactory operationsFactory,
+         int batchSize) {
+      return new CloseableIteratorMapper<>(remoteCache.retrieveEntries(
+            // Use the ToEmptyBytesKeyValueFilterConverter to remove value payload
+            "org.infinispan.server.hotrod.HotRodServer$ToEmptyBytesKeyValueFilterConverter", batchSize),
+            e -> (K) e.getKey());
    }
 }
