41d0b0a33e07075088dd0ca52d779d7736231a0f
Radim Vansa <rvansa@redhat.com>
2018-05-17 17:10:03 +0200

    ISPN-9128 RehashWithSharedStoreTest.testRehashes random failures

--- core/src/test/java/org/infinispan/test/ViewChangeListener.java
+++ core/src/test/java/org/infinispan/test/ViewChangeListener.java
@@ -14,7 +14,7 @@ import org.infinispan.notifications.cachemanagerlistener.event.ViewChangedEvent;
  * Listens for view changes.  Note that you do NOT have to register this listener; it does so automatically when
  * constructed.
  */
-@Listener
+@Listener(observation = Listener.Observation.POST)
 public class ViewChangeListener {
    CacheContainer cm;
    final CountDownLatch latch = new CountDownLatch(1);
