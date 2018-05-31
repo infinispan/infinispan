86c006825dac779d6b63763290aac5f6136775b5
Adrian Nistor <anistor@redhat.com>
2018-05-31 11:56:35 +0300

    Improve javadoc of ProgrammaticSearchMappingProvider

--- query/src/main/java/org/infinispan/query/spi/ProgrammaticSearchMappingProvider.java
+++ query/src/main/java/org/infinispan/query/spi/ProgrammaticSearchMappingProvider.java
@@ -4,13 +4,13 @@ import org.hibernate.search.cfg.SearchMapping;
 import org.infinispan.Cache;
 
 /**
- * An advanced SPI to be implemented by Infinispan modules that want to customize the {@link SearchMapping} object
- * before the bootstrap of the {@link org.hibernate.search.SearchFactory} belonging to an indexed {@link Cache}. The
- * {@link SearchMapping} object provided via the "hibernate.search.model_mapping" config property is used as a starting
- * point if it was present, otherwise an empty {@link SearchMapping} is used.
+ * An advanced SPI to be implemented by Infinispan modules that want to customize the {@link SearchMapping} object right
+ * before the bootstrap of the {@link org.hibernate.search.spi.SearchIntegrator} belonging to an indexed {@link Cache}.
+ * The {@link SearchMapping} object provided via the "hibernate.search.model_mapping" config property is used as a
+ * starting point if it was present, otherwise an empty {@link SearchMapping} is used.
  * <p>
- * Please note that in case multiple providers are found the order of invocation is not predictable so implementations
- * must be careful not to step on each others toes.
+ * Please note that in case multiple providers are found in classpath the order of invocation is not predictable so
+ * implementations must be careful not to step on each others toes.
  *
  * @author anistor@redhat.com
  * @since 6.0
@@ -18,9 +18,9 @@ import org.infinispan.Cache;
 public interface ProgrammaticSearchMappingProvider {
 
    /**
-    * Supply some custom programmatic mappings.
+    * Supply some custom programmatic mappings to Hibernate Search.
     *
-    * @param cache         the indexed cache
+    * @param cache         the indexed cache instance
     * @param searchMapping the mapping object to customize
     */
    void defineMappings(Cache cache, SearchMapping searchMapping);
