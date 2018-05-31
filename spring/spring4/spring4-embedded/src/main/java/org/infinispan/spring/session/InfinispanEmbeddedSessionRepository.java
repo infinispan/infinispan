811ae7136d8ff0671933b583442e9178b6da9d03
Tristan Tarrant <ttarrant@redhat.com>
2018-05-31 10:46:51 +0200

    Fix InfinispanRemoteSessionRepositoryTest

--- spring/spring4/spring4-embedded/src/main/java/org/infinispan/spring/session/InfinispanEmbeddedSessionRepository.java
+++ spring/spring4/spring4-embedded/src/main/java/org/infinispan/spring/session/InfinispanEmbeddedSessionRepository.java
@@ -1,24 +1,14 @@
 package org.infinispan.spring.session;
 
-import java.util.Collections;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 import org.infinispan.spring.provider.SpringCache;
-import org.springframework.session.FindByIndexNameSessionRepository;
-import org.springframework.session.MapSession;
 
 /**
  * Session Repository for Infinispan in Embedded mode.
  *
  * @author Sebastian Łaskawiec
  * @since 9.0
- * @see FindByIndexNameSessionRepository
  */
-public class InfinispanEmbeddedSessionRepository extends AbstractInfinispanSessionRepository implements FindByIndexNameSessionRepository<MapSession> {
-
-   protected final PrincipalNameResolver principalNameResolver = new PrincipalNameResolver();
+public class InfinispanEmbeddedSessionRepository extends AbstractInfinispanSessionRepository {
 
    /**
     * Creates new repository based on {@link SpringCache}
@@ -27,17 +17,5 @@ public class InfinispanEmbeddedSessionRepository extends AbstractInfinispanSessi
     */
    public InfinispanEmbeddedSessionRepository(SpringCache cache) {
       super(cache, new EmbeddedApplicationPublishedBridge(cache));
-   }
-
-   @Override
-   public Map<String, MapSession> findByIndexNameAndIndexValue(String indexName, String indexValue) {
-      if (!PRINCIPAL_NAME_INDEX_NAME.equals(indexName)) {
-         return Collections.emptyMap();
-      }
-
-      return cache.getNativeCache().values().stream()
-            .map(cacheValue -> (MapSession) cacheValue)
-            .filter(session -> indexValue.equals(principalNameResolver.resolvePrincipal(session)))
-            .collect(Collectors.toMap(MapSession::getId, Function.identity()));
    }
 }
