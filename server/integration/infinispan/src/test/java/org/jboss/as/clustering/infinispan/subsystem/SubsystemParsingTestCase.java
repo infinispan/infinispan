c5e792b912c3b8b25a3b42ed389709ef05a00ff9
Gustavo Fernandes <gustavonalle@gmail.com>
2018-06-27 13:29:40 +0100

    ISPN-9326 Fix marshall of the modules configuration

--- server/integration/infinispan/src/test/java/org/jboss/as/clustering/infinispan/subsystem/SubsystemParsingTestCase.java
+++ server/integration/infinispan/src/test/java/org/jboss/as/clustering/infinispan/subsystem/SubsystemParsingTestCase.java
@@ -78,7 +78,7 @@ public class SubsystemParsingTestCase extends ClusteringSubsystemTest {
               {Namespace.INFINISPAN_SERVER_9_0, 142, null, null},
               {Namespace.INFINISPAN_SERVER_9_1, 144, null, null},
               {Namespace.INFINISPAN_SERVER_9_2, 154, null, null},
-              {Namespace.INFINISPAN_SERVER_9_3, 154, "schema/jboss-infinispan-core_9_3.xsd", new String[]{"/subsystem-templates/infinispan-core.xml"}},
+              {Namespace.INFINISPAN_SERVER_9_3, 157, "schema/jboss-infinispan-core_9_3.xsd", new String[]{"/subsystem-templates/infinispan-core.xml"}},
         };
         return Arrays.asList(data);
     }
