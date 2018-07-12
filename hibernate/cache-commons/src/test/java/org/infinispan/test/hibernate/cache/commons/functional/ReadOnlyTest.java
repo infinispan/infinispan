9d7ec19b15549bca4fb9295bb537741544577f79
Galder Zamarreño <galder@zamarreno.com>
2018-07-12 14:25:29 +0100

    ISPN-9369 Add test case

--- hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/functional/ReadOnlyTest.java
+++ hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/functional/ReadOnlyTest.java
@@ -9,15 +9,18 @@ package org.infinispan.test.hibernate.cache.commons.functional;
 import java.util.List;
 import java.util.Map;
 
+import org.hibernate.testing.TestForIssue;
 import org.infinispan.hibernate.cache.commons.InfinispanBaseRegion;
 import org.infinispan.hibernate.cache.commons.util.InfinispanMessageLogger;
 import org.hibernate.stat.Statistics;
 import org.infinispan.test.hibernate.cache.commons.functional.entities.Item;
+import org.infinispan.test.hibernate.cache.commons.util.InducedException;
 import org.infinispan.test.hibernate.cache.commons.util.TestRegionFactory;
 import org.infinispan.util.ControlledTimeService;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Parent tests for both transactional and
@@ -90,6 +93,44 @@ public class ReadOnlyTest extends SingleNodeTest {
 			s.delete(found);
 		});
 	}
+
+   @Test
+   @TestForIssue(jiraKey = "ISPN-9369")
+   public void testCacheQueryResultsOnRollback() throws Exception {
+      final Statistics stats = sessionFactory().getStatistics();
+      stats.clear();
+
+      final Item item = new Item("query-cache-rollback", "Querying caching on rollback");
+      withTxSession(s -> s.persist(item));
+
+      // Delay added to guarantee that query cache results won't be considered
+      // as not up to date due to persist session and query results from first
+      // query happening simultaneously.
+      TIME_SERVICE.advance(60001);
+
+      try {
+         withTxSession(s -> {
+            final List results = TEST_SESSION_ACCESS.execQueryListCacheable(s, "from Item");
+            assertFalse(results.isEmpty());
+            assertEquals(0, stats.getQueryCacheHitCount());
+            assertEquals(1, stats.getQueryCacheMissCount());
+            assertEquals(1, stats.getQueryCachePutCount());
+            throw new InducedException("Force it to rollback");
+         });
+      } catch (InducedException e) {
+         // ignore
+      }
+
+      stats.clear();
+
+      withTxSession(s -> {
+         final List results = TEST_SESSION_ACCESS.execQueryListCacheable(s, "from Item");
+         assertFalse(results.isEmpty());
+         assertEquals(1, stats.getQueryCacheHitCount());
+         assertEquals(0, stats.getQueryCacheMissCount());
+         assertEquals(0, stats.getQueryCachePutCount());
+      });
+   }
 
 	@Override
 	protected void addSettings(Map settings) {
