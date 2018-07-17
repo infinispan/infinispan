ff0a8af5f896d45b05aef29f1447cc4db2395474
Radim Vansa <rvansa@redhat.com>
2018-07-17 17:18:34 +0200

    ISPN-8518 FunctionalDistributionTest.testDistributionFromNonOwner random failures

--- core/src/test/java/org/infinispan/functional/FunctionalDistributionTest.java
+++ core/src/test/java/org/infinispan/functional/FunctionalDistributionTest.java
@@ -1,13 +1,19 @@
 package org.infinispan.functional;
 
 import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import org.infinispan.AdvancedCache;
+import org.infinispan.commands.functional.ReadWriteKeyCommand;
+import org.infinispan.distribution.BlockingInterceptor;
 import org.infinispan.functional.FunctionalMap.ReadWriteMap;
 import org.infinispan.functional.impl.FunctionalMapImpl;
 import org.infinispan.functional.impl.ReadWriteMapImpl;
-import org.infinispan.test.TestingUtil;
+import org.infinispan.interceptors.impl.EntryWrappingInterceptor;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -38,39 +44,35 @@ public class FunctionalDistributionTest extends AbstractFunctionalTest {
       super.createBeforeClass();
    }
 
-   public void testDistributionFromPrimaryOwner() throws InterruptedException {
+   public void testDistributionFromPrimaryOwner() throws Exception {
       Object key = "testDistributionFromPrimaryOwner";
       doTestDistribution(key, cacheManagers.stream()
             .map(cm -> cm.<Object, Integer>getCache(DIST).getAdvancedCache())
-            .filter(cache -> cache.getDistributionManager().getPrimaryLocation(key)
-                  .equals(cache.getRpcManager().getAddress()))
+            .filter(cache -> cache.getDistributionManager().getCacheTopology().getDistribution(key).isPrimary())
             .findAny()
             .get());
    }
 
-   public void testDistributionFromSecondaryOwner() throws InterruptedException {
+   public void testDistributionFromSecondaryOwner() throws Exception {
       Object key = "testDistributionFromSecondaryOwner";
       doTestDistribution(key, cacheManagers.stream()
             .map(cm -> cm.<Object, Integer>getCache(DIST).getAdvancedCache())
             // owner...
-            .filter(cache -> cache.getDistributionManager().getLocality(key).isLocal()
-                  // ...but not primary owner
-                  && !cache.getDistributionManager().getPrimaryLocation(key)
-                        .equals(cache.getRpcManager().getAddress()))
+            .filter(cache -> cache.getDistributionManager().getCacheTopology().getDistribution(key).isWriteBackup())
             .findAny()
             .get());
    }
 
-   public void testDistributionFromNonOwner() throws InterruptedException {
+   public void testDistributionFromNonOwner() throws Exception {
       Object key = "testDistributionFromNonOwner";
       doTestDistribution(key, cacheManagers.stream()
             .map(cm -> cm.<Object, Integer>getCache(DIST).getAdvancedCache())
-            .filter(cache -> !cache.getDistributionManager().getLocality(key).isLocal())
+            .filter(cache -> !cache.getDistributionManager().getCacheTopology().isWriteOwner(key))
             .findAny()
             .get());
    }
 
-   private void doTestDistribution(Object key, AdvancedCache<Object, Integer> originator) throws InterruptedException {
+   private void doTestDistribution(Object key, AdvancedCache<Object, Integer> originator) throws Exception {
       ReadWriteMap<Object, Integer> rw = ReadWriteMapImpl.create(FunctionalMapImpl.create(originator));
 
       // with empty cache:
@@ -79,21 +81,36 @@ public class FunctionalDistributionTest extends AbstractFunctionalTest {
       iterate(key, rw, 2);
    }
 
-   private void iterate(Object key, ReadWriteMap<Object, Integer> rw, int expectedValue) throws InterruptedException {
-      rw.eval(key, entry -> {
-               // we need a small delay so that the value gets committed before the replication finishes:
-               TestingUtil.sleepThread(10);
-               return entry.set(entry.find().orElse(0) + 1);
-            }).join();
+   private void iterate(Object key, ReadWriteMap<Object, Integer> rw, int expectedValue) throws Exception {
+      List<AdvancedCache<Object, Object>> owners = cacheManagers
+            .stream().map(cm -> cm.getCache(DIST).getAdvancedCache())
+            .filter(cache -> cache.getDistributionManager().getCacheTopology().isWriteOwner(key))
+            .collect(Collectors.toList());
 
-      // since we're in async mode, we need to wait before the replication finishes
-      Thread.sleep(100); // TODO: find a better way?
+      CyclicBarrier barrier = new CyclicBarrier(numDistOwners + 1);
+      for (AdvancedCache cache : owners) {
+         BlockingInterceptor bi = new BlockingInterceptor<>(barrier, ReadWriteKeyCommand.class, true, false);
+         cache.getAsyncInterceptorChain().addInterceptorBefore(bi, EntryWrappingInterceptor.class);
+      }
+
+      // While the command execution could be async the BlockingInterceptor would block us on primary owner == originator
+      // On backup == originator the blocking interceptor is not hit until
+      Future<Void> f = fork(() -> rw.eval(key, entry -> entry.set(entry.find().orElse(0) + 1)).join());
+
+      barrier.await(10, TimeUnit.SECONDS);
+      for (AdvancedCache cache : owners) {
+         cache.getAsyncInterceptorChain().findInterceptorWithClass(BlockingInterceptor.class).suspend(true);
+      }
+      barrier.await(10, TimeUnit.SECONDS);
+
+      for (AdvancedCache cache : owners) {
+         cache.getAsyncInterceptorChain().removeInterceptor(BlockingInterceptor.class);
+      }
+
+      f.get(10, TimeUnit.SECONDS);
 
       // we want to ensure that each of the owners executes the function only once:
-      Assert.assertEquals(
-            (Object) cacheManagers.stream()
-                  .map(cm -> cm.<Object, Integer>getCache(DIST).getAdvancedCache())
-                  .filter(cache -> cache.getDistributionManager().getLocality(key).isLocal())
+      Assert.assertEquals(owners.stream()
                   .map(cache -> cache.getDataContainer().get(key).getValue())
                   .collect(Collectors.toList()),
             Collections.nCopies(numDistOwners, expectedValue));
