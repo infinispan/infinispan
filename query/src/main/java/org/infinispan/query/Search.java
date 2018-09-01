b9b0229569be5393f55afbe93a4a0742737fc28e
Adrian Nistor <anistor@redhat.com>
2018-09-02 02:43:24 +0300

    Search/SearchManager/SearchManagerImpl javadoc

--- query/src/main/java/org/infinispan/query/Search.java
+++ query/src/main/java/org/infinispan/query/Search.java
@@ -22,7 +22,10 @@ import org.infinispan.security.AuthorizationPermission;
 import org.infinispan.util.logging.LogFactory;
 
 /**
- * Helper class to get a SearchManager out of an indexing enabled cache.
+ * This is the entry point for the Infinispan query API. It's allows you the locate the {@link SearchManager} for a
+ * cache and start building Lucene queries (with or without the help of Hibernate Search DSL) for indexed caches. It
+ * also provides the {@link QueryFactory} which is your starting point for building DSL-based or query string based
+ * Ickle queries, continuous queries and event filters, for both indexed and unindexed caches.
  *
  * @author Sanne Grinovero <sanne@hibernate.org> (C) 2011 Red Hat Inc.
  * @author anistor@redhat.com
@@ -32,21 +35,34 @@ public final class Search {
    private static final Log log = LogFactory.getLog(Search.class, Log.class);
 
    private Search() {
+      // prevent instantiation
    }
 
+   /**
+    * Create an event filter out of an Ickle query string.
+    */
    public static <K, V> CacheEventFilterConverter<K, V, ObjectFilter.FilterResult> makeFilter(String queryString) {
       return makeFilter(queryString, null);
    }
 
+   /**
+    * Create an event filter out of an Ickle query string.
+    */
    public static <K, V> CacheEventFilterConverter<K, V, ObjectFilter.FilterResult> makeFilter(String queryString, Map<String, Object> namedParameters) {
       IckleFilterAndConverter<K, V> filterAndConverter = new IckleFilterAndConverter<>(queryString, namedParameters, ReflectionMatcher.class);
       return new IckleCacheEventFilterConverter<>(filterAndConverter);
    }
 
+   /**
+    * Create an event filter out of an Ickle query.
+    */
    public static <K, V> CacheEventFilterConverter<K, V, ObjectFilter.FilterResult> makeFilter(Query query) {
       return makeFilter(query.getQueryString(), query.getParameters());
    }
 
+   /**
+    * Obtain the query factory for building DSL based Ickle queries.
+    */
    public static QueryFactory getQueryFactory(Cache<?, ?> cache) {
       if (cache == null || cache.getAdvancedCache() == null) {
          throw new IllegalArgumentException("cache parameter shall not be null");
@@ -60,10 +76,16 @@ public final class Search {
       return new EmbeddedQueryFactory(queryEngine);
    }
 
+   /**
+    * Obtain the {@link ContinuousQuery} object for a cache.
+    */
    public static <K, V> ContinuousQuery<K, V> getContinuousQuery(Cache<K, V> cache) {
       return new ContinuousQueryImpl<>(cache);
    }
 
+   /**
+    * Obtain the {@link SearchManager} object for a cache.
+    */
    public static SearchManager getSearchManager(Cache<?, ?> cache) {
       if (cache == null || cache.getAdvancedCache() == null) {
          throw new IllegalArgumentException("cache parameter shall not be null");
@@ -73,7 +95,7 @@ public final class Search {
       return new SearchManagerImpl(advancedCache);
    }
 
-   private static void ensureAccessPermissions(final AdvancedCache<?, ?> cache) {
+   private static void ensureAccessPermissions(AdvancedCache<?, ?> cache) {
       AuthorizationManager authorizationManager = SecurityActions.getCacheAuthorizationManager(cache);
       if (authorizationManager != null) {
          authorizationManager.checkPermission(AuthorizationPermission.BULK_READ);
