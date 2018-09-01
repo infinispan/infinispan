23431d6d72146fd6a9dd38b09ba49a3edc416611
Adrian Nistor <anistor@redhat.com>
2018-09-02 02:11:09 +0300

    ISPN-9504 Refactor Transformer and friends
    * Transformable.transformer should use generics - the compiler bug that prevented this ages ago is no longer relevant

--- query/src/main/java/org/infinispan/query/Transformer.java
+++ query/src/main/java/org/infinispan/query/Transformer.java
@@ -1,16 +1,28 @@
 package org.infinispan.query;
 
 /**
- * The task of this interface is to convert keys from their original types to a String representation (which can be
- * used in Lucene) and vice versa.
+ * Convert objects (cache keys only) from their original Java types to a String representation (which is suitable to be
+ * used in a Lucene index) and vice versa. Transformers are needed only for custom types. Primitive types (boxed),
+ * java.lang.String and byte arrays are internally handled without the need of a user-supplied Transformer.
  *
  * @author Manik Surtani
  * @since 4.0
  */
 public interface Transformer {
 
-   Object fromString(String s);
+   /**
+    * Transforms a String into an Object.
+    *
+    * @param str cannot be null
+    * @return the Object that is encoded in the given String
+    */
+   Object fromString(String str);
 
-   String toString(Object customType);
-
+   /**
+    * Transforms an Object into a String.
+    *
+    * @param obj cannot be null
+    * @return the String representation of the object
+    */
+   String toString(Object obj);
 }
