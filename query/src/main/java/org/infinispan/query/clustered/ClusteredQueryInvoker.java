95f9c86d37f33b295cd1c22cccccd570eebe6fe1
Adrian Nistor <anistor@redhat.com>
2018-09-03 16:32:26 +0300

    Remove the spider webs from ClusteredQueryInvoker

--- query/src/main/java/org/infinispan/query/clustered/ClusteredQueryInvoker.java
+++ query/src/main/java/org/infinispan/query/clustered/ClusteredQueryInvoker.java
@@ -1,12 +1,11 @@
 package org.infinispan.query.clustered;
 
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
@@ -28,7 +27,7 @@ import org.infinispan.remoting.transport.Address;
  * @author Sanne Grinovero <sanne@infinispan.org> (C) 2011 Red Hat Inc.
  * @since 5.1
  */
-public class ClusteredQueryInvoker {
+final class ClusteredQueryInvoker {
 
    private final RpcManager rpcManager;
    private final Cache<?, ?> localCacheInstance;
@@ -37,50 +36,43 @@ public class ClusteredQueryInvoker {
    private final RpcOptions rpcOptions;
 
    ClusteredQueryInvoker(Cache<?, ?> localCacheInstance, ExecutorService asyncExecutor) {
-      this.asyncExecutor = asyncExecutor;
-      this.rpcManager = localCacheInstance.getAdvancedCache().getComponentRegistry().getLocalComponent(RpcManager.class);
       this.localCacheInstance = localCacheInstance;
-      this.myAddress = localCacheInstance.getAdvancedCache().getRpcManager().getAddress();
+      this.asyncExecutor = asyncExecutor;
+      this.rpcManager = localCacheInstance.getAdvancedCache().getRpcManager();
+      this.myAddress = rpcManager.getAddress();
       this.rpcOptions = rpcManager.getRpcOptionsBuilder(ResponseMode.SYNCHRONOUS).timeout(10000, TimeUnit.MILLISECONDS).build();
    }
 
    /**
     * Retrieves the value (using doc index) in a remote query instance
     *
-    * @param doc
-    *           Doc index of the value on remote query
-    * @param address
-    *           Address of the node who has the value
-    * @param queryId
-    *           Id of the query
+    * @param doc     Doc index of the value on remote query
+    * @param address Address of the node who has the value
+    * @param queryId Id of the query
     * @return The value of index doc of the query with queryId on node at address
     */
    public Object getValue(int doc, Address address, UUID queryId) {
       ClusteredQueryCommand clusteredQuery = ClusteredQueryCommand.retrieveKeyFromLazyQuery(
-               localCacheInstance, queryId, doc);
+            localCacheInstance, queryId, doc);
 
       if (address.equals(myAddress)) {
          Future<QueryResponse> localResponse = localInvoke(clusteredQuery);
          try {
             return localResponse.get().getFetchedValue();
          } catch (InterruptedException e) {
-            throw new SearchException("interrupted while searching locally", e);
+            throw new SearchException("Interrupted while searching locally", e);
          } catch (ExecutionException e) {
             throw new SearchException("Exception while searching locally", e);
          }
       } else {
-         List<Address> addresss = new ArrayList<Address>(1);
-         addresss.add(address);
-
-         Map<Address, Response> responses = rpcManager.invokeRemotely(addresss, clusteredQuery, rpcOptions);
+         Map<Address, Response> responses = rpcManager.invokeRemotely(Collections.singletonList(address), clusteredQuery, rpcOptions);
          List<QueryResponse> objects = cast(responses);
          return objects.get(0).getFetchedValue();
       }
    }
 
    /**
-    * Broadcast this ClusteredQueryCommand to all cluster nodes. The command will be also invoked on
-    * local node.
+    * Broadcast this ClusteredQueryCommand to all cluster nodes. The command will be also invoked on local node.
     *
     * @param clusteredQuery
     * @return A list with all responses
@@ -104,13 +96,17 @@ public class ClusteredQueryInvoker {
    }
 
    private Future<QueryResponse> localInvoke(ClusteredQueryCommand clusteredQuery) {
-      ClusteredQueryCallable clusteredQueryCallable = new ClusteredQueryCallable(clusteredQuery,
-               localCacheInstance);
-      return asyncExecutor.submit(clusteredQueryCallable);
+      return asyncExecutor.submit(() -> {
+         try {
+            return clusteredQuery.perform(localCacheInstance);
+         } catch (Throwable e) {
+            throw new SearchException(e);
+         }
+      });
    }
 
    private List<QueryResponse> cast(Map<Address, Response> responses) {
-      List<QueryResponse> objects = new LinkedList<QueryResponse>();
+      List<QueryResponse> objects = new LinkedList<>();
       for (Entry<Address, Response> pair : responses.entrySet()) {
          Response resp = pair.getValue();
          if (resp instanceof SuccessfulResponse) {
@@ -120,36 +116,6 @@ public class ClusteredQueryInvoker {
             throw new SearchException("Unexpected response: " + resp);
          }
       }
-
       return objects;
    }
-
-   /**
-    * Created to call a ClusteredQueryCommand on own node.
-    *
-    * @author Israel Lacerra <israeldl@gmail.com>
-    * @since 5.1
-    */
-   private static final class ClusteredQueryCallable implements Callable<QueryResponse> {
-
-      private final ClusteredQueryCommand clusteredQuery;
-
-      private final Cache<?, ?> localInstance;
-
-      ClusteredQueryCallable(ClusteredQueryCommand clusteredQuery, Cache<?, ?> localInstance) {
-         this.clusteredQuery = clusteredQuery;
-         this.localInstance = localInstance;
-      }
-
-      @Override
-      public QueryResponse call() throws Exception {
-         try {
-            return clusteredQuery.perform(localInstance);
-         } catch (Throwable e) {
-            throw new Exception(e);
-         }
-      }
-
-   }
-
 }
