71f3f1b4b96d239a9441f91645916f7bcbb7133f
Adrian Nistor <anistor@redhat.com>
2018-09-05 11:17:09 +0300

    Revise javadoc of org.infinispan.configuration.cache.Index enum

--- core/src/main/java/org/infinispan/configuration/cache/Index.java
+++ core/src/main/java/org/infinispan/configuration/cache/Index.java
@@ -1,20 +1,38 @@
 package org.infinispan.configuration.cache;
 
 /**
- * Used to configure indexing of entries in the cache for searching.
+ * Used to configure indexing of entries in the cache for index-assisted searching.
  *
  * @author Paul Ferraro
  */
 public enum Index {
-   NONE,
+
    /**
-    * Use PRIMARY_OWNER instead
+    * No indexing is performed.
+    */
+   NONE,
+
+   /**
+    * The local (originator) node is responsible for adding the entry to the index.
+    * This indexing mode is deprecated; use {@link #PRIMARY_OWNER} instead.
     * @deprecated since 9.1
     */
-   @Deprecated LOCAL,
+   @Deprecated
+   LOCAL,
+
+   /**
+    * All cluster nodes will add the entry to the index.
+    */
    ALL,
+
+   /**
+    * Only the primary owner of an entry will add it to the index.
+    */
    PRIMARY_OWNER;
 
+   /**
+    * Is indexing enabled?
+    */
    public boolean isEnabled() {
       return this != NONE;
    }
@@ -27,5 +45,4 @@ public enum Index {
    public boolean isPrimaryOwner() {
       return this == PRIMARY_OWNER;
    }
-
 }
