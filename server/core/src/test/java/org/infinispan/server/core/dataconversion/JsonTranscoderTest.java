215e7deac4d4702c1a668c3ec33ce212d0c415bc
Gustavo Fernandes <gustavonalle@gmail.com>
2018-09-11 13:45:50 +0100

    ISPN-9499 Json to text conversion ignore charset

--- server/core/src/test/java/org/infinispan/server/core/dataconversion/JsonTranscoderTest.java
+++ server/core/src/test/java/org/infinispan/server/core/dataconversion/JsonTranscoderTest.java
@@ -1,8 +1,14 @@
 package org.infinispan.server.core.dataconversion;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_JSON;
+import static org.infinispan.commons.dataconversion.MediaType.TEXT_PLAIN;
+import static org.infinispan.commons.dataconversion.StandardConversions.convertCharset;
 import static org.infinispan.server.core.dataconversion.JsonTranscoder.TYPE_PROPERTY;
 import static org.testng.Assert.assertEquals;
+import static org.testng.internal.junit.ArrayAsserts.assertArrayEquals;
 
+import java.nio.charset.Charset;
 import java.util.Collections;
 
 import org.infinispan.commons.configuration.ClassWhiteList;
@@ -32,7 +38,7 @@ public class JsonTranscoderTest extends AbstractTranscoderTest {
 
    @Override
    public void testTranscoderTranscode() throws Exception {
-      MediaType jsonMediaType = MediaType.APPLICATION_JSON;
+      MediaType jsonMediaType = APPLICATION_JSON;
       MediaType personMediaType = MediaType.fromString("application/x-java-object;type=org.infinispan.test.data.Person");
 
       Object result = transcoder.transcode(dataSrc, personMediaType, jsonMediaType);
@@ -50,4 +56,46 @@ public class JsonTranscoderTest extends AbstractTranscoderTest {
       assertEquals(fromJson, dataSrc);
    }
 
+   @Test
+   public void testCharset() {
+      Charset korean = Charset.forName("EUC-KR");
+      MediaType textPlainKorean = TEXT_PLAIN.withCharset(korean);
+      MediaType jsonKorean = APPLICATION_JSON.withCharset(korean);
+      MediaType textPlainAsString = TEXT_PLAIN.withClassType(String.class);
+      MediaType jsonAsString = APPLICATION_JSON.withClassType(String.class);
+
+      String content = "{\"city_kr\":\"서울\"}";
+      byte[] contentUTF = content.getBytes(UTF_8);
+      byte[] contentKorean = convertCharset(contentUTF, UTF_8, korean);
+
+      byte[] result = (byte[]) transcoder.transcode(contentKorean, jsonKorean, TEXT_PLAIN);
+      assertArrayEquals(result, contentUTF);
+
+      String strResult = (String) transcoder.transcode(contentKorean, jsonKorean, textPlainAsString);
+      assertEquals(strResult, content);
+
+      result = (byte[]) transcoder.transcode(contentKorean, jsonKorean, textPlainKorean);
+      assertArrayEquals(result, contentKorean);
+
+      result = (byte[]) transcoder.transcode(contentKorean, textPlainKorean, APPLICATION_JSON);
+      assertArrayEquals(result, contentUTF);
+
+      strResult = (String) transcoder.transcode(contentKorean, jsonKorean, jsonAsString);
+      assertEquals(strResult, content);
+
+      result = (byte[]) transcoder.transcode(contentKorean, textPlainKorean, jsonKorean);
+      assertArrayEquals(result, contentKorean);
+
+      result = (byte[]) transcoder.transcode(contentUTF, TEXT_PLAIN, jsonKorean);
+      assertArrayEquals(result, contentKorean);
+
+      result = (byte[]) transcoder.transcode(content, textPlainAsString, jsonKorean);
+      assertArrayEquals(result, contentKorean);
+
+      result = (byte[]) transcoder.transcode(contentUTF, APPLICATION_JSON, TEXT_PLAIN);
+      assertArrayEquals(result, contentUTF);
+
+      result = (byte[]) transcoder.transcode(contentUTF, APPLICATION_JSON, textPlainKorean);
+      assertArrayEquals(result, contentKorean);
+   }
 }
