2126454d802c42e912cbd704c1a414b1e5c9b2f5
Adrian Nistor <anistor@redhat.com>
2018-10-02 01:07:06 +0300

    Refactor IndexingConfiguration

--- core/src/main/java/org/infinispan/configuration/cache/IndexOverlay.java
+++ core/src/main/java/org/infinispan/configuration/cache/IndexOverlay.java
@@ -3,7 +3,7 @@ package org.infinispan.configuration.cache;
 import org.infinispan.commons.util.TypedProperties;
 
 /**
- * Holds default configurations about indexing
+ * Holds indexing default configurations to be used for auto-configuration.
  *
  * @author gustavonalle
  * @since 7.0
@@ -17,12 +17,11 @@ enum IndexOverlay {
    NON_DISTRIBUTED_FS {
       @Override
       void apply(TypedProperties properties) {
-         properties.putIfAbsent(Constants.DIRECTORY_PROVIDER, "filesystem");
-         properties.putIfAbsent(Constants.EXCLUSIVE_INDEX_USE, "true");
-         properties.putIfAbsent(Constants.INDEX_MANAGER, "near-real-time");
-         properties.putIfAbsent(Constants.READER_STRATEGY, "shared");
+         properties.putIfAbsent(DIRECTORY_PROVIDER, "filesystem");
+         properties.putIfAbsent(EXCLUSIVE_INDEX_USE, "true");
+         properties.putIfAbsent(INDEX_MANAGER, "near-real-time");
+         properties.putIfAbsent(READER_STRATEGY, "shared");
       }
-
    },
 
    /**
@@ -33,25 +32,22 @@ enum IndexOverlay {
    DISTRIBUTED_INFINISPAN {
       @Override
       void apply(TypedProperties properties) {
-         properties.putIfAbsent(Constants.DIRECTORY_PROVIDER, "infinispan");
-         properties.putIfAbsent(Constants.EXCLUSIVE_INDEX_USE, "true");
-         properties.putIfAbsent(Constants.INDEX_MANAGER, "org.infinispan.query.indexmanager.InfinispanIndexManager");
-         properties.putIfAbsent(Constants.READER_STRATEGY, "shared");
+         properties.putIfAbsent(DIRECTORY_PROVIDER, "infinispan");
+         properties.putIfAbsent(EXCLUSIVE_INDEX_USE, "true");
+         properties.putIfAbsent(INDEX_MANAGER, "org.infinispan.query.indexmanager.InfinispanIndexManager");
+         properties.putIfAbsent(READER_STRATEGY, "shared");
       }
-
    };
 
+   private static final String DIRECTORY_PROVIDER = "hibernate.search.default.directory_provider";
+   private static final String EXCLUSIVE_INDEX_USE = "hibernate.search.default.exclusive_index_use";
+   private static final String INDEX_MANAGER = "hibernate.search.default.indexmanager";
+   private static final String READER_STRATEGY = "hibernate.search.default.reader.strategy";
+
    /**
-    * Applies pre-defined configurations
+    * Applies pre-defined configurations.
     *
-    * @param properties existing Properties
+    * @param properties existing TypedProperties
     */
    abstract void apply(TypedProperties properties);
-
-   private static class Constants {
-      static final String DIRECTORY_PROVIDER = "hibernate.search.default.directory_provider";
-      static final String EXCLUSIVE_INDEX_USE = "hibernate.search.default.exclusive_index_use";
-      static final String INDEX_MANAGER = "hibernate.search.default.indexmanager";
-      static final String READER_STRATEGY = "hibernate.search.default.reader.strategy";
-   }
 }
