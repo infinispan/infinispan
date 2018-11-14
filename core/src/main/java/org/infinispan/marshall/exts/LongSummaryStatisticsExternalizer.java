c73cf0409e16e102d3366ca9cfac9ff57697b041
Tomas Hofman <thofman@redhat.com>
2018-11-14 14:49:48 +0100

    ISPN-9720 JDK11: An illegal reflective access operation has occurred

--- core/src/main/java/org/infinispan/marshall/exts/LongSummaryStatisticsExternalizer.java
+++ core/src/main/java/org/infinispan/marshall/exts/LongSummaryStatisticsExternalizer.java
@@ -3,6 +3,7 @@ package org.infinispan.marshall.exts;
 import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectOutput;
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.LongSummaryStatistics;
 import java.util.Set;
@@ -26,15 +27,30 @@ public class LongSummaryStatisticsExternalizer extends AbstractExternalizer<Long
    static final Field maxField;
 
    static final boolean canSerialize;
+   static final Constructor<LongSummaryStatistics> constructor;
 
    static {
-      countField = SecurityActions.getField(LongSummaryStatistics.class, "count");
-      sumField = SecurityActions.getField(LongSummaryStatistics.class, "sum");
-      minField = SecurityActions.getField(LongSummaryStatistics.class, "min");
-      maxField = SecurityActions.getField(LongSummaryStatistics.class, "max");
+      constructor = SecurityActions
+              .getConstructor(LongSummaryStatistics.class, long.class, long.class, long.class, long.class);
 
-      // We can only properly serialize if all of the fields are non null
-      canSerialize = countField != null && sumField != null && minField != null && maxField != null;
+      if (constructor != null) {
+         // since JDK 10, *SummaryStatistics have parametrized constructors, so reflection can be avoided
+
+         countField = null;
+         sumField = null;
+         minField = null;
+         maxField = null;
+
+         canSerialize = true;
+      } else {
+         countField = SecurityActions.getField(LongSummaryStatistics.class, "count");
+         sumField = SecurityActions.getField(LongSummaryStatistics.class, "sum");
+         minField = SecurityActions.getField(LongSummaryStatistics.class, "min");
+         maxField = SecurityActions.getField(LongSummaryStatistics.class, "max");
+
+         // We can only properly serialize if all of the fields are non null
+         canSerialize = countField != null && sumField != null && minField != null && maxField != null;
+      }
    }
 
    @Override
@@ -51,29 +67,41 @@ public class LongSummaryStatisticsExternalizer extends AbstractExternalizer<Long
    @Override
    public void writeObject(ObjectOutput output, LongSummaryStatistics object) throws IOException {
       verifySerialization();
-      try {
-         output.writeLong(countField.getLong(object));
-         output.writeLong(sumField.getLong(object));
-         output.writeLong(minField.getLong(object));
-         output.writeLong(maxField.getLong(object));
-      } catch (IllegalAccessException e) {
-         // This can't happen as we force accessibility in the getField
-         throw new IOException(e);
-      }
+      output.writeLong(object.getCount());
+      output.writeLong(object.getSum());
+      output.writeLong(object.getMin());
+      output.writeLong(object.getMax());
    }
 
    @Override
    public LongSummaryStatistics readObject(ObjectInput input) throws IOException, ClassNotFoundException {
       verifySerialization();
-      LongSummaryStatistics summaryStatistics = new LongSummaryStatistics();
-      try {
-         countField.setLong(summaryStatistics, input.readLong());
-         sumField.setLong(summaryStatistics, input.readLong());
-         minField.setLong(summaryStatistics, input.readLong());
-         maxField.setLong(summaryStatistics, input.readLong());
-      } catch (IllegalAccessException e) {
-         // This can't happen as we force accessibility in the getField
-         throw new IOException(e);
+      final LongSummaryStatistics summaryStatistics;
+
+      final long count = input.readLong();
+      final long sum = input.readLong();
+      final long min = input.readLong();
+      final long max = input.readLong();
+
+      if (constructor != null) {
+         // JDK 10+, pass values to constructor
+         try {
+            summaryStatistics = constructor.newInstance(count, min, max, sum);
+         } catch (ReflectiveOperationException e) {
+            throw new IOException(e);
+         }
+      } else {
+         // JDK 9 or older, fall back to reflection
+         summaryStatistics = new LongSummaryStatistics();
+         try {
+            countField.setLong(summaryStatistics, count);
+            sumField.setLong(summaryStatistics, sum);
+            minField.setLong(summaryStatistics, min);
+            maxField.setLong(summaryStatistics, max);
+         } catch (IllegalAccessException e) {
+            // This can't happen as we force accessibility in the getField
+            throw new IOException(e);
+         }
       }
       return summaryStatistics;
    }
