6cb3d56b4940bd918d74f776d00368dfc07fc974
Don Naro <dnaro@redhat.com>
2018-12-03 14:07:53 +0000

    ISPN-9788 edits to obtaining cache docs

--- core/src/main/java/org/infinispan/globalstate/ScopeFilter.java
+++ core/src/main/java/org/infinispan/globalstate/ScopeFilter.java
@@ -11,6 +11,9 @@ import org.infinispan.commons.marshall.AdvancedExternalizer;
 import org.infinispan.commons.marshall.Ids;
 import org.infinispan.filter.KeyFilter;
 
+/**
+ * A filter for {@link ScopedState} that allows listeners of the global state cache to choose events by scope.
+ */
 public class ScopeFilter implements KeyFilter<ScopedState>, Predicate<ScopedState> {
    private final String scope;
 
