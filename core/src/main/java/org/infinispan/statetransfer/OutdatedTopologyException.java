bc891b17d79bbaada53e8e3d5188a10dfb11d006
Dan Berindei <dan@infinispan.org>
2018-12-04 12:05:42 +0200

    ISPN-7889 Introduce OutdatedTopologyException.RETRY_SAME_TOPOLOGY
    And rename OTE.INSTANCE to OTE.RETRY_NEXT_TOPOLOGY

--- core/src/main/java/org/infinispan/statetransfer/OutdatedTopologyException.java
+++ core/src/main/java/org/infinispan/statetransfer/OutdatedTopologyException.java
@@ -3,11 +3,12 @@ package org.infinispan.statetransfer;
 import org.infinispan.commons.CacheException;
 
 /**
- * An exception signalling that a command should be retried because it was executed with an outdated
- * topology.
- * <p>
- * This can happen for non-tx caches, if the primary owner doesn't respond (either because it left the
- * cluster or because this particular cache is no longer running).
+ * An exception signalling that a command should be retried because a newer topology was seen during execution.
+ *
+ * <p>Most of the time, read commands can be retried in the same topology, see {@link #RETRY_SAME_TOPOLOGY}.</p>
+ *
+ * <p>This exception can be thrown very often, so it has not stack trace information, and using the constants
+ * is preferred.</p>
  *
  * @author Dan Berindei
  * @since 6.0
@@ -16,31 +17,36 @@ public class OutdatedTopologyException extends CacheException {
    public final int requestedTopologyId;
 
    /**
-    * A cached instance of {@link OutdatedTopologyException}.
-    * <p>
-    * This exception has not stack trace information and it should be used internally to notify a topology change while
-    * handle a command.
-    * <p>
-    * It avoids the cost associated to create and collect the stack when it isn't needed.
+    * A cached instance that requests the command's topology id + 1.
     */
    @SuppressWarnings("ThrowableInstanceNeverThrown")
-   public static final OutdatedTopologyException INSTANCE = new OutdatedTopologyException();
+   public static final OutdatedTopologyException RETRY_NEXT_TOPOLOGY =
+      new OutdatedTopologyException("Retry in the next topology", -1);
 
-   private OutdatedTopologyException() {
-      super("Topology changed while handling command", null, false, false);
-      requestedTopologyId = -1;
-   }
+   /**
+    * A cached instance, used for read commands that need to be retried in the same topology.
+    *
+    * <p>This happens because we read from backup owners when the primary owners no longer have the entry,
+    * so we only retry when all of the owners reply with an UnsureResponse.
+    * Topologies T and T+1 always have at least one read owner in common, so receiving UnsureResponse from all the
+    * owners means either one owner had topology T+2 and by now we have at least T+1, or one owner had topology T-1
+    * and another had T+1, and by now all should have at least T.</p>
+    */
+   public static final OutdatedTopologyException RETRY_SAME_TOPOLOGY =
+      new OutdatedTopologyException("Retry command in the same topology", 0);
 
-   public OutdatedTopologyException(String msg) {
+   /**
+    * Request an explicit topology id and use a custom message.
+    */
+   public OutdatedTopologyException(String msg, int requestedTopologyId) {
       super(msg, null, false, false);
-      requestedTopologyId = -1;
+      this.requestedTopologyId = requestedTopologyId;
    }
 
    /**
     * Request retrying the command in explicitly set topology (or later one).
     */
    public OutdatedTopologyException(int requestedTopologyId) {
-      super(null, null, false, false);
-      this.requestedTopologyId = requestedTopologyId;
+      this(null, requestedTopologyId);
    }
 }
