41e83844c3a4ec216d6506e41e009fb2479f204c
Tristan Tarrant <ttarrant@redhat.com>
2018-12-13 11:58:34 +0100

    ISPN-9823 Include JavaDocs for org.infinispan.globalstate

--- core/src/main/java/org/infinispan/globalstate/ConfigurationStorage.java
+++ core/src/main/java/org/infinispan/globalstate/ConfigurationStorage.java
@@ -1,5 +1,7 @@
 package org.infinispan.globalstate;
 
+import org.infinispan.configuration.global.GlobalStateConfiguration;
+
 /**
  * Configuration storage
  *
@@ -9,23 +11,24 @@ package org.infinispan.globalstate;
 
 public enum ConfigurationStorage {
    /**
-    * An immutable configuration storage provider. This forbids creating/removing caches
+    * An immutable configuration storage provider. This forbids creating/removing caches.
     */
    IMMUTABLE,
    /**
-    * A volatile configuration storage provider which doesn't support {@link org.infinispan.commons.api.CacheContainerAdmin.AdminFlag#PERMANENT}
+    * A volatile configuration storage provider which doesn't support {@link org.infinispan.commons.api.CacheContainerAdmin.AdminFlag#PERMANENT}.
     */
    VOLATILE,
    /**
-    * A configuration storage provider which stores configurations on the filesystem
+    * A configuration storage provider which stores configurations on the filesystem.
+    * Cache configurations are persisted to the {@link GlobalStateConfiguration#persistentLocation()} in a <pre>caches.xml</pre> file which is read on startup.
     */
    OVERLAY,
    /**
-    * A configuration storage provider which stores configurations in a managed environment (e.g. the server model)
+    * A configuration storage provider which stores configurations in a managed environment, for example the server model.
     */
    MANAGED,
    /**
-    * A custom configuration storage provider
+    * A custom configuration storage provider. Providers must implement the {@link LocalConfigurationStorage} interface.
     */
    CUSTOM,
 }
