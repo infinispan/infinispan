838010af52068236d8545430b905f35ee2974de7
William Burns <mudokonman@gmail.com>
2019-01-17 09:08:14 -0500

    ISPN-9784 Replace DistributedExecutor with ClusterExecutor for distributed tasks

--- tasks/scripting/src/main/java/org/infinispan/scripting/impl/DistributedRunner.java
+++ tasks/scripting/src/main/java/org/infinispan/scripting/impl/DistributedRunner.java
@@ -1,15 +1,18 @@
 package org.infinispan.scripting.impl;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import org.infinispan.Cache;
-import org.infinispan.distexec.DefaultExecutorService;
+import org.infinispan.commons.CacheException;
+import org.infinispan.manager.ClusterExecutor;
+import org.infinispan.remoting.transport.Address;
 import org.infinispan.scripting.logging.Log;
-import org.infinispan.util.concurrent.CompletableFutures;
+import org.infinispan.util.function.TriConsumer;
 import org.infinispan.util.logging.LogFactory;
 
 /**
@@ -31,15 +34,21 @@ public class DistributedRunner implements ScriptRunner {
       if (masterCacheNode == null) {
          throw log.distributedTaskNeedCacheInBinding(metadata.name());
       }
-      DefaultExecutorService des = new DefaultExecutorService(masterCacheNode);
-      try {
-         Map<String, Object> ctxParams = extractContextParams(metadata, binding);
-         List<CompletableFuture<T>> tasks = des.submitEverywhere(new DistributedScript<T>(metadata, ctxParams));
+      Map<String, Object> ctxParams = extractContextParams(metadata, binding);
+      ClusterExecutor clusterExecutor = masterCacheNode.getCacheManager().executor();
+      List<T> results = new ArrayList<>();
+      TriConsumer<Address, T, Throwable> triConsumer = (a, v, t) -> {
+         if (t != null) {
+            throw new CacheException(t);
+         }
+         synchronized (this) {
+            results.add(v);
+         }
+      };
+      CompletableFuture<Void> future = clusterExecutor.submitConsumer(new DistributedScript<T>(masterCacheNode.getName(),
+                  metadata, ctxParams), triConsumer);
 
-         return (CompletableFuture<T>) CompletableFutures.sequence(tasks);
-      } finally {
-         des.shutdown();
-      }
+      return (CompletableFuture<T>) future.thenApply(ignore -> results);
    }
 
    private Map<String, Object> extractContextParams(ScriptMetadata metadata, CacheScriptBindings binding) {
