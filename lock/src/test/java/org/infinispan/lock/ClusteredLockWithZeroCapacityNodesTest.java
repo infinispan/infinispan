63bf4fa635e75adb20461e8ee2fb918add43ef6e
Katia Aresti <karesti@redhat.com>
2019-01-17 13:34:24 +0100

    ISPN-9699 Global 0.0f Capacity Factor

---
+++ lock/src/test/java/org/infinispan/lock/ClusteredLockWithZeroCapacityNodesTest.java
@@ -0,0 +1,24 @@
+package org.infinispan.lock;
+
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.global.GlobalConfigurationBuilder;
+import org.testng.annotations.Test;
+
+@Test(groups = "functional", testName = "clusteredLock.ClusteredLockWithZeroCapacityNodesTest")
+public class ClusteredLockWithZeroCapacityNodesTest extends ClusteredLockTest {
+
+   public ClusteredLockWithZeroCapacityNodesTest() {
+      super();
+      numOwner = 1;
+      cacheMode = CacheMode.DIST_SYNC;
+   }
+
+   protected int clusterSize() {
+      return 3;
+   }
+
+   @Override
+   protected GlobalConfigurationBuilder configure(int nodeId) {
+      return super.configure(nodeId).zeroCapacityNode(nodeId % 2 == 1);
+   }
+}
