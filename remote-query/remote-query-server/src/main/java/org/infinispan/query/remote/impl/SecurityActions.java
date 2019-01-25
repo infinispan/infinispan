1e9acbe41777660f42ffd8a9d4a667023b2e6407
Tristan Tarrant <ttarrant@redhat.com>
2019-01-25 09:04:43 +0100

    ISPN-7333 Unwrap protobuf cache security for internal uses

--- remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/SecurityActions.java
+++ remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/SecurityActions.java
@@ -8,6 +8,7 @@ import org.infinispan.Cache;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.security.AuthorizationManager;
 import org.infinispan.security.Security;
+import org.infinispan.security.impl.SecureCacheImpl;
 
 /**
  * SecurityActions for the org.infinispan.query.remote.impl package. Do not move and do not change class and method
@@ -34,7 +35,15 @@ final class SecurityActions {
       return doPrivileged(cache::getAuthorizationManager);
    }
 
-   static <K, V> Cache<K, V> getCache(EmbeddedCacheManager cacheManager, String cacheName) {
-      return doPrivileged(() -> cacheManager.getCache(cacheName));
+   static <K, V> Cache<K, V> getUnwrappedCache(EmbeddedCacheManager cacheManager, String cacheName) {
+      return doPrivileged(() -> {
+         Cache<K, V> cache = cacheManager.getCache(cacheName);
+         if (cache instanceof SecureCacheImpl) {
+            return ((SecureCacheImpl) cache).getDelegate();
+         } else {
+            return cache;
+         }
+      });
+
    }
 }
