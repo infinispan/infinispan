35b522d459bfdd3a2552abf7b8db31b9dce5dd54
Ryan Emerson <remerson@redhat.com>
2019-01-31 15:13:56 +0000

    ISPN-9916 JDBC Publish Flowable should explicitly close all ResultSets

--- persistence/jdbc/src/main/java/org/infinispan/persistence/jdbc/stringbased/JdbcStringBasedStore.java
+++ persistence/jdbc/src/main/java/org/infinispan/persistence/jdbc/stringbased/JdbcStringBasedStore.java
@@ -527,7 +527,7 @@ public class JdbcStringBasedStore<K,V> implements SegmentedAdvancedLoadWriteStor
          }
          ps.setFetchSize(tableManager.getFetchSize());
          ResultSet rs = ps.executeQuery();
-         return function.apply(rs);
+         return function.apply(rs).doOnComplete(() -> JdbcUtil.safeClose(rs));
       }, FlowableConnection::close);
    }
 
