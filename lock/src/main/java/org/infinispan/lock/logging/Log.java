e95837c90ee2b35e9dd8a50f535c8b613a0dbbfd
Katia Aresti <karesti@redhat.com>
2019-02-05 22:35:16 +0100

    ISPN-9921 Simple warning instead of error with zero capacity node

--- lock/src/main/java/org/infinispan/lock/logging/Log.java
+++ lock/src/main/java/org/infinispan/lock/logging/Log.java
@@ -39,6 +39,4 @@ public interface Log extends BasicLogger {
    @Message(value = "Invalid scope for tag <clustered-lock>. Expected CACHE_CONTAINER but was %s", id = 29007)
    ClusteredLockException invalidScope(ParserScope scope);
 
-   @Message(value = "When the node is configured as a zero-capacity node, you need to specify the number of owners for the lock", id = 29008)
-   ClusteredLockException zeroCapacityNodeError();
 }
