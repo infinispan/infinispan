5fa9a98e687f317fc9ac604f6d328dc2a61130d6
Dan Berindei <dan@infinispan.org>
2019-02-05 09:07:02 +0200

    ISPN-9933 Cache components are not stopped after startup error
    Change DefaultCacheManager to stop failed caches
    and remove the static field from NonStringKeyPreloadTest

--- persistence/jdbc/src/test/java/org/infinispan/persistence/jdbc/stringbased/NonStringKeyPreloadTest.java
+++ persistence/jdbc/src/test/java/org/infinispan/persistence/jdbc/stringbased/NonStringKeyPreloadTest.java
@@ -4,24 +4,11 @@ import static org.infinispan.test.TestingUtil.clearCacheLoader;
 import static org.infinispan.test.TestingUtil.withCacheManager;
 import static org.testng.AssertJUnit.assertEquals;
 
-import java.sql.Connection;
-
 import org.infinispan.AdvancedCache;
 import org.infinispan.Cache;
 import org.infinispan.commons.CacheException;
-import org.infinispan.commons.configuration.BuiltBy;
-import org.infinispan.commons.configuration.attributes.AttributeSet;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
-import org.infinispan.configuration.global.GlobalConfiguration;
-import org.infinispan.persistence.jdbc.configuration.AbstractJdbcStoreConfigurationBuilder;
-import org.infinispan.persistence.jdbc.configuration.ConnectionFactoryConfiguration;
-import org.infinispan.persistence.jdbc.configuration.ConnectionFactoryConfigurationBuilder;
 import org.infinispan.persistence.jdbc.configuration.JdbcStringBasedStoreConfigurationBuilder;
-import org.infinispan.persistence.jdbc.configuration.PooledConnectionFactoryConfiguration;
-import org.infinispan.persistence.jdbc.configuration.PooledConnectionFactoryConfigurationBuilder;
-import org.infinispan.persistence.jdbc.connectionfactory.ConnectionFactory;
-import org.infinispan.persistence.jdbc.impl.connectionfactory.PooledConnectionFactory;
-import org.infinispan.persistence.spi.PersistenceException;
 import org.infinispan.test.AbstractInfinispanTest;
 import org.infinispan.test.CacheManagerCallable;
 import org.infinispan.test.fwk.TestCacheManagerFactory;
@@ -39,7 +26,7 @@ public class NonStringKeyPreloadTest extends AbstractInfinispanTest {
 
    public void testPreloadWithKey2StringMapper() throws Exception {
       String mapperName = PersonKey2StringMapper.class.getName();
-      ConfigurationBuilder cfg = createCacheStoreConfig(mapperName, false, true);
+      ConfigurationBuilder cfg = createCacheStoreConfig(mapperName, true);
 
       withCacheManager(new CacheManagerCallable(
             TestCacheManagerFactory.createCacheManager(cfg)) {
@@ -57,7 +44,7 @@ public class NonStringKeyPreloadTest extends AbstractInfinispanTest {
 
    public void testPreloadWithTwoWayKey2StringMapper() throws Exception {
       String mapperName = TwoWayPersonKey2StringMapper.class.getName();
-      ConfigurationBuilder config = createCacheStoreConfig(mapperName, true, true);
+      ConfigurationBuilder config = createCacheStoreConfig(mapperName, true);
       final Person mircea = new Person("Markus", "Mircea", 30);
       final Person dan = new Person("Dan", "Dude", 30);
       withCacheManager(new CacheManagerCallable(
@@ -85,9 +72,10 @@ public class NonStringKeyPreloadTest extends AbstractInfinispanTest {
          }
       });
    }
+
    public void testPreloadWithTwoWayKey2StringMapperAndBoundedCache() throws Exception {
       String mapperName = TwoWayPersonKey2StringMapper.class.getName();
-      ConfigurationBuilder config = createCacheStoreConfig(mapperName, true, true);
+      ConfigurationBuilder config = createCacheStoreConfig(mapperName, true);
       config.memory().size(3);
       withCacheManager(new CacheManagerCallable(
             TestCacheManagerFactory.createCacheManager(config)) {
@@ -119,78 +107,19 @@ public class NonStringKeyPreloadTest extends AbstractInfinispanTest {
       });
    }
 
-   static ConfigurationBuilder createCacheStoreConfig(String mapperName, boolean wrap, boolean preload) {
+   static ConfigurationBuilder createCacheStoreConfig(String mapperName, boolean preload) {
       ConfigurationBuilder cfg = TestCacheManagerFactory.getDefaultCacheConfiguration(false);
 
       JdbcStringBasedStoreConfigurationBuilder store = cfg
          .persistence()
+            .connectionAttempts(1)
             .addStore(JdbcStringBasedStoreConfigurationBuilder.class)
                .fetchPersistentState(true)
                .preload(preload)
                .key2StringMapper(mapperName);
       UnitTestDatabaseManager.buildTableManipulation(store.table());
-      if (wrap) {
-         ConnectionFactoryConfigurationBuilder<?> tmp = UnitTestDatabaseManager.configureUniqueConnectionFactory(new ConfigurationBuilder().persistence().addStore(JdbcStringBasedStoreConfigurationBuilder.class));
-         store.connectionFactory(new SharedConnectionFactoryConfigurationBuilder(store)).read((PooledConnectionFactoryConfiguration)tmp.create());
-      } else {
-         UnitTestDatabaseManager.configureUniqueConnectionFactory(store);
-      }
+      UnitTestDatabaseManager.configureUniqueConnectionFactory(store);
 
       return cfg;
-   }
-
-   public static class SharedConnectionFactory extends ConnectionFactory {
-      static PooledConnectionFactory sharedFactory;
-      static boolean started = false;
-
-      @Override
-      public void start(ConnectionFactoryConfiguration config, ClassLoader classLoader) throws PersistenceException {
-         if (!started) {
-            sharedFactory = new PooledConnectionFactory();
-            sharedFactory.start(config, classLoader);
-            started = true;
-         }
-      }
-
-      @Override
-      public void stop() {
-         //ignores
-      }
-
-      @Override
-      public Connection getConnection() throws PersistenceException {
-         return sharedFactory.getConnection();
-      }
-
-      @Override
-      public void releaseConnection(Connection conn) {
-         sharedFactory.releaseConnection(conn);
-      }
-   }
-
-   @BuiltBy(SharedConnectionFactoryConfigurationBuilder.class)
-   public static class SharedConnectionFactoryConfiguration extends PooledConnectionFactoryConfiguration {
-
-      protected SharedConnectionFactoryConfiguration(AttributeSet attributes) {
-         super(attributes);
-      }
-
-      @Override
-      public Class<? extends ConnectionFactory> connectionFactoryClass() {
-         return SharedConnectionFactory.class;
-      }
-   }
-
-   public static class SharedConnectionFactoryConfigurationBuilder extends PooledConnectionFactoryConfigurationBuilder {
-      public SharedConnectionFactoryConfigurationBuilder(AbstractJdbcStoreConfigurationBuilder builder) {
-         super(builder);
-      }
-      @Override
-      public void validate() {
-      }
-
-      @Override
-      public void validate(GlobalConfiguration globalConfig) {
-      }
    }
 }
