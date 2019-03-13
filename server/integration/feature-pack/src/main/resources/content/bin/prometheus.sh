096528fe6cf4a1f04635ddeefde338d906e65861
Ryan Emerson <remerson@redhat.com>
2019-03-13 13:05:12 +0000

    ISPN-10046 Server standalone.sh --jmx fails with Java 11

--- server/integration/feature-pack/src/main/resources/content/bin/prometheus.sh
+++ server/integration/feature-pack/src/main/resources/content/bin/prometheus.sh
@@ -14,4 +14,5 @@ fi
 PROMETHEUS_JAR=$(ls $JBOSS_HOME/modules/system/add-ons/@infinispan.module.slot.prefix@/io/prometheus/jmx/@infinispan.module.slot@/*.jar | sed -e "s| |:|g")
 JBOSS_LOG_MANAGER_LIB=$(ls $JBOSS_HOME/modules/system/layers/base/org/jboss/logmanager/main/*.jar | sed -e "s| |:|g")
 WILDFLY_COMMON_LIB=$(ls $JBOSS_HOME/modules/system/layers/base/org/wildfly/common/main/*.jar | sed -e "s| |:|g")
-export JAVA_OPTS="$JAVA_OPTS -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Xbootclasspath/p:$JBOSS_LOG_MANAGER_LIB -Xbootclasspath/p:$WILDFLY_COMMON_LIB -javaagent:$PROMETHEUS_JAR=$JMX_CONF"
+LOGGING="-Dsun.util.logging.disableCallerCheck=true -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Djboss.modules.system.pkgs=org.jboss.byteman,org.jboss.logmanager -Xbootclasspath/a:$JBOSS_LOG_MANAGER_LIB:$WILDFLY_COMMON_LIB"
+export JAVA_OPTS="$JAVA_OPTS $LOGGING -javaagent:$PROMETHEUS_JAR=$JMX_CONF"
