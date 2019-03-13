8c019c2b429ce768590502bf563595e5ef6400b8
Ryan Emerson <remerson@redhat.com>
2019-03-13 13:54:02 +0000

    ISPN-10048 Remove Jolokia support

--- server/integration/feature-pack/src/main/resources/content/bin/jolokia.bat
+++
@@ -1,5 +0,0 @@
-set AGENT_BOND_JAR=
-for /r %JBOSS_HOME%\modules\system\add-ons\@infinispan.module.slot.prefix@\io\fabric8\agent-bond\@infinispan.module.slot@ %%X in (*.jar) do (
-  set AGENT_BOND_JAR=%%X
-)
-set JAVA_OPTS=%JAVA_OPTS% -javaagent:%AGENT_BOND_JAR%=%1
