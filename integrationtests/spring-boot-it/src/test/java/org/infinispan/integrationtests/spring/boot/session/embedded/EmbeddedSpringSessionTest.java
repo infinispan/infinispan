5cefd787a5e68a2ca26c80079b9e2784f55215d5
Dan Berindei <dan@infinispan.org>
2019-04-30 18:18:36 +0300

    ISPN-10106 Clean up resources in spring-boot integration tests

--- integrationtests/spring-boot-it/src/test/java/org/infinispan/integrationtests/spring/boot/session/embedded/EmbeddedSpringSessionTest.java
+++ integrationtests/spring-boot-it/src/test/java/org/infinispan/integrationtests/spring/boot/session/embedded/EmbeddedSpringSessionTest.java
@@ -3,10 +3,12 @@ package org.infinispan.integrationtests.spring.boot.session.embedded;
 import org.infinispan.integrationtests.spring.boot.session.AbstractSpringSessionTCK;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = EmbeddedConfiguration.class, webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class EmbeddedSpringSessionTest extends AbstractSpringSessionTCK {
 
 }
