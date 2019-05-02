d29f31a9166670a742912ac4cddaf04ddb99742f
Tristan Tarrant <ttarrant@redhat.com>
2019-05-02 14:03:25 +0200

    ISPN-10163 Optionally use the fat server when running tests

--- server/integration/build/bin/moddep.sh
+++ server/integration/build/bin/moddep.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 DIRNAME=`dirname "$0"`
 BASEDIR=$DIRNAME/..
-MODULES=`find $BASEDIR/target/server-distribution-thin/infinispan-server-* -name 'module.xml'`
-$DIRNAME/moduleTools.py $@ $BASEDIR/target/server-distribution-thin/infinispan-server-*/domain/configuration/domain.xml $MODULES
+MODULES=`find $BASEDIR/target/infinispan-server-* -name 'module.xml'`
+$DIRNAME/moduleTools.py $@ $BASEDIR/target/infinispan-server-*/domain/configuration/domain.xml $MODULES
 
 # moddep.sh | tr . / | sed 's/$/\/main\/\*\*/' > trim-modules.txt
