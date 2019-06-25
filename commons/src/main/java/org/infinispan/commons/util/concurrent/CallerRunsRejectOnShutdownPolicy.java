0230a3c73736450e71077853a54fe669f5f7857c
William Burns <mudokonman@gmail.com>
2019-06-25 10:30:21 -0400

    ISPN-10349 PersistenceManager.addSegments may never complete
    * Change rejected policy to throw exception when shutdown

---
+++ commons/src/main/java/org/infinispan/commons/util/concurrent/CallerRunsRejectOnShutdownPolicy.java
@@ -0,0 +1,21 @@
+package org.infinispan.commons.util.concurrent;
+
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * A handler for rejected tasks that runs the rejected task
+ * directly in the calling thread of the {@code execute} method. If
+ * the executor was shutdown, it will instead throw a {@link RejectedExecutionException}.
+ * @author wburns
+ * @since 10.0
+ */
+public class CallerRunsRejectOnShutdownPolicy extends ThreadPoolExecutor.AbortPolicy {
+   @Override
+   public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+      if (executor.isShutdown()) {
+         super.rejectedExecution(r, executor);
+      }
+      r.run();
+   }
+}
