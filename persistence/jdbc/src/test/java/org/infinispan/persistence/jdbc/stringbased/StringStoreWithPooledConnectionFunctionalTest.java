d58ce2524a7b6d0bbd7aa5cb664a6afd015a1a95
Gustavo Lira <glira@redhat.com>
2019-06-26 21:29:08 -0300

    ISPN-10348 Migrate JDBC String based Cache Store from jdg-functional-tests to upstream

---
+++ persistence/jdbc/src/test/java/org/infinispan/persistence/jdbc/stringbased/StringStoreWithPooledConnectionFunctionalTest.java
@@ -0,0 +1,37 @@
+package org.infinispan.persistence.jdbc.stringbased;
+
+import org.infinispan.persistence.jdbc.configuration.ConnectionFactoryConfiguration;
+import org.infinispan.persistence.jdbc.configuration.JdbcStringBasedStoreConfigurationBuilder;
+import org.infinispan.persistence.jdbc.configuration.PooledConnectionFactoryConfiguration;
+import org.infinispan.persistence.jdbc.connectionfactory.ConnectionFactory;
+import org.infinispan.persistence.jdbc.impl.connectionfactory.PooledConnectionFactory;
+import org.testng.annotations.Test;
+
+import static org.infinispan.test.fwk.UnitTestDatabaseManager.configureUniqueConnectionFactory;
+
+@Test(groups = "functional", testName = "persistence.jdbc.stringbased.StringStoreWithPooledConnectionFunctionalTest")
+public class StringStoreWithPooledConnectionFunctionalTest extends AbstractStringBasedCacheStore {
+    private PooledConnectionFactoryConfiguration customFactoryConfiguration;
+
+    public StringStoreWithPooledConnectionFunctionalTest() {}
+
+    //Invoked from external resource
+    @SuppressWarnings("unused")
+    public StringStoreWithPooledConnectionFunctionalTest(PooledConnectionFactoryConfiguration customFactoryConfiguration) {
+        this.customFactoryConfiguration = customFactoryConfiguration;
+    }
+
+    @Override
+    public ConnectionFactory getConnectionFactory(JdbcStringBasedStoreConfigurationBuilder storeBuilder) {
+        ConnectionFactoryConfiguration connectionFactoryConfiguration;
+        if(customFactoryConfiguration != null) {
+            connectionFactoryConfiguration = storeBuilder.connectionPool().read(customFactoryConfiguration).create();
+        } else {
+            connectionFactoryConfiguration = configureUniqueConnectionFactory(storeBuilder).create();
+        }
+
+        final ConnectionFactory connectionFactory = ConnectionFactory.getConnectionFactory(PooledConnectionFactory.class);
+        connectionFactory.start(connectionFactoryConfiguration, connectionFactory.getClass().getClassLoader());
+        return connectionFactory;
+    }
+}
