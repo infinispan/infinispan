c84028ec175591f267d97f04dd561375922dadd2
Gustavo Fernandes <gustavonalle@gmail.com>
2019-07-05 13:41:28 +0100

    ISPN-8736 Use an executor and avoid reusing HttpResponses

--- server/rest/src/main/java/org/infinispan/rest/authentication/impl/VoidAuthenticator.java
+++ server/rest/src/main/java/org/infinispan/rest/authentication/impl/VoidAuthenticator.java
@@ -1,7 +1,9 @@
 package org.infinispan.rest.authentication.impl;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 
+import org.infinispan.rest.NettyRestResponse;
 import org.infinispan.rest.authentication.Authenticator;
 import org.infinispan.rest.framework.RestRequest;
 import org.infinispan.rest.framework.RestResponse;
@@ -16,6 +18,6 @@ import io.netty.channel.ChannelHandlerContext;
 public class VoidAuthenticator implements Authenticator {
    @Override
    public CompletionStage<RestResponse> challenge(RestRequest request, ChannelHandlerContext ctx) {
-      return COMPLETED_VOID_RESPONSE;
+      return CompletableFuture.completedFuture(new NettyRestResponse.Builder().build());
    }
 }
