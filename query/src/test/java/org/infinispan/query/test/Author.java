962bf0cdb290c69d3aba7ddafe669b881cf8472e
Adrian Nistor <anistor@redhat.com>
2019-08-05 12:54:42 +0300

    ISPN-10403 Fix generics in query results, fix asserts in query tests
    * remove unwanted constructor changes from previous PR
    * replace assert with assertTrue/assertEquals

--- query/src/test/java/org/infinispan/query/test/Author.java
+++ query/src/test/java/org/infinispan/query/test/Author.java
@@ -1,5 +1,7 @@
 package org.infinispan.query.test;
 
+import java.util.Objects;
+
 import org.hibernate.search.annotations.Field;
 import org.hibernate.search.annotations.Indexed;
 
@@ -8,6 +10,7 @@ public class Author {
 
    @Field
    private String name;
+
    @Field
    private String surname;
 
@@ -34,33 +37,16 @@ public class Author {
 
    @Override
    public int hashCode() {
-      final int prime = 31;
-      int result = 1;
-      result = prime * result + ((name == null) ? 0 : name.hashCode());
-      result = prime * result + ((surname == null) ? 0 : surname.hashCode());
-      return result;
+      return Objects.hash(name, surname);
    }
 
    @Override
    public boolean equals(Object obj) {
       if (this == obj)
          return true;
-      if (obj == null)
-         return false;
-      if (getClass() != obj.getClass())
+      if (obj == null || getClass() != obj.getClass())
          return false;
       Author other = (Author) obj;
-      if (name == null) {
-         if (other.name != null)
-            return false;
-      } else if (!name.equals(other.name))
-         return false;
-      if (surname == null) {
-         if (other.surname != null)
-            return false;
-      } else if (!surname.equals(other.surname))
-         return false;
-      return true;
+      return Objects.equals(name, other.name) && Objects.equals(surname, other.surname);
    }
-
 }
