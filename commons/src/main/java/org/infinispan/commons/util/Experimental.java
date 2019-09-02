6d7599e4fdc97a456defbbe6d97493a9eb5c2d33
Adrian Nistor <anistor@redhat.com>
2019-09-02 10:53:45 +0300

    Experimental annotation's retention does not need to be RUNTIME
    * targets LOCAL_VARIABLE and PARAMETER do not make sense

--- commons/src/main/java/org/infinispan/commons/util/Experimental.java
+++ commons/src/main/java/org/infinispan/commons/util/Experimental.java
@@ -1,13 +1,12 @@
 package org.infinispan.commons.util;
 
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
 import static java.lang.annotation.ElementType.CONSTRUCTOR;
 import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.LOCAL_VARIABLE;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.PACKAGE;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import static java.lang.annotation.RetentionPolicy.CLASS;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
@@ -18,8 +17,8 @@ import java.lang.annotation.Target;
  *
  * @since 8.0
  */
-@Retention(RUNTIME)
-@Target({TYPE, METHOD, FIELD, PACKAGE, CONSTRUCTOR, LOCAL_VARIABLE, PARAMETER})
+@Retention(CLASS)
+@Target({PACKAGE, TYPE, ANNOTATION_TYPE, METHOD, CONSTRUCTOR, FIELD})
 public @interface Experimental {
    String comment() default "";
 }
