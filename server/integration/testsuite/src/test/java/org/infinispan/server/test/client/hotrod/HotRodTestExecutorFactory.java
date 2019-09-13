306f37a5edadf1a96adff85e00f61429e262ff95
Tristan Tarrant <ttarrant@redhat.com>
2019-09-13 15:07:49 +0200

    ISPN-10594 Remove WildFly server
    * Adjust WildFly modules accordingly

--- server/integration/testsuite/src/test/java/org/infinispan/server/test/client/hotrod/HotRodTestExecutorFactory.java
+++
@@ -1,7 +0,0 @@
-package org.infinispan.server.test.client.hotrod;
-
-import org.infinispan.client.hotrod.impl.async.DefaultAsyncExecutorFactory;
-
-public class HotRodTestExecutorFactory extends DefaultAsyncExecutorFactory {
-
-}
--- server/integration/testsuite/src/test/java/org/infinispan/server/test/client/hotrod/HotRodTestMarshaller.java
+++
@@ -1,7 +0,0 @@
-package org.infinispan.server.test.client.hotrod;
-
-import org.infinispan.jboss.marshalling.commons.AbstractJBossMarshaller;
-
-public class HotRodTestMarshaller extends AbstractJBossMarshaller {
-    // all methods provided by superclass
-}
