37059e0efa1e8d3b7693895e6a4789a3c26683b9
Tristan Tarrant <ttarrant@redhat.com>
2019-10-03 09:31:00 +0200

    ISPN-10427 Server Windows batch files

---
+++ server/runtime/src/main/server/bin/common.bat
@@ -0,0 +1,69 @@
+@echo off
+
+pushd "%DIRNAME%.."
+set "RESOLVED_ISPN_HOME=%CD%"
+popd
+
+if "x%ISPN_HOME%" == "x" (
+   set "ISPN_HOME=%RESOLVED_ISPN_HOME%"
+)
+
+pushd "%ISPN_HOME%"
+set "SANITIZED_ISPN_HOME=%CD%"
+popd
+
+if /i "%RESOLVED_ISPN_HOME%" NEQ "%SANITIZED_ISPN_HOME%" (
+   echo.
+   echo   WARNING: The ISPN_HOME ^("%SANITIZED_ISPN_HOME%"^) that this script uses points to a different installation than the one that this script resides in ^("%RESOLVED_ISPN_HOME%"^). Unpredictable results may occur.
+   echo.
+   echo       ISPN_HOME: "%ISPN_HOME%"
+   echo.
+)
+
+if "x%JAVA_HOME%" == "x" (
+   set JAVA=java
+) else (
+   set "JAVA=%JAVA_HOME%\bin\java"
+)
+
+set "JAVA_OPTS="
+
+set "CLASSPATH="
+
+for %%j in (%ISPN_HOME%\boot\*.jar) do (
+   set "CLASSPATH=%CLASSPATH%;%%~j"
+)
+
+set DEBUG_MODE=false
+set DEBUG_PORT=8787
+
+:ARGS_LOOP_START
+if "%~1" == "" (
+   goto ARGS_LOOP_END
+) else if "%~1" == "--debug" (
+   set DEBUG_MODE=true
+   if "%~2"=="" goto ARGS_LOOP_END
+   set "var="&for /f "delims=0123456789" %%i in ("%~2") do set var=%%i
+   if defined var (
+      rem Use the default port
+   ) else (
+      set DEBUG_PORT=%2
+      shift
+   )
+) else (
+   set "ARGUMENTS=%ARGUMENTS% %1"
+)
+shift
+goto ARGS_LOOP_START
+
+:ARGS_LOOP_END
+
+rem Set debug settings if not already set
+if "%DEBUG_MODE%" == "true" (
+   echo "%JAVA_OPTS%" | findstr /I "\-agentlib:jdwp" > nul
+  if errorlevel == 1 (
+     set "JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_socket,address=%DEBUG_PORT%,server=y,suspend=n"
+  ) else (
+     echo Debug already enabled in JAVA_OPTS, ignoring --debug argument
+  )
+)
---
+++ server/runtime/src/main/server/bin/server.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set LOADER_CLASS=org.infinispan.server.loader.Loader
+set MAIN_CLASS=org.infinispan.server.Bootstrap
+set ARGUMENTS=
+
+set "DIRNAME=%~dp0%"
+
+setlocal EnableDelayedExpansion
+call "!DIRNAME!common.bat" %*
+setlocal DisableDelayedExpansion
+
+"%JAVA%" %JAVA_OPTS% ^
+   -Djava.util.logging.manager=org.jboss.logmanager.LogManager ^
+   "-Dinfinispan.server.home.path=%ISPN_HOME%" ^
+   -classpath %CLASSPATH% %LOADER_CLASS% %MAIN_CLASS% %ARGUMENTS%
