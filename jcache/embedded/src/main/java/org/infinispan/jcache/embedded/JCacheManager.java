96cc9ee9b66f8d3c3089ce04cd466325571196b0
Dan Berindei <dan@infinispan.org>
2019-10-04 12:47:28 +0300

    ISPN-10719 JCache: CacheManager.destroyCache should not remove internal caches
    * Hide internal caches in CacheManager.getCacheNames()
    * Disallow internal caches in CacheManager.getOrCreateCache()

--- jcache/embedded/src/main/java/org/infinispan/jcache/embedded/JCacheManager.java
+++ jcache/embedded/src/main/java/org/infinispan/jcache/embedded/JCacheManager.java
@@ -3,6 +3,8 @@ package org.infinispan.jcache.embedded;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.net.URI;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
@@ -22,6 +24,7 @@ import org.infinispan.jcache.AbstractJCacheManager;
 import org.infinispan.jcache.embedded.logging.Log;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.registry.InternalCacheRegistry;
 import org.infinispan.util.logging.LogFactory;
 
 /**
@@ -35,6 +38,7 @@ public class JCacheManager extends AbstractJCacheManager {
    private static final Log log = LogFactory.getLog(JCacheManager.class, Log.class);
 
    private final EmbeddedCacheManager cm;
+   private final InternalCacheRegistry icr;
 
    /**
     * Create a new InfinispanCacheManager given a cache name and a {@link ClassLoader}. Cache name
@@ -65,15 +69,21 @@ public class JCacheManager extends AbstractJCacheManager {
             + "/provider=" + provider.toString();
       // Set cache manager class loader and apply name to cache manager MBean
       globalBuilder.classLoader(classLoader)
-            .globalJmxStatistics().enable().cacheManagerName(cacheManagerName);
+                   .cacheManagerName(cacheManagerName)
+                   .globalJmxStatistics().enable();
 
       cm = new DefaultCacheManager(cbh, true);
+      icr = SecurityActions.getGlobalComponentRegistry(cm).getComponent(InternalCacheRegistry.class);
+
       registerPredefinedCaches();
    }
 
    public JCacheManager(URI uri, EmbeddedCacheManager cacheManager, CachingProvider provider) {
       super(uri, null, provider, null, true);
+
       this.cm = cacheManager;
+      this.icr = SecurityActions.getGlobalComponentRegistry(cm).getComponent(InternalCacheRegistry.class);
+
       registerPredefinedCaches();
    }
 
@@ -84,9 +94,12 @@ public class JCacheManager extends AbstractJCacheManager {
       Set<String> cacheNames = cm.getCacheNames();
       for (String cacheName : cacheNames) {
          // With pre-defined caches, obey only pre-defined configuration
-         registerPredefinedCache(cacheName, new JCache<Object, Object>(
-               cm.getCache(cacheName).getAdvancedCache(), this,
-               ConfigurationAdapter.create()));
+         if (icr.isInternalCache(cacheName))
+            continue;
+
+         registerPredefinedCache(cacheName, new JCache<>(
+            cm.getCache(cacheName).getAdvancedCache(), this,
+            ConfigurationAdapter.create()));
       }
    }
 
@@ -126,7 +139,14 @@ public class JCacheManager extends AbstractJCacheManager {
 
    @Override
    protected Iterable<String> delegateCacheNames() {
-      return cm.getCacheNames();
+      // Unlike EmbeddedCacheManager, we do not list user-visible internal caches
+      Set<String> cacheNames = new HashSet<>();
+      for (String cacheName : cm.getCacheNames()) {
+         if (!icr.isInternalCache(cacheName)) {
+            cacheNames.add(cacheName);
+         }
+      }
+      return Collections.unmodifiableSet(cacheNames);
    }
 
    @Override
@@ -142,6 +162,8 @@ public class JCacheManager extends AbstractJCacheManager {
 
    @Override
    protected <K, V, C extends Configuration<K, V>> AbstractJCache<K, V> create(String cacheName, C configuration) {
+      checkNotInternalCache(cacheName);
+
       org.infinispan.configuration.cache.Configuration baseConfig = cm.getCacheConfiguration(cacheName);
       ConfigurationAdapter<K, V> adapter = ConfigurationAdapter.create(configuration);
       cm.defineConfiguration(cacheName, adapter.build(baseConfig));
@@ -157,6 +179,14 @@ public class JCacheManager extends AbstractJCacheManager {
 
    @Override
    protected <K, V, I extends BasicCache<K, V>> AbstractJCache<K, V> create(I ispnCache) {
+      checkNotInternalCache(ispnCache.getName());
+
       return new JCache<>((AdvancedCache<K, V>) ispnCache, this, ConfigurationAdapter.create());
+   }
+
+   private void checkNotInternalCache(String cacheName) {
+      if (icr.isInternalCache(cacheName))
+         throw new IllegalArgumentException("Cache name " + cacheName +
+                                            "is not allowed as it clashes with an internal cache");
    }
 }
