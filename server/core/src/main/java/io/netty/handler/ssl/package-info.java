2ea4dee89f6a2f7da6690333843dcacafa232959
Radoslav Husar <radosoft@gmail.com>
2019-10-31 21:21:09 +0100

    Cosmetic: fix WildFly stylization

--- server/core/src/main/java/io/netty/handler/ssl/package-info.java
+++ server/core/src/main/java/io/netty/handler/ssl/package-info.java
@@ -3,7 +3,7 @@
  *
  * <p>
  *    Unfortunately JDK 8 doesn't have support handling TLS/ALPN for negotiating protocols. Using OpenSSL doesn't
- *    improve the situation neither since we obtain an initialized {@link javax.net.ssl.SSLContext} from Wildfly.
+ *    improve the situation neither since we obtain an initialized {@link javax.net.ssl.SSLContext} from WildFly.
  *    There's also 3rd element of the puzzle - Netty, which contains its own class hierarchy for handling TLS.
  *
  *    So in order to support TLS/ALPN without Jetty's agent (which requires specific version per JVM version), we need
