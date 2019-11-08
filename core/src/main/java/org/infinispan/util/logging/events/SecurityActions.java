7aa48fe1d6b414f8748d3e58b9b167ee9146822a
Ryan Emerson <remerson@redhat.com>
2019-11-08 11:34:43 +0000

    ISPN-10928 SecurityException: ISPN000287: Unauthorized access: subject 'null' lacks 'ADMIN'

--- core/src/main/java/org/infinispan/util/logging/events/SecurityActions.java
+++ core/src/main/java/org/infinispan/util/logging/events/SecurityActions.java
@@ -1,15 +1,12 @@
-package org.infinispan.counter.impl.manager;
+package org.infinispan.util.logging.events;
 
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 
-import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.factories.GlobalComponentRegistry;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.security.Security;
-import org.infinispan.security.actions.GetCacheManagerConfigurationAction;
 import org.infinispan.security.actions.GetGlobalComponentRegistryAction;
-import org.infinispan.security.actions.SetThreadNameAction;
 
 /**
  * SecurityActions for the org.infinispan.counter package.
@@ -17,8 +14,8 @@ import org.infinispan.security.actions.SetThreadNameAction;
  * Do not move. Do not change class and method visibility to avoid being called from other
  * {@link java.security.CodeSource}s, thus granting privilege escalation to external code.
  *
- * @author Dan Berindei
- * @since 10.0
+ * @author Ryan Emerson
+ * @since 10.1
  */
 final class SecurityActions {
 
@@ -27,18 +24,10 @@ final class SecurityActions {
 
    private static <T> T doPrivileged(PrivilegedAction<T> action) {
       return System.getSecurityManager() != null ?
-             AccessController.doPrivileged(action) : Security.doPrivileged(action);
+            AccessController.doPrivileged(action) : Security.doPrivileged(action);
    }
 
    static GlobalComponentRegistry getGlobalComponentRegistry(EmbeddedCacheManager cacheManager) {
       return doPrivileged(new GetGlobalComponentRegistryAction(cacheManager));
-   }
-
-   static GlobalConfiguration getCacheManagerConfiguration(EmbeddedCacheManager cacheManager) {
-      return doPrivileged(new GetCacheManagerConfigurationAction(cacheManager));
-   }
-
-   static void setThreadName(String threadName) {
-      doPrivileged(new SetThreadNameAction(threadName));
    }
 }
