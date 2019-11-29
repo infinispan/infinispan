9da8b7916dea16e2b3ad61c0d80a623f68d93df0
Adrian Nistor <anistor@redhat.com>
2019-11-29 12:20:11 +0200

    Experimental annotation must be documented
    * also document MBean related annotations

--- component-annotations/src/main/java/org/infinispan/jmx/annotations/Parameter.java
+++ component-annotations/src/main/java/org/infinispan/jmx/annotations/Parameter.java
@@ -1,5 +1,6 @@
 package org.infinispan.jmx.annotations;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +8,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.CLASS)
+@Documented
 public @interface Parameter {
 
    /**
