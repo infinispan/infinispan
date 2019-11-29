87485895ff4021810c8d37a5c1c8060c8e8f97a7
Adrian Nistor <anistor@redhat.com>
2019-11-29 13:51:12 +0200

    Hide component registry generated classes from javadocs
    * add @private tag

--- core/src/main/java/org/infinispan/CoreModule.java
+++ core/src/main/java/org/infinispan/CoreModule.java
@@ -3,6 +3,9 @@ package org.infinispan;
 import org.infinispan.factories.annotations.InfinispanModule;
 import org.infinispan.lifecycle.ModuleLifecycle;
 
+/**
+ * @private
+ */
 @InfinispanModule(name = "core")
 public class CoreModule implements ModuleLifecycle {
 }
