9fc81157f26c3681e7de2e0a7a02015278da9096
Adrian Nistor <anistor@redhat.com>
2019-12-06 20:45:21 +0200

    ISPN-11047 Deprecate the memcached protocol server

--- server/memcached/src/main/java/org/infinispan/server/memcached/configuration/package-info.java
+++ server/memcached/src/main/java/org/infinispan/server/memcached/configuration/package-info.java
@@ -4,5 +4,4 @@
  * @public
  * @deprecated since 10.1. Will be removed unless a binary protocol encoder/decoder is implemented.
  */
-@Deprecated
 package org.infinispan.server.memcached.configuration;
