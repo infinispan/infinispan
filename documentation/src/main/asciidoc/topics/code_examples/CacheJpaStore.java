371d47c902237d0b7b27799486f865303294213d
Don Naro <don.naro.jr@gmail.com>
2019-12-11 12:44:43 +0000

    ISPN-10596 persistence doc productization

--- documentation/src/main/asciidoc/topics/code_examples/CacheJpaStore.java
+++ documentation/src/main/asciidoc/topics/code_examples/CacheJpaStore.java
@@ -1,4 +1,7 @@
-Cache<String, User> usersCache = cacheManager.getCache("myJPACache"); // configured for User entity class
-usersCache.put("raytsang", new User());
-Cache<Integer, Teacher> teachersCache = cacheManager.getCache("myJPACache"); // cannot do this when this cache is configured to use a JPA cache store
+Cache<String, User> usersCache = cacheManager.getCache("myJPACache");
+// Cache is configured for the User entity class
+usersCache.put("username", new User());
+// Cannot configure caches to use another entity class with JPA cache stores
+Cache<Integer, Teacher> teachersCache = cacheManager.getCache("myJPACache");
 teachersCache.put(1, new Teacher());
+// The put request does not work for the Teacher entity class
