b088114d0e184d0d324514312db42c34e1f5cb51
Tristan Tarrant <ttarrant@redhat.com>
2019-12-16 16:49:01 +0100

    ISPN-11075 Externalize security code examples
    * Also avoid serverName()/realm() when using defaults

---
+++ documentation/src/main/asciidoc/topics/code_examples/HotRodPlain.java
@@ -0,0 +1,12 @@
+ConfigurationBuilder clientBuilder = new ConfigurationBuilder();
+clientBuilder
+   .addServer()
+      .host("127.0.0.1")
+      .port(11222)
+   .security()
+      .authentication()
+         .saslMechanism("PLAIN")
+         .username("myuser")
+         .password("qwer1234!");
+remoteCacheManager = new RemoteCacheManager(clientBuilder.build());
+RemoteCache<String, String> cache = remoteCacheManager.getCache("secured");
---
+++ documentation/src/main/asciidoc/topics/code_examples/HotRodTLS.java
@@ -0,0 +1,11 @@
+ConfigurationBuilder clientBuilder = new ConfigurationBuilder();
+clientBuilder
+   .addServer()
+      .host("127.0.0.1")
+      .port(11222)
+   .security()
+      .ssl()
+         // TrustStore is a KeyStore which contains part of the server certificate chain (e.g. the CA Root public cert)
+         .trustStoreFileName("/path/to/truststore")
+         .trustStorePassword("truststorepassword".toCharArray());
+RemoteCache<String, String> cache=remoteCacheManager.getCache("secured");
