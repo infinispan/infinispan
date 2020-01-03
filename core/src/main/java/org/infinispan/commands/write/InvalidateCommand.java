3603994919c28a0ba79f72f65d546caaffd9b04d
Dan Berindei <dan@infinispan.org>
2020-01-03 12:04:07 +0200

    ISPN-11116 Invalidation commands should not load the previous value

--- core/src/main/java/org/infinispan/commands/write/InvalidateCommand.java
+++ core/src/main/java/org/infinispan/commands/write/InvalidateCommand.java
@@ -184,8 +184,7 @@ public class InvalidateCommand extends AbstractTopologyAffectedCommand implement
 
    @Override
    public LoadType loadType() {
-      // TODO Return LoadType.OWNER only if there are invalidation listeners registered
-      return LoadType.OWNER;
+      return LoadType.DONT_LOAD;
    }
 
    @Override
