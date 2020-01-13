c020d37b33c90dd383bdcbc8a788cf3d1582aa9e
Tristan Tarrant <ttarrant@redhat.com>
2020-01-13 17:31:37 +0100

    ISPN-11149 CLI logging file

--- server/runtime/src/main/server/bin/cli.bat
+++ server/runtime/src/main/server/bin/cli.bat
@@ -14,5 +14,6 @@ setlocal DisableDelayedExpansion
 "%JAVA%" %JAVA_OPTS% ^
    -Dvisualvm.display.name=%PROCESS_NAME% \
    -Djava.util.logging.manager=org.apache.logging.log4j.jul.LogManager ^
+   -Dlog4j.configurationFile="%DIRNAME%\cli.log4j2.xml" ^
    "-Dinfinispan.server.home.path=%ISPN_HOME%" ^
    -classpath %CLASSPATH% %LOADER_CLASS% %MAIN_CLASS% %ARGUMENTS%
