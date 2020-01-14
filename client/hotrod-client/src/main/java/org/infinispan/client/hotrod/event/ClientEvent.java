036a43e27cac72a98a15192def2a92d7eba99feb
Adrian Nistor <anistor@redhat.com>
2020-01-14 15:34:16 +0200

    ISPN-11188 Deprecate ClientEvents.mkCachefailoverEvent

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/event/ClientEvent.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/event/ClientEvent.java
@@ -4,9 +4,13 @@ package org.infinispan.client.hotrod.event;
  * @author Galder Zamarreño
  */
 public interface ClientEvent {
-   static enum Type {
-      CLIENT_CACHE_ENTRY_CREATED, CLIENT_CACHE_ENTRY_MODIFIED,
-      CLIENT_CACHE_ENTRY_REMOVED, CLIENT_CACHE_ENTRY_EXPIRED, CLIENT_CACHE_FAILOVER
+
+   enum Type {
+      CLIENT_CACHE_ENTRY_CREATED,
+      CLIENT_CACHE_ENTRY_MODIFIED,
+      CLIENT_CACHE_ENTRY_REMOVED,
+      CLIENT_CACHE_ENTRY_EXPIRED,
+      CLIENT_CACHE_FAILOVER
    }
 
    Type getType();
