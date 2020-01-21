d9fe7b54d16ac983f351c6f9aa309e74d90bf5d1
Don Naro <don.naro.jr@gmail.com>
2020-01-21 21:24:29 +0000

    ISPN-10839 declarative and programmatic config updates

--- documentation/src/main/asciidoc/topics/code_examples/ConfigThreadPools.java
+++
@@ -1,4 +0,0 @@
-GlobalConfiguration globalConfig = new GlobalConfigurationBuilder()
-   .replicationQueueThreadPool()
-     .threadPoolFactory(ScheduledThreadPoolExecutorFactory.create())
-  .build();
