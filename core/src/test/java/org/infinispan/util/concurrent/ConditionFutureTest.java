a8a4edab66b2f6150a55c771211eb360067b78d3
Dan Berindei <dan@infinispan.org>
2020-02-07 09:44:51 +0200

    ISPN-11280 ConcurrentModificationException in ConditionFuture

--- core/src/test/java/org/infinispan/util/concurrent/ConditionFutureTest.java
+++ core/src/test/java/org/infinispan/util/concurrent/ConditionFutureTest.java
@@ -1,12 +1,14 @@
 package org.infinispan.util.concurrent;
 
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.infinispan.util.concurrent.CompletionStages.isCompletedSuccessfully;
 import static org.testng.AssertJUnit.assertFalse;
 import static org.testng.AssertJUnit.assertTrue;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.infinispan.test.AbstractInfinispanTest;
 import org.testng.annotations.AfterClass;
@@ -24,7 +26,7 @@ public class ConditionFutureTest extends AbstractInfinispanTest {
 
    public void testBeforeFirstUpdate() {
       ConditionFuture<Integer> conditionFuture = new ConditionFuture<>(timeoutExecutor);
-      CompletionStage<Void> stage = conditionFuture.newConditionStage(i -> i > 0, 1, TimeUnit.SECONDS);
+      CompletionStage<Void> stage = conditionFuture.newConditionStage(i -> i > 0, 10, SECONDS);
       assertFalse(stage.toCompletableFuture().isDone());
 
       conditionFuture.update(1);
@@ -34,7 +36,36 @@ public class ConditionFutureTest extends AbstractInfinispanTest {
    public void testAlreadyCompleted() {
       ConditionFuture<Integer> conditionFuture = new ConditionFuture<>(timeoutExecutor);
       conditionFuture.update(1);
-      CompletionStage<Void> stage = conditionFuture.newConditionStage(i -> i > 0, 1, TimeUnit.SECONDS);
+      CompletionStage<Void> stage = conditionFuture.newConditionStage(i -> i > 0, 10, SECONDS);
       assertTrue(stage.toCompletableFuture().isDone());
+   }
+
+   public void testConcurrentModification() {
+      ConditionFuture<Integer> conditionFuture = new ConditionFuture<>(timeoutExecutor);
+      CompletionStage<Void> stage11 = conditionFuture.newConditionStage(i -> i > 0, 10, SECONDS);
+      CompletionStage<Void> stage12 = conditionFuture.newConditionStage(i -> i > 0, 10, SECONDS);
+
+      // Block the completion of stage11
+      CompletableFuture<Void> updateReleased = new CompletableFuture<>();
+      stage11.thenRun(updateReleased::join);
+      stage12.thenRun(updateReleased::join);
+
+      // Update the condition future, triggering the completion of stage1x
+      conditionFuture.updateAsync(1, testExecutor());
+      eventually(() -> isCompletedSuccessfully(stage11) || isCompletedSuccessfully(stage12));
+
+      // Add 2 new condition stages while the update is blocked, to increment modCount by 2
+      CompletionStage<Void> stage21 = conditionFuture.newConditionStage(i -> i > 1, 10, SECONDS);
+      CompletionStage<Void> stage22 = conditionFuture.newConditionStage(i -> i > 1, 10, SECONDS);
+
+      // Unblock the condition future update
+      updateReleased.complete(null);
+      CompletionStages.join(stage11);
+      CompletionStages.join(stage12);
+
+      // Update again to complete stage21 and stage22
+      conditionFuture.update(2);
+      CompletionStages.join(stage21);
+      CompletionStages.join(stage22);
    }
 }
