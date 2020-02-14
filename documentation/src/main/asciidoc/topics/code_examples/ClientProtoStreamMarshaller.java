b29fe0faad3133150fc14a93fbedc0853f5efde5
Don Naro <don.naro.jr@gmail.com>
2020-02-14 13:48:07 +0000

    ISPN-10515 hot rod java client

--- documentation/src/main/asciidoc/topics/code_examples/ClientProtoStreamMarshaller.java
+++ documentation/src/main/asciidoc/topics/code_examples/ClientProtoStreamMarshaller.java
@@ -1,5 +1,8 @@
-ConfigurationBuilder builder = new ConfigurationBuilder()
-      .addServer().host("localhost").port(hotRodServer.getPort())
-      .addContextInitializers(new LibraryInitializerImpl(), new AnotherExampleSciImpl())
-      .build();
+ConfigurationBuilder clientBuilder = new ConfigurationBuilder();
+clientBuilder
+   .addServer()
+    .host("127.0.0.1")
+    .port(11222)
+   .addContextInitializers(new LibraryInitializerImpl(), new AnotherExampleSciImpl())
+   .build();
 RemoteCacheManager rcm = new RemoteCacheManager(builder);
