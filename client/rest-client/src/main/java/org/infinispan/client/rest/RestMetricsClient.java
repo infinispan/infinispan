2f6d5806dc51811aae1026abecafba924d8b9b98
Adrian Nistor <anistor@redhat.com>
2020-03-02 14:34:31 +0200

    ISPN-11403 RestMetricsResource fails due to NettyTransportConnectionStats.getNumberOfGlobalConnections metrics not working with jmx disabled
    * this just works around the issue and returns a 0 for that metric when not computable rather than failing with a RuntimeException

--- client/rest-client/src/main/java/org/infinispan/client/rest/RestMetricsClient.java
+++ client/rest-client/src/main/java/org/infinispan/client/rest/RestMetricsClient.java
@@ -3,7 +3,7 @@ package org.infinispan.client.rest;
 import java.util.concurrent.CompletionStage;
 
 /**
- * An experimental rest client for microprofile metrics and OpenMetrics.
+ * An experimental rest client for microprofile metrics in native and Prometheus compatible OpenMetrics format.
  *
  * @author anistor@redhat.com
  * @since 10.0
@@ -14,9 +14,9 @@ public interface RestMetricsClient {
 
    CompletionStage<RestResponse> metrics(String path);
 
-   CompletionStage<RestResponse> metrics(boolean openMetrics);
+   CompletionStage<RestResponse> metrics(boolean openMetricsFormat);
 
-   CompletionStage<RestResponse> metrics(String path, boolean openMetrics);
+   CompletionStage<RestResponse> metrics(String path, boolean openMetricsFormat);
 
    CompletionStage<RestResponse> metricsMetadata();
 
