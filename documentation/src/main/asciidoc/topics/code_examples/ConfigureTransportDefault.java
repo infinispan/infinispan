773f3650ab74563bab96813f5d94ab61837a14de
Don Naro <don.naro.jr@gmail.com>
2020-03-06 17:46:29 +0000

    ISPN-11428 JGroups clustering docs

--- documentation/src/main/asciidoc/topics/code_examples/ConfigureTransportDefault.java
+++ documentation/src/main/asciidoc/topics/code_examples/ConfigureTransportDefault.java
@@ -1,7 +1,5 @@
 GlobalConfiguration globalConfig = new GlobalConfigurationBuilder().transport()
         .defaultTransport()
         .clusterName("qa-cluster")
-         // Use default JGroups stacks with the addProperty() method.
-        .addProperty("configurationFile", "default-jgroups-tcp.xml")
-        .machineId("qa-machine").rackId("qa-rack")
+        .addProperty("configurationFile", "default-jgroups-udp.xml") <1>
         .build();
