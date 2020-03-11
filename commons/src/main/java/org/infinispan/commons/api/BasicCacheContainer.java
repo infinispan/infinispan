752781ea60253950d9e036ece3bca6d931619b8b
Dan Berindei <dan@infinispan.org>
2020-03-11 16:53:46 +0200

    ISPN-11367 Update cache manager javadoc for default caches

--- commons/src/main/java/org/infinispan/commons/api/BasicCacheContainer.java
+++ commons/src/main/java/org/infinispan/commons/api/BasicCacheContainer.java
@@ -15,30 +15,18 @@ public interface BasicCacheContainer extends Lifecycle {
    /**
     * Retrieves the default cache associated with this cache container.
     * <p/>
-    * As such, this method is always guaranteed to return the default cache, unless one has not been supplied to the
-    * cache container.
-    * <p />
-    * <b>NB:</b> Shared caches are supported (and in fact encouraged) but if they are used it's the users responsibility to
-    * ensure that <i>at least one</i> but <i>only one</i> caller calls stop() on the cache, and it does so with the awareness
-    * that others may be using the cache.
     *
     * @return the default cache.
+    * @throws org.infinispan.commons.CacheConfigurationException if a default cache does not exist.
     */
    <K, V> BasicCache<K, V> getCache();
 
    /**
-    * Retrieves a named cache from the system.  If the cache has been previously created with the same name, the running
-    * cache instance is returned.  Otherwise, this method attempts to create the cache first.
+    * Retrieves a cache by name.
     * <p/>
-    * In the case of a {@link org.infinispan.manager.EmbeddedCacheManager}: when creating a new cache, this method will
-    * use the configuration passed in to the EmbeddedCacheManager on construction, as a template, and then optionally
-    * apply any overrides previously defined for the named cache using the {@link EmbeddedCacheManager#defineConfiguration(String, org.infinispan.config.Configuration)}
-    * or {@link EmbeddedCacheManager#defineConfiguration(String, String, org.infinispan.config.Configuration)}
-    * methods, or declared in the configuration file.
-    * <p />
-    * <b>NB:</b> Shared caches are supported (and in fact encouraged) but if they are used it's the users responsibility to
-    * ensure that <i>at least one</i> but <i>only one</i> caller calls stop() on the cache, and it does so with the awareness
-    * that others may be using the cache.
+    * If the cache has been previously created with the same name, the running
+    * cache instance is returned.
+    * Otherwise, this method attempts to create the cache first.
     *
     * @param cacheName name of cache to retrieve
     * @return a cache instance identified by cacheName
@@ -46,17 +34,14 @@ public interface BasicCacheContainer extends Lifecycle {
    <K, V> BasicCache<K, V> getCache(String cacheName);
 
    /**
-    * This method returns a collection of caches names which contains the
-    * caches that have been defined via XML or programmatically, and the
-    * caches that have been created at runtime via this cache manager
-    * instance.
+    * This method returns a collection of all cache names.
+    * <p/>
+    * The configurations may have been defined via XML, in the programmatic configuration,
+    * or at runtime.
+    * <p/>
+    * Internal-only caches are not included.
     *
-    * If no named caches are registered or no caches have been created, this
-    * method returns an empty set.  The list of caches does not include
-    * internal-only caches {@link org.infinispan.registry.InternalCacheRegistry}
-    *
-    * @return an immutable set of caches registered or
-    * created with this cache manager.
+    * @return an immutable set of cache names registered in this cache manager.
     */
    Set<String> getCacheNames();
 }
