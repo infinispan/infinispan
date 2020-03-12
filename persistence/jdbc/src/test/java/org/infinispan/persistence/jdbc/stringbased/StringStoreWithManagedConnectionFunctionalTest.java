d238aee3d2c522b58525093a0d1df13212a8cf1e
Gustavo Lira <glira@redhat.com>
2020-03-12 12:29:49 -0300

    ISPN-11464 Make possible to override table manipulation configuration from AbstractStringBasedCacheStore

--- persistence/jdbc/src/test/java/org/infinispan/persistence/jdbc/stringbased/StringStoreWithManagedConnectionFunctionalTest.java
+++ persistence/jdbc/src/test/java/org/infinispan/persistence/jdbc/stringbased/StringStoreWithManagedConnectionFunctionalTest.java
@@ -1,9 +1,6 @@
 package org.infinispan.persistence.jdbc.stringbased;
 
-import org.infinispan.persistence.jdbc.configuration.ConnectionFactoryConfiguration;
 import org.infinispan.persistence.jdbc.configuration.JdbcStringBasedStoreConfigurationBuilder;
-import org.infinispan.persistence.jdbc.configuration.ManagedConnectionFactoryConfiguration;
-import org.infinispan.persistence.jdbc.impl.connectionfactory.ManagedConnectionFactory;
 import org.infinispan.persistence.jdbc.impl.connectionfactory.SimpleConnectionFactory;
 import org.infinispan.persistence.jdbc.connectionfactory.ConnectionFactory;
 import org.testng.annotations.Test;
@@ -13,30 +10,10 @@ import static org.infinispan.persistence.jdbc.UnitTestDatabaseManager.configureS
 @Test(groups = "functional", testName = "persistence.jdbc.stringbased.StringStoreWithManagedConnectionFunctionalTest")
 public class StringStoreWithManagedConnectionFunctionalTest extends AbstractStringBasedCacheStore {
 
-    private ManagedConnectionFactoryConfiguration customFactoryConfiguration;
-
-    public StringStoreWithManagedConnectionFunctionalTest() {}
-
-    @SuppressWarnings("unused")
-    public StringStoreWithManagedConnectionFunctionalTest(ManagedConnectionFactoryConfiguration customFactoryConfiguration) {
-        this.customFactoryConfiguration = customFactoryConfiguration;
-    }
-
     @Override
     protected ConnectionFactory getConnectionFactory(JdbcStringBasedStoreConfigurationBuilder storeBuilder) {
-        if(customFactoryConfiguration != null) {
-            ConnectionFactoryConfiguration connectionFactoryConfiguration = storeBuilder
-                    .dataSource()
-                    .read(customFactoryConfiguration)
-                    .create();
-            final ConnectionFactory connectionFactory = ConnectionFactory.getConnectionFactory(ManagedConnectionFactory.class);
-            connectionFactory.start(connectionFactoryConfiguration, connectionFactory.getClass().getClassLoader());
-            return connectionFactory;
-        } else {
-            SimpleConnectionFactory simpleFactory = new SimpleConnectionFactory();
-            simpleFactory.start(configureSimpleConnectionFactory(storeBuilder).create(), Thread.currentThread().getContextClassLoader());
-            return simpleFactory;
-        }
-
+        SimpleConnectionFactory simpleFactory = new SimpleConnectionFactory();
+        simpleFactory.start(configureSimpleConnectionFactory(storeBuilder).create(), Thread.currentThread().getContextClassLoader());
+        return simpleFactory;
     }
 }
