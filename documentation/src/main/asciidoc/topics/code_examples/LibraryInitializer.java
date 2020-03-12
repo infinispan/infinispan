00de72fe9b1f51b3855134466ec7dcee21c415b9
Don Naro <don.naro.jr@gmail.com>
2020-03-12 11:17:13 +0000

    ISPN-11457 marshalling docs

--- documentation/src/main/asciidoc/topics/code_examples/LibraryInitializer.java
+++ documentation/src/main/asciidoc/topics/code_examples/LibraryInitializer.java
@@ -3,8 +3,8 @@
             Book.class,
             Author.class,
       },
-      schemaFileName = "library.proto",
-      schemaFilePath = "proto/",
+      schemaFileName = "library.proto", <1>
+      schemaFilePath = "proto/", <2>
       schemaPackageName = "book_sample")
 interface LibraryInitializer extends SerializationContextInitializer {
 }
