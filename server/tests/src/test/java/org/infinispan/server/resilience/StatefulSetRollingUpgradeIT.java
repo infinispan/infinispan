113c102e1beadcd05f9b7b00d60e1610141e8a88
Ryan Emerson <remerson@redhat.com>
2020-03-26 13:47:36 +0000

    ISPN-11537 Parameterize StatefulSetRollingUpgradeIT cluster size

--- server/tests/src/test/java/org/infinispan/server/resilience/StatefulSetRollingUpgradeIT.java
+++ server/tests/src/test/java/org/infinispan/server/resilience/StatefulSetRollingUpgradeIT.java
@@ -3,6 +3,8 @@ package org.infinispan.server.resilience;
 import static org.infinispan.server.security.Common.sync;
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
 
@@ -13,43 +15,61 @@ import org.infinispan.server.test.core.ServerRunMode;
 import org.infinispan.server.test.junit4.InfinispanServerRule;
 import org.infinispan.server.test.junit4.InfinispanServerRuleBuilder;
 import org.infinispan.server.test.junit4.InfinispanServerTestMethodRule;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
 import io.netty.handler.codec.http.HttpResponseStatus;
 
 /**
- * Start cluster (A,B) redeploy after upgrade. Rolling upgrades always occur in the order B,A and A does not restart
- * until B has completed successfully.
+ * Start cluster (0..numServers) redeploy after upgrade. Rolling upgrades always occur in the order numServers...0 and
+ * the node with index numServers - 1 does not restart until node numServers has completed successfully.
  *
  * @author Ryan Emerson
  * @since 11.0
  */
+@RunWith(Parameterized.class)
 public class StatefulSetRollingUpgradeIT {
 
-   private static final int NUM_NODES = 2;
    private static final int NUM_ROLLING_UPGRADES = 4;
    private static final String CACHE_MANAGER = "default";
 
-   @ClassRule
-   public static final InfinispanServerRule SERVER =
-         InfinispanServerRuleBuilder.config("configuration/ClusteredServerTest.xml")
-               .numServers(NUM_NODES)
-               .runMode(ServerRunMode.CONTAINER)
-               .parallelStartup(false)
-               .build();
+   private final int numServers;
+   private InfinispanServerRule serverRule;
+   private InfinispanServerTestMethodRule methodRule;
+
+   @Parameterized.Parameters(name = "{0}")
+   public static Collection<Object[]> data() {
+      return Arrays.asList(new Object[][]{{2}, {3}, {4}, {5}});
+   }
+
+   public StatefulSetRollingUpgradeIT(int numServers) {
+      this.numServers = numServers;
+   }
 
    @Rule
-   public InfinispanServerTestMethodRule SERVER_TEST = new InfinispanServerTestMethodRule(SERVER);
+   public RuleChain getRuleChain() {
+      serverRule = InfinispanServerRuleBuilder.config("configuration/ClusteredServerTest.xml")
+            .numServers(numServers)
+            .runMode(ServerRunMode.CONTAINER)
+            .parallelStartup(false)
+            .build();
+
+      methodRule = new InfinispanServerTestMethodRule(serverRule);
+
+      return RuleChain.outerRule(serverRule)
+            .around(methodRule);
+   }
 
    @Test
    public void testStatefulSetRollingUpgrade() {
-      IntStream.range(0, NUM_NODES).forEach(this::assertLiveness);
-      ContainerInfinispanServerDriver serverDriver = (ContainerInfinispanServerDriver) SERVER.getServerDriver();
+      IntStream.range(0, numServers).forEach(this::assertLiveness);
+      ContainerInfinispanServerDriver serverDriver = (ContainerInfinispanServerDriver) serverRule.getServerDriver();
 
-      for (int i  = 0; i < NUM_ROLLING_UPGRADES; i++) {
-         for (int j = NUM_NODES - 1; j > -1; j--) {
+      for (int i = 0; i < NUM_ROLLING_UPGRADES; i++) {
+         for (int j = numServers - 1; j > -1; j--) {
             serverDriver.sigterm(j);
             serverDriver.restart(j);
             assertLiveness(j);
@@ -58,8 +78,8 @@ public class StatefulSetRollingUpgradeIT {
    }
 
    private void assertLiveness(int server) {
-      RestClient rest = SERVER_TEST.rest().get(server);
-      RestResponse rsp = sync(rest.cacheManager(CACHE_MANAGER).healthStatus(), 10,  TimeUnit.SECONDS);
+      RestClient rest = methodRule.rest().get(server);
+      RestResponse rsp = sync(rest.cacheManager(CACHE_MANAGER).healthStatus(), 10, TimeUnit.SECONDS);
       assertEquals(HttpResponseStatus.OK.code(), rsp.getStatus());
    }
 }
