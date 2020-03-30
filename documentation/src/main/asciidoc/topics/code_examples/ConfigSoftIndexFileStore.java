49249f4b6ebda2764e822df72016d267b3fd6980
Don Naro <don.naro.jr@gmail.com>
2020-03-30 10:07:24 +0100

    ISPN-11517 set global persistent location for file-based cache stores

--- documentation/src/main/asciidoc/topics/code_examples/ConfigSoftIndexFileStore.java
+++ documentation/src/main/asciidoc/topics/code_examples/ConfigSoftIndexFileStore.java
@@ -1,5 +1,5 @@
 ConfigurationBuilder b = new ConfigurationBuilder();
 b.persistence()
     .addStore(SoftIndexFileStoreConfigurationBuilder.class)
-        .indexLocation("/tmp/sifs/testCache/index");
-        .dataLocation("/tmp/sifs/testCache/data")
+        .indexLocation("testCache/index");
+        .dataLocation("testCache/data")
