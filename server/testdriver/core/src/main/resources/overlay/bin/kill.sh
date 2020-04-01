20d4b4a5ed689d9156cd2df92f768e584712847f
Tristan Tarrant <ttarrant@redhat.com>
2020-04-01 15:32:36 +0200

    ISPN-11582 Use pgrep instead of jps for server resilience tests

--- server/testdriver/core/src/main/resources/overlay/bin/kill.sh
+++ server/testdriver/core/src/main/resources/overlay/bin/kill.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-PID=`jps -lm | grep "org.infinispan.server.loader.Loader org.infinispan.server.Bootstrap" | cut -f1 -d' '`
+PID=`pgrep -f "org.infinispan.server.loader.Loader org.infinispan.server.Bootstrap"`
 kill -SIGKILL $PID
 echo "Killed server with PID=$PID"
