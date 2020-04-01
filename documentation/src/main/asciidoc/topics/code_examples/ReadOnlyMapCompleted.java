e93d24e3190aeba841b2bd5e9761c0fca574337b
Don Naro <dnaro@redhat.com>
2020-04-01 16:11:17 +0100

    ISPN-11579 pruivo fixes functional_api.adoc

--- documentation/src/main/asciidoc/topics/code_examples/ReadOnlyMapCompleted.java
+++ documentation/src/main/asciidoc/topics/code_examples/ReadOnlyMapCompleted.java
@@ -1,6 +1,6 @@
-import org.infinispan.commons.api.functional.EntryView.*;
-import org.infinispan.commons.api.functional.FunctionalMap.*;
-import org.infinispan.commons.api.functional.Param.*;
+import org.infinispan.functional.EntryView.*;
+import org.infinispan.functional.FunctionalMap.*;
+import org.infinispan.functional.Param.*;
 
 ReadOnlyMap<String, String> readOnlyMap = ...
 ReadOnlyMap<String, String> readOnlyMapCompleted = readOnlyMap.withParams(FutureMode.COMPLETED);
