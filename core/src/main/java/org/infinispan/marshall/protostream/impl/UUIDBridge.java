736c0fa0906b7bfa3b5e05a9037a1e63a27f94d1
Ryan Emerson <remerson@redhat.com>
2020-04-08 12:57:37 +0100

    ISPN-11426 Add UUID to core SerializationContexts
    Utilise FIXED64 in UUIDBridge

--- core/src/main/java/org/infinispan/marshall/protostream/impl/UUIDBridge.java
+++ core/src/main/java/org/infinispan/marshall/protostream/impl/UUIDBridge.java
@@ -14,17 +14,31 @@ import org.infinispan.protostream.descriptors.Type;
 public class UUIDBridge {
 
    @ProtoFactory
-   UUID create(long mostSigBits, long leastSigBits) {
+   UUID create(Long mostSigBits, Long leastSigBits, Long mostSigBitsFixed, Long leastSigBitsFixed) {
+      if (mostSigBits == null)
+         return new UUID(mostSigBitsFixed, leastSigBitsFixed);
+
+      // Create the UUID using the old fields
       return new UUID(mostSigBits, leastSigBits);
    }
 
-   @ProtoField(number = 1, type = Type.UINT64, defaultValue = "0")
-   long getMostSigBits(UUID uuid) {
+   @ProtoField(number = 1, type = Type.UINT64)
+   Long getMostSigBits(UUID uuid) {
+      return null;
+   }
+
+   @ProtoField(number = 2, type = Type.UINT64)
+   Long getLeastSigBits(UUID uuid) {
+      return null;
+   }
+
+   @ProtoField(number = 3, type = Type.FIXED64, defaultValue = "0")
+   Long getMostSigBitsFixed(UUID uuid) {
       return uuid.getMostSignificantBits();
    }
 
-   @ProtoField(number = 2, type = Type.UINT64, defaultValue = "0")
-   long getLeastSigBits(UUID uuid) {
+   @ProtoField(number = 4, type = Type.FIXED64, defaultValue = "0")
+   Long getLeastSigBitsFixed(UUID uuid) {
       return uuid.getLeastSignificantBits();
    }
 }
