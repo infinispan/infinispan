5f22a44db59da90b654b04b0199cf0bd34a775a0
Gustavo Fernandes <gustavonalle@gmail.com>
2020-04-16 09:37:36 +0100

    ISPN-11649 Remove Wildfly Modules

--- wildfly/infinispan/src/main/java/org/infinispan/server/infinispan/spi/InfinispanSubsystem.java
+++
@@ -1,9 +0,0 @@
-package org.infinispan.server.infinispan.spi;
-
-import org.jboss.msc.service.ServiceName;
-
-public class InfinispanSubsystem {
-   public static final String SUBSYSTEM_NAME = "datagrid-infinispan";
-
-   public static final ServiceName SERVICE_NAME_BASE = ServiceName.JBOSS.append(InfinispanSubsystem.SUBSYSTEM_NAME);
-}
