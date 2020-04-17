f9e8227e2f159ec02ce7e4d7cd4c5636795dedfe
Tristan Tarrant <ttarrant@redhat.com>
2020-04-17 17:46:02 +0200

    ISPN-11642 Allow supplying hotrod-client.properties via URI in JCache remote

---
+++ documentation/src/main/asciidoc/topics/code_examples/JCacheCreateCacheCustomConfig.java
@@ -0,0 +1,11 @@
+import java.net.URI;
+import javax.cache.*;
+import javax.cache.configuration.*;
+
+// Load configuration from an absolute filesystem path
+URI uri = URI.create("file:///path/to/infinispan.xml");
+// Load configuration from a classpath resource
+// URI uri = this.getClass().getClassLoader().getResource("infinispan.xml").toURI();
+
+// Create a cache manager using the above configuration
+CacheManager cacheManager = Caching.getCachingProvider().getCacheManager(uri, this.getClass().getClassLoader(), null);
