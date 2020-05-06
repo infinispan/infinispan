9e5f49db8eadf09ce7d901ccc913d323f974b01a
Adrian Nistor <anistor@redhat.com>
2020-05-06 11:47:06 +0300

    ISPN-11773 Package org.infinispan.context is missing from javadocs of 10.1.x and 11 releases

--- core/src/main/java/org/infinispan/context/package-info.java
+++ core/src/main/java/org/infinispan/context/package-info.java
@@ -1,5 +1,7 @@
 /**
  * Contexts contain information of a specific invocation on the cache, such as its origins, scope
  * (transactional or non-transactional), as well as invocation-specific flags.
+ *
+ * @public
  */
 package org.infinispan.context;
