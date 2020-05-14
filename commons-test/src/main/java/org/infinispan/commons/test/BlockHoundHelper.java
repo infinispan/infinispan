482293c5fd5cdcb8473d3cad355949dc66adced0
William Burns <mudokonman@gmail.com>
2020-05-14 13:41:17 -0400

    ISPN-11830 Provide invocation based non blocking detection to test suite

--- commons-test/src/main/java/org/infinispan/commons/test/BlockHoundHelper.java
+++ commons-test/src/main/java/org/infinispan/commons/test/BlockHoundHelper.java
@@ -1,8 +1,11 @@
 package org.infinispan.commons.test;
 
+import java.util.concurrent.Executor;
+import java.util.function.Supplier;
+
 import reactor.blockhound.BlockHound;
 
-class BlockHoundHelper {
+public class BlockHoundHelper {
    private BlockHoundHelper() { }
 
    /**
@@ -10,6 +13,62 @@ class BlockHoundHelper {
     */
    static void installBlockHound() {
       // Automatically registers all services that implement BlockHoundIntegration interface
-      BlockHound.install();
+
+      // This is a terrible hack but gets around the issue that blockhound doesn't allow the registering thread
+      // to be a dynamic blocking thread - in which case our checks in
+      // AbstractInfinispanTest#currentThreadRequiresNonBlocking will never be evaluated
+      // To be removed when BlockHound 1.0.4.RELEASE is available
+      Thread otherThread = new Thread(BlockHound::install);
+      otherThread.start();
+      try {
+         otherThread.join();
+      } catch (InterruptedException e) {
+         throw new RuntimeException(e);
+      }
+   }
+
+   /**
+    * Returns true if the current thread at this time requires all invocations to be non blocking
+    */
+   public static boolean currentThreadRequiresNonBlocking() {
+      return isNonBlocking.get() == Boolean.TRUE;
+   }
+
+   private static final ThreadLocal<Boolean> isNonBlocking = ThreadLocal.withInitial(() -> Boolean.FALSE);
+
+   /**
+    * Invokes the provided Supplier in a scope that guarantees that the current thread is not blocked. If blocking is
+    * found the invoking test will fail.
+    */
+   public static <V> V ensureNonBlocking(Supplier<V> supplier) {
+      Boolean previousSetting = isNonBlocking.get();
+      isNonBlocking.set(Boolean.TRUE);
+      try {
+         return supplier.get();
+      } finally {
+         isNonBlocking.set(previousSetting);
+      }
+   }
+
+   /**
+    * Invokes the provided Runnable in a scope that guarantees that the current thread is not blocked. If blocking is
+    * found the invoking test will fail.
+    */
+   public static void ensureNonBlocking(Runnable runnable) {
+      Boolean previousSetting = isNonBlocking.get();
+      isNonBlocking.set(Boolean.TRUE);
+      try {
+         runnable.run();
+      } finally {
+         isNonBlocking.set(previousSetting);
+      }
+   }
+
+   /**
+    * Returns an Executor that when supplied a task, will guarantee that task does not block when invoked. If the
+    * task does block it will fail the invoking test.
+    */
+   public static Executor ensureNonBlockingExecutor() {
+      return BlockHoundHelper::ensureNonBlocking;
    }
 }
