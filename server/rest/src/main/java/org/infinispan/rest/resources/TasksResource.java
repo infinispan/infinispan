6eb41f09e54b53b48d7980a2dfb8c13defa38247
Ryan Emerson <remerson@redhat.com>
2020-05-29 12:44:06 +0100

    ISPN-11935 Add REST resource util class

--- server/rest/src/main/java/org/infinispan/rest/resources/TasksResource.java
+++ server/rest/src/main/java/org/infinispan/rest/resources/TasksResource.java
@@ -2,15 +2,18 @@ package org.infinispan.rest.resources;
 
 import static java.util.concurrent.CompletableFuture.completedFuture;
 import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_JAVASCRIPT;
-import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_JSON_TYPE;
 import static org.infinispan.commons.dataconversion.MediaType.TEXT_PLAIN_TYPE;
 import static org.infinispan.rest.framework.Method.GET;
 import static org.infinispan.rest.framework.Method.POST;
 import static org.infinispan.rest.framework.Method.PUT;
+import static org.infinispan.rest.resources.ResourceUtil.addEntityAsJson;
+import static org.infinispan.rest.resources.ResourceUtil.asJsonResponseFuture;
 
 import java.security.PrivilegedAction;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
+
+import javax.security.auth.Subject;
 
 import org.infinispan.commons.dataconversion.MediaType;
 import org.infinispan.commons.dataconversion.StandardConversions;
@@ -26,12 +29,6 @@ import org.infinispan.scripting.ScriptingManager;
 import org.infinispan.tasks.Task;
 import org.infinispan.tasks.TaskContext;
 import org.infinispan.tasks.TaskManager;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-
-import io.netty.handler.codec.http.HttpResponseStatus;
-
-import javax.security.auth.Subject;
 
 /**
  * @since 10.1
@@ -58,15 +55,8 @@ public class TasksResource implements ResourceHandler {
 
       EmbeddedCacheManager cacheManager = invocationHelper.getRestCacheManager().getInstance();
       TaskManager taskManager = SecurityActions.getGlobalComponentRegistry(cacheManager).getComponent(TaskManager.class);
-      NettyRestResponse.Builder builder = new NettyRestResponse.Builder();
-      try {
-         List<Task> tasks = userOnly ? taskManager.getUserTasks() : taskManager.getTasks();
-         byte[] resultBytes = invocationHelper.getMapper().writeValueAsBytes(tasks);
-         builder.contentType(APPLICATION_JSON_TYPE).entity(resultBytes);
-      } catch (JsonProcessingException e) {
-         builder.status(HttpResponseStatus.INTERNAL_SERVER_ERROR).entity(e.getMessage());
-      }
-      return completedFuture(builder.build());
+      List<Task> tasks = userOnly ? taskManager.getUserTasks() : taskManager.getTasks();
+      return asJsonResponseFuture(tasks, invocationHelper);
    }
 
    private CompletionStage<RestResponse> createScriptTask(RestRequest request) {
@@ -101,15 +91,10 @@ public class TasksResource implements ResourceHandler {
 
       return runResult.thenApply(result -> {
          NettyRestResponse.Builder builder = new NettyRestResponse.Builder();
-         try {
-            if (result instanceof byte[]) {
-               builder.contentType(TEXT_PLAIN_TYPE).entity(result);
-            } else {
-               byte[] resultBytes = invocationHelper.getMapper().writeValueAsBytes(result);
-               builder.contentType(APPLICATION_JSON_TYPE).entity(resultBytes);
-            }
-         } catch (JsonProcessingException e) {
-            builder.status(HttpResponseStatus.INTERNAL_SERVER_ERROR).entity(e.getMessage());
+         if (result instanceof byte[]) {
+            builder.contentType(TEXT_PLAIN_TYPE).entity(result);
+         } else {
+            addEntityAsJson(result, builder, invocationHelper);
          }
          return builder.build();
       });
