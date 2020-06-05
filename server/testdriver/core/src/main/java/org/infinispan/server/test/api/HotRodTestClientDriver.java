d9a8f9ec0f0cc7edc9bfe361d9fc7b80b869da8f
Ryan Emerson <remerson@redhat.com>
2020-06-05 12:52:46 +0100

    ISPN-11948 server/tests HotRod clients should utilise the ProtoStreamMarshaller

--- server/testdriver/core/src/main/java/org/infinispan/server/test/api/HotRodTestClientDriver.java
+++ server/testdriver/core/src/main/java/org/infinispan/server/test/api/HotRodTestClientDriver.java
@@ -4,6 +4,8 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ClientIntelligence;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
+import org.infinispan.commons.marshall.Marshaller;
+import org.infinispan.commons.marshall.ProtoStreamMarshaller;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.server.test.core.TestClient;
 import org.infinispan.server.test.core.TestServer;
@@ -31,18 +33,32 @@ public class HotRodTestClientDriver extends BaseTestClientDriver<HotRodTestClien
          // Configure admin user by default
          builder.security().authentication().username(TestUser.ADMIN.getUser()).password(TestUser.ADMIN.getPassword());
       }
-
       this.clientConfiguration = builder;
    }
 
    /**
-    * Provide a custom client configuration to connect to the server
+    * Provide a custom client configuration to connect to the server. This method always overrides the configured marshaller
+    * so if a marshaller other than the default {@link ProtoStreamMarshaller} is required, a subsequent call to
+    * {@link #withMarshaller(Class)} is required.
     *
     * @param clientConfiguration
     * @return the current {@link HotRodTestClientDriver} instance with the client configuration override
     */
    public HotRodTestClientDriver withClientConfiguration(ConfigurationBuilder clientConfiguration) {
       this.clientConfiguration = clientConfiguration;
+      // Explicitly configure the ProtoStreamMarshaller as the jboss-marshalling module is on the classpath
+      this.clientConfiguration.marshaller(ProtoStreamMarshaller.class);
+      return this;
+   }
+
+   /**
+    * The {@link Marshaller} to be used by the client.
+    *
+    * @param marshallerClass
+    * @return the current {@link HotRodTestClientDriver} instance with the Marshaller configuration override
+    */
+   public HotRodTestClientDriver withMarshaller(Class<? extends Marshaller> marshallerClass) {
+      this.clientConfiguration.marshaller(marshallerClass);
       return this;
    }
 
