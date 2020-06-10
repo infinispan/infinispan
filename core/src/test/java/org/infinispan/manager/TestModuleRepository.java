e817fa66e7d057cbc15a1d41a697b7791d783b6d
Adrian Nistor <anistor@redhat.com>
2020-06-10 20:58:38 +0300

    TestModuleRepository relies on package-local stuff located in another java module
    * refactor to avoid module access violations
    * make TestModuleRepository final
    * generify several usages of ComponentAccessor

--- core/src/test/java/org/infinispan/manager/TestModuleRepository.java
+++ core/src/test/java/org/infinispan/manager/TestModuleRepository.java
@@ -1,18 +1,14 @@
 package org.infinispan.manager;
 
-import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 
 /**
- * Allow tests to create instances of {@link ModuleRepository}.
+ * Provide a default {@link ModuleRepository} for tests.
  */
-public class TestModuleRepository {
-   private static final ModuleRepository MODULE_REPOSITORY =
-      new ModuleRepository.Builder(TestModuleRepository.class.getClassLoader()).build(new GlobalConfigurationBuilder().build());
+public final class TestModuleRepository {
 
-   public static ModuleRepository newModuleRepository(ClassLoader classLoader, GlobalConfiguration globalConfiguration) {
-      return new ModuleRepository.Builder(classLoader).build(globalConfiguration);
-   }
+   private static final ModuleRepository MODULE_REPOSITORY = ModuleRepository.newModuleRepository(
+         TestModuleRepository.class.getClassLoader(), new GlobalConfigurationBuilder().build());
 
    public static ModuleRepository defaultModuleRepository() {
       return MODULE_REPOSITORY;
