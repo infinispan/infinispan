2e323cc5d33592a3bed5e375afd36b9cebe1920d
Gustavo Fernandes <gustavonalle@gmail.com>
2020-06-11 19:41:04 +0100

    Fix ClusteredDistCacheTest
    This test uses "null" as storageType since it does not override the
    factory from the parent. Before the fix on ISPN-11980, this was
    assumed to be a protostream cache, but now it is correctly assumed
    as an application/x-java-object and thus the key is not byte[] anymore,
    requiring a transformer

--- query/src/test/java/org/infinispan/query/blackbox/ClusteredDistCacheTest.java
+++ query/src/test/java/org/infinispan/query/blackbox/ClusteredDistCacheTest.java
@@ -4,6 +4,8 @@ import org.infinispan.Cache;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.query.helper.StaticTestingErrorHandler;
+import org.infinispan.query.test.CustomKey3;
+import org.infinispan.query.test.CustomKey3Transformer;
 import org.infinispan.query.test.Person;
 import org.infinispan.query.test.QueryTestSCI;
 import org.testng.annotations.Test;
@@ -21,11 +23,11 @@ public class ClusteredDistCacheTest extends ClusteredCacheTest {
       ConfigurationBuilder cacheCfg = getDefaultClusteredCacheConfig(CacheMode.DIST_SYNC, false);
       cacheCfg.indexing()
             .enable()
+            .addKeyTransformer(CustomKey3.class, CustomKey3Transformer.class)
             .addIndexedEntity(Person.class)
             .addProperty("default.directory_provider", "local-heap")
             .addProperty("error_handler", StaticTestingErrorHandler.class.getName());
 
-      cacheCfg.memory().storageType(storageType);
       enhanceConfig(cacheCfg);
       createClusteredCaches(3, QueryTestSCI.INSTANCE, cacheCfg);
       cache1 = cache(0);
