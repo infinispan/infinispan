f88c5668ec574ece98f0bd1835ad076ddbfabe25
Tristan Tarrant <ttarrant@redhat.com>
2020-06-22 13:41:01 +0200

    ISPN-12038 Fix RocksDB compression options
    * add xpress and xstd types
    * map the Infinispan parser names to the underlying byte value instead of using strings
    * don't overwrite compression properties with the configuration value if it has not been set

--- persistence/rocksdb/src/main/java/org/infinispan/persistence/rocksdb/configuration/CompressionType.java
+++ persistence/rocksdb/src/main/java/org/infinispan/persistence/rocksdb/configuration/CompressionType.java
@@ -1,10 +1,22 @@
 package org.infinispan.persistence.rocksdb.configuration;
 
 public enum CompressionType {
-   NONE,
-   SNAPPY,
-   ZLIB,
-   BZLIB2,
-   LZ4,
-   LZ4HC
+   NONE(org.rocksdb.CompressionType.NO_COMPRESSION),
+   SNAPPY(org.rocksdb.CompressionType.SNAPPY_COMPRESSION),
+   ZLIB(org.rocksdb.CompressionType.ZLIB_COMPRESSION),
+   BZLIB2(org.rocksdb.CompressionType.BZLIB2_COMPRESSION),
+   LZ4(org.rocksdb.CompressionType.LZ4_COMPRESSION),
+   LZ4HC(org.rocksdb.CompressionType.LZ4HC_COMPRESSION),
+   XPRESS(org.rocksdb.CompressionType.XPRESS_COMPRESSION),
+   ZSTD(org.rocksdb.CompressionType.ZSTD_COMPRESSION);
+
+   private final org.rocksdb.CompressionType value;
+
+   CompressionType(org.rocksdb.CompressionType value) {
+      this.value = value;
+   }
+
+   public org.rocksdb.CompressionType getValue() {
+      return value;
+   }
 }
