3d631235788d5e4747a2f8c01a5bea70350c20fa
Dan Berindei <dan@infinispan.org>
2020-07-16 20:03:28 +0300

    ISPN-11304 Validate anchored keys configuration requirements
    * Require replicated mode
    * Disallow transactions
    * Require state transfer w/out await-initial-transfer
    * Require ALLOW_READ_WRITES and merge policy PREFERRED_NON_NULL

--- anchored-keys/src/main/java/org/infinispan/anchored/configuration/AnchoredKeysConfigurationBuilder.java
+++ anchored-keys/src/main/java/org/infinispan/anchored/configuration/AnchoredKeysConfigurationBuilder.java
@@ -1,11 +1,18 @@
 package org.infinispan.anchored.configuration;
 
-import org.infinispan.commons.CacheConfigurationException;
+import static org.infinispan.anchored.impl.Log.CONFIG;
+
 import org.infinispan.commons.configuration.Builder;
+import org.infinispan.commons.configuration.attributes.Attribute;
 import org.infinispan.commons.configuration.attributes.AttributeSet;
 import org.infinispan.commons.util.Experimental;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.PartitionHandlingConfiguration;
+import org.infinispan.configuration.cache.StateTransferConfiguration;
+import org.infinispan.conflict.EntryMergePolicy;
+import org.infinispan.conflict.MergePolicy;
+import org.infinispan.partitionhandling.PartitionHandling;
 
 /**
  * Configuration module builder to transform an {@link CacheMode#INVALIDATION_SYNC} cache into an "anchored keys" cache.
@@ -42,14 +49,42 @@ public class AnchoredKeysConfigurationBuilder implements Builder<AnchoredKeysCon
    @Override
    public void validate() {
       if (!rootBuilder.clustering().cacheMode().isReplicated()) {
-         throw new CacheConfigurationException("Anchored keys requires cache to be in replication mode");
+         throw CONFIG.replicationModeRequired();
       }
-      if (rootBuilder.transaction().transactionMode() != null && rootBuilder.transaction().transactionMode().isTransactional()) {
-         throw new CacheConfigurationException("Anchored keys do not support transactions");
+      if (rootBuilder.transaction().transactionMode() != null &&
+          rootBuilder.transaction().transactionMode().isTransactional()) {
+         throw CONFIG.transactionsNotSupported();
+      }
+      Attribute<Boolean> stateTransferEnabledAttribute =
+            rootBuilder.clustering().stateTransfer().attributes()
+                       .attribute(StateTransferConfiguration.FETCH_IN_MEMORY_STATE);
+      if (!stateTransferEnabledAttribute.get()) {
+         throw CONFIG.stateTransferRequired();
       }
 
-      // TODO Maybe just assert that awaitInitialTransfer is disabled instead?
+      Attribute<Boolean> awaitStateTransferAttribute =
+            rootBuilder.clustering().stateTransfer().attributes()
+                       .attribute(StateTransferConfiguration.AWAIT_INITIAL_TRANSFER);
+      if (awaitStateTransferAttribute.get() && awaitStateTransferAttribute.isModified()) {
+         throw CONFIG.awaitInitialTransferNotSupported();
+      }
       rootBuilder.clustering().stateTransfer().awaitInitialTransfer(false);
+
+      Attribute<PartitionHandling> whenSplitAttribute =
+            rootBuilder.clustering().partitionHandling().attributes()
+                       .attribute(PartitionHandlingConfiguration.WHEN_SPLIT);
+      if (whenSplitAttribute.get() != PartitionHandling.ALLOW_READ_WRITES && whenSplitAttribute.isModified()) {
+         throw CONFIG.whenSplitNotSupported();
+      }
+      rootBuilder.clustering().partitionHandling().whenSplit(PartitionHandling.ALLOW_READ_WRITES);
+
+      Attribute<EntryMergePolicy> mergePolicyAttribute =
+            rootBuilder.clustering().partitionHandling().attributes()
+                       .attribute(PartitionHandlingConfiguration.MERGE_POLICY);
+      if (mergePolicyAttribute.get() != MergePolicy.PREFERRED_NON_NULL && mergePolicyAttribute.isModified()) {
+         throw CONFIG.mergePolicyNotSupported();
+      }
+      rootBuilder.clustering().partitionHandling().mergePolicy(MergePolicy.PREFERRED_NON_NULL);
    }
 
    @Override
