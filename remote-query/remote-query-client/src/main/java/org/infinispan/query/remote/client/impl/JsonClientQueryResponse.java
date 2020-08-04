06168ca63eabd56bef8900474df56fdd5a3d9e74
Gustavo Fernandes <gustavonalle@gmail.com>
2020-08-04 15:20:46 +0100

    ISPN-12191 Remove GSON

--- remote-query/remote-query-client/src/main/java/org/infinispan/query/remote/client/impl/JsonClientQueryResponse.java
+++ remote-query/remote-query-client/src/main/java/org/infinispan/query/remote/client/impl/JsonClientQueryResponse.java
@@ -1,40 +1,34 @@
 package org.infinispan.query.remote.client.impl;
 
-import static java.util.stream.StreamSupport.stream;
-
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.infinispan.commons.dataconversion.internal.Json;
 import org.infinispan.protostream.SerializationContext;
-
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
 /**
  * @since 9.4
  */
-public class JsonClientQueryResponse implements BaseQueryResponse {
+public class JsonClientQueryResponse implements BaseQueryResponse<String> {
 
    private static final String JSON_TOTAL_RESULTS = "total_results";
    private static final String JSON_HITS = "hits";
    private static final String JSON_HIT = "hit";
 
-   private final JsonObject jsonObject;
+   private final Json jsonObject;
 
-   public JsonClientQueryResponse(JsonObject jsonObject) {
+   public JsonClientQueryResponse(Json jsonObject) {
       this.jsonObject = jsonObject;
    }
 
    @Override
-   public List<?> extractResults(SerializationContext serializationContext) {
-      JsonArray hits = jsonObject.get(JSON_HITS).getAsJsonArray();
-      return stream(hits.spliterator(), false)
-            .map(hit -> hit.getAsJsonObject().get(JSON_HIT).toString())
+   public List<String> extractResults(SerializationContext serializationContext) {
+      return jsonObject.at(JSON_HITS).asJsonList().stream().map(j -> j.at(JSON_HIT).toString())
             .collect(Collectors.toList());
    }
 
    @Override
    public long getTotalResults() {
-      return jsonObject.get(JSON_TOTAL_RESULTS).getAsInt();
+      return jsonObject.at(JSON_TOTAL_RESULTS).asInteger();
    }
 }
