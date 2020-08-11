231bc031f97b69adc184ffba3816643e9c606240
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2020-08-11 15:09:00 +0200

    ISPN-12169 Avoid to create search mapping when registerProtoFile
    There is no reason to keep doing this, since all protobuf indexed entities must be defined at cache starting time.

--- remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/mapping/SerializationContextSearchMapping.java
+++ remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/mapping/SerializationContextSearchMapping.java
@@ -1,13 +1,9 @@
 package org.infinispan.query.remote.impl.mapping;
 
-import java.util.Arrays;
 import java.util.Set;
 
 import org.hibernate.search.mapper.pojo.mapping.definition.programmatic.ProgrammaticMappingConfigurationContext;
-import org.infinispan.Cache;
-import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.protostream.SerializationContext;
-import org.infinispan.query.impl.ComponentRegistryUtils;
 import org.infinispan.query.remote.impl.indexing.ProtobufEntityConverter;
 import org.infinispan.query.remote.impl.mapping.model.ProtobufBootstrapIntrospector;
 import org.infinispan.query.remote.impl.mapping.reference.GlobalReferenceHolder;
@@ -17,8 +13,6 @@ import org.infinispan.search.mapper.mapping.SearchMappingHolder;
 
 public class SerializationContextSearchMapping {
 
-   private static final String[] EXCLUDE_CACHE_NAMES = {"___script_cache", "___protobuf_metadata"};
-
    private final SerializationContext serializationContext;
 
    public static SerializationContextSearchMapping acquire(SerializationContext serializationContext) {
@@ -27,27 +21,6 @@ public class SerializationContextSearchMapping {
 
    private SerializationContextSearchMapping(SerializationContext serializationContext) {
       this.serializationContext = serializationContext;
-   }
-
-   public void updateSearchMappingHolders(EmbeddedCacheManager cacheManager) {
-      for (String cacheName : cacheManager.getCacheConfigurationNames()) {
-         if (Arrays.asList(EXCLUDE_CACHE_NAMES).contains(cacheName)) {
-            continue;
-         }
-
-         // if the cache has not started yet, there is no reason to create SearchMapping,
-         // it will be created at `org.infinispan.query.remote.impl.LifecycleManager#cacheStarting`,
-         // that is the best way for Search 6.
-         if (!cacheManager.isRunning(cacheName)) {
-            continue;
-         }
-
-         Cache<Object, Object> cache = cacheManager.getCache(cacheName);
-         SearchMappingHolder searchMappingHolder = ComponentRegistryUtils.getSearchMappingHolder(cache);
-         if (searchMappingHolder != null) {
-            buildMapping(searchMappingHolder, cache.getCacheConfiguration().indexing().indexedEntityTypes());
-         }
-      }
    }
 
    public void buildMapping(SearchMappingHolder mappingHolder, Set<String> indexedEntityTypes) {
