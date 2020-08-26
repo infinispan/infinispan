3824f0714e415ef9aede98cc2a8549bfba3d518a
Dan Berindei <dan@infinispan.org>
2020-08-26 17:49:39 +0300

    ISPN-12256 InfinispanServerExtensionContainerTest fails to start
    * Make InfinispanServerExtensionContainerTest use custom configuration
    * Add test-tcp and test-udp stacks to custom configuration
    * Remove surefire <argLine> element and use args from parent POM
    * Define infinispan.cluster.stack=test-tcp in all testdriver modules

--- server/testdriver/junit4/src/main/java/org/infinispan/server/test/junit4/InfinispanServerRuleBuilder.java
+++ server/testdriver/junit4/src/main/java/org/infinispan/server/test/junit4/InfinispanServerRuleBuilder.java
@@ -1,9 +1,9 @@
 package org.infinispan.server.test.junit4;
 
+import static org.infinispan.server.test.core.AbstractInfinispanServerDriver.DEFAULT_CLUSTERED_INFINISPAN_CONFIG_FILE_NAME;
+
 import org.infinispan.server.test.core.AbstractServerConfigBuilder;
 import org.infinispan.server.test.core.ServerRunMode;
-
-import static org.infinispan.server.test.core.AbstractInfinispanServerDriver.DEFAULT_CLUSTERED_INFINISPAN_CONFIG_FILE_NAME;
 
 /**
  * Builder for {@link InfinispanServerRule}.
@@ -26,6 +26,14 @@ public class InfinispanServerRuleBuilder extends AbstractServerConfigBuilder<Inf
       return new InfinispanServerRuleBuilder(DEFAULT_CLUSTERED_INFINISPAN_CONFIG_FILE_NAME, true)
             .numServers(1)
             .runMode(container? ServerRunMode.CONTAINER : ServerRunMode.EMBEDDED)
+            .parallelStartup(false)
+            .build();
+   }
+
+   public static InfinispanServerRule server(String configurationFile) {
+      return new InfinispanServerRuleBuilder(configurationFile, false)
+            .numServers(1)
+            .runMode(ServerRunMode.CONTAINER)
             .parallelStartup(false)
             .build();
    }
