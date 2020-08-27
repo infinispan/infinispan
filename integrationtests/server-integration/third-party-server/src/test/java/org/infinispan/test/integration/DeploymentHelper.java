88ded7950fc42fb6c7c896df8f6f5975ded37760
Diego Lovison <diegolovison@gmail.com>
2020-08-27 13:58:42 -0300

    ISPN-12271 Allow different servers for server integration test suite

---
+++ integrationtests/server-integration/third-party-server/src/test/java/org/infinispan/test/integration/DeploymentHelper.java
@@ -0,0 +1,47 @@
+package org.infinispan.test.integration;
+
+import java.io.File;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.FileAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.maven.Maven;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenResolverSystem;
+
+public class DeploymentHelper {
+
+   public static WebArchive createDeployment() {
+
+      WebArchive war = ShrinkWrap.create(WebArchive.class, "infinispan-server-integration.war");
+      String arquillianLaunch = System.getProperty("arquillian.launch");
+
+      if (arquillianLaunch.equals("tomcat")) {
+         tomcat(war);
+      } else if (arquillianLaunch.equals("wildfly")) {
+         wildfly(war);
+      } else {
+         throw new IllegalStateException(String.format("%s not supported", arquillianLaunch));
+      }
+
+      MavenResolverSystem resolver = Maven.resolver();
+      addLibrary(war, resolver
+            .loadPomFromFile("pom.xml")
+            .resolve("org.infinispan:infinispan-client-hotrod")
+            .withTransitivity().as(File.class));
+      return war;
+   }
+
+   private static void wildfly(WebArchive war) {
+      war.add(new FileAsset(new File("src/test/resources/wildfly/jboss-deployment-structure.xml")), "WEB-INF/jboss-deployment-structure.xml");
+   }
+
+   private static void tomcat(WebArchive war) {
+      // intentionally empty. if you need to implement a custom deploy the code is 'ready'
+   }
+
+   private static void addLibrary(WebArchive war, File[]... libGroup) {
+      for (File[] group : libGroup) {
+         war.addAsLibraries(group);
+      }
+   }
+}
--- integrationtests/server-integration/wildfly-modules/src/test/java/org/infinispan/test/integration/as/client/HotRodClientIT.java
+++ integrationtests/server-integration/wildfly-modules/src/test/java/org/infinispan/test/integration/as/client/HotRodClientIT.java
@@ -1,11 +1,5 @@
 package org.infinispan.test.integration.as.client;
 
-import static org.junit.Assert.assertEquals;
-
-import org.infinispan.client.hotrod.RemoteCache;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.configuration.Configuration;
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.commons.util.Version;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
@@ -16,8 +10,6 @@ import org.jboss.shrinkwrap.api.asset.StringAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.jboss.shrinkwrap.descriptor.api.Descriptors;
 import org.jboss.shrinkwrap.descriptor.api.spec.se.manifest.ManifestDescriptor;
-import org.junit.After;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -27,46 +19,20 @@ import org.junit.runner.RunWith;
  * @since 7.0
  */
 @RunWith(Arquillian.class)
-public class HotRodClientIT {
-
-   private RemoteCacheManager rcm;
+public class HotRodClientIT extends AbstractHotRodClientIT {
 
    @Deployment
    public static Archive<?> deployment() {
       return ShrinkWrap
             .create(WebArchive.class, "remote-client.war")
             .addClass(HotRodClientIT.class)
+            .addClass(AbstractHotRodClientIT.class)
             .add(manifest(), "META-INF/MANIFEST.MF");
-   }
-
-   @After
-   public void cleanUp() {
-      if (rcm != null)
-         rcm.stop();
-   }
-
-   @Test
-   public void testCacheManager() {
-      rcm = createCacheManager();
-      RemoteCache<String, String> cache = rcm.getCache();
-      cache.clear();
-      cache.put("a", "a");
-      assertEquals("a", cache.get("a"));
    }
 
    private static Asset manifest() {
       String manifest = Descriptors.create(ManifestDescriptor.class).attribute("Dependencies", "org.infinispan:" + Version.getModuleSlot() + " services").exportAsString();
       return new StringAsset(manifest);
-   }
-
-   private static RemoteCacheManager createCacheManager() {
-      return new RemoteCacheManager(createConfiguration(), true);
-   }
-
-   private static Configuration createConfiguration() {
-      ConfigurationBuilder config = new ConfigurationBuilder();
-      config.addServer().host("127.0.0.1");
-      return config.build();
    }
 
 }
