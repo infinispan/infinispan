9d0b1f2d3a8fdba4b637a10f011825b1f71202fd
Radoslav Husar <radosoft@gmail.com>
2020-09-29 21:18:59 +0200

    ISPN-12368 ServerRunMode.FORKED always leaks running server processes when cluster shutdown fails

--- server/testdriver/core/src/main/java/org/infinispan/server/test/core/ForkedServer.java
+++ server/testdriver/core/src/main/java/org/infinispan/server/test/core/ForkedServer.java
@@ -1,6 +1,7 @@
 package org.infinispan.server.test.core;
 
-import static org.infinispan.commons.test.Exceptions.unchecked;
+import org.infinispan.commons.logging.Log;
+import org.infinispan.commons.util.OS;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,6 +11,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,11 +21,20 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
 
-import org.infinispan.commons.logging.Log;
-import org.infinispan.commons.util.OS;
+import static org.infinispan.commons.test.Exceptions.unchecked;
 
 /**
+ * Forked server starts the server using batch scripts from the Infinispan Server distribution to start the server.
+ * The driver uses REST API to shutdown the server.
+ * Server log is cleaned before start and is not configurable.
+ * Appends random UUID for server process to identify the corresponding child Java process of the server.
+ *
+ * FIXME: Add proper Windows platform support.
+ * FIXME: The server.log file must be present before starting the server as the monitoring process is pointed to it -
+ *        - groundwork for making the purging of the log directory optional.
+ *
  * @author Gustavo Lira &lt;glira@redhat.com&gt;
+ * @author Radoslav Husar
  * @since 11.0
  **/
 public class ForkedServer {
@@ -32,25 +43,31 @@ public class ForkedServer {
 
    private static final String START_PATTERN = "ISPN080001";
 
-   private List<String> commands = new ArrayList<>();
+   private final List<String> commands = new ArrayList<>();
+   private final UUID serverId;
    private Process process;
-   private String serverHome, serverLogDir, serverLog;
+   private final String serverHome;
+   private final String serverLogDir;
+   private final String serverLog;
    public static final int TIMEOUT_SECONDS = Integer.getInteger(TestSystemPropertyNames.INFINISPAN_TEST_SERVER_FORKED_TIMEOUT_SECONDS, 30);
    public static final Integer DEFAULT_SINGLE_PORT = 11222;
    public static final int OFFSET_FACTOR = 100;
 
    public ForkedServer(String serverHome) {
+      this.serverId = UUID.randomUUID();
       this.serverHome = serverHome;
-      this.serverLogDir = serverHome + "/server/log";
-      this.serverLog = serverLogDir + "/server.log";
+      this.serverLogDir = serverHome + File.separator + "server" + File.separator + "log";
+      this.serverLog = serverLogDir + File.separator + "server.log";
       cleanServerLog();
       callInitScript();
    }
 
-   private ForkedServer callInitScript() {
+   private void callInitScript() {
       String extension = OS.getCurrentOs() == OS.WINDOWS ? ".bat" : ".sh";
-      commands.add(serverHome + String.format("/bin/server%s", extension));
-      return this;
+      commands.add(serverHome + File.separator + "bin" + File.separator + "server" + extension);
+
+      // Append random UUID for this server to be used in the case of forceful shutdown.
+      commands.add(String.format("-D%s-pid=%s", this.getClass().getName(), this.serverId));
    }
 
    public ForkedServer setServerConfiguration(String serverConfiguration) {
@@ -122,11 +139,35 @@ public class ForkedServer {
    }
 
    public File getServerLib() {
-      return Paths.get(serverHome + "/server/lib").toFile();
+      return Paths.get(serverHome + File.separator + "server" + File.separator + "lib").toFile();
    }
 
+   /**
+    * Returns the process ID of the Java process as opposed to the pid of the shell script which started the actual
+    * Java process. The process is matched containing the randomized UUID.
+    *
+    * @return process ID (pid) of the server's Java process.
+    */
    public long getPid() {
-      return org.infinispan.commons.jdkspecific.ProcessInfo.of(process).getPid();
+      try {
+         Process psProcess = Runtime.getRuntime().exec("ps");
+         BufferedReader input = new BufferedReader(new InputStreamReader(psProcess.getInputStream()));
+         String psLine;
+         while ((psLine = input.readLine()) != null) {
+            if (psLine.contains(this.serverId.toString()) && psLine.contains("bin" + File.separator + "java")) {
+               psProcess.destroyForcibly();
+               long pid = Long.parseLong(psLine.substring(0, psLine.indexOf(" ")));
+               log.infof("Obtained pid is %d for process %s.", pid, this.serverId);
+               return pid;
+            }
+         }
+         input.close();
+         process.destroy();
+      } catch (Exception ex) {
+         // Ignore.
+      }
+
+      throw new UnsupportedOperationException();
    }
 
 }
