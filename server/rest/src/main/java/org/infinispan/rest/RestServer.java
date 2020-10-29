0c7c23dbfec2b3faadffbf61c37a942eb256d8fc
Tristan Tarrant <ttarrant@redhat.com>
2020-10-29 12:31:56 +0100

    ISPN-12470 Metrics authentication should be optional

--- server/rest/src/main/java/org/infinispan/rest/RestServer.java
+++ server/rest/src/main/java/org/infinispan/rest/RestServer.java
@@ -129,7 +129,7 @@ public class RestServer extends AbstractProtocolServer<RestServerConfiguration> 
       resourceManager.registerResource(restContext, new SearchAdminResource(invocationHelper));
       resourceManager.registerResource(restContext, new TasksResource(invocationHelper));
       resourceManager.registerResource(restContext, new ProtobufResource(invocationHelper));
-      resourceManager.registerResource(rootContext, new MetricsResource());
+      resourceManager.registerResource(rootContext, new MetricsResource(auth.metricsAuth()));
       Path staticResources = configuration.staticResources();
       if (staticResources != null) {
          Path console = configuration.staticResources().resolve("console");
