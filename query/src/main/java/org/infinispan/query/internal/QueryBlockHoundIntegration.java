59e474076efba9518eb99911adff7d0ce9734fdc
Dan Berindei <dan@infinispan.org>
2020-12-08 10:55:08 +0200

    ISPN-12559 SearchAdminResource.searchStats blocking call
    Ignore file reads in NamedSPILoader, which is a re-implementation
    of java.util.ServiceLoader.

--- query/src/main/java/org/infinispan/query/internal/QueryBlockHoundIntegration.java
+++ query/src/main/java/org/infinispan/query/internal/QueryBlockHoundIntegration.java
@@ -1,10 +1,16 @@
 package org.infinispan.query.internal;
 
+import org.apache.lucene.util.NamedSPILoader;
+import org.kohsuke.MetaInfServices;
+
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.integration.BlockHoundIntegration;
 
+@MetaInfServices
 public class QueryBlockHoundIntegration implements BlockHoundIntegration {
    @Override
    public void applyTo(BlockHound.Builder builder) {
+      // Loading a service may require opening a file from classpath
+      builder.allowBlockingCallsInside(NamedSPILoader.class.getName(), "reload");
    }
 }
