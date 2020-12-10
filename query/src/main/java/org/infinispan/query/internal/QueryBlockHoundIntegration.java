2198ac61d1113bc10d3e7def05df416baf1c446e
William Burns <mudokonman@gmail.com>
2020-12-10 15:03:31 -0500

    ISPN-12569 SearchWorkspaceImpl is blocking

--- query/src/main/java/org/infinispan/query/internal/QueryBlockHoundIntegration.java
+++ query/src/main/java/org/infinispan/query/internal/QueryBlockHoundIntegration.java
@@ -12,5 +12,9 @@ public class QueryBlockHoundIntegration implements BlockHoundIntegration {
    public void applyTo(BlockHound.Builder builder) {
       // Loading a service may require opening a file from classpath
       builder.allowBlockingCallsInside(NamedSPILoader.class.getName(), "reload");
+
+      // This should be made non blocking
+      // https://issues.redhat.com/browse/ISPN-12569
+      builder.allowBlockingCallsInside("org.infinispan.search.mapper.scope.impl.SearchWorkspaceImpl", "purge");
    }
 }
