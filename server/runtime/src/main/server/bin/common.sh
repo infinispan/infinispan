555474f6b030af27e1ca45d016c9850fc7973175
Tristan Tarrant <ttarrant@redhat.com>
2020-12-21 09:35:26 +0100

    ISPN-12588 Fix server's common.sh to handle custom paths correctly

--- server/runtime/src/main/server/bin/common.sh
+++ server/runtime/src/main/server/bin/common.sh
@@ -13,12 +13,13 @@ DEBUG_MODE="${DEBUG:-false}"
 DEBUG_PORT="${DEBUG_PORT:-8787}"
 GC_LOG="$GC_LOG"
 JAVA_OPTS_EXTRA=""
+PROPERTIES=""
 while [ "$#" -gt 0 ]
 do
     case "$1" in
       --debug)
           DEBUG_MODE=true
-          if [ -n "$2" ] && [ "$2" = `echo "$2" | sed 's/-//'` ]; then
+          if [ -n "$2" ] && [ "$2" = $(echo "$2" | sed 's/-//') ]; then
               DEBUG_PORT=$2
               shift
           fi
@@ -26,8 +27,24 @@ do
       --)
           shift
           break;;
+      -s)
+          ISPN_ROOT_DIR="$2"
+          ARGUMENTS="$ARGUMENTS '$1' '$2'"
+          shift
+          ;;
       -D*)
           JAVA_OPTS_EXTRA="$JAVA_OPTS_EXTRA '$1'"
+          ;;
+      -P)
+          if [ ! -f "$2" ]; then
+            echo "Could not load property file: $2"
+            exit
+          fi
+          while read -r LINE; do
+            PROPERTIES="$PROPERTIES '-D$LINE'"
+          done < "$2"
+          ARGUMENTS="$ARGUMENTS '$1' '$2'"
+          shift
           ;;
       *)
           ARGUMENTS="$ARGUMENTS '$1'"
@@ -35,6 +52,7 @@ do
     esac
     shift
 done
+echo $PROPERTIES
 
 GREP="grep"
 
@@ -52,7 +70,7 @@ linux=false;
 solaris=false;
 freebsd=false;
 other=false
-case "`uname`" in
+case "$(uname)" in
     CYGWIN*)
         cygwin=true
         ;;
@@ -77,34 +95,34 @@ esac
 # For Cygwin, ensure paths are in UNIX format before anything is touched
 if $cygwin ; then
     [ -n "$ISPN_HOME" ] &&
-        ISPN_HOME=`cygpath --unix "$ISPN_HOME"`
+        ISPN_HOME=$(cygpath --unix "$ISPN_HOME")
     [ -n "$JAVA_HOME" ] &&
-        JAVA_HOME=`cygpath --unix "$JAVA_HOME"`
+        JAVA_HOME=$(cygpath --unix "$JAVA_HOME")
     [ -n "$JAVAC_JAR" ] &&
-        JAVAC_JAR=`cygpath --unix "$JAVAC_JAR"`
+        JAVAC_JAR=$(cygpath --unix "$JAVAC_JAR")
 fi
 
 # Setup ISPN_HOME
-RESOLVED_ISPN_HOME=`cd "$DIRNAME/.." >/dev/null; pwd`
+RESOLVED_ISPN_HOME=$(cd "$DIRNAME/.." >/dev/null; pwd)
 if [ "x$ISPN_HOME" = "x" ]; then
     # get the full path (without any relative bits)
     ISPN_HOME=$RESOLVED_ISPN_HOME
 else
- SANITIZED_ISPN_HOME=`cd "$ISPN_HOME"; pwd`
- if [ "$RESOLVED_ISPN_HOME" != "$SANITIZED_ISPN_HOME" ]; then
-   echo ""
-   echo "   WARNING:  ISPN_HOME may be pointing to a different installation - unpredictable results may occur."
-   echo ""
-   echo "             ISPN_HOME: $ISPN_HOME"
-   echo ""
-   sleep 2s
- fi
+  SANITIZED_ISPN_HOME=$(cd "$ISPN_HOME"; pwd)
+  if [ "$RESOLVED_ISPN_HOME" != "$SANITIZED_ISPN_HOME" ]; then
+    echo ""
+    echo "   WARNING:  ISPN_HOME may be pointing to a different installation - unpredictable results may occur."
+    echo ""
+    echo "             ISPN_HOME: $ISPN_HOME"
+    echo ""
+    sleep 2s
+  fi
 fi
 export ISPN_HOME
 
 # Read an optional running configuration file
 if [ "x$RUN_CONF" = "x" ]; then
-    BASEPROGNAME=`basename "$PROGNAME" .sh`
+    BASEPROGNAME=$(basename "$PROGNAME" .sh)
     RUN_CONF="$DIRNAME/$BASEPROGNAME.conf"
 fi
 if [ -r "$RUN_CONF" ]; then
@@ -115,7 +133,7 @@ JAVA_OPTS="$JAVA_OPTS_EXTRA $JAVA_OPTS"
 
 # Set debug settings if not already set
 if [ "$DEBUG_MODE" = "true" ]; then
-    DEBUG_OPT=`echo $JAVA_OPTS | $GREP "\-agentlib:jdwp"`
+    DEBUG_OPT=$(echo "$JAVA_OPTS" | $GREP "\-agentlib:jdwp")
     if [ "x$DEBUG_OPT" = "x" ]; then
         JAVA_OPTS="$JAVA_OPTS -agentlib:jdwp=transport=dt_socket,address=$DEBUG_PORT,server=y,suspend=n"
     else
@@ -134,21 +152,21 @@ fi
 
 if $linux; then
     # consolidate the server and command line opts
-    CONSOLIDATED_OPTS="$JAVA_OPTS $ARGUMENTS"
+    CONSOLIDATED_OPTS="$JAVA_OPTS $ARGUMENTS $PROPERTIES"
     # process the standalone options
     for var in $CONSOLIDATED_OPTS
     do
        # Remove quotes
-       p=`echo $var | tr -d "'"`
+       p=$(echo "$var" | tr -d "'")
        case $p in
-         -Dinfinispan.server.base.dir=*)
-              ISPN_BASE_DIR=`readlink -m ${p#*=}`
+         -Dinfinispan.server.root.path=*|--server-root=*)
+              ISPN_ROOT_DIR=$(readlink -m "${p#*=}")
               ;;
-         -Dinfinispan.server.log.dir=*)
-              ISPN_LOG_DIR=`readlink -m ${p#*=}`
+         -Dinfinispan.server.log.path=*)
+              ISPN_LOG_DIR=$(readlink -m "${p#*=}")
               ;;
-         -Dinfinispan.server.config.dir=*)
-              ISPN_CONFIG_DIR=`readlink -m ${p#*=}`
+         -Dinfinispan.server.config.path=*)
+              ISPN_CONFIG_DIR=$(readlink -m "${p#*=}")
               ;;
        esac
     done
@@ -156,21 +174,21 @@ fi
 
 if $solaris; then
     # consolidate the server and command line opts
-    CONSOLIDATED_OPTS="$JAVA_OPTS $ARGUMENTS"
+    CONSOLIDATED_OPTS="$JAVA_OPTS $ARGUMENTS $PROPERTIES"
     # process the standalone options
     for var in $CONSOLIDATED_OPTS
     do
        # Remove quotes
-       p=`echo $var | tr -d "'"`
-      case $p in
-        -Dinfinispan.server.base.dir=*)
-             ISPN_BASE_DIR=`echo $p | awk -F= '{print $2}'`
+       p=$(echo "$var" | tr -d "'")
+       case $p in
+         -Dinfinispan.server.root.path=*|--server-root=*)
+             ISPN_ROOT_DIR=$(echo "$p" | awk -F= '{print $2}')
              ;;
-        -Dinfinispan.server.log.dir=*)
-             ISPN_LOG_DIR=`echo $p | awk -F= '{print $2}'`
+         -Dinfinispan.server.log.path=*)
+             ISPN_LOG_DIR=$(echo "$p" | awk -F= '{print $2}')
              ;;
-        -Dinfinispan.server.config.dir=*)
-             ISPN_CONFIG_DIR=`echo $p | awk -F= '{print $2}'`
+         -Dinfinispan.server.config.path=*)
+             ISPN_CONFIG_DIR=$(echo "$p" | awk -F= '{print $2}')
              ;;
       esac
     done
@@ -179,62 +197,62 @@ fi
 # No readlink -m on BSD
 if $darwin || $freebsd || $other ; then
     # consolidate the server and command line opts
-    CONSOLIDATED_OPTS="$JAVA_OPTS $ARGUMENTS"
+    CONSOLIDATED_OPTS="$JAVA_OPTS $ARGUMENTS $PROPERTIES"
     # process the standalone options
     for var in $CONSOLIDATED_OPTS
     do
        # Remove quotes
-       p=`echo $var | tr -d "'"`
+       p=$(echo "$var" | tr -d "'")
        case $p in
-         -Dinfinispan.server.base.dir=*)
-              ISPN_BASE_DIR=`cd ${p#*=} ; pwd -P`
+         -Dinfinispan.server.root.path=*|--server-root=*)
+              ISPN_ROOT_DIR=$(cd "${p#*=}" ; pwd -P)
               ;;
-         -Dinfinispan.server.log.dir=*)
+         -Dinfinispan.server.log.path=*)
               if [ -d "${p#*=}" ]; then
-                ISPN_LOG_DIR=`cd ${p#*=} ; pwd -P`
+                ISPN_LOG_DIR=$(cd "${p#*=}" ; pwd -P)
              else
                 #since the specified directory doesn't exist we don't validate it
                 ISPN_LOG_DIR=${p#*=}
              fi
              ;;
-         -Dinfinispan.server.config.dir=*)
-              ISPN_CONFIG_DIR=`cd ${p#*=} ; pwd -P`
+         -Dinfinispan.server.config.path=*)
+              ISPN_CONFIG_DIR=$(cd "${p#*=}" ; pwd -P)
               ;;
        esac
     done
 fi
 
 # determine the default base dir, if not set
-if [ "x$ISPN_BASE_DIR" = "x" ]; then
-   ISPN_BASE_DIR="$ISPN_HOME/server"
+if [ "x$ISPN_ROOT_DIR" = "x" ]; then
+   ISPN_ROOT_DIR="$ISPN_HOME/server"
 fi
 # determine the default log dir, if not set
 if [ "x$ISPN_LOG_DIR" = "x" ]; then
-   ISPN_LOG_DIR="$ISPN_BASE_DIR/log"
+   ISPN_LOG_DIR="$ISPN_ROOT_DIR/log"
 fi
 # determine the default configuration dir, if not set
 if [ "x$ISPN_CONFIG_DIR" = "x" ]; then
-   ISPN_CONFIG_DIR="$ISPN_BASE_DIR/conf"
+   ISPN_CONFIG_DIR="$ISPN_ROOT_DIR/conf"
 fi
 
 # For Cygwin, switch paths to Windows format before running java
 if $cygwin; then
-    ISPN_HOME=`cygpath --path --windows "$ISPN_HOME"`
-    JAVA_HOME=`cygpath --path --windows "$JAVA_HOME"`
-    ISPN_MODULEPATH=`cygpath --path --windows "$ISPN_MODULEPATH"`
-    ISPN_BASE_DIR=`cygpath --path --windows "$ISPN_BASE_DIR"`
-    ISPN_LOG_DIR=`cygpath --path --windows "$ISPN_LOG_DIR"`
-    ISPN_CONFIG_DIR=`cygpath --path --windows "$ISPN_CONFIG_DIR"`
+    ISPN_HOME=$(cygpath --path --windows "$ISPN_HOME")
+    JAVA_HOME=$(cygpath --path --windows "$JAVA_HOME")
+    ISPN_MODULEPATH=$(cygpath --path --windows "$ISPN_MODULEPATH")
+    ISPN_ROOT_DIR=$(cygpath --path --windows "$ISPN_ROOT_DIR")
+    ISPN_LOG_DIR=$(cygpath --path --windows "$ISPN_LOG_DIR")
+    ISPN_CONFIG_DIR=$(cygpath --path --windows "$ISPN_CONFIG_DIR")
 fi
 
 if [ "$PRESERVE_JAVA_OPTS" != "true" ]; then
     # Check for -d32/-d64 in JAVA_OPTS
-    JVM_D64_OPTION=`echo $JAVA_OPTS | $GREP "\-d64"`
-    JVM_D32_OPTION=`echo $JAVA_OPTS | $GREP "\-d32"`
+    JVM_D64_OPTION=$(echo $JAVA_OPTS | $GREP "\-d64")
+    JVM_D32_OPTION=$(echo $JAVA_OPTS | $GREP "\-d32")
 
     # Check If server or client is specified
-    SERVER_SET=`echo $JAVA_OPTS | $GREP "\-server"`
-    CLIENT_SET=`echo $JAVA_OPTS | $GREP "\-client"`
+    SERVER_SET=$(echo $JAVA_OPTS | $GREP "\-server")
+    CLIENT_SET=$(echo $JAVA_OPTS | $GREP "\-client")
 
     if [ "x$JVM_D32_OPTION" != "x" ]; then
         JVM_OPTVERSION="-d32"
