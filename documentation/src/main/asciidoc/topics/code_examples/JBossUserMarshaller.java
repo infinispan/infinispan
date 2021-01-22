eb6a358c3b10aeb6e239c6a0eaaeb8dbeccaf22e
Don Naro <dnaro@redhat.com>
2021-01-22 13:20:48 +0000

    update to jboss marshalling

--- documentation/src/main/asciidoc/topics/code_examples/JBossUserMarshaller.java
+++ documentation/src/main/asciidoc/topics/code_examples/JBossUserMarshaller.java
@@ -1,2 +1,5 @@
 GlobalConfigurationBuilder builder = new GlobalConfigurationBuilder();
-builder.serialization().marshaller(new JBossUserMarshaller());
+builder.serialization()
+       .marshaller(new GenericJBossMarshaller())
+       .allowList()
+       .addRegexps("org.infinispan.example.", "org.infinispan.concrete.SomeClass");
