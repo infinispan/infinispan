0adc7b834b89879045d05d6484be1d3487349d47
Pedro Ruivo <pruivo@redhat.com>
2021-02-09 11:34:30 +0000

    ISPN-12716 Hot Rod iteration shouldn't require ADMIN permission

---
+++ core/src/main/java/org/infinispan/security/actions/GetEmbeddedCacheManagerAction.java
@@ -0,0 +1,22 @@
+package org.infinispan.security.actions;
+
+import org.infinispan.AdvancedCache;
+import org.infinispan.manager.EmbeddedCacheManager;
+
+/**
+ * GetEmbeddedCacheManagerAction.
+ *
+ * @author Pedro Ruivo
+ * @since 12.0
+ */
+public class GetEmbeddedCacheManagerAction extends AbstractAdvancedCacheAction<EmbeddedCacheManager> {
+
+   public GetEmbeddedCacheManagerAction(AdvancedCache<?, ?> cache) {
+      super(cache);
+   }
+
+   @Override
+   public EmbeddedCacheManager run() {
+      return cache.getCacheManager();
+   }
+}
