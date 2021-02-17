8b4c13470c4ebe0f96b858ce3df2471ba3e8db3c
Don Naro <dnaro@redhat.com>
2021-02-17 15:31:05 +0000

    ISPN-12050 consistency edits for Query docs

--- query/src/main/java/org/infinispan/query/Indexer.java
+++ query/src/main/java/org/infinispan/query/Indexer.java
@@ -3,36 +3,37 @@ package org.infinispan.query;
 import java.util.concurrent.CompletionStage;
 
 /**
- * The indexer is used to interact directly with a cache's index.
+ * Interacts directly with cache indexes.
  *
  * @since 11.0
  */
 public interface Indexer {
 
    /**
-    * Deletes all the indexes from a cache and rebuilds them from the existing data.
-    * This operation can take a long time depending on the cache size, and searches
-    * during this process will be affected.
+    * Deletes all indexes for the cache and rebuilds them.
+    * The indexing operation can take a long time to complete, depending on the size of the cache.
+    * You should not query caches until the indexing operation is complete because it affects query performance and results.
+    *
     */
    CompletionStage<Void> run();
 
    /**
-    * Re-indexes only the values associated with the provided keys.
+    * Re-indexes values associated with the provided keys only.
     */
    CompletionStage<Void> run(Object... keys);
 
    /**
-    * Remove all the indexes from the cache.
+    * Removes all indexes from the cache.
     */
    CompletionStage<Void> remove();
 
    /**
-    * Remove all entities of particular class from the index of a cache.
+    * Removes all entities of a particular class from the index of the cache.
     */
    CompletionStage<Void> remove(Class<?>... entities);
 
    /**
-    * @return true if the indexer process was started on this node and hasn't finished yet.
+    * @return true if the indexer process was started on this node and has not finished yet.
     */
    boolean isRunning();
 }
--- query/src/main/java/org/infinispan/query/package-info.java
+++ query/src/main/java/org/infinispan/query/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Query API.
+ * Query API that lets you perform Ickle queries and look up values in caches.
+ * You can use the Query API with both embedded and remote caches.
  *
  * @public
  */
