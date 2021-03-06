01697b7a2fba0ea3b78fa5eecea3f38ba12fd384
Dan Berindei <dan@infinispan.org>
2021-03-06 16:38:10 +0200

    ISPN-12807 Simple cache does not update eviction statistics
    EvictionManagerImpl must work with both CacheMgmtInterceptor
    (regular cache) and StatsCollector (simple cache).

--- core/src/main/java/org/infinispan/eviction/impl/EvictionManagerImpl.java
+++ core/src/main/java/org/infinispan/eviction/impl/EvictionManagerImpl.java
@@ -8,12 +8,14 @@ import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.context.impl.ImmutableContext;
 import org.infinispan.eviction.EvictionManager;
 import org.infinispan.factories.annotations.Inject;
+import org.infinispan.factories.annotations.Start;
 import org.infinispan.factories.impl.ComponentRef;
 import org.infinispan.factories.scopes.Scope;
 import org.infinispan.factories.scopes.Scopes;
 import org.infinispan.interceptors.AsyncInterceptorChain;
 import org.infinispan.interceptors.impl.CacheMgmtInterceptor;
 import org.infinispan.notifications.cachelistener.CacheNotifier;
+import org.infinispan.stats.impl.StatsCollector;
 
 import net.jcip.annotations.ThreadSafe;
 
@@ -23,6 +25,15 @@ public class EvictionManagerImpl<K, V> implements EvictionManager<K, V> {
    @Inject CacheNotifier<K, V> cacheNotifier;
    @Inject ComponentRef<AsyncInterceptorChain> interceptorChain;
    @Inject Configuration cfg;
+   @Inject StatsCollector simpleCacheStatsCollector;
+
+   private CacheMgmtInterceptor cacheMgmtInterceptor;
+
+   @Start
+   public void findCacheMgmtInterceptor() {
+      // Allow the interceptor chain to start later, otherwise we'd have a dependency cycle
+      cacheMgmtInterceptor = interceptorChain.wired().findInterceptorExtending(CacheMgmtInterceptor.class);
+   }
 
    @Override
    public CompletionStage<Void> onEntryEviction(Map<K, Map.Entry<K,V>> evicted, FlagAffectedCommand command) {
@@ -35,10 +46,10 @@ public class EvictionManagerImpl<K, V> implements EvictionManager<K, V> {
    }
 
    private void updateEvictionStatistics(Map<K, Map.Entry<K, V>> evicted) {
-      CacheMgmtInterceptor mgmtInterceptor =
-            interceptorChain.running().findInterceptorExtending(CacheMgmtInterceptor.class);
-      if (mgmtInterceptor != null) {
-         mgmtInterceptor.addEvictions(evicted.size());
+      if (cacheMgmtInterceptor != null) {
+         cacheMgmtInterceptor.addEvictions(evicted.size());
+      } else if (simpleCacheStatsCollector != null) {
+         simpleCacheStatsCollector.recordEvictions(evicted.size());
       }
    }
 }
