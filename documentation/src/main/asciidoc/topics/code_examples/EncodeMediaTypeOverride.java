b96548f680e1783e3a981548ae281a9ff919bfae
Don Naro <dnaro@redhat.com>
2021-03-08 09:26:35 +0000

    ISPN-12810 docs revision: cache encoding

--- documentation/src/main/asciidoc/topics/code_examples/EncodeMediaTypeOverride.java
+++ documentation/src/main/asciidoc/topics/code_examples/EncodeMediaTypeOverride.java
@@ -1,9 +1,9 @@
 DefaultCacheManager cacheManager = new DefaultCacheManager();
 
-// The cache will store POJO for keys and values
+// Encode keys and values as Protobuf
 ConfigurationBuilder cfg = new ConfigurationBuilder();
-cfg.encoding().key().mediaType("application/x-java-object");
-cfg.encoding().value().mediaType("application/x-java-object");
+cfg.encoding().key().mediaType("application/x-protostream");
+cfg.encoding().value().mediaType("application/x-protostream");
 
 cacheManager.defineConfiguration("mycache", cfg.build());
 
@@ -11,7 +11,7 @@ Cache<Integer, Person> cache = cacheManager.getCache("mycache");
 
 cache.put(1, new Person("John","Doe"));
 
-// Wraps cache using 'application/x-java-object' for keys but JSON for values
-Cache<Integer, byte[]> jsonValuesCache = (Cache<Integer, byte[]>) cache.getAdvancedCache().withMediaType("application/x-java-object", "application/json");
+// Use Protobuf for keys and JSON for values
+Cache<Integer, byte[]> jsonValuesCache = (Cache<Integer, byte[]>) cache.getAdvancedCache().withMediaType("application/x-protostream", "application/json");
 
 byte[] json = jsonValuesCache.get(1);
