38171686172f59c4e72875d2e800afe2b7bdb96d
Don Naro <dnaro@redhat.com>
2021-03-08 09:26:35 +0000

    ISPN-12810 docs revision: cache encoding

--- documentation/src/main/asciidoc/topics/code_examples/EncoderOverride.java
+++
@@ -1,9 +0,0 @@
-// Read and write POJO, storage will be byte[] since for
-// OFF_HEAP the GlobalMarshallerEncoder is used internally:
-cache.put(1, new Pojo())
-Pojo value = cache.get(1)
-
-// Get the content in its stored format by overriding
-// the internal encoder with a no-op encoder (IdentityEncoder)
-Cache<?,?> rawContent = cache.getAdvancedCache().withEncoding(IdentityEncoder.class);
-byte[] marshalled = (byte[]) rawContent.get(1);
