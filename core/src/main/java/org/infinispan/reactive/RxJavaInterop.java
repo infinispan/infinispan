f18f425ef3024cdd860de664628169e1e6067886
William Burns <mudokonman@gmail.com>
2021-03-24 17:42:41 -0400

    ISPN-12886 Fix Stream iteration Undelievered test failures
    * Properly cancel upstream from partition change
    * Ignore publisher error if already cancelled

--- core/src/main/java/org/infinispan/reactive/RxJavaInterop.java
+++ core/src/main/java/org/infinispan/reactive/RxJavaInterop.java
@@ -1,11 +1,16 @@
 package org.infinispan.reactive;
 
+import java.lang.invoke.MethodHandles;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.infinispan.util.concurrent.CompletionStages;
+import org.infinispan.util.logging.Log;
+import org.infinispan.util.logging.LogFactory;
 
 import io.reactivex.rxjava3.core.Flowable;
 import io.reactivex.rxjava3.processors.AsyncProcessor;
+import io.reactivex.rxjava3.processors.UnicastProcessor;
 
 /**
  * Static factory class that provides methods to obtain commonly used instances for interoperation between RxJava
@@ -16,6 +21,8 @@ import io.reactivex.rxjava3.processors.AsyncProcessor;
 public class RxJavaInterop extends org.infinispan.commons.reactive.RxJavaInterop {
    private RxJavaInterop() { }
 
+   protected final static Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass());
+
    public static <R> Flowable<R> voidCompletionStageToFlowable(CompletionStage<Void> stage) {
       if (CompletionStages.isCompletedSuccessfully(stage)) {
          return Flowable.empty();
@@ -25,6 +32,43 @@ public class RxJavaInterop extends org.infinispan.commons.reactive.RxJavaInterop
       stage.whenComplete((value, t) -> {
          if (t != null) {
             ap.onError(t);
+         } else {
+            ap.onComplete();
+         }
+      });
+
+      return ap;
+   }
+
+   /**
+    * Same as {@link #voidCompletionStageToFlowable(CompletionStage)} except that you can optionally have it so that if
+    * a throwable occurs in the stage that it isn't propagated if the returned Flowable's subscription was cancelled.
+    * <p>
+    * This method also only allows for a single subscriber to the Flowable, any additional subscribers will receive
+    * an exception when subscribing to the returned Flowable.
+    * {@link #voidCompletionStageToFlowable(CompletionStage)} can support any number of subscribers.
+    * @param stage stage to complete
+    * @param ignoreErrorIfCancelled whether to ignore an error if cancelled
+    * @param <R> stage type
+    * @return a Flowable that is completed when the stage is
+    */
+   public static <R> Flowable<R> voidCompletionStageToFlowable(CompletionStage<Void> stage, boolean ignoreErrorIfCancelled) {
+      if (!ignoreErrorIfCancelled) {
+         return voidCompletionStageToFlowable(stage);
+      }
+      if (CompletionStages.isCompletedSuccessfully(stage)) {
+         return Flowable.empty();
+      }
+      AtomicBoolean cancelled = new AtomicBoolean();
+      UnicastProcessor<R> ap = UnicastProcessor.create(1, () -> cancelled.set(true));
+
+      stage.whenComplete((value, t) -> {
+         if (t != null) {
+            if (!cancelled.get()) {
+               ap.onError(t);
+            } else {
+               log.debug("Ignoring throwable as the UnicastProcessor is already completed", t);
+            }
          } else {
             ap.onComplete();
          }
