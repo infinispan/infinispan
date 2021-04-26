4ffd7df493cbae5423ce34b6a02cf4a5afe14021
Wolf-Dieter Fink <wfink@redhat.com>
2021-04-26 08:30:16 +0200

    ISPN-12810 docs revision: ProtoStream marshaller

--- documentation/src/main/asciidoc/topics/code_examples/Book.java
+++ documentation/src/main/asciidoc/topics/code_examples/Book.java
@@ -1,26 +1,33 @@
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
-...
 
 public class Book {
    @ProtoField(number = 1)
-   final String title;
+   public final UUID id;
 
    @ProtoField(number = 2)
+   final String title;
+
+   @ProtoField(number = 3)
    final String description;
 
-   @ProtoField(number = 3, defaultValue = "0")
+   @ProtoField(number = 4, defaultValue = "0")
    final int publicationYear;
 
-   @ProtoField(number = 4, collectionImplementation = ArrayList.class)
+   @ProtoField(number = 5, collectionImplementation = ArrayList.class)
    final List<Author> authors;
 
+   @ProtoField(number = 6)
+   public Language language;
+
    @ProtoFactory
-   Book(String title, String description, int publicationYear, List<Author> authors) {
+   Book(UUID id, String title, String description, int publicationYear, List<Author> authors, Language language) {
+      this.id = id;
       this.title = title;
       this.description = description;
       this.publicationYear = publicationYear;
       this.authors = authors;
+      this.language = language;
    }
-   // public Getter methods omitted for brevity
+   // public Getter methods not included for brevity
 }
