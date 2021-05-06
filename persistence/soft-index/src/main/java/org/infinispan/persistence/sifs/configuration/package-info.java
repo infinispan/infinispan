a7ce6cdf57c0298deb135e2f5d616564ed9f7f6f
William Burns <mudokonman@gmail.com>
2021-05-06 14:57:50 -0400

    ISPN-12990 Change SoftIndexFileStore to be the Infinispan default file store
    * Remove soft index file store module

--- persistence/soft-index/src/main/java/org/infinispan/persistence/sifs/configuration/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Configuration for {@link org.infinispan.persistence.sifs.SoftIndexFileStore}.
- *
- * @public
- */
-package org.infinispan.persistence.sifs.configuration;
--- persistence/soft-index/src/main/java/org/infinispan/persistence/sifs/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Soft Index {@link org.infinispan.persistence.spi.AdvancedLoadWriteStore}.
- *
- * @public
- */
-package org.infinispan.persistence.sifs;
