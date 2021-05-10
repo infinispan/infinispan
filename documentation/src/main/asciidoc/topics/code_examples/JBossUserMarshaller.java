e56ceec4778315eb310152b9b22e9628053b3253
Don Naro <dnaro@redhat.com>
2021-05-10 16:00:17 +0100

    ISPN-13028 docs mention allowlist

--- documentation/src/main/asciidoc/topics/code_examples/JBossUserMarshaller.java
+++ documentation/src/main/asciidoc/topics/code_examples/JBossUserMarshaller.java
@@ -1,5 +1,5 @@
 GlobalConfigurationBuilder builder = new GlobalConfigurationBuilder();
 builder.serialization()
        .marshaller(new GenericJBossMarshaller())
-       .allowList()
+       .whiteList()
        .addRegexps("org.infinispan.example.", "org.infinispan.concrete.SomeClass");
