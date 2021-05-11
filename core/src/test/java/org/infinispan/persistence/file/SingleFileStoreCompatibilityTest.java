d4b872817c968ae8cc31e493d9cdbefe51012e3c
William Burns <mudokonman@gmail.com>
2021-05-11 14:01:21 -0400

    IPROTO-211 UUIDAdapter has the incorrect Id
    * Test for ISPN

--- core/src/test/java/org/infinispan/persistence/file/SingleFileStoreCompatibilityTest.java
+++ core/src/test/java/org/infinispan/persistence/file/SingleFileStoreCompatibilityTest.java
@@ -7,12 +7,15 @@ import java.nio.file.StandardCopyOption;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.infinispan.Cache;
 import org.infinispan.commons.util.FileLookupFactory;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.persistence.IdentityKeyValueWrapper;
 import org.infinispan.persistence.PersistenceCompatibilityTest;
 import org.infinispan.test.data.Value;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 /**
  * Tests if {@link SingleFileStore} can migrate data from Infinispan 10.1.x.
@@ -23,10 +26,13 @@ import org.testng.annotations.Test;
 @Test(groups = "functional", testName = "persistence.file.SingleFileStoreCompatibilityTest")
 public class SingleFileStoreCompatibilityTest extends PersistenceCompatibilityTest<Value> {
 
-   private static final Map<Version, String> data = new HashMap<>(2);
+   private static final String EVENT_LOG_11_0 = Version._11_0.toString() + "_event";
+
+   private static final Map<String, String> data = new HashMap<>(2);
    static {
-      data.put(Version._10_1, "sfs/10_1/sfs-store-cache.dat");
-      data.put(Version._11_0, "sfs/11_0/sfs-store-cache.dat");
+      data.put(Version._10_1.toString(), "sfs/10_1/sfs-store-cache.dat");
+      data.put(Version._11_0.toString(), "sfs/11_0/sfs-store-cache.dat");
+      data.put(EVENT_LOG_11_0, "sfs/11_0/event_log_cache.dat");
    }
 
    public SingleFileStoreCompatibilityTest() {
@@ -35,6 +41,10 @@ public class SingleFileStoreCompatibilityTest extends PersistenceCompatibilityTe
 
    @Override
    protected void beforeStartCache(Version version) throws Exception {
+      beforeStartCache(version.toString());
+   }
+
+   private void beforeStartCache(String version) throws Exception {
       InputStream is = FileLookupFactory.newInstance()
             .lookupFile(data.get(version), Thread.currentThread().getContextClassLoader());
       File sfsFile = SingleFileStore
@@ -58,5 +68,15 @@ public class SingleFileStoreCompatibilityTest extends PersistenceCompatibilityTe
       builder.persistence().addSingleFileStore()
             .segmented(false)
             .location(tmpDirectory);
+   }
+
+   public void testReadWriteFrom11EventCache() throws Exception {
+      // 10 keys
+      // even keys stored, odd keys removed
+      beforeStartCache(EVENT_LOG_11_0);
+
+      Cache<Object, Object> cache = cacheManager.getCache(cacheName());
+
+      assertEquals(10, cache.size());
    }
 }
