97d39192e4044535f8b8d776f361befec5010fd5
Diego Lovison <diegolovison@gmail.com>
2021-05-17 16:05:48 -0300

    jctools NonBlockingHashMap

--- core/src/main/java/org/infinispan/container/impl/PeekableTouchableContainerMap.java
+++ core/src/main/java/org/infinispan/container/impl/PeekableTouchableContainerMap.java
@@ -1,17 +1,17 @@
 package org.infinispan.container.impl;
 
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 import org.infinispan.commons.util.AbstractDelegatingConcurrentMap;
 import org.infinispan.container.entries.InternalCacheEntry;
+import org.jctools.maps.NonBlockingHashMap;
 
 public class PeekableTouchableContainerMap<K, V> extends AbstractDelegatingConcurrentMap<K, InternalCacheEntry<K, V>>
       implements PeekableTouchableMap<K, V> {
    private final ConcurrentMap<K, InternalCacheEntry<K, V>> map;
 
    public PeekableTouchableContainerMap() {
-      this(new ConcurrentHashMap<>());
+      this(new NonBlockingHashMap<>());
    }
 
    public PeekableTouchableContainerMap(ConcurrentMap<K, InternalCacheEntry<K, V>> map) {
