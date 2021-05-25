1205f7740c5696d3f5b4438b4245dd6f17c29aee
Don Naro <dnaro@redhat.com>
2021-05-25 18:07:16 +0100

    ISPN-13052 correct Hot Rod config examples

--- documentation/src/main/asciidoc/topics/code_examples/HotRodCreateCacheWithXML.java
+++ documentation/src/main/asciidoc/topics/code_examples/HotRodCreateCacheWithXML.java
@@ -1,9 +1,8 @@
 String cacheName = "CacheWithXMLConfiguration";
-String xml = String.format("<distributed-cache name=\"%s\" mode=\"SYNC\"
-                              statistics=\"true\">" +
+String xml = String.format("<distributed-cache name=\"%s\" mode=\"SYNC\">" +
+                              "<encoding media-type=\"application/x-protostream\"/>" +
                               "<locking isolation=\"READ_COMMITTED\"/>" +
                               "<transaction mode=\"NON_XA\"/>" +
                               "<expiration lifespan=\"60000\" interval=\"20000\"/>" +
-                           "</distributed-cache>" +
-                           , cacheName);
+                           "</distributed-cache>" , cacheName);
 remoteCacheManager.administration().getOrCreateCache(cacheName, new XMLStringConfiguration(xml));
