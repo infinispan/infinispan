3dad6672fc563c6996cdd347d48234927128ada7
Dan Berindei <dan@infinispan.org>
2021-05-25 21:57:33 +0300

    ISPN-13127 Cache TimeService component in ComponentRegistry
    getTimeService() looks like a cached component, but it looks up
    the component in the global registry every time.

--- core/src/main/java/org/infinispan/factories/ComponentRegistry.java
+++ core/src/main/java/org/infinispan/factories/ComponentRegistry.java
@@ -115,6 +115,8 @@ public class ComponentRegistry extends AbstractComponentRegistry {
    private ComponentRef<TransactionTable> transactionTable;
    private ComponentRef<VersionGenerator> versionGenerator;
    private ComponentRef<XSiteStateTransferManager> xSiteStateTransferManager;
+   // Global, but we don't cache components at global scope
+   private TimeService timeService;
 
    /**
     * Creates an instance of the component registry.  The configuration passed in is automatically registered.
@@ -277,7 +279,7 @@ public class ComponentRegistry extends AbstractComponentRegistry {
 
    @Override
    public TimeService getTimeService() {
-      return globalComponents.getTimeService();
+      return timeService;
    }
 
    public String getCacheName() {
@@ -396,6 +398,7 @@ public class ComponentRegistry extends AbstractComponentRegistry {
       transactionTable = basicComponentRegistry.getComponent(org.infinispan.transaction.impl.TransactionTable.class);
       versionGenerator = basicComponentRegistry.getComponent(VersionGenerator.class);
       xSiteStateTransferManager = basicComponentRegistry.getComponent(XSiteStateTransferManager.class);
+      timeService = basicComponentRegistry.getComponent(TimeService.class).running();
 
       // Initialize components that don't have any strong references from the cache
       basicComponentRegistry.getComponent(ClusterCacheStats.class);
