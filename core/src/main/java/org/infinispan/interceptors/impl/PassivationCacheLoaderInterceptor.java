28633e6f3dee5e64767f26f4bdff63ff0305b0db
Dan Berindei <dan@infinispan.org>
2021-06-03 17:33:42 +0300

    Micro-optimization: Avoid some allocations in passivation

--- core/src/main/java/org/infinispan/interceptors/impl/PassivationCacheLoaderInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/impl/PassivationCacheLoaderInterceptor.java
@@ -3,13 +3,13 @@ package org.infinispan.interceptors.impl;
 import java.lang.invoke.MethodHandles;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-import java.util.function.Supplier;
 
 import org.infinispan.commands.FlagAffectedCommand;
 import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.context.InvocationContext;
 import org.infinispan.eviction.impl.ActivationManager;
 import org.infinispan.factories.annotations.Inject;
+import org.infinispan.util.concurrent.CompletionStages;
 import org.infinispan.util.concurrent.DataOperationOrderer;
 import org.infinispan.util.concurrent.DataOperationOrderer.Operation;
 import org.infinispan.util.logging.Log;
@@ -21,45 +21,52 @@ public class PassivationCacheLoaderInterceptor<K, V> extends CacheLoaderIntercep
 
    @Inject
    DataOperationOrderer orderer;
-   @Inject ActivationManager activationManager;
+   @Inject
+   ActivationManager activationManager;
 
    @Override
    public CompletionStage<InternalCacheEntry<K, V>> loadAndStoreInDataContainer(InvocationContext ctx, Object key,
                                                                                 int segment, FlagAffectedCommand cmd) {
-      Supplier<CompletionStage<InternalCacheEntry<K, V>>> supplier = () -> super.loadAndStoreInDataContainer(ctx, key, segment, cmd);
-      return handlePassivationLoad(key, segment, orderer, activationManager, supplier);
-   }
-
-   static <K, V> CompletionStage<InternalCacheEntry<K, V>> handlePassivationLoad(Object key, int segment,
-                                                                                 DataOperationOrderer orderer,
-                                                                                 ActivationManager activationManager,
-                                                                                 Supplier<CompletionStage<InternalCacheEntry<K, V>>> supplier) {
       CompletableFuture<Operation> future = new CompletableFuture<>();
       CompletionStage<Operation> delayStage = orderer.orderOn(key, future);
 
       CompletionStage<InternalCacheEntry<K, V>> retrievalStage;
-      if (delayStage != null) {
-         retrievalStage = delayStage.thenCompose(ignore -> supplier.get());
+      if (delayStage != null && !CompletionStages.isCompletedSuccessfully(delayStage)) {
+         retrievalStage = delayStage.thenCompose(ignore -> super.loadAndStoreInDataContainer(ctx, key, segment, cmd));
       } else {
-         retrievalStage = supplier.get();
+         retrievalStage = super.loadAndStoreInDataContainer(ctx, key, segment, cmd);
       }
-      return retrievalStage.whenComplete((value, t) -> {
-         if (value != null) {
-            if (trace) {
-               log.tracef("Activating key: %s - not waiting for response", value.getKey());
-            }
-            // Note we don't wait on this to be removed, which allows the load to continue ahead. However
-            // we can't release the orderer acquisition until the remove is complete
-            activationManager.activateAsync(value.getKey(), segment)
-                  .whenComplete((ignore, throwable) -> {
-                     if (throwable != null) {
-                        log.warnf("Activation of key %s failed for some reason", t);
-                     }
-                     orderer.completeOperation(key, future, Operation.READ);
-                  });
+      if (CompletionStages.isCompletedSuccessfully(retrievalStage)) {
+         InternalCacheEntry<K, V> ice = CompletionStages.join(retrievalStage);
+         activateAfterLoad(key, segment, orderer, activationManager, future, ice, null);
+         return retrievalStage;
+      } else {
+         return retrievalStage.whenComplete((value, t) -> {
+            activateAfterLoad(key, segment, orderer, activationManager, future, value, t);
+         });
+      }
+   }
+
+   static <K, V> void activateAfterLoad(Object key, int segment, DataOperationOrderer orderer, ActivationManager activationManager, CompletableFuture<Operation> future, InternalCacheEntry<K, V> value, Throwable t) {
+      if (value != null) {
+         if (trace) {
+            log.tracef("Activating key: %s - not waiting for response", value.getKey());
+         }
+         // Note we don't wait on this to be removed, which allows the load to continue ahead.
+         // However, we can't release the orderer acquisition until the remove is complete
+         CompletionStage<Void> activationStage = activationManager.activateAsync(value.getKey(), segment);
+         if (!CompletionStages.isCompletedSuccessfully(activationStage)) {
+            activationStage.whenComplete((ignore, throwable) -> {
+               if (throwable != null) {
+                  log.warnf("Activation of key %s failed for some reason", t);
+               }
+               orderer.completeOperation(key, future, Operation.READ);
+            });
          } else {
             orderer.completeOperation(key, future, Operation.READ);
          }
-      });
+      } else {
+         orderer.completeOperation(key, future, Operation.READ);
+      }
    }
 }
--- core/src/main/java/org/infinispan/interceptors/impl/PassivationClusteredCacheLoaderInterceptor.java
+++ core/src/main/java/org/infinispan/interceptors/impl/PassivationClusteredCacheLoaderInterceptor.java
@@ -1,24 +1,40 @@
 package org.infinispan.interceptors.impl;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
-import java.util.function.Supplier;
 
 import org.infinispan.commands.FlagAffectedCommand;
 import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.context.InvocationContext;
 import org.infinispan.eviction.impl.ActivationManager;
 import org.infinispan.factories.annotations.Inject;
+import org.infinispan.util.concurrent.CompletionStages;
 import org.infinispan.util.concurrent.DataOperationOrderer;
 
 public class PassivationClusteredCacheLoaderInterceptor<K, V> extends ClusteredCacheLoaderInterceptor<K, V> {
-   @Inject
-   DataOperationOrderer orderer;
+   @Inject DataOperationOrderer orderer;
    @Inject ActivationManager activationManager;
 
    @Override
    public CompletionStage<InternalCacheEntry<K, V>> loadAndStoreInDataContainer(InvocationContext ctx, Object key,
                                                                                 int segment, FlagAffectedCommand cmd) {
-      Supplier<CompletionStage<InternalCacheEntry<K, V>>> supplier = () -> super.loadAndStoreInDataContainer(ctx, key, segment, cmd);
-      return PassivationCacheLoaderInterceptor.handlePassivationLoad(key, segment, orderer, activationManager, supplier);
+       CompletableFuture<DataOperationOrderer.Operation> future = new CompletableFuture<>();
+      CompletionStage<DataOperationOrderer.Operation> delayStage = orderer.orderOn(key, future);
+
+      CompletionStage<InternalCacheEntry<K, V>> retrievalStage;
+      if (delayStage != null && !CompletionStages.isCompletedSuccessfully(delayStage)) {
+         retrievalStage = delayStage.thenCompose(ignore -> super.loadAndStoreInDataContainer(ctx, key, segment, cmd));
+      } else {
+         retrievalStage = super.loadAndStoreInDataContainer(ctx, key, segment, cmd);
+      }
+       if (CompletionStages.isCompletedSuccessfully(retrievalStage)) {
+           InternalCacheEntry<K, V> ice = CompletionStages.join(retrievalStage);
+           PassivationCacheLoaderInterceptor.activateAfterLoad(key, segment, orderer, activationManager, future, ice, null);
+           return retrievalStage;
+       } else {
+           return retrievalStage.whenComplete((value, t) -> {
+               PassivationCacheLoaderInterceptor.activateAfterLoad(key, segment, orderer, activationManager, future, value, t);
+           });
+       }
    }
 }
