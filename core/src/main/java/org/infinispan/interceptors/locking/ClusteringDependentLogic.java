5469a79f4d5de5e6f19e73aed8d3e24bc4e5f9d9
Dan Berindei <dan@infinispan.org>
2021-06-08 08:15:44 +0300

    ISPN-13127 Skip allocation in xClusteringDistributionLogic
    The commit stage is already completed when there is no persistence
    or the store write/activation is synchronous.

--- core/src/main/java/org/infinispan/interceptors/locking/ClusteringDependentLogic.java
+++ core/src/main/java/org/infinispan/interceptors/locking/ClusteringDependentLogic.java
@@ -443,7 +443,7 @@ public interface ClusteringDependentLogic {
                expired = false;
             }
 
-            if (stage == null) {
+            if (stage == null || CompletionStages.isCompletedSuccessfully(stage)) {
                return NotifyHelper.entryCommitted(notifier, functionalNotifier, created, removed, expired,
                      entry, ctx, command, previousValue, previousMetadata, evictionManager);
             } else {
