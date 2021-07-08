dd0cc9d29759f47cdd8aa11841a213a31a7bc5e4
Don Naro <dnaro@redhat.com>
2021-07-08 19:49:13 +0100

    ISPN-13157 file store docs

--- documentation/src/main/asciidoc/topics/code_examples/ConfigSoftIndexFileStore.java
+++
@@ -1,5 +0,0 @@
-ConfigurationBuilder b = new ConfigurationBuilder();
-b.persistence()
-    .addStore(SoftIndexFileStoreConfigurationBuilder.class)
-        .indexLocation("testCache/index");
-        .dataLocation("testCache/data")
