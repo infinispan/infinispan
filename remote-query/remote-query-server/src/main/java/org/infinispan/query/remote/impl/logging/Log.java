16f4eb1979c1c0e381beff6ad597cf9c212bab8f
Tristan Tarrant <ttarrant@redhat.com>
2021-07-27 09:38:53 +0200

    ISPN-13202 Log SCI registration as debug

--- remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/logging/Log.java
+++ remote-query/remote-query-server/src/main/java/org/infinispan/query/remote/impl/logging/Log.java
@@ -1,6 +1,5 @@
 package org.infinispan.query.remote.impl.logging;
 
-import static org.jboss.logging.Logger.Level.INFO;
 import static org.jboss.logging.Logger.Level.WARN;
 
 import org.infinispan.commons.CacheConfigurationException;
@@ -23,23 +22,23 @@ import org.jboss.logging.annotations.ValidIdRange;
 @ValidIdRange(min = 28001, max = 28500)
 public interface Log extends BasicLogger {
 
-   @Message(value = "Unknown field %s in type %s", id = 28001)
+   /*@Message(value = "Unknown field %s in type %s", id = 28001)
    IllegalArgumentException unknownField(String fieldName, String fullyQualifiedTypeName);
 
    @Message(value = "Field %s from type %s is not indexed", id = 28002)
    IllegalArgumentException fieldIsNotIndexed(String fieldName, String fullyQualifiedTypeName);
 
    @Message(value = "An exception has occurred during query execution", id = 28003)
-   CacheException errorExecutingQuery(@Cause Throwable cause);
+   CacheException errorExecutingQuery(@Cause Throwable cause);*/
 
    @Message(value = "Querying is not enabled on cache %s", id = 28004)
    CacheException queryingNotEnabled(String cacheName);
 
-   @Message(value = "Field %s from type %s is not analyzed", id = 28005)
+   /*@Message(value = "Field %s from type %s is not analyzed", id = 28005)
    IllegalArgumentException fieldIsNotAnalyzed(String fieldName, String fullyQualifiedTypeName);
 
    @Message(value = "An exception has occurred during filter execution", id = 28006)
-   CacheException errorFiltering(@Cause Throwable cause);
+   CacheException errorFiltering(@Cause Throwable cause);*/
 
    @Message(value = "The key must be a String : %s", id = 28007)
    CacheException keyMustBeString(Class<?> c);
@@ -71,19 +70,19 @@ public interface Log extends BasicLogger {
          "Please configure the cache encoding as 'application/x-protostream' or 'application/x-java-object'")
    void warnNoMediaType(String cacheName);
 
-   @Message(id = 28017, value = "Type '%s' was not declared as an indexed entity. " +
+   /*@Message(id = 28017, value = "Type '%s' was not declared as an indexed entity. " +
          "Please declare it in the indexing configuration of your cache and ensure the type is defined by a schema before this cache is started.")
-   CacheConfigurationException indexingUndeclaredType(String typeName);
+   CacheConfigurationException indexingUndeclaredType(String typeName);*/
 
    @Message(id = 28018, value = "It is not possible to create indexes for a field having type %s. Field: %s.")
    CacheException fieldTypeNotIndexable(String typeName, String fieldName);
 
-   @LogMessage(level = INFO)
+   /*@LogMessage(level = INFO)
    @Message(id = 28019, value = "Registering protostream serialization context initializer: %s")
-   void registeringSerializationContextInitializer(String className);
+   void registeringSerializationContextInitializer(String className);*/
 
-   @Message(id = 28020, value = "It is not possible to create indexes for a field having type %s. Field: %s.")
-   CacheException typeNotIndexable(String typeName, String fieldName);
+   /*@Message(id = 28020, value = "It is not possible to create indexes for a field having type %s. Field: %s.")
+   CacheException typeNotIndexable(String typeName, String fieldName);*/
 
    @Message(id = 28021, value = "The configured indexed-entity type '%s' must be indexed. Please annotate it with @Indexed and make sure at least one field has the @Field annotation, or remove it from the configuration.")
    CacheConfigurationException typeNotIndexed(String typeName);
