ca3b71e046081b7b84c68d9f90b67c6450454e72
Dan Berindei <dan@infinispan.org>
2021-08-22 21:49:02 +0300

    ISPN-11902 Make async store tests faster
    Instead of using a DummyInMemoryStore to delay all operations
    by the same amount, use a DelayStore to delay with a completable
    future.
    
    f

--- core/src/test/java/org/infinispan/persistence/support/ReplAsyncStoreTest.java
+++ core/src/test/java/org/infinispan/persistence/support/ReplAsyncStoreTest.java
@@ -12,11 +12,10 @@ import java.util.stream.Stream;
 
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.CacheMode;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.distribution.MagicKey;
-import org.infinispan.persistence.dummy.DummyInMemoryStoreConfigurationBuilder;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.infinispan.test.TestDataSCI;
+import org.infinispan.test.TestingUtil;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
@@ -45,7 +44,7 @@ public class ReplAsyncStoreTest extends MultipleCacheManagersTest {
       KEY_ITERATOR {
          @Override
          void perform(MagicKey key, Cache<MagicKey, String> cache) {
-            Iterator iterator = cache.keySet().iterator();
+            Iterator<?> iterator = cache.keySet().iterator();
             assertTrue(iterator.hasNext());
             assertEquals(key, iterator.next());
             assertFalse(iterator.hasNext());
@@ -54,7 +53,7 @@ public class ReplAsyncStoreTest extends MultipleCacheManagersTest {
       ENTRY_COLLECT {
          @Override
          void perform(MagicKey key, Cache<MagicKey, String> cache) {
-            List<Map.Entry<MagicKey, String>> list = cache.entrySet().stream().collect(() -> Collectors.toList());
+            List<Map.Entry<MagicKey, String>> list = cache.entrySet().stream().collect(Collectors::toList);
             assertEquals(1, list.size());
             assertEquals(key, list.get(0).getKey());
          }
@@ -84,11 +83,11 @@ public class ReplAsyncStoreTest extends MultipleCacheManagersTest {
 
    @Override
    protected void createCacheManagers() throws Throwable {
-      ConfigurationBuilder cfg = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, true);
+      org.infinispan.configuration.cache.ConfigurationBuilder
+            cfg = getDefaultClusteredCacheConfig(CacheMode.REPL_SYNC, true);
 
       cfg.persistence()
-            .addStore(DummyInMemoryStoreConfigurationBuilder.class)
-               .slow(true)
+            .addStore(DelayStore.ConfigurationBuilder.class)
                .storeName(shared ? ReplAsyncStoreTest.class.getName() : null)
                .shared(shared)
                .async().enable();
@@ -108,8 +107,15 @@ public class ReplAsyncStoreTest extends MultipleCacheManagersTest {
    @Test(dataProvider = "async-ops")
    public void testOperationAfterInsertAndEvict(boolean primary, Op consumer) {
       Cache<MagicKey, String> primaryOwner = cache(0, CACHE_NAME);
+      Cache<MagicKey, String> backupOwner = cache(1, CACHE_NAME);
       MagicKey key = getKeyForCache(primaryOwner);
-      // The actual store write is delayed 100 ms
+
+      // Delay the underlying store write
+      DelayStore primaryStore = TestingUtil.getFirstStore(primaryOwner);
+      primaryStore.delayBeforeModification(1);
+      DelayStore backupStore = TestingUtil.getFirstStore(backupOwner);
+      backupStore.delayBeforeModification(1);
+
       primaryOwner.put(key, "some-value");
 
       Cache<MagicKey, String> cacheToUse;
@@ -117,12 +123,15 @@ public class ReplAsyncStoreTest extends MultipleCacheManagersTest {
       if (primary) {
          cacheToUse = primaryOwner;
       } else {
-         cacheToUse = cache(1, CACHE_NAME);
+         cacheToUse = backupOwner;
       }
 
-      // This messes up some things - so evict so it is only in store (which shouldn't be written yet)
+      // Evict the key so it only exists in the store
       cacheToUse.evict(key);
 
       consumer.perform(key, cacheToUse);
+
+      primaryStore.endDelay();
+      backupStore.endDelay();
    }
 }
