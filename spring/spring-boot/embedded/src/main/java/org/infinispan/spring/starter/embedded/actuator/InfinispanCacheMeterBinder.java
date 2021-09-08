79cfb0fcf030fd50c78425e72992969051e22b24
Ramon Gordillo <gordillo.ramon@gmail.com>
2021-09-08 18:42:18 +0200

    Fixed size statistic

--- spring/spring-boot/embedded/src/main/java/org/infinispan/spring/starter/embedded/actuator/InfinispanCacheMeterBinder.java
+++ spring/spring-boot/embedded/src/main/java/org/infinispan/spring/starter/embedded/actuator/InfinispanCacheMeterBinder.java
@@ -28,7 +28,7 @@ public class InfinispanCacheMeterBinder extends CacheMeterBinder {
    protected Long size() {
       if (cache == null) return 0L;
 
-      return cache.getAdvancedCache().getStats().getStores();
+      return cache.getAdvancedCache().getStats().getNumberOfEntries();
    }
 
    @Override
