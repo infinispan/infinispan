4b831656a766644455d39a5f2bc1c785c426031c
Dan Berindei <dan@infinispan.org>
2021-09-24 11:24:32 +0300

    ISPN-12713 Fix GreetingServiceIT and GreetingCacheManagerIT
    * Include all the test classes in the web archive
    * Include the JCache dependency explicitly

--- integrationtests/server-integration/third-party-server/src/test/java/org/infinispan/test/integration/thirdparty/cdi/GreetingServiceIT.java
+++ integrationtests/server-integration/third-party-server/src/test/java/org/infinispan/test/integration/thirdparty/cdi/GreetingServiceIT.java
@@ -6,23 +6,23 @@ import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.Archive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Ignore;
 import org.junit.runner.RunWith;
 
 /**
  * @author Kevin Pollet &lt;pollet.kevin@gmail.com&gt; (C) 2011
  */
-@Ignore("ISPN-12713")
 @RunWith(Arquillian.class)
 public class GreetingServiceIT extends AbstractGreetingServiceIT {
 
    @Deployment
    public static Archive<?> deployment() {
       WebArchive war = DeploymentHelper.createDeployment();
-      war.addClass(AbstractGreetingServiceIT.class);
+      war.addPackage(AbstractGreetingServiceIT.class.getPackage());
       war.addAsWebInfResource("beans.xml");
       DeploymentHelper.addLibrary(war, "org.infinispan:infinispan-cdi-embedded");
       DeploymentHelper.addLibrary(war, "org.infinispan:infinispan-jcache");
+      // The JCache dependency is in the provided scope, so it's not added automatically
+      DeploymentHelper.addLibrary(war, "javax.cache:cache-api");
       return war;
    }
 }
