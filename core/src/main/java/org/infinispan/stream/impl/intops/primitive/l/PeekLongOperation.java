bbeea7d3d19c2a9a2bacc3ef56aa366fe3f4cb52
Dan Berindei <dan@infinispan.org>
2021-10-18 13:10:35 +0300

    ISPN-13398 Run ForEach/Peek user code on a blocking thread

--- core/src/main/java/org/infinispan/stream/impl/intops/primitive/l/PeekLongOperation.java
+++ core/src/main/java/org/infinispan/stream/impl/intops/primitive/l/PeekLongOperation.java
@@ -6,14 +6,17 @@ import java.util.stream.LongStream;
 import org.infinispan.factories.ComponentRegistry;
 import org.infinispan.stream.CacheAware;
 import org.infinispan.stream.impl.intops.IntermediateOperation;
+import org.infinispan.util.concurrent.BlockingManager;
 
 import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Single;
 
 /**
  * Performs peek operation on a {@link LongStream}
  */
 public class PeekLongOperation implements IntermediateOperation<Long, LongStream, Long, LongStream> {
    private final LongConsumer consumer;
+   private BlockingManager blockingManager;
 
    public PeekLongOperation(LongConsumer consumer) {
       this.consumer = consumer;
@@ -30,6 +33,7 @@ public class PeekLongOperation implements IntermediateOperation<Long, LongStream
 
    @Override
    public void handleInjection(ComponentRegistry registry) {
+      blockingManager = registry.getBlockingManager().running();
       if (consumer instanceof CacheAware) {
          ((CacheAware) consumer).injectCache(registry.getCache().running());
       } else {
@@ -39,6 +43,10 @@ public class PeekLongOperation implements IntermediateOperation<Long, LongStream
 
    @Override
    public Flowable<Long> mapFlowable(Flowable<Long> input) {
-      return input.doOnNext(consumer::accept);
+      return input.concatMapSingle(t -> Single.fromCompletionStage(
+            blockingManager.supplyBlocking(() -> {
+               consumer.accept(t);
+               return t;
+            }, "publisher-peek")));
    }
 }
