82e5deb7bae5d32fe55c933c77c92870b6093560
Pedro Ruivo <pruivo@redhat.com>
2021-11-12 14:45:13 +0000

    ISPN-13473 Improve SingleSiteRequest.sitesUnreachable()
    * Use only one thread to handle the event for each site.
    * While a thread is handling the event, fail the new requests.

---
+++ core/src/main/java/org/infinispan/remoting/transport/impl/SiteUnreachableXSiteResponse.java
@@ -0,0 +1,42 @@
+package org.infinispan.remoting.transport.impl;
+
+import static org.infinispan.util.logging.Log.CLUSTER;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.infinispan.commons.time.TimeService;
+import org.infinispan.remoting.transport.XSiteResponse;
+import org.infinispan.remoting.transport.jgroups.SuspectException;
+import org.infinispan.xsite.XSiteBackup;
+
+/**
+ * A {@link XSiteResponse} which is competed with a {@link SuspectException}.
+ *
+ * @since 14.0
+ */
+public class SiteUnreachableXSiteResponse<T> extends CompletableFuture<T> implements XSiteResponse<T> {
+
+   private final XSiteBackup backup;
+   private final long sendTimeNanos;
+   private final SuspectException exception;
+
+   public SiteUnreachableXSiteResponse(XSiteBackup backup, TimeService timeService) {
+      this.backup = backup;
+      this.sendTimeNanos = timeService.time();
+      this.exception = CLUSTER.remoteNodeSuspected(null);
+      completeExceptionally(exception);
+   }
+
+   @Override
+   public void whenCompleted(XSiteResponseCompleted xSiteResponseCompleted) {
+      xSiteResponseCompleted.onCompleted(backup, sendTimeNanos, -1, exception);
+   }
+
+   @Override
+   public String toString() {
+      return "SiteUnreachableXSiteResponse{" +
+            "backup=" + backup +
+            ", sendTimeNanos=" + sendTimeNanos +
+            '}';
+   }
+}
