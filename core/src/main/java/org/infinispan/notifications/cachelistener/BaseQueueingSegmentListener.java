9eb8fccc5828ab2c0a0ff4b11ec0bfc819293a3d
William Burns <mudokonman@gmail.com>
2021-11-16 14:26:41 -0500

    ISPN-13485 Listener includeCurrentState memory improvements

--- core/src/main/java/org/infinispan/notifications/cachelistener/BaseQueueingSegmentListener.java
+++ core/src/main/java/org/infinispan/notifications/cachelistener/BaseQueueingSegmentListener.java
@@ -1,19 +1,20 @@
 package org.infinispan.notifications.cachelistener;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Optional;
-import java.util.Set;
-import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
+import org.infinispan.commands.SegmentSpecificCommand;
 import org.infinispan.container.entries.CacheEntry;
+import org.infinispan.distribution.ch.KeyPartitioner;
 import org.infinispan.notifications.cachelistener.event.Event;
+import org.infinispan.reactive.RxJavaInterop;
 import org.infinispan.reactive.publisher.impl.SegmentPublisherSupplier;
-import org.infinispan.util.concurrent.CompletableFutures;
 import org.infinispan.util.logging.Log;
+import org.reactivestreams.Publisher;
+
+import io.reactivex.rxjava3.core.Flowable;
 
 /**
  * This is the base class for use when listening to segment completions when doing initial event
@@ -21,98 +22,80 @@ import org.infinispan.util.logging.Log;
  * appropriate methods at the given time.
  * <p>
  * This base class provides a working set for tracking of entries as they are iterated on, assuming
- * the {@link QueueingSegmentListener#test(SegmentPublisherSupplier.Notification)}
- * method is invoked for each event serially (includes both segment and entries).  Also this class provides the events
- * that caused entry creations that may not be processed yet that are returned by the
- * {@link QueueingSegmentListener#findCreatedEntries()} method.
+ * the {@link QueueingSegmentListener#apply(SegmentPublisherSupplier.Notification)}
+ * method is invoked for each event serially (includes both segment and entries).
  *
  * @author wburns
  * @since 7.0
  */
 abstract class BaseQueueingSegmentListener<K, V, E extends Event<K, V>> implements QueueingSegmentListener<K, V, E> {
    protected final AtomicBoolean completed = new AtomicBoolean(false);
-   protected final ConcurrentMap<K, Object> notifiedKeys;
+   protected final AtomicReferenceArray<ConcurrentMap<K, Object>> notifiedKeys;
+   protected final KeyPartitioner keyPartitioner;
 
-   protected BaseQueueingSegmentListener() {
-      this.notifiedKeys = new ConcurrentHashMap<>();
+   protected BaseQueueingSegmentListener(int numSegments, KeyPartitioner keyPartitioner) {
+      this.notifiedKeys = new AtomicReferenceArray<>(numSegments);
+      this.keyPartitioner = keyPartitioner;
+      for (int i = 0; i < numSegments; ++i) {
+         notifiedKeys.set(i, new ConcurrentHashMap<>());
+      }
+   }
+
+   int segmentFromEventWrapper(EventWrapper<K, V, E> eventWrapper) {
+      return SegmentSpecificCommand.extractSegment(eventWrapper.getCommand(), eventWrapper.getKey(), keyPartitioner);
    }
 
    @Override
-   public Optional<CacheEntry<K, V>> apply(SegmentPublisherSupplier.Notification<CacheEntry<K, V>> cacheEntryNotification) throws Throwable {
+   public Publisher<CacheEntry<K, V>> apply(SegmentPublisherSupplier.Notification<CacheEntry<K, V>> cacheEntryNotification) throws Throwable {
       if (cacheEntryNotification.isSegmentComplete()) {
-         segmentComplete(cacheEntryNotification.completedSegment());
-         return Optional.empty();
+         return segmentComplete(cacheEntryNotification.completedSegment());
       }
+
+      int segment = cacheEntryNotification.valueSegment();
 
       CacheEntry<K, V> cacheEntry = cacheEntryNotification.value();
       K key = cacheEntry.getKey();
       // By putting the NOTIFIED value it has signaled that any more updates for this key have to be enqueued instead
       // of taking the last one
-      Object value = notifiedKeys.put(key, NOTIFIED);
+      Object value = notifiedKeys.get(segment).put(key, NOTIFIED);
       if (value == null)
-         return Optional.of(cacheEntry);
+         return Flowable.just(cacheEntry);
 
       if (getLog().isTraceEnabled()) {
          getLog().tracef("Processing key %s as a concurrent update occurred with value %s", key, value);
       }
-      return value != QueueingSegmentListener.REMOVED ? Optional.of(((CacheEntry<K, V>) value)) : Optional.empty();
+      return value != QueueingSegmentListener.REMOVED ? Flowable.just(((CacheEntry<K, V>) value)) : Flowable.empty();
    }
 
-   @Override
-   public Set<CacheEntry<K, V>> findCreatedEntries() {
-      Set<CacheEntry<K, V>> set = new HashSet<>();
-      // We also have to look for any additional creations that we didn't iterate on
-      for (Map.Entry<K, Object> entry : notifiedKeys.entrySet()) {
-         Object value = entry.getValue();
-         if (value != NOTIFIED) {
-            K key = entry.getKey();
-            Object replaceValue = value;
-            // Now try to put NOTIFIED in there - this is in case if another concurrent event comes in like a
-            // PUT/REMOVE/CLEAR
-            while (replaceValue != NOTIFIED && !notifiedKeys.replace(key, replaceValue, NOTIFIED)) {
-               replaceValue = notifiedKeys.get(key);
-            }
-            // Technically we should never get NOTIFIED as this is required to be called after manually marking
-            // keys as processed
-            if (replaceValue != NOTIFIED && replaceValue != REMOVED) {
-               set.add((CacheEntry<K, V>)replaceValue);
-            }
+
+   Flowable<CacheEntry<K, V>> segmentComplete(int segment) {
+      ConcurrentMap<K, Object> map = notifiedKeys.get(segment);
+      // Ensure `addEvent` below knows if the value was added or not
+      synchronized (map) {
+         notifiedKeys.set(segment, null);
+      }
+      return Flowable.fromIterable(map.entrySet())
+            // We only process entries we can remove ourselves to guarantee consistency with atomic updates
+            // Normally we would use iterator, but remove for iterator doesn't notify us if it actually removed the entry
+            .filter(e -> map.remove(e.getKey()) != null)
+            .map(RxJavaInterop.entryToValueFunction())
+            .filter(v -> v != NOTIFIED && v != REMOVED)
+            .map(v -> (CacheEntry<K, V>) v);
+   }
+
+   protected boolean addEvent(K key, int segment, Object value) {
+      ConcurrentMap<K, Object> map = notifiedKeys.get(segment);
+      if (map == null) {
+         return false;
+      }
+
+      synchronized (map) {
+         // Need to double check inside synchronized just in case concurrent segmentComplete occurred
+         if (notifiedKeys.get(segment) == map) {
+            return map.compute(key, (k, v) -> v == NOTIFIED ? v : value) == value;
          }
       }
-      return set;
-   }
-
-   @Override
-   public CompletionStage<Void> delayProcessing() {
-      return CompletableFutures.completedNull();
-   }
-
-   void segmentComplete(int segment) {
-      // Don't do anything here - should implement accept if segment completions are desired
-   }
-
-   protected boolean addEvent(K key, Object value) {
-      boolean returnValue;
-      Object prevEvent = notifiedKeys.get(key);
-      if (prevEvent == null) {
-         Object nowPrevious = notifiedKeys.putIfAbsent(key, value);
-         if (nowPrevious == null) {
-            returnValue = true;
-         } else if (nowPrevious != NOTIFIED) {
-            returnValue = addEvent(key, value);
-         } else {
-            returnValue = false;
-         }
-      } else if (prevEvent != NOTIFIED) {
-         if (notifiedKeys.replace(key, prevEvent, value)) {
-            returnValue = true;
-         } else {
-            returnValue = addEvent(key, value);
-         }
-      } else {
-         returnValue = false;
-      }
-      return returnValue;
+      return false;
    }
 
    protected abstract Log getLog();
