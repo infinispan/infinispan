dc17767b13a64b7311f13b1017ac9f121dc01ecb
Dan Berindei <dan@infinispan.org>
2022-01-07 12:11:55 +0200

    ISPN-13617 Remove deprecated ExpirationManager methods

--- core/src/main/java/org/infinispan/expiration/ExpirationManager.java
+++ core/src/main/java/org/infinispan/expiration/ExpirationManager.java
@@ -2,8 +2,6 @@ package org.infinispan.expiration;
 
 import org.infinispan.configuration.cache.ExpirationConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
-import org.infinispan.container.entries.InternalCacheEntry;
-import org.infinispan.persistence.spi.MarshallableEntry;
 
 import net.jcip.annotations.ThreadSafe;
 
@@ -31,52 +29,4 @@ public interface ExpirationManager<K, V> {
     * @return true if expiration reaper thread is enabled, false otherwise
     */
    boolean isEnabled();
-
-   /**
-    * This should be invoked passing in an entry that is now expired.  This method may attempt to lock this key to
-    * preserve atomicity.
-    * @param entry entry that is now expired
-    * @param currentTime the current time in milliseconds
-    * @deprecated since 9.3 this method is not intended for external use
-    */
-   @Deprecated
-   void handleInMemoryExpiration(InternalCacheEntry<K, V> entry, long currentTime);
-
-   /**
-    * This is to be invoked when a store entry expires.  This method may attempt to lock this key to preserve atomicity.
-    * <p>
-    * Note this method doesn't currently take a {@link InternalCacheEntry} and this is due to a limitation in the
-    * cache store API.  This may cause some values to be removed if they were updated at the same time.
-    * @param key the key of the expired entry
-    * @deprecated since 9.3 this method is not intended for external use
-    */
-   @Deprecated
-   void handleInStoreExpiration(K key);
-
-   /**
-    * This is to be invoked when a store entry expires and the value and/or metadata is available to be used.  This
-    * method is preferred over {@link ExpirationManager#handleInStoreExpiration(Object)} as it allows for more
-    * specific expiration to possibly occur.
-    * @param marshalledEntry the entry that can be unmarshalled as needed
-    * @deprecated since 9.3 this method is not intended for external use
-    */
-   @Deprecated
-   void handleInStoreExpiration(MarshallableEntry<K, V> marshalledEntry);
-
-   /**
-    * This is to be invoked with a when a write is known to occur to prevent expiration from happening.  This way we
-    * won't have a swarm of remote calls required.
-    * @param key the key to use
-    * @deprecated since 9.3 There is no reason for this method and is implementation specific
-    */
-   @Deprecated
-   default void registerWriteIncoming(K key) { }
-
-   /**
-    * This should always be invoked after registering write but after performing any operations required.
-    * @param key the key to use
-    * @deprecated since 9.3 There is no reason for this method and is implementation specific
-    */
-   @Deprecated
-   default void unregisterWrite(K key) { }
 }
