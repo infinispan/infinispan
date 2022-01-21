fd353961b870bb1f5bf8a8b9ab897f9613a1ce74
Dan Berindei <dan@infinispan.org>
2022-01-21 19:06:49 +0200

    ISPN-13549 EntryWrappingInterceptor data race handling expired entries
    Address review comments.
    Fix handling of GetAllCommand.

--- core/src/test/java/org/infinispan/expiration/impl/ClusterExpirationFunctionalTest.java
+++ core/src/test/java/org/infinispan/expiration/impl/ClusterExpirationFunctionalTest.java
@@ -8,6 +8,7 @@ import static org.testng.AssertJUnit.assertTrue;
 
 import java.lang.invoke.MethodHandles;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
@@ -382,22 +383,36 @@ public class ClusterExpirationFunctionalTest extends MultipleCacheManagersTest {
    public void testPutAllExpiredEntries() {
       SkipTestNG.skipIf(cacheMode.isDistributed() && transactional,
                         "Disabled in transactional caches because of ISPN-13618");
-      SkipTestNG.skipIf(cacheMode.isScattered(), "Disabled in scattered caches because of ISPN-13619");
+      SkipTestNG.skipIf(cacheMode.isScattered(), "Disabled in transactional caches because of ISPN-13619");
 
-      Map<String, String> v1s = new HashMap<>();
       // Can reproduce ISPN-13549 with nKey=20_000 and no trace logs (and without the fix)
       int nKeys = 4;
-      Map<String, String> v2s = new HashMap<>();
-      for (int i = 0; i < nKeys; i++) {
-         v2s.put("k" + i, "v2");
-      }
-
       for (int i = 0; i < nKeys * 3 / 4; i++) {
          cache0.put("k" + i, "v1", -1, SECONDS, 10, SECONDS);
       }
 
       incrementAllTimeServices(11, SECONDS);
 
+      Map<String, String> v2s = new HashMap<>();
+      for (int i = 0; i < nKeys; i++) {
+         v2s.put("k" + i, "v2");
+      }
       cache0.putAll(v2s, -1, SECONDS, 10, SECONDS);
+   }
+
+   public void testGetAllExpiredEntries() {
+      // Can reproduce ISPN-13549 with nKey=20_000 and no trace logs (and without the fix)
+      int nKeys = 4;
+      for (int i = 0; i < nKeys * 3 / 4; i++) {
+         cache0.put("k" + i, "v1", -1, SECONDS, 10, SECONDS);
+      }
+
+      incrementAllTimeServices(11, SECONDS);
+
+      Map<String, String> v1s = new HashMap<>();
+      for (int i = 0; i < nKeys; i++) {
+         v1s.put("k" + i, "v1");
+      }
+      assertEquals(Collections.emptyMap(), cache0.getAdvancedCache().getAll(v1s.keySet()));
    }
 }
