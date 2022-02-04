b126f1d07e7e733af20a0f49125f636e22801f7b
Pedro Ruivo <pruivo@redhat.com>
2022-02-04 20:15:07 +0000

    ISPN-13446 IRAC: configure the tombstone cleanup task
    Dynamic task based on a target map size: if the current number of stored
    tombstones are higher, decrease the schedule interval to make the
    cleanup task more frequent (and vice-versa).

--- core/src/test/java/org/infinispan/xsite/XSiteParsingTest.java
+++ core/src/test/java/org/infinispan/xsite/XSiteParsingTest.java
@@ -1,14 +1,22 @@
 package org.infinispan.xsite;
 
+import static org.infinispan.commons.test.Exceptions.expectException;
+import static org.infinispan.test.TestingUtil.extractGlobalComponent;
+import static org.infinispan.test.TestingUtil.wrapXMLWithSchema;
+import static org.testng.AssertJUnit.assertTrue;
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+import org.infinispan.commons.CacheConfigurationException;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.global.GlobalConfiguration;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.remoting.transport.Transport;
 import org.infinispan.test.AbstractInfinispanTest;
-import org.infinispan.test.TestingUtil;
-import org.testng.AssertJUnit;
 import org.testng.annotations.Test;
 
 /**
@@ -21,7 +29,7 @@ public class XSiteParsingTest extends AbstractInfinispanTest {
 
    // https://issues.redhat.com/browse/ISPN-13623 reproducer
    public void testMultipleStackParents() throws IOException {
-      String config = TestingUtil.wrapXMLWithSchema(
+      String config = wrapXMLWithSchema(
             "<jgroups>" +
                   "<stack name=\"parent\" extends=\"udp\">" +
                   "  <UDP mcast_port=\"54444\"/>" +
@@ -44,9 +52,38 @@ public class XSiteParsingTest extends AbstractInfinispanTest {
 
       try (DefaultCacheManager cm = new DefaultCacheManager(new ByteArrayInputStream(config.getBytes(StandardCharsets.UTF_8)))) {
          // just to make sure the DefaultCacheManager starts.
-         AssertJUnit.assertTrue(TestingUtil.extractGlobalComponent(cm, Transport.class).isSiteCoordinator());
+         assertTrue(extractGlobalComponent(cm, Transport.class).isSiteCoordinator());
       }
+   }
 
+   public void testInvalidMaxTombstoneCleanupDelay() {
+      String config1 = wrapXMLWithSchema(
+            "<cache-container>" +
+                  "   <transport/>" +
+                  "      <distributed-cache name=\"A\">" +
+                  "         <backups max-cleanup-delay=\"-1\"/>" +
+                  "      </distributed-cache>" +
+                  "</cache-container>"
+      );
+      assertCacheConfigurationException(config1, "A", "ISPN000951: Invalid value -1 for attribute max-cleanup-delay: must be a number greater than zero");
+      String config2 = wrapXMLWithSchema(
+            "<cache-container>" +
+                  "   <transport/>" +
+                  "      <distributed-cache name=\"B\">" +
+                  "         <backups max-cleanup-delay=\"0\"/>" +
+                  "      </distributed-cache>" +
+                  "</cache-container>"
+      );
+      assertCacheConfigurationException(config2, "B", "ISPN000951: Invalid value 0 for attribute max-cleanup-delay: must be a number greater than zero");
+   }
+
+   private void assertCacheConfigurationException(String config, String cacheName, String messageRegex) {
+      ParserRegistry parserRegistry = new ParserRegistry();
+      ConfigurationBuilderHolder holder = parserRegistry.parse(config);
+      GlobalConfiguration globalConfiguration = holder.getGlobalConfigurationBuilder().build();
+      ConfigurationBuilder builder = holder.getNamedConfigurationBuilders().get(cacheName);
+      expectException(CacheConfigurationException.class, messageRegex, builder::validate);
+      expectException(CacheConfigurationException.class, messageRegex, () -> builder.validate(globalConfiguration));
    }
 
 }
