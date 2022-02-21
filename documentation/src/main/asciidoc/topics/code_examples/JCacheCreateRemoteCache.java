f1cd3356879c97d1184a6dda9fcec09f241381a4
Don Naro <dnaro@redhat.com>
2022-02-21 17:57:27 +0000

    ISPN-13197 developer guide content re-org

--- documentation/src/main/asciidoc/topics/code_examples/JCacheCreateRemoteCache.java
+++ documentation/src/main/asciidoc/topics/code_examples/JCacheCreateRemoteCache.java
@@ -1,7 +1,7 @@
 import javax.cache.*;
 import javax.cache.configuration.*;
 
-// Retrieve the system wide cache manager via org.infinispan.jcache.remote.JCachingProvider
+// Retrieve the system wide Cache Manager via org.infinispan.jcache.remote.JCachingProvider
 CacheManager cacheManager = Caching.getCachingProvider("org.infinispan.jcache.remote.JCachingProvider").getCacheManager();
 // Define a named cache with default JCache configuration
 Cache<String, String> cache = cacheManager.createCache("remoteNamedCache",
