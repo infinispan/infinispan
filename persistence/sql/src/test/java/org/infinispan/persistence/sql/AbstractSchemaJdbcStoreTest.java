15f8f05977a06aa564276969dd939fe54891a43f
varunshastry <varunshastry@rediffmail.com>
2022-03-01 08:59:10 +0530

    Adding junit for AbstractSchemaJdbcStore

---
+++ persistence/sql/src/test/java/org/infinispan/persistence/sql/AbstractSchemaJdbcStoreTest.java
@@ -0,0 +1,57 @@
+package org.infinispan.persistence.sql;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.sql.Types;
+
+import org.junit.jupiter.api.Test;
+
+public class AbstractSchemaJdbcStoreTest {
+
+	@Test
+	void testProtostreamFieldTypeFrom() {
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.INT_32,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.INTEGER));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.INT_32,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.NUMERIC));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.INT_64,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.BIGINT));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.FLOAT,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.FLOAT));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.DOUBLE,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.DOUBLE));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.BOOL,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.BIT));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.BOOL,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.BOOLEAN));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.STRING,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.VARCHAR));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.STRING,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.NVARCHAR));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.STRING,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.LONGVARCHAR));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.STRING,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.LONGNVARCHAR));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.BYTES,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.BLOB));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.BYTES,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.BINARY));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.BYTES,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.VARBINARY));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.BYTES,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.LONGVARBINARY));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.DATE,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.DATE));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.DATE,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.TIME));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.DATE,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.TIMESTAMP));
+		assertEquals(AbstractSchemaJdbcStore.ProtostreamFieldType.DATE,
+				AbstractSchemaJdbcStore.ProtostreamFieldType.from(Types.TIMESTAMP_WITH_TIMEZONE));
+		assertThrows(IllegalArgumentException.class, () -> {
+			AbstractSchemaJdbcStore.ProtostreamFieldType.from(1234);
+		});
+
+	}
+}
