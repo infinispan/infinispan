707a5976cd6a2a8d7ead4b771304c01c28ba14a6
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-04-05 10:18:08 +0200

    ISPN-13499 Test fulltext parameters

---
+++ query/src/test/java/org/infinispan/query/model/Book.java
@@ -0,0 +1,31 @@
+package org.infinispan.query.model;
+
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.GenericField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
+@Indexed
+public class Book {
+
+   @GenericField
+   private String title;
+
+   @FullTextField
+   private String description;
+
+   public String getTitle() {
+      return title;
+   }
+
+   public void setTitle(String title) {
+      this.title = title;
+   }
+
+   public String getDescription() {
+      return description;
+   }
+
+   public void setDescription(String description) {
+      this.description = description;
+   }
+}
