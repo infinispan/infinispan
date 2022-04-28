0ab50c13fa8a90fa30c815f58695fa6ad63ddc74
Tristan Tarrant <ttarrant@redhat.com>
2022-04-28 10:53:10 +0200

    Add a docs index dropdown

--- documentation/src/main/asciidoc/js/js.js
+++ documentation/src/main/asciidoc/js/js.js
@@ -2,6 +2,35 @@ $(document).ready(function() {
     var prefix = "/docs/"
     var path = document.location.pathname;
     var version = path.substring(prefix.length, path.indexOf("/", prefix.length));
+    $('#toctitle').before('<select id="dchooser"></select>');
+    $('#dchooser').append('<option>Other guides</option>');
+    $('#dchooser').append('<option value="/titles/cli/cli.html">Using the Infinispan Command Line Interface</option>');
+    $('#dchooser').append('<option value="/titles/configuring/configuring.html">Configuring Infinispan caches</option>');
+    $('#dchooser').append('<option value="/titles/contributing/contributing.html">Contributor&quot;s guide</option>');
+    $('#dchooser').append('<option value="/titles/developing/developing.html">Infinispan Developer Guide</option>');
+    $('#dchooser').append('<option value="/titles/embedding/embedding.html">Embedding Infinispan caches in Java applications</option>');
+    $('#dchooser').append('<option value="/titles/encoding/encoding.html">Encoding Infinispan caches and marshalling data</option>');
+    $('#dchooser').append('<option value="/titles/getting_started/getting_started.html">Getting started with Infinispan</option>');
+    $('#dchooser').append('<option value="/titles/hibernate/hibernate.html">Hibernate second-level caching (2LC)</option>');
+    $('#dchooser').append('<option value="/titles/hotrod_java/hotrod_java.html">Using Hot Rod Java clients</option>');
+    $('#dchooser').append('<option value="/titles/hotrod_protocol/hotrod_protocol.html">Hot Rod protocol reference</option>');
+    $('#dchooser').append('<option value="/titles/memcached/memcached.html">Using the Memcached protocol endpoint with Infinispan</option>');
+    $('#dchooser').append('<option value="/titles/query/query.html">Querying Infinispan caches</option>');
+    $('#dchooser').append('<option value="/titles/resp/resp-endpoint.html">Using the RESP protocol endpoint with Infinispan</option>');
+    $('#dchooser').append('<option value="/titles/rest/rest.html">Using the Infinispan REST API</option>');
+    $('#dchooser').append('<option value="/titles/security/security.html">Infinispan Security Guide</option>');
+    $('#dchooser').append('<option value="/titles/server/server.html">Guide to Infinispan Server</option>');
+    $('#dchooser').append('<option value="/titles/spring_boot/starter.html">Infinispan Spring Boot Starter</option>');
+    $('#dchooser').append('<option value="/titles/spring/spring.html">Spring Cache and Spring Sessions with Infinispan</option>');
+    $('#dchooser').append('<option value="/titles/tuning/tuning.html">Infinispan performance considerations and tuning guidelines</option>');
+    $('#dchooser').append('<option value="/titles/upgrading/upgrading.html">Upgrading Infinispan deployments</option>');
+    $('#dchooser').append('<option value="/titles/xsite/xsite.html">Infinispan cross-site replication</option>');
+    $('#dchooser').change(function(e) {
+       if (this.value !== '') {
+          window.location.href = path.substring(0, path.indexOf('/titles/')) + this.value;
+       }
+    });
+    $('#dchooser').after('<hr/>');
     $.ajax({type: 'GET', dataType: 'xml', url: '/docs/versions.xml',
             success: function(xml) {
                 $('#toctitle').before('<select id="vchooser"></select>');
@@ -15,6 +44,7 @@ $(document).ready(function() {
                     if (this.value !== '')
                         window.location.href = path.replace(version, this.value);
                 });
+                $('#vchooser').after('<hr/>');
             }
     });
 
