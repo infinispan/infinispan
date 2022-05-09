e0c7dbacb58edd43047c80566d1eaeef23dfb75d
Pedro Ruivo <pruivo@redhat.com>
2022-05-09 16:10:15 +0100

    ISPN-13296 Remove JPA cachestore

--- documentation/src/main/asciidoc/topics/code_examples/CacheJpaStore.java
+++
@@ -1,7 +0,0 @@
-Cache<String, User> usersCache = cacheManager.getCache("myJPACache");
-// Cache is configured for the User entity class
-usersCache.put("username", new User());
-// Cannot configure caches to use another entity class with JPA cache stores
-Cache<Integer, Teacher> teachersCache = cacheManager.getCache("myJPACache");
-teachersCache.put(1, new Teacher());
-// The put request does not work for the Teacher entity class
--- documentation/src/main/asciidoc/topics/code_examples/ConfigJpaStore.java
+++
@@ -1,5 +0,0 @@
-Configuration cacheConfig = new ConfigurationBuilder().persistence()
-             .addStore(JpaStoreConfigurationBuilder.class)
-             .persistenceUnitName("org.infinispan.loaders.jpa.configurationTest")
-             .entityClass(User.class)
-             .build();
