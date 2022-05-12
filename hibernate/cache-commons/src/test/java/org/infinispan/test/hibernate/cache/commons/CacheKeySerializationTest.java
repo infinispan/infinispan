f8bdc096dd9ad8f46499a5c69eb61f5c6e289c70
William Burns <mudokonman@gmail.com>
2022-05-12 12:28:34 -0400

    ISPN-13886 Update Hibernate to 6.0.1

--- hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/CacheKeySerializationTest.java
+++ hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/CacheKeySerializationTest.java
@@ -71,12 +71,11 @@ public class CacheKeySerializationTest extends BaseUnitTestCase {
 		testId(DefaultCacheKeysFactory.INSTANCE, WithSimpleId.class.getName(), 1L);
 	}
 
-	// TEST disabled until https://hibernate.atlassian.net/browse/HHH-15150 can be resolved
-//	@Test
-//	@TestForIssue(jiraKey = "HHH-11202")
-//	public void testDefaultCacheKeyEmbeddedId() throws Exception {
-//		testId(DefaultCacheKeysFactory.INSTANCE, WithEmbeddedId.class.getName(), new PK(1L));
-//	}
+	@Test
+	@TestForIssue(jiraKey = "HHH-11202")
+	public void testDefaultCacheKeyEmbeddedId() throws Exception {
+		testId(DefaultCacheKeysFactory.INSTANCE, WithEmbeddedId.class.getName(), new PK(1L));
+	}
 
 	private void testId(CacheKeysFactory cacheKeysFactory, String entityName, Object id) throws Exception {
 		final SessionFactoryImplementor sessionFactory = getSessionFactory(cacheKeysFactory.getClass().getName());
