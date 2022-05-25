88834b29d2104efad792021b4d29df39707cc297
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-05-25 12:37:08 +0200

    ISPN-13923 Test embedded protobuf-encoded indexed cache

---
+++ query/src/test/java/org/infinispan/query/model/Game.java
@@ -0,0 +1,41 @@
+package org.infinispan.query.model;
+
+import org.hibernate.search.engine.backend.types.Projectable;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.KeywordField;
+import org.infinispan.protostream.GeneratedSchema;
+import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
+
+@Indexed(index = "play")
+public class Game {
+
+   private String name;
+
+   private String description;
+
+   @ProtoFactory
+   public Game(String name, String description) {
+      this.name = name;
+      this.description = description;
+   }
+
+   @KeywordField(projectable = Projectable.YES)
+   @ProtoField(1)
+   public String getName() {
+      return name;
+   }
+
+   @FullTextField
+   @ProtoField(2)
+   public String getDescription() {
+      return description;
+   }
+
+   @AutoProtoSchemaBuilder(includeClasses = Game.class)
+   public interface GameSchema extends GeneratedSchema {
+      GameSchema INSTANCE = new GameSchemaImpl();
+   }
+}
