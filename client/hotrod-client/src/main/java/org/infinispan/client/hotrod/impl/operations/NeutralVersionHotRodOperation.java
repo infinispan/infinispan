4d0cc1fdf653ad3dc43a09e75d08d853b9642e6f
Pedro Ruivo <pruivo@redhat.com>
2022-08-01 09:34:10 +0100

    Fix compile error caused by ISPN-13248 and ISPN-13725

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/operations/NeutralVersionHotRodOperation.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/operations/NeutralVersionHotRodOperation.java
@@ -1,12 +1,14 @@
 package org.infinispan.client.hotrod.impl.operations;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
-import net.jcip.annotations.Immutable;
 import org.infinispan.client.hotrod.ProtocolVersion;
 import org.infinispan.client.hotrod.configuration.Configuration;
+import org.infinispan.client.hotrod.impl.ClientTopology;
 import org.infinispan.client.hotrod.impl.protocol.Codec;
 import org.infinispan.client.hotrod.impl.transport.netty.ChannelFactory;
+
+import net.jcip.annotations.Immutable;
 
 /**
  * An extension of {@link HotRodOperation} for backwards compatibility after introducing HR 4.0. We override the
@@ -18,8 +20,8 @@ import org.infinispan.client.hotrod.impl.transport.netty.ChannelFactory;
 public abstract class NeutralVersionHotRodOperation<T> extends HotRodOperation<T> {
 
    protected NeutralVersionHotRodOperation(short requestCode, short responseCode, Codec codec, int flags, Configuration cfg,
-                                           byte[] cacheName, AtomicInteger topologyId, ChannelFactory channelFactory) {
-      super(requestCode, responseCode, chooseCodec(codec), flags, cfg, cacheName, topologyId, channelFactory);
+                                           byte[] cacheName, AtomicReference<ClientTopology> clientTopology, ChannelFactory channelFactory) {
+      super(requestCode, responseCode, chooseCodec(codec), flags, cfg, cacheName, clientTopology, channelFactory);
    }
 
    private static Codec chooseCodec(Codec codec) {
