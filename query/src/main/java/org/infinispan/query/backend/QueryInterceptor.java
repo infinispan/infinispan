aedcfa2f7393d61b1a8b3740476b90fc225e445b
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-09-07 12:29:38 +0200

    ISPN-14115 Rename the should remove method
    to express the idea that this is true if and only if we're replacing the type of the entity

--- query/src/main/java/org/infinispan/query/backend/QueryInterceptor.java
+++ query/src/main/java/org/infinispan/query/backend/QueryInterceptor.java
@@ -413,7 +413,7 @@ public final class QueryInterceptor extends DDAsyncInterceptor {
             if (shouldModifyIndexes(command, ctx, storedKey)) {
                operation = removeFromIndexes(key, segment);
             }
-         } else if (isPotentiallyIndexedType(oldValue) && (newValue == null || shouldRemove(newValue, oldValue))
+         } else if (isPotentiallyIndexedType(oldValue) && (newValue == null || replacedWithADifferentEntity(newValue, oldValue))
                && shouldModifyIndexes(command, ctx, storedKey)) {
             operation = removeFromIndexes(oldValue, key, segment);
          } else if (log.isTraceEnabled()) {
@@ -437,7 +437,7 @@ public final class QueryInterceptor extends DDAsyncInterceptor {
       return operation;
    }
 
-   private boolean shouldRemove(Object value, Object previousValue) {
+   private boolean replacedWithADifferentEntity(Object value, Object previousValue) {
       return value != null && previousValue != null && value.getClass() != previousValue.getClass();
    }
 
