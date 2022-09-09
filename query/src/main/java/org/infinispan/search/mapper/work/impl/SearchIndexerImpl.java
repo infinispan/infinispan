94298e1242434870d1fe4dfd6675c6864de4c7a2
William Burns <mudokonman@gmail.com>
2022-09-09 08:57:24 -0700

    ISPN-14127 Initial work

--- query/src/main/java/org/infinispan/search/mapper/work/impl/SearchIndexerImpl.java
+++ query/src/main/java/org/infinispan/search/mapper/work/impl/SearchIndexerImpl.java
@@ -11,21 +11,71 @@ import org.infinispan.search.mapper.mapping.EntityConverter;
 import org.infinispan.search.mapper.session.impl.InfinispanIndexedTypeContext;
 import org.infinispan.search.mapper.session.impl.InfinispanTypeContextProvider;
 import org.infinispan.search.mapper.work.SearchIndexer;
+import org.infinispan.util.concurrent.BlockingManager;
+import org.infinispan.util.concurrent.NonBlockingManager;
+
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.processors.FlowableProcessor;
+import io.reactivex.rxjava3.processors.UnicastProcessor;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 
 /**
  * @author Fabio Massimo Ercoli
  */
-public class SearchIndexerImpl implements SearchIndexer {
+public class SearchIndexerImpl implements SearchIndexer, Consumer<SearchIndexerImpl.IndexerOperation> {
 
    private final PojoIndexer delegate;
    private final EntityConverter entityConverter;
    private final InfinispanTypeContextProvider typeContextProvider;
+   private final NonBlockingManager nonBlockingManager;
+
+   private final FlowableProcessor<IndexerOperation> indexProcessor;
 
    public SearchIndexerImpl(PojoIndexer delegate, EntityConverter entityConverter,
-                            InfinispanTypeContextProvider typeContextProvider) {
+         InfinispanTypeContextProvider typeContextProvider, BlockingManager blockingManager,
+         NonBlockingManager nonBlockingManager) {
       this.delegate = delegate;
       this.entityConverter = entityConverter;
       this.typeContextProvider = typeContextProvider;
+      this.nonBlockingManager = nonBlockingManager;
+
+      this.indexProcessor = UnicastProcessor.<IndexerOperation>create().toSerialized();
+
+      indexProcessor
+            .observeOn(Schedulers.from(blockingManager.asExecutor("search-indexer")))
+            // TODO: log this error
+            .subscribe(this, e -> e.printStackTrace());
+
+   }
+
+   abstract class IndexerOperation extends CompletableFuture<Void> {
+      abstract void invoke(PojoIndexer pojoIndexer, NonBlockingManager nonBlockingManager);
+   }
+
+   class AddIndexerOperation extends IndexerOperation {
+      private final Object providedId;
+      private final String routingKey;
+      private final ConvertedValue convertedValue;
+
+      AddIndexerOperation(Object providedId, String routingKey, ConvertedValue convertedValue) {
+         this.providedId = providedId;
+         this.routingKey = routingKey;
+         this.convertedValue = convertedValue;
+      }
+
+      @Override
+      void invoke(PojoIndexer pojoIndexer, NonBlockingManager nonBlockingManager) {
+         delegate.add(convertedValue.typeIdentifier, providedId,
+                     DocumentRoutesDescriptor.fromLegacyRoutingKey(routingKey), convertedValue.value,
+                     DocumentCommitStrategy.NONE, DocumentRefreshStrategy.NONE)
+               .whenComplete((v, t) -> {
+                  if (t != null) {
+                     nonBlockingManager.completeExceptionally(this, t);
+                  } else {
+                     nonBlockingManager.complete(this, null);
+                  }
+               });
+      }
    }
 
    @Override
@@ -35,9 +85,9 @@ public class SearchIndexerImpl implements SearchIndexer {
          return CompletableFuture.completedFuture(null);
       }
 
-      return delegate.add(convertedValue.typeIdentifier, providedId,
-            DocumentRoutesDescriptor.fromLegacyRoutingKey(routingKey), convertedValue.value,
-            DocumentCommitStrategy.NONE, DocumentRefreshStrategy.NONE);
+      AddIndexerOperation addIndexerOperation = new AddIndexerOperation(providedId, routingKey, convertedValue);
+      indexProcessor.onNext(addIndexerOperation);
+      return addIndexerOperation;
    }
 
    @Override
@@ -47,6 +97,7 @@ public class SearchIndexerImpl implements SearchIndexer {
          return CompletableFuture.completedFuture(null);
       }
 
+      // TODO: needs task added and use flowable
       return delegate.addOrUpdate(convertedValue.typeIdentifier, providedId,
             DocumentRoutesDescriptor.fromLegacyRoutingKey(routingKey), convertedValue.value,
             DocumentCommitStrategy.NONE, DocumentRefreshStrategy.NONE);
@@ -59,6 +110,7 @@ public class SearchIndexerImpl implements SearchIndexer {
          return CompletableFuture.completedFuture(null);
       }
 
+      // TODO: needs task added and use flowable
       return delegate.delete(convertedValue.typeIdentifier, providedId,
             DocumentRoutesDescriptor.fromLegacyRoutingKey(routingKey), convertedValue.value,
             DocumentCommitStrategy.NONE, DocumentRefreshStrategy.NONE);
@@ -66,6 +118,7 @@ public class SearchIndexerImpl implements SearchIndexer {
 
    @Override
    public CompletableFuture<?> purge(Object providedId, String routingKey) {
+      // TODO: needs task added and use flowable
       return CompletableFuture.allOf(typeContextProvider.allTypeIdentifiers().stream()
             .map((typeIdentifier) -> delegate.delete(typeIdentifier, providedId,
                   DocumentRoutesDescriptor.fromLegacyRoutingKey(routingKey),
@@ -97,6 +150,11 @@ public class SearchIndexerImpl implements SearchIndexer {
       }
 
       return new ConvertedValue(typeContext, converted.value());
+   }
+
+   @Override
+   public void accept(IndexerOperation o) {
+      o.invoke(delegate, nonBlockingManager);
    }
 
    private static class ConvertedValue {
