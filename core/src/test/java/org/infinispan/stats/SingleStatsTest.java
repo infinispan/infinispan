6d70300b40cb3c8f2ee979872784b57f98546546
William Burns <mudokonman@gmail.com>
2022-09-09 14:54:09 -0700

    ISPN-14128 Stats throw UnsupportedOperationException with EXCEPTION eviction

--- core/src/test/java/org/infinispan/stats/SingleStatsTest.java
+++ core/src/test/java/org/infinispan/stats/SingleStatsTest.java
@@ -5,17 +5,23 @@ import static org.infinispan.container.offheap.UnpooledOffHeapMemoryAllocator.of
 import static org.testng.AssertJUnit.assertEquals;
 import static org.testng.AssertJUnit.assertNull;
 import static org.testng.AssertJUnit.assertTrue;
+import static org.testng.AssertJUnit.fail;
+
+import java.util.Arrays;
 
 import org.infinispan.Cache;
+import org.infinispan.commons.CacheException;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.MemoryConfigurationBuilder;
 import org.infinispan.configuration.cache.StorageType;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
 import org.infinispan.container.offheap.OffHeapConcurrentMap;
 import org.infinispan.context.Flag;
-import org.infinispan.eviction.EvictionType;
+import org.infinispan.eviction.EvictionStrategy;
 import org.infinispan.persistence.dummy.DummyInMemoryStoreConfigurationBuilder;
 import org.infinispan.test.MultipleCacheManagersTest;
+import org.infinispan.transaction.TransactionMode;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
@@ -29,29 +35,33 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
    protected final int EVICTION_MAX_ENTRIES = 3;
    protected final int TOTAL_ENTRIES = 5;
    protected StorageType storageType;
-   protected EvictionType evictionType;
+   protected boolean countBasedEviction;
+   protected EvictionStrategy evictionStrategy = EvictionStrategy.REMOVE;
    protected Cache<String, String> cache;
    protected Stats stats;
 
    @Override
    public Object[] factory() {
-      return new Object[]{
-            new SingleStatsTest().withStorage(StorageType.BINARY).withEvictionType(EvictionType.MEMORY),
-            new SingleStatsTest().withStorage(StorageType.BINARY).withEvictionType(EvictionType.COUNT),
-            new SingleStatsTest().withStorage(StorageType.OBJECT).withEvictionType(EvictionType.COUNT),
-            new SingleStatsTest().withStorage(StorageType.OFF_HEAP).withEvictionType(EvictionType.COUNT),
-            new SingleStatsTest().withStorage(StorageType.OFF_HEAP).withEvictionType(EvictionType.MEMORY),
-      };
+      return Arrays.stream(EvictionStrategy.values())
+            .flatMap(strategy ->
+                  Arrays.stream(new Object[]{
+                        new SingleStatsTest().withStorage(StorageType.BINARY).withCountEviction(false).withEvictionStrategy(strategy),
+                        new SingleStatsTest().withStorage(StorageType.BINARY).withCountEviction(true).withEvictionStrategy(strategy),
+                        new SingleStatsTest().withStorage(StorageType.HEAP).withCountEviction(true).withEvictionStrategy(strategy),
+                        new SingleStatsTest().withStorage(StorageType.OFF_HEAP).withCountEviction(true).withEvictionStrategy(strategy),
+                        new SingleStatsTest().withStorage(StorageType.OFF_HEAP).withCountEviction(false).withEvictionStrategy(strategy)
+                  })
+            ).toArray();
    }
 
    @Override
    protected String[] parameterNames() {
-      return concat(super.parameterNames(), "StorageType", "EvictionType");
+      return concat(super.parameterNames(), "StorageType", "CountBasedEviction", "EvictionStrategy");
    }
 
    @Override
    protected Object[] parameterValues() {
-      return concat(super.parameterValues(), storageType, evictionType);
+      return concat(super.parameterValues(), storageType, countBasedEviction, evictionStrategy);
    }
 
    @Override
@@ -67,26 +77,33 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
 
    protected void configure(ConfigurationBuilder cfg) {
       long size = EVICTION_MAX_ENTRIES;
-      if (evictionType == EvictionType.MEMORY) {
-         if (storageType == StorageType.BINARY) {
-            // Binary key/value size is 128 bytes
-            size *= 128;
-         } else {
+      MemoryConfigurationBuilder memoryConfigurationBuilder = cfg.memory();
+      memoryConfigurationBuilder.storage(storageType);
+      if (countBasedEviction) {
+         memoryConfigurationBuilder.maxCount(size);
+      } else {
+         if (storageType == StorageType.OFF_HEAP) {
             // Off heap key/value size is 80 bytes
             size = estimateSizeOverhead(size * OFF_HEAP_SIZE);
             // Have to also include address count overhead
             size += estimateSizeOverhead(OffHeapConcurrentMap.INITIAL_SIZE << 3);
+         } else {
+            // Binary key/value size is 128 bytes
+            // Exception requires transactions which adds additional overhead
+            size *= (128 + (evictionStrategy.isExceptionBased() ? 48 : 0));
          }
+         memoryConfigurationBuilder.maxSize(Long.toString(size));
+      }
+
+      memoryConfigurationBuilder.whenFull(evictionStrategy);
+      if (evictionStrategy == EvictionStrategy.EXCEPTION) {
+         cfg.transaction().transactionMode(TransactionMode.TRANSACTIONAL);
       }
 
       cfg
-         .statistics()
+            .statistics()
             .enable()
-         .memory()
-            .storageType(storageType)
-            .evictionType(evictionType)
-            .size(size)
-         .persistence()
+            .persistence()
             .passivation(true)
             .addStore(DummyInMemoryStoreConfigurationBuilder.class)
             .purgeOnStartup(true);
@@ -97,8 +114,13 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
       return this;
    }
 
-   public SingleStatsTest withEvictionType(EvictionType evictionType) {
-      this.evictionType = evictionType;
+   public SingleStatsTest withCountEviction(boolean countBasedEviction) {
+      this.countBasedEviction = countBasedEviction;
+      return this;
+   }
+
+   public SingleStatsTest withEvictionStrategy(EvictionStrategy evictionStrategy) {
+      this.evictionStrategy = evictionStrategy;
       return this;
    }
 
@@ -109,28 +131,38 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
    }
 
    public void testStats() {
+      int insertErrors = 0;
       for (int i = 0; i < TOTAL_ENTRIES; i++) {
-         cache.put("key" + i, "value" + i);
+         try {
+            cache.put("key" + i, "value" + i);
+         } catch (CacheException e) {
+            insertErrors++;
+         }
+      }
+      if (insertErrors > 0 && TOTAL_ENTRIES - EVICTION_MAX_ENTRIES != insertErrors) {
+         fail("Number of failed errors was: " + insertErrors + " manually check them.");
       }
 
+      int expectedSize = TOTAL_ENTRIES - insertErrors;
+
       refreshStats();
-      assertEquals(TOTAL_ENTRIES, stats.getCurrentNumberOfEntries());
+      assertEquals(expectedSize, stats.getCurrentNumberOfEntries());
       assertEquals(EVICTION_MAX_ENTRIES, stats.getCurrentNumberOfEntriesInMemory());
 
       // Approximate size stats are the same as the accurate size stats with DummyInMemoryStore
       // Only expiration is ignored, and we do not have expired entries
-      assertEquals(TOTAL_ENTRIES, stats.getApproximateEntries());
+      assertEquals(expectedSize, stats.getApproximateEntries());
       assertEquals(EVICTION_MAX_ENTRIES, stats.getApproximateEntriesInMemory());
       assertEquals(primaryKeysCount(cache), stats.getApproximateEntriesUnique());
 
       // Eviction stats with passivation can be delayed
-      eventuallyEquals((long) TOTAL_ENTRIES - EVICTION_MAX_ENTRIES, () -> {
+      eventuallyEquals((long) expectedSize - EVICTION_MAX_ENTRIES, () -> {
          refreshStats();
          return stats.getEvictions();
       });
 
       int additionalMisses = 0;
-      for (int i = 0; i < TOTAL_ENTRIES; i++) {
+      for (int i = 0; i < expectedSize; i++) {
          Cache<?, ?> skipLoaderCache = cache.getAdvancedCache().withFlags(Flag.SKIP_CACHE_LOAD);
          String key = "key" + i;
          if (skipLoaderCache.containsKey(key)) {
@@ -141,7 +173,7 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
       }
 
       refreshStats();
-      assertEquals(TOTAL_ENTRIES - EVICTION_MAX_ENTRIES + 1, stats.getEvictions());
+      assertEquals(expectedSize - EVICTION_MAX_ENTRIES + 1, stats.getEvictions());
 
       assertEquals("value1", cache.get("key1"));
       assertEquals("value2", cache.get("key2"));
@@ -170,7 +202,7 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
       assertTrue(stats.getAverageReadTimeNanos() >= 0);
       assertTrue(stats.getAverageRemoveTimeNanos() >= 0);
       assertTrue(stats.getAverageWriteTimeNanos() >= 0);
-      if (evictionType == EvictionType.COUNT) {
+      if (countBasedEviction || evictionStrategy.isExceptionBased()) {
          assertEquals(-1L, stats.getDataMemoryUsed());
       } else {
          assertTrue(stats.getDataMemoryUsed() > 0);
@@ -181,7 +213,7 @@ public class SingleStatsTest extends MultipleCacheManagersTest {
    }
 
    protected long primaryKeysCount(Cache<?, ?> cache) {
-      return TOTAL_ENTRIES;
+      return evictionStrategy == EvictionStrategy.EXCEPTION ? EVICTION_MAX_ENTRIES : TOTAL_ENTRIES;
    }
 
    protected void refreshStats() {
