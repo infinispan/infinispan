5459461c54cc62e4f91a1cd28820b68b478d84b7
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2022-09-23 08:44:51 +0200

    ISPN-14164 Avoid to use BaseFilter to set prefixes
    We cannot use filters since we're going to merge the registries to fix the issue in the next commit :(

--- core/src/main/java/org/infinispan/metrics/impl/BaseMemoryAdditionalMetrics.java
+++ core/src/main/java/org/infinispan/metrics/impl/BaseMemoryAdditionalMetrics.java
@@ -1,5 +1,7 @@
 package org.infinispan.metrics.impl;
 
+import static org.infinispan.metrics.impl.BaseAdditionalMetrics.PREFIX;
+
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryMXBean;
 
@@ -14,42 +16,42 @@ public class BaseMemoryAdditionalMetrics implements MeterBinder {
    public void bindTo(MeterRegistry registry) {
       MemoryMXBean memoryBean = ManagementFactory.getMemoryMXBean();
 
-      Gauge.builder("memory.committedHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getCommitted())
+      Gauge.builder(PREFIX + "memory.committedHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getCommitted())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the amount of memory that is committed for the Java virtual machine to use.")
             .register(registry);
 
-      Gauge.builder("memory.maxHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getMax())
+      Gauge.builder(PREFIX + "memory.maxHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getMax())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the maximum amount of memory, in bytes, that can be used for memory management.")
             .register(registry);
 
-      Gauge.builder("memory.usedHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getUsed())
+      Gauge.builder(PREFIX + "memory.usedHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getUsed())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the amount of used memory.")
             .register(registry);
 
-      Gauge.builder("memory.initHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getInit())
+      Gauge.builder(PREFIX + "memory.initHeap", memoryBean, (mem) -> mem.getHeapMemoryUsage().getInit())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the initial amount of allocated heap memory in bytes.")
             .register(registry);
 
-      Gauge.builder("memory.committedNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getCommitted())
+      Gauge.builder(PREFIX + "memory.committedNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getCommitted())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the amount of memory that is committed for the Java virtual machine to use.")
             .register(registry);
 
-      Gauge.builder("memory.maxNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getMax())
+      Gauge.builder(PREFIX + "memory.maxNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getMax())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the maximum amount of memory in bytes that can be used for memory management.")
             .register(registry);
 
-      Gauge.builder("memory.usedNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getUsed())
+      Gauge.builder(PREFIX + "memory.usedNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getUsed())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the amount of used memory.")
             .register(registry);
 
-      Gauge.builder("memory.initNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getInit())
+      Gauge.builder(PREFIX + "memory.initNonHeap", memoryBean, (mem) -> mem.getNonHeapMemoryUsage().getInit())
             .baseUnit(BaseUnits.BYTES)
             .description("Displays the initial amount of allocated memory, in bytes, for off-heap storage.")
             .register(registry);
--- core/src/main/java/org/infinispan/metrics/impl/VendorAdditionalMetrics.java
+++ core/src/main/java/org/infinispan/metrics/impl/VendorAdditionalMetrics.java
@@ -16,12 +16,14 @@ import io.micrometer.core.lang.NonNullFields;
 @NonNullFields
 class VendorAdditionalMetrics implements MeterBinder {
 
+   static final String PREFIX = "vendor.";
+
    @Override
    public void bindTo(MeterRegistry registry) {
       List<BufferPoolMXBean> bufferPoolBeans = ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class);
       for (BufferPoolMXBean bufferPoolBean : bufferPoolBeans) {
          String name = bufferPoolBean.getName();
-         Gauge.builder("BufferPool.used.memory." + name, bufferPoolBean, BufferPoolMXBean::getMemoryUsed)
+         Gauge.builder(PREFIX + "BufferPool.used.memory." + name, bufferPoolBean, BufferPoolMXBean::getMemoryUsed)
                .baseUnit(BaseUnits.BYTES)
                .description("The memory used by the NIO pool:" + name)
                .register(registry);
@@ -30,12 +32,12 @@ class VendorAdditionalMetrics implements MeterBinder {
       List<MemoryPoolMXBean> memoryPoolBeans = ManagementFactory.getMemoryPoolMXBeans();
       for (MemoryPoolMXBean memoryPoolBean : memoryPoolBeans) {
          String name = memoryPoolBean.getName();
-         Gauge.builder("memoryPool." + name + ".usage", memoryPoolBean, (mem) -> mem.getUsage().getUsed())
+         Gauge.builder(PREFIX + "memoryPool." + name + ".usage", memoryPoolBean, (mem) -> mem.getUsage().getUsed())
                .baseUnit(BaseUnits.BYTES)
                .description("Current usage of the " + name + " memory pool")
                .register(registry);
 
-         Gauge.builder("memoryPool." + name + ".usage.max", memoryPoolBean, (mem) -> mem.getPeakUsage().getUsed())
+         Gauge.builder(PREFIX + "memoryPool." + name + ".usage.max", memoryPoolBean, (mem) -> mem.getPeakUsage().getUsed())
                .baseUnit(BaseUnits.BYTES)
                .description("Peak usage of the " + name + " memory pool")
                .register(registry);
