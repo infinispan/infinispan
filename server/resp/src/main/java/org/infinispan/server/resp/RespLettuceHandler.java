750972cb420b4f7956a5e82531fd6e159b436667
William Burns <mudokonman@gmail.com>
2022-10-19 12:06:36 -0700

    ISPN-14242 RESP server can send results in incorrect order if a non blocking client is used

--- server/resp/src/main/java/org/infinispan/server/resp/RespLettuceHandler.java
+++ server/resp/src/main/java/org/infinispan/server/resp/RespLettuceHandler.java
@@ -2,9 +2,12 @@ package org.infinispan.server.resp;
 
 import java.lang.invoke.MethodHandles;
 import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 import org.infinispan.commons.logging.LogFactory;
+import org.infinispan.commons.util.Util;
 import org.infinispan.server.resp.logging.Log;
+import org.infinispan.util.concurrent.CompletionStages;
 
 import io.lettuce.core.codec.ByteArrayCodec;
 import io.lettuce.core.output.PushOutput;
@@ -19,6 +22,7 @@ public class RespLettuceHandler extends ByteToMessageDecoder {
 
    private final RedisStateMachine stateMachine = new RedisStateMachine(ByteBufAllocator.DEFAULT);
    private RespRequestHandler requestHandler;
+   private boolean disabledRead = false;
 
    public RespLettuceHandler(RespRequestHandler initialHandler) {
       this.requestHandler = initialHandler;
@@ -26,11 +30,46 @@ public class RespLettuceHandler extends ByteToMessageDecoder {
 
    @Override
    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
+      // Don't read any of the ByteBuf if we disabled reads
+      if (disabledRead) {
+         return;
+      }
       PushOutput<byte[], byte[]> pushOutput = new PushOutput<>(ByteArrayCodec.INSTANCE);
       if (stateMachine.decode(in, pushOutput)) {
          String type = pushOutput.getType().toUpperCase();
          List content = pushOutput.getContent();
-         requestHandler = requestHandler.handleRequest(ctx, type, content.subList(1, content.size()));
+         List<byte[]> contentToUse = content.subList(1, content.size());
+         log.tracef("Received command: %s with arguments %s", type, Util.toStr(contentToUse));
+         CompletionStage<RespRequestHandler> stage = requestHandler.handleRequest(ctx, type, contentToUse);
+         if (CompletionStages.isCompletedSuccessfully(stage)) {
+            requestHandler = CompletionStages.join(stage);
+         } else {
+            log.tracef("Disabling auto read for channel %s until previous command is complete", ctx.channel());
+            // Disable reading any more from socket - until command is complete
+            ctx.channel().config().setAutoRead(false);
+            disabledRead = true;
+            stage.whenComplete((handler, t) -> {
+               assert ctx.channel().eventLoop().inEventLoop();
+               log.tracef("Re-enabling auto read for channel %s as previous command is complete", ctx.channel());
+               ctx.channel().config().setAutoRead(true);
+               disabledRead = false;
+               if (t != null) {
+                  exceptionCaught(ctx, t);
+               } else {
+                  // Instate the new handler if there was no exception
+                  requestHandler = handler;
+               }
+
+               // If there is any readable bytes left before we paused make sure to try to decode, just in case
+               // if a pending message was read before we disabled auto read
+               ByteBuf buf = internalBuffer();
+               if (buf.isReadable()) {
+                  log.tracef("Bytes available from previous read for channel %s, trying decode directly", ctx.channel());
+                  // callDecode will call us until the ByteBuf is no longer consumed
+                  callDecode(ctx, buf, List.of());
+               }
+            });
+         }
       }
    }
 
--- server/resp/src/main/java/org/infinispan/server/resp/SubscriberHandler.java
+++ server/resp/src/main/java/org/infinispan/server/resp/SubscriberHandler.java
@@ -2,7 +2,9 @@ package org.infinispan.server.resp;
 
 import java.lang.invoke.MethodHandles;
 import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -11,19 +13,21 @@ import java.util.concurrent.CompletionStage;
 
 import org.infinispan.commons.logging.LogFactory;
 import org.infinispan.commons.marshall.WrappedByteArray;
+import org.infinispan.commons.util.concurrent.CompletableFutures;
 import org.infinispan.notifications.Listener;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
 import org.infinispan.notifications.cachelistener.annotation.CacheEntryModified;
 import org.infinispan.notifications.cachelistener.event.CacheEntryEvent;
 import org.infinispan.server.resp.logging.Log;
-import org.infinispan.commons.util.concurrent.CompletableFutures;
+import org.infinispan.util.concurrent.AggregateCompletionStage;
+import org.infinispan.util.concurrent.CompletionStages;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.CharsetUtil;
 
-public class SubscriberHandler implements RespRequestHandler {
+public class SubscriberHandler extends RespRequestHandler {
    private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass(), Log.class);
    // Random bytes to keep listener keys separate from others
    public static final byte[] PREFIX_CHANNEL_BYTES = new byte[]{-114, 16, 78, -3, 127};
@@ -77,12 +81,14 @@ public class SubscriberHandler implements RespRequestHandler {
 
    Map<WrappedByteArray, PubSubListener> specificChannelSubscribers = new HashMap<>();
 
+
    @Override
-   public RespRequestHandler handleRequest(ChannelHandlerContext ctx, String type,
-                                       List<byte[]> arguments) {
+   public CompletionStage<RespRequestHandler> handleRequest(ChannelHandlerContext ctx, String type,
+         List<byte[]> arguments) {
 
       switch (type) {
          case "SUBSCRIBE":
+            AggregateCompletionStage<Void> aggregateCompletionStage = CompletionStages.aggregateCompletionStage();
             for (byte[] keyChannel : arguments) {
                if (log.isTraceEnabled()) {
                   log.tracef("Subscriber for channel: " + CharsetUtil.UTF_8.decode(ByteBuffer.wrap(keyChannel)));
@@ -92,83 +98,91 @@ public class SubscriberHandler implements RespRequestHandler {
                   PubSubListener pubSubListener = new PubSubListener(ctx.channel());
                   specificChannelSubscribers.put(wrappedByteArray, pubSubListener);
                   byte[] channel = keyToChannel(keyChannel);
-                  respServer.getCache().addListenerAsync(pubSubListener, (key, prevValue, prevMetadata, value, metadata, eventType) ->
-                              Arrays.equals(key, channel), null)
-                        .whenComplete((ignore, t) -> {
-                           if (t != null) {
-                              log.exceptionWhileRegisteringListener(t, CharsetUtil.UTF_8.decode(ByteBuffer.wrap(channel)));
-                              ctx.writeAndFlush("-ERR Failure adding client listener");
-                           } else {
-                              ByteBuf subscribeBuffer = ctx.alloc().buffer(20 + (int) Math.log10(keyChannel.length) + 1 + keyChannel.length + 2);
-                              subscribeBuffer.writeCharSequence("*2\r\n$9\r\nsubscribe\r\n$" + keyChannel.length + "\r\n", CharsetUtil.UTF_8);
-                              subscribeBuffer.writeBytes(keyChannel);
-                              subscribeBuffer.writeCharSequence("\r\n", CharsetUtil.UTF_8);
-                              ctx.writeAndFlush(subscribeBuffer);
-                           }
-                        });
+                  CompletionStage<Void> stage = respServer.getCache().addListenerAsync(pubSubListener,
+                        (key, prevValue, prevMetadata, value, metadata, eventType) -> Arrays.equals(key, channel), null);
+                  aggregateCompletionStage.dependsOn(handleStageListenerError(stage, keyChannel, true));
                }
             }
-            break;
+            return sendSubscriptions(ctx, aggregateCompletionStage.freeze(), arguments, true);
          case "UNSUBSCRIBE":
+            aggregateCompletionStage = CompletionStages.aggregateCompletionStage();
             if (arguments.size() == 0) {
-               for (Iterator<Map.Entry<WrappedByteArray, PubSubListener>> iterator = specificChannelSubscribers.entrySet().iterator(); iterator.hasNext(); ) {
-                  Map.Entry<WrappedByteArray, PubSubListener> entry = iterator.next();
-                  PubSubListener listener = entry.getValue();
-                  respServer.getCache().removeListenerAsync(listener);
-                  iterator.remove();
-                  sendUnsubscribe(ctx, entry.getKey().getBytes());
-               }
+               return unsubscribeAll(ctx);
             } else {
                for (byte[] keyChannel : arguments) {
                   WrappedByteArray wrappedByteArray = new WrappedByteArray(keyChannel);
                   PubSubListener listener = specificChannelSubscribers.remove(wrappedByteArray);
                   if (listener != null) {
-                     respServer.getCache().removeListenerAsync(listener)
-                           .whenComplete((ignore, t) -> {
-                              if (t != null) {
-                                 log.exceptionWhileRemovingListener(t, CharsetUtil.UTF_8.decode(ByteBuffer.wrap(keyChannel)));
-                                 ctx.writeAndFlush("-ERR Failure unsubscribing client listener");
-                              } else {
-                                 sendUnsubscribe(ctx, keyChannel);
-                              }
-                           });
-                  } else {
-                     sendUnsubscribe(ctx, keyChannel);
+                     aggregateCompletionStage.dependsOn(handleStageListenerError(respServer.getCache().removeListenerAsync(listener), keyChannel, false));
                   }
                }
             }
-            break;
+            return sendSubscriptions(ctx, aggregateCompletionStage.freeze(), arguments, false);
          case "PING":
-            // Note we don't return the handler and just use it to handle the ping
+            // Note we don't return the handler and just use it to handle the ping - we assume stage is always complete
             handler.handleRequest(ctx, type, arguments);
             break;
          case "RESET":
-            for (Iterator<Map.Entry<WrappedByteArray, PubSubListener>> iterator = specificChannelSubscribers.entrySet().iterator(); iterator.hasNext(); ) {
-               Map.Entry<WrappedByteArray, PubSubListener> entry = iterator.next();
-               PubSubListener listener = entry.getValue();
-               respServer.getCache().removeListenerAsync(listener);
-               iterator.remove();
-               sendUnsubscribe(ctx, entry.getKey().getBytes());
-            }
-            return handler.handleRequest(ctx, type, arguments);
-         case "QUIT":
-            ctx.close();
-            break;
+            return unsubscribeAll(ctx)
+                  .thenCompose(returnedHandler -> returnedHandler.handleRequest(ctx, type, arguments));
          case "PSUBSCRIBE":
          case "PUNSUBSCRIBE":
             ctx.writeAndFlush(RespRequestHandler.stringToByteBuf("-ERR not implemented yet\r\n", ctx.alloc()));
             break;
          default:
-            return RespRequestHandler.super.handleRequest(ctx, type, arguments);
+            return super.handleRequest(ctx, type, arguments);
       }
-      return this;
+      return myStage;
    }
 
-   private void sendUnsubscribe(ChannelHandlerContext ctx, byte[] keyChannel) {
-      ByteBuf subscribeBuffer = ctx.alloc().buffer(22 + (int) Math.log10(keyChannel.length) + 1 + keyChannel.length + 2);
-      subscribeBuffer.writeCharSequence("*2\r\n$11\r\nunsubscribe\r\n$" + keyChannel.length + "\r\n", CharsetUtil.UTF_8);
-      subscribeBuffer.writeBytes(keyChannel);
-      subscribeBuffer.writeCharSequence("\r\n", CharsetUtil.UTF_8);
-      ctx.writeAndFlush(subscribeBuffer);
+   private CompletionStage<Void> handleStageListenerError(CompletionStage<Void> stage, byte[] keyChannel, boolean subscribeOrUnsubscribe) {
+      return stage.whenComplete((__, t) -> {
+         if (t != null) {
+            if (subscribeOrUnsubscribe) {
+               log.exceptionWhileRegisteringListener(t, CharsetUtil.UTF_8.decode(ByteBuffer.wrap(keyChannel)));
+            } else {
+               log.exceptionWhileRemovingListener(t, CharsetUtil.UTF_8.decode(ByteBuffer.wrap(keyChannel)));
+            }
+         }
+      });
+   }
+
+   private CompletionStage<RespRequestHandler> unsubscribeAll(ChannelHandlerContext ctx) {
+      var aggregateCompletionStage = CompletionStages.aggregateCompletionStage();
+      List<byte[]> channels = new ArrayList<>(specificChannelSubscribers.size());
+      for (Iterator<Map.Entry<WrappedByteArray, PubSubListener>> iterator = specificChannelSubscribers.entrySet().iterator(); iterator.hasNext(); ) {
+         Map.Entry<WrappedByteArray, PubSubListener> entry = iterator.next();
+         PubSubListener listener = entry.getValue();
+         CompletionStage<Void> stage = respServer.getCache().removeListenerAsync(listener);
+         byte[] keyChannel = entry.getKey().getBytes();
+         channels.add(keyChannel);
+         aggregateCompletionStage.dependsOn(handleStageListenerError(stage, keyChannel, false));
+         iterator.remove();
+      }
+      return sendSubscriptions(ctx, aggregateCompletionStage.freeze(), channels, false);
+   }
+
+   private CompletionStage<RespRequestHandler> sendSubscriptions(ChannelHandlerContext ctx, CompletionStage<Void> stageToWaitFor,
+         Collection<byte[]> keyChannels, boolean subscribeOrUnsubscribe) {
+      return stageToReturn(stageToWaitFor, ctx, (__, t) -> {
+         if (t != null) {
+            if (subscribeOrUnsubscribe) {
+               ctx.writeAndFlush("-ERR Failure adding client listener");
+            } else {
+               ctx.writeAndFlush("-ERR Failure unsubscribing client listener");
+            }
+            return;
+         }
+         for (byte[] keyChannel : keyChannels) {
+            int bufferCap = subscribeOrUnsubscribe ? 20 : 22;
+            String initialCharSeq = subscribeOrUnsubscribe ? "*2\r\n$9\r\nsubscribe\r\n$" : "*2\r\n$11\r\nunsubscribe\r\n$";
+
+            ByteBuf subscribeBuffer = ctx.alloc().buffer(bufferCap + (int) Math.log10(keyChannel.length) + 1 + keyChannel.length + 2);
+            subscribeBuffer.writeCharSequence(initialCharSeq + keyChannel.length + "\r\n", CharsetUtil.UTF_8);
+            subscribeBuffer.writeBytes(keyChannel);
+            subscribeBuffer.writeCharSequence("\r\n", CharsetUtil.UTF_8);
+            ctx.writeAndFlush(subscribeBuffer);
+         }
+      });
    }
 }
