230480b44e33f161aec0979d4b9d8a0c3cbd23fe
Dominika Vagnerova <dvagnero@redhat.com>
2022-11-23 15:21:13 +0100

    ISPN-14333 fix code snippets in the embedded doc
    ISPN-14333 add generic user roles

--- documentation/src/main/asciidoc/topics/code_examples/MutableRolePermissionMapper.java
+++ documentation/src/main/asciidoc/topics/code_examples/MutableRolePermissionMapper.java
@@ -1,3 +1,3 @@
-MutablePermissionMapper mapper = (MutablePermissionMapper) cacheManager.getCacheManagerConfiguration().security().authorization().rolePermissionMapper();
+MutableRolePermissionMapper mapper = (MutableRolePermissionMapper) cacheManager.getCacheManagerConfiguration().security().authorization().rolePermissionMapper();
 mapper.addRole(Role.newRole("myroleone", true, AuthorizationPermission.ALL_WRITE, AuthorizationPermission.LISTEN));
 mapper.addRole(Role.newRole("myroletwo", true, AuthorizationPermission.READ, AuthorizationPermission.WRITE));
