8625271278325b48082189429382f47a7dd5fa75
Tristan Tarrant <ttarrant@redhat.com>
2022-12-15 17:59:19 +0100

    ISPN-14421 Netty 4.1.86.Final
    OKHTTP updated to 3.14.9
    
    Netty introduced a message buffer in
    ApplicationProtocolNegotiationHandler and this buffer cannot be used by
    multiple pipelines concurrently.
    
    Changed our ALPNHandler class and removed the Shareable annotation.

--- server/rest/src/main/java/org/infinispan/rest/RestChannelInitializer.java
+++ server/rest/src/main/java/org/infinispan/rest/RestChannelInitializer.java
@@ -1,9 +1,13 @@
 package org.infinispan.rest;
 
+import java.util.Collections;
+
+import org.infinispan.rest.configuration.RestServerConfiguration;
 import org.infinispan.server.core.transport.NettyChannelInitializer;
 import org.infinispan.server.core.transport.NettyTransport;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
 import io.netty.handler.ssl.ApplicationProtocolConfig;
 import io.netty.handler.ssl.ApplicationProtocolNames;
 
@@ -11,57 +15,56 @@ import io.netty.handler.ssl.ApplicationProtocolNames;
  * Creates Netty Channels for this server.
  *
  * <p>
- *    With ALPN support, this class acts only as a bridge between Server Core and ALPN Handler which bootstraps
- *    pipeline handlers
+ * With ALPN support, this class acts only as a bridge between Server Core and ALPN Handler which bootstraps
+ * pipeline handlers
  * </p>
  *
  * @author Sebastian Łaskawiec
  */
-public class RestChannelInitializer extends NettyChannelInitializer {
+public class RestChannelInitializer extends NettyChannelInitializer<RestServerConfiguration> {
 
    static final int MAX_INITIAL_LINE_SIZE = 4096;
    static final int MAX_HEADER_SIZE = 8192;
-
-   private final ALPNHandler alpnHandler;
-
    private final RestServer restServer;
 
    /**
     * Creates new {@link RestChannelInitializer}.
     *
     * @param restServer Rest Server this initializer belongs to.
-    * @param transport Netty transport.
+    * @param transport  Netty transport.
     */
    public RestChannelInitializer(RestServer restServer, NettyTransport transport) {
       super(restServer, transport, null, null);
       this.restServer = restServer;
-      alpnHandler = new ALPNHandler(restServer);
    }
 
    @Override
    public void initializeChannel(Channel ch) throws Exception {
       super.initializeChannel(ch);
       if (server.getConfiguration().ssl().enabled()) {
-         configureSSL(ch);
+         ch.pipeline().addLast(new ALPNHandler(restServer));
       } else {
-         alpnHandler.configurePipeline(ch.pipeline(), ApplicationProtocolNames.HTTP_1_1);
+         ALPNHandler.configurePipeline(ch.pipeline(), ApplicationProtocolNames.HTTP_1_1, restServer, Collections.emptyMap());
       }
-   }
-
-   private void configureSSL(Channel ch) {
-      ch.pipeline().addLast(alpnHandler);
-   }
-
-   protected int maxContentLength() {
-      return this.restServer.getConfiguration().maxContentLength() + MAX_INITIAL_LINE_SIZE + MAX_HEADER_SIZE;
    }
 
    @Override
    protected ApplicationProtocolConfig getAlpnConfiguration() {
-      return alpnHandler.getAlpnConfiguration();
+      if (restServer.getConfiguration().ssl().enabled()) {
+         return new ApplicationProtocolConfig(
+               ApplicationProtocolConfig.Protocol.ALPN,
+               // NO_ADVERTISE is currently the only mode supported by both OpenSsl and JDK providers.
+               ApplicationProtocolConfig.SelectorFailureBehavior.NO_ADVERTISE,
+               // ACCEPT is currently the only mode supported by both OpenSsl and JDK providers.
+               ApplicationProtocolConfig.SelectedListenerFailureBehavior.ACCEPT,
+               ApplicationProtocolNames.HTTP_2,
+               ApplicationProtocolNames.HTTP_1_1);
+      }
+      return null;
    }
 
-   public ALPNHandler getAlpnHandler() {
-      return alpnHandler;
+   public ChannelHandler getRestHandler() {
+      return new RestRequestHandler(restServer);
    }
+
 }
