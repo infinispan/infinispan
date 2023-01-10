d8772cf324f4831b8bc49926ded2ad2d67a0c43c
William Burns <mudokonman@gmail.com>
2023-01-10 10:29:50 -0800

    ISPN-14361 Create Hibernate 2LC implementation for Hibernate 6.2.x
    * Drop 6.0 support
    * Update jakarta dependency

--- hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/CacheKeySerializationTest.java
+++ hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/CacheKeySerializationTest.java
@@ -13,13 +13,16 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
+import org.hibernate.Session;
 import org.hibernate.cache.internal.DefaultCacheKeysFactory;
 import org.hibernate.cache.internal.SimpleCacheKeysFactory;
 import org.hibernate.cache.spi.CacheKeysFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.cache.CachingRegionFactory;
@@ -95,18 +98,30 @@ public class CacheKeySerializationTest extends BaseUnitTestCase {
 		final Object keyClone = ois.readObject();
 
 		try {
-			assertEquals( key, keyClone );
-			assertEquals( keyClone, key );
+			assertEquals(key, keyClone);
+			assertEquals(keyClone, key);
 
-			assertEquals( key.hashCode(), keyClone.hashCode() );
+			assertEquals(key.hashCode(), keyClone.hashCode());
 
-			final Object idClone = cacheKeysFactory.getEntityId( keyClone );
+			final Object idClone;
+			if (cacheKeysFactory == SimpleCacheKeysFactory.INSTANCE) {
+				idClone = cacheKeysFactory.getEntityId(keyClone);
+			} else {
+				// DefaultCacheKeysFactory#getEntityId will return a disassembled version
+				try (Session session = sessionFactory.openSession()) {
+					idClone = persister.getIdentifierType().assemble(
+							(Serializable) cacheKeysFactory.getEntityId(keyClone),
+							(SharedSessionContractImplementor) session,
+							null
+					);
+				}
+			}
 
-			assertEquals( id.hashCode(), idClone.hashCode() );
-			assertEquals( id, idClone );
-			assertEquals( idClone, id );
-			assertTrue( persister.getIdentifierType().isEqual( id, idClone, sessionFactory ) );
-			assertTrue( persister.getIdentifierType().isEqual( idClone, id, sessionFactory ) );
+			assertEquals(id.hashCode(), idClone.hashCode());
+			assertEquals(id, idClone);
+			assertEquals(idClone, id);
+			assertTrue(persister.getIdentifierType().isEqual(id, idClone, sessionFactory));
+			assertTrue(persister.getIdentifierType().isEqual(idClone, id, sessionFactory));
 			sessionFactory.close();
 		}
 		finally {
