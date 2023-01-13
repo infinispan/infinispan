8eda8d3fe0b34f4a69a6de88b4d4a0b1d598a393
William Burns <mudokonman@gmail.com>
2023-01-13 09:23:09 -0800

    ISPN-14361 Create Hibernate 2LC implementation for Hibernate 6.2.x
    * Adding back in Hibernate 6.0.2 support

--- hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/CacheKeySerializationTest.java
+++ hibernate/cache-commons/src/test/java/org/infinispan/test/hibernate/cache/commons/CacheKeySerializationTest.java
@@ -13,16 +13,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
-import org.hibernate.Session;
 import org.hibernate.cache.internal.DefaultCacheKeysFactory;
 import org.hibernate.cache.internal.SimpleCacheKeysFactory;
 import org.hibernate.cache.spi.CacheKeysFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.cfg.Environment;
 import org.hibernate.engine.spi.SessionFactoryImplementor;
-import org.hibernate.engine.spi.SharedSessionContractImplementor;
 import org.hibernate.persister.entity.EntityPersister;
 import org.hibernate.testing.TestForIssue;
 import org.hibernate.testing.cache.CachingRegionFactory;
@@ -103,19 +100,7 @@ public class CacheKeySerializationTest extends BaseUnitTestCase {
 
 			assertEquals(key.hashCode(), keyClone.hashCode());
 
-			final Object idClone;
-			if (cacheKeysFactory == SimpleCacheKeysFactory.INSTANCE) {
-				idClone = cacheKeysFactory.getEntityId(keyClone);
-			} else {
-				// DefaultCacheKeysFactory#getEntityId will return a disassembled version
-				try (Session session = sessionFactory.openSession()) {
-					idClone = persister.getIdentifierType().assemble(
-							(Serializable) cacheKeysFactory.getEntityId(keyClone),
-							(SharedSessionContractImplementor) session,
-							null
-					);
-				}
-			}
+			final Object idClone = cacheKeysFactory.getEntityId(keyClone);
 
 			assertEquals(id.hashCode(), idClone.hashCode());
 			assertEquals(id, idClone);
