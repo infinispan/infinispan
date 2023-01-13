088ae3683e754f5d061405afb963a9d8b473d917
William Burns <mudokonman@gmail.com>
2023-01-13 09:23:09 -0800

    ISPN-14361 Create Hibernate 2LC implementation for Hibernate 6.2.x
    * Adding back in Hibernate 6.0.2 support

---
+++ hibernate/cache-v60/src/main/java/org/infinispan/hibernate/cache/v60/impl/LocalInvalidationInvocation.java
@@ -0,0 +1,41 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.infinispan.hibernate.cache.v60.impl;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.infinispan.hibernate.cache.commons.access.PutFromLoadValidator;
+import org.infinispan.hibernate.cache.commons.util.InfinispanMessageLogger;
+
+/**
+ * Synchronization that should release the locks after invalidation is complete.
+ *
+ * @author Radim Vansa &lt;rvansa@redhat.com&gt;
+ */
+public class LocalInvalidationInvocation implements Invocation {
+   private final static InfinispanMessageLogger log = InfinispanMessageLogger.Provider.getLog(LocalInvalidationInvocation.class);
+
+   private final Object lockOwner;
+   private final PutFromLoadValidator validator;
+   private final Object key;
+
+   public LocalInvalidationInvocation(PutFromLoadValidator validator, Object key, Object lockOwner) {
+      assert lockOwner != null;
+      this.validator = validator;
+      this.key = key;
+      this.lockOwner = lockOwner;
+   }
+
+   @Override
+   public CompletableFuture<Void> invoke(boolean success) {
+      if (log.isTraceEnabled()) {
+         log.tracef("After completion callback, success=%b", success);
+      }
+      validator.endInvalidatingKey(lockOwner, key, success);
+      return null;
+   }
+}
---
+++ hibernate/cache-v60/src/main/java/org/infinispan/hibernate/cache/v60/impl/NonTxInvalidationCacheAccessDelegate.java
@@ -0,0 +1,58 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.infinispan.hibernate.cache.v60.impl;
+
+import java.util.concurrent.CompletableFuture;
+
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.infinispan.commands.write.RemoveCommand;
+import org.infinispan.context.InvocationContext;
+import org.infinispan.context.impl.FlagBitSets;
+import org.infinispan.hibernate.cache.commons.InfinispanDataRegion;
+import org.infinispan.hibernate.cache.commons.access.PutFromLoadValidator;
+
+/**
+ * Delegate for non-transactional invalidation caches
+ *
+ * @author Radim Vansa &lt;rvansa@redhat.com&gt;
+ */
+class NonTxInvalidationCacheAccessDelegate extends org.infinispan.hibernate.cache.commons.access.NonTxInvalidationCacheAccessDelegate {
+   public NonTxInvalidationCacheAccessDelegate(InfinispanDataRegion region, PutFromLoadValidator validator) {
+      super(region, validator);
+   }
+
+   @Override
+   protected void registerLocalInvalidation(Object session, Object lockOwner, Object key) {
+      Sync sync = (Sync) ((SharedSessionContractImplementor) session).getCacheTransactionSynchronization();
+      if (log.isTraceEnabled()) {
+         log.tracef("Registering synchronization on transaction in %s, cache %s: %s", lockOwner, cache.getName(), key);
+      }
+      sync.registerAfterCommit(new LocalInvalidationInvocation(putValidator, key, lockOwner));
+   }
+
+   @Override
+   protected void registerClusteredInvalidation(Object session, Object lockOwner, Object key) {
+      Sync sync = (Sync) ((SharedSessionContractImplementor) session).getCacheTransactionSynchronization();
+      if (log.isTraceEnabled()) {
+         log.tracef("Registering synchronization on transaction in %s, cache %s: %s", lockOwner, cache.getName(), key);
+      }
+      sync.registerAfterCommit(new InvalidationInvocation(nonTxPutFromLoadInterceptor, key, lockOwner));
+   }
+
+   @Override
+   protected void invoke(Object session, InvocationContext ctx, RemoveCommand command) {
+      CompletableFuture<Object> future = invoker.invokeAsync(ctx, command);
+      // LockingInterceptor removes this flag when the command is applied and we only wait for RPC
+      if (!command.hasAnyFlag(FlagBitSets.FORCE_WRITE_LOCK)) {
+         Sync sync = (Sync) ((SharedSessionContractImplementor) session).getCacheTransactionSynchronization();
+         sync.registerBeforeCommit(future);
+      } else {
+         log.trace("Removal was not applied immediately, waiting.");
+         future.join();
+      }
+   }
+}
