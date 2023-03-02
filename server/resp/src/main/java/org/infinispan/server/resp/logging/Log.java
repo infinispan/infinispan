5b9d1f1fb0a66e91582f5fe07c20cb47b5f669ce
William Burns <mudokonman@gmail.com>
2023-03-02 09:06:08 -0800

    ISPN-14544 RESP endpoint cache shouldn't require no expiration configured

--- server/resp/src/main/java/org/infinispan/server/resp/logging/Log.java
+++ server/resp/src/main/java/org/infinispan/server/resp/logging/Log.java
@@ -20,8 +20,8 @@ import org.jboss.logging.annotations.MessageLogger;
  */
 @MessageLogger(projectCode = "ISPN")
 public interface Log extends BasicLogger {
-   @Message(value = "Cache '%s' has expiration enabled which violates the RESP protocol.", id = 12001)
-   CacheConfigurationException invalidExpiration(String cacheName);
+//   @Message(value = "Cache '%s' has expiration enabled which violates the RESP protocol.", id = 12001)
+//   CacheConfigurationException invalidExpiration(String cacheName);
 
    @Message(value = "Cannot enable authentication without an authenticator.", id = 12002)
    CacheConfigurationException authenticationWithoutAuthenticator();
