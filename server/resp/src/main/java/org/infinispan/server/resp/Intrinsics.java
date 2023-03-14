dfe375eb4f5d06b67c8264dbcd5d0bfd6e7a766a
William Burns <mudokonman@gmail.com>
2023-03-14 10:30:56 -0700

    ISPN-14585 Convert RESP endpoint to use parser generator

---
+++ server/resp/src/main/java/org/infinispan/server/resp/Intrinsics.java
@@ -0,0 +1,155 @@
+package org.infinispan.server.resp;
+
+import java.nio.charset.StandardCharsets;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.util.ByteProcessor;
+
+public class Intrinsics {
+   private static final int TERMINATOR_LENGTH = 2;
+
+   public static byte singleByte(ByteBuf buffer) {
+      if (buffer.isReadable()) {
+         return buffer.readByte();
+      } else return 0;
+   }
+
+   public static String simpleString(ByteBuf buf) {
+      // Find the end LF (would be nice to do this without iterating twice)
+      int offset = buf.forEachByte(ByteProcessor.FIND_LF);
+      if (offset <= 0) {
+         return null;
+      }
+      if (buf.getByte(offset - 1) != '\r') {
+         throw new IllegalStateException("No matching \r character found before \n");
+      }
+      String simpleString = buf.toString(buf.readerIndex(), offset - 1 - buf.readerIndex(), StandardCharsets.US_ASCII);
+      // toString doesn't move the reader index forward
+      buf.readerIndex(offset + 1);
+      return simpleString;
+   }
+
+   public static long readNumber(ByteBuf buf, Resp2LongProcessor longProcessor) {
+      long value = longProcessor.getValue(buf);
+      // The longProcessor can technically read up to only /r, we need to ensure there is /r and /n
+      if (longProcessor.complete && buf.readableBytes() >= longProcessor.bytesRead + TERMINATOR_LENGTH) {
+         buf.skipBytes(longProcessor.bytesRead + TERMINATOR_LENGTH);
+      }
+      return value;
+   }
+
+   public static byte[] readTerminatedBytes(ByteBuf buf) {
+      // Find the end LF (would be nice to do this without iterating twice)
+      int offset = buf.forEachByte(ByteProcessor.FIND_LF);
+      if (offset <= 0) {
+         return null;
+      }
+      if (buf.getByte(offset - 1) != '\r') {
+         throw new IllegalStateException("No matching \r character found before \n");
+      }
+      byte[] data = new byte[offset - 1 - buf.readerIndex()];
+      buf.readBytes(data);
+      buf.skipBytes(TERMINATOR_LENGTH);
+      return data;
+   }
+
+   public static String bulkString(ByteBuf buf, Resp2LongProcessor longProcessor) {
+      buf.markReaderIndex();
+      int pos = buf.readerIndex();
+      long longSize = readNumber(buf, longProcessor);
+      if (longSize > Integer.MAX_VALUE) {
+         throw new IllegalArgumentException("Bytes cannot be longer than " + Integer.MAX_VALUE);
+      }
+      // If we didn't read any bytes then the number wasn't parsed properly
+      if (pos == buf.readerIndex()) {
+         return null;
+      }
+      int size = (int) longSize;
+      if (buf.readableBytes() < size + TERMINATOR_LENGTH) {
+         buf.resetReaderIndex();
+         return null;
+      }
+      String stringValue = buf.toString(buf.readerIndex(), size, StandardCharsets.US_ASCII);
+      buf.skipBytes(size + TERMINATOR_LENGTH);
+      return stringValue;
+   }
+
+   public static byte[] bulkArray(ByteBuf buf, Resp2LongProcessor longProcessor) {
+      buf.markReaderIndex();
+      int pos = buf.readerIndex();
+      long longSize = readNumber(buf, longProcessor);
+      if (longSize > Integer.MAX_VALUE) {
+         throw new IllegalArgumentException("Bytes cannot be longer than " + Integer.MAX_VALUE);
+      }
+      // If we didn't read any bytes then the number wasn't parsed properly
+      if (pos == buf.readerIndex()) {
+         return null;
+      }
+      int size = (int) longSize;
+      if (buf.readableBytes() < size + TERMINATOR_LENGTH) {
+         buf.resetReaderIndex();
+         return null;
+      }
+      byte[] array = new byte[size];
+      buf.readBytes(array);
+      buf.skipBytes(TERMINATOR_LENGTH);
+      return array;
+   }
+
+   static class Resp2LongProcessor implements ByteProcessor {
+      long result;
+      int bytesRead;
+      boolean complete;
+      boolean negative;
+      boolean first;
+
+      public long getValue(ByteBuf buffer) {
+
+         this.result = 0;
+         this.bytesRead = 0;
+         this.complete = false;
+         this.first = true;
+
+         // We didn't have enough to read the number
+         if (buffer.forEachByte(this) == -1) {
+            complete = false;
+            return -1;
+         }
+         complete = true;
+
+         if (!this.negative) {
+            this.result = -this.result;
+         }
+
+         return this.result;
+      }
+
+      @Override
+      public boolean process(byte value) {
+
+         if (value == '\r') {
+            return false;
+         }
+
+         bytesRead++;
+
+         if (first) {
+            first = false;
+
+            if (value == '-') {
+               negative = true;
+            } else {
+               negative = false;
+               int digit = value - '0';
+               result = result * 10 - digit;
+            }
+            return true;
+         }
+
+         int digit = value - '0';
+         result = result * 10 - digit;
+
+         return true;
+      }
+   }
+}
---
+++ server/resp/src/test/java/org/infinispan/server/resp/RespDecoderTest.java
@@ -0,0 +1,89 @@
+package org.infinispan.server.resp;
+
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertNotNull;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.CompletionStage;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.embedded.EmbeddedChannel;
+import io.netty.handler.codec.FixedLengthFrameDecoder;
+
+/**
+ * Test to ensure decoder works properly for various requests, types and replay
+ *
+ * @author William Burns
+ * @since 15.0
+ */
+@Test(groups = "functional", testName = "server.resp.RespDecoderTest")
+public class RespDecoderTest {
+
+   EmbeddedChannel channel;
+   Queue<Request> queuedCommands;
+
+   static class Request {
+      private final String command;
+      private final List<byte[]> arguments;
+
+      Request(String command, List<byte[]> arguments) {
+         this.command = command;
+         // Make a copy, because the decoder may reuse the list
+         this.arguments = new ArrayList<>(arguments);
+      }
+   }
+
+   @BeforeClass
+   public void beforeClass() {
+      queuedCommands = new ArrayDeque<>();
+      RespRequestHandler myRespRequestHandler = new RespRequestHandler() {
+         @Override
+         public CompletionStage<RespRequestHandler> handleRequest(ChannelHandlerContext ctx, String type, List<byte[]> arguments) {
+            queuedCommands.add(new Request(type, arguments));
+            return myStage;
+         }
+      };
+      channel = new EmbeddedChannel(new FixedLengthFrameDecoder(1), new RespDecoder(myRespRequestHandler));
+   }
+
+   @AfterClass
+   public void afterClass() {
+      channel.close();
+   }
+
+   @Test
+   public void testMixtureOfTypes() {
+      String commandName = "ALLTYPES";
+      String minValueStr = String.valueOf(Long.MIN_VALUE);
+      ByteBuf buffer = Unpooled.copiedBuffer("*6\r\n+" + commandName + "\r\n$3\r\nkey\r\n+value\r\n:23\r\n$5\r\nworks\r\n:" + minValueStr + "\r\n", StandardCharsets.US_ASCII);
+      channel.writeInbound(buffer);
+
+      channel.checkException();
+
+      Request req = queuedCommands.poll();
+      assertNotNull(req);
+      assertEquals(commandName, req.command);
+      List<byte[]> arguments = req.arguments;
+      assertEquals(5, arguments.size());
+
+      assertEquals("key".getBytes(StandardCharsets.US_ASCII), arguments.get(0));
+      assertEquals("value".getBytes(StandardCharsets.US_ASCII), arguments.get(1));
+      assertEquals("23".getBytes(StandardCharsets.US_ASCII), arguments.get(2));
+      assertEquals("works".getBytes(StandardCharsets.US_ASCII), arguments.get(3));
+      assertEquals(minValueStr.getBytes(StandardCharsets.US_ASCII), arguments.get(4));
+
+      assertEquals(0, queuedCommands.size());
+
+      assertEquals(0, buffer.readableBytes());
+   }
+}
