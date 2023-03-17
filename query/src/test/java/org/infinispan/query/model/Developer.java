3659429f3c143416089846f29003ab84e31a8239
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-03-17 09:47:21 +0100

    ISPN-14478 Fix Developer / index startup test classes
    Not related to this feature, but we will use the class to test ISPN-14478 as well

--- query/src/test/java/org/infinispan/query/model/Developer.java
+++ query/src/test/java/org/infinispan/query/model/Developer.java
@@ -6,22 +6,20 @@ import org.infinispan.api.annotations.indexing.Basic;
 import org.infinispan.api.annotations.indexing.Indexed;
 import org.infinispan.api.annotations.indexing.Keyword;
 import org.infinispan.api.annotations.indexing.Text;
+import org.infinispan.protostream.GeneratedSchema;
+import org.infinispan.protostream.annotations.AutoProtoSchemaBuilder;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
 
 @Indexed
 public class Developer implements Serializable {
 
-   @Keyword
    private String nick;
 
-   @Keyword
    private String email;
 
-   @Text
    private String biography;
 
-   @Basic
    private Integer contributions = 0;
 
    @ProtoFactory
@@ -32,6 +30,7 @@ public class Developer implements Serializable {
       this.contributions = contributions;
    }
 
+   @Keyword(projectable = true)
    @ProtoField(number = 1)
    public String getNick() {
       return nick;
@@ -41,6 +40,7 @@ public class Developer implements Serializable {
       this.nick = nick;
    }
 
+   @Keyword
    @ProtoField(number = 2)
    public String getEmail() {
       return email;
@@ -50,6 +50,7 @@ public class Developer implements Serializable {
       this.email = email;
    }
 
+   @Text
    @ProtoField(number = 3)
    public String getBiography() {
       return biography;
@@ -59,6 +60,7 @@ public class Developer implements Serializable {
       this.biography = biography;
    }
 
+   @Basic(projectable = true)
    @ProtoField(number = 4, defaultValue = "0")
    public Integer getContributions() {
       return contributions;
@@ -76,5 +78,14 @@ public class Developer implements Serializable {
             ", biography='" + biography + '\'' +
             ", contributions=" + contributions +
             '}';
+   }
+
+   @AutoProtoSchemaBuilder(
+         includeClasses = Developer.class,
+         schemaFileName = "developer.proto",
+         schemaPackageName = "io.dev"
+   )
+   public interface DeveloperSchema extends GeneratedSchema {
+      DeveloperSchema INSTANCE = new DeveloperSchemaImpl();
    }
 }
