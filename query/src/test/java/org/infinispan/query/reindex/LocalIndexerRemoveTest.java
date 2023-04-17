e1ae46be80612806252e2dc980c35894e25f385e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-04-17 14:29:59 +0200

    ISPN-14189 Test index statistics count/size on reindex/purge

---
+++ query/src/test/java/org/infinispan/query/reindex/LocalIndexerRemoveTest.java
@@ -0,0 +1,93 @@
+package org.infinispan.query.reindex;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.infinispan.util.concurrent.CompletionStages.join;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import org.infinispan.Cache;
+import org.infinispan.commons.test.annotation.TestForIssue;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.IndexStorage;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.query.Indexer;
+import org.infinispan.query.Search;
+import org.infinispan.query.core.stats.IndexInfo;
+import org.infinispan.query.core.stats.SearchStatistics;
+import org.infinispan.query.model.Color;
+import org.infinispan.test.SingleCacheManagerTest;
+import org.infinispan.test.fwk.TestCacheManagerFactory;
+import org.testng.annotations.Test;
+
+@Test(groups = "functional", testName = "query.reindex.LocalIndexerRemoveTest")
+@TestForIssue(jiraKey = "ISPN-14189")
+public class LocalIndexerRemoveTest extends SingleCacheManagerTest {
+
+   private static final String CACHE_NAME = "types";
+   private static final int ENTRIES = 5_000;
+
+   @Override
+   protected EmbeddedCacheManager createCacheManager() throws Exception {
+      ConfigurationBuilder config = new ConfigurationBuilder();
+      config
+            .indexing()
+            .enable()
+            .storage(IndexStorage.LOCAL_HEAP)
+            .addIndexedEntity(Color.class);
+
+      EmbeddedCacheManager result = TestCacheManagerFactory.createCacheManager();
+      result.defineConfiguration(CACHE_NAME, config.build());
+
+      return result;
+   }
+
+   @Test
+   public void test() throws Exception {
+      Cache<Integer, Color> typesCache = cacheManager.getCache(CACHE_NAME);
+      Indexer indexer = Search.getIndexer(typesCache);
+      SearchStatistics searchStatistics = Search.getSearchStatistics(typesCache);
+
+      Map<Integer, Color> values = IntStream.range(0, ENTRIES).boxed()
+            .collect(Collectors.toMap(Function.identity(), i -> new Color("color-name-" + 1, "description: " + i)));
+      typesCache.putAll(values);
+
+      // the indexing mode is manual, thus the index is empty
+      IndexInfo indexInfo = indexInfo(searchStatistics);
+      assertThat(indexInfo.count()).isEqualTo(ENTRIES);
+
+      // removing the index data
+      join(indexer.remove());
+
+      indexInfo = indexInfo(searchStatistics);
+      assertThat(indexInfo.count()).isZero();
+
+      indexInfo = indexInfo(searchStatistics);
+      assertThat(indexInfo.count()).isZero();
+      long firstCallSize = indexInfo.size();
+
+      indexInfo = indexInfo(searchStatistics);
+      assertThat(indexInfo.count()).isZero();
+      long secondCallSize = indexInfo.size();
+
+      // it seems that the first call to the state after the purge is always wrong and the second call is always correct,
+      // no matter how much time we wait after the purge
+      assertThat(firstCallSize - secondCallSize).isLessThanOrEqualTo(1000);
+
+      // the indexer fills the index
+      join(indexer.runLocal());
+
+      indexInfo = indexInfo(searchStatistics);
+      assertThat(indexInfo.count()).isEqualTo(ENTRIES);
+   }
+
+   private IndexInfo indexInfo(SearchStatistics searchStatistics) {
+      Map<String, IndexInfo> indexInfos = join(searchStatistics.getIndexStatistics().computeIndexInfos());
+      String key = Color.class.getName();
+      assertThat(indexInfos).containsKey(key);
+      IndexInfo indexInfo = indexInfos.get(key);
+      return indexInfo;
+   }
+}
---
+++ server/rest/src/test/java/org/infinispan/rest/search/reindex/LocalRestIndexerRemoveTest.java
@@ -0,0 +1,115 @@
+package org.infinispan.rest.search.reindex;
+
+import static org.infinispan.rest.assertion.ResponseAssertion.assertThat;
+import static org.infinispan.util.concurrent.CompletionStages.join;
+import static org.testng.AssertJUnit.assertEquals;
+import static org.testng.AssertJUnit.assertFalse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import org.infinispan.Cache;
+import org.infinispan.client.rest.RestCacheClient;
+import org.infinispan.client.rest.RestClient;
+import org.infinispan.client.rest.RestEntity;
+import org.infinispan.client.rest.RestResponse;
+import org.infinispan.client.rest.configuration.RestClientConfigurationBuilder;
+import org.infinispan.commons.dataconversion.MediaType;
+import org.infinispan.commons.dataconversion.internal.Json;
+import org.infinispan.commons.test.TestResourceTracker;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.cache.IndexStorage;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.query.model.Game;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
+import org.infinispan.rest.helper.RestServerHelper;
+import org.infinispan.test.SingleCacheManagerTest;
+import org.infinispan.test.fwk.TestCacheManagerFactory;
+import org.testng.annotations.Test;
+
+@Test(groups = "unit", testName = "rest.search.reindex.LocalRestIndexerRemoveTest")
+public class LocalRestIndexerRemoveTest extends SingleCacheManagerTest {
+
+   private static final String CACHE_NAME = "types";
+
+   private static final int ENTRIES = 5_000;
+
+   private RestServerHelper restServer;
+   private RestClient restClient;
+
+   @Override
+   protected EmbeddedCacheManager createCacheManager() throws Exception {
+      EmbeddedCacheManager cacheManager = TestCacheManagerFactory.createCacheManager();
+
+      // Register proto schema
+      Cache<String, String> metadataCache = cacheManager.getCache(ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME);
+      metadataCache.putIfAbsent(Game.GameSchema.INSTANCE.getProtoFileName(), Game.GameSchema.INSTANCE.getProtoFile());
+      assertFalse(metadataCache.containsKey(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX));
+
+      ConfigurationBuilder config = new ConfigurationBuilder();
+      config
+            .encoding()
+            .mediaType(MediaType.APPLICATION_PROTOSTREAM_TYPE)
+            .indexing()
+            .enable()
+            .storage(IndexStorage.LOCAL_HEAP)
+            .addIndexedEntity("Game");
+      cacheManager.createCache(CACHE_NAME, config.build());
+
+      restServer = new RestServerHelper(cacheManager);
+      restServer.start(TestResourceTracker.getCurrentTestShortName());
+      restClient = RestClient.forConfiguration(new RestClientConfigurationBuilder().addServer()
+            .host(restServer.getHost()).port(restServer.getPort())
+            .build());
+
+      return cacheManager;
+   }
+
+   @Test
+   public void test() {
+      RestCacheClient cacheClient = restClient.cache(CACHE_NAME);
+      List<CompletionStage<RestResponse>> responses = new ArrayList<>(ENTRIES);
+
+      for (int i = 0; i < ENTRIES; i++) {
+         Json game = Json.object()
+               .set("_type", "Game")
+               .set("name", "Game n." + i)
+               .set("description", "This is the game #" + i);
+
+         responses.add(cacheClient.put("game-" + i, RestEntity.create(MediaType.APPLICATION_JSON, game.toString())));
+      }
+      for (CompletionStage<RestResponse> response : responses) {
+         assertThat(response).isOk();
+      }
+
+      assertThat(cacheClient.reindex()).isOk();
+      assertEquals(ENTRIES, count(cacheClient));
+
+      assertThat(cacheClient.clearIndex()).isOk();
+      assertEquals(0, count(cacheClient));
+   }
+
+   @Override
+   protected void teardown() {
+      try {
+         restClient.close();
+      } catch (IOException ex) {
+         // ignore it
+      } finally {
+         try {
+            restServer.stop();
+         } finally {
+            super.teardown();
+         }
+      }
+   }
+
+   private int count(RestCacheClient cacheClient) {
+      RestResponse response = join(cacheClient.searchStats());
+      Json stat = Json.read(response.getBody());
+      Json indexGame = stat.at("index").at("types").at("Game");
+      return indexGame.at("count").asInteger();
+   }
+}
