1118c8cf1439488113e23b58311bc23cfac18641
jabolina <jbolina@redhat.com>
2023-04-18 14:49:10 -0300

    ISPN-14782 Unable to reenable rebalance after cluster scale up

--- core/src/test/java/org/infinispan/globalstate/ThreeNodeReplGlobalStateRestartTest.java
+++ core/src/test/java/org/infinispan/globalstate/ThreeNodeReplGlobalStateRestartTest.java
@@ -1,7 +1,16 @@
 package org.infinispan.globalstate;
 
+import static org.testng.AssertJUnit.assertTrue;
+
+import java.util.Map;
+
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.distribution.ch.ConsistentHash;
+import org.infinispan.manager.DefaultCacheManager;
+import org.infinispan.remoting.transport.jgroups.JGroupsAddress;
+import org.infinispan.topology.PersistentUUID;
+
 import org.testng.annotations.Test;
 
 @Test(testName = "globalstate.ThreeNodeReplGlobalStateRestartTest", groups = "functional")
@@ -31,6 +40,34 @@ public class ThreeNodeReplGlobalStateRestartTest extends AbstractGlobalStateRest
 
    public void testFailedRestartWithExtraneousNode() throws Throwable {
       shutdownAndRestart(1, false);
+   }
+
+   public void testDisableRebalanceRestartEnableRebalance() throws Throwable {
+      Map<JGroupsAddress, PersistentUUID> addressMappings = createInitialCluster();
+      ConsistentHash oldConsistentHash = advancedCache(0, CACHE_NAME).getDistributionManager().getWriteConsistentHash();
+
+      manager(0).getGlobalComponentRegistry().getLocalTopologyManager().setRebalancingEnabled(false);
+
+      for (int i = 0; i < getClusterSize(); i++) {
+         ((DefaultCacheManager) manager(i)).shutdownAllCaches();
+         manager(i).stop();
+      }
+
+      cacheManagers.clear();
+
+      createStatefulCacheManagers(false, -1, false);
+
+      for (int i = 0; i < getClusterSize() - 1; i++) {
+         cache(i, CACHE_NAME);
+      }
+
+      manager(0).getGlobalComponentRegistry().getLocalTopologyManager().setRebalancingEnabled(true);
+
+      // Last missing.
+      cache(getClusterSize() - 1, CACHE_NAME);
+
+      assertHealthyCluster(addressMappings, oldConsistentHash);
+      assertTrue(manager(0).getGlobalComponentRegistry().getLocalTopologyManager().isRebalancingEnabled());
    }
 
    public void testPersistentStateIsDeletedAfterRestart() throws Throwable {
