407db5146d06bf227febcded9b3a713ede8ecf5a
rigazilla <vrigamon@redhat.com>
2023-04-27 17:33:32 +0200

    Implement incrby decrby and tests. fixes #ISPN-14647 #ISPN-14652

---
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/string/INCRBY.java
@@ -0,0 +1,31 @@
+package org.infinispan.server.resp.commands.string;
+
+import io.netty.channel.ChannelHandlerContext;
+import org.infinispan.server.resp.commands.Resp3Command;
+import org.infinispan.server.resp.Consumers;
+import org.infinispan.server.resp.Resp3Handler;
+import org.infinispan.server.resp.RespCommand;
+import org.infinispan.server.resp.RespRequestHandler;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+/**
+ * @link https://redis.io/commands/incrby/
+ * @since 15.0
+ */
+public class INCRBY extends RespCommand implements Resp3Command {
+   public INCRBY() {
+      super(3, 1, 1, 1);
+   }
+
+   @Override
+   public CompletionStage<RespRequestHandler> perform(Resp3Handler handler,
+                                                      ChannelHandlerContext ctx,
+                                                      List<byte[]> arguments) {
+      return handler
+            .stageToReturn(CounterIncOrDec.counterIncOrDecBy(handler.cache(), arguments.get(0), new String(arguments.get(1), StandardCharsets.US_ASCII), true),
+                  ctx, Consumers.LONG_BICONSUMER);
+   }
+}
