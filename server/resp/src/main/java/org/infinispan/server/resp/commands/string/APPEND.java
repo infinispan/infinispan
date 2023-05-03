9ba392350e4deee851d03cd1ab7132f6bbc8dbac
rigazilla <vrigamon@redhat.com>
2023-05-03 11:02:57 +0200

    added check on args num

--- server/resp/src/main/java/org/infinispan/server/resp/commands/string/APPEND.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/string/APPEND.java
@@ -2,6 +2,7 @@ package org.infinispan.server.resp.commands.string;
 
 import io.netty.channel.ChannelHandlerContext;
 import org.infinispan.server.resp.commands.Resp3Command;
+import org.infinispan.server.resp.ByteBufferUtils;
 import org.infinispan.server.resp.Consumers;
 import org.infinispan.server.resp.Resp3Handler;
 import org.infinispan.server.resp.RespCommand;
@@ -21,8 +22,13 @@ public class APPEND extends RespCommand implements Resp3Command {
 
    @Override
    public CompletionStage<RespRequestHandler> perform(Resp3Handler handler,
-                                                      ChannelHandlerContext ctx,
-                                                      List<byte[]> arguments) {
+         ChannelHandlerContext ctx,
+         List<byte[]> arguments) {
+      if (arguments.size() != getArity() - 1) {
+         ByteBufferUtils.stringToByteBuf("-ERR wrong number of arguments for 'append' command\r\n",
+               handler.allocatorToUse());
+         return handler.myStage();
+      }
       return handler
             .stageToReturn(StringMutators.append(handler.cache(), arguments.get(0), arguments.get(1)),
                   ctx, Consumers.LONG_BICONSUMER);
