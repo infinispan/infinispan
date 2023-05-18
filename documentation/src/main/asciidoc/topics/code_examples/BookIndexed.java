60c369dbf88a2d3b2ff7919df13d3cc358666de8
dvagnero <dvagnero@redhat.com>
2023-05-18 09:52:51 +0200

    ISPN-14008 update code examples for query
    Co-authored-by: Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>

--- documentation/src/main/asciidoc/topics/code_examples/BookIndexed.java
+++ documentation/src/main/asciidoc/topics/code_examples/BookIndexed.java
@@ -1,28 +1,23 @@
-//TODO: update example
-import org.hibernate.search.annotations.Analyze;
-import org.hibernate.search.annotations.Field;
-import org.hibernate.search.annotations.Index;
-import org.hibernate.search.annotations.Indexed;
-import org.hibernate.search.annotations.Store;
-
+import org.infinispan.api.annotations.indexing.Basic;
+import org.infinispan.api.annotations.indexing.Indexed;
+import org.infinispan.api.annotations.indexing.Text;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
 
 @Indexed
 public class Book {
 
-   @Field(index=Index.YES, analyze = Analyze.YES, store = Store.NO)
+   @Text
    @ProtoField(number = 1)
    final String title;
 
-   @Field(index=Index.YES, analyze = Analyze.YES, store = Store.NO)
+   @Text
    @ProtoField(number = 2)
    final String description;
 
-   @Field(index=Index.YES, store = Store.NO)
+   @Basic
    @ProtoField(number = 3, defaultValue = "0")
    final int publicationYear;
-
 
    @ProtoFactory
    Book(String title, String description, int publicationYear) {
