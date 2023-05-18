6e2b7afa45a4f23dbaeea57b7d7e73b77e60310c
Tristan Tarrant <ttarrant@redhat.com>
2023-05-18 18:18:58 +0200

    ISPN-14884 RESP MODULE LIST|LOAD|LOADEX|UNLOAD

---
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/connection/MODULE.java
@@ -0,0 +1,43 @@
+package org.infinispan.server.resp.commands.connection;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.concurrent.CompletionStage;
+
+import org.infinispan.security.AuthorizationPermission;
+import org.infinispan.server.resp.ByteBufferUtils;
+import org.infinispan.server.resp.Resp3Handler;
+import org.infinispan.server.resp.RespCommand;
+import org.infinispan.server.resp.RespRequestHandler;
+import org.infinispan.server.resp.commands.Resp3Command;
+
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * <a href="https://redis.io/commands/module-list/">MODULE LIST</a>
+ *
+ * @since 15.0
+ */
+public class MODULE extends RespCommand implements Resp3Command {
+   public MODULE() {
+      super(-1, 0, 0, 0);
+   }
+
+   @Override
+   public CompletionStage<RespRequestHandler> perform(Resp3Handler handler, ChannelHandlerContext ctx,
+                                                      List<byte[]> arguments) {
+      handler.checkPermission(AuthorizationPermission.ADMIN);
+      String subcommand = new String(arguments.get(0), StandardCharsets.UTF_8).toUpperCase();
+      switch (subcommand) {
+         case "LIST":
+            ByteBufferUtils.stringToByteBuf("*0\r\n", handler.allocatorToUse());
+            break;
+         case "LOAD":
+         case "LOADEX":
+         case "UNLOAD":
+            ByteBufferUtils.stringToByteBuf("-ERR module loading/unloading unsupported\r\n", handler.allocatorToUse());
+            break;
+      }
+      return handler.myStage();
+   }
+}
