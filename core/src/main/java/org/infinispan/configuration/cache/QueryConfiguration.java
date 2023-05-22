4da4fdfc545780dc9b9eef3133ef3d79ac7a879a
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-05-22 10:32:41 +0200

    ISPN-14195 Add config property for hit count accuracy

--- core/src/main/java/org/infinispan/configuration/cache/QueryConfiguration.java
+++ core/src/main/java/org/infinispan/configuration/cache/QueryConfiguration.java
@@ -12,9 +12,10 @@ import org.infinispan.configuration.parsing.Element;
 public class QueryConfiguration extends ConfigurationElement<QueryConfiguration> {
 
    public static final AttributeDefinition<Integer> DEFAULT_MAX_RESULTS = AttributeDefinition.builder(org.infinispan.configuration.parsing.Attribute.DEFAULT_MAX_RESULTS, 100).immutable().build();
+   public static final AttributeDefinition<Integer> HIT_COUNT_ACCURACY = AttributeDefinition.builder(org.infinispan.configuration.parsing.Attribute.HIT_COUNT_ACCURACY, 10_000).immutable().build();
 
    static AttributeSet attributeDefinitionSet() {
-      return new AttributeSet(QueryConfiguration.class, AbstractTypedPropertiesConfiguration.attributeSet(), DEFAULT_MAX_RESULTS);
+      return new AttributeSet(QueryConfiguration.class, AbstractTypedPropertiesConfiguration.attributeSet(), DEFAULT_MAX_RESULTS, HIT_COUNT_ACCURACY);
    }
 
    protected QueryConfiguration(AttributeSet attributes) {
@@ -23,9 +24,19 @@ public class QueryConfiguration extends ConfigurationElement<QueryConfiguration>
 
    /**
     * Limits the number of results returned by a query. Applies to indexed, non-indexed, and hybrid queries.
-    * Setting the default-max-results significantly improves performance of queries that don't have an explicit limit set.
+    * Setting the default-max-results significantly improves the performance of queries that don't have an explicit limit set.
     */
    public int defaultMaxResults() {
       return attributes.attribute(DEFAULT_MAX_RESULTS).get();
+   }
+
+   /**
+    * Limit the required accuracy of the hit count for the indexed queries to an upper-bound.
+    * Setting the hit-count-accuracy could improve the performance of queries targeting large data sets.
+    * For optimal performances set this value not much above the expected hit count.
+    * If you do not require accurate hit counts, set it to a low value.
+    */
+   public int hitCountAccuracy() {
+      return attributes.attribute(HIT_COUNT_ACCURACY).get();
    }
 }
