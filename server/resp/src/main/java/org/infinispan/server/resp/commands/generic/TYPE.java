9d0bfa949394f21593fb05c5a742844652c9a5b9
Tristan Tarrant <ttarrant@redhat.com>
2023-06-16 13:17:52 +0200

    ISPN-14951 RESP TYPE for list, hash, set, zset

--- server/resp/src/main/java/org/infinispan/server/resp/commands/generic/TYPE.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/generic/TYPE.java
@@ -4,6 +4,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
+import org.infinispan.commons.dataconversion.MediaType;
+import org.infinispan.multimap.impl.HashMapBucket;
+import org.infinispan.multimap.impl.ListBucket;
+import org.infinispan.multimap.impl.SetBucket;
+import org.infinispan.multimap.impl.SortedSetBucket;
 import org.infinispan.server.resp.Consumers;
 import org.infinispan.server.resp.Resp3Handler;
 import org.infinispan.server.resp.RespCommand;
@@ -28,12 +33,24 @@ public class TYPE extends RespCommand implements Resp3Command {
                                                       ChannelHandlerContext ctx,
                                                       List<byte[]> arguments) {
       byte[] keyBytes = arguments.get(0);
-
-      return handler.stageToReturn(handler.cache().getCacheEntryAsync(keyBytes).thenApply(e -> {
+      MediaType vmt = handler.cache().getValueDataConversion().getStorageMediaType();
+      return handler.stageToReturn(handler.cache().withMediaType(MediaType.APPLICATION_OCTET_STREAM, vmt).getCacheEntryAsync(keyBytes).thenApply(e -> {
          if (e == null) {
             return RespTypes.none.name().getBytes(StandardCharsets.US_ASCII);
-         } else {
+         }
+         Class<?> c = e.getValue().getClass();
+         if (c == HashMapBucket.class) {
+            return RespTypes.hash.name().getBytes(StandardCharsets.US_ASCII);
+         } else if (c == ListBucket.class) {
+            return RespTypes.list.name().getBytes(StandardCharsets.US_ASCII);
+         } else if (c == SetBucket.class) {
+            return RespTypes.set.name().getBytes(StandardCharsets.US_ASCII);
+         } else if (c == SortedSetBucket.class) {
+            return RespTypes.zset.name().getBytes(StandardCharsets.US_ASCII);
+         } else if (c == byte[].class) {
             return RespTypes.string.name().getBytes(StandardCharsets.US_ASCII);
+         } else {
+            return RespTypes.unknown.name().getBytes(StandardCharsets.US_ASCII);
          }
       }), ctx, Consumers.GET_BICONSUMER);
    }
