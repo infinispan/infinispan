68dd1514e20ce4d2e4e25305692916e46adf3329
Alexander Schwartz <aschwart@redhat.com>
2023-06-28 19:40:57 +0200

    ISPN-14997 Filter illegal chars in pool names

--- core/src/main/java/org/infinispan/metrics/impl/VendorAdditionalMetrics.java
+++ core/src/main/java/org/infinispan/metrics/impl/VendorAdditionalMetrics.java
@@ -23,7 +23,9 @@ class VendorAdditionalMetrics implements MeterBinder {
       List<BufferPoolMXBean> bufferPoolBeans = ManagementFactory.getPlatformMXBeans(BufferPoolMXBean.class);
       for (BufferPoolMXBean bufferPoolBean : bufferPoolBeans) {
          String name = bufferPoolBean.getName();
-         Gauge.builder(PREFIX + "BufferPool.used.memory." + name, bufferPoolBean, BufferPoolMXBean::getMemoryUsed)
+         // avoid illegal characters due to beans named for example: "mapped - 'non-volatile memory'"
+         String metricName = normalizeMetricName(name);
+         Gauge.builder(PREFIX + "BufferPool.used.memory." + metricName, bufferPoolBean, BufferPoolMXBean::getMemoryUsed)
                .baseUnit(BaseUnits.BYTES)
                .description("The memory used by the NIO pool:" + name)
                .register(registry);
@@ -32,15 +34,26 @@ class VendorAdditionalMetrics implements MeterBinder {
       List<MemoryPoolMXBean> memoryPoolBeans = ManagementFactory.getMemoryPoolMXBeans();
       for (MemoryPoolMXBean memoryPoolBean : memoryPoolBeans) {
          String name = memoryPoolBean.getName();
-         Gauge.builder(PREFIX + "memoryPool." + name + ".usage", memoryPoolBean, (mem) -> mem.getUsage().getUsed())
+         // avoid illegal characters due to beans named for example: "CodeHeap 'non-nmethods'"
+         String metricName = normalizeMetricName(name);
+
+         Gauge.builder(PREFIX + "memoryPool." + metricName + ".usage", memoryPoolBean, (mem) -> mem.getUsage().getUsed())
                .baseUnit(BaseUnits.BYTES)
                .description("Current usage of the " + name + " memory pool")
                .register(registry);
 
-         Gauge.builder(PREFIX + "memoryPool." + name + ".usage.max", memoryPoolBean, (mem) -> mem.getPeakUsage().getUsed())
+         Gauge.builder(PREFIX + "memoryPool." + metricName + ".usage.max", memoryPoolBean, (mem) -> mem.getPeakUsage().getUsed())
                .baseUnit(BaseUnits.BYTES)
                .description("Peak usage of the " + name + " memory pool")
                .register(registry);
       }
+   }
+
+   private static String normalizeMetricName(String name) {
+      return NameUtils
+              // map all illegal characters to "_"
+              .filterIllegalChars(name)
+              // remove underscores at the start and end of the name
+              .replaceAll("^_", "").replaceAll("_$", "");
    }
 }
