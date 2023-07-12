8602a318e05fe331799948f1ee97f90adff6e1de
José Bolina <jbolina@redhat.com>
2023-07-12 16:46:21 -0300

    ISPN-14894 Testing data structure with distributed server
    * Using the vert.x client and multiple servers;

---
+++ server/tests/src/test/java/org/infinispan/server/functional/resp/AbstractRespTest.java
@@ -0,0 +1,53 @@
+package org.infinispan.server.functional.resp;
+
+import java.net.InetSocketAddress;
+
+import org.infinispan.server.functional.ClusteredIT;
+import org.infinispan.server.test.junit5.InfinispanServerExtension;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.vertx.core.Vertx;
+import io.vertx.junit5.VertxExtension;
+import io.vertx.redis.client.RedisAPI;
+import io.vertx.redis.client.RedisClientType;
+import io.vertx.redis.client.RedisOptions;
+
+@ExtendWith(VertxExtension.class)
+public abstract class AbstractRespTest {
+
+   @RegisterExtension
+   public static InfinispanServerExtension SERVERS = ClusteredIT.SERVERS;
+
+   private static RedisOptions options;
+
+   @BeforeAll
+   static void beforeAll() {
+      int size = SERVERS.getServerDriver().getConfiguration().numServers();
+      RedisOptions opts = new RedisOptions()
+            .setPoolName("string-operations-test");
+
+      if (size > 1) {
+         opts = opts.setType(RedisClientType.CLUSTER);
+      } else {
+         opts = opts.setType(RedisClientType.STANDALONE);
+      }
+
+      for (int i = 0; i < size; i++) {
+         InetSocketAddress serverSocket = SERVERS.getServerDriver().getServerSocket(i, 11222);
+         String uri = "redis://" + serverSocket.getHostString() + ":" + serverSocket.getPort();
+         opts = opts.addConnectionString(uri);
+      }
+
+      AbstractRespTest.options = opts;
+   }
+
+   protected final RedisAPI createConnection(Vertx vertx) {
+      return RedisAPI.api(SERVERS.resp().withOptions(options).withVertx(vertx).get());
+   }
+
+   protected final RedisAPI createConnection(Vertx vertx, RedisOptions options) {
+      return RedisAPI.api(SERVERS.resp().withOptions(options).withVertx(vertx).get());
+   }
+}
