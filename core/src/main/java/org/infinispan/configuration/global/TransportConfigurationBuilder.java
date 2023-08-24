9c47fab21561ec252cc1b718619a27de15367794
cfredri4 <50839054+cfredri4@users.noreply.github.com>
2023-08-24 20:45:11 +0200

    Fix javadoc that TransportConfigurationBuilder does not use object instance

--- core/src/main/java/org/infinispan/configuration/global/TransportConfigurationBuilder.java
+++ core/src/main/java/org/infinispan/configuration/global/TransportConfigurationBuilder.java
@@ -138,10 +138,6 @@ public class TransportConfigurationBuilder extends AbstractGlobalConfigurationBu
     * Class that represents a network transport. Must implement
     * org.infinispan.remoting.transport.Transport
     *
-    * NOTE: Currently Infinispan will not use the object instance, but instead instantiate a new
-    * instance of the class. Therefore, do not expect any state to survive, and provide a no-args
-    * constructor to any instance.
-    *
     * @param transport transport instance
     */
    public TransportConfigurationBuilder transport(Transport transport) {
