680b1cdb9073e777588c87d40da31f7eb12a8e90
rigazilla <vrigamon@redhat.com>
2023-09-12 09:59:46 +0200

    fix

--- server/resp/src/main/java/org/infinispan/server/resp/commands/string/MSETNX.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/string/MSETNX.java
@@ -2,6 +2,7 @@ package org.infinispan.server.resp.commands.string;
 
 import io.netty.channel.ChannelHandlerContext;
 import org.infinispan.server.resp.commands.Resp3Command;
+import org.infinispan.server.resp.logging.Log;
 import org.infinispan.server.resp.ByteBufferUtils;
 import org.infinispan.server.resp.Consumers;
 import org.infinispan.server.resp.Resp3Handler;
@@ -15,6 +16,11 @@ import java.util.concurrent.CompletionStage;
 
 /**
  * @link https://redis.io/commands/msetnx/
+ *       Derogating to the above description, this implementation is not atomic:
+ *       msetnx first checks the nonexistence of all the keys
+ *       and then performs the set. A concurrent set of any of the keys by
+ *       another client
+ *       will be overwritten.
  * @since 15.0
  */
 public class MSETNX extends RespCommand implements Resp3Command {
@@ -31,6 +37,7 @@ public class MSETNX extends RespCommand implements Resp3Command {
                handler.allocator());
          return handler.myStage();
       }
+      Log.SERVER.msetnxConsistencyMessage();
       var entries = new HashMap<byte[], byte[]>();
       for (int i = 0; i < arguments.size(); i++) {
          entries.put(arguments.get(i), arguments.get(++i));
@@ -38,7 +45,8 @@ public class MSETNX extends RespCommand implements Resp3Command {
       // Change to loop?
       var existingEntries = handler.cache().getAll(entries.keySet());
       if (existingEntries.size() == 0) {
-         return  handler.stageToReturn(handler.cache().putAllAsync(entries).thenApply(v -> 1L), ctx, Consumers.LONG_BICONSUMER);
+         return handler.stageToReturn(handler.cache().putAllAsync(entries).thenApply(v -> 1L), ctx,
+               Consumers.LONG_BICONSUMER);
       }
       return handler
             .stageToReturn(CompletableFuture.completedFuture(0L),
