deb4423206f828169e4b6106f68f339313669e3c
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-09-13 12:19:25 +0200

    ISPN-15156 Avoid to spam DEBUG log
    When OpenTelemetry is not found on the client

--- client/hotrod/src/main/java/org/infinispan/hotrod/telemetry/impl/TelemetryService.java
+++ client/hotrod/src/main/java/org/infinispan/hotrod/telemetry/impl/TelemetryService.java
@@ -1,17 +1,30 @@
 package org.infinispan.hotrod.telemetry.impl;
 
+import org.infinispan.hotrod.impl.logging.Log;
+import org.infinispan.hotrod.impl.logging.LogFactory;
 import org.infinispan.hotrod.impl.protocol.HeaderParams;
 
 public interface TelemetryService {
 
-   /**
-    * Try to create a {@link TelemetryService} instance.
-    *
-    * @throws ClassCastException if the OpenTelemetry API module is not in the classpath
-    * @return a {@link TelemetryService} instance
-    */
-   static TelemetryService create() {
-      return new TelemetryServiceImpl();
+   Log log = LogFactory.getLog(TelemetryService.class, Log.class);
+
+   TelemetryService INSTANCE = createInstance();
+
+   static TelemetryService createInstance() {
+      try {
+         Class.forName("io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator", false,
+               TelemetryService.class.getClassLoader());
+      } catch (ClassNotFoundException e) {
+         log.noOpenTelemetryAPI();
+         return null;
+      }
+
+      try {
+         return new TelemetryServiceImpl();
+      } catch (Throwable e) {
+         log.errorCreatingPropagationContext(e);
+         return null;
+      }
    }
 
    void injectSpanContext(HeaderParams header);
