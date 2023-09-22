46cb9feb67843774bdf1bd041519d30159d4d85b
Pedro Ruivo <pruivo@redhat.com>
2023-09-22 15:29:51 +0100

    ISPN-15304 Virtual Threads improvements
    * Enable JGroups virtual threads with org.infinispan.threads.virtual or
      jgroups.thread.virtual
    * Create VirtualThreadPerTaskExecutor for blocking thread pool
    * Do not use virtual threads in Netty event loop

--- commons/all/src/main/java/org/infinispan/commons/jdkspecific/ThreadCreator.java
+++ commons/all/src/main/java/org/infinispan/commons/jdkspecific/ThreadCreator.java
@@ -1,5 +1,8 @@
 package org.infinispan.commons.jdkspecific;
 
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+
 import org.infinispan.commons.logging.Log;
 import org.infinispan.commons.util.Util;
 
@@ -8,11 +11,15 @@ import org.infinispan.commons.util.Util;
  * @since 11.0
  **/
 public class ThreadCreator {
-   static org.infinispan.commons.spi.ThreadCreator INSTANCE = getInstance();
+   private static final org.infinispan.commons.spi.ThreadCreator INSTANCE = getInstance();
+
+   public static boolean useVirtualThreads() {
+      return Boolean.getBoolean("org.infinispan.threads.virtual");
+   }
 
    private static org.infinispan.commons.spi.ThreadCreator getInstance() {
       try {
-         if (Boolean.getBoolean("org.infinispan.threads.virtual")) {
+         if (useVirtualThreads()) {
             org.infinispan.commons.spi.ThreadCreator instance = Util.getInstance("org.infinispan.commons.jdk21.ThreadCreatorImpl", ThreadCreator.class.getClassLoader());
             Log.CONTAINER.infof("Virtual threads support enabled");
             return instance;
@@ -24,8 +31,12 @@ public class ThreadCreator {
    }
 
 
-   public static Thread createThread(ThreadGroup threadGroup, Runnable target, boolean lightweight) {
-      return INSTANCE.createThread(threadGroup, target, lightweight);
+   public static Thread createThread(ThreadGroup threadGroup, Runnable target, boolean useVirtualThread) {
+      return INSTANCE.createThread(threadGroup, target, useVirtualThread);
+   }
+
+   public static Optional<ExecutorService> createBlockingExecutorService() {
+      return INSTANCE.newVirtualThreadPerTaskExecutor();
    }
 
    static class ThreadCreatorImpl implements org.infinispan.commons.spi.ThreadCreator {
@@ -33,6 +44,11 @@ public class ThreadCreator {
       @Override
       public Thread createThread(ThreadGroup threadGroup, Runnable target, boolean lightweight) {
          return new Thread(threadGroup, target);
+      }
+
+      @Override
+      public Optional<ExecutorService> newVirtualThreadPerTaskExecutor() {
+         return Optional.empty();
       }
    }
 }
--- commons/spi/src/main/java/org/infinispan/commons/spi/ThreadCreator.java
+++ commons/spi/src/main/java/org/infinispan/commons/spi/ThreadCreator.java
@@ -1,8 +1,13 @@
 package org.infinispan.commons.spi;
 
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+
 /**
  * @since 15.0
  **/
 public interface ThreadCreator {
    Thread createThread(ThreadGroup threadGroup, Runnable target, boolean lightweight);
+
+   Optional<ExecutorService> newVirtualThreadPerTaskExecutor();
 }
