088bbbfce926314e41b768670f29c2ab81e47166
William Burns <mudokonman@gmail.com>
2023-10-26 08:08:15 -0700

    ISPN-15262 IndexedTests can fail on clear due to blocking call

--- core/src/main/java/org/infinispan/util/CoreBlockHoundIntegration.java
+++ core/src/main/java/org/infinispan/util/CoreBlockHoundIntegration.java
@@ -29,6 +29,7 @@ import org.jgroups.protocols.pbcast.GMS;
 import org.jgroups.util.TimeScheduler3;
 import org.kohsuke.MetaInfServices;
 
+import io.reactivex.rxjava3.internal.operators.flowable.BlockingFlowableIterable;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.integration.BlockHoundIntegration;
 
@@ -80,6 +81,9 @@ public class CoreBlockHoundIntegration implements BlockHoundIntegration {
 
       // Reads from a file during initialization which is during store startup
       builder.allowBlockingCallsInside("org.infinispan.persistence.sifs.Index", "checkForExistingIndexSizeFile");
+
+      // FlowableIterable may be completed on a non blocking thread, however this is very short
+      builder.allowBlockingCallsInside(BlockingFlowableIterable.class.getName() + "$FlowableSubscriber", "signalConsumer");
 
       methodsToBeRemoved(builder);
 
