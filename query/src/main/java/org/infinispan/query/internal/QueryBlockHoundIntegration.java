53447a71d6616d646c891df1b5f8249b9eff206e
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-11-07 08:30:40 +0100

    ISPN-15298 Add block hound exception for index submitToQueue

--- query/src/main/java/org/infinispan/query/internal/QueryBlockHoundIntegration.java
+++ query/src/main/java/org/infinispan/query/internal/QueryBlockHoundIntegration.java
@@ -1,6 +1,7 @@
 package org.infinispan.query.internal;
 
 import org.apache.lucene.util.NamedSPILoader;
+import org.hibernate.search.engine.backend.work.execution.OperationSubmitter;
 import org.kohsuke.MetaInfServices;
 
 import reactor.blockhound.BlockHound;
@@ -8,9 +9,17 @@ import reactor.blockhound.integration.BlockHoundIntegration;
 
 @MetaInfServices
 public class QueryBlockHoundIntegration implements BlockHoundIntegration {
+
    @Override
    public void applyTo(BlockHound.Builder builder) {
       // Loading a service may require opening a file from classpath
       builder.allowBlockingCallsInside(NamedSPILoader.class.getName(), "reload");
+
+      // With Infinispan the implementation is OffloadingExecutorOperationSubmitter.
+      // Here the submitToQueue method invokes BlockingQueue#offer, the single argument one,
+      // that uses the lock of the blocking queue just to check safely the size of the data structure,
+      // releasing immediately the lock after this super-short check.
+      builder.allowBlockingCallsInside(OperationSubmitter.class.getName() + "$OffloadingExecutorOperationSubmitter",
+            "submitToQueue");
    }
 }
