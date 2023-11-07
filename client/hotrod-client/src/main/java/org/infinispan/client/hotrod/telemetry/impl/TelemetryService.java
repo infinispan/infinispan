f039943a2aa92922216c98da0eadf46c0148557b
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2023-11-07 16:35:37 +0100

    ISPN-15156 Fix logging of tracing enabled

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/telemetry/impl/TelemetryService.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/telemetry/impl/TelemetryService.java
@@ -1,31 +1,8 @@
 package org.infinispan.client.hotrod.telemetry.impl;
 
 import org.infinispan.client.hotrod.impl.protocol.HeaderParams;
-import org.infinispan.client.hotrod.logging.Log;
-import org.infinispan.client.hotrod.logging.LogFactory;
 
 public interface TelemetryService {
-
-   Log log = LogFactory.getLog(TelemetryService.class, Log.class);
-
-   TelemetryService INSTANCE = createInstance();
-
-   static TelemetryService createInstance() {
-      try {
-         Class.forName("io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator", false,
-               TelemetryService.class.getClassLoader());
-      } catch (ClassNotFoundException e) {
-         log.noOpenTelemetryAPI();
-         return null;
-      }
-
-      try {
-         return new TelemetryServiceImpl();
-      } catch (Throwable e) {
-         log.errorCreatingPropagationContext(e);
-         return null;
-      }
-   }
 
    void injectSpanContext(HeaderParams header);
 
