43b63838ede1c032ffcc7ab7b87c5af81f630fcc
Tristan Tarrant <ttarrant@redhat.com>
2023-11-17 07:26:22 +0100

    ISPN-12158 Remove AnyScopeComponentFactory.construct(Class)

--- core/src/main/java/org/infinispan/factories/AnyScopeComponentFactory.java
+++ core/src/main/java/org/infinispan/factories/AnyScopeComponentFactory.java
@@ -1,7 +1,5 @@
 package org.infinispan.factories;
 
-import org.infinispan.commons.CacheConfigurationException;
-import org.infinispan.commons.util.ReflectionUtil;
 import org.infinispan.configuration.global.GlobalConfiguration;
 import org.infinispan.factories.annotations.Inject;
 import org.infinispan.factories.scopes.Scope;
@@ -10,31 +8,8 @@ import org.infinispan.util.logging.Log;
 import org.infinispan.util.logging.LogFactory;
 
 @Scope(Scopes.NONE)
-public class AnyScopeComponentFactory implements ComponentFactory {
+public abstract class AnyScopeComponentFactory implements ComponentFactory {
    protected static final Log log = LogFactory.getLog(AbstractComponentFactory.class);
    @Inject protected GlobalComponentRegistry globalComponentRegistry;
    @Inject protected GlobalConfiguration globalConfiguration;
-
-   @Override
-   public Object construct(String name) {
-      Class<?> componentType;
-      try {
-         componentType = ReflectionUtil.getClassForName(name, globalComponentRegistry.getClassLoader());
-      } catch (ClassNotFoundException e) {
-         throw new CacheConfigurationException(e);
-      }
-      return construct(componentType);
-   }
-
-   /**
-    * Constructs a component.
-    *
-    * @param componentType type of component
-    * @return a component
-    * @deprecated Since 9.4, please override {@link ComponentFactory#construct(String)} instead.
-    */
-   @Deprecated
-   public <T> T construct(Class<T> componentType) {
-      throw new UnsupportedOperationException();
-   }
 }
