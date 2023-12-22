55e607ecbfbd8b267ee39c291fab6d71babef179
Pedro Ruivo <pruivo@redhat.com>
2023-12-22 11:35:14 +0000

    ISPN-15401 Remove worker-threads section from tuning guide

--- server/hotrod/src/main/java/org/infinispan/server/hotrod/configuration/HotRodServerConfiguration.java
+++ server/hotrod/src/main/java/org/infinispan/server/hotrod/configuration/HotRodServerConfiguration.java
@@ -18,15 +18,13 @@ public class HotRodServerConfiguration extends ProtocolServerConfiguration<HotRo
 
    public static final AttributeDefinition<String> PROXY_HOST = AttributeDefinition.builder(Attribute.EXTERNAL_HOST, null, String.class).immutable().build();
    public static final AttributeDefinition<Integer> PROXY_PORT = AttributeDefinition.builder(Attribute.EXTERNAL_PORT, -1).immutable().build();
-   // The Hot Rod server has a different default
-   public static final AttributeDefinition<Integer> WORKER_THREADS = AttributeDefinition.builder("worker-threads", 160).immutable().build();
 
    private final TopologyCacheConfiguration topologyCache;
    private final EncryptionConfiguration encryption;
 
    public static AttributeSet attributeDefinitionSet() {
       return new AttributeSet(HotRodServerConfiguration.class, ProtocolServerConfiguration.attributeDefinitionSet(),
-            WORKER_THREADS, PROXY_HOST, PROXY_PORT);
+            PROXY_HOST, PROXY_PORT);
    }
 
    HotRodServerConfiguration(AttributeSet attributes,
