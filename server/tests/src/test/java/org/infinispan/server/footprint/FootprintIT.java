534adb6061d20fe4521ef58aa1308deadefdd36f
Tristan Tarrant <ttarrant@redhat.com>
2024-01-26 11:18:46 +0100

    ISPN-15555 Adjust server disk footprint

--- server/tests/src/test/java/org/infinispan/server/footprint/FootprintIT.java
+++ server/tests/src/test/java/org/infinispan/server/footprint/FootprintIT.java
@@ -30,8 +30,8 @@ public class FootprintIT {
    private static final int LOADED_CLASS_COUNT_UPPER_BOUND = 11_220;
    private static final long HEAP_USAGE_LOWER_BOUND = 23_000_000L;
    private static final long HEAP_USAGE_UPPER_BOUND = 25_000_000L;
-   private static final long DISK_USAGE_LOWER_BOUND = 87_000_000L;
-   private static final long DISK_USAGE_UPPER_BOUND = 88_000_000L;
+   private static final long DISK_USAGE_LOWER_BOUND = 88_000_000L;
+   private static final long DISK_USAGE_UPPER_BOUND = 89_000_000L;
    public static final String HEAP_DUMP = "footprint.hprof";
 
    @RegisterExtension
