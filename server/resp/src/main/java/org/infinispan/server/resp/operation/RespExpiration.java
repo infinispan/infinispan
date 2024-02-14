53749824553327299da72d5aefa2baa86ee9fe4e
José Bolina <jbolina@redhat.com>
2024-02-14 16:26:16 -0300

    ISPN-14655 PSETEX

--- server/resp/src/main/java/org/infinispan/server/resp/operation/RespExpiration.java
+++ server/resp/src/main/java/org/infinispan/server/resp/operation/RespExpiration.java
@@ -33,6 +33,7 @@ public enum RespExpiration {
    };
 
    public static final byte[] EX_BYTES = "EX".getBytes(StandardCharsets.US_ASCII);
+   public static final byte[] PX_BYTES = "PX".getBytes(StandardCharsets.US_ASCII);
    public static final byte[] EXAT_BYTES = "EXAT".getBytes(StandardCharsets.US_ASCII);
    public static final byte[] PXAT_BYTES = "PXAT".getBytes(StandardCharsets.US_ASCII);
 
