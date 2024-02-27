dfec8a620c903bc8521a886e0441b537833995c2
Pedro Ruivo <pruivo@redhat.com>
2024-02-27 11:16:07 +0000

    ISPN-15755 Add WebSphere Liberty TransactionManager

--- commons/all/src/main/java/org/infinispan/commons/tx/lookup/LookupNames.java
+++ commons/all/src/main/java/org/infinispan/commons/tx/lookup/LookupNames.java
@@ -1,11 +1,19 @@
 package org.infinispan.commons.tx.lookup;
 
+import static org.infinispan.commons.logging.Log.CONTAINER;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Optional;
+
+import org.infinispan.commons.logging.Log;
+import org.infinispan.commons.logging.LogFactory;
+import org.infinispan.commons.util.Util;
 
 import jakarta.transaction.TransactionManager;
-
-import org.infinispan.commons.util.Util;
 
 /**
  * The JDNI and {@link TransactionManager} factories we know of.
@@ -14,6 +22,8 @@ import org.infinispan.commons.util.Util;
  * @since 9.3
  */
 public final class LookupNames {
+
+   private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass());
 
    private LookupNames() {
    }
@@ -51,6 +61,7 @@ public final class LookupNames {
     * TransactionManager factories we know of.
     */
    public enum TransactionManagerFactory {
+      WEBSPHERE_LIBERTY("com.ibm.tx.jta.TransactionManagerFactory", "WebSphere Liberty", "getTransactionManager"),
       WEBSPHERE_51_6("com.ibm.ws.Transaction.TransactionManagerFactory", "WebSphere 5.1 and 6.0",
             "getTransactionManager"),
       WEBSPHERE_6("com.ibm.ejs.jts.jta.TransactionManagerFactory", "WebSphere 5.0", "getTransactionManager"),
@@ -86,6 +97,74 @@ public final class LookupNames {
             return null;
          }
       }
+   }
+
+   /**
+    * Lookup for {@link TransactionManager}.
+    * <p>
+    * It looks up by JNDI first using {@link #lookupByJNDI()} and then by {@link TransactionManager} factories, using
+    * {@link #lookupByTransactionManagerFactory(ClassLoader)}.
+    *
+    * @param classLoader The {@link ClassLoader} to be used by {@link #lookupByTransactionManagerFactory(ClassLoader)}
+    * @return An {@link Optional} with the {@link TransactionManager} if found.
+    */
+   public static Optional<TransactionManager> lookupKnownTransactionManagers(ClassLoader classLoader) {
+      return lookupByJNDI()
+            .or(() -> lookupByTransactionManagerFactory(classLoader));
+   }
+
+   /**
+    * Lookup for a {@link TransactionManager} by known JNDI names.
+    *
+    * @return An {@link Optional} with the {@link TransactionManager} if found.
+    */
+   public static Optional<TransactionManager> lookupByJNDI() {
+      InitialContext ctx;
+      try {
+         ctx = new InitialContext();
+      } catch (NamingException e) {
+         CONTAINER.failedToCreateInitialCtx(e);
+         return Optional.empty();
+      }
+
+      try {
+         for (var lookup : JndiTransactionManager.values()) {
+            Object jndiObject;
+            try {
+               log.debugf("Trying to lookup TransactionManager for %s", lookup.getPrettyName());
+               jndiObject = ctx.lookup(lookup.getJndiLookup());
+            } catch (NamingException e) {
+               log.debugf("Failed to perform a lookup for [%s (%s)]",
+                     lookup.getJndiLookup(), lookup.getPrettyName());
+               continue;
+            }
+            if (jndiObject instanceof TransactionManager) {
+               log.debugf("Found TransactionManager for %s", lookup.getPrettyName());
+               return Optional.of((TransactionManager) jndiObject);
+            }
+         }
+      } finally {
+         Util.close(ctx);
+      }
+      return Optional.empty();
+   }
+
+   /**
+    * Lookup a {@link TransactionManager} by factory.
+    *
+    * @param classLoader The {@link ClassLoader} to use to instantiate the factory.
+    * @return An {@link Optional} with the {@link TransactionManager} if found.
+    */
+   public static Optional<TransactionManager> lookupByTransactionManagerFactory(ClassLoader classLoader) {
+      for (var factory : TransactionManagerFactory.values()) {
+         log.debugf("Trying %s: %s", factory.getPrettyName(), factory.getFactoryClazz());
+         TransactionManager transactionManager = factory.tryLookup(classLoader);
+         if (transactionManager != null) {
+            log.debugf("Found %s: %s", factory.getPrettyName(), factory.getFactoryClazz());
+            return Optional.of(transactionManager);
+         }
+      }
+      return Optional.empty();
    }
 
 
