fe9ef822d78030e637319f4c8af4be486708a1f7
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-03-01 03:42:22 +0100

    ISPN-15772 Allow report file writing on cert exception

--- server/insights/src/test/java/org/infinispan/server/insights/InsightsActivationTest.java
+++ server/insights/src/test/java/org/infinispan/server/insights/InsightsActivationTest.java
@@ -2,6 +2,10 @@ package org.infinispan.server.insights;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.server.insights.config.InsightsActivation;
 import org.infinispan.test.AbstractInfinispanTest;
@@ -10,12 +14,32 @@ import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
+import com.redhat.insights.config.EnvAndSysPropsInsightsConfiguration;
 import com.redhat.insights.reports.InsightsReport;
 
 @Test(groups = "functional", testName = "server.insights.InsightsActivationTest")
 public class InsightsActivationTest extends AbstractInfinispanTest {
 
-   public void test() throws Exception {
+   public void enabled() throws Exception {
+      Path tmpDir = Paths.get(System.getProperty("java.io.tmpdir"), "blablabla");
+      Files.createDirectories(tmpDir);
+      System.setProperty(InsightsModule.REDHAT_INSIGHTS_ACTIVATION_PROPERTY_NAME, InsightsActivation.ENABLED.name());
+      System.setProperty(InfinispanInsightsConfiguration.ENV_RHEL_MACHINE_ID_FILE_PATH.toLowerCase().replace('_', '.'), tmpDir.toString());
+      System.setProperty(EnvAndSysPropsInsightsConfiguration.ENV_ARCHIVE_UPLOAD_DIR.toLowerCase().replace('_', '.'), tmpDir.toString());
+      try (EmbeddedCacheManager cacheManager = TestCacheManagerFactory.createServerModeCacheManager()) {
+         InsightsService service = cacheManager.getGlobalComponentRegistry().getComponent(InsightsService.class);
+         assertThat(service).isNotNull();
+         InsightsReport report = service.report();
+         String json = report.serialize();
+         assertThat(json).isNotEmpty();
+      } finally {
+         System.clearProperty(InfinispanInsightsConfiguration.ENV_RHEL_MACHINE_ID_FILE_PATH.toLowerCase().replace('_', '.'));
+         System.clearProperty(InfinispanInsightsConfiguration.ENV_ARCHIVE_UPLOAD_DIR.toLowerCase().replace('_', '.'));
+      }
+      eventually(() -> "report is produced locally", () -> Files.list(tmpDir).count() > 0);
+   }
+
+   public void notEnabled() throws Exception {
       System.setProperty(InsightsModule.REDHAT_INSIGHTS_ACTIVATION_PROPERTY_NAME, InsightsActivation.LOCAL.name());
       try (EmbeddedCacheManager cacheManager = TestCacheManagerFactory.createServerModeCacheManager()) {
          InsightsService service = cacheManager.getGlobalComponentRegistry().getComponent(InsightsService.class);
@@ -29,15 +53,6 @@ public class InsightsActivationTest extends AbstractInfinispanTest {
       try (EmbeddedCacheManager cacheManager = TestCacheManagerFactory.createServerModeCacheManager()) {
          InsightsService service = cacheManager.getGlobalComponentRegistry().getComponent(InsightsService.class);
          assertThat(service).isNull();
-      }
-
-      System.setProperty(InsightsModule.REDHAT_INSIGHTS_ACTIVATION_PROPERTY_NAME, InsightsActivation.ENABLED.name());
-      try (EmbeddedCacheManager cacheManager = TestCacheManagerFactory.createServerModeCacheManager()) {
-         InsightsService service = cacheManager.getGlobalComponentRegistry().getComponent(InsightsService.class);
-         assertThat(service).isNotNull();
-         InsightsReport report = service.report();
-         String json = report.serialize();
-         assertThat(json).isNotEmpty();
       }
    }
 
