67f060ebeefdfc9bcd20948b96e7fc890b1537cd
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-03-01 03:42:22 +0100

    ISPN-15772 Allow report file writing on cert exception

--- server/insights/src/main/java/org/infinispan/server/insights/InfinispanInsightsConfiguration.java
+++ server/insights/src/main/java/org/infinispan/server/insights/InfinispanInsightsConfiguration.java
@@ -6,6 +6,8 @@ import com.redhat.insights.config.EnvAndSysPropsInsightsConfiguration;
 
 public class InfinispanInsightsConfiguration extends EnvAndSysPropsInsightsConfiguration {
 
+   public static final String ENV_RHEL_MACHINE_ID_FILE_PATH = "RHT_INSIGHTS_RHEL_MACHINE_ID_FILE_PATH";
+
    private final Supplier<String> identificationName;
 
    public InfinispanInsightsConfiguration(Supplier<String> identificationName) {
@@ -15,5 +17,22 @@ public class InfinispanInsightsConfiguration extends EnvAndSysPropsInsightsConfi
    @Override
    public String getIdentificationName() {
       return identificationName.get();
+   }
+
+   @Override
+   public String getMachineIdFilePath() {
+      String value = lookup(ENV_RHEL_MACHINE_ID_FILE_PATH);
+      if (value != null) {
+         return value;
+      }
+      return super.getMachineIdFilePath();
+   }
+
+   private String lookup(String env) {
+      String value = System.getenv(env);
+      if (value == null) {
+         value = System.getProperty(env.toLowerCase().replace('_', '.'));
+      }
+      return value;
    }
 }
