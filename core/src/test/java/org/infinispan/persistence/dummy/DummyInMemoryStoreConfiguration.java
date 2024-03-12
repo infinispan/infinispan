4fc1748adc3bffe0eed9d567ab10be64d7985a2b
William Burns <mudokonman@gmail.com>
2024-03-12 14:50:18 -0700

    ISPN-15860 Exception with async store when not segmented

--- core/src/test/java/org/infinispan/persistence/dummy/DummyInMemoryStoreConfiguration.java
+++ core/src/test/java/org/infinispan/persistence/dummy/DummyInMemoryStoreConfiguration.java
@@ -11,10 +11,11 @@ import org.infinispan.configuration.cache.AsyncStoreConfiguration;
 @ConfigurationFor(DummyInMemoryStore.class)
 public class DummyInMemoryStoreConfiguration extends AbstractStoreConfiguration<DummyInMemoryStoreConfiguration> {
    static final AttributeDefinition<Boolean> SLOW = AttributeDefinition.builder("slow", false).immutable().build();
+   static final AttributeDefinition<Boolean> ASYNC_OPERATION = AttributeDefinition.builder("async-operation", false).immutable().build();
    static final AttributeDefinition<String> STORE_NAME = AttributeDefinition.builder("store-name", null, String.class).immutable().build();
    static final AttributeDefinition<Integer> START_FAILURES = AttributeDefinition.builder("start-failures", 0).immutable().build();
    public static AttributeSet attributeDefinitionSet() {
-      return new AttributeSet(DummyInMemoryStoreConfiguration.class, AbstractStoreConfiguration.attributeDefinitionSet(), SLOW, STORE_NAME, START_FAILURES);
+      return new AttributeSet(DummyInMemoryStoreConfiguration.class, AbstractStoreConfiguration.attributeDefinitionSet(), SLOW, ASYNC_OPERATION, STORE_NAME, START_FAILURES);
    }
 
    public DummyInMemoryStoreConfiguration(AttributeSet attributes, AsyncStoreConfiguration async) {
@@ -23,6 +24,10 @@ public class DummyInMemoryStoreConfiguration extends AbstractStoreConfiguration<
 
    public boolean slow() {
       return attributes.attribute(SLOW).get();
+   }
+
+   public boolean asyncOperation() {
+      return attributes.attribute(ASYNC_OPERATION).get();
    }
 
    public String storeName() {
