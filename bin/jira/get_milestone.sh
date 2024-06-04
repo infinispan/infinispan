1b46c4049b44eb628bcf279b4c0aa5c8a7dc4289
Ryan Emerson <remerson@redhat.com>
2024-06-04 10:53:26 +0100

    Utilise major and minor version when auto populating PR milestone

--- bin/jira/get_milestone.sh
+++ bin/jira/get_milestone.sh
@@ -3,12 +3,12 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 source "${SCRIPT_DIR}/common.sh"
 
-requiredEnv TOKEN PROJECT_KEY MAJOR_VERSION
+requiredEnv TOKEN PROJECT_KEY MAJOR_MINOR_VERSION
 
 VERSIONS_URL="${API_URL}/project/${PROJECT_KEY}/versions"
 
 # Return the oldest unreleased version for the project
 curl ${VERSIONS_URL} \
-  | jq -r ".[] | select(.released==false) | select(.name | startswith(\"${MAJOR_VERSION}\")) | .name" \
+  | jq -r ".[] | select(.released==false) | select(.name | startswith(\"${MAJOR_MINOR_VERSION}\")) | .name" \
   | sort -V \
   | head -n1
