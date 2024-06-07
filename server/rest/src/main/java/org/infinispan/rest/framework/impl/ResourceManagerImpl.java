5f6f022254c3f5dfc1c52aec9ef8d30f095efb9d
Tristan Tarrant <ttarrant@redhat.com>
2024-06-07 15:37:11 +0200

    ISPN-16165 ResourceManagerImpl.lookupResource micro-optimization
    * Use simple loop to split path instead of stream (4x faster)
    * Use constants where possible to avoid extra allocations

--- server/rest/src/main/java/org/infinispan/rest/framework/impl/ResourceManagerImpl.java
+++ server/rest/src/main/java/org/infinispan/rest/framework/impl/ResourceManagerImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.infinispan.rest.framework.LookupResult;
 import org.infinispan.rest.framework.Method;
@@ -22,11 +21,12 @@ import io.netty.handler.codec.http.QueryStringDecoder;
 public class ResourceManagerImpl implements ResourceManager {
 
    private final static Log logger = LogFactory.getLog(ResourceManagerImpl.class, Log.class);
+   private static final StringPathItem ROOT = new StringPathItem("/");
 
    private final ResourceNode resourceTree;
 
    public ResourceManagerImpl() {
-      this.resourceTree = new ResourceNode(new StringPathItem("/"), null);
+      this.resourceTree = new ResourceNode(ROOT, null);
    }
 
    @Override
@@ -35,7 +35,7 @@ public class ResourceManagerImpl implements ResourceManager {
          Set<String> paths = invocation.paths();
          paths.forEach(path -> {
             validate(path);
-            List<PathItem> p = Arrays.stream(path.split("/")).filter(s -> !s.isEmpty()).map(PathItem::fromString).collect(Collectors.toList());
+            List<PathItem> p = Arrays.stream(path.split("/")).filter(s -> !s.isEmpty()).map(PathItem::fromString).toList();
             List<PathItem> pathWithCtx = new ArrayList<>();
             pathWithCtx.add(new StringPathItem(context));
             pathWithCtx.addAll(p);
@@ -52,8 +52,25 @@ public class ResourceManagerImpl implements ResourceManager {
 
    @Override
    public LookupResult lookupResource(Method method, String path, String action) {
-      List<PathItem> pathItems = Arrays.stream(path.replaceAll("//+", "/").split("/"))
-            .map(s -> s.isEmpty() ? "/" : s).map(QueryStringDecoder::decodeComponent).map(StringPathItem::new).collect(Collectors.toList());
+      List<PathItem> pathItems = new ArrayList<>();
+      final int l = path.length();
+      int s = 0, pos = 0;
+      while (pos < l) {
+         char ch = path.charAt(pos);
+         if (ch == '/') {
+            if (pos == 0) {
+               pathItems.add(ROOT);
+            } else if (pos > s) {
+               pathItems.add(new StringPathItem(QueryStringDecoder.decodeComponent(path.substring(s, pos))));
+            }
+            s = ++pos;
+         } else {
+            ++pos;
+         }
+      }
+      if (s < pos) {
+         pathItems.add(new StringPathItem(QueryStringDecoder.decodeComponent(path.substring(s))));
+      }
       return resourceTree.find(method, pathItems, action);
    }
 
