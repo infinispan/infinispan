23602029232a09c678b7ea721da90eb43d5feb0a
José Bolina <jbolina@redhat.com>
2024-06-28 10:15:41 -0300

    ISPN-16250 [RESP] Fixes HINCRBY and HINCRBYFLOAT
    * The issue is the underlying method parsing bytes to numbers.

--- server/resp/src/main/java/org/infinispan/server/resp/commands/hash/HINCRBY.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/hash/HINCRBY.java
@@ -2,6 +2,7 @@ package org.infinispan.server.resp.commands.hash;
 
 import java.util.List;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.BiConsumer;
 
 import org.infinispan.multimap.impl.EmbeddedMultimapPairCache;
@@ -9,6 +10,7 @@ import org.infinispan.server.resp.ByteBufPool;
 import org.infinispan.server.resp.Consumers;
 import org.infinispan.server.resp.Resp3Handler;
 import org.infinispan.server.resp.RespCommand;
+import org.infinispan.server.resp.RespErrorUtil;
 import org.infinispan.server.resp.RespRequestHandler;
 import org.infinispan.server.resp.commands.ArgumentUtils;
 import org.infinispan.server.resp.commands.Resp3Command;
@@ -38,13 +40,28 @@ public class HINCRBY extends RespCommand implements Resp3Command {
    public CompletionStage<RespRequestHandler> perform(Resp3Handler handler, ChannelHandlerContext ctx, List<byte[]> arguments) {
       EmbeddedMultimapPairCache<byte[], byte[], byte[]> multimap = handler.getHashMapMultimap();
       long delta = ArgumentUtils.toLong(arguments.get(2));
+      AtomicBoolean failed = new AtomicBoolean(false);
       CompletionStage<byte[]> cs = multimap.compute(arguments.get(0), arguments.get(1), (ignore, prev) -> {
          if (prev == null) return arguments.get(2);
 
          // This is supposed to throw when the `prev` is not a long.
          long prevLong = ArgumentUtils.toLong(prev);
-         return ArgumentUtils.toByteArray(prevLong + delta);
+         long result = prevLong + delta;
+
+         // Same check as Math#addExact(long, long).
+         // We bring it here to avoid throwing an exception.
+         if (((prevLong ^ result) & (delta ^ result)) < 0) {
+            failed.set(true);
+            return prev;
+         }
+         return ArgumentUtils.toByteArray(result);
       });
-      return handler.stageToReturn(cs, ctx, CONVERTER);
+      return handler.stageToReturn(cs, ctx, (res, alloc) -> {
+         if (failed.get()) {
+            RespErrorUtil.customError("increment or decrement would overflow", alloc);
+         } else {
+            CONVERTER.accept(res, alloc);
+         }
+      });
    }
 }
