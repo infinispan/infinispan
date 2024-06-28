25afcb970544db4aa3cb0d0bd2576091a11f1be4
rigazilla <vrigamon@redhat.com>
2024-06-28 16:28:13 +0200

    no flaky

--- commons/all/src/test/java/org/infinispan/commons/util/MutableIntSetTest.java
+++ commons/all/src/test/java/org/infinispan/commons/util/MutableIntSetTest.java
@@ -59,27 +59,6 @@ public class MutableIntSetTest {
     }
 
    @Test
-   public void testFlaky1() {
-        double r = Math.random();
-        if (r < 0.3) {
-            fail("oops");
-        }
-    }
-
-   @Test
-   public void testFlaky2() {
-      double r = Math.random();
-      if (r < 0.3) {
-            fail("oops2");
-      }
-   }
-
-   @Test
-   public void Fail() {
-      fail("always fails");
-   }
-
-   @Test
    public void testIsEmpty() {
       assertTrue(intSet.isEmpty());
       intSet.add(1);
