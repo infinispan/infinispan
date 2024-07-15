6198c2078cc337385431764201e6af0567bee8a9
José Bolina <jbolina@redhat.com>
2024-07-15 10:22:59 -0300

    ISPN-16296 Create matcher for glob patterns

---
+++ commons/all/src/main/java/org/infinispan/commons/util/GlobMatcher.java
@@ -0,0 +1,191 @@
+package org.infinispan.commons.util;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * A matcher for glob patterns.
+ *
+ * <p>
+ * Matches the glob pattern without transforming into a {@link java.util.regex.Pattern}. Otherwise, it is possible to
+ * convert with {@link GlobUtils#globToRegex(String)}.
+ * </p>
+ *
+ * @since 15.0
+ * @author José Bolina
+ * @see <a href="https://github.com/valkey-io/valkey/blob/dd4bd5065b6c36ff2dfcf41a49781e5f659f4d41/src/util.c#L57">Valkey implementation.</a>
+ */
+public final class GlobMatcher {
+
+   private GlobMatcher() { }
+
+   /**
+    * Checks whether the given string matches the glob pattern.
+    *
+    * <p>
+    * <b>Warning:</b> This methods does not accept multi-byte strings.
+    * </p>
+    *
+    * @param pattern: Glob pattern to check against.
+    * @param string: String to check for matching.
+    * @return <code>true</code> if a match is found, <code>false</code>, otherwise.
+    * @throws AssertionError: If the {@param string} is a multi-byte string.
+    */
+   public static boolean match(String pattern, String string) {
+      assertSingleByte(string);
+      return match(pattern.getBytes(StandardCharsets.US_ASCII), string.getBytes(StandardCharsets.US_ASCII));
+   }
+
+   /**
+    * Checks whether the given byte sequence matches the glob pattern describe as a byte sequence.
+    *
+    * <p>
+    * <b>Warning</b>: This method does not accept multi-byte characters.
+    * </p>
+    *
+    * @param pattern: The glob pattern represented as a byte sequence.
+    * @param string: The string represented as a byte sequence.
+    * @return <code>true</code> if a match is found, <code>false</code>, otherwise.
+    */
+   public static boolean match(byte[] pattern, byte[] string) {
+      assertSingleByte(string);
+      ByRef.Boolean skipLongerMatches = new ByRef.Boolean(false);
+      return match(pattern, 0, string, 0, skipLongerMatches);
+   }
+
+   private static boolean match(byte[] pattern, int patternPos, byte[] string, int stringPos, ByRef.Boolean skipLongerMatches) {
+      while (patternPos < pattern.length && stringPos < string.length) {
+         switch (pattern[patternPos]) {
+
+            // A star matches zero or more characters.
+            case '*':
+               // Consume sequence of star characters.
+               // A `**` allows for recursive searches.
+               while (patternPos < pattern.length - 1 && pattern[patternPos + 1] == '*') {
+                  patternPos++;
+               }
+
+               // If last character is star, we don't need to check a match.
+               if (patternPos == pattern.length) return true;
+
+               // Try to recursively match the * with the characters in the string.
+               // Utilize the ByRef.Boolean to avoid an explosion in the backtracking.
+               while (stringPos <= string.length) {
+                  if (match(pattern, patternPos + 1, string, stringPos, skipLongerMatches))
+                     return true;
+
+                  if (skipLongerMatches.get()) return false;
+
+                  stringPos++;
+               }
+
+               skipLongerMatches.set(true);
+               return false;
+
+            // Matches exactly one character.
+            case '?':
+               stringPos++;
+               break;
+
+            // Start a range of characters to match.
+            // The `^` character to negate the sequence in the range.
+            case '[':
+               patternPos++;
+
+               // Check for the negation character at the start of the range.
+               boolean notOp = pattern[patternPos] == '^';
+               if (notOp) {
+                  patternPos++;
+               }
+
+               // Check for matches or the end of range.
+               boolean match = false;
+               while (true) {
+                  // Consume the complete pattern without closing the range.
+                  if (patternPos == pattern.length) {
+                     patternPos--;
+                     break;
+                  }
+
+                  // Escape character in the string.
+                  if (patternPos < pattern.length - 1 && pattern[patternPos] == '\\') {
+                     patternPos++;
+
+                     if (pattern[patternPos] == string[stringPos])
+                        match = true;
+
+                  // End of the range.
+                  } else if (pattern[patternPos] == ']') {
+                     break;
+
+                  // Identifies a complete range between two characters. The dash represent the start and end.
+                  } else if (patternPos < pattern.length - 2 && pattern[patternPos + 1] == '-') {
+                     byte start = pattern[patternPos];
+                     byte end = pattern[patternPos + 2];
+                     byte c = string[stringPos];
+
+                     if (start > end) {
+                        byte t = start;
+                        start = end;
+                        end = t;
+                     }
+
+                     patternPos += 2;
+
+                     if (c >= start && c <= end) match = true;
+
+                  // Else, we check for a direct match between the characters in the range and the string.
+                  } else {
+                     match = pattern[patternPos] == string[stringPos];
+                  }
+
+                  patternPos++;
+               }
+
+               // If there was a negation in the range, we check for the inverse.
+               if (notOp) match = !match;
+               if (!match) return false;
+
+               stringPos++;
+               break;
+
+            // Escaping a character in the sequence. We consume the escape and fallthrough to check for a match.
+            case '\\':
+               if (patternPos < pattern.length - 1) patternPos++;
+
+               // Fallthrough.
+
+            // Verify if the characters match.
+            default:
+               if (pattern[patternPos] != string[stringPos])
+                  return false;
+
+               stringPos++;
+               break;
+         }
+
+         // If we consumed the string, but there is still missing characters on the pattern.
+         // The remaining characters need to be `*`.
+         patternPos++;
+         if (stringPos == string.length) {
+            while (patternPos < pattern.length && pattern[patternPos] == '*') {
+               patternPos++;
+            }
+            break;
+         }
+      }
+
+      // Only a match if we consumed both the pattern and the sequence.
+      return patternPos == pattern.length && stringPos == string.length;
+   }
+
+   private static void assertSingleByte(String s) {
+      byte[] bytes = s.getBytes();
+      assertSingleByte(bytes);
+   }
+
+   private static void assertSingleByte(byte[] bytes) {
+      for (byte b : bytes) {
+         assert (b & 0x80) == 0 : "Multi-byte character not accepted";
+      }
+   }
+}
---
+++ commons/all/src/test/java/org/infinispan/commons/util/GlobMatcherTest.java
@@ -0,0 +1,70 @@
+package org.infinispan.commons.util;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
+
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ThreadLocalRandom;
+
+import org.junit.Test;
+
+public class GlobMatcherTest {
+
+   @Test
+   public void testFuzzMatcher() {
+      byte[] str = new byte[32];
+      byte[] pat = new byte[32];
+
+      int cycles = 10_000_000;
+      while (cycles-- > 0) {
+         ThreadLocalRandom.current().nextBytes(str);
+         nextBytes(pat);
+         GlobMatcher.match(str, pat);
+      }
+   }
+
+   private void nextBytes(byte[] bytes) {
+      for (int i = 0, len = bytes.length; i < len; )
+         for (int rnd = ThreadLocalRandom.current().nextInt(0x80),
+              n = Math.min(len - i, Integer.SIZE/Byte.SIZE);
+              n-- > 0;)
+            bytes[i++] = (byte) rnd;
+   }
+
+   @Test
+   public void testMultiByteString() {
+      assertThrows(AssertionError.class, () -> GlobMatcher.match("p?tte*n", "José"));
+      assertThrows(AssertionError.class, () -> GlobMatcher.match("p?tte*n".getBytes(StandardCharsets.US_ASCII), "José".getBytes()));
+      assertThrows(AssertionError.class, () -> GlobMatcher.match("p?tte*n", "€10,00"));
+      assertThrows(AssertionError.class, () -> GlobMatcher.match("p?tte*n".getBytes(StandardCharsets.US_ASCII), "€10,00".getBytes()));
+
+      assertTrue(GlobMatcher.match("Jo?*", "Jose"));
+      assertTrue(GlobMatcher.match("$[1-9]*,[0-9][0-9]", "$101,23"));
+      assertTrue(GlobMatcher.match("$[1-9]*,[0-9][0-9]".getBytes(StandardCharsets.US_ASCII), "$101,23".getBytes(StandardCharsets.US_ASCII)));
+   }
+
+   @Test
+   public void testValidInputs() {
+      assertFalse(GlobMatcher.match("a*b", "aaa"));
+      assertFalse(GlobMatcher.match("a*a*b", "aaaa"));
+
+      // Hangs when parsing as regex.
+      assertFalse(GlobMatcher.match("a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*b", "a".repeat(55)));
+
+      assertTrue(GlobMatcher.match("a*a*b", "aaa7b"));
+      assertTrue(GlobMatcher.match("a*[0-9]b", "aH5b"));
+      assertTrue(GlobMatcher.match("a*[k\\-0-9]b", "aHk5b"));
+      assertTrue(GlobMatcher.match("a*[k\\-0-9]b", "aH-5b"));
+      assertFalse(GlobMatcher.match("a*[k\\-0-9]b", "aHb"));
+      assertFalse(GlobMatcher.match("a*[0-9]b", "aHRb"));
+      assertTrue(GlobMatcher.match("a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*a*b", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"));
+   }
+
+   @Test
+   public void testValidInputBytes() {
+      assertFalse(GlobMatcher.match(new byte[] { 'a', '*', 'b'}, new byte[] { 'a', 'a', 'a' }));
+      assertTrue(GlobMatcher.match(new byte[] { 'a', '*', 'b'}, new byte[] { 'a', 'a', 'a', 'b' }));
+      assertTrue(GlobMatcher.match(new byte[] { 'a', '*', '[', '0', '-', '9', ']', 'b'}, new byte[] { 'a', 'H', '5', 'b' }));
+   }
+}
