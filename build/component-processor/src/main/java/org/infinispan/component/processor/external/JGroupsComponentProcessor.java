10a429d007e31b9ad67b8ae52938a8af4096538c
Pedro Ruivo <pruivo@redhat.com>
2024-08-06 15:21:01 +0100

    ISPN-16347 Bump JGroups to 5.3.10.Final

--- build/component-processor/src/main/java/org/infinispan/component/processor/external/JGroupsComponentProcessor.java
+++ build/component-processor/src/main/java/org/infinispan/component/processor/external/JGroupsComponentProcessor.java
@@ -1,5 +1,16 @@
 package org.infinispan.component.processor.external;
 
+import org.infinispan.external.JGroupsProtocolComponent;
+import org.jgroups.annotations.ManagedAttribute;
+import org.jgroups.conf.ClassConfigurator;
+import org.jgroups.protocols.MsgStats;
+import org.jgroups.protocols.RED;
+import org.jgroups.protocols.TP;
+import org.jgroups.stack.Protocol;
+import org.jgroups.util.ThreadPool;
+import org.jgroups.util.Util;
+import org.kohsuke.MetaInfServices;
+
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.Processor;
 import javax.annotation.processing.RoundEnvironment;
@@ -10,24 +21,16 @@ import javax.lang.model.element.TypeElement;
 import javax.lang.model.type.DeclaredType;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.infinispan.external.JGroupsProtocolComponent;
-import org.jgroups.annotations.ManagedAttribute;
-import org.jgroups.conf.ClassConfigurator;
-import org.jgroups.protocols.FlowControl;
-import org.jgroups.protocols.MsgStats;
-import org.jgroups.protocols.RED;
-import org.jgroups.protocols.TP;
-import org.jgroups.stack.Protocol;
-import org.jgroups.util.ThreadPool;
-import org.kohsuke.MetaInfServices;
 
 /**
  * @since 14.0
@@ -35,31 +38,6 @@ import org.kohsuke.MetaInfServices;
 @SupportedAnnotationTypes("org.infinispan.external.JGroupsProtocolComponent")
 @MetaInfServices(Processor.class)
 public class JGroupsComponentProcessor extends AbstractProcessor {
-
-   private static final Map<String, String> FLOW_CONTROL_METRICS = Map.of(
-         "getNumberOfCreditRequestsReceived", "Number of credit requests received",
-         "getNumberOfCreditRequestsSent", "Number of credit requests sent",
-         "getNumberOfCreditResponsesReceived", "Number of credit responses received",
-         "getNumberOfCreditResponsesSent", "Number of credit responses sent"
-   );
-
-   private static final List<JgrpMetric> MSG_STATS_METRICS = List.of(
-         new JgrpMetric("getNumBatchesSent", "Number of message batches sent"),
-         new JgrpMetric("getNumBatchesReceived", "Number of message batches received"),
-         new JgrpMetric("getNumMcastBytesReceived", "Number of multicast bytes received"),
-         new JgrpMetric("getNumMcastBytesSent", "Number of multicast bytes sent"),
-         new JgrpMetric("getNumMcastMsgsReceived", "getNumMcastsReceived", "Number of multicast messages received"),
-         new JgrpMetric("getNumMcastMsgsSent", "getNumMcastsSent", "getNumMcastMsgsSent"),
-         new JgrpMetric("getNumSingleMsgsSent", "Number of single messages sent"),
-         new JgrpMetric("getNumUcastBytesReceived", "Number of unicast bytes received"),
-         new JgrpMetric("getNumUcastBytesSent", "Number of unicast bytes sent"),
-         new JgrpMetric("getNumUcastMsgsReceived", "getNumUcastsReceived", "Number of unicast messages received"),
-         new JgrpMetric("getNumUcastMsgsSent", "getNumUcastsSent", "Number of unicast messages sent")
-   );
-
-   private static final List<JgrpMetric> THREAD_POOL_METRICS = List.of(
-         new JgrpMetric("getNumRejectedMsgs", "numberOfRejectedMessages", "Number of dropped messages that were rejected by the thread pool")
-   );
 
    @Override
    public SourceVersion getSupportedSourceVersion() {
@@ -129,26 +107,19 @@ public class JGroupsComponentProcessor extends AbstractProcessor {
       if (protocol == null || !Protocol.class.isAssignableFrom(protocol) || Modifier.isAbstract(protocol.getModifiers())) {
          return;
       }
+      Map<String, JGroupsMetrics> methods = findAndWriteMetrics(protocol, null);
       AtomicBoolean hasAttributes = new AtomicBoolean(false);
 
-      for (Method method : protocol.getMethods()) {
-         ManagedAttribute annotation = method.getAnnotation(ManagedAttribute.class);
-         if (annotation != null && !Modifier.isStatic(method.getModifiers()) && method.getParameterCount() == 0 && isNumber(method.getReturnType())) {
-            if (hasAttributes.compareAndSet(false, true)) {
-               w.println("      attributes = new ArrayList<>();");
-            }
-            w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, %s::%s));%n",
-                  method.getName(), annotation.description().replace('"', '\''), protocol.getName(), method.getName());
+      if (!methods.isEmpty()) {
+         if (hasAttributes.compareAndSet(false, true)) {
+            w.println("      attributes = new ArrayList<>();");
          }
+         methods.values().forEach(m -> m.write(w));
       }
 
       if (TP.class.isAssignableFrom(protocol)) {
-         addThreadPoolComponent(protocol, w, hasAttributes);
-         addMsgStatsComponent(protocol, w, hasAttributes);
-      }
-
-      if (FlowControl.class.isAssignableFrom(protocol)) {
-         addFlowControlMetrics(protocol, w, hasAttributes);
+         addTPComponent("getThreadPool", protocol, ThreadPool.class, w, hasAttributes);
+         addTPComponent("getMessageStats", protocol, MsgStats.class, w, hasAttributes);
       }
 
       if (hasAttributes.get()) {
@@ -157,63 +128,68 @@ public class JGroupsComponentProcessor extends AbstractProcessor {
       }
    }
 
-   private static void addThreadPoolComponent(Class<?> protocol, PrintWriter w, AtomicBoolean hasAttributes) {
-      addTPComponent("getThreadPool", protocol, ThreadPool.class, w, hasAttributes);
-      if (hasAttributes.compareAndSet(false, true)) {
-         w.println("      attributes = new ArrayList<>();");
-      }
-      THREAD_POOL_METRICS.forEach(metric -> w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, ((Function<%s, Number>) p -> p.getThreadPool().%s())));%n",
-            metric.metric, metric.description.replace('"', '\''), protocol.getName(), metric.method));
-   }
-
-
-   private static void addMsgStatsComponent(Class<?> protocol, PrintWriter w, AtomicBoolean hasAttributes) {
-      addTPComponent("getMessageStats", protocol, MsgStats.class, w, hasAttributes);
-      if (hasAttributes.compareAndSet(false, true)) {
-         w.println("      attributes = new ArrayList<>();");
-      }
-      MSG_STATS_METRICS.forEach(metric -> w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, ((Function<%s, Number>) p -> p.getMessageStats().%s())));%n",
-            metric.metric, metric.description.replace('"', '\''), protocol.getName(), metric.method));
-   }
-
-   private static void addFlowControlMetrics(Class<?> protocol, PrintWriter w, AtomicBoolean hasAttributes) {
-      if (hasAttributes.compareAndSet(false, true)) {
-         w.println("      attributes = new ArrayList<>();");
-      }
-      FLOW_CONTROL_METRICS.forEach((method, description) -> w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, %s::%s));%n",
-            method, description.replace('"', '\''), protocol.getName(), method));
-   }
-
    private static boolean isNumber(Class<?> type) {
       return short.class == type || byte.class == type || long.class == type || int.class == type || float.class == type || double.class == type || Number.class.isAssignableFrom(type);
    }
 
    private static void addTPComponent(String getterMethodName, Class<?> protocol, Class<?> component, PrintWriter w, AtomicBoolean hasAttributes) {
-      for (Method method : component.getMethods()) {
-         ManagedAttribute annotation = method.getAnnotation(ManagedAttribute.class);
-         if (annotation != null && !Modifier.isStatic(method.getModifiers()) && method.getParameterCount() == 0 && isNumber(method.getReturnType())) {
-            if (hasAttributes.compareAndSet(false, true)) {
-               w.println("      attributes = new ArrayList<>();");
-            }
-            w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, ((Function<%s, Number>) p -> p.%s().%s())));%n",
-                  method.getName(), annotation.description().replace('"', '\''), protocol.getName(), getterMethodName, method.getName());
-         }
+      Map<String, JGroupsMetrics> methods = findAndWriteMetrics(component, protocol);
+      if (methods.isEmpty()) {
+         return;
       }
+      if (hasAttributes.compareAndSet(false, true)) {
+         w.println("      attributes = new ArrayList<>();");
+      }
+      methods.values().forEach(m -> m.writeComponent(w, getterMethodName));
+
    }
 
-   private static class JgrpMetric {
-      final String metric;
-      final String method;
-      final String description;
-
-      JgrpMetric(String metric, String method, String description) {
-         this.metric = metric;
-         this.method = method;
-         this.description = description;
+   private static Map<String, JGroupsMetrics> findAndWriteMetrics(Class<?> clazz, Class<?> rootClass) {
+      Map<String, JGroupsMetrics> metrics = new HashMap<>();
+      String className = rootClass == null ? clazz.getName() : rootClass.getName();
+      for (Method method : clazz.getMethods()) {
+         ManagedAttribute annotation = method.getAnnotation(ManagedAttribute.class);
+         if (annotation == null || isMethodInvalid(method)) {
+            continue;
+         }
+         metrics.put(method.getName(), new JGroupsMetrics(className, method.getName(), annotation.description()));
       }
 
-      JgrpMetric(String metric, String description) {
-         this(metric, metric, description);
+      for (Field field : clazz.getDeclaredFields()) {
+         ManagedAttribute annotation = field.getAnnotation(ManagedAttribute.class);
+         if (annotation == null || Modifier.isStatic(field.getModifiers())) {
+            continue; // skip
+         }
+         String methodName = Util.attributeNameToMethodName(field.getName());
+         Method method = Util.findMethod(clazz, List.of(methodName.substring(0, 1).toLowerCase() + methodName.substring(1), "get" + methodName));
+         if (isMethodInvalid(method)) {
+            continue;
+         }
+         metrics.put(method.getName(), new JGroupsMetrics(className, method.getName(), annotation.description()));
+      }
+      return metrics;
+   }
+
+   private static boolean isMethodInvalid(Method method) {
+      return method == null || !Modifier.isPublic(method.getModifiers()) || Modifier.isStatic(method.getModifiers()) || method.getParameterCount() != 0 || !isNumber(method.getReturnType());
+   }
+
+   private record JGroupsMetrics(String className, String name, String description) {
+
+      private JGroupsMetrics(String className, String name, String description) {
+         this.className = Objects.requireNonNull(className);
+         this.name = Objects.requireNonNull(name);
+         this.description = Objects.requireNonNull(description).replace('"', '\'');
+      }
+
+      void write(PrintWriter w) {
+         w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, %s::%s));%n",
+                 name, description, className, name);
+      }
+
+      void writeComponent(PrintWriter w, String componentGetterMethod) {
+         w.printf("      attributes.add(new GaugeMetricInfo<>(\"%s\", \"%s\", null, ((Function<%s, Number>) p -> p.%s().%s())));%n",
+                 name, description, className, componentGetterMethod, name);
       }
    }
 }
