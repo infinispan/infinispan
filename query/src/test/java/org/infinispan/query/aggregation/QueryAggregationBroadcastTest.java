363602f57c2252e6613b128fd1e9edc56fecf242
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-08-21 12:01:12 +0200

    ISPN-16349 Reduce put-all chuck size from 1000 to 100

--- query/src/test/java/org/infinispan/query/aggregation/QueryAggregationBroadcastTest.java
+++ query/src/test/java/org/infinispan/query/aggregation/QueryAggregationBroadcastTest.java
@@ -4,8 +4,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.infinispan.configuration.cache.IndexStorage.LOCAL_HEAP;
 import static org.infinispan.query.aggregation.QueryAggregationCountTest.AGGREGATION_RESULT;
 import static org.infinispan.query.aggregation.QueryAggregationCountTest.CHUNK_SIZE;
+import static org.infinispan.query.aggregation.QueryAggregationCountTest.END_DAY;
+import static org.infinispan.query.aggregation.QueryAggregationCountTest.FULL_AGGREGATION_RESULT;
 import static org.infinispan.query.aggregation.QueryAggregationCountTest.NUMBER_OF_DAYS;
 import static org.infinispan.query.aggregation.QueryAggregationCountTest.REV_AGGREGATION_RESULT;
+import static org.infinispan.query.aggregation.QueryAggregationCountTest.START_DAY;
 import static org.infinispan.query.aggregation.QueryAggregationCountTest.chunk;
 
 import java.util.Optional;
@@ -45,21 +48,40 @@ public class QueryAggregationBroadcastTest extends MultipleCacheManagersTest {
 
       Query<Object[]> query;
 
-      query = cache.query("select status, count(code) from org.infinispan.query.model.Sale where day = :day group by status order by status");
-      query.setParameter("day", NUMBER_OF_DAYS / 2);
+      query = cache.query("select status, count(code) from org.infinispan.query.model.Sale where day >= :start and day <= :end group by status order by status");
+      query.setParameter("start", START_DAY);
+      query.setParameter("end", END_DAY);
       assertThat(query.list()).containsExactly(AGGREGATION_RESULT);
 
-      query = cache.query("select count(code), status from org.infinispan.query.model.Sale where day = :day group by status order by status");
-      query.setParameter("day", NUMBER_OF_DAYS / 2);
+      query = cache.query("select count(code), status from org.infinispan.query.model.Sale where day >= :start and day <= :end group by status order by status");
+      query.setParameter("start", START_DAY);
+      query.setParameter("end", END_DAY);
       assertThat(query.list()).containsExactly(REV_AGGREGATION_RESULT);
 
-      query = cache.query("select status, count(code) from org.infinispan.query.model.Sale where day = :day group by status");
-      query.setParameter("day", NUMBER_OF_DAYS / 2);
+      query = cache.query("select status, count(code) from org.infinispan.query.model.Sale where day >= :start and day <= :end group by status");
+      query.setParameter("start", START_DAY);
+      query.setParameter("end", END_DAY);
       assertThat(query.list()).containsExactlyInAnyOrder(AGGREGATION_RESULT);
 
       query = cache.query("select status, count(code) from org.infinispan.query.model.Sale group by status");
       Optional<Integer> totalNotNullItems = query.list().stream()
             .map(objects -> ((Long) objects[1]).intValue()).reduce(Integer::sum);
       assertThat(totalNotNullItems).hasValue(CHUNK_SIZE * NUMBER_OF_DAYS);
+
+      // alias
+      query = cache.query("select s.status, count(s.code) from org.infinispan.query.model.Sale s where s.day >= :start and s.day <= :end group by s.status order by s.status");
+      query.setParameter("start", START_DAY);
+      query.setParameter("end", END_DAY);
+      assertThat(query.list()).containsExactly(AGGREGATION_RESULT);
+      // alias && count on entity
+      query = cache.query("select s.status, count(s) from org.infinispan.query.model.Sale s where s.day >= :start and s.day <= :end group by s.status order by s.status");
+      query.setParameter("start", START_DAY);
+      query.setParameter("end", END_DAY);
+      assertThat(query.list()).containsExactly(FULL_AGGREGATION_RESULT);
+      // no alias && count on entity
+      query = cache.query("select status, count(*) from org.infinispan.query.model.Sale where day >= :start and day <= :end group by status");
+      query.setParameter("start", START_DAY);
+      query.setParameter("end", END_DAY);
+      assertThat(query.list()).containsExactly(FULL_AGGREGATION_RESULT);
    }
 }
