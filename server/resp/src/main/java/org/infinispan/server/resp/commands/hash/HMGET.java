6f5e971eb57d7af079e9d669186b6f9a54fbcd0f
José Bolina <jbolina@redhat.com>
2024-10-03 18:02:06 -0300

    ISPN-16329 HMGET wrap byte array keys

--- server/resp/src/main/java/org/infinispan/server/resp/commands/hash/HMGET.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/hash/HMGET.java
@@ -2,9 +2,12 @@ package org.infinispan.server.resp.commands.hash;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletionStage;
 
+import org.infinispan.commons.marshall.WrappedByteArray;
 import org.infinispan.commons.util.Util;
 import org.infinispan.multimap.impl.EmbeddedMultimapPairCache;
 import org.infinispan.server.resp.Resp3Handler;
@@ -36,13 +39,19 @@ public class HMGET extends RespCommand implements Resp3Command {
    public CompletionStage<RespRequestHandler> perform(Resp3Handler handler, ChannelHandlerContext ctx, List<byte[]> arguments) {
       EmbeddedMultimapPairCache<byte[], byte[], byte[]> multimap = handler.getHashMapMultimap();
       CompletionStage<Collection<byte[]>> cs = multimap.get(arguments.get(0), arguments.subList(1, arguments.size()).toArray(Util.EMPTY_BYTE_ARRAY_ARRAY))
+            .thenApply(this::wrapKeys)
             .thenApply(entries -> {
                List<byte[]> result = new ArrayList<>(arguments.size() - 1);
                for (byte[] argument : arguments.subList(1, arguments.size())) {
-                  result.add(entries.get(argument));
+                  result.add(entries.get(new WrappedByteArray(argument)));
                }
                return result;
             });
       return handler.stageToReturn(cs, ctx, Resp3Response.ARRAY_BULK_STRING);
+   }
+
+   private Map<WrappedByteArray, byte[]> wrapKeys(Map<byte[], byte[]> original) {
+      return original.entrySet().stream()
+            .collect(HashMap::new, (m, v) -> m.put(new WrappedByteArray(v.getKey()), v.getValue()), HashMap::putAll);
    }
 }
