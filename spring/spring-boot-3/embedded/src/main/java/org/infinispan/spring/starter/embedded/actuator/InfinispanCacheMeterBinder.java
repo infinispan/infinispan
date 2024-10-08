c3f961a931d7e5857a06909b7718b8ed9ab4c0d8
Tristan Tarrant <ttarrant@redhat.com>
2024-10-08 18:41:17 +0200

    ISPN-16644 Fix flaky InfinispanCacheMetricBinderTest#dereferencedCacheIsGarbageCollected

--- spring/spring-boot-3/embedded/src/main/java/org/infinispan/spring/starter/embedded/actuator/InfinispanCacheMeterBinder.java
+++ spring/spring-boot-3/embedded/src/main/java/org/infinispan/spring/starter/embedded/actuator/InfinispanCacheMeterBinder.java
@@ -1,12 +1,13 @@
 package org.infinispan.spring.starter.embedded.actuator;
 
+import java.util.concurrent.TimeUnit;
+
+import org.infinispan.Cache;
+
 import io.micrometer.core.instrument.Gauge;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Tag;
 import io.micrometer.core.instrument.binder.cache.CacheMeterBinder;
-import org.infinispan.Cache;
-
-import java.util.concurrent.TimeUnit;
 
 /**
  * Implements {@link CacheMeterBinder} to expose Infinispan embedded metrics
@@ -14,61 +15,58 @@ import java.util.concurrent.TimeUnit;
  * @author Katia Aresti, karesti@redhat.com
  * @since 2.1
  */
-public class InfinispanCacheMeterBinder extends CacheMeterBinder {
+public class InfinispanCacheMeterBinder<K, V> extends CacheMeterBinder<Cache<K, V>> {
 
-   private final Cache cache;
-
-   public InfinispanCacheMeterBinder(Cache cache, Iterable<Tag> tags) {
+   public InfinispanCacheMeterBinder(Cache<K, V> cache, Iterable<Tag> tags) {
       super(cache, cache.getName(), tags);
-      this.cache = cache;
    }
 
    @Override
    protected Long size() {
-      if (cache == null) return 0L;
+      if (getCache() == null) return 0L;
 
-      return cache.getAdvancedCache().getStats().getApproximateEntriesInMemory();
+      return getCache().getAdvancedCache().getStats().getApproximateEntriesInMemory();
    }
 
    @Override
    protected long hitCount() {
-      if (cache == null) return 0L;
+      if (getCache() == null) return 0L;
 
-      return cache.getAdvancedCache().getStats().getHits();
+      return getCache().getAdvancedCache().getStats().getHits();
    }
 
    @Override
    protected Long missCount() {
-      if (cache == null) return 0L;
+      if (getCache() == null) return 0L;
 
-      return cache.getAdvancedCache().getStats().getMisses();
+      return getCache().getAdvancedCache().getStats().getMisses();
    }
 
    @Override
    protected Long evictionCount() {
-      if (cache == null) return 0L;
+      if (getCache() == null) return 0L;
 
-      return cache.getAdvancedCache().getStats().getEvictions();
+      return getCache().getAdvancedCache().getStats().getEvictions();
    }
 
    @Override
    protected long putCount() {
-      if (cache == null) return 0L;
+      if (getCache() == null) return 0L;
 
-      return cache.getAdvancedCache().getStats().getStores();
+      return getCache().getAdvancedCache().getStats().getStores();
    }
 
    @Override
    protected void bindImplementationSpecificMetrics(MeterRegistry registry) {
-      if (cache == null) return;
+      if (getCache() == null) return;
 
-      Gauge.builder("cache.start", cache, cache -> cache.getAdvancedCache().getStats().getTimeSinceStart())
+      Gauge.builder("cache.start", getCache(), cache -> cache.getAdvancedCache().getStats().getTimeSinceStart())
             .baseUnit(TimeUnit.SECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Time elapsed since start")
             .register(registry);
 
-      Gauge.builder("cache.reset", cache, cache -> cache.getAdvancedCache().getStats().getTimeSinceReset())
+      Gauge.builder("cache.reset", getCache(), cache -> cache.getAdvancedCache().getStats().getTimeSinceReset())
             .baseUnit(TimeUnit.SECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Time elapsed since the last statistics reset")
@@ -80,38 +78,40 @@ public class InfinispanCacheMeterBinder extends CacheMeterBinder {
    }
 
    private void memory(MeterRegistry registry) {
-      Gauge.builder("cache.memory.size", cache, cache -> cache.getAdvancedCache().getStats().getCurrentNumberOfEntriesInMemory())
+      Cache<K, V> cache = getCache();
+      Gauge.builder("cache.memory.size", cache, c -> c.getAdvancedCache().getStats().getCurrentNumberOfEntriesInMemory())
             .tags(getTagsWithCacheName())
             .description("Approximate number of entries in the cache, excluding passivated entries")
             .register(registry);
 
-      if (cache.getCacheConfiguration().memory().evictionStrategy().isEnabled()) {
-         Gauge.builder("cache.memory.used", cache, cache -> cache.getAdvancedCache().getStats().getDataMemoryUsed())
+      if (cache.getCacheConfiguration().memory().isEvictionEnabled()) {
+         Gauge.builder("cache.memory.used", cache, c -> c.getAdvancedCache().getStats().getDataMemoryUsed())
                .tags(getTagsWithCacheName())
                .description("Provides how much memory the current eviction algorithm estimates is in use for data")
                .register(registry);
       }
 
-      Gauge.builder("cache.memory.offHeap", cache, cache -> cache.getAdvancedCache().getStats().getOffHeapMemoryUsed())
+      Gauge.builder("cache.memory.offHeap", cache, c -> c.getAdvancedCache().getStats().getOffHeapMemoryUsed())
             .tags(getTagsWithCacheName())
             .description("The amount of off-heap memory used by this cache")
             .register(registry);
    }
 
    private void averages(MeterRegistry registry) {
-      Gauge.builder("cache.puts.latency", cache, cache -> cache.getAdvancedCache().getStats().getAverageWriteTime())
+      Cache<K, V> cache = getCache();
+      Gauge.builder("cache.puts.latency", cache, c -> c.getAdvancedCache().getStats().getAverageWriteTime())
             .baseUnit(TimeUnit.MILLISECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Cache puts")
             .register(registry);
 
-      Gauge.builder("cache.gets.latency", cache, cache -> cache.getAdvancedCache().getStats().getAverageReadTime())
+      Gauge.builder("cache.gets.latency", cache, c -> c.getAdvancedCache().getStats().getAverageReadTime())
             .baseUnit(TimeUnit.MILLISECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Cache gets")
             .register(registry);
 
-      Gauge.builder("cache.removes.latency", cache, cache -> cache.getAdvancedCache().getStats().getAverageRemoveTime())
+      Gauge.builder("cache.removes.latency", cache, c -> c.getAdvancedCache().getStats().getAverageRemoveTime())
             .baseUnit(TimeUnit.MILLISECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Cache removes")
@@ -119,19 +119,20 @@ public class InfinispanCacheMeterBinder extends CacheMeterBinder {
    }
 
    private void averagesNanos(MeterRegistry registry) {
-      Gauge.builder("cache.puts.nlatency", cache, cache -> cache.getAdvancedCache().getStats().getAverageWriteTimeNanos())
+      Cache<K, V> cache = getCache();
+      Gauge.builder("cache.puts.nlatency", cache, c -> c.getAdvancedCache().getStats().getAverageWriteTimeNanos())
             .baseUnit(TimeUnit.NANOSECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Cache puts in nanos")
             .register(registry);
 
-      Gauge.builder("cache.gets.nlatency", cache, cache -> cache.getAdvancedCache().getStats().getAverageReadTimeNanos())
+      Gauge.builder("cache.gets.nlatency", cache, c -> c.getAdvancedCache().getStats().getAverageReadTimeNanos())
             .baseUnit(TimeUnit.NANOSECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Cache gets in nanos")
             .register(registry);
 
-      Gauge.builder("cache.removes.nlatency", cache, cache -> cache.getAdvancedCache().getStats().getAverageRemoveTimeNanos())
+      Gauge.builder("cache.removes.nlatency", cache, c -> c.getAdvancedCache().getStats().getAverageRemoveTimeNanos())
             .baseUnit(TimeUnit.NANOSECONDS.name())
             .tags(getTagsWithCacheName())
             .description("Cache removes in nanos")
