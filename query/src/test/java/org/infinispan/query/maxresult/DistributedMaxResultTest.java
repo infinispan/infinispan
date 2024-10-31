817e037ee777a46493b6656c4ee7b520003aba13
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2024-10-31 12:59:23 +0100

    ISPN-16808 Do not limit delete cq command
    Co-authored-by: Pedro Ruivo <pruivo@redhat.com>
    
    Pedro found the issue. I added the test.

--- query/src/test/java/org/infinispan/query/maxresult/DistributedMaxResultTest.java
+++ query/src/test/java/org/infinispan/query/maxresult/DistributedMaxResultTest.java
@@ -3,11 +3,15 @@ package org.infinispan.query.maxresult;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.infinispan.configuration.cache.IndexStorage.LOCAL_HEAP;
 
+import java.util.List;
+
 import org.infinispan.Cache;
 import org.infinispan.commons.api.query.Query;
 import org.infinispan.commons.api.query.QueryResult;
+import org.infinispan.commons.test.annotation.TestForIssue;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.query.model.Developer;
 import org.infinispan.query.model.Game;
 import org.infinispan.test.MultipleCacheManagersTest;
 import org.testng.annotations.Test;
@@ -16,6 +20,7 @@ import org.testng.annotations.Test;
 public class DistributedMaxResultTest extends MultipleCacheManagersTest {
 
    private Cache<Integer, Game> node1;
+   private Cache<Integer, Developer> node2;
 
    @Override
    protected void createCacheManagers() throws Throwable {
@@ -27,13 +32,15 @@ public class DistributedMaxResultTest extends MultipleCacheManagersTest {
             .indexing().enable()
                .storage(LOCAL_HEAP)
                .addIndexedEntity("org.infinispan.query.model.Game")
+               .addIndexedEntity(Developer.class)
             .query().defaultMaxResults(50);
 
       createClusteredCaches(2, config);
       node1 = cache(0);
-      cache(1);
+      node2 = cache(1);
    }
 
+   @Test
    public void executeSmokeTest() {
       for (int i = 1; i <= 110; i++) {
          node1.put(i, new Game("Game " + i, "This is the game " + i + "# of a series"));
@@ -51,5 +58,24 @@ public class DistributedMaxResultTest extends MultipleCacheManagersTest {
 
       assertThat(result.count().value()).isEqualTo(110);
       assertThat(result.list()).hasSize(110);
+   }
+
+   @Test
+   @TestForIssue(jiraKey = "ISPN-16808")
+   public void deleteMoreThan100Elements() {
+      for (int i=0; i<60; i++) {
+         Developer developer = new Developer("james", "james@blablabla.edu", "Hibernate developer", 2004, "Hibernate developer");
+         node2.put(i, developer);
+      }
+
+      Query<Object[]> query = node2.query("select count(d) from org.infinispan.query.model.Developer d");
+      List<Object[]> result = query.list();
+      assertThat(result).extracting(item -> item[0]).containsExactly(60L);
+
+      node2.query("delete from org.infinispan.query.model.Developer").execute();
+
+      query = node2.query("select count(d) from org.infinispan.query.model.Developer d");
+      result = query.list();
+      assertThat(result).extracting(item -> item[0]).containsExactly(0L);
    }
 }
