430134e72ba81c496a06e835857838b8af69fd3b
rigazilla <vrigamon@redhat.com>
2024-11-21 10:56:04 +0100

    jsondoc

---
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/json/JsonDoc.java
@@ -0,0 +1,20 @@
+package org.infinispan.server.resp.commands.json;
+
+import org.infinispan.commons.marshall.ProtoStreamTypeIds;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
+import org.infinispan.protostream.annotations.ProtoTypeId;
+
+@ProtoTypeId(ProtoStreamTypeIds.RESP_JSON_DOC)
+public class JsonDoc {
+   byte[] bytesDocument;
+   @ProtoFactory
+   JsonDoc(byte[] bytesDoc) {
+      this.bytesDocument=bytesDoc;
+   }
+
+   @ProtoField(number = 1)
+   byte[] bytesDocument() {
+      return bytesDocument;
+   }
+}
