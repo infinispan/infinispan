573c8fe9d5eb866e3e156b7959cb586c5c50d0a8
rigazilla <vrigamon@redhat.com>
2024-11-29 17:34:53 +0100

    Create issue on GH instead of Jira for flaky tests. Fixes #13238

---
+++ bin/gh/common.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e -o pipefail
+shopt -s expand_aliases
+
+function requiredEnv() {
+  for ENV in $@; do
+      if [ -z "${!ENV}" ]; then
+        echo "${ENV} variable must be set"
+        exit 1
+      fi
+  done
+}
