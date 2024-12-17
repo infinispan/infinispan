3d9891c1a34e71f7e1a7ef42231108d0a60bbbc2
Ryan Emerson <remerson@redhat.com>
2024-12-17 15:26:34 +0000

    Fix query failures

--- query/src/main/java/org/infinispan/query/impl/protostream/adapters/LuceneTopDocsAdapter.java
+++ query/src/main/java/org/infinispan/query/impl/protostream/adapters/LuceneTopDocsAdapter.java
@@ -1,9 +1,13 @@
 package org.infinispan.query.impl.protostream.adapters;
 
+import java.util.stream.Stream;
+
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.search.TotalHits;
 import org.infinispan.commons.marshall.ProtoStreamTypeIds;
+import org.infinispan.marshall.protostream.impl.WrappedMessages;
+import org.infinispan.protostream.WrappedMessage;
 import org.infinispan.protostream.annotations.ProtoAdapter;
 import org.infinispan.protostream.annotations.ProtoFactory;
 import org.infinispan.protostream.annotations.ProtoField;
@@ -13,8 +17,9 @@ import org.infinispan.protostream.annotations.ProtoTypeId;
 @ProtoTypeId(ProtoStreamTypeIds.LUCENE_TOP_DOCS)
 public class LuceneTopDocsAdapter {
    @ProtoFactory
-   static TopDocs protoFactory(long totalHits, ScoreDoc[] scoreDocs) {
-      return new TopDocs(new TotalHits(totalHits, TotalHits.Relation.EQUAL_TO), scoreDocs);
+   static TopDocs protoFactory(long totalHits, WrappedMessage[] scoreDocs) {
+      ScoreDoc[] docs = Stream.of(scoreDocs).map(WrappedMessages::unwrap).toArray(ScoreDoc[]::new);
+      return new TopDocs(new TotalHits(totalHits, TotalHits.Relation.EQUAL_TO), docs);
    }
 
    @ProtoField(value = 1, defaultValue = "0")
@@ -23,7 +28,8 @@ public class LuceneTopDocsAdapter {
    }
 
    @ProtoField(2)
-   ScoreDoc[] getScoreDocs(TopDocs topDocs) {
-      return topDocs.scoreDocs;
+   WrappedMessage[] getScoreDocs(TopDocs topDocs) {
+      // We must use a WrappedMessage here to allow for inheritance as this can either be FieldDoc or ScoreDoc
+      return Stream.of(topDocs.scoreDocs).map(WrappedMessages::orElseNull).toArray(WrappedMessage[]::new);
    }
 }
