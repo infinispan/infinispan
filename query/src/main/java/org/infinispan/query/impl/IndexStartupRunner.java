24bb6e282243b6f831288de6d1adbe1fb91ecc59
Fabio Massimo Ercoli <fabiomassimo.ercoli@gmail.com>
2025-01-10 09:44:37 +0100

    [#13284] Reindex on recovery of crashed node
    In case of shared cache storage with max memory

--- query/src/main/java/org/infinispan/query/impl/IndexStartupRunner.java
+++ query/src/main/java/org/infinispan/query/impl/IndexStartupRunner.java
@@ -1,5 +1,7 @@
 package org.infinispan.query.impl;
 
+import java.util.List;
+
 import org.infinispan.configuration.cache.Configuration;
 import org.infinispan.configuration.cache.IndexStartupMode;
 import org.infinispan.configuration.cache.IndexStorage;
@@ -28,10 +30,10 @@ public final class IndexStartupRunner {
 
    private static IndexStartupMode computeFinalMode(Configuration configuration) {
       IndexStartupMode startupMode = configuration.indexing().startupMode();
-      boolean dataIsVolatile = configuration.persistence().stores().stream()
-            .allMatch(StoreConfiguration::purgeOnStartup);
+      DataKind dataKind = computeDataKind(configuration);
       boolean indexesAreVolatile = IndexStorage.LOCAL_HEAP.equals(configuration.indexing().storage());
-      if (dataIsVolatile && !indexesAreVolatile) {
+
+      if (DataKind.VOLATILE.equals(dataKind) && !indexesAreVolatile) {
          switch (startupMode) {
             case AUTO, PURGE,
                  // reindex is equivalent to purge, since there is no data in the caches
@@ -44,7 +46,8 @@ public final class IndexStartupRunner {
             }
          }
       }
-      if (!dataIsVolatile && indexesAreVolatile) {
+
+      if (DataKind.PERSISTENT.equals(dataKind) && indexesAreVolatile) {
          switch (startupMode) {
             case AUTO, REINDEX -> {
                return IndexStartupMode.REINDEX;
@@ -55,6 +58,48 @@ public final class IndexStartupRunner {
             }
          }
       }
+
+      if (DataKind.SHARED_STORE.equals(dataKind) && !indexesAreVolatile &&
+            IndexStartupMode.AUTO.equals(startupMode)) {
+         // @fax4ever: I'm against this. In my opinion run always a reindex, even if
+         // configuration.memory().isEvictionEnabled() is false, is wrong.
+         // This may penalize the average user using a shared cache store that does not do any eviction!
+         // But since the others of the team want this at all cost, I give up in the spirit of collaboration.
+         return IndexStartupMode.REINDEX;
+      }
+
       return (IndexStartupMode.AUTO.equals(startupMode)) ? IndexStartupMode.NONE : startupMode;
+   }
+
+   private enum DataKind {
+      VOLATILE, PERSISTENT, SHARED_STORE
+   }
+
+   private static DataKind computeDataKind(Configuration configuration) {
+      List<StoreConfiguration> cacheStores = configuration.persistence().stores();
+      if (cacheStores.isEmpty()) {
+         return DataKind.VOLATILE;
+      }
+
+      boolean sharedStore = false;
+      for (StoreConfiguration cacheStore : cacheStores) {
+         if (cacheStore.purgeOnStartup()) {
+            continue;
+         }
+         if (cacheStore.shared()) {
+            // @fax4ever: I'm against this. In my opinion run always a reindex, even if
+            // configuration.memory().isEvictionEnabled() is false, is wrong.
+            // This may penalize the average user using a shared cache store that does not do any eviction!
+            // But since the others of the team want this at all cost, I give up in the spirit of collaboration.
+            sharedStore = true;
+         } else {
+            return DataKind.PERSISTENT;
+         }
+      }
+
+      if (sharedStore) {
+         return DataKind.SHARED_STORE;
+      }
+      return DataKind.VOLATILE;
    }
 }
