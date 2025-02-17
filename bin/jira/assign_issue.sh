f27e66dc541fe92bf13f19c003b67a75ff02b1fb
Ryan Emerson <remerson@redhat.com>
2025-02-17 15:53:17 +0000

    [#14064] Remove updateJira jobs from GitHub Action workflow

--- bin/jira/assign_issue.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# A script to update a Jira issues assignee
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-source "${SCRIPT_DIR}/common.sh"
-
-requiredEnv TOKEN ISSUE_KEY ASSIGNEE
-
-curl -X PUT ${API_URL}/issue/${ISSUE_KEY}/assignee --data "{\"name\":\"${ASSIGNEE}\"}"
