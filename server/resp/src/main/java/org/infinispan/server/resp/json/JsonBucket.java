0f7f8abe55c25c2ae782cf2f8b7a4033751420e2
Ryan Emerson <remerson@redhat.com>
2025-02-18 14:21:34 +0000

    [#14093] JsonBucket should not be included in the PersistenceContextInitializer

--- server/resp/src/main/java/org/infinispan/server/resp/json/JsonBucket.java
+++ server/resp/src/main/java/org/infinispan/server/resp/json/JsonBucket.java
@@ -1,8 +1,13 @@
 package org.infinispan.server.resp.json;
 
-import org.infinispan.commons.marshall.ProtoStreamTypeIds;
-import org.infinispan.protostream.annotations.ProtoField;
-import org.infinispan.protostream.annotations.ProtoTypeId;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
+import java.util.Collections;
+import java.util.Set;
+
+import org.infinispan.commons.marshall.AdvancedExternalizer;
+import org.infinispan.server.resp.ExternalizerIds;
 
 /**
  * Bucket used to store Set data type.
@@ -10,12 +15,34 @@ import org.infinispan.protostream.annotations.ProtoTypeId;
  * @author Vittorio Rigamonti
  * @since 15.0
  */
-@ProtoTypeId(ProtoStreamTypeIds.RESP_JSON_BUCKET)
 public record JsonBucket(byte[] value) {
 
-   @ProtoField(number = 1)
+   public static final AdvancedExternalizer<JsonBucket> EXTERNALIZER = new JsonBucket.Externalizer();
+
    public byte[] value() {
       return value;
    }
 
+   private static class Externalizer implements AdvancedExternalizer<JsonBucket> {
+
+      @Override
+      public void writeObject(ObjectOutput output, JsonBucket object) throws IOException {
+         JSONUtil.writeBytes(output, object.value);
+      }
+
+      @Override
+      public JsonBucket readObject(ObjectInput input) throws IOException {
+         return new JsonBucket(JSONUtil.readBytes(input));
+      }
+
+      @Override
+      public Set<Class<? extends JsonBucket>> getTypeClasses() {
+         return Collections.singleton(JsonBucket.class);
+      }
+
+      @Override
+      public Integer getId() {
+         return ExternalizerIds.JSON_BUCKET;
+      }
+   }
 }
