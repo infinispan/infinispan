8dedf89514f08399147285ebad15d7cec5c82f43
Tristan Tarrant <ttarrant@redhat.com>
2025-02-24 18:50:11 +0100

    [#14160] Separate upgrade and changes guides

--- documentation/src/main/asciidoc/js/js.js
+++ documentation/src/main/asciidoc/js/js.js
@@ -2,31 +2,48 @@ $(document).ready(function() {
     var prefix = "/docs/"
     var path = document.location.pathname;
     var version = path.substring(prefix.length, path.indexOf("/", prefix.length));
+
     $('#toctitle').before('<select id="dchooser"></select>');
     $('#dchooser').append('<option>Documentation index</option>');
+    $('#dchooser').append('<optgroup label="Get Started">');
     $('#dchooser').append('<option value="/titles/getting_started/getting_started.html">Getting started</option>');
-    $('#dchooser').append('<option value="/titles/tuning/tuning.html">Deployment planning and tuning</option>');
+    $('#dchooser').append('</optgroup>');
+    $('#dchooser').append('<optgroup label="Developers">');
     $('#dchooser').append('<option value="/titles/configuring/configuring.html">Configuring caches</option>');
     $('#dchooser').append('<option value="/titles/encoding/encoding.html">Encoding and marshalling</option>');
     $('#dchooser').append('<option value="/titles/query/query.html">Querying caches</option>');
-    $('#dchooser').append('<option value="/titles/xsite/xsite.html">Cross-site replication</option>');
+    $('#dchooser').append('<option value="/titles/security/security.html">Security guide</option>');
     $('#dchooser').append('<option value="/titles/embedding/embedding.html">Embedding Infinispan caches</option>');
-    $('#dchooser').append('<option value="/titles/server/server.html">Guide to Infinispan Server</option>');
-    $('#dchooser').append('<option value="/titles/cli/cli.html">Command Line Interface (CLI)</option>');
     $('#dchooser').append('<option value="/titles/rest/rest.html">REST API</option>');
     $('#dchooser').append('<option value="/titles/hotrod_java/hotrod_java.html">Hot Rod Java clients</option>');
     $('#dchooser').append('<option value="/titles/hotrod_protocol/hotrod_protocol.html">Hot Rod protocol reference</option>');
     $('#dchooser').append('<option value="/titles/memcached/memcached.html">Memcached protocol endpoint</option>');
     $('#dchooser').append('<option value="/titles/resp/resp-endpoint.html">RESP protocol endpoint</option>');
-    $('#dchooser').append('<option value="/titles/security/security.html">Security guide</option>');
-    $('#dchooser').append('<option value="/titles/hibernate/hibernate.html">Hibernate second-level caching (2LC)</option>');
+    $('#dchooser').append('<option value="/titles/changes/changes.html">Changes between versions</option>');
+    $('#dchooser').append('<option value="/titles/contributing/contributing.html">Contributor&rsquo;s guide</option>');
+    $('#dchooser').append('</optgroup>');
+    $('#dchooser').append('<optgroup label="Operations">');
+    $('#dchooser').append('<option value="/titles/server/server.html">Server</option>');
+    $('#dchooser').append('<option value="/docs/infinispan-operator/main/operator.html">Operator</option>');
+    $('#dchooser').append('<option value="/docs/helm-chart/main/helm-chart.html">Helm Chart</option>');
+    $('#dchooser').append('<option value="/titles/cli/cli.html">Command Line Interface (CLI)</option>');
+    $('#dchooser').append('<option value="/titles/tuning/tuning.html">Deployment planning and tuning</option>');
+    $('#dchooser').append('<option value="/titles/xsite/xsite.html">Cross-site replication</option>');
+    $('#dchooser').append('<option value="/titles/upgrading/upgrading.html">Upgrading deployments</option>');
+    $('#dchooser').append('</optgroup>');
+    $('#dchooser').append('<optgroup label="Integrations">');
     $('#dchooser').append('<option value="/titles/spring_boot/starter.html">Spring Boot Starter</option>');
     $('#dchooser').append('<option value="/titles/spring/spring.html">Spring Cache and Spring Sessions</option>');
-    $('#dchooser').append('<option value="/titles/upgrading/upgrading.html">Upgrading deployments</option>');
-    $('#dchooser').append('<option value="/titles/contributing/contributing.html">Contributor&rsquo;s guide</option>');
+    $('#dchooser').append('<option value="/titles/hibernate/hibernate.html">Hibernate second-level caching (2LC)</option>');
+    $('#dchooser').append('</optgroup>');
+
     $('#dchooser').change(function(e) {
        if (this.value !== '') {
-          window.location.href = path.substring(0, path.indexOf('/titles/')) + this.value;
+          if (this.value.startsWith('/titles/')) {
+            window.location.href = path.substring(0, path.indexOf('/titles/')) + this.value;
+          } else {
+            window.location.href = path.substring(0, path.indexOf('/docs/')) + this.value;
+          }
        }
     });
     $('#dchooser').after('<hr/>');
