b52337122de73f397495b014fcf3093deecdccb3
Ryan Emerson <remerson@redhat.com>
2025-03-28 15:21:48 +0000

    Make VisitableCommand implement CacheRpcCommand

--- core/src/main/java/org/infinispan/commands/remote/CacheRpcCommand.java
+++ core/src/main/java/org/infinispan/commands/remote/CacheRpcCommand.java
@@ -3,15 +3,9 @@ package org.infinispan.commands.remote;
 import java.util.concurrent.CompletionStage;
 
 import org.infinispan.commands.ReplicableCommand;
-import org.infinispan.commands.VisitableCommand;
-import org.infinispan.context.InvocationContext;
-import org.infinispan.context.InvocationContextFactory;
 import org.infinispan.factories.ComponentRegistry;
 import org.infinispan.remoting.transport.Address;
 import org.infinispan.util.ByteString;
-import org.infinispan.util.concurrent.locks.RemoteLockCommand;
-import org.infinispan.util.logging.Log;
-import org.infinispan.util.logging.LogFactory;
 
 /**
  * The {@link org.infinispan.remoting.rpc.RpcManager} only replicates commands wrapped in a {@link CacheRpcCommand}.
@@ -22,8 +16,6 @@ import org.infinispan.util.logging.LogFactory;
  */
 public interface CacheRpcCommand extends ReplicableCommand {
 
-   static final Log log = LogFactory.getLog(CacheRpcCommand.class);
-
    /**
     * Invoke the command asynchronously.
     * <p>This method replaces {@link #invoke()} for remote execution.
@@ -33,17 +25,6 @@ public interface CacheRpcCommand extends ReplicableCommand {
     * @since 11.0
     */
    default CompletionStage<?> invokeAsync(ComponentRegistry registry) throws Throwable {
-      if (getCacheName() != null && this instanceof VisitableCommand cmd) {
-         cmd.init(registry);
-         InvocationContextFactory icf = registry.getInvocationContextFactory().running();
-         InvocationContext ctx = icf.createRemoteInvocationContextForCommand(cmd, getOrigin());
-         if (cmd instanceof RemoteLockCommand remoteLockCmd) {
-            ctx.setLockOwner(remoteLockCmd.getKeyLockOwner());
-         }
-         if (log.isTraceEnabled())
-            log.tracef("Invoking command %s, with originLocal flag set to %b", cmd, ctx.isOriginLocal());
-         return registry.getInterceptorChain().running().invokeAsync(ctx, cmd);
-      }
       return invokeAsync();
    }
 
