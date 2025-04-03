a826945b7a92105478fe35eb1338e51a6c7ceb53
rigazilla <vrigamon@redhat.com>
2025-04-03 12:03:19 +0200

    [#14506] Ad-hoc JsonBucket mem size

--- server/resp/src/main/java/org/infinispan/server/resp/commands/connection/MemoryEntrySizeUtils.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/connection/MemoryEntrySizeUtils.java
@@ -4,6 +4,7 @@ import org.ehcache.sizeof.SizeOf;
 import org.infinispan.container.entries.CacheEntrySizeCalculator;
 import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.container.entries.PrimitiveEntrySizeCalculator;
+import org.infinispan.server.resp.json.JsonBucket;
 
 public class MemoryEntrySizeUtils {
    private static PrimitiveEntrySizeCalculator pesc = new PrimitiveEntrySizeCalculator();
@@ -15,8 +16,17 @@ public class MemoryEntrySizeUtils {
       return cesc.calculateSize(key, ice);
    }
 
+   public static long calculateSize(Object o) {
+      return sizeof.deepSizeOf(o);
+   }
+
    static long internalCalculateSize(byte[] key, Object value) {
       try {
+         if (value instanceof JsonBucket jb) {
+            // JsonBucket is a special case, we need to use the sizeOf library
+            // to calculate the size of the JsonBucket
+            return sizeof.deepSizeOf(key) + sizeof.deepSizeOf(jb.value())+ JsonBucket.memoryHeaderSize();
+         }
          return sizeof.deepSizeOf(key) + sizeof.deepSizeOf(value);
       } catch (Exception ex) {
          // Try an old style computation
--- server/resp/src/main/java/org/infinispan/server/resp/json/JsonBucket.java
+++ server/resp/src/main/java/org/infinispan/server/resp/json/JsonBucket.java
@@ -3,6 +3,7 @@ package org.infinispan.server.resp.json;
 import org.infinispan.commons.marshall.ProtoStreamTypeIds;
 import org.infinispan.protostream.annotations.Proto;
 import org.infinispan.protostream.annotations.ProtoTypeId;
+import org.infinispan.server.resp.commands.connection.MemoryEntrySizeUtils;
 
 /**
  * Bucket used to store Set data type.
@@ -13,4 +14,14 @@ import org.infinispan.protostream.annotations.ProtoTypeId;
 @Proto
 @ProtoTypeId(ProtoStreamTypeIds.RESP_JSON_BUCKET)
 public record JsonBucket(byte[] value) {
+public static long memoryHeaderSize() {
+   return MemoryHeader.headerSize;
+}
+
+// Only used to get the overhead without hardcoding
+private static class MemoryHeader {
+   @SuppressWarnings("unused")
+   byte[] value;
+   static long headerSize = MemoryEntrySizeUtils.calculateSize(new MemoryHeader());
+}
 }
