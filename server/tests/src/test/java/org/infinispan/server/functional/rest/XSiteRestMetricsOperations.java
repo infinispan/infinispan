00a174c6c98b4a789b9afd6849fb4ab021478960
cfredri4 <christian.fredriksson.2@volvocars.com>
2025-04-11 13:07:34 +0200

    [#14323] Refactor metrics
    Closes #14323

--- server/tests/src/test/java/org/infinispan/server/functional/rest/XSiteRestMetricsOperations.java
+++ server/tests/src/test/java/org/infinispan/server/functional/rest/XSiteRestMetricsOperations.java
@@ -53,35 +53,35 @@ public class XSiteRestMetricsOperations {
 
    private static final String[] TAGGED_METRICS = {
          // TakeOfflineManager metrics
-         "x_site_admin_status",
-         "x_site_admin_millis_since_first_failure",
-         "x_site_admin_failures_count",
+         "infinispan_x_site_admin_status",
+         "infinispan_x_site_admin_failures_count",
+         "infinispan_x_site_admin_millis_since_first_failure",
 
          // RpcManager metrics/gauges
-         "rpc_manager_number_xsite_requests_received_from_site",
-         "rpc_manager_number_xsite_requests_sent_to_site",
-         "rpc_manager_minimum_xsite_replication_time_to_site",
-         "rpc_manager_average_xsite_replication_time_to_site",
-         "rpc_manager_maximum_xsite_replication_time_to_site",
+         "infinispan_rpc_manager_number_xsite_requests_sent_to_site",
+         "infinispan_rpc_manager_number_xsite_requests_received_from_site",
+         "infinispan_rpc_manager_average_xsite_replication_time_to_site",
+         "infinispan_rpc_manager_minimum_xsite_replication_time_to_site",
+         "infinispan_rpc_manager_maximum_xsite_replication_time_to_site",
 
          // RpcManager metrics/histogram
-         "rpc_manager_replication_times_to_site_seconds_count",
-         "rpc_manager_replication_times_to_site_seconds_sum",
-         "rpc_manager_replication_times_to_site_seconds_max",
-   };
-   private static final String[] UNTAGGED_METRICS = {
+         "infinispan_rpc_manager_replication_times_to_site_seconds_count",
+         "infinispan_rpc_manager_replication_times_to_site_seconds_sum",
+         "infinispan_rpc_manager_replication_times_to_site_seconds_max",
+     };
+     private static final String[] UNTAGGED_METRICS = {
          // RpcManager metrics/gauges
-         "rpc_manager_number_xsite_requests_received",
-         "rpc_manager_number_xsite_requests",
-         "rpc_manager_minimum_xsite_replication_time",
-         "rpc_manager_average_xsite_replication_time",
-         "rpc_manager_maximum_xsite_replication_time",
+         "infinispan_rpc_manager_number_xsite_requests_received",
+         "infinispan_rpc_manager_number_xsite_requests",
+         "infinispan_rpc_manager_average_xsite_replication_time",
+         "infinispan_rpc_manager_minimum_xsite_replication_time",
+         "infinispan_rpc_manager_maximum_xsite_replication_time",
 
          // RpcManager metrics/histogram
-         "rpc_manager_cross_site_replication_times_seconds_count",
-         "rpc_manager_cross_site_replication_times_seconds_sum",
-         "rpc_manager_cross_site_replication_times_seconds_max",
-   };
+         "infinispan_rpc_manager_cross_site_replication_times_seconds_count",
+         "infinispan_rpc_manager_cross_site_replication_times_seconds_sum",
+         "infinispan_rpc_manager_cross_site_replication_times_seconds_max",
+     };
 
    @Test
    public void testSiteStatus() {
@@ -96,9 +96,9 @@ public class XSiteRestMetricsOperations {
       //noinspection resource
       SERVERS.rest(NYC).withServerConfiguration(new StringConfiguration(nycXML)).create();
 
-      String statusMetricName = "x_site_admin_status";
+      String statusMetricName = "infinispan_x_site_admin_status";
 
-      assertTrue(getMetrics(metricsClient).stream().anyMatch(m -> m.matches("vendor_" + statusMetricName)));
+      assertTrue(getMetrics(metricsClient).stream().anyMatch(m -> m.matches(statusMetricName)));
 
       assertSiteStatusMetrics(metricsClient, statusMetricName, 1);
 
@@ -141,8 +141,8 @@ public class XSiteRestMetricsOperations {
    }
 
    private static void expectNycSiteTag(boolean containsTag, String name, List<Metric> metrics) {
-      Optional<Metric> optMetric = metrics.stream().filter(metric -> metric.matches("vendor_" + name)).findAny();
-      assertTrue(optMetric.isPresent(), "Missing tag. metric: " + name);
+      Optional<Metric> optMetric = metrics.stream().filter(metric -> metric.matches(name)).findAny();
+      assertTrue(optMetric.isPresent(), "Missing metric: " + name);
       if (containsTag) {
          optMetric.get().assertTagPresent("site", "NYC");
       } else {
@@ -151,7 +151,7 @@ public class XSiteRestMetricsOperations {
    }
 
    private static void assertSiteStatusMetrics(RestMetricsClient client, String metric, int expected) {
-      var m = getMetrics(client).stream().filter(m1 -> m1.matches("vendor_" + metric)).findFirst();
+      var m = getMetrics(client).stream().filter(m1 -> m1.matches(metric)).findFirst();
       assertTrue(m.isPresent());
       m.get().value().isEqualTo(expected);
    }
