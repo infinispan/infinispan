f246f297d34aa6f4fd6dde3b719066fcdb3b461f
Katia Aresti <karesti@redhat.com>
2025-04-16 17:50:23 +0200

    wip reproducer

--- query/src/test/java/org/infinispan/query/model/Player.java
+++ query/src/test/java/org/infinispan/query/model/Player.java
@@ -1,6 +1,7 @@
 package org.infinispan.query.model;
 
 import org.infinispan.api.annotations.indexing.Basic;
+import org.infinispan.api.annotations.indexing.Indexed;
 import org.infinispan.protostream.annotations.Proto;
 
 @Proto
