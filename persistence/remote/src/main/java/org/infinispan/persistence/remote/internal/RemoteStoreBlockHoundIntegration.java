d654d24c4c3e7081b4302f30a98742ee8821bafe
rigazilla <vrigamon@redhat.com>
2025-05-21 11:39:43 +0200

    [#14539] Added ops that can block

--- persistence/remote/src/main/java/org/infinispan/persistence/remote/internal/RemoteStoreBlockHoundIntegration.java
+++ persistence/remote/src/main/java/org/infinispan/persistence/remote/internal/RemoteStoreBlockHoundIntegration.java
@@ -15,5 +15,11 @@ public class RemoteStoreBlockHoundIntegration implements BlockHoundIntegration {
       // https://issues.redhat.com/browse/ISPN-12180
       builder.allowBlockingCallsInside("org.infinispan.client.hotrod.impl.transport.netty.ChannelInitializer", "initSsl");
       CommonsBlockHoundIntegration.allowPublicMethodsToBlock(builder, ChannelFactory.class);
+      // Some tests have client failover and it is possible for it need to acquire the read lock
+      builder.allowBlockingCallsInside("org.infinispan.client.hotrod.impl.transport.netty.OperationDispatcher", "getCacheInfo");
+      builder.allowBlockingCallsInside("org.infinispan.client.hotrod.impl.transport.netty.OperationDispatcher", "getClusterInfo");
+
+      // When a server fails we need to acquire the write lock very briefly to update internals
+      builder.allowBlockingCallsInside("org.infinispan.client.hotrod.impl.transport.netty.OperationDispatcher", "handleConnectionFailure");
    }
 }
