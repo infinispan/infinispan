745526fb5bce00fe5092d8bd7f05bd53c473cfac
Tristan Tarrant <tristan@infinispan.org>
2025-06-05 10:31:39 +0200

    [#14993] Remove 11.0 deprecations: ClusterRoleMapper, CustomFailurePolicy

--- core/src/main/java/org/infinispan/security/impl/ClusterRoleMapper.java
+++
@@ -1,9 +0,0 @@
-package org.infinispan.security.impl;
-
-/**
- * @author Tristan Tarrant &lt;tristan@infinispan.org&gt;
- * @deprecated use {@link org.infinispan.security.mappers.ClusterRoleMapper} instead. This class will be removed in Infinispan 14.0
- **/
-@Deprecated(forRemoval=true, since = "11.0")
-public class ClusterRoleMapper extends org.infinispan.security.mappers.ClusterRoleMapper {
-}
