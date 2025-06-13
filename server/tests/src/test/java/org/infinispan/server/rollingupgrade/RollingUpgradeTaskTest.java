1be3447dbda085e9e496d451e35219836c872933
José Bolina <joseaugusto.bolina@hotmail.com>
2025-06-13 17:17:31 -0300

    [#15082] Rolling upgrades support more options
    * Add support for custom configurations, artifacts, and environment
      properties.
    * Container inspect fix to fallback to snapshot image name.

---
+++ server/tests/src/test/java/org/infinispan/server/rollingupgrade/RollingUpgradeTaskTest.java
@@ -0,0 +1,39 @@
+package org.infinispan.server.rollingupgrade;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.client.hotrod.RemoteCacheManager;
+import org.infinispan.commons.configuration.StringConfiguration;
+import org.infinispan.server.functional.ClusteredIT;
+import org.infinispan.server.test.core.rollingupgrade.RollingUpgradeConfigurationBuilder;
+import org.infinispan.server.test.core.rollingupgrade.RollingUpgradeHandler;
+import org.junit.jupiter.api.Test;
+
+public class RollingUpgradeTaskTest {
+
+   @Test
+   public void testHelloWorldTask() throws Throwable {
+      RollingUpgradeConfigurationBuilder builder = new RollingUpgradeConfigurationBuilder("15.2.0.Final", "15.2.1.Final")
+            .useCustomServerConfiguration("configuration/ClusteredServerTest.xml")
+            .addArtifacts(ClusteredIT.artifacts())
+            .addMavenArtifacts(ClusteredIT.mavenArtifacts())
+            .nodeCount(3);
+
+      builder.handlers(ruh -> {
+         RemoteCacheManager rcm = ruh.getRemoteCacheManager();
+         RemoteCache<String, String> cache = rcm.administration().getOrCreateCache("task-cache",
+               new StringConfiguration("<replicated-cache><encoding media-type=\"application/x-protostream\"/></replicated-cache>"));
+         Object hello = cache.execute("hello");
+         assertThat(hello).isEqualTo("Hello world");
+      }, ruh -> {
+         RemoteCacheManager rcm = ruh.getRemoteCacheManager();
+         RemoteCache<String, String> cache = rcm.getCache("task-cache");
+         Object hello = cache.execute("hello");
+         assertThat(hello).isEqualTo("Hello world");
+         return true;
+      });
+
+      RollingUpgradeHandler.performUpgrade(builder.build());
+   }
+}
