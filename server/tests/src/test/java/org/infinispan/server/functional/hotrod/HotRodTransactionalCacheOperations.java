b1f02a89c1ae09b21211a62cdb26eb793ee05a5d
José Bolina <jose@infinispan.org>
2025-06-17 17:32:59 -0300

    [#13263] Add IT tests mixing HR flags
    * Create simple tests with common APIs varying every combination of
      flags.

--- server/tests/src/test/java/org/infinispan/server/functional/hotrod/HotRodTransactionalCacheOperations.java
+++ server/tests/src/test/java/org/infinispan/server/functional/hotrod/HotRodTransactionalCacheOperations.java
@@ -1,21 +1,30 @@
 package org.infinispan.server.functional.hotrod;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
+import jakarta.transaction.TransactionManager;
 
+import org.infinispan.client.hotrod.Flag;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.configuration.TransactionMode;
 import org.infinispan.client.hotrod.transaction.lookup.RemoteTransactionManagerLookup;
 import org.infinispan.commons.configuration.StringConfiguration;
-import org.infinispan.configuration.parsing.Parser;
+import org.infinispan.commons.test.Combinations;
 import org.infinispan.server.functional.ClusteredIT;
 import org.infinispan.server.test.api.TestClientDriver;
 import org.infinispan.server.test.junit5.InfinispanServer;
+import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
+import org.junit.jupiter.params.support.ParameterDeclarations;
 
-import jakarta.transaction.TransactionManager;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 /**
  * @author Tristan Tarrant &lt;tristan@infinispan.org&gt;
@@ -34,9 +43,21 @@ public class HotRodTransactionalCacheOperations {
    @InfinispanServer(ClusteredIT.class)
    public static TestClientDriver SERVERS;
 
-   @ParameterizedTest(name = "{0}")
-   @EnumSource(value = Parser.TransactionMode.class, names = {"NON_XA", "NON_DURABLE_XA", "FULL_XA"})
-   public void testTransactionalCache(Parser.TransactionMode txMode) throws Exception {
+   static final class ArgsProvider implements ArgumentsProvider {
+      @Override
+      public Stream<? extends Arguments> provideArguments(ParameterDeclarations parameters, ExtensionContext context) {
+         return Combinations.combine(Flag.class).stream()
+               // Remove filter after https://github.com/infinispan/infinispan/issues/14926
+               .filter(Set::isEmpty)
+               .flatMap(f ->
+                     Stream.of(TransactionMode.NON_XA, TransactionMode.NON_DURABLE_XA, TransactionMode.FULL_XA)
+                           .map(mode -> Arguments.of(mode, f)));
+      }
+   }
+
+   @ParameterizedTest(name = "{0}-{1}")
+   @ArgumentsSource(ArgsProvider.class)
+   public void testTransactionalCache(TransactionMode txMode, EnumSet<Flag> flags) throws Exception {
       ConfigurationBuilder config = new ConfigurationBuilder();
       config.remoteCache(SERVERS.getMethodName())
             .transactionMode(TransactionMode.NON_XA)
@@ -44,7 +65,8 @@ public class HotRodTransactionalCacheOperations {
 
       String xml = String.format(TEST_CACHE_XML_CONFIG, SERVERS.getMethodName(), txMode.name());
 
-      RemoteCache<String, String> cache = SERVERS.hotrod().withClientConfiguration(config).withServerConfiguration(new StringConfiguration(xml)).create();
+      RemoteCache<String, String> cache = SERVERS.hotrod().withClientConfiguration(config).withServerConfiguration(new StringConfiguration(xml))
+            .<String, String>create().withFlags(flags.toArray(new Flag[0]));
       TransactionManager tm = cache.getTransactionManager();
       tm.begin();
       cache.put("k", "v1");
