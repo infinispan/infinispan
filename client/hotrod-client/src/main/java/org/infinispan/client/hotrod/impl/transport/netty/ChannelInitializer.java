edfe9ddca53161a25319924dc694dcbf6c684f25
William Burns <mudokonman@gmail.com>
2025-06-26 09:17:58 -0700

    Revert "[#15137] Client still prints error message when shutting down from event loop being stopped"
    This reverts commit c865158906e25a1ddc8f047912b3ac2ae6bca260.

--- client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/transport/netty/ChannelInitializer.java
+++ client/hotrod-client/src/main/java/org/infinispan/client/hotrod/impl/transport/netty/ChannelInitializer.java
@@ -80,9 +80,6 @@ class ChannelInitializer extends io.netty.channel.ChannelInitializer<Channel> {
    }
 
    ChannelFuture createChannel() {
-      if (bootstrap.config().group().isShuttingDown()) {
-         throw new IllegalStateException("Event loop is already shutdown!");
-      }
       return bootstrap.clone().connect();
    }
 
