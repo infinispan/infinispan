4644ff3f9c98610711071291f4114e0be4e707b9
Tristan Tarrant <tristan@infinispan.org>
2025-06-27 12:59:38 +0200

    [#14993] Remove 11.0 deprecations: user-tool

--- server/runtime/src/main/server/bin/user-tool.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-echo "This tool is deprecated and will be removed in the future. Please use \"cli.bat user create\" instead"
-
-set "DIRNAME=%~dp0%"
-
-setlocal EnableDelayedExpansion
-call "!DIRNAME!cli.bat" user create %*
-setlocal DisableDelayedExpansion
