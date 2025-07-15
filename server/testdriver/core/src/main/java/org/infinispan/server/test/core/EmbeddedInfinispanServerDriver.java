34ec6f04c4b3fe690ac2b1d4f71bc5adbf7a56e9
José Bolina <jose@infinispan.org>
2025-07-15 14:42:57 -0300

    [#15295] Change server status after post start
    * Change server to running only after post start.
    * Embedded driver wait until server is ready (or failed).

--- server/testdriver/core/src/main/java/org/infinispan/server/test/core/EmbeddedInfinispanServerDriver.java
+++ server/testdriver/core/src/main/java/org/infinispan/server/test/core/EmbeddedInfinispanServerDriver.java
@@ -1,5 +1,7 @@
 package org.infinispan.server.test.core;
 
+import static org.infinispan.commons.test.Eventually.DEFAULT_POLL_INTERVAL_MILLIS;
+import static org.infinispan.commons.test.Eventually.eventually;
 import static org.infinispan.server.test.core.TestSystemPropertyNames.INFINISPAN_TEST_SERVER_EMBEDDED_TIMEOUT_SECONDS;
 
 import java.io.Closeable;
@@ -18,6 +20,7 @@ import javax.management.MBeanServerConnection;
 
 import org.infinispan.commons.test.Exceptions;
 import org.infinispan.commons.util.StringPropertyReplacer;
+import org.infinispan.lifecycle.ComponentStatus;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.remoting.transport.Address;
@@ -57,6 +60,7 @@ public class EmbeddedInfinispanServerDriver extends AbstractInfinispanServerDriv
          Server server = createServerInstance(name, rootDir, new File(configuration.configurationFile()), i, serverRoot);
          serverFutures.add(server.run());
          waitCompletion(server.cacheManagerStart().toCompletableFuture());
+         holdUntilStatusRunning(server);
          servers.add(server);
       }
       // Ensure that the cluster has formed if we start more than one server
@@ -64,6 +68,16 @@ public class EmbeddedInfinispanServerDriver extends AbstractInfinispanServerDriv
       if(cacheManagers.size() > 1) {
          blockUntilViewsReceived(cacheManagers);
       }
+   }
+
+   private static void holdUntilStatusRunning(Server server) {
+      eventually(
+            () -> new AssertionError("Server never started"),
+            () -> {
+               ComponentStatus s = server.getStatus();
+               return s.allowInvocations() || s.isStopping() || s.isTerminated();
+            },
+            30_000, DEFAULT_POLL_INTERVAL_MILLIS, TimeUnit.MILLISECONDS);
    }
 
    private static void waitCompletion(CompletableFuture<?> cf) {
