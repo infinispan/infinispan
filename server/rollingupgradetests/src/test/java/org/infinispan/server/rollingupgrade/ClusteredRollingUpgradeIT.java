d4815ff09546791e27115f8e88043f40e514f4b5
William Burns <mudokonman@gmail.com>
2025-08-01 15:06:34 -0700

    [#15097] ClusteredIT cannot be ran with more than 2 nodes
    * Distributed queries use segment as clauses so need to increase

--- server/rollingupgradetests/src/test/java/org/infinispan/server/rollingupgrade/ClusteredRollingUpgradeIT.java
+++ server/rollingupgradetests/src/test/java/org/infinispan/server/rollingupgrade/ClusteredRollingUpgradeIT.java
@@ -85,7 +85,7 @@ public class ClusteredRollingUpgradeIT extends InfinispanSuite {
             .useCustomServerConfiguration("configuration/ClusteredServerTest.xml")
             .addMavenArtifacts(ClusteredIT.mavenArtifacts())
             .addArchives(ClusteredIT.artifacts())
-            .addProperty("infinispan.query.lucene.max-boolean-clauses", "1025");
+            .addProperty("infinispan.query.lucene.max-boolean-clauses", String.valueOf(ClusteredIT.MAX_BOOLEAN_CLAUSES));
       SERVERS = new RollingUpgradeHandlerExtension(builder);
    }
 
