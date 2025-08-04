74ffcd281a84cb1d4a745e619102920ff94204d4
William Burns <mudokonman@gmail.com>
2025-08-04 12:27:47 -0700

    [#15395] Score Projection fails with HSEARCH000610: Unknown field '__ISPN_Score' when using a join and a cache with numNodes > numOwners
    * Strip score before passing to ickle parser

--- server/tests/src/test/java/org/infinispan/server/functional/hotrod/HotRodCacheQueries.java
+++ server/tests/src/test/java/org/infinispan/server/functional/hotrod/HotRodCacheQueries.java
@@ -41,6 +41,7 @@ import org.infinispan.server.functional.ClusteredIT;
 import org.infinispan.server.functional.extensions.entities.Entities;
 import org.infinispan.server.test.api.TestClientDriver;
 import org.infinispan.server.test.junit5.InfinispanServer;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
