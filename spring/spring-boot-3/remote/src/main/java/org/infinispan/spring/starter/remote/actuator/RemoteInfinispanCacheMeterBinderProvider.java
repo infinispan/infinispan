0eab13dea373d8ebd5eae065b3c6d2d54bb53db8
Michael Wirth <michael.wirth@mimacom.com>
2025-08-15 18:24:55 +0200

    [#15504] Fix NPE in RemoteInfinispanCacheMeterBinderProvider

--- spring/spring-boot-3/remote/src/main/java/org/infinispan/spring/starter/remote/actuator/RemoteInfinispanCacheMeterBinderProvider.java
+++ spring/spring-boot-3/remote/src/main/java/org/infinispan/spring/starter/remote/actuator/RemoteInfinispanCacheMeterBinderProvider.java
@@ -1,15 +1,14 @@
 package org.infinispan.spring.starter.remote.actuator;
 
+import io.micrometer.core.instrument.Tag;
+import io.micrometer.core.instrument.binder.MeterBinder;
 import org.infinispan.client.hotrod.RemoteCache;
+import org.infinispan.spring.common.provider.SpringCache;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.actuate.metrics.cache.CacheMeterBinderProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.cache.Cache;
 import org.springframework.stereotype.Component;
-
-import io.micrometer.core.instrument.Tag;
-import io.micrometer.core.instrument.binder.MeterBinder;
 
 /**
  * When actuate dependency is found in the classpath, this component links Infinispan cache metrics with Actuator
@@ -21,17 +20,17 @@ import io.micrometer.core.instrument.binder.MeterBinder;
 @Qualifier(RemoteInfinispanCacheMeterBinderProvider.NAME)
 @ConditionalOnClass(name = "org.springframework.boot.actuate.metrics.cache.CacheMeterBinderProvider")
 @ConditionalOnProperty(value = "infinispan.remote.enabled", havingValue = "true", matchIfMissing = true)
-public class RemoteInfinispanCacheMeterBinderProvider implements CacheMeterBinderProvider<Cache> {
+public class RemoteInfinispanCacheMeterBinderProvider implements CacheMeterBinderProvider<SpringCache> {
 
-   public static final String NAME = "remoteInfinispanCacheMeterBinderProvider";
+    public static final String NAME = "remoteInfinispanCacheMeterBinderProvider";
 
-   @Override
-   public MeterBinder getMeterBinder(Cache cache, Iterable<Tag> tags) {
+    @Override
+    public MeterBinder getMeterBinder(SpringCache cache, Iterable<Tag> tags) {
 
-      if (cache.getNativeCache() instanceof RemoteCache) {
-         return new RemoteInfinispanCacheMeterBinder((RemoteCache) cache.getNativeCache(), tags);
-      } else {
-         return new RemoteInfinispanCacheMeterBinder(null, tags);
-      }
-   }
+        if (cache.getNativeCache() instanceof RemoteCache) {
+            return new RemoteInfinispanCacheMeterBinder((RemoteCache) cache.getNativeCache(), tags);
+        } else {
+            return null;
+        }
+    }
 }
