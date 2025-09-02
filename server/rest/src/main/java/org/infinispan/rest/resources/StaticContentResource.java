34c66af83579a4c3bfa86ef7e6facc383270a0ff
Katia Aresti <katia@infinispan.org>
2025-09-02 15:07:47 +0200

    [#14983] Dynamic base and context path

--- server/rest/src/main/java/org/infinispan/rest/resources/StaticContentResource.java
+++ server/rest/src/main/java/org/infinispan/rest/resources/StaticContentResource.java
@@ -8,8 +8,11 @@ import static org.infinispan.rest.framework.Method.GET;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Date;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.TimeUnit;
 
@@ -36,6 +39,7 @@ public class StaticContentResource implements ResourceHandler {
 
    private static final int CACHE_MAX_AGE_SECONDS = 60 * 60 * 24 * 31;  // 1 Month
    public static final String DEFAULT_RESOURCE = "index.html";
+   public static final String CONFIG_RESOURCE = "config.js";
    private final String noFileUri;
    private final ResourceResolver customResourceResolver;
    private final String rootUri;
@@ -95,7 +99,6 @@ public class StaticContentResource implements ResourceHandler {
 
    private CompletionStage<RestResponse> serveFile(RestRequest restRequest) {
       NettyRestResponse.Builder responseBuilder = invocationHelper.newResponse(restRequest);
-
       String uri = restRequest.uri();
 
       if (uri.equals(noFileUri)) {
@@ -134,9 +137,48 @@ public class StaticContentResource implements ResourceHandler {
       String resolved = MediaTypeResolver.getMediaType(file.getName());
       String mediaType = resolved != null ? resolved : APPLICATION_OCTET_STREAM_TYPE;
 
-      responseBuilder
+       if (resource.equals(DEFAULT_RESOURCE)) {
+           return loadIndexFileAndReplaceValue("/console/",
+                   "{{INFINISPAN_BASE_PATH}}",
+                   mediaType,
+                   restRequest, file, responseBuilder);
+       }
+
+       if (resource.equals(CONFIG_RESOURCE)) {
+           String restContextPath = "/" + invocationHelper.getConfiguration().contextPath();
+           return loadIndexFileAndReplaceValue(restContextPath,
+                   "{{INFINISPAN_REST_CONTEXT_PATH}}",
+                   mediaType,
+                   restRequest, file, responseBuilder);
+       }
+
+       responseBuilder
             .contentType(mediaType)
             .entity(file);
       return completedFuture(responseBuilder.build());
    }
+
+    private CompletableFuture<RestResponse> loadIndexFileAndReplaceValue(String defaultBasePath,
+                                                                         String remplacementName,
+                                                                         String mediaType,
+                                                                         RestRequest restRequest,
+                                                                         File file,
+                                                                         NettyRestResponse.Builder responseBuilder) {
+        try {
+            String content = Files.readString(file.toPath(), StandardCharsets.UTF_8);
+            String xFowardedPrefix = restRequest.header("X-Forwarded-Prefix");
+            String currentBasePath = defaultBasePath;
+            if (xFowardedPrefix != null && !xFowardedPrefix.isEmpty()) {
+                currentBasePath = xFowardedPrefix + currentBasePath;
+            }
+            String processedContent = content
+                    .replace(remplacementName, currentBasePath);
+            responseBuilder
+                    .contentType(mediaType)
+                    .entity(processedContent);
+            return completedFuture(responseBuilder.build());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
