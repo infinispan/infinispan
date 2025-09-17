aa26d6148f4c7107eb22791fba3a1777503bd5a0
Tristan Tarrant <tristan@infinispan.org>
2025-09-17 10:14:28 +0200

    Fix javadoc

--- commons/all/src/main/java/org/infinispan/query/dsl/package-info.java
+++ commons/all/src/main/java/org/infinispan/query/dsl/package-info.java
@@ -1,14 +1,2 @@
-/**
- * Query DSL API. This API offers a fluent builder for the Ickle query string.
- * <p>
- * <b>WARNING:</b> Most classes in this package are deprecated although the package itself is not entirely deprecated
- * yet. See deprecation note on {@link org.infinispan.query.dsl.QueryBuilder}. Please do not use any of the deprecated
- * methods or else you will experience difficulties in porting your code to the new query API that will be introduced by
- * Infinispan 12.
- *
- * @author anistor@redhat.com
- * @since 6.0
- *
- * @api.public
- */
+@Deprecated
 package org.infinispan.query.dsl;
