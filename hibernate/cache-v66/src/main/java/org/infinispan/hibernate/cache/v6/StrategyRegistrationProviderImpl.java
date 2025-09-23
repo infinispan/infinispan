8bc9481f59f7327eb855a92dd862bd96bd5fb2dc
William Burns <mudokonman@gmail.com>
2025-09-23 09:33:30 -0700

    [#14921] Support Hibernate 7 2LC
    * Move from cache-v62 to cache-v66

---
+++ hibernate/cache-v66/src/main/java/org/infinispan/hibernate/cache/v6/StrategyRegistrationProviderImpl.java
@@ -0,0 +1,36 @@
+package org.infinispan.hibernate.cache.v6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.boot.registry.selector.SimpleStrategyRegistrationImpl;
+import org.hibernate.boot.registry.selector.StrategyRegistration;
+import org.hibernate.boot.registry.selector.StrategyRegistrationProvider;
+import org.hibernate.cache.spi.RegionFactory;
+import org.kohsuke.MetaInfServices;
+
+/**
+ * Makes the contained region factory implementations available to the Hibernate
+ * {@link org.hibernate.boot.registry.selector.spi.StrategySelector} service.
+ *
+ * @author Steve Ebersole
+ */
+@MetaInfServices(StrategyRegistrationProvider.class)
+public class StrategyRegistrationProviderImpl implements StrategyRegistrationProvider {
+   @Override
+   public Iterable<StrategyRegistration> getStrategyRegistrations() {
+      final List<StrategyRegistration> strategyRegistrations = new ArrayList<StrategyRegistration>();
+
+      strategyRegistrations.add(
+            new SimpleStrategyRegistrationImpl<>(
+                  RegionFactory.class,
+                  InfinispanRegionFactory.class,
+                  "infinispan",
+                  "infinispan-jndi",
+                  InfinispanRegionFactory.class.getName(),
+                  InfinispanRegionFactory.class.getSimpleName()
+            )
+      );
+      return strategyRegistrations;
+   }
+}
