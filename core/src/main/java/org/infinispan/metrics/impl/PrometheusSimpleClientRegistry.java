f0d4fab3a7c6dd70dc7d57d574315c185a44575e
Ryan Emerson <remerson@ibm.com>
2025-09-25 11:58:12 +0100

    [#15699] Add support for micrometer-registry-prometheus-simpleclient
    Co-authored-by: Pedro Ruivo <1492066+pruivo@users.noreply.github.com>

---
+++ core/src/main/java/org/infinispan/metrics/impl/PrometheusSimpleClientRegistry.java
@@ -0,0 +1,59 @@
+package org.infinispan.metrics.impl;
+
+import org.infinispan.metrics.config.MicrometerMeterRegistryConfiguration;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.prometheus.PrometheusConfig;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+
+/**
+ * Concrete implementation of {@link AbstractMetricsRegistry}.
+ * <p>
+ * It uses {@link MeterRegistry} from micrometer. It can use the instance configured from
+ * {@link MicrometerMeterRegistryConfiguration#meterRegistry()} or, if not configured, it instantiates
+ * {@link PrometheusMeterRegistry}.
+ */
+public class PrometheusSimpleClientRegistry extends AbstractMetricsRegistry {
+
+   ScrapeRegistry createScrapeRegistry(MeterRegistry registry) {
+      boolean externalManaged = true;
+      if (registry == null) {
+         registry = new PrometheusMeterRegistry(PrometheusConfig.DEFAULT);
+         externalManaged = false;
+      }
+      return registry instanceof PrometheusMeterRegistry ?
+            new PrometheusRegistrySimpleClient((PrometheusMeterRegistry) registry, externalManaged) :
+            new NoScrapeRegistry(registry, externalManaged);
+   }
+
+   private static class PrometheusRegistrySimpleClient implements AbstractMetricsRegistry.ScrapeRegistry {
+
+      final PrometheusMeterRegistry registry;
+      final boolean externalManaged;
+
+      PrometheusRegistrySimpleClient(PrometheusMeterRegistry registry, boolean externalManaged) {
+         this.registry = registry;
+         this.externalManaged = externalManaged;
+      }
+
+      @Override
+      public boolean supportsScrape() {
+         return true;
+      }
+
+      @Override
+      public String scrape(String contentType) {
+         return registry.scrape(contentType);
+      }
+
+      @Override
+      public MeterRegistry registry() {
+         return registry;
+      }
+
+      @Override
+      public boolean externalManaged() {
+         return externalManaged;
+      }
+   }
+}
