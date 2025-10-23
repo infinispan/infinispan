6ee4cd73bbf60e87dcc79d28324d1d57897a1671
José Bolina <jose@infinispan.org>
2025-10-23 19:05:09 -0300

    [#15962] Refresh documentation
    * Update embedded documentation
    * Update planning guide

--- documentation/src/main/asciidoc/topics/code_examples/ReadWriteMapMarshallableFunctions.java
+++ documentation/src/main/asciidoc/topics/code_examples/ReadWriteMapMarshallableFunctions.java
@@ -4,6 +4,6 @@ import org.infinispan.commons.marshall.MarshallableFunctions;
 
 ReadWriteMap<String, String> readWriteMap = ...
 
-CompletableFuture<Boolean> future = readWriteMap.eval("key1,
+CompletableFuture<Boolean> future = readWriteMap.eval("key1",
    MarshallableFunctions.setValueIfAbsentReturnBoolean());
 future.thenAccept(stored -> System.out.printf("Value was put? %s%n", stored));
