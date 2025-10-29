2449e39c6bb4054fc3415ffa67fe57085229f215
José Bolina <jose@infinispan.org>
2025-10-29 15:32:26 -0300

    [#16022] Fine-grained configuration match
    * Dedicated match for cluster
    * Extra configuration when enabling batch

--- core/src/test/java/org/infinispan/configuration/serializer/ConfigurationSerializerTest.java
+++ core/src/test/java/org/infinispan/configuration/serializer/ConfigurationSerializerTest.java
@@ -1,5 +1,15 @@
 package org.infinispan.configuration.serializer;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.infinispan.commons.dataconversion.MediaType;
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.Configuration;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
+import org.infinispan.configuration.parsing.ParserRegistry;
+import org.infinispan.distribution.ch.impl.HashFunctionPartitioner;
+import org.infinispan.distribution.ch.impl.RESPHashFunctionPartitioner;
 import org.testng.annotations.Test;
 
 @Test(testName = "configuration.serializer.ConfigurationSerializerTest", groups = "functional")
