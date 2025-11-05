c4b27ec5b34af8adffbd47b135020f481b6f360f
Tristan Tarrant <tristan@infinispan.org>
2025-11-05 10:59:56 +0100

    [#16148] Release branching action

--- bin/reversion.sh
+++ bin/reversion.sh
@@ -8,17 +8,17 @@ function rewriteXSDs() {
     NEWMAJOR=$4
     NEWMINOR=$5
     
-    SCHEMAS=`find . -path "*/src/main/resources/*$OLDMAJOR$SEPARATOR$OLDMINOR.xsd"`
+    SCHEMAS=$(find . -path "*/src/main/resources/*$OLDMAJOR$SEPARATOR$OLDMINOR.xsd" -type f)
     for OLDSCHEMA in $SCHEMAS; do
         # Rewrite the filename
-        NEWSCHEMA=`echo "$OLDSCHEMA"|sed "s/$OLDMAJOR$SEPARATOR$OLDMINOR/$NEWMAJOR$SEPARATOR$NEWMINOR/"`
+        NEWSCHEMA=${OLDSCHEMA//${OLDMAJOR}${SEPARATOR}${OLDMINOR}/${NEWMAJOR}${SEPARATOR}${NEWMINOR}}
         echo "$OLDSCHEMA ==> $NEWSCHEMA"
-        mv $OLDSCHEMA $NEWSCHEMA
+        mv "$OLDSCHEMA" "$NEWSCHEMA"
         # We need to do different replacements
-        sed -i "s/:$OLDMAJOR.$OLDMINOR\"/:$NEWMAJOR.$NEWMINOR\"/g" $NEWSCHEMA
-        sed -i "s/-$OLDMAJOR$SEPARATOR$OLDMINOR.xsd/-$NEWMAJOR$SEPARATOR$NEWMINOR.xsd/g" $NEWSCHEMA
-        git rm $OLDSCHEMA
-        git add $NEWSCHEMA
+        sed -i "s/:${OLDMAJOR}.${OLDMINOR}\"/:${NEWMAJOR}.${NEWMINOR}\"/g" "${NEWSCHEMA}"
+        sed -i "s/-$OLDMAJOR$SEPARATOR$OLDMINOR.xsd/-$NEWMAJOR$SEPARATOR$NEWMINOR.xsd/g" "$NEWSCHEMA"
+        git rm "$OLDSCHEMA"
+        git add "$NEWSCHEMA"
     done
 }
 
@@ -29,9 +29,9 @@ function rewriteXML() {
     NEWMAJOR=$4
     NEWMINOR=$5
 
-    sed -i "s/:$OLDMAJOR.$OLDMINOR/:$NEWMAJOR.$NEWMINOR/g" $XML
-    sed -i "s/-$OLDMAJOR.$OLDMINOR.xsd/-$NEWMAJOR.$NEWMINOR.xsd/g" $XML
-    git add $XML
+    sed -i "s/:$OLDMAJOR.$OLDMINOR/:$NEWMAJOR.$NEWMINOR/g" "$XML"
+    sed -i "s/-$OLDMAJOR.$OLDMINOR.xsd/-$NEWMAJOR.$NEWMINOR.xsd/g" "$XML"
+    git add "$XML"
 }
 
 function copyXMLs() {
@@ -41,21 +41,23 @@ function copyXMLs() {
     NEWMAJOR=$4
     NEWMINOR=$5
 
-    for XML in $(find . -name "*${OLDMAJOR}${SEPARATOR}${OLDMINOR}.xml" -print); do
-       if ! git check-ignore -q $XML; then
-          NEWXML=${XML/${OLDMAJOR}${SEPARATOR}${OLDMINOR}/${NEWMAJOR}${SEPARATOR}${NEWMINOR}}
-          echo Adding $NEWXML
-          cp $XML $NEWXML
-          rewriteXML $NEWXML $OLDMAJOR $OLDMINOR $NEWMAJOR $NEWMINOR
+    XMLS=$(find . -name "*${OLDMAJOR}${SEPARATOR}${OLDMINOR}.xml" -type f)
+    for XML in $XMLS; do
+       if ! git check-ignore -q "$XML"; then
+          NEWXML=${XML//${OLDMAJOR}${SEPARATOR}${OLDMINOR}/${NEWMAJOR}${SEPARATOR}${NEWMINOR}}
+          echo Adding "$NEWXML"
+          cp "$XML" "$NEWXML"
+          rewriteXML "$NEWXML" "$OLDMAJOR" "$OLDMINOR" "$NEWMAJOR" "$NEWMINOR"
        fi
     done
-    
-    for PROPERTIES in $(find . -name "*${OLDMAJOR}${SEPARATOR}${OLDMINOR}.properties" -print); do
-       if ! git check-ignore -q $PROPERTIES; then
-          NEWPROPERTIES=${PROPERTIES/${OLDMAJOR}${SEPARATOR}${OLDMINOR}/${NEWMAJOR}${SEPARATOR}${NEWMINOR}}
-          echo Adding $NEWPROPERTIES
-          cp $PROPERTIES $NEWPROPERTIES
-          git add $NEWPROPERTIES
+
+    PROPERTIES_FILES=$(find . -name "*${OLDMAJOR}${SEPARATOR}${OLDMINOR}.properties" -type f)
+    for PROPERTIES in $PROPERTIES_FILES; do
+       if ! git check-ignore -q "$PROPERTIES"; then
+          NEWPROPERTIES=${PROPERTIES//${OLDMAJOR}${SEPARATOR}${OLDMINOR}/${NEWMAJOR}${SEPARATOR}${NEWMINOR}}
+          echo Adding "$NEWPROPERTIES"
+          cp "$PROPERTIES" "$NEWPROPERTIES"
+          git add "$NEWPROPERTIES"
        fi
     done
 }
@@ -66,33 +68,34 @@ function addNamespace() {
     NEWMAJOR=$3
     NEWMINOR=$4
 
-    for NAMESPACES in $(find . -name "*.namespaces" -print); do
-        OLDNAMESPACE=`grep ${OLDMAJOR}.${OLDMINOR} $NAMESPACES`
-        echo "Modifying $NAMESPACES"
-        echo "${OLDNAMESPACE/$OLDMAJOR.$OLDMINOR/$NEWMAJOR.$NEWMINOR}" >> $NAMESPACES
+    NAMESPACES=$(find . -name "*.namespaces" -type f)
+    for NAMESPACE in $NAMESPACES; do
+        OLDNAMESPACE=$(grep "${OLDMAJOR}.${OLDMINOR}" "$NAMESPACE")
+        echo "Modifying $NAMESPACE"
+        echo "${OLDNAMESPACE/$OLDMAJOR.$OLDMINOR/$NEWMAJOR.$NEWMINOR}" >> "$NAMESPACE"
     done
 }
 
 while test $# -gt 0; do
     case "$1" in
             -h|--help)
-                    echo "This script changes the version"
+                    echo "This script changes the versions, updates the schemas and the configuration files"
                     echo " "
                     echo "if no arguments specified - use interactive mode"
                     echo " "
                     echo "options:"
-                    echo "-h, --help                Show brief help"
-                    echo "--new-revision 			Specify new revision"
-                    echo "--pom-location 			Main POM file location"
-                    echo "--no-schemas              Do not upgrade the schemas"
+                    echo "-h, --help          Show brief help"
+                    echo "--new-version       Specify new revision"
+                    echo "--pom-location      Main POM file location"
+                    echo "--no-schemas        Do not upgrade the schemas"
                     exit 1
                     ;;
-            --new-revision*)
-                    NEWVERSION=`echo $2`
+            --new-version*)
+                    NEWVERSION="$2"
                     shift
                     ;;
             --pom-location*)
-                    POMLOCATION=`echo $2`
+                    POMLOCATION="$2"
                     shift
                     ;;
             --no-schemas*)
@@ -105,12 +108,12 @@ while test $# -gt 0; do
     esac
 done
 
-DIR=`dirname $0`
-BASEDIR=`readlink -f "$DIR"`
-if [ -z $POMLOCATION ]; then
+DIR=$(dirname $0)
+BASEDIR=$(readlink -f "$DIR")
+if [ -z "$POMLOCATION" ]; then
 	POMLOCATION='pom.xml'
 fi
-VERSION=`xsltproc $BASEDIR/extract-gav.xslt $POMLOCATION | cut -d: -f4`
+VERSION=$(xsltproc "$BASEDIR/extract-gav.xslt" "$POMLOCATION" | cut -d: -f4)
 
 echo "Pom file location: $POMLOCATION"
 echo "Current version: $VERSION"
@@ -120,11 +123,11 @@ if [ -z "$NEWVERSION" ]; then
 fi
 
 echo "Changing from $VERSION to $NEWVERSION"
-
-for POM in $(find . -name 'pom.xml'); do
-   sed -i "s/<version>$VERSION<\/version>/<version>$NEWVERSION<\/version>/g" $POM
+while IFS= read -r -d '' POM
+do
+   sed -i "s/<version>$VERSION<\/version>/<version>$NEWVERSION<\/version>/g" "$POM"
    git add "$POM"
-done
+done < <(find . -name 'pom.xml' -type f -print0)
 
 sed -i "s/<version.infinispan>$VERSION<\/version.infinispan>/<version.infinispan>$NEWVERSION<\/version.infinispan>/g" build/configuration/pom.xml
 sed -i "s/<infinispan.codename>.*<\/infinispan.codename>/<infinispan.codename>N\/A<\/infinispan.codename>/g" build/configuration/pom.xml
@@ -153,19 +156,19 @@ if [ "$OLDSCHEMAVERSION" != "$NEWSCHEMAVERSION" ] && [ "$PROCESS_SCHEMAS" = true
     addNamespace "$OLDSCHEMAMAJOR" "$OLDSCHEMAMINOR" "$NEWSCHEMAMAJOR" "$NEWSCHEMAMINOR"
 
     # Update the server configurations
-    CONFIGS=$(find server/runtime/src/main/server/server/conf/ -name '*xml')
+    CONFIGS=$(find server/runtime/src/main/server/server/conf/ -name '*.xml' -type -f)
     for CONFIG in $CONFIGS; do
          rewriteXML "$CONFIG" "$OLDSCHEMAMAJOR" "$OLDSCHEMAMINOR" "$NEWSCHEMAMAJOR" "$NEWSCHEMAMINOR"
     done
 
-    # Update the server test configurations
-    CONFIGS=$(find server/tests/ -regex '.*/src/test/resources/[^0-9]*xml')
+    # Update the server test configurations. Ignore configuration files that start with a number
+    CONFIGS=$(find server -regex '.*/src/test/resources/[^0-9].*xml' -type f)
     for CONFIG in $CONFIGS; do
          rewriteXML "$CONFIG" "$OLDSCHEMAMAJOR" "$OLDSCHEMAMINOR" "$NEWSCHEMAMAJOR" "$NEWSCHEMAMINOR"
     done
 
     # Update the server configurations
-    CONFIGS=$(find integrationtests/server-integration/server-integration-commons/ -name '*xml')
+    CONFIGS=$(find integrationtests/server-integration/server-integration-commons/ -name '*.xml' -type f)
     for CONFIG in $CONFIGS; do
          rewriteXML "$CONFIG" "$OLDSCHEMAMAJOR" "$OLDSCHEMAMINOR" "$NEWSCHEMAMAJOR" "$NEWSCHEMAMINOR"
     done
@@ -177,6 +180,4 @@ if [ "$OLDSCHEMAVERSION" != "$NEWSCHEMAVERSION" ] && [ "$PROCESS_SCHEMAS" = true
 
     echo "DONE!"
 fi
-
-
 
