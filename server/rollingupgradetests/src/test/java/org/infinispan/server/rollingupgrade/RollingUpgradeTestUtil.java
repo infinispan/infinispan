2b768b8aebfff527c984bc57edea5b6d42b3c5d7
José Bolina <jose@infinispan.org>
2025-11-10 11:42:26 -0300

    [#16119] Rolling upgrade tests use latest tag

--- server/rollingupgradetests/src/test/java/org/infinispan/server/rollingupgrade/RollingUpgradeTestUtil.java
+++ server/rollingupgradetests/src/test/java/org/infinispan/server/rollingupgrade/RollingUpgradeTestUtil.java
@@ -9,7 +9,7 @@ import java.util.Objects;
 
 public class RollingUpgradeTestUtil {
    public static String getFromVersion() {
-      return System.getProperty(INFINISPAN_ROLLING_UPGRADE_FROM_VERSION, "16.0.0.Dev06");
+      return System.getProperty(INFINISPAN_ROLLING_UPGRADE_FROM_VERSION, "latest");
    }
 
    public static String getToVersion() {
