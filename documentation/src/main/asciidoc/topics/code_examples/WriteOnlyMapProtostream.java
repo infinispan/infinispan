ceae0e1d85aeb6cb797b881a457a288ae68afcc5
Tristan Tarrant <tristan@infinispan.org>
2025-11-14 11:44:32 +0100

    Remove duplicate file with wrong case

--- documentation/src/main/asciidoc/topics/code_examples/WriteOnlyMapProtostream.java
+++ documentation/src/main/asciidoc/topics/code_examples/WriteOnlyMapProtostream.java
@@ -1,16 +1,26 @@
 import org.infinispan.functional.EntryView.*;
 import org.infinispan.functional.FunctionalMap.*;
-import org.infinispan.protostream.annotations.Proto;
+import org.infinispan.protostream.annotations.ProtoFactory;
+import org.infinispan.protostream.annotations.ProtoField;
 
 WriteOnlyMap<String, String> writeOnlyMap = ...
 
-Consumer<WriteEntryView<String>> function = new SetStringConstant<>();
+// Force a function to be Serializable
+Consumer<WriteEntryView<String>> function = new SetStringConstant<>("value1");
 CompletableFuture<Void> writeFuture = writeOnlyMap.eval("key1", function);
 
-@Proto
 class SetStringConstant implements Consumer<WriteEntryView<String>> {
+
+   @ProtoField(1)
+   final String target;
+
+   @ProtoFactory
+   public SetStringConstant(String target) {
+      this.target = target;
+   }
+
    @Override
    public void accept(WriteEntryView<String> view) {
-      view.set("value1");
+      view.set(target);
    }
 }
