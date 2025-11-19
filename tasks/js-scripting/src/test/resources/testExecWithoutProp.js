f884a4368dc9d2e756f107d10ff6aa63f3b2f0de
andreatp <andrea.peruffo1982@gmail.com>
2025-11-19 15:30:50 +0000

    fix

--- tasks/js-scripting/src/test/resources/testExecWithoutProp.js
+++ tasks/js-scripting/src/test/resources/testExecWithoutProp.js
@@ -1,7 +1,8 @@
 // mode=local,language=javascript
 
-function process(user_input) { // TODO: missing system input
+function process(user_input) {
     const cache = from_java.get_cache("script-exec");
     var val = from_java.get(cache, "processValue");
     from_java.put(cache, "processValue", val + ":additionFromJavascript");
+    return val;
 }
