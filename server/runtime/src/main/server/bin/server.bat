4d768f8f4b0191dee3a69c9e5a2d593d0aa1ae63
Tristan Tarrant <tristan@infinispan.org>
2025-11-20 16:27:50 +0100

    [#16206] Fix Windows batch files

--- server/runtime/src/main/server/bin/server.bat
+++ server/runtime/src/main/server/bin/server.bat
@@ -1,15 +1,15 @@
 @echo off
 
-set LOADER_CLASS=org.infinispan.server.loader.Loader
-set MAIN_CLASS=org.infinispan.server.Bootstrap
 set ARGUMENTS=
-set PROCESS_NAME=${infinispan.brand.short-name}-server
+set PROCESS_NAME=infinispan-server
 
 set "DIRNAME=%~dp0%"
 
 call "%DIRNAME%common.bat" %*
 
+FOR %%J IN ("%ISPN_HOME%\lib\infinispan-server-runtime-*.jar") DO (
 "%JAVA%" %JAVA_OPTS% ^
    -Dvisualvm.display.name=%PROCESS_NAME% ^
    "-Dinfinispan.server.home.path=%ISPN_HOME%" ^
-   -classpath "%CLASSPATH%" %LOADER_CLASS% %MAIN_CLASS% %ARGUMENTS%
+   -jar %%J %ARGUMENTS%
+)
