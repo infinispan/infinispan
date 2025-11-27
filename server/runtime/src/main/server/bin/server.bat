5eb94b095c136bfebf395e127ae84547ecbbd646
Tristan Tarrant <tristan@infinispan.org>
2025-11-27 10:11:24 +0100

    [#16263] Enable heap dumps in server by default

--- server/runtime/src/main/server/bin/server.bat
+++ server/runtime/src/main/server/bin/server.bat
@@ -1,15 +1,18 @@
 @echo off
-
+setlocal
 set ARGUMENTS=
 set PROCESS_NAME=infinispan-server
 
 set "DIRNAME=%~dp0%"
 
+if exist "%DIRNAME%server-conf.bat" call "%DIRNAME%server-conf.bat"
+
 call "%DIRNAME%common.bat" %*
 
-FOR %%J IN ("%ISPN_HOME%\lib\infinispan-server-runtime-*.jar") DO (
+for %%J in ("%ISPN_HOME%\lib\infinispan-server-runtime-*.jar") do (
 "%JAVA%" %JAVA_OPTS% ^
    -Dvisualvm.display.name=%PROCESS_NAME% ^
    "-Dinfinispan.server.home.path=%ISPN_HOME%" ^
    -jar %%J %ARGUMENTS%
 )
+endlocal
