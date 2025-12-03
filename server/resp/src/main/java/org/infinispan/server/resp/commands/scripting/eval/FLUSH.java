62f6a6ed04667810110cf5af1ddc7ec1b1306994
Tristan Tarrant <tristan@infinispan.org>
2025-12-03 08:30:07 +0100

    [#16295] RESP: Fix SCRIPT FLUSH security authz

--- server/resp/src/main/java/org/infinispan/server/resp/commands/scripting/eval/FLUSH.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/scripting/eval/FLUSH.java
@@ -3,11 +3,14 @@ package org.infinispan.server.resp.commands.scripting.eval;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
+import org.infinispan.security.Security;
+import org.infinispan.server.core.transport.ConnectionMetadata;
 import org.infinispan.server.resp.AclCategory;
 import org.infinispan.server.resp.Resp3Handler;
 import org.infinispan.server.resp.RespCommand;
 import org.infinispan.server.resp.RespRequestHandler;
 import org.infinispan.server.resp.commands.Resp3Command;
+import org.infinispan.server.resp.scripting.LuaTaskEngine;
 
 import io.netty.channel.ChannelHandlerContext;
 
@@ -25,15 +28,14 @@ public class FLUSH extends RespCommand implements Resp3Command {
    @Override
    public CompletionStage<RespRequestHandler> perform(Resp3Handler handler, ChannelHandlerContext ctx, List<byte[]> arguments) {
       try {
-         return handler.getBlockingManager()
-               .supplyBlocking(() -> {
-                  handler.respServer().luaEngine().scriptFlush();
-                  return null;
-               }, "script flush")
-               .thenApplyAsync(__ -> {
-                  handler.writer().ok();
-                  return handler;
-               }, ctx.channel().eventLoop());
+         return handler.getBlockingManager().supplyBlocking(() -> {
+            LuaTaskEngine engine = handler.respServer().luaEngine();
+            Security.doAs(ConnectionMetadata.getInstance(ctx.channel()).subject(), engine::scriptFlush);
+            return null;
+         }, "script flush").thenApplyAsync(__ -> {
+            handler.writer().ok();
+            return handler;
+         }, ctx.channel().eventLoop());
       } catch (Exception e) {
          handler.writer().customError(e.getMessage());
          return handler.myStage();
