5e2467340e5ef21901d321b444cd99c029aa4a97
José Bolina <jose@infinispan.org>
2026-02-09 16:43:44 -0300

    [#16679] RESP cache only use alias 0 if available
    * The alias is a 1:1 mapping to a cache configuration.
    * Instead of always utilizing alias 0, we verify if it is already in use
      by some other configuration.

--- server/resp/src/test/java/org/infinispan/server/resp/test/RespTestingUtil.java
+++ server/resp/src/test/java/org/infinispan/server/resp/test/RespTestingUtil.java
@@ -10,6 +10,8 @@ import java.util.concurrent.atomic.AtomicInteger;
 import javax.security.auth.Subject;
 
 import org.infinispan.commons.dataconversion.MediaType;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.distribution.ch.impl.RESPHashFunctionPartitioner;
 import org.infinispan.manager.EmbeddedCacheManager;
 import org.infinispan.security.AuthorizationPermission;
 import org.infinispan.server.core.logging.Log;
@@ -43,6 +45,13 @@ public class RespTestingUtil {
    static {
       Map<AuthorizationPermission, Subject> subjects = TestingUtil.makeAllSubjects();
       ADMIN = subjects.get(AuthorizationPermission.ALL);
+   }
+
+   public static ConfigurationBuilder defaultRespConfiguration() {
+      ConfigurationBuilder builder = new ConfigurationBuilder();
+      builder.encoding().key().mediaType(MediaType.APPLICATION_OCTET_STREAM);
+      builder.clustering().hash().keyPartitioner(new RESPHashFunctionPartitioner()).numSegments(256);
+      return builder;
    }
 
    public static RedisClient createClient(long timeout, int port) {
