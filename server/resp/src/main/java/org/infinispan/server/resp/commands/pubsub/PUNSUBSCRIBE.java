17439d481d98d48bb8f645b8ba408fedf3775292
Tristan Tarrant <tristan@infinispan.org>
2026-02-19 10:39:28 +0100

    [#16040] Implement RESP PSUBSCRIBE and PUNSUBSCRIBE commands
    * Also add a few more tests to SUBSCRIBE/UNSUBSCRIBE

--- server/resp/src/main/java/org/infinispan/server/resp/commands/pubsub/PUNSUBSCRIBE.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/pubsub/PUNSUBSCRIBE.java
@@ -3,11 +3,17 @@ package org.infinispan.server.resp.commands.pubsub;
 import java.util.List;
 import java.util.concurrent.CompletionStage;
 
+import org.infinispan.commons.marshall.WrappedByteArray;
+import org.infinispan.commons.util.concurrent.AggregateCompletionStage;
+import org.infinispan.commons.util.concurrent.CompletionStages;
 import org.infinispan.server.resp.AclCategory;
+import org.infinispan.server.resp.Resp3Handler;
 import org.infinispan.server.resp.RespCommand;
 import org.infinispan.server.resp.RespRequestHandler;
 import org.infinispan.server.resp.SubscriberHandler;
 import org.infinispan.server.resp.commands.PubSubResp3Command;
+import org.infinispan.server.resp.commands.Resp3Command;
+import org.infinispan.server.resp.meta.ClientMetadata;
 
 import io.netty.channel.ChannelHandlerContext;
 
@@ -17,16 +23,37 @@ import io.netty.channel.ChannelHandlerContext;
  * @see <a href="https://redis.io/commands/punsubscribe/">PUNSUBSCRIBE</a>
  * @since 14.0
  */
-public class PUNSUBSCRIBE extends RespCommand implements PubSubResp3Command {
+public class PUNSUBSCRIBE extends RespCommand implements Resp3Command, PubSubResp3Command {
+   public static final String NAME = "PUNSUBSCRIBE";
+
    public PUNSUBSCRIBE() {
-      super(-1, 0,0, 0, AclCategory.PUBSUB.mask() | AclCategory.SLOW.mask());
+      super(NAME, -1, 0, 0, 0, AclCategory.PUBSUB.mask() | AclCategory.SLOW.mask());
    }
 
    @Override
    public CompletionStage<RespRequestHandler> perform(SubscriberHandler handler,
                                                       ChannelHandlerContext ctx,
                                                       List<byte[]> arguments) {
-      handler.writer().customError("not implemented yet");
-      return handler.myStage();
+      AggregateCompletionStage<Void> aggregateCompletionStage = CompletionStages.aggregateCompletionStage();
+      if (arguments.isEmpty()) {
+         return handler.punsubscribeAll(ctx);
+      } else {
+         ClientMetadata metadata = handler.respServer().metadataRepository().client();
+         for (byte[] patternArg : arguments) {
+            WrappedByteArray wrappedByteArray = new WrappedByteArray(patternArg);
+            RespCacheListener listener = handler.patternSubscribers().remove(wrappedByteArray);
+            if (listener != null) {
+               aggregateCompletionStage.dependsOn(handler.handleStageListenerError(handler.cache().removeListenerAsync(listener), patternArg, false));
+               metadata.decrementPubSubClients();
+            }
+         }
+      }
+      return handler.sendSubscriptions(ctx, aggregateCompletionStage.freeze(), arguments, false, true);
+   }
+
+   @Override
+   public CompletionStage<RespRequestHandler> perform(Resp3Handler handler, ChannelHandlerContext ctx, List<byte[]> arguments) {
+      SubscriberHandler subscriberHandler = new SubscriberHandler(handler.respServer(), handler);
+      return subscriberHandler.handleRequest(ctx, this, arguments);
    }
 }
