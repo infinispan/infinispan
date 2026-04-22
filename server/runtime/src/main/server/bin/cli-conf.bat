61c774708da97960f46fd2e8d71951f244634bc0
Tristan Tarrant <tristan@infinispan.org>
2026-04-22 19:56:28 +0200

    [#17310] Split JAVA_OPTS into granular category variables

--- server/runtime/src/main/server/bin/cli-conf.bat
+++ server/runtime/src/main/server/bin/cli-conf.bat
@@ -18,17 +18,26 @@ REM PRESERVE_JAVA_OPTS=true
 REM
 REM Specify options to pass to the Java VM.
 REM
+if "%JAVA_OPTS_BASE%" == "" (
+   set "JAVA_OPTS_BASE=-Djava.awt.headless=true -XX:+ExitOnOutOfMemoryError"
+)
+if "%JAVA_OPTS_NETWORK%" == "" (
+   set "JAVA_OPTS_NETWORK=-Djava.net.preferIPv4Stack=true"
+)
+if "%JAVA_OPTS_MEMORY%" == "" (
+   set "JAVA_OPTS_MEMORY=-XX:MetaspaceSize=32M -Xms32m -Xmx128m"
+)
 if "%JAVA_OPTS%" == "" (
-   set "JAVA_OPTS=-Djava.awt.headless=true -Djava.net.preferIPv4Stack=true -XX:+ExitOnOutOfMemoryError -XX:MetaspaceSize=64M -Xms32m -Xmx128m %CLI_JAVA_OPTIONS%"
+   set "JAVA_OPTS=%JAVA_OPTS_BASE% %JAVA_OPTS_NETWORK% %JAVA_OPTS_MEMORY% %CLI_JAVA_OPTIONS%"
 ) else (
    echo "JAVA_OPTS already set in environment; overriding default settings with values: %JAVA_OPTS%"
 )
 
 REM Sample JPDA settings for remote socket debugging
-REM set "JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n"
+REM set "JAVA_OPTS_DEBUG=-agentlib:jdwp=transport=dt_socket,address=8787,server=y,suspend=n"
 
 REM Sample JPDA settings for shared memory debugging
-REM set "JAVA_OPTS=%JAVA_OPTS% -agentlib:jdwp=transport=dt_shmem,server=y,suspend=n,address=infinispan"
+REM set "JAVA_OPTS_DEBUG=-agentlib:jdwp=transport=dt_shmem,server=y,suspend=n,address=infinispan"
 
 REM enable garbage collection logging if not set in environment differently
 if "%GC_LOG%" == "" (
