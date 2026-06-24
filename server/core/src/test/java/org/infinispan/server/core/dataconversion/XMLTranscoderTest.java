adfc0401afe456ff987359a47779c8d342229536
Tristan Tarrant <tristan@infinispan.org>
2026-06-24 14:19:19 +0200

    [#17691] Restore APPLICATION_UNKNOWN and APPLICATION_OCTET_STREAM transcoding support
    - JsonTranscoder/XMLTranscoder: restore APPLICATION_OCTET_STREAM and APPLICATION_UNKNOWN
      destination handling removed in 48281f43b57
    - CacheResourceV2, BaseRemoteQueryManager, DefaultIterationManager: treat
      APPLICATION_OCTET_STREAM the same as APPLICATION_UNKNOWN

--- server/core/src/test/java/org/infinispan/server/core/dataconversion/XMLTranscoderTest.java
+++ server/core/src/test/java/org/infinispan/server/core/dataconversion/XMLTranscoderTest.java
@@ -3,6 +3,8 @@ package org.infinispan.server.core.dataconversion;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.util.Collections.singletonList;
 import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_OBJECT;
+import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_OCTET_STREAM;
+import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_UNKNOWN;
 import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_WWW_FORM_URLENCODED;
 import static org.infinispan.commons.dataconversion.MediaType.APPLICATION_XML;
 import static org.infinispan.commons.dataconversion.MediaType.TEXT_PLAIN;
@@ -39,6 +41,18 @@ public class XMLTranscoderTest {
    public void testWWWFormUrlEncoded() {
       byte[] transcoded = (byte[]) xmlTranscoder.transcode("%3Cstring%3EHello%20World%21%3C%2Fstring%3E", APPLICATION_WWW_FORM_URLENCODED, APPLICATION_XML);
       assertEquals("<string>Hello World!</string>", new String(transcoded));
+   }
+
+   public void testXmlToOctetStream() {
+      byte[] xml = "<root><key>value</key></root>".getBytes(UTF_8);
+      byte[] result = (byte[]) xmlTranscoder.transcode(xml, APPLICATION_XML, APPLICATION_OCTET_STREAM);
+      assertArrayEquals(xml, result);
+   }
+
+   public void testXmlToUnknown() {
+      byte[] xml = "<root><key>value</key></root>".getBytes(UTF_8);
+      byte[] result = (byte[]) xmlTranscoder.transcode(xml, APPLICATION_XML, APPLICATION_UNKNOWN);
+      assertArrayEquals(xml, result);
    }
 
    public void testTextToXML() {
