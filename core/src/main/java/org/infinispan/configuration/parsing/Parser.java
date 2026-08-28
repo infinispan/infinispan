5b784e89186fd96d84c277593719b113762c8b97
José Bolina <jose@infinispan.org>
2026-08-28 12:39:31 -0300

    [#18013] Fix immutable configuration storage parsing

--- core/src/main/java/org/infinispan/configuration/parsing/Parser.java
+++ core/src/main/java/org/infinispan/configuration/parsing/Parser.java
@@ -1265,6 +1265,8 @@ public class Parser extends CacheParser {
                if (storage != null) {
                   throw ParseUtils.unexpectedElement(reader);
                }
+               ParseUtils.requireNoAttributes(reader);
+               ParseUtils.requireNoContent(reader);
                storage = ConfigurationStorage.IMMUTABLE;
                break;
             }
